3b2bf02215400bebc2bb76714c2879591f6bf6ab
Ken Haste Andersen <ken@haste.dk>
2002-02-28 17:29:38 +0000

    Re-added Makefile.in

--- mixxx/config.h
+++ mixxx/config.h
@@ -1,54 +1,83 @@
 /* config.h.  Generated automatically by configure.  */
 /* config.h.in.  Generated automatically from configure.in by autoheader.  */
 
-/* The number of bytes in a char.  */
-#define SIZEOF_CHAR 1
-
-/* The number of bytes in a char *.  */
-#define SIZEOF_CHAR_P 4
-
-/* The number of bytes in a int.  */
-#define SIZEOF_INT 4
-
-/* The number of bytes in a long.  */
-#define SIZEOF_LONG 4
-
-/* Define if you have the res_init function.  */
-#define HAVE_RES_INIT 1
-
-/* Define if you have the <pthread/linuxthreads/pthread.h> header file.  */
-/* #undef HAVE_PTHREAD_LINUXTHREADS_PTHREAD_H */
-
-/* Name of package */
-#define PACKAGE "mixxx"
-
-/* Version number of package */
-#define VERSION "0.1"
-
-/* C++ compiler supports template repository */
-#define HAVE_TEMPLATE_REPOSITORY 1
-
 /* Defines if your system has the crypt function */
 #define HAVE_CRYPT 1
 
-/* Define the real type of socklen_t */
-/* #undef socklen_t */
-
-/* Compatibility define */
-#define ksize_t socklen_t
-
-/* Define if you have libz */
-#define HAVE_LIBZ 1
-
-/* Define if you want Xinerama support */
-/* #undef HAVE_XINERAMA */
-
-/* Define if you have libpng */
-#define HAVE_LIBPNG 1
+/* Define if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
 
 /* Define if you have libjpeg */
 #define HAVE_LIBJPEG 1
 
+/* Define if you have libpng */
+#define HAVE_LIBPNG 1
+
 /* Define if you have a working libpthread (will enable threaded code) */
 #define HAVE_LIBPTHREAD 1
 
+/* Define if you have libz */
+#define HAVE_LIBZ 1
+
+/* Define if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the <pthread/linuxthreads/pthread.h> header file. */
+/* #undef HAVE_PTHREAD_LINUXTHREADS_PTHREAD_H */
+
+/* Define if you have the res_init function */
+#define HAVE_RES_INIT 1
+
+/* Define if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* C++ compiler supports template repository */
+#define HAVE_TEMPLATE_REPOSITORY 1
+
+/* Define if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if you want Xinerama support */
+/* #undef HAVE_XINERAMA */
+
+/* Name of package */
+#define PACKAGE "mixxx"
+
+/* The size of a `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of a `char *', as computed by sizeof. */
+#define SIZEOF_CHAR_P 4
+
+/* The size of a `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of a `long', as computed by sizeof. */
+#define SIZEOF_LONG 4
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Compatibility define */
+#define ksize_t socklen_t
+
+/* Define the real type of socklen_t */
+/* #undef socklen_t */
