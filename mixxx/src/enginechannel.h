323b0dd8b171dd3c960aa068f440e8dbe48d29ee
tuehaste <>
2002-04-28 20:31:19 +0000

    Engine channel added

---
+++ mixxx/src/enginechannel.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                          enginechannel.h  -  description
+                             -------------------
+    begin                : Sun Apr 28 2002
+    copyright            : (C) 2002 by 
+    email                : 
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef ENGINECHANNEL_H
+#define ENGINECHANNEL_H
+
+#include "engineobject.h"
+#include "engineclipping.h"
+#include "enginepregain.h"
+#include "enginefilterlbh.h"
+#include "controllogpotmeter.h"
+#include "dlgchannel.h"
+#include "midiobject.h"
+
+/**
+  *@author 
+  */
+
+class EngineChannel : public EngineObject  {
+public:
+	EngineChannel(DlgChannel *dlg, MidiObject *midi, int midiPregain, int midiLow,
+                  int midiBand, int midiHigh, int midiVolume);
+	~EngineChannel();
+    CSAMPLE *process(const CSAMPLE *, const int);
+private:
+    EnginePregain* pregain;
+    EngineFilterLBH* filter;
+    EngineClipping* clipping;
+    EnginePregain* volume;
+};
+
+#endif
