0277e00f5b75c519d41b162e6abaa14f70993ca4
kenand <>
2002-07-15 20:55:34 +0000

    Fixed 7th bit check in midi again.

--- mixxx/src/midiobjectalsa.cpp
+++ mixxx/src/midiobjectalsa.cpp
@@ -66,7 +66,7 @@ void MidiObjectALSA::run()
             int no = snd_rawmidi_read(handle,&buffer[0],1);
             if (no != 1)
                 qWarning("Warning: midiobject recieved %i bytes.", no);
-        } while (buffer[0] & 128 != 128);
+        } while ((buffer[0] & 128) != 128);
 
         // and then get the following 2 bytes:
         char channel;
