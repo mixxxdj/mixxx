9304e54675510a6f4599a755a5eae238d0fcaa60
Tue Haste Andersen <tue@haste.dk>
2003-02-27 08:57:56 +0000

    Changes in controller mappings.

---
+++ mixxx/src/controleventmidi.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+                          controleventmidi.cpp  -  description
+                             -------------------
+    begin                : Thu Feb 20 2003
+    copyright            : (C) 2003 by Tue & Ken Haste Andersen
+    email                : haste@diku.dk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "controleventmidi.h"
+
+ControlEventMidi::ControlEventMidi(char channel, char control, char value) : QCustomEvent(10001), mchannel(channel), mcontrol(control), mvalue(value)
+{
+};
+
+ControlEventMidi::~ControlEventMidi()
+{
+};
+
+char ControlEventMidi::channel() const
+{
+    return mchannel;
+};
+
+char ControlEventMidi::control() const
+{
+    return mcontrol;
+};
+
+char ControlEventMidi::value() const
+{
+    return mvalue;
+};
