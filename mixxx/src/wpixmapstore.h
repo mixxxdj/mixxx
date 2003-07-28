d1f6219d1a306c97fe555aa88caa2c635264fda9
tuehaste <>
2003-07-28 14:19:36 +0000

    Pixmap storage class

---
+++ mixxx/src/wpixmapstore.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+                          wpixmapstore.h  -  description
+                             -------------------
+    begin                : Mon Jun 28 2003
+    copyright            : (C) 2003 by Tue & Ken Haste Andersen
+    email                : haste@diku.dk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef WPIXMAPSTORE_H
+#define WPIXMAPSTORE_H
+
+#include <qpixmap.h>
+
+/**
+  *
+  *@author Tue & Ken Haste Andersen
+  */
+
+typedef struct {
+	QPixmap *pixmap;
+	QString path;
+} PixmapInfoType;
+
+class WPixmapStore {
+public: 
+    WPixmapStore();
+    static QPixmap *getPixmap(const QString &fileName);
+private:
+	/** List of pixmaps already instantiated */
+	static QPtrList<PixmapInfoType> list;
+};
+
+#endif
