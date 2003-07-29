a77e3bd35e0a9ba74aeab57589632d8870b74115
Tue Haste Andersen <tue@haste.dk>
2003-07-29 08:05:02 +0000

    ParseHeader fix

--- mixxx/src/soundsourcemp3.cpp
+++ mixxx/src/soundsourcemp3.cpp
@@ -249,10 +249,13 @@ int SoundSourceMp3::ParseHeader(TrackInfoObject *Track)
             getField(tag,"TIT2",Track->m_sTitle);
             getField(tag,"TPE1",Track->m_sArtist);
 
+            /*
+            // On some tracks this segfaults. TLEN is very seldom used anyway...
             QString dur;
             getField(tag,"TLEN",dur);
             if (dur.length()>0)
                 Track->m_iDuration = dur.toInt();
+            */
         }
         id3_file_close(fh);
     } 
@@ -351,9 +354,18 @@ int SoundSourceMp3::ParseHeader(TrackInfoObject *Track)
 void SoundSourceMp3::getField(id3_tag *tag, const char *frameid, QString str)
 {
     id3_frame *frame = id3_tag_findframe(tag, frameid, 0);
-    if (frame!=0)
+    if (frame)
     {
-        id3_utf16_t *framestr = id3_ucs4_utf16duplicate(id3_field_getstrings(&frame->fields[1],0));
+        /*
+        // Latin1 handling
+        union id3_field const *field = &frame->fields[1];
+        const char *s = id3_ucs4_latin1duplicate(id3_field_getstrings(field, 0));
+        str = s;
+        delete [] s;
+        */
+        
+        // Unicode handling.
+        id3_utf16_t *framestr = id3_ucs4_utf16duplicate(id3_field_getstrings(&frame->fields[1], 0));
         int strlen = 0; while (framestr[strlen]!=0) strlen++;
         if (strlen>0)
             str.setUnicodeCodes((ushort *)framestr,strlen);
