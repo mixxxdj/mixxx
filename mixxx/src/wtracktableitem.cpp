6783e94173a9702a2c0b498bd76ee8e2d7390659
tuehaste <>
2004-05-05 04:05:27 +0000

    BPM confidence added as color code in playlist

--- mixxx/src/wtracktableitem.cpp
+++ mixxx/src/wtracktableitem.cpp
@@ -22,9 +22,12 @@
 #include <qcolor.h>
 #include <qrect.h>
 #include <qstring.h>
+#include "mathstuff.h"
 
 QColor WTrackTableItem::kqRowColor1;
 QColor WTrackTableItem::kqRowColor2;
+QColor WTrackTableItem::kqBpmConfidenceColor1;
+QColor WTrackTableItem::kqBpmConfidenceColor2;
 
 WTrackTableItem::WTrackTableItem(TrackInfoObject *pTrackInfoObject, QTable *table, EditType et, const QString &text, enumType eType) : QTableItem(table, et, text)
 {
@@ -42,17 +45,38 @@ void WTrackTableItem::setRowColors(QColor r1, QColor r2)
     kqRowColor2 = r2;
 }
 
+void WTrackTableItem::setBpmConfidenceColors(QColor c1, QColor c2)
+{
+    kqBpmConfidenceColor1 = c1;
+    kqBpmConfidenceColor2 = c2;
+}
+
 void WTrackTableItem::paint(QPainter *p, const QColorGroup &cg, const QRect &cr, bool selected)
 {
     QColorGroup g( cg );
     // last row is the sum row - we want to make it more visible by
-    // using a red background
-//    g.setColor( QColorGroup::Text, QColor(0,0,0));
+    // using a red#9EFF75 background
+    //g.setColor( QColorGroup::Text, QColor(0,0,0));
+    
+    // Set row color
     if ((row()/2)*2 == row())
         g.setColor( QColorGroup::Base, kqRowColor1);
     else
         g.setColor( QColorGroup::Base, kqRowColor2);
 
+    // Set background color on BPM field based on coinfidence
+    if (col()==COL_BPM)
+    {
+        float scale = max(0., min(1., m_pTrackInfoObject->getBpmConfidence()/(float)1e10));
+        
+        QColor conf;
+        conf.setRgb(kqBpmConfidenceColor1.red()+scale*(kqBpmConfidenceColor2.red()-kqBpmConfidenceColor1.red()),
+                    kqBpmConfidenceColor1.green()+scale*(kqBpmConfidenceColor2.green()-kqBpmConfidenceColor1.green()),
+                    kqBpmConfidenceColor1.blue()+scale*(kqBpmConfidenceColor2.blue()-kqBpmConfidenceColor1.blue()));
+        
+        g.setColor(QColorGroup::Base, conf);
+    }
+        
     QTableItem::paint( p, g, cr, selected);
 }
 /*
