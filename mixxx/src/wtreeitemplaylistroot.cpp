b2d4e096f829e37a53853089f05f54d8e952ec4d
Tue Haste Andersen <tue@haste.dk>
2004-06-01 05:18:36 +0000

    Added new files needed in playlist handling

---
+++ mixxx/src/wtreeitemplaylistroot.cpp
@@ -0,0 +1,37 @@
+//
+// C++ Implementation: wtreeitemplaylistroot
+//
+// Description: 
+//
+//
+// Author: Tue Haste Andersen <haste@diku.dk>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include <qpopupmenu.h>
+#include <qcursor.h>
+#include "wtreeitemplaylistroot.h"
+#include "track.h"
+
+WTreeItemPlaylistRoot::WTreeItemPlaylistRoot(QListView *parent, const QString &s1) : WTreeItem(parent, s1)
+{
+}
+
+WTreeItemPlaylistRoot::~WTreeItemPlaylistRoot()
+{
+}
+
+void WTreeItemPlaylistRoot::popupMenu()
+{
+    QPopupMenu *menu = new QPopupMenu();
+    menu->insertItem("New", spTrack, SLOT(slotNewPlaylist()));
+    menu->insertItem("Import", spTrack, SLOT(slotImportPlaylist()));
+    menu->exec(QCursor::pos());
+}
+
+void WTreeItemPlaylistRoot::drag(QWidget *viewport)
+{
+}
+
---
+++ mixxx/src/wtreeitemplaylistroot.h
@@ -0,0 +1,31 @@
+//
+// C++ Interface: wtreeitemplaylistroot
+//
+// Description: 
+//
+//
+// Author: Tue Haste Andersen <haste@diku.dk>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef WTREEITEMPLAYLISTROOT_H
+#define WTREEITEMPLAYLISTROOT_H
+
+#include "wtreeitem.h"
+
+
+/**
+@author Tue Haste Andersen
+*/
+class WTreeItemPlaylistRoot : public WTreeItem
+{
+public:
+    WTreeItemPlaylistRoot(QListView *parent, const QString &s1);
+    ~WTreeItemPlaylistRoot();
+
+    void popupMenu();
+    void drag(QWidget *viewport); 
+};
+
+#endif
