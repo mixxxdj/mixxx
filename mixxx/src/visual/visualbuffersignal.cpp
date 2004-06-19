a82621190fd725e5ea12036707832e7bb0223688
Tue Haste Andersen <tue@haste.dk>
2004-06-19 00:22:15 +0000

    Changed back to line strip

--- mixxx/src/visual/visualbuffersignal.cpp
+++ mixxx/src/visual/visualbuffersignal.cpp
@@ -90,5 +90,5 @@ void VisualBufferSignal::draw(GLfloat *p, int iLen, float)
 {
     glVertexPointer(3, GL_FLOAT, 0, p);
     glEnableClientState(GL_VERTEX_ARRAY);
-    glDrawArrays(GL_QUAD_STRIP,0,iLen);
+    glDrawArrays(GL_LINE_STRIP,0,iLen);
 }
