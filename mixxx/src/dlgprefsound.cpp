73d2a62a1d14e65e2c80a5c3245e9e3ee4e54060
Tue Haste Andersen <tue@haste.dk>
2004-10-13 08:02:12 +0000

    Fixed error in latency calculation

--- mixxx/src/dlgprefsound.cpp
+++ mixxx/src/dlgprefsound.cpp
@@ -220,7 +220,7 @@ void DlgPrefSound::slotApply()
 
 void DlgPrefSound::slotQueryLatency()
 {
-    int iLatencyMsec = (int)ceil(1000.*((float)Player::getBufferSize()/(float)EngineObject::getPlaySrate()));
+    int iLatencyMsec = (int)ceil(1000.*((float)Player::getBufferSize()/((float)EngineObject::getPlaySrate()*2.)));
     qDebug("got latency msec %i, buffer size %i",iLatencyMsec,Player::getBufferSize());
     
     // Only correct latency slider if it's more than two milliseconds off the actual value.
--- mixxx/src/playerportaudio.cpp
+++ mixxx/src/playerportaudio.cpp
@@ -129,7 +129,7 @@ bool PlayerPortAudio::open()
 
     // Setup latency
     int iFramesPerBuffer;
-    int iLatency = (int)(iSrate*(m_pConfig->getValueString(ConfigKey("[Soundcard]","Latency")).toFloat()/1000.));
+    int iLatency = (int)((float)iSrate*2.*(m_pConfig->getValueString(ConfigKey("[Soundcard]","Latency")).toFloat()/1000.));
 
     // Apply simple rule to determine number of buffers
     if (iLatency/kiMaxFrameSize<2)
@@ -322,7 +322,7 @@ void PlayerPortAudio::setDefaults()
     }
 
     // Set currently used latency in config database
-    int msec = 1000*(2*1024)/(*it).toInt();
+    int msec = (int)(1000.*(2.*1024.)/(2.*(float)(*it).toInt()));
     m_pConfig->set(ConfigKey("[Soundcard]","Latency"), ConfigValue(msec));
 }
 
