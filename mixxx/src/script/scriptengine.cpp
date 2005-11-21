f55f8a7b32379176fc883a234620f623f041638f
Adam Davison <adamd@mixxx.org>
2005-11-21 23:12:48 +0000

    Fixed segmentation fault on playing back empty script

--- mixxx/src/script/scriptengine.cpp
+++ mixxx/src/script/scriptengine.cpp
@@ -16,6 +16,10 @@ ScriptEngine::~ScriptEngine() {
 }
 
 void ScriptEngine::executeScript(const char* script) {
+	// Don't execute a null script
+	if (script == 0x0) {
+		return;
+	}
 	// For now just call lua, but this layer is here in anticipation of not
 	// necessarily wanting to use it
 	m_lua->executeScript(script);
