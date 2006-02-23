6059da47cdda3595af35540de5ea2d2123ffdd9b
Adam Davison <adamd@mixxx.org>
2006-02-23 15:33:06 +0000

    Added these as part of python update

---
+++ mixxx/src/script/playinterface.h
@@ -0,0 +1,57 @@
+#ifndef SCRIPT_PLAYINTERFACE_H
+#define SCRIPT_PLAYINTERFACE_H
+
+#include "scriptcontrolqueue.h"
+#include "qvaluelist.h"
+#include "qdatetime.h"
+
+class PlayInterface {
+	public:
+		PlayInterface(ScriptControlQueue* q);
+		~PlayInterface();
+		void setProcess(int);
+		void clearProcess();
+		void doStartFade(const char* group, const char* name, \
+				int interp);	
+		// Functions for scripts to call below here
+		void stop(int channel);
+		void play(int channel);
+		void setFader(double fade);
+		void test();
+
+		void setTag(int tag);
+		void clearTag();
+	
+		void kill();
+		void killTag(int tag);
+		
+		double getFader();
+		double getValue(const char* group, const char* name);
+		
+		void startFadeCrossfader();
+		void startList(const char* group, const char* name);
+		void startFade(const char* group, const char* name);
+		void point(int time, double value);
+		void fadePoint(int time, double value);
+		void endFade();
+		void endList();
+
+		void playChannel1(int time, char* path);
+		void playChannel2(int time, char* path);
+
+
+	private:
+		ScriptControlQueue* m_q;
+		
+		QDateTime m_time;
+		QValueList<int>* m_times;
+		QValueList<double>* m_values;
+		
+		const char* m_group;
+		const char* m_name;
+		int m_interp;
+		int m_process;
+		int m_tag;
+};
+
+#endif
