3cb85980850be7b2a53abe553be7a4e44458ebb6
ironstorm <>
2006-08-24 02:15:53 +0000

    * works with Hercule DJ Console MK2  * fixed control scale to be 0..255 on bass, mid, treb, etc  * reverted pitch control back to pitch knob, added PitchChange method to limit pitch change  * fixed volume sliders, crossfader  * TODO: fix the JogDials to do something like a scratch (they do a temporary pitch bend now :\)  * TODO: reset m_iPitchOffsetLeft or m_iPitchOffsetRight value to -9999 when mouse/keyboard adjusts pitch slider (see PitchChange method header)  * TODO: figure out how to get the LEDs working

--- mixxx/src/herculeslinux.h
+++ mixxx/src/herculeslinux.h
@@ -15,6 +15,16 @@
  *                                                                         *
  ***************************************************************************/
 
+/*
+ * =========Version History=============
+ * Version 1.50 Wed Aug 23 2006 - modfications by Garth Dahlstrom <ironstorm@users.sf.net>
+ * - works with Hercule DJ Console MK2
+ * - reverted pitch control back to pitch knob, added PitchChange method to limit pitch change
+ * - fixed volume sliders, crossfader
+ * - TODO: reset m_iPitchOffsetLeft or m_iPitchOffsetRight value to -9999 when mouse/keyboard adjusts pitch slider
+ *
+ */
+
 #ifndef HERCULESLINUX_H
 #define HERCULESLINUX_H
 
@@ -28,57 +38,62 @@
   * Linux code for handling the Hercules DJ console.
   *
   *@author Tue Haste Andersen
+  *@author Garth Dahlstrom <ironstorm@users.sf.net>
+  *  - Added Hercules DJ Console MK2 support to Mixxx 1.5.0 (see also herculeslinux.cpp)
+  *  - Applied Hercules Control codes orginially mapped out by Eric J. Shattow <shadow@serverart.org> & Oliver M. Bolzer <oliver@fakeroot.net> in their DJConsole-mod.rb ruby script
   */
 
 const int kiHerculesNumValidPrefixes = 3;
 static QString kqHerculesValidPrefix[kiHerculesNumValidPrefixes] =
 {
-    "Hercules Hercules DJ Console",
     "Hercules Hercules DJ Console Mk2",
+    "Hercules Hercules DJ Console",
     "Hercules Hercules DJ Control MP3" // Not sure about this one
 };
 const int kiHerculesNumEventDevices = 16;
 
-const int kiHerculesLeftTreble = 5;
-const int kiHerculesLeftMiddle = 4;
-const int kiHerculesLeftBass = 3;
-const int kiHerculesLeftVolume = 9;
-const int kiHerculesLeftPitch = 7;
-const int kiHerculesLeftJog = 11;
-const int kiHerculesLeftBtnHeadphone = 308;
-const int kiHerculesLeftBtnPitchBendMinus = 307;
-const int kiHerculesLeftBtnPitchBendPlus = 306;
-const int kiHerculesLeftBtnTrackPrev = 298;
-const int kiHerculesLeftBtnTrackNext = 299;
-const int kiHerculesLeftBtnCue = 296;
-const int kiHerculesLeftBtnPlay = 295;
-const int kiHerculesLeftBtnAutobeat = 297;
-const int kiHerculesLeftBtnMasterTempo = 309;
-const int kiHerculesLeftBtn1 = 302;
-const int kiHerculesLeftBtn2 = 301;
-const int kiHerculesLeftBtn3 = 300;
-const int kiHerculesLeftBtnFx = 294;
-const int kiHerculesRightTreble = 6;
-const int kiHerculesRightMiddle = 1;
-const int kiHerculesRightBass = 0;
-const int kiHerculesRightVolume = 10;
-const int kiHerculesRightPitch = 8;
-const int kiHerculesRightJog = 12;
-const int kiHerculesRightBtnHeadphone = 312;
-const int kiHerculesRightBtnPitchBendMinus = 311;
-const int kiHerculesRightBtnPitchBendPlus = 310;
-const int kiHerculesRightBtnTrackPrev = 292;
-const int kiHerculesRightBtnTrackNext = 293;
-const int kiHerculesRightBtnCue = 290;
-const int kiHerculesRightBtnPlay = 289;
-const int kiHerculesRightBtnAutobeat = 291;
-const int kiHerculesRightBtnMasterTempo = 313;
-const int kiHerculesRightBtn1 = 303;
-const int kiHerculesRightBtn2 = 304;
-const int kiHerculesRightBtn3 = 305;
-const int kiHerculesRightBtnFx = 288;
-const int kiHerculesCrossfade = 2;
+const int kiHerculesLeftTreble = 0x05;
+const int kiHerculesLeftMiddle = 0x04;
+const int kiHerculesLeftBass = 0x03;
+const int kiHerculesLeftVolume = 0x29;
+const int kiHerculesLeftPitch = 0x2b;
+const int kiHerculesLeftJog = 0x2d;
+const int kiHerculesLeftBtnHeadphone = 0x34 + 0x100; // Left Headphones
+const int kiHerculesLeftBtnPitchBendMinus = 0x33 + 0x100; // Left PitchDec
+const int kiHerculesLeftBtnPitchBendPlus = 0x32 + 0x100; // Left PitchInc
+const int kiHerculesLeftBtnTrackPrev = 0x2a + 0x100; // Left TrackDec
+const int kiHerculesLeftBtnTrackNext = 0x2b + 0x100; // Left TrackInc
+const int kiHerculesLeftBtnCue = 0x28 + 0x100; // Left Cue
+const int kiHerculesLeftBtnPlay = 0x27 + 0x100; // Left Play
+const int kiHerculesLeftBtnAutobeat = 0x29 + 0x100; // Left AutoBeat
+const int kiHerculesLeftBtnMasterTempo = 0x35 + 0x100; // Left Tempo
+const int kiHerculesLeftBtn1 = 0x2e + 0x100; // Left Fx1
+const int kiHerculesLeftBtn2 = 0x2d + 0x100; // Left Fx2
+const int kiHerculesLeftBtn3 = 0x2c + 0x100; // Left Fx3
+const int kiHerculesLeftBtnFx = 0x26 + 0x100; // Left FxArrow
 
+const int kiHerculesRightTreble = 0x6;
+const int kiHerculesRightMiddle = 0x1;
+const int kiHerculesRightBass = 0x0;
+const int kiHerculesRightVolume = 0x2a;
+const int kiHerculesRightPitch = 0x2c;
+const int kiHerculesRightJog = 0x2e;
+const int kiHerculesRightBtnHeadphone = 0x38 + 0x100; // Right Headphones
+const int kiHerculesRightBtnPitchBendMinus = 0x37 + 0x100; // Right PitchDec
+const int kiHerculesRightBtnPitchBendPlus = 0x36 + 0x100; // Right PitchInc
+const int kiHerculesRightBtnTrackPrev = 0x24 + 0x100; // Right TrackDec
+const int kiHerculesRightBtnTrackNext = 0x25 + 0x100; // Right TrackInc
+const int kiHerculesRightBtnCue = 0x22 + 0x100; // Right Cue
+const int kiHerculesRightBtnPlay = 0x21 + 0x100; // Right Play
+const int kiHerculesRightBtnAutobeat = 0x23 + 0x100; // Right AutoBeat
+const int kiHerculesRightBtnMasterTempo = 0x39 + 0x100; // Right Tempo
+const int kiHerculesRightBtn1 = 0x2f + 0x100; // Right Fx1
+const int kiHerculesRightBtn2 = 0x30 + 0x100; // Right Fx2
+const int kiHerculesRightBtn3 = 0x31 + 0x100; // Right Fx3
+const int kiHerculesRightBtnFx = 0x20 + 0x100; // Right FxArrow
+const int kiHerculesCrossfade = 0x28;
+
+// TODO: 1.5.0+ investigate and revise led codes
 const int kiHerculesLedRightSync = 0;
 const int kiHerculesLedLeftLoop = 1;
 const int kiHerculesLedRightLoop = 2;
@@ -120,7 +135,12 @@ protected:
     /** File set used in select() call */
     fd_set fdset;
     int m_iJogLeft, m_iJogRight;
+
+    int m_iPitchOffsetLeft, m_iPitchOffsetRight;
+    int m_iPitchLeft, m_iPitchRight;
     double m_dJogLeftOld, m_dJogRightOld;
+
+    double PitchChange(const QString ControlSide, const int ev_value, int &m_iPitchPrevious, int &m_iPitchOffset);
 };
 
 #endif
