5e41cb3780f4ccf3434c7526adbe8a829eb4cd28
gamegod <>
2007-03-14 02:56:30 +0000

    Added note about cause of nasty drifting bug...

--- mixxx/src/rotary.cpp
+++ mixxx/src/rotary.cpp
@@ -17,6 +17,7 @@
 
 #include "rotary.h"
 #include "mathstuff.h"
+#include "controlobject.h"
 
 Rotary::Rotary()
 {
@@ -35,6 +36,15 @@ Rotary::~Rotary()
     delete [] m_pFilter;
 }
 
+/* Note: There's probably a bug in this function (or this class) somewhere.
+   The filter function seems to be the cause of the "drifting" bug in the Hercules stuff.
+   What happens is that filter() gets called to do some magic to a value that's returned
+   from the Hercules device, and that magic adds "momentum" to it's motion (ie. it doesn't
+   stop dead when you stop spinning the jog wheels.) The problem with this "magic" is that
+   when herculeslinux.cpp passes the filtered value off to the wheel ControlObject (or what
+   have you), the ControlObject's internal value never goes back to zero properly.
+   - Albert (March 13, 2007)
+*/
 double Rotary::filter(double dValue)
 {
     // Update filter buffer
@@ -47,8 +57,8 @@ double Rotary::filter(double dValue)
         dMagnitude += m_pFilter[i];
     }
     dMagnitude /= (double)m_iFilterLength;
-//     qDebug("filter in %f, out %f",dValue,dMagnitude);
-    
+     //qDebug("filter in %e, out %e",dValue,dMagnitude);
+        
     if (m_dLastValue!=dMagnitude)
         return dMagnitude;
     
