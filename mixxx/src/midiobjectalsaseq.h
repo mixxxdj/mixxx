33f0d481cff54e5810f9c2b8b8db439e56091aef
gamegod <>
2007-04-06 22:24:46 +0000

    Added two missing files from merging CTAF's ALSA-seq MIDI code

---
+++ mixxx/src/midiobjectalsaseq.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+                          midiobjectalsaseq.h  -  description
+                             -------------------
+    begin                : Mon Sep 25 2006
+    copyright            : (C) 2006 by Cedric GESTES
+    email                : goctaf@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MIDIOBJECTALSASEQ_H
+#define MIDIOBJECTALSASEQ_H
+
+#include <midiobject.h>
+#include <alsa/asoundlib.h>
+
+/**
+  *@author Cedric GESTES
+  */
+
+class MidiObjectALSASeq : public MidiObject  {
+public: 
+    MidiObjectALSASeq();
+    ~MidiObjectALSASeq();
+    void devOpen(QString device);
+    void devClose();
+protected:
+    void run();
+    
+    snd_seq_t *m_handle;
+    int m_client;
+    int m_input;
+    int m_queue;
+};
+
+#endif
