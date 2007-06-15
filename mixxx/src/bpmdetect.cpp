b9d76865fb9eb8ccadf7aae4a2a761cc8c4110c3
Micah Lee <snipexv@gmail.com>
2007-06-15 18:45:12 +0000

    Switched to the next bpm detection algorithm in the list. This one has been tested in another app and is accurate and fast. It also requires no new dependencies for mixxx.

--- mixxx/src/bpmdetect.cpp
+++ mixxx/src/bpmdetect.cpp
@@ -1,434 +1,311 @@
-#include "bpmdetect.h"
-
-#ifdef __WIN__
-#define _USE_MATH_DEFINES
-#endif
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Beats-per-minute (BPM) detection routine.
+///
+/// The beat detection algorithm works as follows:
+/// - Use function 'inputSamples' to input a chunks of samples to the class for
+///   analysis. It's a good idea to enter a large sound file or stream in smallish
+///   chunks of around few kilosamples in order not to extinguish too much RAM memory.
+/// - Inputted sound data is decimated to approx 500 Hz to reduce calculation burden,
+///   which is basically ok as low (bass) frequencies mostly determine the beat rate.
+///   Simple averaging is used for anti-alias filtering because the resulting signal
+///   quality isn't of that high importance.
+/// - Decimated sound data is enveloped, i.e. the amplitude shape is detected by
+///   taking absolute value that's smoothed by sliding average. Signal levels that
+///   are below a couple of times the general RMS amplitude level are cut away to
+///   leave only notable peaks there.
+/// - Repeating sound patterns (e.g. beats) are detected by calculating short-term 
+///   autocorrelation function of the enveloped signal.
+/// - After whole sound data file has been analyzed as above, the bpm level is 
+///   detected by function 'getBpm' that finds the highest peak of the autocorrelation 
+///   function, calculates it's precise location and converts this reading to bpm's.
+///
+/// Author        : Copyright (c) Olli Parviainen
+/// Author e-mail : oparviai 'at' iki.fi
+/// SoundTouch WWW: http://www.surina.net/soundtouch
+///
+////////////////////////////////////////////////////////////////////////////////
+//
+// Last changed  : $Date: 2006/02/05 16:44:06 $
+// File revision : $Revision: 1.7 $
+//
+// $Id: BPMDetect.cpp,v 1.7 2006/02/05 16:44:06 Olli Exp $
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// License :
+//
+//  SoundTouch audio processing library
+//  Copyright (c) Olli Parviainen
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+////////////////////////////////////////////////////////////////////////////////
 
 #include <math.h>
+#include <assert.h>
+#include <string.h>
+#include "FIFOSampleBuffer.h"
+#include "PeakFinder.h"
+#include "bpmDetect.h"
 
- #ifdef __LINUX__
-	#include <sys/time.h>
-	#include <unistd.h>
+using namespace soundtouch;
+
+#define INPUT_BLOCK_SAMPLES       2048
+#define DECIMATED_BLOCK_SAMPLES   256
+
+typedef unsigned short ushort;
+
+/// decay constant for calculating RMS volume sliding average approximation 
+/// (time constant is about 10 sec)
+const float avgdecay = 0.99986f;
+
+/// Normalization coefficient for calculating RMS sliding average approximation.
+const float avgnorm = (1 - avgdecay);
+
+
+
+BPMDetect::BPMDetect(int numChannels, int sampleRate)
+{
+    xcorr = NULL;
+
+    buffer = new FIFOSampleBuffer();
+
+    decimateSum = 0;
+    decimateCount = 0;
+    decimateBy = 0;
+
+    this->sampleRate = sampleRate;
+    this->channels = numChannels;
+
+    envelopeAccu = 0;
+
+    // Initialize RMS volume accumulator to RMS level of 3000 (out of 32768) that's
+    // a typical RMS signal level value for song data. This value is then adapted
+    // to the actual level during processing.
+#ifdef INTEGER_SAMPLES
+    // integer samples
+    RMSVolumeAccu = (3000 * 3000) / avgnorm;
+#else
+    // float samples, scaled to range [-1..+1[
+    RMSVolumeAccu = (0.092f * 0.092f) / avgnorm;
 #endif
 
-#ifdef __WIN__
-#include <winsock2.h>
-int gettimeofday
-      (struct timeval* tp, void* tzp) {
-    DWORD t;
-    t = timeGetTime();
-    tp->tv_sec = t / 1000;
-    tp->tv_usec = t % 1000;
-    /* 0 indicates success. */
-    return 0;
-}
-#endif
-
-
-using namespace std;
-
-static void incRange(map<double,int>& bpms, double ix)
-{
-	double low=ix-0.25;
-	double high=ix+0.25;
-	map<double,int>::iterator it;
-
-	for(it=bpms.begin();it!=bpms.end();++it)
-	{
-		if((*it).first >= low && (*it).first <= high)
-			(*it).second++;
-	}
+    init(numChannels, sampleRate);
 }
 
-static double findMax(map<double,int>& bpms, int limit=-1)
-{
-	map<double,int>::iterator it;
-	int max=0;
-	double maxbpm=0.0;
 
-	for(it=bpms.begin();it!=bpms.end();++it)
-	{
-		if((*it).second > max && (limit == -1 || (*it).second < limit))
-		{
-			max=(*it).second;
-			maxbpm=(*it).first;
-		}
-	}
-	return maxbpm;
+
+BPMDetect::~BPMDetect()
+{
+    delete[] xcorr;
+    delete buffer;
 }
 
-BpmDetect::BpmDetect(int inrate, unsigned long (*cb)(float *, unsigned long, void *), void *dt, unsigned long slen)
+
+/// low-pass filter & decimate to about 500 Hz. return number of outputted samples.
+///
+/// Decimation is used to remove the unnecessary frequencies and thus to reduce 
+/// the amount of data needed to be processed as calculating autocorrelation 
+/// function is a very-very heavy operation.
+///
+/// Anti-alias filtering is done simply by averaging the samples. This is really a 
+/// poor-man's anti-alias filtering, but it's not so critical in this kind of application
+/// (it'd also be difficult to design a high-quality filter with steep cut-off at very 
+/// narrow band)
+int BPMDetect::decimate(SAMPLETYPE *dest, const SAMPLETYPE *src, int numsamples)
 {
-	buffer_fill=0;
-	input_rate=inrate;
-	measure_rate=11025;
-	sample_length=slen;
-	callback=cb;
-	data=dt;
-	block=0;
+    int count, outcount;
+    LONG_SAMPLETYPE out;
 
-	if(!slen)
-		measured=131072;
-	else
-	{
-		unsigned long atmost=(long long)sample_length*measure_rate/inrate;
-		measured=1;
-		while(measured <= atmost)
-			measured*=2;
-		measured/=2;
-		if(measured > 131072)
-			measured=131072;
-	}
-	audio=new double[measured];
-	en=new double[measured];
-	ts=new double[measured];
-	co=new double[measured];
+    assert(decimateBy != 0);
+    outcount = 0;
+    for (count = 0; count < numsamples; count ++) 
+    {
+        decimateSum += src[count];
 
-	char wisdom[1024];
-	sprintf(wisdom, "%s/.BpmWisdom", getenv("HOME"));
-	FILE *wisdom_file=fopen(wisdom, "r");
-	if(wisdom_file)
-	{
-		fftw_import_wisdom_from_file(wisdom_file);
-		fclose(wisdom_file);
-	}
-
-	forward = fftw_plan_r2r_1d(measured, &(audio[0]), en, FFTW_R2HC, FFTW_MEASURE);
-	backward = fftw_plan_r2r_1d(measured, ts, co, FFTW_HC2R, FFTW_MEASURE);
-
-	wisdom_file=fopen(wisdom, "w");
-	if(wisdom_file)
-	{
-		fftw_export_wisdom_to_file(wisdom_file);
-		fclose(wisdom_file);
-	}
-
-	setLimits(85.0,160.0);
+        decimateCount ++;
+        if (decimateCount >= decimateBy) 
+        {
+            // Store every Nth sample only
+            out = (LONG_SAMPLETYPE)(decimateSum / decimateBy);
+            decimateSum = 0;
+            decimateCount = 0;
+#ifdef INTEGER_SAMPLES
+            // check ranges for sure (shouldn't actually be necessary)
+            if (out > 32767) 
+            {
+                out = 32767;
+            } 
+            else if (out < -32768) 
+            {
+                out = -32768;
+            }
+#endif // INTEGER_SAMPLES
+            dest[outcount] = (SAMPLETYPE)out;
+            outcount ++;
+        }
+    }
+    return outcount;
 }
 
-void BpmDetect::setLimits(double lower_boundary, double higher_boundary)
+
+
+// Calculates autocorrelation function of the sample history buffer
+void BPMDetect::updateXCorr(int process_samples)
 {
-	double freq=higher_boundary/60.0;
-	b1=4*(int)(measure_rate/freq);
-	freq=lower_boundary/60.0;
-	b2=4*(int)(measure_rate/freq);
-	lower=lower_boundary;
-	upper=higher_boundary;
+    int offs;
+    SAMPLETYPE *pBuffer;
+    
+    assert(buffer->numSamples() >= (uint)(process_samples + windowLen));
+
+    pBuffer = buffer->ptrBegin();
+    for (offs = windowStart; offs < windowLen; offs ++) 
+    {
+        LONG_SAMPLETYPE sum;
+        int i;
+
+        sum = 0;
+        for (i = 0; i < process_samples; i ++) 
+        {
+            sum += pBuffer[i] * pBuffer[i + offs];    // scaling the sub-result shouldn't be necessary
+        }
+//        xcorr[offs] *= xcorr_decay;   // decay 'xcorr' here with suitable coefficients 
+                                        // if it's desired that the system adapts automatically to
+                                        // various bpms, e.g. in processing continouos music stream.
+                                        // The 'xcorr_decay' should be a value that's smaller than but 
+                                        // close to one, and should also depend on 'process_samples' value.
+
+        xcorr[offs] += (float)sum;
+    }
 }
 
-BpmDetect::~BpmDetect()
+
+
+// Calculates envelope of the sample data
+void BPMDetect::calcEnvelope(SAMPLETYPE *samples, int numsamples) 
 {
-	delete[] audio;
-	delete[] en;
-	delete[] ts;
-	delete[] co;
+    const float decay = 0.7f;               // decay constant for smoothing the envelope
+    const float norm = (1 - decay);
+
+    int i;
+    LONG_SAMPLETYPE out;
+    float val;
+
+    for (i = 0; i < numsamples; i ++) 
+    {
+        // calc average RMS volume
+        RMSVolumeAccu *= avgdecay;
+        val = (float)fabs((float)samples[i]);
+        RMSVolumeAccu += val * val;
+
+        // cut amplitudes that are below 2 times average RMS volume
+        // (we're interested in peak values, not the silent moments)
+        val -= 2 * (float)sqrt(RMSVolumeAccu * avgnorm);
+        val = (val > 0) ? val : 0;
+
+        // smooth amplitude envelope
+        envelopeAccu *= decay;
+        envelopeAccu += val;
+        out = (LONG_SAMPLETYPE)(envelopeAccu * norm);
+
+#ifdef INTEGER_SAMPLES
+        // cut peaks (shouldn't be necessary though)
+        if (out > 32767) out = 32767;
+#endif // INTEGER_SAMPLES
+        samples[i] = (SAMPLETYPE)out;
+    }
 }
 
-double BpmDetect::run(float *samples, unsigned long sample_count, int *current, int *bound)
+
+
+void BPMDetect::inputSamples(SAMPLETYPE *samples, int numSamples)
 {
-	unsigned long samps=(long long)measured*input_rate/measure_rate;
+    SAMPLETYPE decimated[DECIMATED_BLOCK_SAMPLES];
 
-	if(!samples)
-	{
-		bpms.clear();
+    // convert from stereo to mono if necessary
+    if (channels == 2)
+    {
+        int i;
 
-		if(!callback)
-			return INDETERMINATE;
+        for (i = 0; i < numSamples; i ++)
+        {
+            samples[i] = (samples[i * 2] + samples[i * 2 + 1]) / 2;
+        }
+    }
+    
+    // decimate
+    numSamples = decimate(decimated, samples, numSamples);
 
-		block=new float[samps*2];
+    // envelope new samples and add them to buffer
+    calcEnvelope(decimated, numSamples);
+    buffer->putSamples(decimated, numSamples);
 
-		while(1)
-		{
-			while(buffer_fill < samps)
-			{
-				unsigned long returned=(*callback)(block+buffer_fill*2, samps-buffer_fill, data);
-				if(returned == 0)
-				{
-					delete[] block;
-					block=0;
-					buffer_fill=0;
-					bpms.clear();
-					return INDETERMINATE;
-				}
-				if(returned > (samps-buffer_fill))
-					returned=samps-buffer_fill;
-				buffer_fill+=returned;
-			}
+    // when the buffer has enought samples for processing...
+    if ((int)buffer->numSamples() > windowLen) 
+    {
+        int processLength;
 
-			double bpm=measure();
+        // how many samples are processed
+        processLength = buffer->numSamples() - windowLen;
 
-			if(bpm == 0.0)
-			{
-				buffer_fill=0;
-				continue;
-			}
-
-			bpms[bpm];
-			incRange(bpms, bpm);
-			double top=findMax(bpms);
-			int next_to_last=bpms[findMax(bpms, bpms[top])];
-			if(next_to_last < 2)
-				next_to_last=2;
-			printf("%g\t%d\t%d (%g)\n",top,next_to_last,bpms[top], bpm);
-			if(bpms[top] >= next_to_last*2 && bpms[top]>5)
-			{
-				delete[] block;
-				block=0;
-				buffer_fill=0;
-				bpms.clear();
-				return top;
-			}
-			if(bpms[top] >= 8)
-			{
-				delete[] block;
-				block=0;
-				buffer_fill=0;
-				bpms.clear();
-				return top;
-			}
-
-			buffer_fill=0;
-		}
-	}
-
-	if(callback)
-		return INDETERMINATE;
-	
-	if(sample_count == 0)
-	{
-		bpms.clear();
-		return INDETERMINATE;
-	}
-
-	if(block)
-		block=(float *)realloc(block, sizeof(float)*2*(buffer_fill+sample_count));
-	else
-		block=(float *)malloc(sizeof(float)*2*sample_count);
-	
-	for(int i=0;i<sample_count*2;++i)
-		block[buffer_fill*2+i]=samples[i];
-	buffer_fill+=sample_count;
-
-	if(buffer_fill < samps)
-		return NEED_MORE_DATA;
-
-	double bpm=measure();
-
-	bpms[bpm];
-	incRange(bpms, bpm);
-
-	double top=findMax(bpms);
-	int next_to_last=bpms[findMax(bpms, bpms[top])];
-	if(next_to_last < 2)
-		next_to_last=2;
-	printf("%g\t%d\t%d (%g)\n",top,next_to_last,bpms[top], bpm);
-	int pass=next_to_last*2;
-	if(pass < 5)
-		pass=6;
-	if(pass > 8)
-		pass=8;
-	if(bound)
-		*bound=pass;
-	if(current)
-		*current=bpms[top];
-
-	if(bpms[top] >= next_to_last*2 && bpms[top]>5)
-	{
-		free(block);
-		block=0;
-		buffer_fill=0;
-		bpms.clear();
-		return top;
-	}
-	if(bpms[top] >= 8)
-	{
-		free(block);
-		block=0;
-		buffer_fill=0;
-		bpms.clear();
-		return top;
-	}
-
-	if(buffer_fill-samps)
-	{
-		memmove(&block[0], &block[samps*2], (buffer_fill-samps)*2*sizeof(float));
-		buffer_fill-=samps;
-		block=(float *)realloc(block, sizeof(float)*2*buffer_fill);
-	}
-	else
-	{
-		free(block);
-		block=0;
-		buffer_fill=0;
-	}
-	return NEED_MORE_DATA;
+        // ... calculate autocorrelations for oldest samples...
+        updateXCorr(processLength);
+        // ... and remove them from the buffer
+        buffer->receiveSamples(processLength);
+    }
 }
 
-double BpmDetect::measure()
+
+void BPMDetect::init(int numChannels, int sampleRate)
 {
-	for(long long i=0;i < measured;i++)
-	{
-		long long posa = i*input_rate/measure_rate;
-		long long posb = (i+1)*input_rate/measure_rate;
-		double sum = 0;
+    this->sampleRate = sampleRate;
 
-		unsigned long samps=(long long)measured*input_rate/measure_rate;
-		if(posb>=samps)
-			posb=samps;
+    // choose decimation factor so that result is approx. 500 Hz
+    decimateBy = sampleRate / 500;
+    assert(decimateBy > 0);
+    assert(INPUT_BLOCK_SAMPLES < decimateBy * DECIMATED_BLOCK_SAMPLES);
 
-		for(unsigned long j=posa;j < posb;j++)
-			sum += fabs(block[j<<1])+fabs(block[(j<<1)|1]);
-		audio[i]=sum/(2*(posb-posa));
-	}
+    // Calculate window length & starting item according to desired min & max bpms
+    windowLen = (60 * sampleRate) / (decimateBy * MIN_BPM);
+    windowStart = (60 * sampleRate) / (decimateBy * MAX_BPM);
 
-	int blsi=1;
-	int tmpr=measure_rate;
+    assert(windowLen > windowStart);
 
-	while(tmpr > 50) 
-	{
-		blsi*=2;
-		tmpr/=2;
-	}
+    // allocate new working objects
+    xcorr = new float[windowLen];
+    memset(xcorr, 0, windowLen * sizeof(float));
 
-	energize(audio,measured,blsi);
-
-	double *w=new double[measured];
-
-	for(unsigned int x=0;x < measured;x++)
-		w[x]=audio[x];
-
-	differentiate(w,measured);
-
-	normalize_abs_max(w,measured);
-
-	normalize_abs_max(audio,measured);
-
-	for(unsigned int x=0;x < measured;x++)
-		audio[x]+=w[x];
-
-	delete[] w;
-
-	for(unsigned long x=0;x < measured;x++)
-		audio[x]*= 0.5 - 0.5 * cos(M_PI*2.0*(double)x/(double)measured);
-
-	timeval start, end;
-
-	gettimeofday(&start, 0);
-	gettimeofday(&end, 0);
-
-	end.tv_sec-=start.tv_sec;
-	end.tv_usec+=end.tv_sec*1000000;
-	end.tv_usec-=start.tv_usec;
-	if(end.tv_usec > 500000)
-		return 0.0;
-
-	for(unsigned long x=1;x < measured/2;x++)
-		en[x]=sqrt(en[x]*en[x]+en[measured-x]*en[measured-x]);
-	for(unsigned long x=1;x < measured/2;x++)
-		en[x]/=measured/x;
-	en[0]=0.0;
-
-	for(unsigned long x=0;x < measured/2;x++)
-		ts[x]=en[x];
-
-	for(unsigned long x=measured/2;x < measured;x++)
-		ts[x]=0;
-	fftw_execute(backward);
-
-	for(unsigned long x=1;x < measured;x++)
-		co[x]/=measured-x;
-	co[0]=0;
-
-	double mf = en[0] = 0;
-	int pf = 0;
-
-	normalize_abs_max(en,measured);
-	normalize_abs_max(co,measured);
-
-	for(unsigned long x=1;x < measured/2;x++)
-		co[x]*=en[measured/x];
-	for(unsigned long x=b1;x < b2;x++)
-	{
-		int m = 1;
-		double E = 0;
-
-		while(m <= 2)
-		{
-			E+=co[x*m];
-			E+=co[x/m];
-			m*=2;
-		}
-		if(E > mf || mf == 0.0)
-		{
-			mf=E;
-			pf=x;
-		}
-	}
-
-	double t=4.0*measure_rate*60.0/(double)pf;
-	return t;
+    // we do processing in mono mode
+    buffer->setChannels(1);
+    buffer->clear();
 }
 
-void BpmDetect::differentiate(double * arr, int s)
+
+
+float BPMDetect::getBpm()
 {
-	for(int i=0;i < s-1;i++)
-		arr[i]=arr[i+1]-arr[i];
-	arr[s-1]=-arr[s-1];
-}
+    float peakPos;
+    PeakFinder peakFinder;
 
-double BpmDetect::find_abs_max(double *data, long l)
-{
-	if(l==0)
-		return 0.0;
-	double m=data[0];
-	for(long i=0;i < l;i++)
-		if(fabs(data[i]) > m)
-			m=fabs(data[i]);
-	return m;
-}
+    // find peak position
+    peakPos = peakFinder.detectPeak(xcorr, windowStart, windowLen);
 
-double BpmDetect::normalize_abs_max(double *data, long l)
-{
-	double m=find_abs_max(data,l);
-	if(m > 0)
-		for(long i=0;i < l;i++)
-			data[i]/=m;
-	return m;
-}
+    assert(decimateBy != 0);
+    if (peakPos < 1e-6) return 0.0; // detection failed.
 
-void BpmDetect::energize(double *data, double audiosize, unsigned long fs)
-{
-	float *rr=new float[fs];
-
-	for(unsigned long i=0;i < fs;i++)
-		rr[i]=0;
-
-	double M=0;
-
-	for(unsigned long x=0;x < audiosize;x++)
-	{
-		M-=rr[x%fs];
-		rr[x%fs]=data[x];
-		M+=data[x];
-		double R=M/fs;
-		data[x >= fs ? x-fs : 0]-=R;
-	}
-
-	for(unsigned long x=0;x < audiosize;x++)
-		data[x]*=data[x];
-
-	for(unsigned long i=0;i < fs;i++)
-		rr[i]=0;
-
-	double S = 0;
-
-	for(unsigned long x=0;x < audiosize;x++)
-	{
-		S-=rr[x%fs];
-		rr[x%fs]=data[x];
-		S+=data[x];
-		double R=sqrt(S/fs);
-		data[x >= fs ? x-fs : 0]=R;
-	}
-	delete[] rr;
+    // calculate BPM
+    return 60.0f * (((float)sampleRate / (float)decimateBy) / peakPos);
 }
