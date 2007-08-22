6b94ae2670beb7546852665858d5506e74b9e015
Albert Santoni <alberts@mixxx.org>
2007-08-22 04:26:44 +0000

    ... and the vinyl control stuff too... (how did I forget these files? o_O)

---
+++ mixxx/src/vinylcontrol.h
@@ -0,0 +1,60 @@
+#ifndef __VINYLCONTROL_H__
+#define __VINYLCONTROL_H__
+
+#include <qthread.h>
+#include "configobject.h"
+#include "controlobject.h"
+
+#define MIXXX_VINYL_FINALSCRATCH "Final Scratch (crappy)"
+#define MIXXX_VINYL_MIXVIBESDVSCD "MixVibes DVS CD"
+#define MIXXX_VINYL_SERATOCV02VINYLSIDEA "Serato CV02 Vinyl, Side A"
+#define MIXXX_VINYL_SERATOCV02VINYLSIDEB "Serato CV02 Vinyl, Side B"
+#define MIXXX_VINYL_SERATOCD "Serato CD"
+#define MIXXX_VINYL_TRAKTORSCRATCHSIDEA "Traktor Scratch, side A"
+#define MIXXX_VINYL_TRAKTORSCRATCHSIDEB "Traktor Scratch, side B"
+
+
+//TODO: Make this an EngineObject instead one day? (need to route all the input audio through the engine that way too...)
+
+class VinylControl : public QThread
+{
+    public:
+        VinylControl(ConfigObject<ConfigValue> *pConfig, const char *_group);
+        ~VinylControl();
+    	virtual void ToggleVinylControl(bool enable) = 0;
+    	virtual bool isEnabled() = 0;
+    	/*virtual void syncPitch(double pitch) = 0;
+    	virtual void syncPosition() = 0; */
+    	virtual void AnalyseSamples(short* samples, size_t size) = 0;  
+    	virtual float getSpeed();
+    protected:
+	    virtual void run() = 0;						// main thread loop
+	    
+	    QString strVinylType;	 
+        ConfigObject<ConfigValue> *m_pConfig;	/** Pointer to config database */
+        const char* group;
+    	ControlObject *playButton;		//The ControlObject used to start/stop playback of the song.
+    	ControlObject *playPos;			//The ControlObject used to change the playback position in the song.
+    	ControlObject *controlScratch;	//The ControlObject used to seek when the record is spinning fast.
+    	ControlObject *rateSlider;		//The ControlObject used to change the speed/pitch of the song.
+    	ControlObject *reverseButton;	//The ControlObject used to reverse playback of the song. 
+    	ControlObject *duration;		//The ControlObject used to get the duration of the current song.    
+        int iLeadInTime;				//The lead-in time...
+    	float dVinylPitch; 			//The speed/pitch of the timecoded vinyl as read by scratchlib.
+    	double dVinylPosition; 			//The position of the needle on the record as read by scratchlib.
+	    double dVinylScratch;
+        double dDriftControl;           //The difference between Mixxx's position and the needle's position on the record.
+                                        //... these two values naturally drift apart, so we need to keep making adjustments to the pitch
+                                        //to stop it from getting bad.
+
+	    int iSampleRate;
+	    bool bIsRunning;
+	    int iRIAACorrection;
+    	bool bRelativeMode;
+    	bool bScratchMode;
+	QWaitCondition waitForNextInput;
+	QMutex         lockInput;
+	QMutex		   lockSamples;
+};
+
+#endif
---
+++ mixxx/src/vinylcontrolproxy.cpp
@@ -0,0 +1,92 @@
+
+
+#include <QtDebug>
+#include "vinylcontrolproxy.h"
+#include "vinylcontrolscratchlib.h"
+#include "vinylcontrolxwax.h"
+
+
+//IVinylControl *VinylControlProxy::m_pVinylControl = 0;
+
+VinylControlProxy::VinylControlProxy(ConfigObject<ConfigValue> *pConfig, const char *_group) : VinylControl(pConfig, _group)
+{
+	QList<QString> xwax_timecodes;
+	QList<QString> scratchlib_timecodes;
+	
+	xwax_timecodes.push_back(MIXXX_VINYL_SERATOCV02VINYLSIDEA);
+    xwax_timecodes.push_back(MIXXX_VINYL_SERATOCV02VINYLSIDEB);
+	xwax_timecodes.push_back(MIXXX_VINYL_SERATOCD);
+	xwax_timecodes.push_back(MIXXX_VINYL_TRAKTORSCRATCHSIDEA);
+	xwax_timecodes.push_back(MIXXX_VINYL_TRAKTORSCRATCHSIDEB);
+	scratchlib_timecodes.push_back(MIXXX_VINYL_FINALSCRATCH);
+	scratchlib_timecodes.push_back(MIXXX_VINYL_MIXVIBESDVSCD);	
+	
+	//Figure out which type of timecoded vinyl we're using.
+	strVinylType = m_pConfig->getValueString(ConfigKey("[VinylControl]","strVinylType"));
+		
+	//Create the VinylControl object that matches the type of vinyl selected in the prefs...
+	if (scratchlib_timecodes.contains(strVinylType))
+	{
+	    m_pVinylControl = new VinylControlScratchlib(pConfig, _group);
+	}    
+	else if (xwax_timecodes.contains(strVinylType))
+	{
+	    m_pVinylControl = new VinylControlXwax(pConfig, _group);
+    }
+    else
+    {
+        qDebug() << "VinylControlProxy: Unknown vinyl type " << strVinylType;
+        qDebug() << "Defaulting to Serato...";
+        strVinylType = MIXXX_VINYL_SERATOCV02VINYLSIDEA;
+        pConfig->set(ConfigKey("[VinylControl]","strVinylType"), ConfigValue(MIXXX_VINYL_SERATOCV02VINYLSIDEA));
+        m_pVinylControl = new VinylControlXwax(pConfig, _group);    
+    }
+}
+
+
+VinylControlProxy::~VinylControlProxy()
+{
+
+    if (m_pVinylControl)
+        delete m_pVinylControl;
+
+
+}
+
+bool VinylControlProxy::isEnabled()
+{ 
+    return bIsRunning;
+}
+
+void VinylControlProxy::ToggleVinylControl(bool enable) 
+{
+    if (m_pVinylControl)
+        m_pVinylControl->ToggleVinylControl(enable);
+}
+
+/*
+void VinylControlProxy::syncPitch(double pitch)
+{
+    
+}
+
+void VinylControlProxy::syncPosition()
+{
+
+} */
+
+void VinylControlProxy::AnalyseSamples(short* samples, size_t size)
+{
+    if (m_pVinylControl && samples)
+        m_pVinylControl->AnalyseSamples(samples, size);
+}
+
+void VinylControlProxy::run()
+{
+}
+
+float VinylControlProxy::getSpeed()
+{
+    if (m_pVinylControl)
+        m_pVinylControl->getSpeed();
+}
