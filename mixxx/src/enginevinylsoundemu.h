9a8d0072e68d5f390c152f68fa2905f51b6bf0ed
gamegod <>
2007-09-03 20:50:38 +0000

    Classic forgotten files...

---
+++ mixxx/src/enginevinylsoundemu.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+                          enginevinylsoundemu.h  -  description
+                             -------------------
+    copyright            : (C) 2007 by Albert Santoni
+    email                :
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef ENGINEVINYLSOUNDEMU_H
+#define ENGINEVINYLSOUNDEMU_H
+
+#include "engineobject.h"
+
+class EngineVinylSoundEmu : public EngineObject
+{
+public:
+    EngineVinylSoundEmu(ConfigObject<ConfigValue> *pConfig, const char *group);
+    ~EngineVinylSoundEmu();
+    void process(const CSAMPLE *pIn, const CSAMPLE *pOut, const int iBufferSize);
+private:
+    ConfigObject<ConfigValue> *m_pConfig;
+    ControlObject *m_pRateEngine;
+    float m_fSpeed;
+    float m_fGainFactor;
+};
+
+#endif
