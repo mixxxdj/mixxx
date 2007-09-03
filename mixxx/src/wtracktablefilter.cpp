0e86e02eb257a7a0f966aa7643287064659b2570
ctaf <>
2007-09-03 23:18:48 +0000

    * add one class to filter the browser

---
+++ mixxx/src/wtracktablefilter.cpp
@@ -0,0 +1,60 @@
+/* -*- mode:C++; indent-tabs-mode:t; tab-width:8; c-basic-offset:4; -*- */
+/***************************************************************************
+    copyright            : (C) 2007 by Cedric GESTES
+    email                : ctaf42@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <Qt>
+#include <QSortFilterProxyModel>
+#include <QDirModel>
+#include <QRegExp>
+
+#include "wtracktablefilter.h"
+
+
+WTrackTableFilter::WTrackTableFilter(QModelIndex ind, QObject *parent)
+: QSortFilterProxyModel(parent),
+  m_index(ind),
+  m_regexp("\\.(mp3|ogg|aiff|aif|wav)", Qt::CaseInsensitive)
+{
+  setFilterCaseSensitivity(Qt::CaseInsensitive);
+}
+
+bool WTrackTableFilter::filterAcceptsRow(int sourceRow,
+                                         const QModelIndex &sourceParent) const
+{
+  QString str;
+  //we accept all parent
+  if (sourceParent != m_index)
+    return true;
+
+  QModelIndex index0 = sourceModel()->index(sourceRow, 0, sourceParent);
+
+  //exclude .
+  if (sourceModel()->data(index0).toString() == ".")
+    return false;
+
+  //include all folder
+  if (((QDirModel*)sourceModel())->isDir(index0))
+    return true;
+
+  //exclude other file than mp3, ogg, etc..
+  if (!sourceModel()->data(index0).toString().contains(m_regexp))
+    return false;
+
+  //exclude file not matching the search
+  return (sourceModel()->data(index0).toString().contains(filterRegExp()));
+}
+
+void WTrackTableFilter::setIndex(QModelIndex index)
+{
+  m_index = index;
+}
