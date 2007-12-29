0b383c9c6dd0f2f42d4092731a6bb2961d135912
Albert Santoni <alberts@mixxx.org>
2007-12-29 06:24:14 +0000

    Cleanup cmetrics constants, report version number

---
+++ mixxx/src/defs_mixxxcmetrics.h
@@ -0,0 +1,7 @@
+
+
+#define MIXXXCMETRICS_MIXXX_CONSTRUCTOR_COMPLETE                    3
+#define MIXXXCMETRICS_MIXXX_DESTRUCTOR_COMPLETE                     4
+#define MIXXXCMETRICS_FAILED_TO_OPEN_SNDDEVICE_AT_STARTUP           5
+#define MIXXXCMETRICS_VERSION                                       6
+
