8a1744ad463b8e65aedeafec8cc32dbbf729f8d0
Albert Santoni <alberts@mixxx.org>
2008-01-04 02:18:17 +0000

    * EngineSideChain might work now * Moved EngineShoutcast into the side chain * Added the side chain to EngineMaster

--- mixxx/src/enginesidechain.cpp
+++ mixxx/src/enginesidechain.cpp
@@ -16,47 +16,91 @@
 
 #include <QtCore>
 #include <QtDebug>
+#include "enginesidechain.h"
 #include "enginebuffer.h"
 
-EngineSideChain::EngineSideChain(ConfigObject<ConfigValue> * pConfig, const char * _group)
+#ifdef __SHOUTCAST__
+#include "engineshoutcast.h"
+#endif
+
+EngineSideChain::EngineSideChain(ConfigObject<ConfigValue> * pConfig)
 {
     m_pConfig = pConfig;
-    m_group = _group;
     
     m_bufferFront = new CSAMPLE[SIDECHAIN_BUFFER_SIZE];
     m_bufferBack  = new CSAMPLE[SIDECHAIN_BUFFER_SIZE];
     m_buffer = m_bufferFront;
     
+    m_iBufferEnd = 0;
+    
+#ifdef __SHOUTCAST__
+    // Shoutcast
+    shoutcast = new EngineShoutcast(m_pConfig);
+#endif    
+    
+    
     start();    //Starts the thread and goes to the "run()" function below.
 }
 
-EngineSideChain::submitSamples(CSAMPLE* buffer)
+EngineSideChain::~EngineSideChain()
+{
+    m_bufferLock.lock();
+    m_waitLock.lock();
+    //Free up memory
+    free(m_bufferFront);
+    free(m_bufferBack);
+    
+#ifdef __SHOUTCAST__
+    delete shoutcast;
+#endif
+    terminate(); //FIXME: Nasty
+
+    m_waitLock.unlock();
+    m_bufferLock.unlock();
+
+}
+
+void EngineSideChain::submitSamples(CSAMPLE* newBuffer, int buffer_size)
 {
     m_bufferLock.lock();
     
     //Copy samples into m_buffer.
-    
-  
-    //Pseudocode:
-    //if buffer is full:
-    //  call swapBuffers()
-    //  and then wake the run thread (below):
-    m_waitLock.lock();
-    m_waitForFullBuffer.wakeAll();
-    m_waitLock.unlock();
+    if (m_iBufferEnd + buffer_size < SIDECHAIN_BUFFER_SIZE)
+        memcpy(&m_buffer[m_iBufferEnd], newBuffer, buffer_size * sizeof(*newBuffer));
+    else //If the new buffer won't fit, copy as much of it as we can over and then copy the rest after swapping.
+    {
+        memcpy(&m_buffer[m_iBufferEnd], newBuffer, (SIDECHAIN_BUFFER_SIZE - m_iBufferEnd)*sizeof(*newBuffer));
+        //Save the number of samples written because m_iBufferEnd gets reset in swapBuffers:
+        int iNumSamplesWritten = SIDECHAIN_BUFFER_SIZE - m_iBufferEnd; 
+        swapBuffers();
+        memcpy(&m_buffer[m_iBufferEnd], newBuffer, (buffer_size - iNumSamplesWritten)*sizeof(*newBuffer));
+        
+        //Since we swapped buffers, we now have a full buffer that needs processing.
+        m_waitLock.lock();
+        m_waitForFullBuffer.wakeAll(); //... so wake the thread up and get processing. :)
+        m_waitLock.unlock();       
+    }
     
     m_bufferLock.unlock();
 }
 
-EngineSideChain::swapBuffers()
+void EngineSideChain::swapBuffers()
 {
     if (m_buffer == m_bufferFront)
+    {
         m_buffer = m_bufferBack;
+        m_filledBuffer = m_bufferFront;
+    }
     else
+    {
         m_buffer = m_bufferFront;
+        m_filledBuffer = m_bufferBack;
+    }
+    
+    m_iBufferEnd = 0;
 }
 
-EngineSideChain::run()
+void EngineSideChain::run()
 {
     while (true)
     {
@@ -67,10 +111,15 @@ EngineSideChain::run()
         //This portion of the code should be able to touch the buffer without having to use
         //a mutex, because the buffers should have been swapped.
         
+        //IMPORTANT: The filled buffer is "m_filledBuffer" - that's the audio we need to process here.
+        
         //Do CPU intensive and non-realtime processing here.
     
         //Eg. Use EngineShoutcast...
-    
+
+#ifdef __SHOUTCAST__
+        shoutcast->process(m_filledBuffer, m_filledBuffer, SIDECHAIN_BUFFER_SIZE);
+#endif    
     
     }
 
