8d6d67a3842d9c27ffe0c383c55019a34026a7fd
Albert Santoni <alberts@mixxx.org>
2008-02-29 15:08:49 +0000

    obligatory missing header

---
+++ mixxx/src/defs_promo.h
@@ -0,0 +1,10 @@
+/** @file defs_promo.h
+  * This file contains constants and a static helper class related to the promo track campaign
+  *
+*/
+#ifndef __DEFS_PROMO_H__
+#define __DEFS_PROMO_H__
+
+#define MIXXX_PROMO_DIR "promo"
+
+#endif 
