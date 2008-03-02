7b255bd14157810c7f5b1811daa019102be5a33e
Albert Santoni <alberts@mixxx.org>
2008-03-02 06:35:46 +0000

    yeah yeah

---
+++ mixxx/src/defs_audiofiles.h
@@ -0,0 +1,16 @@
+/** @file defs_audiofiles.h
+ *
+ *  This file contains constants that are related to the types of audio files we support.
+ *
+ */
+
+#ifndef __DEFS_AUDIOFILES_H__
+#define __DEFS_AUDIOFILES_H__
+
+/** The types of audio files we support */
+#define MIXXX_SUPPORTED_AUDIO_FILETYPES "*.wav *.mp3 *.ogg *.aiff *.aif *.flac"
+
+/** A regex for the types of audio files we support */
+#define MIXXX_SUPPORTED_AUDIO_FILETYPES_REGEX "\\.(mp3|ogg|aiff|aif|wav|flac)"
+
+#endif
