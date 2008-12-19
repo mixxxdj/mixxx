ad86e0de773827ea7c816a37c891ad81d03df80c
ironstorm <>
2008-12-19 22:43:08 +0000

    * Added script.debug method to midi-mappings-scripts.js * Added Hercules RMX scratch button toggle script-binding

--- mixxx/src/midi/midi-mappings-scripts.js
+++ mixxx/src/midi/midi-mappings-scripts.js
@@ -3,3 +3,24 @@
 nop = function () {
     // Only here so you don't get a syntax error on load
 }
+
+function script() {}
+script.debug = function (channel, device, control, value) {
+    print("Script.Debug --- channel: " + channel + " device: " + device + " control: " + control + " value: " + value);
+}
+
+
+function HerculesRMX () {}
+HerculesRMX.leds = { "scratch": 0x29 };
+HerculesRMX.toggle_scratch_mode = function (channel, device, control, value) {
+//    script.debug(channel, device, control, value);
+    if (value > 0) {
+        var scratch_mode = engine.getValue("[Channel1]","scratch") == 0 && engine.getValue("[Channel2]","scratch") == 0 ? 0x7F : 0;
+//        print("scratch mode to " + scratch_mode);
+        engine.setValue("[Channel1]","scratch", scratch_mode);
+        engine.setValue("[Channel2]","scratch", scratch_mode);
+        midi.sendShortMsg(0xB0, HerculesRMX.leds["scratch"] , scratch_mode, device);  // Scratch button
+    }
+}
+
+
