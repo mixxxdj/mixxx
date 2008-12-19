ef51f26c9b630f0d8fe7c9688d64d70067527704
ironstorm <>
2008-12-19 02:36:21 +0000

    * applied Pegasus_RPG's MIDIScriptControlAccess.patch

--- mixxx/src/midi/midi-mappings-scripts.js
+++ mixxx/src/midi/midi-mappings-scripts.js
@@ -1,3 +1,5 @@
-// Scripts common to all controllers go in this file
-//
-// Note: All script files MUST end with a blank line or you will get an error!
+// Functions common to all controllers go in this file
+
+nop = function () {
+    // Only here so you don't get a syntax error on load
+}
