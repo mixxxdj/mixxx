f7763d7f066d20effd85792b7aba197913f50f1e
ironstorm <>
2008-12-20 11:10:19 +0000

    * Renamed Hercules DJ Console Rmx.midi.xml -> Hercules DJ Console RMX.midi.xml * Changed controller id to be "Hercules DJ Console RMX" * Added couple of functions * FIXME script crash: Debug: ControlObject::getControl returning NULL for ( "@PS" , "SelectNextTrack" ) * FIXME can't bind 2 keys to same script function

--- mixxx/src/midi/midi-mappings-scripts.js
+++ mixxx/src/midi/midi-mappings-scripts.js
@@ -11,16 +11,40 @@ script.debug = function (channel, device, control, value) {
 
 
 function HerculesRMX () {}
-HerculesRMX.leds = { "scratch": 0x29 };
+HerculesRMX.leds = { "scratch":0x29 };
+HerculesRMX.scratchMode = false;
+HerculesRMX.playlistJogScrollMode = false;
+HerculesRMX.jogWheels = { 0x0F:"[Channel1]", 0x30:"[Channel2]" };
 HerculesRMX.toggle_scratch_mode = function (channel, device, control, value) {
-//    script.debug(channel, device, control, value);
     if (value > 0) {
-        var scratch_mode = engine.getValue("[Channel1]","scratch") == 0 && engine.getValue("[Channel2]","scratch") == 0 ? 0x7F : 0;
-//        print("scratch mode to " + scratch_mode);
-        engine.setValue("[Channel1]","scratch", scratch_mode);
-        engine.setValue("[Channel2]","scratch", scratch_mode);
-        midi.sendShortMsg(0xB0, HerculesRMX.leds["scratch"] , scratch_mode, device);  // Scratch button
+	HerculesRMX.scratchMode = !HerculesRMX.scratchMode;
+        midi.sendShortMsg(0xB0, HerculesRMX.leds["scratch"] , (HerculesRMX.scratchMode ? 0x7F : 0x0), device);  // Scratch button
     }
 }
 
+HerculesRMX.up_down_arrows = function (channel, device, control, value) {
+   script.debug(channel, device, control, value);
+   HerculesRMX.playlistJogScrollMode = value > 0;
+   if (value > 0) {
+       switch (control) {
+         case 0x2A: engine.setValue("[Playlist]","SelectPrevTrack", 1); break;
+         case 0x2B: engine.setValue("[Playlist]","SelectNextTrack", 1); break;
+       }
+   }
+}
+
+HerculesRMX.jog_wheel = function (channel, device, control, value) {
+//  7F > 40: CCW Slow > Fast - 127 > 64 
+//  01 > 3F: CW Slow > Fast - 0 > 63
+   script.debug(channel, device, control, value);
+   jogValue = value >=0x7F ? value - 0x80 : value; // -64 to +63, - = CCW, + = CW
+   if (HerculesRMX.playlistJogScrollMode) { // zip through library quickly
+        engine.setValue("[Playlist]","SelectTrackKnob", jogValue);
+   } else if (HerculesRMX.scratchMode) { // do some scratching
+       print("do scratching " + jogValue);
+       // engine.setValue(HerculesRMX.jogWheels[control],"scratch", scratch_mode);
+   } else { // do pitch adjustment
+       print("do pitching " + jogValue);
+   }
+}
 
