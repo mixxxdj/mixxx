e9f4b35cfc0ed06fc613fa5db26f8f1267911a84
pegasusrpg <>
2009-01-18 23:24:40 +0000

    Added MidiMapping class source files. (They still need work before using.) These handle the MIDI mapping XML file parsing and writing. (Code lifted from dlgprefmidibindings where it didn't belong in the first place.)

---
+++ mixxx/src/midimapping.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+                             midimapping.h
+                           MIDI Mapping Class
+                           -------------------
+    begin                : Sat Jan 17 2009
+    copyright            : (C) 2009 Sean M. Pappalardo
+    email                : pegasus@c64.org
+
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef MIDIMAPPING_H
+#define MIDIMAPPING_H
+
+// #include "midiobject.h"
+
+
+class MidiMapping : public QObject
+{
+    public:
+        /** Constructor also loads & applies the default XML MIDI mapping file */
+        MidiMapping(MidiObject* midi_object);
+        ~MidiMapping();
+        
+        void loadPreset(QString path);
+        void loadPreset(QDomElement root);
+        
+        QList<QHash>* getRowParams();
+        QList<QHash>* getOutputRowParams();
+        void deleteRowParams();
+        
+        void savePreset(QString path = BINDINGS_PATH);
+        void applyPreset();
+        void clearPreset();
+        
+        void addControl(QDomElement control, QString device);
+        void addOutput(QDomElement output, QString device);
+
+    private:
+#ifdef __SCRIPT__
+        /** Adds a script file name and function prefix to the list to be loaded */
+        void addScriptFile(QString filename, QString functionprefix);
+
+        QList<QString> m_pScriptFileNames;
+        QList<QString> m_pScriptFunctionPrefixes;
+        MidiScriptEngine *m_pScriptEngine;
+#endif
+        QDomElement m_pBindings;
+        MidiObject* m_pMidiObject;
+        QList<QHash> m_pAddRowParams;
+        QList<QHash> m_pAddOutputRowParams;
+};
+
+#endif
