7a6596e659d22f8dfa5dff8fa24c25da231024fc
Garth Dahlstrom <garth@mixxx.org>
2009-02-09 07:51:57 +0000

    * include configmidi.h header in configmidi.cpp

--- mixxx/src/configmidi.cpp
+++ mixxx/src/configmidi.cpp
@@ -13,6 +13,7 @@
 *   (at your option) any later version.                                   *
 *                                                                         *
 ***************************************************************************/
+#include "configmidi.h"
 
 ConfigObject::ConfigOption * ConfigObject::set(ConfigKey * k, ConfigValue * v)
 {
