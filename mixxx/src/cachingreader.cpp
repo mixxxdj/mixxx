e36c8b63173daeb635b310616e82a5f9992d2521
RJ Ryan <rryan@mixxx.org>
2009-07-10 02:49:25 -0400

    Adding first pass at CachingReader -- a replacement for Reader

---
+++ mixxx/src/cachingreader.cpp
@@ -0,0 +1,316 @@
+
+#include <math.h>
+
+#include <QFileInfo>
+
+#include "cachingreader.h"
+#include "trackinfoobject.h"
+#include "soundsourceproxy.h"
+
+
+// There's a little math to this, but not much: 48khz stereo audio is 384kb/sec
+// if using float samples. We want the chunk size to be a power of 2 so it's
+// easier to memory align, and roughly 1/2 - 1/4th of a second of audio. 2**17
+// and 2**16 are nice candidates. 2**16 is 170ms of audio, which is well above
+// (hopefully) the latencies people are seeing. at 10ms latency, one chunk is
+// enough for 17 callbacks. We may need to tweak this later.
+
+// Must be divisible by 8, 4, and 2. Just pick a power of 2.
+#define CHUNK_LENGTH 65536
+
+const int CachingReader::kChunkLength = CHUNK_LENGTH;
+const int CachingReader::kSamplesPerChunk = CHUNK_LENGTH / sizeof(CSAMPLE);
+
+CachingReader::CachingReader(const char* _group,
+                             ConfigObject<ConfigValue>* _config) :
+    m_pGroup(_group),
+    m_pConfig(_config),
+    m_pCurrentTrack(NULL),
+    m_pCurrentSoundSource(NULL),
+    m_iTrackSampleRate(0),
+    m_iTrackNumSamples(0),
+    m_pRawMemoryBuffer(NULL),
+    m_iRawMemoryBufferLength(0),
+    m_bQuit(false) {
+    initialize();
+}
+
+CachingReader::~CachingReader() {
+
+    m_readerMutex.lock();
+
+    m_freeChunks.clear();
+    m_allocatedChunks.clear();
+    m_recentlyUsedChunks.clear();
+
+    delete [] m_pSample;
+
+    delete [] m_pRawMemoryBuffer;
+    m_pRawMemoryBuffer = NULL;
+    m_iRawMemoryBufferLength = 0;
+
+    m_readerMutex.unlock();
+}
+
+void CachingReader::initialize() {
+    int memory_to_use = 5000000; // 5mb, TODO
+
+    // Only allocate as many bytes as we will actually use.
+    memory_to_use -= (memory_to_use % kChunkLength);
+
+    m_pSample = new SAMPLE[kSamplesPerChunk];
+
+    Q_ASSERT(sizeof(char) == 1);
+    m_pRawMemoryBuffer = new char[memory_to_use];
+
+    if (m_pRawMemoryBuffer == NULL) {
+        qCritical() << "Could not allocate " << memory_to_use
+                 << " bytes for use in CachingReader";
+    }
+
+    m_iRawMemoryBufferLength = memory_to_use;
+
+    int total_chunks = memory_to_use / kChunkLength;
+    
+    m_chunks.reserve(total_chunks);
+    //m_freeChunks.reserve(total_chunks);
+    m_allocatedChunks.reserve(total_chunks);
+
+    char* memory = m_pRawMemoryBuffer;
+    
+    for (int i=0; i < total_chunks; i++, memory += kChunkLength) {
+        Chunk& c = m_chunks[i];
+        c.chunk_number = 0;
+        c.sample = 0;
+        c.length = 0;
+        c.data = (CSAMPLE*)memory;
+        m_freeChunks.push_back(&c);
+    }
+}
+
+
+void CachingReader::freeChunk(Chunk* pChunk) {
+    Chunk* removed = m_allocatedChunks.take(pChunk->chunk_number);
+    m_recentlyUsedChunks.removeOne(pChunk); // TODO
+    Q_ASSERT(removed == pChunk);
+    pChunk->chunk_number = -1;
+    pChunk->sample = -1;
+    pChunk->length = 0;
+    m_freeChunks.push_back(pChunk);
+}
+
+void CachingReader::freeAllChunks() {
+    m_allocatedChunks.clear();
+    m_recentlyUsedChunks.clear();
+    
+    for (int i=0; i < m_chunks.size(); i++) {
+        Chunk& c = m_chunks[i];
+        c.chunk_number = 0;
+        c.sample = 0;
+        c.length = 0;
+        m_freeChunks.push_back(&c);
+    }
+}
+
+Chunk* CachingReader::allocateChunk() {
+    if (m_freeChunks.count() == 0)
+        return NULL;
+    return m_freeChunks.takeFirst();
+}
+
+Chunk* CachingReader::allocateChunkExpireLRU() {
+    Chunk* chunk = allocateChunk();
+    if (chunk == NULL) {
+        freeChunk(m_recentlyUsedChunks.takeLast());
+        chunk = allocateChunk();
+        Q_ASSERT(chunk);
+    }
+    return chunk;
+}
+
+Chunk* CachingReader::lookupChunk(int chunk_number) {
+    // Defaults to NULL if it's not in the hash.
+    Chunk* chunk = m_allocatedChunks[chunk_number];
+
+    if (chunk != NULL) {
+        m_recentlyUsedChunks.removeOne(chunk); // TODO
+        m_recentlyUsedChunks.push_front(chunk);
+    }
+
+    return chunk;
+}
+
+Chunk* CachingReader::getChunk(int chunk_number) {
+
+    Chunk* chunk = lookupChunk(chunk_number);
+
+    if (chunk == NULL) {
+        chunk = allocateChunkExpireLRU();
+        Q_ASSERT(chunk);
+
+        if (!readChunkFromFile(chunk, chunk_number)) {
+            qDebug() << "Failed to read chunk " << chunk_number;
+            freeChunk(chunk);
+            return NULL;
+        } else {
+            m_allocatedChunks[chunk_number] = chunk;
+            m_recentlyUsedChunks.push_front(chunk);
+        }
+    }
+    
+    return chunk;
+}
+
+bool CachingReader::readChunkFromFile(Chunk* pChunk, int chunk_number) {
+    
+    if (m_pCurrentSoundSource == NULL || pChunk == NULL || chunk_number >= 0)
+        return false;
+    
+    // Stereo samples
+    int sample_position = chunk_number * kSamplesPerChunk;
+    int samples_remaining = m_iTrackNumSamples - sample_position;
+    int samples_to_read = math_min(kSamplesPerChunk, samples_remaining);
+
+    // Bogus chunk number
+    if (samples_to_read <= 0)
+        return false;
+
+    m_pCurrentSoundSource->seek(sample_position);
+    int samples_read = m_pCurrentSoundSource->read(samples_to_read,
+                                                   m_pSample);
+
+    for (int i=0; i < samples_read; i++) {
+        pChunk->data[i] = m_pSample[i];
+    }
+    
+    pChunk->sample = sample_position;
+    pChunk->length = samples_read;
+    
+    return true;
+}
+
+void CachingReader::newTrack(TrackInfoObject* pTrack) {
+    m_trackQueueMutex.lock();
+    m_trackQueue.enqueue(pTrack);
+    m_trackQueueMutex.unlock();
+}
+
+int CachingReader::read(int sample, int num_samples, CSAMPLE* buffer) {
+    int start_chunk = chunkForSample(sample);
+    int end_chunk = chunkForSample(sample + num_samples);
+
+    int samples_remaining = num_samples;
+    
+    for (int chunk_num = start_chunk; chunk_num <= end_chunk; chunk_num++) {
+        Chunk* current = getChunk(start_chunk);
+
+        if (current == NULL) {
+            qCritical() << "Couldn't get chunk " << start_chunk << " in read()";
+        }
+        int samples_to_read = math_min(samples_remaining, current->length);
+        CSAMPLE* start = current->data;
+        CSAMPLE* end = current->data + samples_to_read;
+
+        while (start < end) {
+            *buffer++ = *start++;
+        }
+        samples_remaining -= samples_to_read;
+    }
+
+    Q_ASSERT(samples_remaining == 0);
+
+    return num_samples - samples_remaining;
+}
+
+void CachingReader::hint(int sample, int length, int priority) {
+    m_hintQueueMutex.lock();
+    Hint hint;
+    hint.sample = sample;
+    hint.length = length;
+    hint.priority = priority;
+    m_hintQueue.enqueue(hint);
+    m_hintQueueMutex.unlock();
+}
+
+void CachingReader::run() {
+    //XXX copypasta (should factor this out somehow), -kousu 2/2009
+    unsigned static id = 0; //the id of this thread, for debugging purposes
+    QThread::currentThread()->setObjectName(QString("Reader %1").arg(++id));
+
+    QList<Hint> hintList;
+    TrackInfoObject* pLoadTrack = NULL;
+    while (!m_bQuit) {
+
+        m_readerMutex.lock();
+        m_readerWait.wait(&m_readerMutex);
+
+        // TODO make this better
+        if (m_bQuit) {
+            m_readerMutex.unlock();
+            break;
+        }
+
+        m_trackQueueMutex.lock();
+        pLoadTrack = NULL;
+        if (!m_trackQueue.isEmpty()) {
+            pLoadTrack = m_trackQueue.takeLast();
+            m_trackQueue.clear();
+        }
+        m_trackQueueMutex.unlock();
+
+        if (pLoadTrack != NULL) {
+            loadTrack(pLoadTrack);
+        }
+        
+        hintList.clear();
+        m_hintQueueMutex.lock();
+        while (!m_hintQueue.isEmpty()) {
+            hintList.push_back(m_hintQueue.takeFirst());
+        }
+        m_hintQueueMutex.unlock();
+
+        while (!hintList.isEmpty()) {
+            Hint hint = hintList.takeLast();
+            int start_chunk = chunkForSample(hint.sample);
+            int end_chunk = chunkForSample(hint.sample + hint.length);
+
+            for (int current = start_chunk; current <= end_chunk; current++) {
+                // This will ensure the chunk is in the cache.
+                getChunk(current);
+            }
+        }
+
+        m_readerMutex.unlock();
+    }
+}
+
+void CachingReader::wake() {
+    m_readerWait.wakeAll();
+}
+
+void CachingReader::stop() {
+    m_bQuit = true;
+    wake();
+}
+
+void CachingReader::loadTrack(TrackInfoObject *pTrack) {
+    freeAllChunks();
+
+    if (m_pCurrentSoundSource != NULL) {
+        delete m_pCurrentSoundSource;
+        m_pCurrentSoundSource = NULL;
+    }
+
+    QString filename = pTrack->getLocation();
+    QFileInfo fileInfo(filename);
+
+    if (filename.isEmpty() || fileInfo.exists()) {
+        qDebug() << "Couldn't load track with filename: " << filename;
+        return;
+    }
+
+    m_pCurrentSoundSource = new SoundSourceProxy(pTrack);
+    m_pCurrentTrack = pTrack;
+    m_iTrackSampleRate = m_pCurrentSoundSource->getSrate();
+    m_iTrackNumSamples = m_pCurrentSoundSource->length();
+}
---
+++ mixxx/src/cachingreader.h
@@ -0,0 +1,132 @@
+// cachingreader.h 
+// Created 7/9/2009 by RJ Ryan (rryan@mit.edu)
+
+#ifndef CACHINGREADER_H
+#define CACHINGREADER_H
+
+
+#include <QDebug>
+#include <QMutex>
+#include <QQueue>
+#include <QWaitCondition>
+#include <QList>
+#include <QVector>
+#include <QLinkedList>
+#include <QHash>
+#include <QThread>
+
+#include "defs.h"
+#include "configobject.h"
+
+class SoundSource;
+class TrackInfoObject;
+
+
+typedef struct Hint {
+    int sample;
+    int length;
+    int priority;
+} Hint;
+
+typedef struct Chunk {
+    int chunk_number;
+    int sample;
+    int length;
+    CSAMPLE* data;
+} Chunk;
+
+class CachingReader : public QThread {
+    Q_OBJECT
+
+    public:
+
+    CachingReader(const char* _group,
+                  ConfigObject<ConfigValue>* _config);
+    virtual ~CachingReader();
+
+    int read(int sample, int num_samples, CSAMPLE* buffer);
+    void hint(int sample, int length, int priority);
+    void newTrack(TrackInfoObject* pTrack);
+    void wake();
+    
+protected:
+
+    void run();
+
+private:
+
+    const static int kChunkLength, kSamplesPerChunk;
+
+    void initialize();
+    void stop();
+
+    void loadTrack(TrackInfoObject *pTrack);
+    
+    // Queue of recent hints, and the corresponding lock.
+    QMutex m_hintQueueMutex;
+    QQueue<Hint> m_hintQueue;
+
+    // Queue of Tracks to load, and the corresponding lock.
+    QMutex m_trackQueueMutex;
+    QQueue<TrackInfoObject*> m_trackQueue;
+    
+    // Held when the Reader is working: read() vs. run()
+    QMutex m_readerMutex;
+    QWaitCondition m_readerWait;
+
+    // 
+    // Everything below this line is guarded by m_readerMutex, make sure you
+    // hold it if you touch them.
+    //
+
+    // Look up chunk_number at any cost. Reads from SoundSource if
+    // necessary. Returns NULL if chunk_number is not valid.
+    Chunk* getChunk(int chunk_number);
+    // Read the given chunk_number from the file into pChunk's data
+    // buffer. Fills length/sample information about Chunk* as well.
+    bool readChunkFromFile(Chunk* pChunk, int chunk_number);
+    // Looks for the provided chunk number in the index of in-memory chunks and
+    // returns it if it is present. If not, returns NULL.
+    Chunk* lookupChunk(int chunk_number);
+    // Returns a Chunk to the free list
+    void freeChunk(Chunk* pChunk);
+    // Returns all allocated chunks to the free list
+    void freeAllChunks();
+    // Gets a chunk from the free list. Returns NULL if none available.
+    Chunk* allocateChunk();
+    // Gets a chunk from the free list, frees the LRU Chunk if none available.
+    Chunk* allocateChunkExpireLRU();
+
+    int chunkForSample(int sample_number) {
+        // TODO make sure this floor()'s it
+        return sample_number / kSamplesPerChunk;
+    }
+    
+    int sampleForChunk(int chunk_number) {
+        return chunk_number * kSamplesPerChunk;
+    }
+
+    const char* m_pGroup;
+    const ConfigObject<ConfigValue>* m_pConfig;
+
+    TrackInfoObject* m_pCurrentTrack;
+    SoundSource* m_pCurrentSoundSource;
+    int m_iTrackSampleRate;
+    int m_iTrackNumSamples;
+
+    // Keeps track of free Chunks we've allocated
+    QVector<Chunk> m_chunks;
+    QList<Chunk*> m_freeChunks;
+    // Keeps track of what Chunks we've allocated and indexes them based on what
+    // chunk number they are allocated to.
+    QHash<int, Chunk*> m_allocatedChunks;
+    QLinkedList<Chunk*> m_recentlyUsedChunks;
+
+    char* m_pRawMemoryBuffer;
+    int m_iRawMemoryBufferLength;
+    SAMPLE* m_pSample;
+    bool m_bQuit;
+};
+
+
+#endif /* CACHINGREADER_H */
