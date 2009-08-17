2863c0483ccb981d20b5d762a664ec87268e5467
Phillip Whelan <madjester@voidwalker>
2009-08-17 03:09:47 -0700

    Copyright changes for RhythmboxPlaylistModel. This is mostly just so people know who to blame when this blows up on them.

--- mixxx/src/rhythmboxplaylistmodel.cpp
+++ mixxx/src/rhythmboxplaylistmodel.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                           rhythmboxPlaylistsource.cpp
+                              -------------------
+     begin                : 8/17/2009
+     copyright            : (C) 2009 Phillip Whelan
+     email                : pwhelan@mixxx.org
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QtCore>
 #include <QtGui>
 #include <QtSql>
