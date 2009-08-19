d808ef8b4503d16fdb14474d6c62837751eb352e
RJ Ryan <rryan@mixxx.org>
2009-08-18 22:51:17 -0400

    Merging r 2470..2471 from lp:~pwhelan/mixxx/mixxx-pitch_bend -- madjester's work on EBSL

--- mixxx/src/engine/enginebufferscalelinear.cpp
+++ mixxx/src/engine/enginebufferscalelinear.cpp
@@ -21,12 +21,16 @@
 
 #define RATE_LERP_LENGTH 200
 
-EngineBufferScaleLinear::EngineBufferScaleLinear() : EngineBufferScale()
+EngineBufferScaleLinear::EngineBufferScaleLinear(ReadAheadManager *pReadAheadManager) : 
+    EngineBufferScale(),
+    m_pReadAheadManager(pReadAheadManager)
 {
     m_dBaseRate = 0.0f;
     m_dTempo = 0.0f;
     m_fOldTempo = 1.0f;
     m_fOldBaseRate = 1.0f;
+    
+    buffer_int = new CSAMPLE[kiLinearScaleReadAheadLength*2];
 }
 
 EngineBufferScaleLinear::~EngineBufferScaleLinear()
@@ -86,82 +90,57 @@ CSAMPLE * EngineBufferScaleLinear::scale(double playpos, unsigned long buf_size,
     float rate_add_new = 2.*m_dBaseRate;
     float rate_add_old = 2.*m_fOldBaseRate; //Smoothly interpolate to new playback rate
     float rate_add = rate_add_new; 
-
+    
     m_fOldBaseRate = m_dBaseRate;           //Update the old base rate because we only need to
                                             //interpolate/ramp up the pitch changes once.
-
+    
     // Determine position in read_buffer to start from
     new_playpos = playpos;
-
+    
+    
     // Prepare buffer
-    if (m_bBackwards)
+    CSAMPLE* pbuf = buffer;
+    long get_samples = buf_size + (long)(floor((float)buf_size * ((float)m_dBaseRate - 1.0)));
+    if ( !even(get_samples))
+        get_samples++;
+    
+    int i;
+    int iAvailSamples = m_pReadAheadManager
+                ->getNextSamples((m_bBackwards ? -1.0f : 1.0f) * m_dBaseRate * m_dTempo,
+                                 buffer_int, get_samples);
+    
+    for (i=0; i<buf_size; i+=2)
     {
-        for (unsigned long i=0; i<buf_size; i+=2)
+        long prev = (long)floor(new_playpos-playpos)%READBUFFERSIZE;
+        if (!even(prev)) prev--;
+        long next = (prev+2)%READBUFFERSIZE;
+        
+        
+        //Smooth any changes in the playback rate over RATE_LERP_LENGTH samples. This
+        //prevvents the change from being discontinuous and helps improve sound
+        //quality.
+        if (i < RATE_LERP_LENGTH)
         {
-            long prev = (long)(floor(new_playpos)+READBUFFERSIZE)%READBUFFERSIZE;
-            if (!even(prev)) prev--;
-            long next = (prev-2+READBUFFERSIZE)%READBUFFERSIZE;
-
-            //Smooth any changes in the playback rate over RATE_LERP_LENGTH samples. This
-            //prevvents the change from being discontinuous and helps improve sound
-            //quality.
-            if (i < RATE_LERP_LENGTH)
-            {
-                rate_add = (rate_add_new-rate_add_old)/RATE_LERP_LENGTH*i + rate_add_old;
-            }
-            else
-                rate_add = rate_add_new;
-            
-            CSAMPLE frac = new_playpos-floor(new_playpos);
-            
-            //Hermite interpolation, see the comments in the "else" block below.
-            //buffer[i  ] = hermite4(frac, pBase[math_min(prev-2, 0)], pBase[prev], pBase[next], pBase[(next+2)%READBUFFERSIZE]); 
-            //buffer[i+1] = hermite4(frac, pBase[math_min(prev-1, 1)], pBase[prev+1], pBase[(next+1)%READBUFFERSIZE], pBase[(next+3)%READBUFFERSIZE]); 
-            
-            //Perform linear interpolation
-            buffer[i  ] = pBase[prev  ] + frac*(pBase[next  ]-pBase[prev  ]);
-            buffer[i+1] = pBase[prev+1] + frac*(pBase[next+1]-pBase[prev+1]);
-
-            new_playpos += rate_add;
+            rate_add = (rate_add_new-rate_add_old)/RATE_LERP_LENGTH*i + rate_add_old;
         }
+        else
+            rate_add = rate_add_new;
+        
+        
+        CSAMPLE frac = (new_playpos - floor(new_playpos))/2;
+        
+        //Perform linear interpolation
+        pbuf[i] = buffer_int[prev] + frac*(buffer_int[next] - buffer_int[prev]);
+        pbuf[i+1] = buffer_int[prev+1] + frac*(buffer_int[next+1] - buffer_int[prev+1]);
+        
+        
+        // Calculate playpos and execute the forward code in the fast-path
+        if ( !m_bBackwards )
+            new_playpos += (rate_add);
+        else
+            new_playpos -= (rate_add);
+        
     }
-    else
-    {
-        for (unsigned long i=0; i<buf_size; i+=2)
-        {
-            long prev = (long)floor(new_playpos)%READBUFFERSIZE;
-            if (!even(prev)) prev--;
-            long next = (prev+2)%READBUFFERSIZE;
-          
-            //Smooth any changes in the playback rate over RATE_LERP_LENGTH samples. This
-            //prevents the change from being discontinuous and helps improve sound
-            //quality.
-            if (i < RATE_LERP_LENGTH)
-            {
-                rate_add = (rate_add_new-rate_add_old)/RATE_LERP_LENGTH*i + rate_add_old;
-            }
-            else
-                rate_add = rate_add_new;
-
-            CSAMPLE frac = (new_playpos - floor(new_playpos))/2;
-            
-            /* //Hermite interpolation - experimented with this to see if there was any noticeable increase
-             *                           in sound quality, but I couldn't hear any (the literature generally
-             *                           agrees that that is the case too) - Albert 04/27/08
-            buffer[i  ] = hermite4(frac, pBase[math_min(prev-2, 0)], 
-                                   pBase[prev], pBase[next], pBase[(next+2)%READBUFFERSIZE]); 
-            buffer[i+1] = hermite4(frac, pBase[math_min(prev-1, 1)], 
-                                   pBase[prev+1], pBase[(next+1)%READBUFFERSIZE], pBase[(next+3)%READBUFFERSIZE]); 
-            */
-            
-            //Perform linear interpolation
-            buffer[i  ] = pBase[prev  ] + frac*(pBase[next  ]-pBase[prev  ]);
-            buffer[i+1] = pBase[prev+1] + frac*(pBase[next+1]-pBase[prev+1]);
-
-            new_playpos += rate_add;
-        }
-    }
-
+        
     return buffer;
 }
-
