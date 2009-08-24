8c4b85e41246c866c60382260744cfb7faf36b00
Albert Santoni <alberts@mixxx.org>
2009-08-23 20:41:08 -0700

    * Created very basic PlaylistTableModel for internal Mixxx playlists * Implemented start of Playlists and PlaylistTracks tables in database * Changed some of the PlaylistFeature code over to start using this model.

--- mixxx/src/library/playlistfeature.cpp
+++ mixxx/src/library/playlistfeature.cpp
@@ -1,16 +1,20 @@
 #include <QtDebug>
 
+#include "library/playlisttablemodel.h"
 #include "library/playlistfeature.h"
 
-PlaylistFeature::PlaylistFeature(QObject* parent)
+PlaylistFeature::PlaylistFeature(QObject* parent, TrackCollection* pTrackCollection)
     : LibraryFeature(parent) {
     playlists.push_back("My Rox");
     playlists.push_back("Trance");
     playlists.push_back("Technoize");
+    
+    m_pTrackCollection = pTrackCollection;
+    m_pPlaylistTableModel = new PlaylistTableModel(NULL, pTrackCollection, 1);
 }
 
 PlaylistFeature::~PlaylistFeature() {
-
+    delete m_pPlaylistTableModel;
 }
 
 QVariant PlaylistFeature::title() {
@@ -35,7 +39,12 @@ void PlaylistFeature::activate() {
 
 void PlaylistFeature::activateChild(int n) {
     qDebug("PlaylistFeature::activateChild(%d)", n);
-    qDebug() << "Activating " << playlists[n];
+    //qDebug() << "Activating " << playlists[n];
+
+    //FIXME: Finish this code by implementing these functions:
+    //m_pTrackCollection->getPlaylistByPosition(n);
+    //pPlaylistTableModel->setPlaylist(playlist_id);
+    emit(showTrackModel(m_pPlaylistTableModel));
 }
 
 void PlaylistFeature::onRightClick(QModelIndex index) {
