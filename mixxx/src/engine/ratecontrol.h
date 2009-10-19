c4db51d9cde1c7595970fd271089dc1242461103
RJ Ryan <rryan@mixxx.org>
2009-10-18 21:28:20 -0400

    Commenting RateControl

--- mixxx/src/engine/ratecontrol.h
+++ mixxx/src/engine/ratecontrol.h
@@ -15,25 +15,32 @@ class ControlObject;
 class ControlPotmeter;
 class ControlPushButton;
 
+// RateControl is an EngineControl that is in charge of managing the rate of
+// playback of a given channel of audio in the Mixxx engine. Using input from
+// various controls, RateControl will calculate the current rate.
 class RateControl : public EngineControl {
     Q_OBJECT
 public:
     RateControl(const char* _group, const ConfigObject<ConfigValue>* _config);
     ~RateControl();
-    double process(const double dRate, 
+
+    // Must be called during each callback of the audio thread so that
+    // RateControl has a chance to update itself.
+    double process(const double dRate,
                    const double currentSample,
                    const double totalSamples,
                    const int iBufferSize);
+    // Returns the current engine rate.
     double calculateRate(double baserate, bool paused);
     double getRawRate();
 
-    /** Set rate change when temp rate button is pressed */
+    // Set rate change when temp rate button is pressed
     static void setTemp(double v);
-    /** Set rate change when temp rate small button is pressed */
+    // Set rate change when temp rate small button is pressed
     static void setTempSmall(double v);
-    /** Set rate change when perm rate button is pressed */
+    // Set rate change when perm rate button is pressed
     static void setPerm(double v);
-    /** Set rate change when perm rate small button is pressed */
+    // Set rate change when perm rate small button is pressed
     static void setPermSmall(double v);
 
 public slots:
@@ -51,10 +58,10 @@ private:
     double getJogFactor();
     double getWheelFactor();
     double getScratchFactor();
-    
-    /** Values used when temp and perm rate buttons are pressed */
+
+    // Values used when temp and perm rate buttons are pressed
     static double m_dTemp, m_dTempSmall, m_dPerm, m_dPermSmall;
-    
+
     ControlPushButton *buttonRateTempDown, *buttonRateTempDownSmall,
         *buttonRateTempUp, *buttonRateTempUpSmall;
     ControlPushButton *buttonRatePermDown, *buttonRatePermDownSmall,
@@ -71,14 +78,13 @@ private:
     ControlObject* m_pJog;
     Rotary* m_pJogFilter;
 
-    /** Is true if a rate temp button is pressed */    
+    // Is true if a rate temp button is pressed
     bool m_bTempPress;
 
-    /** Old playback rate. Stored in this variable while a temp pitch change
-      * buttons is in effect. It does not work to just decrease the pitch slider
-      * by the value it has been increased with when the temp button was
-      * pressed, because there is a fixed limit on the range of the pitch
-      * slider */
+    // Old playback rate. Stored in this variable while a temp pitch change
+    // buttons is in effect. It does not work to just decrease the pitch slider
+    // by the value it has been increased with when the temp button was pressed,
+    // because there is a fixed limit on the range of the pitch slider
     double m_dOldRate;
 
 };
