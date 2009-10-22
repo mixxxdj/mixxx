6f9af37b2e9e1c0f70606a2efc1c2a97ad3d6489
RJ Ryan <rryan@mixxx.org>
2009-10-22 02:01:32 -0400

    Removing cruft from PlaylistFeature

--- mixxx/src/library/playlistfeature.h
+++ mixxx/src/library/playlistfeature.h
@@ -22,18 +22,17 @@ public:
     int numChildren();
     QVariant child(int n);
     bool dropAccept(const QModelIndex& index, QUrl url);
-    bool dragMoveAccept(const QModelIndex& index, QUrl url);                             
+    bool dragMoveAccept(const QModelIndex& index, QUrl url);
 public slots:
     void activate();
     void activateChild(int n);
     void onRightClick(const QPoint& globalPos, QModelIndex index);
     void onClick(QModelIndex index);
-  
+
     void slotCreatePlaylist();
     void slotDeletePlaylist();
  private:
-    QList<QString> playlists;
-    PlaylistTableModel* m_pPlaylistTableModel;   
+    PlaylistTableModel* m_pPlaylistTableModel;
     TrackCollection* m_pTrackCollection;
     QAction *m_pCreatePlaylistAction;
     QAction *m_pDeletePlaylistAction;
