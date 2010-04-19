b972a4cdb839a38a953215e199784e8aea6fa379
Sean M. Pappalardo <pegasus@renegadetech.com>
2010-04-19 22:57:20 +0200

    - Removed messageBoxHandler, the first, flawed attempt at solving the initial problem - Extended the heck out of ErrorDialog: any thread can use the global g_pDialogHelper->requestErrorDialog() to create one with standard buttons, and connect its stdButtonClicked() signal to be fired when one is clicked. See MidiScriptEngine.cpp scriptErrorDialog() and errorDialogButton() for an example of this. - Removed old log file cruft from main.cpp - Permanently made qWarning only log, as intended - Set ErrorDialog to attempt to exit gracefully on fatal errors as well (should we do this?) - MidiMapping: added reset() slot that resets the associated MIDI controller by restarting the script engine and re-applying the preset - MidiMapping: made friendlier dialogs using the new method for when a mapping can't be bound, and fixed the hex-to-text conversion - MidiScriptEngine: Changed qDebugs to qWarnings as appropriate, since they now just log and will still show up if we ever disable qDebugs for release builds - update.cpp: clarified warning and actually made it a qWarning now for logging purposes

--- mixxx/src/errordialog.cpp
+++ mixxx/src/errordialog.cpp
@@ -20,55 +20,168 @@
 #include <QtDebug>
 #include <QtCore>
 
+DialogProperties::DialogProperties() {
+    type = DLG_NONE;
+    icon = QMessageBox::NoIcon;
+    m_title = "Mixxx";
+    modal = true;
+}
+
+void DialogProperties::setTitle(QString title) {
+    m_title.append(" - ").append(title);
+}
+
+QString DialogProperties::getTitle() {
+    return m_title;
+}
+
+void DialogProperties::setType(DialogType typeToSet) {
+    type = typeToSet;
+    switch (type) {
+        case DLG_FATAL:     // Fatal uses critical icon
+        case DLG_CRITICAL:  icon = QMessageBox::Critical; break;
+        case DLG_WARNING:   icon = QMessageBox::Warning; break;
+        case DLG_INFO:      icon = QMessageBox::Information; break;
+        case DLG_QUESTION:  icon = QMessageBox::Question; break;
+        case DLG_NONE:
+        default:
+            // default is NoIcon
+            break;
+    }
+}
+
+// ErrorDialog begins here
 ErrorDialog::ErrorDialog() {
+    m_pSignalMapper = new QSignalMapper(this);
+    connect(m_pSignalMapper, SIGNAL(mapped(QString)), this, SLOT(boxClosed(QString)));
+    
     m_errorCondition=false;
-    connect(this, SIGNAL(showErrorDialog(int, QString, QString)), this, SLOT(errorDialog(int, QString, QString)));
+    connect(this, SIGNAL(showErrorDialog(DialogProperties*)),
+            this, SLOT(errorDialog(DialogProperties*)));
 }
 
 ErrorDialog::~ErrorDialog()
 {
+    delete m_pSignalMapper;
 }
 
-void ErrorDialog::requestErrorDialog(int type, QString message) {
+bool ErrorDialog::requestErrorDialog(DialogType type, QString message) {
+    DialogProperties* props = new DialogProperties();
+    props->setType(type);
+    props->text = message;
     switch (type) {
-        case 1: requestErrorDialog(type,"Mixxx - Critical error",message); break;
-        case 0:
+        case DLG_FATAL:     props->setTitle("Fatal error"); break;
+        case DLG_CRITICAL:  props->setTitle("Critical error"); break;
+        case DLG_WARNING:   props->setTitle("Warning"); break;
+        case DLG_INFO:      props->setTitle("Information"); break;
+        case DLG_QUESTION:  props->setTitle("Question"); break;
+        case DLG_NONE:
         default:
-            requestErrorDialog(type,"Mixxx - Warning",message);
+            // Default title & (lack of) icon is fine
             break;
     }
+    return requestErrorDialog(props);
 }
 
-void ErrorDialog::requestErrorDialog(int type, QString title, QString message) {
-    emit (showErrorDialog(type, title, message));
-    disconnect(this, SIGNAL(showErrorDialog(int, QString, QString)), this, SLOT(errorDialog(int, QString, QString))); // Avoid multiple dialogs until this one is acknowledged
-}
-
-void ErrorDialog::errorDialog(int type, QString title, QString message) {
-    QMessageBox msgBox;
-    msgBox.setText(message);
-    msgBox.setWindowTitle(title);
-    switch (type) {
-        case 1: msgBox.setIcon(QMessageBox::Critical); break;
-        case 0:
-        default:
-            msgBox.setIcon(QMessageBox::Warning);
-            break;
+bool ErrorDialog::requestErrorDialog(DialogProperties* props) {
+    // If no key was set, use the window text since it is likely to be unique
+    if (props->key.isEmpty()) props->key = props->text;
+    
+    // Skip if a dialog with the same key is already displayed
+    if (m_dialogKeys.contains(props->key)) {
+        DialogProperties* dlgPropsTemp = props;
+        props = NULL;
+        delete dlgPropsTemp;
+        return false;
     }
-    msgBox.exec();  // Block so the user can read it before exiting
+    
+    emit (showErrorDialog(props));
+    return true;
+}
 
-    if (type==1) {  // If critical/fatal, gracefully exit application if possible
+void ErrorDialog::errorDialog(DialogProperties* props) {
+    
+    QMessageBox* msgBox = new QMessageBox();
+    
+    msgBox->setIcon(props->icon);
+    msgBox->setWindowTitle(props->getTitle());
+    msgBox->setText(props->text);
+    if (!props->infoText.isEmpty()) msgBox->setInformativeText(props->infoText);
+    if (!props->details.isEmpty()) msgBox->setDetailedText(props->details);
+    
+    QPushButton* buttonToSet;
+    while(!props->buttons.isEmpty()) {
+        if ((props->buttons.first() != props->defaultButton) &&
+            (props->buttons.first() != props->escapeButton))
+            // If this isn't the default or escape button, we don't care about the returned pointer
+            msgBox->addButton(props->buttons.takeFirst());
+        else
+            // If this is, save the returned pointer to set it to the default and/or escape buttons
+            buttonToSet = msgBox->addButton(props->buttons.takeFirst());
+    }
+    if (buttonToSet == (QPushButton*)msgBox->button(props->defaultButton)) {
+        msgBox->setDefaultButton(buttonToSet);
+    }
+    
+    // This is currently ineffective since the GUI takes over Esc at all times,
+    //  which shouldn't be, See https://bugs.launchpad.net/mixxx/+bug/565793
+    //  Once that's fixed, this should suddenly work.
+    if (buttonToSet == (QPushButton*)msgBox->button(props->escapeButton)) {
+        msgBox->setEscapeButton(msgBox->button(props->escapeButton));
+    }
+    
+    msgBox->setModal(props->modal);
+    
+    // This deletes the msgBox automatically, avoiding a memory leak
+    msgBox->setAttribute(Qt::WA_DeleteOnClose, true);
+    
+    m_dialogKeys.append(props->key);    // To avoid duplicate dialogs on the same error
+    
+    // Signal mapper calls our slot with the key parameter so it knows which to remove from the list
+    connect(msgBox, SIGNAL(finished(int)), m_pSignalMapper, SLOT(map()));
+    m_pSignalMapper->setMapping(msgBox, props->key);
+
+    if (props->modal) msgBox->exec();    // Blocks so the user has a chance to read it before application exit
+    else msgBox->show();
+
+    // If fatal, should we just abort here and not try to exit gracefully?
+    
+    if (props->type>=DLG_CRITICAL) {  // If critical/fatal, gracefully exit application if possible
         m_errorCondition=true;
         if (QCoreApplication::instance()) {
             QCoreApplication::instance()->exit(-1);
         }
         else {
             qDebug() << "QCoreApplication::instance() is NULL! Abruptly quitting...";
-            exit(-1);
+            if (props->type==DLG_FATAL) abort();
+            else exit(-1);
         }
     }
+    
+    DialogProperties* dlgPropsTemp = props;
+    props = NULL;
+    delete dlgPropsTemp;
+}
 
-    connect(this, SIGNAL(showErrorDialog(int, QString, QString)), this, SLOT(errorDialog(int, QString, QString)));    // reconnect the signal
+void ErrorDialog::boxClosed(QString key)
+{
+    QMessageBox* msgBox = (QMessageBox*)m_pSignalMapper->mapping(key);
+
+    QMessageBox::StandardButton whichStdButton = msgBox->standardButton(msgBox->clickedButton());
+    
+    emit stdButtonClicked(key, whichStdButton);
+    
+    // If the user clicks "Ignore," we leave the key in the list so the same
+    //  error is not displayed again for the duration of the session
+    if (whichStdButton == QMessageBox::Ignore) {
+        qDebug() << "Suppressing this" << msgBox->windowTitle() << "error box for the duration of the application.";
+        return;
+    }
+    
+    if (m_dialogKeys.contains(key)) {
+        if (!m_dialogKeys.removeOne(key)) qWarning() << "Key removal from list failed!";
+    }
+    else qWarning() << "Key is missing from key list!";
 }
 
 bool ErrorDialog::checkError() {
--- mixxx/src/errordialog.h
+++ mixxx/src/errordialog.h
@@ -19,6 +19,8 @@
 #define ERRORDIALOG_H
 
 #include <QObject>
+#include <QMessageBox>
+#include <QSignalMapper>
 
 /**
   * Class used to allow all threads to display message boxes on error conditions
@@ -26,26 +28,73 @@
   *@author Sean M. Pappalardo
   */
 
+typedef enum {
+    DLG_FATAL       = 5,
+    DLG_CRITICAL    = 4,
+    DLG_WARNING     = 3,
+    DLG_INFO        = 2,
+    DLG_QUESTION    = 1,
+    DLG_NONE        = 0 // No icon (default)
+} DialogType;
+
+class DialogProperties {
+    public:
+        DialogProperties();
+
+        void setTitle(QString title);
+        QString getTitle();
+        /** Automatically sets the icon to match the type for convenience. */
+        void setType(DialogType typeToSet);
+        
+        DialogType type;
+        QMessageBox::Icon icon;
+        /** Set a key to prevent multiple dialogs until the first is closed */
+        QString key;
+        QString text;
+        QString infoText;
+        QString details;
+        bool modal;
+        /** Note that a QMessageBox::Ignore button, if clicked, will suppress
+            error boxes with the same key for the duration of the application. */
+        QList<QMessageBox::StandardButton> buttons;
+        /** The default button to highlight, if any. */
+        QMessageBox::StandardButton defaultButton;
+        /** The button that's clicked if the Escape key is pressed. */
+        QMessageBox::StandardButton escapeButton;
+        
+    private:
+        /** We keep control of this so "Mixxx" is always part of the title */
+        QString m_title;
+};
+
+
 class ErrorDialog : public QObject {
    Q_OBJECT
 public:
     ErrorDialog();
     ~ErrorDialog();
-    /** A qMessageHandler calls either of these to emit a signal to display the requested message box */
-    void requestErrorDialog(int type, QString message);
-    void requestErrorDialog(int type, QString title, QString message);
+    /** A qMessageHandler or any thread calls either of these to emit a signal to display the requested message box */
+    /** They return false if a dialog with the same key (or title if no key) is already displayed */
+    bool requestErrorDialog(DialogType type, QString message);
+    bool requestErrorDialog(DialogProperties* props);
     /** Allows a means for main() to skip exec() if there was a critical or fatal error dialog displayed on app initialization */
     bool checkError();
-
+    
 signals:
-    void showErrorDialog(int type, QString title, QString message);
+    void showErrorDialog(DialogProperties* props);
+    void stdButtonClicked(QString key, QMessageBox::StandardButton whichStdButton);
 
 private:
     bool m_errorCondition;
+    QList<QString> m_dialogKeys;
+    QSignalMapper* m_pSignalMapper;
 
 private slots:
     /** Actually displays the box */
-    void errorDialog(int type, QString title, QString message);
+    void errorDialog(DialogProperties* props);
+    void boxClosed(QString key);
 };
+
+extern ErrorDialog* g_pDialogHelper;
 
 #endif
