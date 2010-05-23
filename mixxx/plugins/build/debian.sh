a0db966d8dcfa8f182d76645324f2619737ee049
Albert Santoni <alberts@mixxx.org>
2010-05-23 15:14:59 -0700

    * Updated the m4a plugin .deb packaging script

--- mixxx/plugins/build/debian.sh
+++ mixxx/plugins/build/debian.sh
@@ -1,6 +1,6 @@
-if [ ! -e "debian/usr/lib/libsoundsourcem4a.so" ]; then
+if [ ! -e "debian/usr/lib/mixxx/plugins/libsoundsourcem4a.so" ]; then
     printf "Error: libsoundsourcem4a.so not found.\n"
-    printf "Please put the libsoundsourcem4a.so file you want to package in debian/usr/lib/ \n"
+    printf "Please put the libsoundsourcem4a.so file you want to package into the debian/usr/lib/mixxx/plugins directory \n"
     exit
 fi
 dpkg-deb --build debian
