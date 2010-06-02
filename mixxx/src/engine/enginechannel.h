6e236c14519f3b0732fb4e2f1183acc36ecf179a
RJ Ryan <rryan@mit.edu>
2010-06-02 04:08:31 -0700

    Re-enable pitch-independent-time-stretch for EngineChannels. It's just a pass-through to the EngineBuffer. features_hydra now has feature-parity with trunk.

--- mixxx/src/engine/enginechannel.h
+++ mixxx/src/engine/enginechannel.h
@@ -52,6 +52,8 @@ public:
     // TODO(XXX) This hack needs to be removed.
     EngineBuffer* getEngineBuffer();
 
+    void setPitchIndpTimeStretch(bool b);
+
 private:
     const QString m_group;
     ConfigObject<ConfigValue>* m_pConfig;
