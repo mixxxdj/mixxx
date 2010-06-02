42301cfb91d36440e098910f1b502483e59e54b0
Tobias Rafreider <trafreider@mixxx.org>
2010-06-02 11:25:34 +0200

    added missing encoder files.

---
+++ mixxx/src/recording/encoder.h
@@ -0,0 +1,60 @@
+/****************************************************************************
+                   encoder.h  - encoder API for mixxx
+                             -------------------
+    copyright            : (C) 2009 by Phillip Whelan
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include <stdlib.h> // needed for random num gen
+#include <time.h> // needed for random num gen
+#include <string.h> // needed for memcpy
+#include <QDebug>
+
+#include "engine/engineabstractrecord.h"
+#include "controlobjectthreadmain.h"
+#include "controlobject.h"
+#include "playerinfo.h"
+#include "trackinfoobject.h"
+
+
+class Encoder : public QObject {
+    Q_OBJECT
+
+public:
+    Encoder();
+    ~Encoder();
+	/*
+	 * General Encoder methods
+	 */
+    virtual int initEncoder(int bitrate) = 0;
+    virtual void encodeBuffer(const CSAMPLE *samples, const int size) = 0;
+	virtual void updateMetaData(char* artist, char* title) = 0;
+	virtual void flush() = 0;
+	
+	//Call this method in conjunction with shoutcast streaming
+	virtual void sendPackages() =0;
+	/*
+	 * If we record to hard disk drive
+	 */
+
+	//creates or opens an audio file
+	virtual void openFile() = 0;
+	//writes not encoded contents to the audio file (for WAV)
+    virtual void writeFile() = 0;
+	//closes the audio file    
+	virtual void closeFile() = 0;
+	
+};
+
+#endif // ENCODER_H
