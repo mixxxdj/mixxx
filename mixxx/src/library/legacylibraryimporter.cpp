1c9316c0b57de58b48a46057ffcf5598510f4625
Owen Williams <owilliams@mixxx.org>
2010-08-24 14:50:20 -0400

    * Tweak legacy library updater so we can run 1.7 along with 1.8 * import CreateDate value into new database as well * small fix in rhythmbox track model * extend wstatuslight to support more than two states

--- mixxx/src/library/legacylibraryimporter.cpp
+++ mixxx/src/library/legacylibraryimporter.cpp
@@ -173,9 +173,16 @@ void LegacyLibraryImporter::import()
             }
         }
 
-        //now change the file to mixxxtrack.bak so that its not readded next time program loads
-        file.copy(QDir::homePath().append("/").append(SETTINGS_PATH).append("mixxxtrack.bak"));
-        file.remove();
+        QString upgrade_filename = QDir::homePath().append("/").append(SETTINGS_PATH).append("DBUPGRADED");
+        //now create stub so that the library is not readded next time program loads
+        QFile upgradefile(upgrade_filename);
+        if (!upgradefile.open(QIODevice::WriteOnly | QIODevice::Text))
+            qDebug() << "Couldn't open" << upgrade_filename << "for writing";
+        else
+        {
+            file.write("",0);
+            file.close();
+        }
     } else {
         qDebug() << errorMsg << " line: " << errorLine << " column: " << errorColumn;
     }
