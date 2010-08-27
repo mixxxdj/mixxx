b1e5ee76f3cc8387241bd00380abb6b7ea96ae60
Owen Williams <owilliams@mixxx.org>
2010-08-27 12:32:43 -0400

    * no really, make xwax more sensitive

--- mixxx/src/vinylcontrolxwax.h
+++ mixxx/src/vinylcontrolxwax.h
@@ -12,7 +12,7 @@ extern "C" {
 #define XWAX_DEVICE_FRAME 32
 #define XWAX_SMOOTHING (128 / XWAX_DEVICE_FRAME) /* result value is in frames */
 #define RING_SIZE 20
-#define MIN_SIGNAL 100
+#define MIN_SIGNAL 50
 
 
 class VinylControlXwax : public VinylControl
