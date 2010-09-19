41704d7bd5198d479fdf6221b699ab491125400d
Albert Santoni <alberts@mixxx.org>
2010-09-18 19:13:50 -0700

    Change default latency to 21 ms

--- mixxx/src/soundmanager.cpp
+++ mixxx/src/soundmanager.cpp
@@ -360,7 +360,7 @@ void SoundManager::setDefaults(bool api, bool devices, bool other)
     {
         //Default samplerate, latency
         m_pConfig->set(ConfigKey("[Soundcard]","Samplerate"), ConfigValue(48000));
-        m_pConfig->set(ConfigKey("[Soundcard]","Latency"), ConfigValue(46));
+        m_pConfig->set(ConfigKey("[Soundcard]","Latency"), ConfigValue(21));
     }
 }
 
