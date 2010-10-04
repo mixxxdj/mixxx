3a8732056bc2a92cafa557e58db1f045ec1f1b73
Albert Santoni <gamegod@mixxx.org>
2010-10-04 02:48:46 -0400

    Forgot to checkin the mirroring script

---
+++ mirror.php
@@ -0,0 +1,39 @@
+<?php
+//Variables:
+$enableMirroring = true;
+$basepath = "";//"/home/mixxx/public_html";
+
+$fullpath = $_GET["f"]; // eg. /home/mixxx/public_html/mixxx-1.8.0/mixxx-1.8.0-win32.exe
+if ($fullpath === "" || $fullpath == null) {
+    header('Location: http://www.mixxx.org');
+    return;
+}
+//echo $fullpath;
+//$patharray = explode("/", $fullpath); 
+$shortpath = substr($fullpath, strlen($basepath)); //eg. mixxx-1.8.0/mixxx-1.8.0-win32.exe
+//echo "<BR>";
+//echo $shortpath;
+//echo "<BR>";
+$mirrors = array(0=>"http://downloads.mixxx.org",
+	         1=>"http://moo.glines.org/mixxx");
+
+$mirror = "";
+//Pick a random mirror if it's enabled
+if ($enableMirroring)
+{
+    $mirror = $mirrors[rand(0, count($mirrors)-1)];
+}
+else {
+    $mirror = "http://downloads.mixxx.org";
+}
+$newURL = $mirror . $shortpath;
+
+//If we're on downloads.mixxx.org, append a ?r to the URL so that
+//the .htaccess rewrite rule doesn't bounce us back to this mirroring script.
+//In other words, it makes sure we actually get to the file. :-)
+if ($mirror === $mirrors[0]) {
+    $newURL .= "?r";
+}
+//echo $newURL;
+header('Location: ' . $newURL); 
+?>
