640ee0ff68c95875e5cd5ded044782b46a24cb64
RJ Ryan <rryan@mixxx.org>
2010-10-05 13:58:38 -0400

    Move CheckForPKG and CheckForPKGConfig to util. Take MIXXX_VERSION out of the env.

--- mixxx/build/util.py
+++ mixxx/build/util.py
@@ -93,3 +93,21 @@ def get_flags(env, argflag, default=0):
 			flags = default
 	env[argflag] = flags
 	return flags
+
+# Checks for pkg-config on Linux
+def CheckForPKGConfig( context, version='0.0.0' ):
+	context.Message( "Checking for pkg-config (at least version %s)... " % version )
+	ret = context.TryAction( "pkg-config --atleast-pkgconfig-version=%s" %version )[0]
+	context.Result( ret )
+	return ret
+
+# Uses pkg-config to check for a minimum version
+def CheckForPKG( context, name, version="" ):
+	if version == "":
+		context.Message( "Checking for %s... \t" % name )
+		ret = context.TryAction( "pkg-config --exists '%s'" % name )[0]
+	else:
+		context.Message( "Checking for %s (%s or higher)... \t" % (name,version) )
+		ret = context.TryAction( "pkg-config --atleast-version=%s '%s'" % (version,name) )[0]
+		context.Result( ret )
+	return ret
