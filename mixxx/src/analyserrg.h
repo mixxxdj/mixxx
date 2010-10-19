f79fa8c6006aeb4db4ed7ead9d96d9b3339eb905
Vittorio <l0rdt@gmx.it>
2010-10-20 01:04:47 +0200

    Added configurable options. ReplayGain support is complete and working.

--- mixxx/src/analyserrg.h
+++ mixxx/src/analyserrg.h
@@ -8,20 +8,20 @@
 #ifndef ANALYSERRG_H_
 #define ANALYSERRG_H_
 
-/*#include <iostream>
-#include <math.h>*/
 #include "analyser.h"
+#include "configobject.h"
 
 class AnalyserGain : public Analyser {
 
   public:
-    AnalyserGain();
+    AnalyserGain(ConfigObject<ConfigValue> *_config);
     void initialise(TrackPointer tio, int sampleRate, int totalSamples);
     void process(const CSAMPLE *pIn, const int iLen);
     void finalise(TrackPointer tio);
 
   private:
     int m_istepcontrol;
+    ConfigObject<ConfigValue> *m_pConfigRG;
     //int m_iStartTime;
 };
 
--- mixxx/src/build.h
+++
@@ -1,3 +0,0 @@
-#define VERSION ""
-#define BUILD_REV ""
-#define BUILD_FLAGS "hifieq midiscript optimize qdebug vinylcontrol"
