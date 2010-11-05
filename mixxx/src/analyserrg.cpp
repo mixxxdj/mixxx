959d215cd2114b17b63486554cc649fa91ed7e3a
Vittorio <l0rdt@gmx.it>
2010-11-05 21:52:32 +0100

    Fixing indentation once again.

--- mixxx/src/analyserrg.cpp
+++ mixxx/src/analyserrg.cpp
@@ -19,16 +19,16 @@ AnalyserGain::AnalyserGain(ConfigObject<ConfigValue> *_config) {
 
 void AnalyserGain::initialise(TrackPointer tio, int sampleRate, int totalSamples) {
 
-	bool bAnalyserEnabled = (bool)m_pConfigRG->getValueString(ConfigKey("[ReplayGain]","ReplayGainAnalyserEnabled")).toInt();
-	float fRG = tio->getRG();
-	if(totalSamples == 0 || fRG != 0 || !bAnalyserEnabled) {
-		qDebug() << "Replaygain Analyser will not start.";
-		if (fRG != 0 ) qDebug() << "Found a ReplayGain value of " << 20*log10(fRG) << "dB for track :" <<(tio->getFilename());
-		return;
-	}
-	m_iStepControl = InitGainAnalysis( (long)sampleRate );
+    bool bAnalyserEnabled = (bool)m_pConfigRG->getValueString(ConfigKey("[ReplayGain]","ReplayGainAnalyserEnabled")).toInt();
+    float fRG = tio->getRG();
+    if(totalSamples == 0 || fRG != 0 || !bAnalyserEnabled) {
+        qDebug() << "Replaygain Analyser will not start.";
+        if (fRG != 0 ) qDebug() << "Found a ReplayGain value of " << 20*log10(fRG) << "dB for track :" <<(tio->getFilename());
+        return;
+    }
+    m_iStepControl = InitGainAnalysis( (long)sampleRate );
 
-	//   m_iStartTime = clock();
+    //   m_iStartTime = clock();
 }
 
 
@@ -36,22 +36,22 @@ void AnalyserGain::initialise(TrackPointer tio, int sampleRate, int totalSamples
 
 void AnalyserGain::process(const CSAMPLE *pIn, const int iLen) {
 
-	if(m_iStepControl!=1) return;
+    if(m_iStepControl!=1) return;
 
-	CSAMPLE *m_fLems = new CSAMPLE[(int)(iLen/2)];
-	CSAMPLE *m_fRems = new CSAMPLE[(int)(iLen/2)];
-	int iRGCounter = 0;
-	for(int i=0; i<iLen; i+=2) {
-		m_fLems[iRGCounter] = pIn[i]*32767;
-		m_fRems[iRGCounter] = pIn[i+1]*32767;
+    CSAMPLE *m_fLems = new CSAMPLE[(int)(iLen/2)];
+    CSAMPLE *m_fRems = new CSAMPLE[(int)(iLen/2)];
+    int iRGCounter = 0;
+    for(int i=0; i<iLen; i+=2) {
+        m_fLems[iRGCounter] = pIn[i]*32767;
+        m_fRems[iRGCounter] = pIn[i+1]*32767;
 
-		iRGCounter++;
-	}
+        iRGCounter++;
+    }
 
-	m_iStepControl = AnalyzeSamples(m_fLems,m_fRems,iRGCounter,2);
+    m_iStepControl = AnalyzeSamples(m_fLems,m_fRems,iRGCounter,2);
 
-	delete m_fLems;
-	delete m_fRems;
+    delete m_fLems;
+    delete m_fRems;
 
 }
 
@@ -60,18 +60,17 @@ void AnalyserGain::process(const CSAMPLE *pIn, const int iLen) {
 
 void AnalyserGain::finalise(TrackPointer tio) {
 
-	if(m_iStepControl!=1) return;
+    if(m_iStepControl!=1) return;
 
-	//TODO: Digg into replay_gain code and modify it so that
-	// it directly sends the result as relative peaks.
-	// In that way there is no need to do this:
+    //TODO: Digg into replay_gain code and modify it so that
+    // it directly sends the result as relative peaks.
+    // In that way there is no need to do this:
 
-	float fGain_Result = pow(10,GetTitleGain()/20);
-	tio->setRG(fGain_Result);
-	if(fGain_Result) qDebug() << "ReplayGain Analyser found a ReplayGain value of "<< 20*log10(fGain_Result) << "dB for track " << (tio->getFilename());
-	m_iStepControl=0;
-	fGain_Result=0;
-	//m_iStartTime = clock() - m_iStartTime;
-	//qDebug() << "AnalyserGain :: Generation took " << double(m_iStartTime) / CLOCKS_PER_SEC << " seconds";
+    float fGain_Result = pow(10,GetTitleGain()/20);
+    tio->setRG(fGain_Result);
+    if(fGain_Result) qDebug() << "ReplayGain Analyser found a ReplayGain value of "<< 20*log10(fGain_Result) << "dB for track " << (tio->getFilename());
+    m_iStepControl=0;
+    fGain_Result=0;
+    //m_iStartTime = clock() - m_iStartTime;
+    //qDebug() << "AnalyserGain :: Generation took " << double(m_iStartTime) / CLOCKS_PER_SEC << " seconds";
 }
-
--- mixxx/src/engine/enginepregain.cpp
+++ mixxx/src/engine/enginepregain.cpp
@@ -29,50 +29,51 @@
    ----------------------------------------------------------------*/
 EnginePregain::EnginePregain(const char * group)
 {
-	potmeterPregain = new ControlLogpotmeter(ConfigKey(group, "pregain"), 4.);
-	//Replay Gain things
-	m_pControlReplayGain = new ControlObject(ConfigKey(group, "replaygain"));
-	m_pTrackGain = new ControlObject(ConfigKey(group, "total_gain"));
+    potmeterPregain = new ControlLogpotmeter(ConfigKey(group, "pregain"), 4.);
+    //Replay Gain things
+    m_pControlReplayGain = new ControlObject(ConfigKey(group, "replaygain"));
+    m_pTotalGain = new ControlObject(ConfigKey(group, "total_gain"));
 
 
-	if(ControlObject::getControl(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"))==NULL)
-	{
-		m_pReplayGainBoost = new ControlPotmeter(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"),0., 15.);
-		m_pEnableRG = new ControlPotmeter(ConfigKey("[ReplayGain]", "ReplayGainEnabled"));
-	}
-	else
-	{
-		m_pReplayGainBoost = (ControlPotmeter*)ControlObject::getControl(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"));
-		m_pEnableRG = (ControlPotmeter*)ControlObject::getControl(ConfigKey("[ReplayGain]", "ReplayGainEnabled"));
-	}
+    if(ControlObject::getControl(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"))==NULL)
+    {
+        m_pReplayGainBoost = new ControlPotmeter(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"),0., 15.);
+        m_pEnableRG = new ControlPotmeter(ConfigKey("[ReplayGain]", "ReplayGainEnabled"));
+    }
+    else
+    {
+        m_pReplayGainBoost = (ControlPotmeter*)ControlObject::getControl(ConfigKey("[ReplayGain]", "InitialReplayGainBoost"));
+        m_pEnableRG = (ControlPotmeter*)ControlObject::getControl(ConfigKey("[ReplayGain]", "ReplayGainEnabled"));
+    }
 
 }
 
 EnginePregain::~EnginePregain()
 {
-	delete potmeterPregain;
-	delete m_pControlReplayGain;
+    delete potmeterPregain;
+    delete m_pControlReplayGain;
+    delete m_pTotalGain;
 }
 
 void EnginePregain::process(const CSAMPLE * pIn, const CSAMPLE * pOut, const int iBufferSize)
 {
 
-	float fEnableRG = m_pEnableRG->get();
-	float fReplayGainBoost = m_pReplayGainBoost->get();
-	CSAMPLE * pOutput = (CSAMPLE *)pOut;
-	float fGain = potmeterPregain->get();
-	float fRGain = m_pControlReplayGain->get();
-	m_fReplayGainCorrection=1;
-	if(fRGain*fEnableRG != 0)
-	{
+    float fEnableRG = m_pEnableRG->get();
+    float fReplayGainBoost = m_pReplayGainBoost->get();
+    CSAMPLE * pOutput = (CSAMPLE *)pOut;
+    float fGain = potmeterPregain->get();
+    float fRGain = m_pControlReplayGain->get();
+    m_fReplayGainCorrection=1;
+    if(fRGain*fEnableRG != 0)
+    {
 
-		//Passing a user defined boost
-		m_fReplayGainCorrection=fRGain*pow(10, fReplayGainBoost/20);
-	}
-	fGain = (fGain/2)*m_fReplayGainCorrection;
-	m_pTrackGain -> set(fGain);
+        //Passing a user defined boost
+        m_fReplayGainCorrection=fRGain*pow(10, fReplayGainBoost/20);
+    }
+    fGain = (fGain/2)*m_fReplayGainCorrection;
+    m_pTotalGain -> set(fGain);
 
 
-	// SampleUtil deals with aliased buffers and gains of 1 or 0.
-	SampleUtil::copyWithGain(pOutput, pIn, fGain, iBufferSize);
+    // SampleUtil deals with aliased buffers and gains of 1 or 0.
+    SampleUtil::copyWithGain(pOutput, pIn, fGain, iBufferSize);
 }
