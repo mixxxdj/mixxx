1147fa1e01633f85a1dc8164e13611d0b6aa72af
Bill Good <bkgood@mixxx.org>
2010-11-18 20:55:08 -0600

    Removed input channel-sharing error reporting (no longer an error, a few revs back)

--- mixxx/src/dlgprefsound.cpp
+++ mixxx/src/dlgprefsound.cpp
@@ -141,9 +141,6 @@ void DlgPrefSound::slotApply() {
         case SOUNDDEVICE_ERROR_DUPLICATE_OUTPUT_CHANNEL:
             error = QString(tr("Two outputs cannot share channels on %1")).arg(deviceName);
             break;
-        case SOUNDDEVICE_ERROR_DUPLICATE_INPUT_CHANNEL:
-            error = QString(tr("Two inputs cannot share channels on %1")).arg(deviceName);
-            break;
         default:
             error = QString(tr("Error opening %1\n%2")).arg(deviceName).arg(detailedError);
             break;
