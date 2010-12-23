c8d0b23d5b1eef45ab259b9d1e355288d5981e6f
Tobias Rafreider <trafreider@mixxx.org>
2010-12-23 20:45:58 +0100

    Implemented drag 'n' drop from Traktor playlists * Hide key column and bitrate by default

--- mixxx/src/library/traktorplaylistmodel.cpp
+++ mixxx/src/library/traktorplaylistmodel.cpp
@@ -113,7 +113,29 @@ bool TraktorPlaylistModel::isColumnInternal(int column) {
 
 QMimeData* TraktorPlaylistModel::mimeData(const QModelIndexList &indexes) const {
     
-    return NULL;
+   QMimeData *mimeData = new QMimeData();
+    QList<QUrl> urls;
+
+    //Ok, so the list of indexes we're given contains separates indexes for
+    //each column, so even if only one row is selected, we'll have like 7 indexes.
+    //We need to only count each row once:
+    QList<int> rows;
+
+    foreach (QModelIndex index, indexes) {
+        if (index.isValid()) {
+            if (!rows.contains(index.row())) {
+                rows.push_back(index.row());
+                QUrl url = QUrl::fromLocalFile(getTrackLocation(index));
+                if (!url.isValid())
+                    qDebug() << "ERROR invalid url\n";
+                else
+                    urls.append(url);
+            }
+        }
+    }
+    mimeData->setUrls(urls);
+    return mimeData;
+
 }
 
 
@@ -198,5 +220,10 @@ void TraktorPlaylistModel::setPlaylist(QString playlist_path)
 	initHeaderData();
 }
 bool TraktorPlaylistModel::isColumnHiddenByDefault(int column) {
+    if (column == fieldIndex(LIBRARYTABLE_KEY))    
+        return true;
+    if(column == fieldIndex(LIBRARYTABLE_BITRATE))
+    	return true;
+    
     return false;
 }
