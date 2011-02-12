649edb79e760fd7c0d6eb65418f9c50d646ee9f2
Tobias Rafreider <trafreider@mixxx.org>
2011-02-12 20:25:05 +0100

    Fix: Let triangle symbol expand folder structure

--- mixxx/src/library/foldertreemodel.h
+++ mixxx/src/library/foldertreemodel.h
@@ -1,33 +1,30 @@
 #ifndef FOLDER_TREE_MODEL
- #define FOLDER_TREE_MODEL
+#define FOLDER_TREE_MODEL
 
- #include <QAbstractItemModel>
- #include <QModelIndex>
- #include <QVariant>
- #include <QList>
+#include <QAbstractItemModel>
+#include <QModelIndex>
+#include <QVariant>
+#include <QList>
 
- #include "library/treeitemmodel.h"
- #include "library/treeitem.h"
+#include "library/treeitemmodel.h"
+#include "library/treeitem.h"
 
 
- class TreeItem;
+class TreeItem;
 /*
  * This class represents a folder item within the Browse Feature
  * The class is derived from TreeItemModel to support lazy model
  * initialization.
  */
 
- class FolderTreeModel : public TreeItemModel
- {
-     Q_OBJECT
+class FolderTreeModel : public TreeItemModel
+{
+    Q_OBJECT
 
- public:
-     FolderTreeModel(QObject *parent = 0);
-     virtual ~FolderTreeModel();
+public:
+    FolderTreeModel(QObject *parent = 0);
+    virtual ~FolderTreeModel();
     virtual bool hasChildren ( const QModelIndex & parent = QModelIndex() ) const;
-    virtual bool canFetchMore(const QModelIndex &parent) const;
-    virtual void fetchMore(const QModelIndex &parent);
-       
- };
+};
 
- #endif
+#endif
