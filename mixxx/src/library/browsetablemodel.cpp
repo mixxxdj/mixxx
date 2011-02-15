120ee570a55411c98eeb6c178c1a7032ca846f4d
Tobias <Tobias@Silverstar>
2011-02-15 17:07:57 +0100

    * Fixed again segfaults on Windows caused by rapid navigation through the folder structure.

--- mixxx/src/library/browsetablemodel.cpp
+++ mixxx/src/library/browsetablemodel.cpp
@@ -3,26 +3,13 @@
 #include <QtSql>
 #include <QStringList>
 #include <QtConcurrentRun>
+#include <QMetaType>
 
 #include "browsetablemodel.h"
 #include "soundsourceproxy.h"
 #include "mixxxutils.cpp"
 
-//constants
-const int COLUMN_FILENAME = 0;
-const int COLUMN_ARTIST = 1;
-const int COLUMN_TITLE = 2;
-const int COLUMN_ALBUM = 3;
-const int COLUMN_TRACK_NUMBER = 4;
-const int COLUMN_YEAR = 5;
-const int COLUMN_GENRE = 6;
-const int COLUMN_COMMENT = 7;
-const int COLUMN_DURATION = 8;
-const int COLUMN_BPM = 9;
-const int COLUMN_KEY = 10;
-const int COLUMN_TYPE = 11;
-const int COLUMN_BITRATE = 12;
-const int COLUMN_LOCATION = 13;
+
 
 BrowseTableModel::BrowseTableModel(QObject* parent)
         : QStandardItemModel(parent),
@@ -53,26 +40,23 @@ BrowseTableModel::BrowseTableModel(QObject* parent)
     addSearchColumn(COLUMN_GENRE);
     addSearchColumn(COLUMN_KEY);
     addSearchColumn(COLUMN_COMMENT);
-     
-    setHorizontalHeaderLabels(header_data); 
 
-    /*
-     * Start background thread.
-     * Used to read the ID3 tags
-     */
-    m_bStopThread = false;
-    m_future = QtConcurrent::run(this, &BrowseTableModel::browserThread);
-    m_path = "";
+    m_backgroundThread.moveToThread(&m_backgroundThread);
+    m_backgroundThread.start(QThread::LowestPriority);
+
+    setHorizontalHeaderLabels(header_data);
+    //qRegisterMetaType<QString>("QString");
+
+    QObject::connect(&m_backgroundThread, SIGNAL(clearModel()),
+            this, SLOT(slotClear()));
+    QObject::connect(&m_backgroundThread, SIGNAL(rowDataAppended(const QList<QStandardItem*>&)),
+            this, SLOT(slotInsert(const QList<QStandardItem*>&)), Qt::DirectConnection);
+
 }
 
 BrowseTableModel::~BrowseTableModel()
 {
-    qDebug() << "Wait to finish browser background thread";
-    m_bStopThread = true;
-    //wake up thread since it might wait for user input
-    m_locationUpdated.wakeAll();
-    m_future.waitForFinished();
-    qDebug() << "Browser background thread terminated!";
+
 }
 
 const QList<int>& BrowseTableModel::searchColumns() const {
@@ -83,8 +67,7 @@ void BrowseTableModel::addSearchColumn(int index) {
 }
 void BrowseTableModel::setPath(QString absPath)
 {
-    m_path = absPath;
-    m_locationUpdated.wakeAll();
+   m_backgroundThread.setPath(absPath);
 
 }
 
@@ -169,99 +152,15 @@ QMimeData* BrowseTableModel::mimeData(const QModelIndexList &indexes) const {
     mimeData->setUrls(urls);
     return mimeData;
 }
-void BrowseTableModel::populateModel()
+
+void BrowseTableModel::slotClear()
 {
-    //Refresh the name filters in case we loaded new
-    //SoundSource plugins.
-    QStringList nameFilters(SoundSourceProxy::supportedFileExtensionsString().split(" "));
-    QDirIterator fileIt(m_path, nameFilters, QDir::Files | QDir::NoDotAndDotDot);
-    QString thisPath(m_path);
-    //remove all rows
     removeRows(0, rowCount());
-    
-    int row = 0;
-    //Iterate over the files
-    while (fileIt.hasNext())
-    {
-        /*
-         * If a user quickly jumps through the folders
-         * the current task becomes "dirty"
-         */
-        if(thisPath != m_path){
-            //qDebug() << "Exit populateModel()";
-            return;
-        }
-        QString filepath = fileIt.next();
-        TrackInfoObject tio(filepath);
-
-        QStandardItem* item = new QStandardItem(tio.getFilename());
-        setItem(row, COLUMN_FILENAME, item);
-           
-        item = new QStandardItem(tio.getArtist());
-        setItem(row, COLUMN_ARTIST, item);
-          
-        item = new QStandardItem(tio.getTitle());
-        setItem(row, COLUMN_TITLE, item);
-           
-        item = new QStandardItem(tio.getAlbum());
-        setItem(row, COLUMN_ALBUM, item);
-
-        QString duration = MixxxUtils::secondsToMinutes(qVariantValue<int>(tio.getDuration()));
-        item = new QStandardItem(duration);
-        setItem(row, COLUMN_DURATION, item);
-
-        item = new QStandardItem(tio.getBpmStr());
-        setItem(row, COLUMN_BPM, item);
-           
-        item = new QStandardItem(tio.getKey());
-        setItem(row, COLUMN_KEY, item);
-            
-        item = new QStandardItem(tio.getYear());
-        setItem(row, COLUMN_YEAR, item);
-
-        item = new QStandardItem(tio.getBitrateStr());
-        setItem(row, COLUMN_BITRATE, item);
-            
-        item = new QStandardItem(tio.getType());
-        setItem(row, COLUMN_TYPE, item);
-            
-        item = new QStandardItem(tio.getGenre());
-        setItem(row, COLUMN_GENRE, item);
-
-        item = new QStandardItem(tio.getTrackNumber());
-        setItem(row, COLUMN_TRACK_NUMBER, item);
-
-        item = new QStandardItem(tio.getComment());
-        setItem(row, COLUMN_COMMENT, item);
-
-        item = new QStandardItem(filepath);
-        setItem(row, COLUMN_LOCATION, item);
-          
-        ++row;
-   
-    }
 }
-void BrowseTableModel::browserThread()
+void BrowseTableModel::slotInsert(const QList<QStandardItem*> &column_data)
 {
-    //Give the thread low priority to prevent GUI freezing
-    QThread* thisThread = QThread::currentThread();
-    thisThread->setPriority(QThread::LowestPriority);
-
-    while(1){
-        m_mutex.lock();
-        //Wait until the user has selected a folder
-        m_locationUpdated.wait(&m_mutex);
-
-        //Terminate thread if Mixxx closes
-        if(m_bStopThread)
-            return;
-
-        /*
-         * Populate the model
-         */
-        populateModel();
-
-        m_mutex.unlock();
-
-    }
+    qDebug() << "BrowseTableModel::slotInsert";
+    appendRow(column_data);
+    //Does not work for some reason
+    //setItem(row, column, item);
 }
