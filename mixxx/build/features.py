05cbfa238e99e976463b8f7c724d5ee3badef39b
RJ Ryan <rryan@mixxx.org>
2011-02-22 23:09:13 -0500

    Add Effects build option.

--- mixxx/build/features.py
+++ mixxx/build/features.py
@@ -77,6 +77,42 @@ class MIDIScript(Feature):
     def sources(self, build):
         return ["midi/midiscriptengine.cpp"]
 
+
+class Effects(Feature):
+
+    def description(self):
+        return "Experimental Effects Support"
+
+    def enabled(self, build):
+        enabled = util.get_flags(build.env, 'effects', 0)
+        build.flags['effects'] = enabled
+        return True if int(enabled) else False
+
+    def add_options(self, build, vars):
+        vars.Add('effects', '(EXPERIMENTAL) Set to 1 to enable effects support.', 0)
+
+    def configure(self, build, conf):
+        if not self.enabled(build):
+            return
+        build.env.Append(CPPDEFINES='__FXUNITS__')
+
+    def sources(self, build):
+        sources = SCons.Split("""engine/engineeffectsunits.cpp
+			         effectsunits/debugbackend.cpp
+			         effectsunits/effectsunitsbackend.cpp
+			         effectsunits/effectsunitscontroller.cpp
+			         effectsunits/effectsunitsinstance.cpp
+			         effectsunits/effectsunitsplugin.cpp
+			         effectsunits/effectsunitspreset.cpp
+			         effectsunits/effectsunitspresetmanager.cpp
+			         effectsunits/ladspabackend.cpp
+			         effectsunitsslot.cpp
+			         effectsunitsview.cpp
+			         effectsunitswidget.cpp
+                                 """)
+        return sources
+
+
 class LADSPA(Feature):
 
     def description(self):
