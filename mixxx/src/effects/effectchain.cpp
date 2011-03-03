f3db1fa902be9969da1a50edc217aed48910d3c0
RJ Ryan <rryan@mixxx.org>
2011-03-03 02:46:53 -0500

    Add all the EffectChain CO's from the spec.

--- mixxx/src/effects/effectchain.cpp
+++ mixxx/src/effects/effectchain.cpp
@@ -8,6 +8,31 @@ EffectChain::EffectChain(QObject* pParent, unsigned int iChainNumber)
           m_iChainNumber(iChainNumber),
           // The control group names are 1-indexed while internally everything is 0-indexed.
           m_group(QString("[EffectChain%1]").arg(iChainNumber+1)) {
+
+    m_pControlNumEffectSlots = new ControlObject(ConfigKey(m_group, "num_effectslots"));
+    m_pControlNumEffectSlots->set(0.0f);
+    connect(m_pControlNumEffectSlots, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlNumEffectSlots(double)));
+
+    m_pControlChainEnabled = new ControlObject(ConfigKey(m_group, "enabled"));
+    connect(m_pControlChainEnabled, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlChainEnabled(double)));
+
+    m_pControlChainMix = new ControlObject(ConfigKey(m_group, "mix"));
+    connect(m_pControlChainMix, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlChainMix(double)));
+
+    m_pControlChainParameter = new ControlObject(ConfigKey(m_group, "parameter"));
+    connect(m_pControlChainParameter, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlChainParameter(double)));
+
+    m_pControlChainNextPreset = new ControlObject(ConfigKey(m_group, "next_preset"));
+    connect(m_pControlChainNextPreset, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlChainNextPreset(double)));
+
+    m_pControlChainPrevPreset = new ControlObject(ConfigKey(m_group, "prev_preset"));
+    connect(m_pControlChainPrevPreset, SIGNAL(valueChanged(double)),
+            this, SLOT(slotControlChainPrevPreset(double)));
 }
 
 EffectChain::~EffectChain() {
@@ -27,8 +52,8 @@ void EffectChain::addEffectSlot() {
     // Rebroadcast effectLoaded signals
     connect(pEffectSlot, SIGNAL(effectLoaded(EffectPointer, unsigned int)),
             this, SLOT(slotEffectLoaded(EffectPointer, unsigned int)));
-
     m_slots.append(EffectSlotPointer(pEffectSlot, &QObject::deleteLater));
+    m_pControlNumEffectSlots->add(1.0f);
 }
 
 void EffectChain::slotEffectLoaded(EffectPointer pEffect, unsigned int slotNumber) {
@@ -44,4 +69,39 @@ EffectSlotPointer EffectChain::getEffectSlot(unsigned int slotNumber) {
         return EffectSlotPointer();
     }
     return m_slots[slotNumber];
+}
+
+void EffectChain::slotControlNumEffectsSlots(double v) {
+    qDebug() << debugString() << "slotControlNumEffectSlots" << v;
+    QMutexLocker locker(&m_mutex);
+    qDebug() << "WARNING: Somebody has set a read-only control. Stability may be compromised.";
+}
+
+void EffectChain::slotControlChainEnabled(double v) {
+    qDebug() << debugString() << "slotControlChainEnabled" << v;
+    //QMutexLocker locker(&m_mutex);
+}
+
+void EffectChain::slotControlChainMix(double v) {
+    qDebug() << debugString() << "slotControlChainMix" << v;
+    //QMutexLocker locker(&m_mutex);
+}
+
+void EffectChain::slotControlChainParameter(double v) {
+    qDebug() << debugString() << "slotControlChainParameter" << v;
+    //QMutexLocker locker(&m_mutex);
+}
+
+void EffectChain::slotControlChainNextPreset(double v) {
+    qDebug() << debugString() << "slotControlChainNextPreset" << v;
+    //QMutexLocker locker(&m_mutex);
+    // const int read is not worth locking for
+    emit(nextPreset(m_iChainNumber));
+}
+
+void EffectChain::slotControlChainPrevPreset(double v) {
+    qDebug() << debugString() << "slotControlChainPrevPreset" << v;
+    //QMutexLocker locker(&m_mutex);
+    // const int read is not worth locking for
+    emit(prevPreset(m_iChainNumber));
 }
--- mixxx/src/effects/effectchain.h
+++ mixxx/src/effects/effectchain.h
@@ -27,9 +27,18 @@ class EffectChain : public QObject {
     // EffectChain chainNumber. pEffect may be an invalid pointer, which
     // indicates that a previously loaded effect was removed from the slot.
     void effectLoaded(EffectPointer pEffect, unsigned int chainNumber, unsigned int slotNumber);
+    void nextPreset(unsigned int chainNumber);
+    void prevPreset(unsigned int chainNumber);
 
   private slots:
     void slotEffectLoaded(EffectPointer pEffect, unsigned int slotNumber);
+
+    void slotControlNumEffectsSlots(double v);
+    void slotControlChainEnabled(double v);
+    void slotControlChainMix(double v);
+    void slotControlChainParameter(double v);
+    void slotControlChainNextPreset(double v);
+    void slotControlChainPrevPreset(double v);
 
   private:
     DISALLOW_COPY_AND_ASSIGN(EffectChain);
@@ -40,6 +49,14 @@ class EffectChain : public QObject {
     mutable QMutex m_mutex;
     const unsigned int m_iChainNumber;
     const QString m_group;
+
+    ControlObject* m_pControlNumEffectSlots;
+    ControlObject* m_pControlChainEnabled;
+    ControlObject* m_pControlChainMix;
+    ControlObject* m_pControlChainParameter;
+    ControlObject* m_pControlChainNextPreset;
+    ControlObject* m_pControlChainPrevPreset;
+
     QList<EffectSlotPointer> m_slots;
 };
 
