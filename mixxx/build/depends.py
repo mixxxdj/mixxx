b7161ebb220d2977a27b5df22cbc3f9dbba7f4a3
Sean M. Pappalardo <spappalardo@mixxx.org>
2011-03-04 10:02:15 +0100

    Alessio Treglia's patch to recognize all architectures Debian builds on

--- mixxx/build/depends.py
+++ mixxx/build/depends.py
@@ -588,7 +588,8 @@ class MixxxCore(Feature):
         # Evaluate this define. There are a lot of different things around the
         # codebase that use different defines. (AMD64, x86_64, x86, i386, i686,
         # EM64T). We need to unify them together.
-        build.env.Append(CPPDEFINES=build.machine)
+        if not build.machine=='alpha':
+            build.env.Append(CPPDEFINES=build.machine)
 
         if build.toolchain_is_gnu:
             # Default GNU Options
