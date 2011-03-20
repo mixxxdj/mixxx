273749aff3569447d8585c12484b60bceb87e57c
Sean M. Pappalardo <spappalardo@mixxx.org>
2011-03-20 21:55:25 +0100

    - Made inclusion of qsqlite.dll optional (defaults to on) for when qtsql.dll is built with it included - Pass winlib_path to NSI script. Cleans up the MSVC DLL stuff a bit. - Replaced hard-coded MSVC 9.0 location with variable - SCS.1m: defaulted the fader-start to off because it was starting deck 1 on load - Skip copying MSVC DLL files, if present in winlib_path, to the dist directory

--- mixxx/build/mixxx.py
+++ mixxx/build/mixxx.py
@@ -240,6 +240,9 @@ class MixxxBuild(object):
         vars = Script.Variables(cachefile)
         vars.Add('prefix', 'Set to your install prefix', '/usr/local')
         vars.Add('qtdir', 'Set to your QT4 directory', '/usr/share/qt4')
+        if 'windows' in self.platform:
+            vars.Add('sqlitedll', 'Set to 1 to enable including QSQLite.dll.\
+\n           Set to 0 if SQLite support is compiled into QtSQL.dll.', 1)
         vars.Add('target', 'Set the build target for cross-compiling (windows, osx, linux, bsd).', '')
         vars.Add('machine', 'Set the machine type for cross-compiling (x86_64, x86, powerpc, powerpc64).', '')
         vars.Add('toolchain', 'Specify the toolchain to use for building (gnu, msvs). Default is gnu.', 'gnu')
