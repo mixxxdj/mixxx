8c4b50e8cdea6b99c00eeaa4052e3d0aee300025
RJ Ryan <rryan@mit.edu>
2011-03-22 14:47:27 -0400

    Use platform_is_windows instead of 'windows' in self.platform

--- mixxx/build/mixxx.py
+++ mixxx/build/mixxx.py
@@ -240,7 +240,7 @@ class MixxxBuild(object):
         vars = Script.Variables(cachefile)
         vars.Add('prefix', 'Set to your install prefix', '/usr/local')
         vars.Add('qtdir', 'Set to your QT4 directory', '/usr/share/qt4')
-        if 'windows' in self.platform:
+        if self.platform_is_windows:
             vars.Add('sqlitedll', 'Set to 1 to enable including QSQLite.dll.\
 \n           Set to 0 if SQLite support is compiled into QtSQL.dll.', 1)
         vars.Add('target', 'Set the build target for cross-compiling (windows, osx, linux, bsd).', '')
@@ -283,7 +283,7 @@ class Dependence(object):
 
     def configure(self, build, conf):
         pass
-    
+
     def post_dependency_check_configure(self, build, conf):
         pass
 
