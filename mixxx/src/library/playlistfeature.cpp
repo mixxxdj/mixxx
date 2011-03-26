3195464497a546988119f0e3d75e4b9373ac3ec9
RJ Ryan <rryan@mixxx.org>
2011-03-26 03:47:01 -0400

    Adding patch from Matthew Mikolay to enable 'Add Playlist to Auto-DJ' in playlist-context menu. Fixes Bug #617318

--- mixxx/src/library/playlistfeature.cpp
+++ mixxx/src/library/playlistfeature.cpp
@@ -30,6 +30,10 @@ PlaylistFeature::PlaylistFeature(QObject* parent, TrackCollection* pTrackCollect
     connect(m_pCreatePlaylistAction, SIGNAL(triggered()),
             this, SLOT(slotCreatePlaylist()));
 
+    m_pAddToAutoDJAction = new QAction(tr("Add to Auto-DJ Queue"),this);
+    connect(m_pAddToAutoDJAction, SIGNAL(triggered()),
+            this, SLOT(slotAddToAutoDJ()));
+
     m_pDeletePlaylistAction = new QAction(tr("Remove"),this);
     connect(m_pDeletePlaylistAction, SIGNAL(triggered()),
             this, SLOT(slotDeletePlaylist()));
@@ -64,6 +68,7 @@ PlaylistFeature::~PlaylistFeature() {
     delete m_pCreatePlaylistAction;
     delete m_pDeletePlaylistAction;
     delete m_pImportPlaylistAction;
+    delete m_pAddToAutoDJAction;
     delete m_pRenamePlaylistAction;
     delete m_pLockPlaylistAction;
 }
@@ -128,6 +133,7 @@ void PlaylistFeature::onRightClickChild(const QPoint& globalPos, QModelIndex ind
     QMenu menu(NULL);
     menu.addAction(m_pCreatePlaylistAction);
     menu.addSeparator();
+    menu.addAction(m_pAddToAutoDJAction);
     menu.addAction(m_pRenamePlaylistAction);
     menu.addAction(m_pDeletePlaylistAction);
     menu.addAction(m_pLockPlaylistAction);
@@ -415,4 +421,17 @@ void PlaylistFeature::slotImportPlaylist()
 
     //delete the parser object
     if(playlist_parser) delete playlist_parser;
+}
+
+void PlaylistFeature::slotAddToAutoDJ() {
+    //qDebug() << "slotAddToAutoDJ() row:" << m_lastRightClickedIndex.data();
+
+    if (m_lastRightClickedIndex.isValid()) {
+        int playlistId = m_playlistDao.getPlaylistIdFromName(
+            m_lastRightClickedIndex.data().toString());
+        if (playlistId >= 0) {
+            m_playlistDao.addToAutoDJQueue(playlistId);
+        }
+    }
+    emit(featureUpdated());
 }
