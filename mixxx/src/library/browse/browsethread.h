6a46c9bfb4736e9f6014d95f6a8ca1b8eccc6173
Tobias Rafreider <trafreider@mixxx.org>
2011-03-27 21:33:05 +0200

    Now made BrowseThread singleton. BrowseTableModel objects will only process received signals if they have asked the thread to do so.

--- mixxx/src/library/browse/browsethread.h
+++ mixxx/src/library/browse/browsethread.h
@@ -7,20 +7,26 @@
 #include <QStandardItem>
 #include <QList>
 
+#include "library/browse/browsetablemodel.h"
+
+class BrowseTableModel;
+
 class BrowseThread : public QThread {
     Q_OBJECT
   public:
-    BrowseThread(QObject *parent = 0);
-    ~BrowseThread();
-
-    void setPath(QString& path);
+    void executePopulation(QString& path, BrowseTableModel* client);
     void run();
+    static BrowseThread* getInstance();
+    static void destroyInstance();
 
   signals:
-    void rowsAppended(const QList< QList<QStandardItem*> >&);
-    void clearModel();
+    void rowsAppended(const QList< QList<QStandardItem*> >&, BrowseTableModel*);
+    void clearModel(BrowseTableModel*);
 
   private:
+    BrowseThread(QObject *parent = 0);
+    virtual ~BrowseThread();
+
     void populateModel();
 
     QMutex m_mutex;
@@ -28,6 +34,9 @@ class BrowseThread : public QThread {
     QList<int> m_searchColumns;
     QString m_path;
     bool m_bStopThread;
+
+    static BrowseThread* m_instance;
+    BrowseTableModel* m_model_observer;
 };
 
 #endif // BROWSETHREAD_H
