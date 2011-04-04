cd85e799bfa8584aa7714f0db0132f2d961ec744
Owen Williams <owen@ywwg.com>
2011-04-04 13:53:07 -0400

    * revert numberrate, don't do throttling here

--- mixxx/src/widget/wnumberrate.cpp
+++ mixxx/src/widget/wnumberrate.cpp
@@ -19,11 +19,6 @@ WNumberRate::WNumberRate(const char * group, QWidget * parent) : WNumber(parent)
     m_pRateRangeControl = new ControlObjectThreadMain(ControlObject::getControl(ConfigKey(group, "rateRange")));
     m_pRateDirControl = new ControlObjectThreadMain(ControlObject::getControl(ConfigKey(group, "rate_dir")));
     m_pRateControl = new ControlObjectThreadMain(ControlObject::getControl(ConfigKey(group, "rate")));
-  
-    m_tLabelUpdateTimer = new QTimer(this);
-	connect(m_tLabelUpdateTimer, SIGNAL(timeout()), this, SLOT(updateLabel()));
-	m_tLabelUpdateTimer->start(250); //update BPM every quarter second
-	m_sLabelText = QString("");
 }
 
 WNumberRate::~WNumberRate()
@@ -42,11 +37,5 @@ void WNumberRate::setValue(double)
         sign = '-';
     }
 
-    m_sLabelText = QString(m_qsText).append(sign).append("%1").arg(fabs(vsign)*100., 0, 'f', 2);
+    m_pLabel->setText(QString(m_qsText).append(sign).append("%1").arg(fabs(vsign)*100., 0, 'f', 2));
 }
-
-void WNumberRate::updateLabel()
-{
-	m_pLabel->setText(m_sLabelText);
-}
-
