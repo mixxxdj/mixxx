afa77c867c3027566be566ad9a8259ba135a514e
RJ Ryan <rryan@mixxx.org>
2011-04-08 00:47:28 -0400

    Fix volume fader control

--- mixxx/src/engine/enginemaster.cpp
+++ mixxx/src/engine/enginemaster.cpp
@@ -409,7 +409,7 @@ void EngineMaster::addChannel(EngineChannel* pChannel) {
     ChannelInfo* pChannelInfo = new ChannelInfo();
     pChannelInfo->m_pChannel = pChannel;
     pChannelInfo->m_pVolumeControl = new ControlLogpotmeter(
-        ConfigKey(pChannel->getGroup(), pChannel->getGroup()), 1.0);
+        ConfigKey(pChannel->getGroup(), "volume"), 1.0);
     pChannelInfo->m_pBuffer = SampleUtil::alloc(MAX_BUFFER_LEN);
     memset(pChannelInfo->m_pBuffer, 0, sizeof(CSAMPLE) * MAX_BUFFER_LEN);
     m_channels.push_back(pChannelInfo);
