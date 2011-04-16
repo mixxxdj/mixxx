86b09f0891466e26dae97ba1becf54e8c606df51
Bill Good <bkgood@mixxx.org>
2011-04-16 11:17:15 -0500

    Implemented VinylControlManager::onInputConnected and ::onInputDisconnected, to create and delete VinylControlProxies as needed

--- mixxx/src/vinylcontrolmanager.cpp
+++ mixxx/src/vinylcontrolmanager.cpp
@@ -26,10 +26,29 @@ void VinylControlManager::receiveBuffer(AudioInput input, const short *pBuffer, 
 }
 
 void VinylControlManager::onInputConnected(AudioInput input) {
-
+    Q_ASSERT(input.getType() == AudioInput::VINYLCONTROL);
+    unsigned char index = input.getIndex();
+    QString group = QString("[Channel%1]").arg(index + 1);
+    VinylControlProxy *newVC = new VinylControlProxy(m_pConfig, group);
+    if (index < m_proxies.size()) {
+        if (m_proxies.at(index)) {
+            delete m_proxies.at(index);
+        }
+        m_proxies.replace(index, newVC);
+    } else if (index == m_proxies.size()) {
+        m_proxies.append(newVC);
+    } else { // index > m_proxies.size()
+        while (index != m_proxies.size()) m_proxies.append(NULL);
+        m_proxies.append(newVC);
+    }
 }
 
 void VinylControlManager::onInputDisconnected(AudioInput input) {
+    Q_ASSERT(input.getType() == AudioInput::VINYLCONTROL);
+    Q_ASSERT(input.getIndex() < m_proxies.size());
+    Q_ASSERT(m_proxies.at(input.getIndex()));
 
+    delete m_proxies.at(input.getIndex());
+    m_proxies.replace(input.getIndex(), NULL);
 }
 
