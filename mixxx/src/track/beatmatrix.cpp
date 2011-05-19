5e303081d090fb86d3ca3b1bee46f4655b4976ba
Vittorio <l0rdt@gmx.it>
2011-05-19 21:43:50 +0200

    Added Tobias bpm calculation patch

--- mixxx/src/track/beatmatrix.cpp
+++ mixxx/src/track/beatmatrix.cpp
@@ -20,28 +20,81 @@ unsigned int BeatMatrix::numBeats() const {
 }
 
 double BeatMatrix::calculateBpm(double dStartSample, double dEndSample) const {
-    BeatList::const_iterator it = qLowerBound(m_beatList.begin(),
-                                                   m_beatList.end(),
-                                                   dStartSample);
-    BeatList::const_iterator EndBeat = qUpperBound(m_beatList.begin(),
-                                                        m_beatList.end(),
-                                                        dEndSample);
-    double HarmonicSum = 0;
-    double N = 0;
-    while(it != EndBeat){
-        double diff = 0;
-        diff -= *it;
-        it++;
-        diff += *it;
-        if (diff){
-            HarmonicSum += 1/diff;
-            N++;
-        }
+
+    /*
+     * Let's compute the average local
+     * BPM for N subsequent beats.
+     * The average BPMs are
+     * added to a list from which the statistical
+     * median is computed
+     *
+     * N=8 seems to work great; We coincide with Traktor's
+     * BPM value in many case but not worse than +-0.2 BPM
+     */
+     QList<double> average_bpm_list;
+     const int N = 8; //computes the avg BPM from N subsequent beats --> should be a DEFINE later on
+
+
+    /*
+     * Just to demonstrate how you would count the beats manually
+     *
+     *    Beat numbers:   1  2  3  4   5  6  7  8    9
+     *    Beat positions: ?  ?  ?  ?  |?  ?  ?  ?  | ?
+     *
+     * Usually one measures the time of N beats. One stops
+     * the timer just before the (N+1)th beat begins.
+     * The BPM is then computed by 60*N/<time needed to count N beats (in seconds)>
+     *
+     * Although beat tracking through Vamp is promising,
+     * the local average BPM of 4 beats varies frequently by +-2 BPM.
+     * Somtimes there N subsequent beats in the grid that are computed wrongly by Vamp.
+     * Their local BPMs can be considered as outliers which would influence the
+     * BPM computation negatively.
+     * Therefore, the average local BPM is build for every N subsequent beats.
+     * The average BPM values are added to a sorted list from which
+     * the central element is choosen in order to compute the median.
+     *
+     */
+
+    for(int i=N; i < m_beatList.size(); i+=N){
+        //get start and end sample of the beats
+        double start_sample = m_beatList.at(i-N);
+        double end_sample = m_beatList.at(i);
+
+        //Time needed to count a bar (4 beats)
+        double time = (end_sample - start_sample)/(m_iSampleRate * 2);
+        double avg_bpm = 60*N / time;
+        //add to local BPM to list
+        average_bpm_list << avg_bpm;
+        //qDebug() << "Average Bar BPM: " << avg_bpm;
     }
-    if (HarmonicSum)
-        return (120*m_iSampleRate)*(HarmonicSum/N);
+    //sort the least of average BPMs
+    qSort(average_bpm_list);
+
+
+    //find the {(n+1)/2} th item in the sorted list
+    int item_position = (average_bpm_list.size() + 1)/2;
+    double median;
+
+    /*
+     * compute the median (bpm)
+     * see http://en.wikipedia.org/wiki/Median#The_sample_median
+     */
+    if(average_bpm_list.size() % 2 == 0){
+        //when the list size is eval
+        double item_value1 = average_bpm_list.at(item_position/2);
+        double item_value2 = average_bpm_list.at((item_position/2) + 1);
+        median = item_value1 + (item_value1 - item_value2)/2;
+
+    }
     else
-        return 0;
+    {
+        median = average_bpm_list.at(item_position/2);
+
+    }
+
+    return median;
+
 }
 
 
