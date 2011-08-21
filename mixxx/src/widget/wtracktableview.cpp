43144d6f84cf87184635572b0450d5132158a33e
RJ Ryan <rryan@mixxx.org>
2011-08-20 22:24:54 -0400

    Add Reload Metadata context menu option patch from Joe Colosimo (already in the credits). Thanks Joe!

--- mixxx/src/widget/wtracktableview.cpp
+++ mixxx/src/widget/wtracktableview.cpp
@@ -81,6 +81,7 @@ WTrackTableView::~WTrackTableView()
         pHeader->saveHeaderState();
     }
 
+    delete m_pReloadMetadataAct;
     delete m_pAutoDJAct;
     delete m_pRemoveAct;
     delete m_pPropertiesAct;
@@ -224,6 +225,9 @@ void WTrackTableView::createActions()
 
     m_pAutoDJAct = new QAction(tr("Add to Auto DJ Queue"),this);
     connect(m_pAutoDJAct, SIGNAL(triggered()), this, SLOT(slotSendToAutoDJ()));
+
+    m_pReloadMetadataAct = new QAction(tr("Reload Track Metadata"), this);
+    connect(m_pReloadMetadataAct, SIGNAL(triggered()), this, SLOT(slotReloadTrackMetadata()));
 }
 
 void WTrackTableView::slotMouseDoubleClicked(const QModelIndex &index)
@@ -362,7 +366,7 @@ void WTrackTableView::contextMenuEvent(QContextMenuEvent * event)
             int iPlaylistId = playlistDao.getPlaylistId(i);
 
             if (!playlistDao.isHidden(iPlaylistId)) {
-                
+
                 QString playlistName = playlistDao.getPlaylistName(iPlaylistId);
                 // No leak because making the menu the parent means they will be
                 // auto-deleted
@@ -401,6 +405,7 @@ void WTrackTableView::contextMenuEvent(QContextMenuEvent * event)
     m_pRemoveAct->setEnabled(!locked);
     m_pMenu->addSeparator();
     m_pMenu->addAction(m_pRemoveAct);
+    m_pMenu->addAction(m_pReloadMetadataAct);
     m_pPropertiesAct->setEnabled(oneSongSelected);
     m_pMenu->addAction(m_pPropertiesAct);
 
@@ -724,8 +729,9 @@ void WTrackTableView::loadSelectedTrackToGroup(QString group) {
 }
 
 void WTrackTableView::slotSendToAutoDJ() {
-    if (!modelHasCapabilities(TrackModel::TRACKMODELCAPS_ADDTOAUTODJ))
+    if (!modelHasCapabilities(TrackModel::TRACKMODELCAPS_ADDTOAUTODJ)) {
         return;
+    }
 
     PlaylistDAO& playlistDao = m_pTrackCollection->getPlaylistDAO();
     int iAutoDJPlaylistId = playlistDao.getPlaylistIdFromName(AUTODJ_TABLE);
@@ -744,6 +750,27 @@ void WTrackTableView::slotSendToAutoDJ() {
             if (iTrackId != -1) {
                 playlistDao.appendTrackToPlaylist(iTrackId, iAutoDJPlaylistId);
             }
+        }
+    }
+}
+
+void WTrackTableView::slotReloadTrackMetadata() {
+    if (!modelHasCapabilities(TrackModel::TRACKMODELCAPS_RELOADMETADATA)) {
+        return;
+    }
+
+    QModelIndexList indices = selectionModel()->selectedRows();
+
+    TrackModel* trackModel = getTrackModel();
+
+    if (trackModel == NULL) {
+        return;
+    }
+
+    foreach (QModelIndex index, indices) {
+        TrackPointer pTrack = trackModel->getTrack(index);
+        if (pTrack) {
+            pTrack->parse();
         }
     }
 }
