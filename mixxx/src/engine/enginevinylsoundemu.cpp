b08a8129d37482678f0398777566852a79bb5288
Owen Williams <owilliams@mixxx.org>
2011-09-17 18:45:58 -0400

    Optimizing vinyl sound emu: * precompute random values for dithering * don't repeatedly calculate absolute value

--- mixxx/src/engine/enginevinylsoundemu.cpp
+++ mixxx/src/engine/enginevinylsoundemu.cpp
@@ -27,12 +27,25 @@
  *   these slow speeds.
  */
 
+float EngineVinylSoundEmu::sm_fNoise[NOISE_BUFFER_SIZE];
+bool EngineVinylSoundEmu::sm_bNoiseInited = false;
+
 EngineVinylSoundEmu::EngineVinylSoundEmu(ConfigObject<ConfigValue> * pConfig, const char * group)
 {
     m_pConfig = pConfig;
     m_pRateEngine = ControlObject::getControl(ConfigKey(group, "rateEngine"));
     m_fSpeed = m_fOldSpeed = 0.0f;
     m_fGainFactor = 1.0f;
+    m_iNoisePos = 0;
+    
+    if (!sm_bNoiseInited)
+    {
+        sm_bNoiseInited = true;
+        for (int i=0; i<NOISE_BUFFER_SIZE; i++)
+        {
+            sm_fNoise[i] = (float)(rand() % 32768) / 32768 - 0.5;
+        }
+    }
 }
 
 EngineVinylSoundEmu::~EngineVinylSoundEmu()
@@ -46,17 +59,18 @@ void EngineVinylSoundEmu::process(const CSAMPLE * pIn, const CSAMPLE * pOut, con
     m_fSpeed = (float)m_pRateEngine->get();
     float rateFrac = 2 * (m_fSpeed - m_fOldSpeed) / (float)iBufferSize;
     float curRate = m_fOldSpeed;
-    float absCurRate = fabs(curRate);
     
     const float thresholdSpeed = 0.070f; //Scale volume if playback speed is below 7%.
     const float ditherSpeed = 0.85f; //Dither if playback speed is below 85%.
     
-    //iterate over old rate to new rate to prevent audible pops    
+    //iterate over old rate to new rate to prevent audible pops
     for (int i=0; i<iBufferSize; i+=2)
     {
+        float absCurRate = fabs(curRate);
         float dither = 0;
         if (absCurRate < ditherSpeed) {
-            dither = (float)(rand() % 32768) / 32768 - 0.5;
+            dither = sm_fNoise[m_iNoisePos];
+            m_iNoisePos = (m_iNoisePos + 1) % NOISE_BUFFER_SIZE;
         }
         
         if (absCurRate < thresholdSpeed) {
