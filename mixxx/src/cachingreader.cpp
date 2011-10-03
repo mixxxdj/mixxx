225f2d5252b203a60a2f24429fefd10b05f0df52
RJ Ryan <rryan@mixxx.org>
2011-10-03 01:57:07 -0400

    Move CO::sync() into an EngineWorker and out of hte callback.

--- mixxx/src/cachingreader.cpp
+++ mixxx/src/cachingreader.cpp
@@ -385,7 +385,10 @@ int CachingReader::read(int sample, int num_samples, CSAMPLE* buffer) {
 
         // If the chunk is not in cache, then we must return an error.
         if (current == NULL) {
-            qDebug() << "Couldn't get chunk " << start_chunk << " in read()";
+            qDebug() << "Couldn't get chunk " << start_chunk
+                     << " in read() of [" << sample << "," << sample+num_samples
+                     << "] chunks " << start_chunk << "-" << end_chunk;
+
             // Something is wrong. Break out of the loop, that should fill the
             // samples requested with zeroes.
             break;
