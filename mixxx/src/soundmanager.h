18b5d24e6dc7213420dd723d7252a0c61cfc9b1d
Daniel Schürmann <daschuer@gmx.de>
2011-10-06 22:53:09 +0200

    clean up after merge

--- mixxx/src/soundmanager.h
+++ mixxx/src/soundmanager.h
@@ -21,8 +21,6 @@
 #include "defs.h"
 #include "configobject.h"
 #include "soundmanagerconfig.h"
-#include "controlobjectthreadmain.h"
-#include <QTimer>
 
 class SoundDevice;
 class EngineMaster;
@@ -94,16 +92,8 @@ private:
     bool m_paInitialized;
     unsigned int m_jackSampleRate;
 #endif
-    
     QHash<AudioOutput, const AudioSource*> m_registeredSources;
     QHash<AudioInput, AudioDestination*> m_registeredDestinations;
-
-    ControlObjectThreadMain* m_pControlObjectLatency;
-    ControlObjectThreadMain* m_pControlObjectSampleRate;
-    ControlObjectThreadMain* m_pControlObjectVinylControlMode;
-    ControlObjectThreadMain* m_pControlObjectVinylControlMode1;
-    ControlObjectThreadMain* m_pControlObjectVinylControlMode2;
-    ControlObjectThreadMain* m_pControlObjectVinylControlGain;
 };
 
 #endif
