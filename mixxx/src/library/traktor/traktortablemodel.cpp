f55abd90523bc4f5347bf003b4fc144d78bf6191
RJ Ryan <rryan@mit.edu>
2011-10-11 18:38:39 -0400

    Add basic beatgrid to tracks loaded from RB, iTunes, or Traktor. Fixes Bug #769541

--- mixxx/src/library/traktor/traktortablemodel.cpp
+++ mixxx/src/library/traktor/traktortablemodel.cpp
@@ -4,6 +4,8 @@
 
 #include "library/trackcollection.h"
 #include "library/traktor/traktortablemodel.h"
+#include "track/beatfactory.h"
+#include "track/beats.h"
 
 TraktorTableModel::TraktorTableModel(QObject* parent,
                                      TrackCollection* pTrackCollection)
@@ -42,9 +44,15 @@ TrackPointer TraktorTableModel::getTrack(const QModelIndex& index) const {
     pTrack->setYear(year);
     pTrack->setGenre(genre);
     pTrack->setBpm(bpm);
+    TrackPointer track(pTrack, &QObject::deleteLater);
 
+    // If the track has a BPM, then give it a static beatgrid.
+    if (bpm > 0) {
+        BeatsPointer pBeats = BeatFactory::makeBeatGrid(track, bpm, 0);
+        track->setBeats(pBeats);
+    }
 
-    return TrackPointer(pTrack, &QObject::deleteLater);
+    return track;
 }
 
 void TraktorTableModel::search(const QString& searchText) {
