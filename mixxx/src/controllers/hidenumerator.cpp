ec99045959c1ca81f79c9ffcb96b37d20ceed4e2
Sean M. Pappalardo <pegasus@8440p>
2011-10-14 13:37:57 +0200

    Added logged error message on USB permissions problem (Linux)

--- mixxx/src/controllers/hidenumerator.cpp
+++ mixxx/src/controllers/hidenumerator.cpp
@@ -41,15 +41,17 @@ QList<Controller*> HidEnumerator::queryDevices() {
     struct hid_device_info *devs, *cur_dev;
     
     devs = hid_enumerate(0x0, 0x0);
-    cur_dev = devs; 
+    cur_dev = devs;
     while (cur_dev) {
-        printf("  Found %ls %ls r%hx, S/N %ls  Interface %d", cur_dev->manufacturer_string, 
+        printf("  Found %ls %ls r%hx, S/N %ls  Interface %d", cur_dev->manufacturer_string,
                cur_dev->product_string, cur_dev->release_number, cur_dev->serial_number,
                cur_dev->interface_number);
-        printf("\n");
-        
-        HidController *currentDevice = new HidController(*(cur_dev));
-        m_devices.push_back((Controller*)currentDevice);
+               printf("\n");
+        if (!cur_dev->serial_number && !cur_dev->product_string) qWarning() << "USB permissions problem (or device error.) Your account needs write access to USB devices.";
+        else {
+            HidController *currentDevice = new HidController(*(cur_dev));
+            m_devices.push_back((Controller*)currentDevice);
+        }
         
         cur_dev = cur_dev->next;
     }
