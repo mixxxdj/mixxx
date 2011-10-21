cda9f8e9d92ff8618e41b5ac08dbd2b77f48c615
RJ Ryan <rryan@mixxx.org>
2011-10-20 22:22:10 -0400

    Cleanup

--- mixxx/src/library/mixxxlibraryfeature.cpp
+++ mixxx/src/library/mixxxlibraryfeature.cpp
@@ -40,11 +40,13 @@ MixxxLibraryFeature::MixxxLibraryFeature(QObject* parent,
             << "library." + LIBRARYTABLE_MIXXXDELETED;
 
     QSqlQuery query(pTrackCollection->getDatabase());
-    QString queryString = "CREATE TEMPORARY VIEW IF NOT EXISTS library_cache_view AS SELECT "
-            + columns.join(",") +
-            " FROM library INNER JOIN track_locations "
-            "ON library.location = track_locations.id";
-
+    QString tableName = "library_cache_view";
+    QString queryString = QString(
+        "CREATE TEMPORARY VIEW IF NOT EXISTS %1 AS "
+        "SELECT %2 FROM library "
+        "INNER JOIN track_locations ON library.location = track_locations.id")
+            .arg(tableName)
+            .arg(columns.join(","));
     query.prepare(queryString);
     if (!query.exec()) {
         LOG_FAILED_QUERY(query);
@@ -61,7 +63,7 @@ MixxxLibraryFeature::MixxxLibraryFeature(QObject* parent,
     }
 
     BaseTrackCache* pBaseTrackCache = new BaseTrackCache(
-        pTrackCollection, "library_cache_view", LIBRARYTABLE_ID, columns, true);
+        pTrackCollection, tableName, LIBRARYTABLE_ID, columns, true);
     connect(&pTrackCollection->getTrackDAO(), SIGNAL(trackDirty(int)),
             pBaseTrackCache, SLOT(slotTrackDirty(int)));
     connect(&pTrackCollection->getTrackDAO(), SIGNAL(trackClean(int)),
