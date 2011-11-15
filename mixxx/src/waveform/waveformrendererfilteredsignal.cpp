a39d4f1d8a907fd94521af7e8bb9726ab29d30ea
RJ Ryan <rryan@mit.edu>
2011-11-15 02:33:46 -0500

    Add performance comments to WaveformRendererFilteredSignal

--- mixxx/src/waveform/waveformrendererfilteredsignal.cpp
+++ mixxx/src/waveform/waveformrendererfilteredsignal.cpp
@@ -48,6 +48,8 @@ void WaveformRendererFilteredSignal::draw(QPainter* painter,
     const Waveform* waveform = trackInfo->getWaveForm();
     const QVector<unsigned char>& waveformData = waveform->getConstData();
 
+    // TODO(rryan): clear() actually clears the memory we have reserve()'d. This
+    // causes memory thrashing.
     m_lowLines.clear();
     m_midLines.clear();
     m_highLines.clear();
@@ -83,6 +85,9 @@ void WaveformRendererFilteredSignal::draw(QPainter* painter,
 
             for (int sampleIndex = 0; sampleIndex < 2*samplesPerPixel;
                  ++sampleIndex) {
+                // TODO(rryan) QVector::operator[] will show very high in
+                // profiling runs due to the bounds checking. Better to call
+                // constData() on the QVector first.
                 maxLow[0] = math_max(
                     maxLow[0], lowData[thisIndex+sampleIndex]);
                 maxLow[1] = math_max(
@@ -97,6 +102,8 @@ void WaveformRendererFilteredSignal::draw(QPainter* painter,
                     maxHigh[1], highData[thisIndex+sampleIndex+1]);
             }
 
+            // TODO(rryan) push_back is allocating memory due to the clear()
+            // above.
             m_lowLines.push_back(
                 QLineF(xPos, (int)(halfHeight-heightFactor*(float)maxLow[0]),
                        xPos, (int)(halfHeight+heightFactor*(float)maxLow[1])));
@@ -113,6 +120,8 @@ void WaveformRendererFilteredSignal::draw(QPainter* painter,
         }
     }
 
+    // TODO(rryan) calling data() on a QVector clones it. This causes intense
+    // memory thrashing and skips on low-end computers.
     painter->setPen(QPen(QBrush(m_lowColor), 2));
     painter->drawLines(m_lowLines.data(), m_lowLines.size());
     painter->setPen(QPen(QBrush(m_midColor), 2));
