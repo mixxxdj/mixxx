232e320a863f7b0bb3e63cd02f3da57e73cbd1c7
vrince <vrince@gmail.com>
2011-11-21 20:40:44 -0500

    rename waveform sampleRate to visualSampleRate analyserwaveform clean-up (fetch and correct waveform visual sampling rate)

--- mixxx/src/analyserwaveform.h
+++ mixxx/src/analyserwaveform.h
@@ -6,6 +6,8 @@
 class EngineFilterButterworth8;
 class Waveform;
 
+class QTime;
+
 class AnalyserWaveform : public Analyser {
 
 public:
@@ -17,43 +19,29 @@ public:
     void process(const CSAMPLE *pIn, const int iLen);
     void finalise(TrackPointer tio);
 
-    //Downsample finding the maximum of each stride buffer
-    void process_old(const CSAMPLE *pIn, const int iLen);
-
 private:
     void storeCurentStridePower();
     void resetCurrentStride();
 
+    void resetFilters(TrackPointer tio);
+    void destroyFilters();
+
 private:
-    QVector<float> *downsample;
-    float *downsampleVector;
-
-    //kind of weaid timing ...
-    int m_iStartTime;
-
-    int m_iStrideLength; //downsample length
-    int m_iSamplePos; //position in the samples
-    int m_iStrideBufferPos; //position in the stride
-
-    float m_timePerStride;
-
-    //Use to find max
-    float m_fLMax;
-    float m_fRMax;
-    float m_fOldLMax;
-    float m_fOldRMax;
-
-    //////////////
     Waveform* m_waveform;
 
     // Current stride
     int m_currentStride;
+    float m_timePerStride;
+    int m_strideLength;
+    int m_strideBufferPos;
     float m_currentStridePower[2];
     float m_currentStrideFiltredPower[2][3];
 
     bool m_ready;
 
     EngineFilterButterworth8* m_filter[3];
+
+    QTime* timer_;
 };
 
 #endif
