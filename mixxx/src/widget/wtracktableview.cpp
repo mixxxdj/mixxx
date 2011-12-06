ca0f644e1013e4d13c66961758d87437bef6e016
RJ Ryan <rryan@mit.edu>
2011-12-06 11:47:12 -0500

    Try changing warning messagebox to have no parent to see if it fixes Bug #900470

--- mixxx/src/widget/wtracktableview.cpp
+++ mixxx/src/widget/wtracktableview.cpp
@@ -827,7 +827,7 @@ void WTrackTableView::slotReloadTrackMetadata() {
     }
 
     if (QMessageBox::warning(
-        this, tr("Reload Track Metadata"),
+        NULL, tr("Reload Track Metadata"),
         tr("Reloading track metadata on a loaded track may cause abrupt volume changes. Are you sure?"),
         QMessageBox::Yes | QMessageBox::No,
         QMessageBox::No) == QMessageBox::No) {
