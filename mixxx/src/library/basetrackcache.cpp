20cf41daf34c3a7154b7b987d01793f2980ad474
RJ Ryan <rryan@mit.edu>
2011-12-18 13:48:51 -0500

    Fix argument formatting in BaseTrackCache and BaseSqlTableModel. Fixes Bug #905776

--- mixxx/src/library/basetrackcache.cpp
+++ mixxx/src/library/basetrackcache.cpp
@@ -7,7 +7,7 @@
 
 namespace {
 
-const bool sDebug = false;
+const bool sDebug = true;
 
 const QHash<QString, int> buildReverseIndex(const QList<QString> items) {
     int i = 0;
@@ -31,8 +31,8 @@ BaseTrackCache::BaseTrackCache(TrackCollection* pTrackCollection,
           m_columns(columns),
           m_columnsJoined(m_columns.join(",")),
           m_columnIndex(buildReverseIndex(m_columns)),
-          m_bIsCaching(isCaching),
           m_bIndexBuilt(false),
+          m_bIsCaching(isCaching),
           m_pTrackCollection(pTrackCollection),
           m_trackDAO(m_pTrackCollection->getTrackDAO()),
           m_database(m_pTrackCollection->getDatabase()) {
@@ -173,7 +173,7 @@ void BaseTrackCache::buildIndex() {
     }
 
     QString queryString = QString("SELECT %1 FROM %2")
-            .arg(m_columnsJoined).arg(m_tableName);
+            .arg(m_columnsJoined, m_tableName);
 
     if (sDebug) {
         qDebug() << this << "buildIndex query:" << queryString;
@@ -208,10 +208,7 @@ void BaseTrackCache::updateTracksInIndex(QSet<int> trackIds) {
     }
 
     QString queryString = QString("SELECT %1 FROM %2 WHERE %3 in (%4)")
-            .arg(m_columnsJoined)
-            .arg(m_tableName)
-            .arg(m_idColumn)
-            .arg(idStrings.join(","));
+            .arg(m_columnsJoined, m_tableName, m_idColumn, idStrings.join(","));
 
     if (sDebug) {
         qDebug() << this << "updateTracksInIndex update query:" << queryString;
@@ -346,7 +343,7 @@ void BaseTrackCache::filterAndSort(const QSet<int>& trackIds,
     QString filter = filterClause(searchQuery, extraFilter, idStrings);
     QString orderBy = orderByClause(sortColumn, sortOrder);
     QString queryString = QString("SELECT %1 FROM %2 %3 %4")
-            .arg(m_idColumn).arg(m_tableName).arg(filter).arg(orderBy);
+            .arg(m_idColumn, m_tableName, filter, orderBy);
 
     if (sDebug) {
         qDebug() << this << "select() executing:" << queryString;
@@ -477,7 +474,7 @@ QString BaseTrackCache::filterClause(QString query, QString extraFilter,
 
     if (idStrings.size() > 0) {
         queryFragments << QString("%1 in (%2)")
-                .arg(m_idColumn).arg(idStrings.join(","));
+                .arg(m_idColumn, idStrings.join(","));
     }
 
     if (!query.isNull() && query != "") {
@@ -492,7 +489,7 @@ QString BaseTrackCache::filterClause(QString query, QString extraFilter,
 
             QStringList columnFragments;
             foreach (QString column, m_searchColumns) {
-                columnFragments << QString("%1 LIKE %2").arg(column).arg(escapedToken);
+                columnFragments << QString("%1 LIKE %2").arg(column, escapedToken);
             }
             tokenFragments << QString("(%1)").arg(columnFragments.join(" OR "));
         }
@@ -515,7 +512,7 @@ QString BaseTrackCache::orderByClause(int sortColumn,
     // this.
     QSqlQuery query(m_database);
     QString queryString = QString("SELECT %1 FROM %2 LIMIT 1")
-            .arg(m_columnsJoined).arg(m_tableName);
+            .arg(m_columnsJoined, m_tableName);
     query.prepare(queryString);
     query.exec();
 
@@ -532,7 +529,7 @@ QString BaseTrackCache::orderByClause(int sortColumn,
         f.name(), QSqlDriver::FieldName);
 
     s.append(QLatin1String("ORDER BY "));
-    QString sort_field = QString("%1.%2").arg(m_tableName).arg(field);
+    QString sort_field = QString("%1.%2").arg(m_tableName, field);
 
     // If the field is a string, sort using its lowercase form so sort is
     // case-insensitive.
