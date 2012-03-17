518ba1b59e37b3f23d75bce0458c58fbbe8c2a25
Sean M. Pappalardo <pegasus@renegadetech.com>
2012-03-18 00:54:51 +0100

    - Prevented redundant MIDI messages from being sent on XML output mappings - Replaced assert with error dialog box when an invalid CO is specified for an XML output mapping - Updated the file version number for Windows (someone needs to make scons generate these lines...I don't know how.) - Added VU meter XML output mapping for the SCS.1m as an example

--- mixxx/src/controllers/midi/midioutputhandler.cpp
+++ mixxx/src/controllers/midi/midioutputhandler.cpp
@@ -29,25 +29,27 @@ MidiOutputHandler::MidiOutputHandler(QString group, QString key,
       m_pController(controller), m_controlno(midino) {
 
     m_cobj = ControlObject::getControl(ConfigKey(group, key));
-
-    if (m_cobj == NULL) {
-        QByteArray err_tmp = QString("Invalid config group: '%1', name: '%2'")
-                                    .arg(group, key).toAscii();
-        Q_ASSERT_X(m_cobj, "MidiOutputHandler", err_tmp);
-        qWarning() << "MidiOutputHandler:" << err_tmp;
-        return;
-    }
-
-    connect(m_cobj, SIGNAL(valueChangedFromEngine(double)), this, SLOT(controlChanged(double)));
-    connect(m_cobj, SIGNAL(valueChanged(double)), this, SLOT(controlChanged(double)));
+    dupes = false;
 }
 
 MidiOutputHandler::~MidiOutputHandler() {
-    ConfigKey cKey = m_cobj->getKey();
-    if (m_pController->debugging()) {
-        qDebug() << QString("Destroying static MIDI output handler on %1 for %2,%3")
-                            .arg(m_pController->getName(), cKey.group, cKey.item);
+    if (m_cobj != NULL) {
+        ConfigKey cKey = m_cobj->getKey();
+        if (m_pController->debugging()) {
+            qDebug() << QString("Destroying static MIDI output handler on %1 for %2,%3")
+                                .arg(m_pController->getName(), cKey.group, cKey.item);
+        }
     }
+}
+
+bool MidiOutputHandler::validate() {
+
+    if (m_cobj == NULL) return false;
+
+    connect(m_cobj, SIGNAL(valueChangedFromEngine(double)), this, SLOT(controlChanged(double)));
+    connect(m_cobj, SIGNAL(valueChanged(double)), this, SLOT(controlChanged(double)));
+    
+    return true;
 }
 
 void MidiOutputHandler::update() {
@@ -55,6 +57,11 @@ void MidiOutputHandler::update() {
 }
 
 void MidiOutputHandler::controlChanged(double value) {
+    // Don't send redundant messages unless asked to
+    if (!dupes && value == m_lastVal) return;
+    
+    m_lastVal = value;
+
     unsigned char byte3 = m_off;
     if (value >= m_min && value <= m_max) { byte3 = m_on; }
 
@@ -65,4 +72,3 @@ void MidiOutputHandler::controlChanged(double value) {
         m_pController->sendShortMsg(m_status, m_controlno, byte3);
     }
 }
-
