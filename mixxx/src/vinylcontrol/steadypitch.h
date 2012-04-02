42cd7d8b1d8fd33b33e0b54624798890f4cb305a
Owen Williams <owilliams@mixxx.org>
2012-04-01 23:31:51 -0400

    Vinyl Control bugfixes:   * fix steady pitch detector to work in reverse and better detect loops   * try not to enter track selection mode when scratching

--- mixxx/src/vinylcontrol/steadypitch.h
+++ mixxx/src/vinylcontrol/steadypitch.h
@@ -13,11 +13,16 @@ class SteadyPitch
         void reset(double pitch, double time);
         double check(double pitch, double time, bool looping);
         double steadyValue(void);
+        bool directionChanged(double pitch);
+        bool resyncDetected(double new_time);
     private:
         double m_dSteadyPitch;
         double m_dOldSteadyPitch;
         double m_dSteadyPitchTime;
+        double m_dLastSteadyDur;
+        double m_dLastTime;
         double m_dPitchThreshold;
+        int m_iPlayDirection;
 };
 
 #endif
