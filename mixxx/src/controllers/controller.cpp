343e4d771381f11799db2fbf090c379d9f186a3a
Sean M. Pappalardo <spappalardo@mixxx.org>
2012-04-07 13:27:36 +0200

    - Removed SCS.1m VU meter XML because script is (suprisingly) faster - Moved XML-handling functions from Controller and MidiController to separate files for clarity - Removed a couple more commented code lines Fixed old MidiMapping holdovers: - Removed useless m_bindings from MidiController - Renamed function to buildXML() to clearly denote what it does and clarified description - Grouped XML functions together where possible in the .h files

--- mixxx/src/controllers/controller.cpp
+++ mixxx/src/controllers/controller.cpp
@@ -18,8 +18,6 @@
 ***************************************************************************/
 
 #include <qapplication.h>   // For command line arguments
-// #include "widget/wwidget.h"    // FIXME: This should be xmlparse.h
-#include "xmlparse.h"
 #include "controller.h"
 #include "defs_controllers.h"
 
@@ -39,13 +37,8 @@ Controller::~Controller() {
 //     close(); // I wish I could put this here to enforce it automatically
 }
 
-QString Controller::defaultPreset() {
-    return PRESETS_PATH.append(m_sDeviceName.replace(" ", "_") + CONTROLLER_PRESET_EXTENSION);
-}
-
-QString Controller::presetExtension() {
-    return CONTROLLER_PRESET_EXTENSION;
-}
+// XML-handling functions in a separate file for clarity
+#include "controller-preset.cpp"
 
 void Controller::startEngine()
 {
@@ -68,79 +61,6 @@ void Controller::stopEngine()
     delete m_pEngine;
     m_pEngine = NULL;
 }
-
-/** loadPreset(bool)
-* Overloaded function for convenience, uses the default device path
-* @param forceLoad Forces the preset to be loaded, regardless of whether or not the controller id
-*        specified within matches the name of this Controller.
-*/
-void Controller::loadPreset(bool forceLoad) {
-    loadPreset(defaultPreset(), forceLoad);
-}
-
-/** loadPreset(QString,bool)
-* Overloaded function for convenience
-* @param path The path to a controller preset XML file.
-* @param forceLoad Forces the preset to be loaded, regardless of whether or not the controller id
-*        specified within matches the name of this Controller.
-*/
-void Controller::loadPreset(QString path, bool forceLoad) {
-    qDebug() << "Loading controller preset from" << path;
-    loadPreset(XmlParse::openXMLFile(path, "controller"), forceLoad);
-}
-
-/** loadPreset(QDomElement,bool)
-* Loads a controller preset from a QDomElement structure.
-* @param root The root node of the XML document for the preset.
-* @param forceLoad Forces the preset to be loaded, regardless of whether or not the controller id
-*        specified within matches the name of this Controller.
-*/
-QDomElement Controller::loadPreset(QDomElement root, bool forceLoad) {
-
-    if (root.isNull()) return root;
-
-    m_scriptFileNames.clear();
-    m_scriptFunctionPrefixes.clear();
-    
-    // For each controller in the DOM
-    QDomElement controller = root.firstChildElement("controller");
-    
-    // For each controller in the preset XML...
-    //(Only parse the <controller> block if its id matches our device name, otherwise
-    //keep looking at the next controller blocks....)
-    QString device;
-    while (!controller.isNull()) {
-        // Get deviceid
-        device = controller.attribute("id","");
-        if (device != rootDeviceName(m_sDeviceName) && !forceLoad) {
-            controller = controller.nextSiblingElement("controller");
-        }
-        else
-            break;
-    }
-    
-    if (!controller.isNull()) {
-        
-        qDebug() << device << "settings found";
-        // Build a list of script files to load
-        
-        QDomElement scriptFile = controller.firstChildElement("scriptfiles").firstChildElement("file");
-        
-        // Default currently required file
-        addScriptFile(REQUIRED_SCRIPT_FILE,"");
-        
-        // Look for additional ones
-        while (!scriptFile.isNull()) {
-            QString functionPrefix = scriptFile.attribute("functionprefix","");
-            QString filename = scriptFile.attribute("filename","");
-            addScriptFile(filename, functionPrefix);
-            
-            scriptFile = scriptFile.nextSiblingElement("file");
-        }
-
-    }
-    return controller;
-}   // END loadPreset(QDomElement)
 
 void Controller::applyPreset() {
     qDebug() << "Applying controller preset...";
@@ -168,63 +88,6 @@ void Controller::applyPreset() {
 void Controller::addScriptFile(QString filename, QString functionprefix) {
     m_scriptFileNames.append(filename);
     m_scriptFunctionPrefixes.append(functionprefix);
-}
-
-void Controller::savePreset() {
-    savePreset(defaultPreset());
-}
-
-void Controller::savePreset(QString path) {
-    qDebug() << "Writing controller preset file" << path;
-    
-    // Need to do this on Windows
-    QDir directory;
-    directory.mkpath(path.left(path.lastIndexOf("/")));
-    
-    QFile output(path);
-    if (!output.open(QIODevice::WriteOnly | QIODevice::Truncate)) return;
-    QTextStream outputstream(&output);
-    // Construct the DOM from the table
-    QDomDocument docPreset = buildDomElement();
-    // Save the DOM to the XML file
-    docPreset.save(outputstream, 4);
-    output.close();
-}
-
-QDomDocument Controller::buildDomElement() {
-
-    QDomDocument doc("Preset");
-    QString blank = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-        "<MixxxControllerPreset schemaVersion=\"" + QString(XML_SCHEMA_VERSION) + "\">\n"
-        "</MixxxControllerPreset>\n";
-
-    doc.setContent(blank);
-
-    QDomElement rootNode = doc.documentElement();
-    QDomElement controller = doc.createElement("controller");
-    // Strip off the serial number
-    controller.setAttribute("id", rootDeviceName(m_sDeviceName));
-    rootNode.appendChild(controller);
-
-    QDomElement scriptFiles = doc.createElement("scriptfiles");
-    controller.appendChild(scriptFiles);
-
-    for (int i = 0; i < m_scriptFileNames.count(); i++) {
-        QString filename = m_scriptFileNames[i];
-
-        //Don't need to write anything for the required mapping file.
-        if (filename != REQUIRED_SCRIPT_FILE) {
-            qDebug() << "  writing script block for" << filename;
-            QString functionPrefix = m_scriptFunctionPrefixes[i];
-            QDomElement scriptFile = doc.createElement("file");
-
-            scriptFile.setAttribute("filename", filename);
-            scriptFile.setAttribute("functionprefix", functionPrefix);
-
-            scriptFiles.appendChild(scriptFile);
-        }
-    }
-    return doc;
 }
 
 void Controller::send(QList<int> data, unsigned int length) {
