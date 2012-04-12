534751cb660e77acf80529496e945a36a71295a0
RJ Ryan <rryan@mixxx.org>
2012-04-12 01:06:16 -0400

    Factor all version string manipulation out of AnalyserBeats. Make the check for whether the settings have changed solely depend on the version/subVersion strings.

--- mixxx/src/track/beatfactory.h
+++ mixxx/src/track/beatfactory.h
@@ -1,6 +1,8 @@
 #ifndef BEATFACTORY_H
 #define BEATFACTORY_H
 
+#include <QHash>
+
 #include "track/beats.h"
 #include "track/beatgrid.h"
 #include "track/beatmatrix.h"
@@ -14,11 +16,23 @@ class BeatFactory {
     static BeatsPointer makeBeatGrid(TrackPointer pTrack,
                                      double dBpm, double dFirstBeatSample);
 
+    static QString getPreferredVersion(
+        const bool bEnableFixedTempoCorrection,
+        const bool bEnableOffsetCorrection,
+        const int iMinBpm, const int iMaxBpm,
+        const QHash<QString, QString> extraVersionInfo);
+
+    static QString getPreferredSubVersion(
+        const bool bEnableFixedTempoCorrection,
+        const bool bEnableOffsetCorrection,
+        const int iMinBpm, const int iMaxBpm,
+        const QHash<QString, QString> extraVersionInfo);
+
     static BeatsPointer makePreferredBeats(
         TrackPointer pTrack, QVector<double> beats,
-        const QString subVersion,
-        bool bEnableFixedTempoCorrection,
-        bool bEnableOffsetCorrection,
+        const QHash<QString, QString> extraVersionInfo,
+        const bool bEnableFixedTempoCorrection,
+        const bool bEnableOffsetCorrection,
         const int iSampleRate, const int iTotalSamples,
         const int iMinBpm, const int iMaxBpm);
 
