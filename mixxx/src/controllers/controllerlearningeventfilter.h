a634da768c0f26b4682534d82d21aad089a5abc8
RJ Ryan <rryan@mit.edu>
2012-04-26 10:48:36 -0400

    Add ControllerLearningEventFilter, a prototype for point-and-click MIDI/HID learning. Any widget clicked in the GUI now prints the control bound to that click on the console.

---
+++ mixxx/src/controllers/controllerlearningeventfilter.h
@@ -0,0 +1,45 @@
+#ifndef CONTROLLERLEARNINGEVENTFILTER_H
+#define CONTROLLERLEARNINGEVENTFILTER_H
+
+#include <QObject>
+#include <QEvent>
+
+#include "controlobject.h"
+#include "controlobjectthreadwidget.h"
+
+struct ControlInfo {
+    ControlInfo()
+            : clickControl(NULL),
+              emitOption(ControlObjectThreadWidget::EMIT_ON_PRESS_AND_RELEASE),
+              leftClickControl(NULL),
+              leftEmitOption(ControlObjectThreadWidget::EMIT_ON_PRESS_AND_RELEASE),
+              rightClickControl(NULL),
+              rightEmitOption(ControlObjectThreadWidget::EMIT_ON_PRESS_AND_RELEASE) {
+    }
+
+    ControlObject* clickControl;
+    ControlObjectThreadWidget::EmitOption emitOption;
+    ControlObject* leftClickControl;
+    ControlObjectThreadWidget::EmitOption leftEmitOption;
+    ControlObject* rightClickControl;
+    ControlObjectThreadWidget::EmitOption rightEmitOption;
+};
+
+class ControllerLearningEventFilter : public QObject {
+    Q_OBJECT
+  public:
+    ControllerLearningEventFilter(QObject* pParent = NULL);
+    virtual ~ControllerLearningEventFilter();
+
+    virtual bool eventFilter(QObject* pObject, QEvent* pEvent);
+
+    void addWidgetClickInfo(QWidget* pWidget, Qt::MouseButton buttonState,
+                            ControlObject* pControl,
+                            ControlObjectThreadWidget::EmitOption emitOption);
+
+  private:
+    QHash<QWidget*, ControlInfo> m_widgetControlInfo;
+};
+
+
+#endif /* CONTROLLERLEARNINGEVENTFILTER_H */
