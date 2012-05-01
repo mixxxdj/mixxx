adac6d8b17a0366d4b91c2222d1c406dd07f8509
RJ Ryan <rryan@mixxx.org>
2012-05-01 01:58:23 -0400

    Minor fix from daschuer.

--- mixxx/src/mixxx.cpp
+++ mixxx/src/mixxx.cpp
@@ -624,9 +624,10 @@ int MixxxApp::noSoundDlg(void)
             msgBox.show();
 
         } else if (msgBox.clickedButton() == exitButton) {
-            return 1;
+            break;
         }
     }
+    return 1;
 }
 
 int MixxxApp::noOutputDlg(bool *continueClicked)
@@ -675,9 +676,10 @@ int MixxxApp::noOutputDlg(bool *continueClicked)
             msgBox.show();
 
         } else if (msgBox.clickedButton() == exitButton) {
-            return 1;
+            break;
         }
     }
+    return 1;
 }
 
 QString buildWhatsThis(QString title, QString text) {
