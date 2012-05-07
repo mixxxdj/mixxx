6217b4853d3ce2653fb9267614d7934c30179627
RJ Ryan <rryan@mixxx.org>
2012-05-07 01:30:14 -0400

    Re-work Beats::findBeats to not append to a QList<double>. Instead, return a BeatIterator which calculates the beat positions in the case of BeatGrid and in the case of BeatMap just iterates the internal beat list.

--- mixxx/src/waveform/renderers/waveformrenderbeat.h
+++ mixxx/src/waveform/renderers/waveformrenderbeat.h
@@ -4,25 +4,23 @@
 #include <QColor>
 
 #include "waveform/renderers/waveformrendererabstract.h"
-#include "track/beats.h"
 #include "util.h"
 
 class ControlObjectThreadMain;
 
 class WaveformRenderBeat : public WaveformRendererAbstract {
   public:
-    explicit WaveformRenderBeat( WaveformWidgetRenderer* waveformWidgetRenderer);
+    explicit WaveformRenderBeat(WaveformWidgetRenderer* waveformWidgetRenderer);
     virtual ~WaveformRenderBeat();
 
     virtual void init();
-    virtual void setup( const QDomNode& node);
-    virtual void draw( QPainter* painter, QPaintEvent* event);
+    virtual void setup(const QDomNode& node);
+    virtual void draw(QPainter* painter, QPaintEvent* event);
 
   private:
     QColor m_beatColor;
     QColor m_highBeatColor;
     ControlObjectThreadMain* m_beatActive;
-    SampleList m_beatsCache;
 
     DISALLOW_COPY_AND_ASSIGN(WaveformRenderBeat);
 };
