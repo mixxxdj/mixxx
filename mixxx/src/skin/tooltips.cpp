783bd39fea27f7750d611d9d552f8f549be930e3
jus <jus@justmail.de>
2012-05-08 08:35:50 +0200

    Add bpm_tap_visual_bpm TooltipId to tooltips.cpp . This is a special case, in some skins (e.g. Deere) we display a transparent png for bpm_tap on top of visual_bpm

--- mixxx/src/skin/tooltips.cpp
+++ mixxx/src/skin/tooltips.cpp
@@ -198,6 +198,12 @@ void Tooltips::addStandardTooltips() {
             << tr("BPM Tap")
             << tr("When tapped repeatedly, adjusts the BPM to match the tapped BPM");
 
+    //this is a special case, in some skins (e.g. Deere) we display a transparent png for bpm_tap on top of visual_bpm
+    add("bpm_tap_visual_bpm")
+            << tr("Tempo and BPM Tap")
+            << tr("Displays the tempo of the loaded track in BPM (beats per minute)");
+            << tr("When tapped repeatedly, adjusts the BPM to match the tapped BPM");
+
     add("show_spinny")
             << tr("Spinning Vinyl")
             << tr("Show/hide the spinning vinyl section");
