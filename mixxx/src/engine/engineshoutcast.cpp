572f537290ea3cc88f8da56c26fe6058fd7dbd29
Bill Good <bkgood@gmail.com>
2012-05-13 14:18:42 -0500

    Added unistd.h include for sleep(3) on non-Windows

--- mixxx/src/engine/engineshoutcast.cpp
+++ mixxx/src/engine/engineshoutcast.cpp
@@ -23,6 +23,8 @@
     #include <windows.h>
     //sleep on linux assumes seconds where as Sleep on Windows assumes milliseconds
     #define sleep(x) Sleep(x*1000)
+#else
+    #include <unistd.h>
 #endif
 
 #include "engine/engineshoutcast.h"
