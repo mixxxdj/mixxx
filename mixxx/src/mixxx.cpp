38bc848c9500d1fa9ee7258d5b967a56df463aab
RJ Ryan <rryan@mixxx.org>
2012-05-14 18:20:37 -0400

    If QLocale::uiLanguages() has English as the first language then do not install a translation. This led us to installing translations inappropriately because we don't have a mixxx_en.qm and sometimes the user won't have a Qt english translation file.

--- mixxx/src/mixxx.cpp
+++ mixxx/src/mixxx.cpp
@@ -67,6 +67,15 @@ bool loadTranslations(const QLocale& systemLocale, QString userLocale,
 
     if (userLocale.size() == 0) {
 #if QT_VERSION >= 0x040800
+        QStringList uiLanguages = systemLocale.uiLanguages();
+        if (uiLanguages.size() > 0 && uiLanguages.first() == "en") {
+            // Don't bother loading a translation if the first ui-langauge is
+            // English because the interface is already in English. This fixes
+            // the case where the user's install of Qt doesn't have an explicit
+            // English translation file and the fact that we don't ship a
+            // mixxx_en.qm.
+            return;
+        }
         return pTranslator->load(systemLocale, translation, prefix, translationPath);
 #else
         userLocale = systemLocale.name();
@@ -217,7 +226,7 @@ MixxxApp::MixxxApp(QApplication *a, struct CmdlineArgs args)
             defaultKeyboard = QString(SETTINGS_PATH).append("keyboard/").append("en_US.kbd.cfg");
             if (!QFile::exists(defaultKeyboard)) {
                 qDebug() << defaultKeyboard << " not found, starting without shortcuts";
-                defaultKeyboard = ""; 
+                defaultKeyboard = "";
             }
         }
         m_pKbdConfig = new ConfigObject<ConfigValueKbd>(defaultKeyboard);
