53c0c2c60f06058d3096d86eda4a1e8fa6ab2205
Owen Williams <owilliams@mixxx.org>
2012-06-10 22:20:19 -0400

    Un-personalize config dir

--- mixxx/build/depends.py
+++ mixxx/build/depends.py
@@ -742,7 +742,7 @@ class MixxxCore(Feature):
         if build.platform_is_linux or \
                 build.platform_is_bsd:
             mixxx_files = [
-                ('SETTINGS_PATH','.mixxx-trunk/'),
+                ('SETTINGS_PATH','.mixxx/'),
                 ('BPMSCHEME_FILE','mixxxbpmscheme.xml'),
                 ('SETTINGS_FILE', 'mixxx.cfg'),
                 ('TRACK_FILE', 'mixxxtrack.xml')]
