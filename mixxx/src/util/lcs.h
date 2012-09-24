b8639c0fc563371c0464e45bc9e5df610740df26
RJ Ryan <rryan@mit.edu>
2012-09-24 01:09:04 -0400

    Replace algorithm for guessing translation rule for foreign iTunes configurations with one that uses the largest-common-substring between the iTunes XML path and the iTunes 'Music Folder' path. Also add tests for LCS implementation. Fixes Bug #1047178

---
+++ mixxx/src/util/lcs.h
@@ -0,0 +1,36 @@
+#include <cstring>
+
+#include <QString>
+
+// Returns the longest common substring shared between a and b. Does not support
+// finding multiple longest common substrings.
+inline QString LCS(const QString& a, const QString& b) {
+    const size_t m = a.size();
+    const size_t n = b.size();
+    const size_t rows = m + 1;
+    const size_t cols = n + 1;
+
+    size_t M[rows][cols];
+    size_t longest = 0;
+    size_t longest_loc = 0;
+
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            M[i][j] = 0;
+        }
+    }
+    for (size_t i = 1; i <= m; ++i) {
+        for (size_t j = 1; j <= n; ++j) {
+            if (a.at(i-1) == b.at(j-1)) {
+                M[i][j] = M[i-1][j-1] + 1;
+                if (M[i][j] > longest) {
+                    longest = M[i][j];
+                    longest_loc = i;
+                }
+            } else {
+                M[i][j] = 0;
+            }
+        }
+    }
+    return a.mid(longest_loc - longest, longest);
+}
