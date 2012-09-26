f8901a371370408100d7d34f945d971623e5550a
Owen Williams <owilliams@mixxx.org>
2012-09-26 11:40:04 -0400

    Update xwax to version 1.2

--- mixxx/lib/xwax/debug.h
+++ mixxx/lib/xwax/debug.h
@@ -17,26 +17,17 @@
  *
  */
 
-#ifndef LUT_H
-#define LUT_H
+#ifndef DEBUG_H
+#define DEBUG_H
 
-typedef unsigned int slot_no_t;
+#include <stdio.h>
 
-struct slot {
-    unsigned int timecode;
-    slot_no_t next; /* next slot with the same hash */
-};
-
-struct lut {
-    struct slot *slot;
-    slot_no_t *table, /* hash -> slot lookup */
-        avail; /* next available slot */
-};
-
-int lut_init(struct lut *lut, int nslots);
-void lut_clear(struct lut *lut);
-
-void lut_push(struct lut *lut, unsigned int timecode);
-unsigned int lut_lookup(struct lut *lut, unsigned int timecode);
+#ifdef DEBUG
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define dassert(x) assert(x)
+#else
+#define debug(...)
+#define dassert(x)
+#endif
 
 #endif
