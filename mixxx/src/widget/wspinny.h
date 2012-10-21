003a5d20a6032ad7c9e30fb9ad5f64469a0674b8
RJ Ryan <rryan@mit.edu>
2012-10-21 15:33:55 -0400

    Make WSpinny's ghost mode use the actual engine slip feature instead of the hack it had. An added benefit is that if slip is enabled on some other feature like a beatloop-roll then the slip marker will show up as a ghost on the spinny. Fixes Bug #1067498

--- mixxx/src/widget/wspinny.h
+++ mixxx/src/widget/wspinny.h
@@ -12,79 +12,84 @@
 
 class ControlObjectThreadMain;
 
-class WSpinny : public QGLWidget
-{
+class WSpinny : public QGLWidget {
     Q_OBJECT
-    public:
-        WSpinny(QWidget* parent, VinylControlManager* pVCMan);
-        virtual ~WSpinny();
-        void setup(QDomNode node, QString group);
-        void dragEnterEvent(QDragEnterEvent *event);
-        void dropEvent(QDropEvent *event);
-    public slots:
-        void updateAngle(double);
-        void updateRate(double);
-        void updateAngleForGhost();
-        void updateVinylControlSpeed(double rpm);
-        void updateVinylControlEnabled(double enabled);
-        void invalidateVinylControl();
-    signals:
-        void trackDropped(QString filename, QString group);
-    protected:
-        //QWidget:
-        void paintEvent(QPaintEvent*);
-        void mouseMoveEvent(QMouseEvent * e);
-        void mousePressEvent(QMouseEvent * e);
-        void mouseReleaseEvent(QMouseEvent * e);
-        void wheelEvent(QWheelEvent *e);
+  public:
+    WSpinny(QWidget* parent, VinylControlManager* pVCMan);
+    virtual ~WSpinny();
 
-        double calculateAngle(double playpos);
-        int calculateFullRotations(double playpos);
-        double calculatePositionFromAngle(double angle);
-    private:
-        QPixmap* m_pBG;
-        QPixmap* m_pFG;
-        QPixmap* m_pGhost;
-        ControlObjectThreadMain* m_pPlay;
-        ControlObjectThreadMain* m_pPlayPos;
-        ControlObjectThreadMain* m_pVisualPlayPos;
-        ControlObjectThreadMain* m_pDuration;
-        ControlObjectThreadMain* m_pTrackSamples;
-        ControlObjectThreadMain* m_pTrackSampleRate;
-        ControlObjectThreadMain* m_pBPM;
-        ControlObjectThreadMain* m_pScratch;
-        ControlObjectThreadMain* m_pScratchToggle;
-        ControlObjectThreadMain* m_pScratchPos;
-        ControlObjectThreadMain* m_pRate;
-        ControlObjectThreadMain* m_pVinylControlSpeedType;
-        ControlObjectThreadMain* m_pVinylControlEnabled;
-        ControlObjectThreadMain* m_pSignalEnabled;
+    void setup(QDomNode node, QString group);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+
+  public slots:
+    // Update the playback angle saved in the widget and repaint. Playpos is a
+    // normalized (0.0-1.0) playback position. (Not an angle!)
+    void updateAngleFromPlaypos(double playpos);
+    void updateGhostAngleFromPlaypos(double playpos);
+
+    void updateRate(double);
+    void updateVinylControlSpeed(double rpm);
+    void updateVinylControlEnabled(double enabled);
+    void invalidateVinylControl();
+
+  signals:
+    void trackDropped(QString filename, QString group);
+
+  protected:
+    //QWidget:
+    void paintEvent(QPaintEvent*);
+    void mouseMoveEvent(QMouseEvent * e);
+    void mousePressEvent(QMouseEvent * e);
+    void mouseReleaseEvent(QMouseEvent * e);
+    void wheelEvent(QWheelEvent *e);
+
+    double calculateAngle(double playpos);
+    int calculateFullRotations(double playpos);
+    double calculatePositionFromAngle(double angle);
+
+  private:
+    QPixmap* m_pBG;
+    QPixmap* m_pFG;
+    QPixmap* m_pGhost;
+    ControlObjectThreadMain* m_pPlay;
+    ControlObjectThreadMain* m_pPlayPos;
+    ControlObjectThreadMain* m_pVisualPlayPos;
+    ControlObjectThreadMain* m_pDuration;
+    ControlObjectThreadMain* m_pTrackSamples;
+    ControlObjectThreadMain* m_pTrackSampleRate;
+    ControlObjectThreadMain* m_pBPM;
+    ControlObjectThreadMain* m_pScratch;
+    ControlObjectThreadMain* m_pScratchToggle;
+    ControlObjectThreadMain* m_pScratchPos;
+    ControlObjectThreadMain* m_pRate;
+    ControlObjectThreadMain* m_pVinylControlSpeedType;
+    ControlObjectThreadMain* m_pVinylControlEnabled;
+    ControlObjectThreadMain* m_pSignalEnabled;
+    ControlObjectThreadMain* m_pSlipEnabled;
+    ControlObjectThreadMain* m_pSlipPosition;
 
 #ifdef __VINYLCONTROL__
-        VinylControlManager* m_pVCManager;
-        VinylControlProxy* m_pVinylControl;
+    VinylControlManager* m_pVCManager;
+    VinylControlProxy* m_pVinylControl;
 #endif
-        double m_dInitialPos;
-        bool m_bVinylActive;
-        bool m_bSignalActive;
-        QImage m_qImage;
-        int m_iSize;
-        int m_iTimerId;
-        int m_iSignalUpdateTick;
-        QString m_group;
-        float m_fAngle; //Degrees
-        float m_fGhostAngle;
-        QTime m_time;
-        double m_dPausedPosition;
-        bool m_bGhostPlayback;
-        QTimer m_ghostPaintTimer;
-        int m_iStartMouseX;
-        int m_iStartMouseY;
-        int m_iFullRotations;
-        double m_dPrevTheta;
-        double m_dTheta;
-        /** Speed of the vinyl rotation. */
-        double m_dRotationsPerSecond;
+    double m_dInitialPos;
+    bool m_bVinylActive;
+    bool m_bSignalActive;
+    QImage m_qImage;
+    int m_iSize;
+    int m_iTimerId;
+    int m_iSignalUpdateTick;
+    QString m_group;
+    float m_fAngle; //Degrees
+    float m_fGhostAngle;
+    int m_iStartMouseX;
+    int m_iStartMouseY;
+    int m_iFullRotations;
+    double m_dPrevTheta;
+    double m_dTheta;
+    /** Speed of the vinyl rotation. */
+    double m_dRotationsPerSecond;
 };
 
 #endif //_WSPINNY_H
