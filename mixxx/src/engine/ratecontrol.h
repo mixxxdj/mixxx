300152f69296e9912e18af74266df48b9d2566e4
RJ Ryan <rryan@mixxx.org>
2012-11-06 21:55:01 -0500

    Initialize boolean vinyl control flags in RateControl. This prevents decks / samplers from pseudo-randomly being stuck. Fixes Bug #1013768

--- mixxx/src/engine/ratecontrol.h
+++ mixxx/src/engine/ratecontrol.h
@@ -82,9 +82,6 @@ public:
     void resetRateTemp(void);
     /** Get the 'Raw' Temp Rate */
     double getTempRate(void);
-    /** Is vinyl control enabled? **/
-    bool m_bVinylControlEnabled;
-    bool m_bVinylControlScratching;
 
     /** Values used when temp and perm rate buttons are pressed */
     static double m_dTemp, m_dTempSmall, m_dPerm, m_dPermSmall;
@@ -139,13 +136,17 @@ public:
         RATERAMP_RAMPBACK_PERIOD
     };
 
+    /** Is vinyl control enabled? **/
+    bool m_bVinylControlEnabled;
+    bool m_bVinylControlScratching;
+
     // The current rate ramping direction. Only holds the last button pressed.
     int m_ePbCurrent;
     //  The rate ramping buttons which are currently being pressed.
     int m_ePbPressed;
 
     /** This is true if we've already started to ramp the rate */
-    int m_bTempStarted;
+    bool m_bTempStarted;
     /** Set to the rate change used for rate temp */
     double m_dTempRateChange;
     /** Set the Temporary Rate Change Mode */
@@ -165,10 +166,6 @@ public:
       * pressed, because there is a fixed limit on the range of the pitch
       * slider */
     double m_dOldRate;
-
-
-    /** Handle for configuration */
-    ConfigObject<ConfigValue>* m_pConfig;
 };
 
 #endif /* RATECONTROL_H */
