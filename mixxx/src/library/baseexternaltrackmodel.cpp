694ac223e72a67f7a74711afa4d874db5797d9f0
RJ Ryan <rryan@mixxx.org>
2012-11-22 01:59:01 -0500

    Get rid of the massive code duplication across RB/iTunes/Traktor by making BaseExternalPlaylistModel / BaseExternalTrackModel.

--- mixxx/src/library/baseexternaltrackmodel.cpp
+++ mixxx/src/library/baseexternaltrackmodel.cpp
@@ -1,34 +1,31 @@
-#include <QtCore>
-#include <QtGui>
-#include <QtSql>
-
+#include "library/baseexternaltrackmodel.h"
 #include "library/trackcollection.h"
-#include "library/traktor/traktortablemodel.h"
-#include "track/beatfactory.h"
-#include "track/beats.h"
 
-TraktorTableModel::TraktorTableModel(QObject* parent,
-                                     TrackCollection* pTrackCollection)
+BaseExternalTrackModel::BaseExternalTrackModel(QObject* parent,
+                                               TrackCollection* pTrackCollection,
+                                               QString settingsNamespace,
+                                               QString trackTable,
+                                               QString trackSource)
         : BaseSqlTableModel(parent, pTrackCollection,
                             pTrackCollection->getDatabase(),
-                            "mixxx.db.model.traktor_tablemodel"),
+                            settingsNamespace),
           m_pTrackCollection(pTrackCollection),
           m_database(m_pTrackCollection->getDatabase()) {
-    connect(this, SIGNAL(doSearch(const QString&)), this,
-            SLOT(slotSearch(const QString&)));
+    connect(this, SIGNAL(doSearch(const QString&)),
+            this, SLOT(slotSearch(const QString&)));
 
     QStringList columns;
     columns << "id";
-    setTable("traktor_library", columns[0], columns,
-             m_pTrackCollection->getTrackSource("traktor"));
+    setTable(trackTable, columns[0], columns,
+             m_pTrackCollection->getTrackSource(trackSource));
     setDefaultSort(fieldIndex("artist"), Qt::AscendingOrder);
     initHeaderData();
 }
 
-TraktorTableModel::~TraktorTableModel() {
+BaseExternalTrackModel::~BaseExternalTrackModel() {
 }
 
-TrackPointer TraktorTableModel::getTrack(const QModelIndex& index) const {
+TrackPointer BaseExternalTrackModel::getTrack(const QModelIndex& index) const {
     QString artist = index.sibling(index.row(), fieldIndex("artist")).data().toString();
     QString title = index.sibling(index.row(), fieldIndex("title")).data().toString();
     QString album = index.sibling(index.row(), fieldIndex("album")).data().toString();
@@ -74,36 +71,33 @@ TrackPointer TraktorTableModel::getTrack(const QModelIndex& index) const {
     return pTrack;
 }
 
-void TraktorTableModel::search(const QString& searchText) {
-    // qDebug() << "TraktorTableModel::search()" << searchText
+void BaseExternalTrackModel::search(const QString& searchText) {
+    // qDebug() << "BaseExternalTrackModel::search()" << searchText
     //          << QThread::currentThread();
     emit(doSearch(searchText));
 }
 
-void TraktorTableModel::slotSearch(const QString& searchText) {
+void BaseExternalTrackModel::slotSearch(const QString& searchText) {
     BaseSqlTableModel::search(searchText);
 }
 
-bool TraktorTableModel::isColumnInternal(int column) {
+bool BaseExternalTrackModel::isColumnInternal(int column) {
     if (column == fieldIndex(LIBRARYTABLE_ID)) {
         return true;
     }
     return false;
 }
 
-Qt::ItemFlags TraktorTableModel::flags(const QModelIndex &index) const {
+Qt::ItemFlags BaseExternalTrackModel::flags(const QModelIndex &index) const {
     return readOnlyFlags(index);
 }
 
-bool TraktorTableModel::isColumnHiddenByDefault(int column) {
-    if (column == fieldIndex(LIBRARYTABLE_KEY) ||
-        column == fieldIndex(LIBRARYTABLE_BITRATE)) {
-        return true;
-    }
+bool BaseExternalTrackModel::isColumnHiddenByDefault(int column) {
+    Q_UNUSED(column);
     return false;
 }
 
-TrackModel::CapabilitiesFlags TraktorTableModel::getCapabilities() const {
+TrackModel::CapabilitiesFlags BaseExternalTrackModel::getCapabilities() const {
     // See src/library/trackmodel.h for the list of TRACKMODELCAPS
     return TRACKMODELCAPS_NONE
             | TRACKMODELCAPS_ADDTOPLAYLIST
