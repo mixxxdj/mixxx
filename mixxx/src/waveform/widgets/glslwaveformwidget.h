3fb262e68e190f31997a14d82d0c7f9a2882c245
RJ Ryan <rryan@mixxx.org>
2012-12-06 01:35:21 -0500

    Fix getType() for GLSLWaveformWidget and GLWaveformWidget.

--- mixxx/src/waveform/widgets/glslwaveformwidget.h
+++ mixxx/src/waveform/widgets/glslwaveformwidget.h
@@ -13,13 +13,13 @@ class GLSLWaveformWidget : public QGLWidget, public WaveformWidgetAbstract {
     GLSLWaveformWidget(const char* group, QWidget* parent);
     virtual ~GLSLWaveformWidget();
 
-    virtual WaveformWidgetType::Type getType() const { return WaveformWidgetType::GLWaveform;}
+    virtual WaveformWidgetType::Type getType() const { return WaveformWidgetType::GLSLWaveform;}
 
     static inline QString getWaveformWidgetName() { return tr("Filtered") + " - " + tr("experimental");}
     static inline bool useOpenGl() { return true;}
     static inline bool useOpenGLShaders() { return true;}
 
-    virtual void resize( int width, int height);
+    virtual void resize(int width, int height);
 
   protected:
     virtual void castToQWidget();
