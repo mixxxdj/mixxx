d6114dbb01b5428106022fc9956dda9c601cb1a5
RJ Ryan <rryan@mit.edu>
2012-12-10 02:13:01 -0500

    In ControlObjectThreadMain, if in setExtern we are already in the main thread, do not post the event to the main thread, just invoke the update. Now that ControlObject::sync runs in the main thread, this saves on posting an event to the event queue for every ControlObjectThreadMain attached to a given control that is updated.

--- mixxx/src/controlobjectthreadmain.cpp
+++ mixxx/src/controlobjectthreadmain.cpp
@@ -1,19 +1,11 @@
-//
-// C++ Implementation: controlobjecthreadmainp.cpp
-//
-// Description:
-//
-//
 // Author: Tue Haste Andersen <haste@diku.dk>, (C) 2004
-//
 // Copyright: See COPYING file that comes with this distribution
-//
-//
 
 #include <QApplication>
 #include <QtDebug>
-//Added by qt3to4:
 #include <QEvent>
+#include <QThread>
+
 #include "controlobjectthreadmain.h"
 #include "controlobject.h"
 #include "controlevent.h"
@@ -44,7 +36,14 @@ bool ControlObjectThreadMain::eventFilter(QObject * o, QEvent * e)
 
 bool ControlObjectThreadMain::setExtern(double v)
 {
-    //qDebug() << "set extern main";
-    QApplication::postEvent(this, new ControlEvent(v));
+    // If we are already running in the main thread, then go ahead and update.
+    if (QThread::currentThread() == QApplication::instance()->thread()) {
+        m_dValue = v;
+        emit(valueChanged(v));
+    } else {
+        // Otherwise, we have to post the event to the main thread event queue
+        // and then catch the event via eventFilter.
+        QApplication::postEvent(this, new ControlEvent(v));
+    }
     return true;
 }
