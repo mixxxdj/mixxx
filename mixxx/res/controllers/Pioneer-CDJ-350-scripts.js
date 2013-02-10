1ae0bb1f29486fa1a411cfde8317974e380634c7
RJ Ryan <rryan@mixxx.org>
2013-02-10 00:41:55 -0500

    Add update for CDJ-350 preset by DJ Tiger (and add forum link).

--- mixxx/res/controllers/Pioneer-CDJ-350-scripts.js
+++ mixxx/res/controllers/Pioneer-CDJ-350-scripts.js
@@ -1,5 +1,7 @@
 function PioneerCDJ350() {}
 
+PioneerCDJ350.playlistMode = false;
+
 PioneerCDJ350.init = function(id) {}
 PioneerCDJ350.shutdown = function(id) {}
 
@@ -13,6 +15,64 @@ PioneerCDJ350.jog_wheel = function(channel, control, value, status, group)
     engine.setValue(group, "jog", newValue);
 };
 
+PioneerCDJ350.select_turn = function(channel, control, value, status, group)
+{
+    // CCW:  98 -> 127
+    // CW:    1 ->  30
+    
+    if (value > 64)
+    {
+        value -= 128;
+    }
+    
+    if (PioneerCDJ350.playlistMode) // playlist
+    {
+        engine.setValue("[Playlist]", "SelectTrackKnob", value);
+    }
+    else // tree
+    {
+        if (value > 0)
+        {
+            engine.setValue("[Playlist]", "SelectNextPlaylist", 1);
+        }
+        else if (value < 0)
+        {
+            engine.setValue("[Playlist]", "SelectPrevPlaylist", 1);
+        }
+    }
+};
+
+PioneerCDJ350.select_push = function(channel, control, value, status, group)
+{
+    if (value == 0x00)
+    {
+        return;
+    }
+    
+    if (PioneerCDJ350.playlistMode) // playlist
+    {
+        engine.setValue(group, "LoadSelectedTrack", 1);
+    }
+    else // tree
+    {
+        PioneerCDJ350.playlistMode = true;
+    }
+};
+
+PioneerCDJ350.exit_playlist = function(channel, control, value, status, group)
+{
+    PioneerCDJ350.playlistMode = false;
+};
+
+
+PioneerCDJ350.toggle_playlist = function(channel, control, value, status, group)
+{
+    if (value == 0x00)
+    {
+        return;
+    }
+    PioneerCDJ350.playlistMode = !PioneerCDJ350.playlistMode;
+};
 PioneerCDJ350.tempo_btn = function(channel, control, value, status, group)
 {
     if (value == 0x00)
@@ -37,9 +97,17 @@ PioneerCDJ350.tempo_btn = function(channel, control, value, status, group)
 
 PioneerCDJ350.loop_end_minus = function(channel, control, value, status, group)
 {
-    engine.setValue(group, "loop_end_position", engine.getValue(group, "loop_end_position") - 255);
+    if (value == 0x00)
+    {
+        return;
+    }
+    engine.setValue(group, "loop_end_position", engine.getValue(group, "loop_end_position") - 256);
 };
 PioneerCDJ350.loop_end_plus = function(channel, control, value, status, group)
 {
-    engine.setValue(group, "loop_end_position", engine.getValue(group, "loop_end_position") + 255);
+    if (value == 0x00)
+    {
+        return;
+    }
+    engine.setValue(group, "loop_end_position", engine.getValue(group, "loop_end_position") + 256);
 };
