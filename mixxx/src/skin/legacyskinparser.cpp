2ff2c8b5f2fc40931a37dd078a9e1f8ebbaf94e3
Daniel Schürmann <daschuer@gmx.de>
2013-04-06 01:13:45 +0200

    added a dummy spinny when system has no openGL support. Possible fix for Bug #1160353

--- mixxx/src/skin/legacyskinparser.cpp
+++ mixxx/src/skin/legacyskinparser.cpp
@@ -904,6 +904,13 @@ QWidget* LegacySkinParser::parseSpinny(QDomElement node) {
     QString channelStr = lookupNodeGroup(node);
     const char* pSafeChannelStr = safeChannelString(channelStr);
     WSpinny* spinny = new WSpinny(m_pParent, m_pVCManager);
+    if (!spinny->isValid()) {
+        delete spinny;
+        QLabel* dummy = new QLabel(m_pParent);
+        //: Shown when Spinny can not be displayd. Please keep \n unchanged
+        dummy->setText(tr("No OpenGL\nsupport."));
+        return dummy;
+    }
     setupWidget(node, spinny);
 
     WaveformWidgetFactory::instance()->addTimerListener(spinny);
