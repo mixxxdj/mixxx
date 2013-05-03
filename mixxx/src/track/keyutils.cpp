572bb7d382075f727b6a3781ac20e1a0d6bc6950
RJ Ryan <rryan@mixxx.org>
2013-05-03 00:54:45 -0400

    Add some utility methods.

--- mixxx/src/track/keyutils.cpp
+++ mixxx/src/track/keyutils.cpp
@@ -1,7 +1,41 @@
 #include <QtDebug>
+#include <QMap>
 
 #include "track/keyutils.h"
 
+const char* keyDebugName(mixxx::track::io::key::ChromaticKey key) {
+    static const char *keyNames[] = {"INVALID", "C","C#","D","D#","E","F","F#","G","G#","A","A#","B","c","c#","d","d#","e","f","f#","g","g#","a","a#","b"};
+    return keyNames[static_cast<int>(key)];
+}
+
 mixxx::track::io::key::ChromaticKey guessKeyFromText(const QString& text) {
     return mixxx::track::io::key::INVALID;
+}
+
+mixxx::track::io::key::ChromaticKey calculateGlobalKey(
+    const KeyChangeList& key_changes, const int iTotalSamples) {
+    const int iTotalFrames = iTotalSamples / 2;
+    QMap<mixxx::track::io::key::ChromaticKey, double> key_histogram;
+
+    for (int i = 0; i < key_changes.size(); ++i) {
+        mixxx::track::io::key::ChromaticKey key = key_changes[i].first;
+        const double start_frame = key_changes[i].second;
+        const double next_frame = (i == key_changes.size() - 1) ?
+                iTotalFrames : key_changes[i+1].second;
+        key_histogram[key] += (next_frame - start_frame);
+    }
+
+
+    double max_delta = 0;
+    mixxx::track::io::key::ChromaticKey max_key = mixxx::track::io::key::INVALID;
+    qDebug() << "Key Histogram";
+    for (QMap<mixxx::track::io::key::ChromaticKey, double>::const_iterator it = key_histogram.begin();
+         it != key_histogram.end(); ++it) {
+        qDebug() << it.key() << ":" << keyDebugName(it.key()) << it.value();
+        if (it.value() > max_delta) {
+            max_key = it.key();
+            max_delta = it.value();
+        }
+    }
+    return max_key;
 }
--- mixxx/src/track/keyutils.h
+++ mixxx/src/track/keyutils.h
@@ -4,8 +4,13 @@
 #include <QString>
 
 #include "proto/keys.pb.h"
+#include "track/keys.h"
+
+const char* keyDebugName(mixxx::track::io::key::ChromaticKey key);
 
 mixxx::track::io::key::ChromaticKey guessKeyFromText(const QString& text);
 
+mixxx::track::io::key::ChromaticKey calculateGlobalKey(
+    const KeyChangeList& key_changes, int iTotalSamples);
 
 #endif /* KEYUTILS_H */
