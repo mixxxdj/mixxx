57b4c03ac89a7bf0d0e355ba704568dff97bf180
RJ Ryan <rryan@mixxx.org>
2013-05-07 00:35:42 -0400

    Bring back EnginePassthrough since we could use it for an auxiliary input.

--- mixxx/build/depends.py
+++ mixxx/build/depends.py
@@ -472,6 +472,7 @@ class MixxxCore(Feature):
                    "engine/enginexfader.cpp",
                    "engine/enginemicrophone.cpp",
                    "engine/enginedeck.cpp",
+                   "engine/enginepassthrough.cpp",
 
                    "engine/enginecontrol.cpp",
                    "engine/ratecontrol.cpp",
