d8995b505a7a1d85ce9e9df3fc81007e7120da30
Owen Williams <owilliams@mixxx.org>
2013-05-19 23:17:04 -0400

    possibly wrong fix for locking issue, remove script since it always changes
    Conflicts:
    	mixxx/res/controllers/Xone-K2-scripts.js

--- mixxx/src/vinylcontrol/vinylcontrolmanager.cpp
+++ mixxx/src/vinylcontrol/vinylcontrolmanager.cpp
@@ -164,19 +164,18 @@ void VinylControlManager::onInputDisconnected(AudioInput input) {
 }
 
 void VinylControlManager::reloadConfig() {
-    m_proxiesLock.lockForWrite();
     for (int i = 0; i < m_proxies.size(); ++i) {
         if (!m_proxies.at(i)) continue;
+        m_proxiesLock.lockForWrite();
         VinylControlProxy *pProxy = m_proxies.at(i);
         QString group = kVCProxyGroup.arg(i + 1);
         m_proxiesLock.unlock();
-        // Unlock to avoid deadlock.
         delete pProxy;
-        pProxy = new VinylControlProxy(m_pConfig, group);
         m_proxiesLock.lockForWrite();
+        pProxy = new VinylControlProxy(m_pConfig, group);
         m_proxies.replace(i, pProxy);
+        m_proxiesLock.unlock();
     }
-    m_proxiesLock.unlock();
 }
 
 QList<VinylControlProxy*> VinylControlManager::vinylControlProxies() {
