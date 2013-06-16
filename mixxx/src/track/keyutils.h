f8264c00c3a5d4f5da977ed44ad179fe79f2f7e5
RJ Ryan <rryan@mixxx.org>
2013-06-16 16:39:56 -0400

    Add KeyUtils methods for dealing with octave changes.

--- mixxx/src/track/keyutils.h
+++ mixxx/src/track/keyutils.h
@@ -2,6 +2,7 @@
 #define KEYUTILS_H
 
 #include <QString>
+#include <cmath>
 
 #include "proto/keys.pb.h"
 #include "track/keys.h"
@@ -42,6 +43,28 @@ class KeyUtils {
 
     static void setNotation(
         const QMap<mixxx::track::io::key::ChromaticKey, QString>& notation);
+
+    // Returns pow(2, octaveChange)
+    static inline double octaveChangeToPowerOf2(const double& octaveChange) {
+        // Some libraries (e.g. SoundTouch) calculate pow(2, octaveChange)
+        // using this identity:
+        // x^y = e^(y*ln(x))
+        // Presumably because exp() is faster than pow() on some machines.
+        // TODO(rryan) benchmark on Intel / AMD / ARM processors and pick accordingly.
+        //static const lg2 = log(2); // 0.69314718056f
+        //return exp(lg2 * octaveChange);
+        return pow(2, octaveChange);
+    }
+
+    static inline double powerOf2ToOctaveChange(const double& power_of_2) {
+        // log2 is in the C99 standard, MSVC only supports C90.
+#ifdef _MSC_VER
+        static const double lg2 = log(2.0);
+        return log(power_of_2) / lg2;
+#else
+        return log2(power_of_2);
+#endif
+    }
 
   private:
     static QMutex s_notationMutex;
