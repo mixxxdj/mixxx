c59f6e240ecb6f68e21c8694ae36d30a15af2770
RJ Ryan <rryan@mixxx.org>
2013-06-17 00:59:03 -0400

    Remove src/util.h.moved.

--- mixxx/src/util.h
+++ mixxx/src/util.h
@@ -1,6 +1,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+// A macro to disallow the copy constructor and operator= functions
+// This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&);               \
   void operator=(const TypeName&)
