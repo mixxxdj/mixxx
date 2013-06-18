87bcffbf9bb1d3b5b1a483ec5def6333966e0793
Daniel Schürmann <daschuer@mixxx.org>
2013-06-18 09:33:25 +0300

    Removed stray line feed

--- mixxx/src/controlobjectthreadmain.cpp
+++ mixxx/src/controlobjectthreadmain.cpp
@@ -50,5 +50,4 @@ void ControlObjectThreadMain::slotValueChanged(double, QObject* pSetter) {
         // and then catch the event via eventFilter.
         QApplication::postEvent(this, new ControlEvent(get(), pSetter));
     }
-
 }
