7af68f92f486b4083f09668499a426f29d650a58
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2013-07-02 12:10:41 -0400

    prepare timbre plugin prefs

--- src/dlgpreftimbre.cpp
+++ src/dlgpreftimbre.cpp
@@ -1,31 +1,86 @@
+#include <vamp-hostsdk/vamp-hostsdk.h>
+#include <qcheckbox.h>
+
+
+#include "vamp/vampanalyser.h"
+#include "track/timbre_preferences.h"
 #include "dlgpreftimbre.h"
 #include "ui_dlgpreftimbredlg.h"
+
+using Vamp::Plugin;
+using Vamp::PluginHostAdapter;
+using Vamp::HostExt::PluginLoader;
+using Vamp::HostExt::PluginWrapper;
+using Vamp::HostExt::PluginInputDomainAdapter;
 
 DlgPrefTimbre::DlgPrefTimbre(QWidget *parent, ConfigObject<ConfigValue> *pConfig):
     QWidget(parent), Ui::DlgPrefTimbreDlg(),
     m_pConfig(pConfig),
     m_bAnalyserEnabled(false) {
     setupUi(this);
+
+    connect(comboBoxPlugin, SIGNAL(currentIndexChanged(int)),
+            this, SLOT(pluginSelected(int)));
+    connect(checkBoxAnalyserEnabled, SIGNAL(stateChanged(int)),
+            this, SLOT(analyserEnabled(int)));
+
 }
 
 DlgPrefTimbre::~DlgPrefTimbre() {
 }
 
 void DlgPrefTimbre::slotApply() {
+    int selected = m_listIdentifier.indexOf(m_selectedAnalyser);
+    if (selected == -1)
+        return;
+
+    m_pConfig->set(
+        ConfigKey(VAMP_CONFIG_KEY, VAMP_ANALYSER_TIMBRE_LIBRARY),
+        ConfigValue(m_listLibrary[selected]));
+    m_pConfig->set(
+        ConfigKey(VAMP_CONFIG_KEY, VAMP_ANALYSER_TIMBRE_PLUGIN_ID),
+        ConfigValue(m_selectedAnalyser));
+    m_pConfig->set(
+        ConfigKey(TIMBRE_CONFIG_KEY, TIMBRE_ANALYSIS_ENABLED),
+        ConfigValue(m_bAnalyserEnabled ? 1 : 0));
     m_pConfig->Save();
 }
 
 void DlgPrefTimbre::slotUpdate() {
+    comboBoxPlugin->setEnabled(m_bAnalyserEnabled);
+    checkBoxAnalyserEnabled->setChecked(m_bAnalyserEnabled);
     slotApply();
+    if (!m_bAnalyserEnabled)
+        return;
+
+    int comboSelected = m_listIdentifier.indexOf(m_selectedAnalyser);
+    if (comboSelected == -1){
+        qDebug() << "DlgPrefTimbre: Plugin not found in slotUpdate()";
+        return;
+    }
+    comboBoxPlugin->setCurrentIndex(comboSelected);
 }
 
 void DlgPrefTimbre::pluginSelected(int i) {
+    if (i==-1)
+        return;
+    m_selectedAnalyser = m_listIdentifier[i];
+    slotUpdate();
 }
 
 void DlgPrefTimbre::analyserEnabled(int i) {
+    m_bAnalyserEnabled = static_cast<bool>(i);
+    slotUpdate();
 }
 
 void DlgPrefTimbre::setDefaults() {
+    m_bAnalyserEnabled = true;
+    m_selectedAnalyser = VAMP_ANALYSER_TIMBRE_DEFAULT_PLUGIN_ID;
+    if (!m_listIdentifier.contains(m_selectedAnalyser)) {
+        qDebug() << "DlgPrefTimbre: qm-similarity Vamp plugin not found";
+        m_bAnalyserEnabled = false;
+    }
+    slotUpdate();
 }
 
 
@@ -33,5 +88,22 @@ void DlgPrefTimbre::populate() {
 }
 
 void DlgPrefTimbre::loadSettings() {
+    if(m_pConfig->getValueString(
+        ConfigKey(VAMP_CONFIG_KEY, VAMP_ANALYSER_TIMBRE_PLUGIN_ID))==QString("")) {
+        setDefaults();
+        slotApply();    // Write to config file so AnalyzerBeats can get the data
+        return;
+    }
+    QString pluginid = m_pConfig->getValueString(
+        ConfigKey(VAMP_CONFIG_KEY, VAMP_ANALYSER_TIMBRE_PLUGIN_ID));
+    m_selectedAnalyser = pluginid;
+
+    m_bAnalyserEnabled = static_cast<bool>(m_pConfig->getValueString(
+        ConfigKey(TIMBRE_CONFIG_KEY, TIMBRE_ANALYSIS_ENABLED)).toInt());
+
+    if (!m_listIdentifier.contains(pluginid)) {
+        setDefaults();
+    }
+    slotUpdate();
 }
 
---
+++ src/track/timbre_preferences.h
@@ -0,0 +1,15 @@
+#ifndef TIMBRE_PREFERENCES_H
+#define TIMBRE_PREFERENCES_H
+
+#define VAMP_CONFIG_KEY "[Vamp]"
+
+// VAMP_CONFIG_KEY Preferences
+#define VAMP_ANALYSER_TIMBRE_LIBRARY "AnalyserTimbreLibrary"
+#define VAMP_ANALYSER_TIMBRE_PLUGIN_ID "AnalyserTimbrePluginID"
+#define VAMP_ANALYSER_TIMBRE_DEFAULT_PLUGIN_ID "qm-similarity"
+
+#define TIMBRE_CONFIG_KEY "[Timbre]"
+
+#define TIMBRE_ANALYSIS_ENABLED "TimbralAnalysisEnabled"
+
+#endif // TIMBRE_PREFERENCES_H
