d9626e37bee8a595fca0efe9876829af9d1e4017
Carl Pillot <carl.pillot@gmail.com>
2013-08-23 20:24:53 -0400

    Added loop tracker class.

---
+++ src/looprecording/looptracker.cpp
@@ -0,0 +1,4 @@
+//  looptracker.cpp
+//  Created by Carl Pillot on 8/23/13.
+
+#include "looptracker.h"
---
+++ src/looprecording/looptracker.h
@@ -0,0 +1,8 @@
+//  looptracker.h
+//  Created by Carl Pillot on 8/23/13.
+
+#ifndef __LOOPTRACKER_H__
+#define __LOOPTRACKER_H__
+
+
+#endif
