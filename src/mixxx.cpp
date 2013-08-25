6eb30f1cf22a07b3c9b4b5c882221fc42fc32470
Owen Williams <owilliams@mixxx.org>
2013-08-25 16:53:04 -0400

    initialize 4 decks by default

--- src/mixxx.cpp
+++ src/mixxx.cpp
@@ -361,6 +361,8 @@ MixxxApp::MixxxApp(QApplication *pApp, const CmdlineArgs& args)
     m_pPlayerManager = new PlayerManager(m_pConfig, m_pSoundManager, m_pEngine);
     m_pPlayerManager->addDeck();
     m_pPlayerManager->addDeck();
+    m_pPlayerManager->addDeck();
+    m_pPlayerManager->addDeck();
     m_pPlayerManager->addSampler();
     m_pPlayerManager->addSampler();
     m_pPlayerManager->addSampler();
