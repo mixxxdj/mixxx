49b901e2f6ac5995fdbeb6999d9677a1fcbeea3d
Carl Pillot <carl.pillot@gmail.com>
2013-08-30 09:35:30 -0400

    Implemented input source text.

--- src/widget/wloopsourcetext.cpp
+++ src/widget/wloopsourcetext.cpp
@@ -13,9 +13,23 @@ WLoopSourceText::~WLoopSourceText() {
 void WLoopSourceText::setValue(double dValue) {
     QString text = "";
     if (dValue == INPUT_NONE) {
-        text = "Off";
+        text = "off";
+    } else if (dValue == INPUT_MASTER) {
+        text = "master";
+    } else if (dValue == INPUT_HEAD) {
+        text = "headphones";
+    } else if (dValue == INPUT_MICROPHONE) {
+        text = "mic";
+    } else if (dValue == INPUT_PT1) {
+        text = "pt1";
+    } else if (dValue == INPUT_PT2) {
+        text = "pt2";
+    } else if (dValue > INPUT_DECK_BASE && dValue < INPUT_SAMPLER_BASE) {
+        text = QString("deck  %1").arg(QString::number(dValue-INPUT_DECK_BASE));
+    } else if (dValue > INPUT_SAMPLER_BASE) {
+        text = QString("sampler %1").arg(QString::number(dValue-INPUT_SAMPLER_BASE));
     } else {
-        text = "On";
+        text = "invalid";
     }
     m_pLabel->setText(QString(m_qsText).append(text));
 }
