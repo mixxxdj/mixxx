db6a439308d8e36d521ce301e31805789908be7b
Owen Williams <owilliams@mixxx.org>
2013-09-06 06:37:31 -0400

    explain choosing 20ms latency

--- src/vinylcontrol/vinylcontrolxwax.cpp
+++ src/vinylcontrol/vinylcontrolxwax.cpp
@@ -110,7 +110,7 @@ VinylControlXwax::VinylControlXwax(ConfigObject<ConfigValue> * pConfig, QString 
     double latency = ControlObject::getControl(
             ConfigKey("[Master]", "latency"))->get();
     if (latency == 0) {
-        qDebug() << "latency is, like 20";
+        qDebug() << "Failed to get master latency, assuming 20 as a reasonable value";
         latency = 20;
     }
     // Set pitch ring size to 1/4 of one revolution -- a full revolution adds too much stickiness
