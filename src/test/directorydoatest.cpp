d447305dc2ddfea4c0148e1cfa61950a3099fc4b
Max Linke <kain88@mixxx.org>
2013-09-12 23:03:35 +0200

    add directorydao test

---
+++ src/test/directorydoatest.cpp
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+
+#include <QtDebug>
+#include <QtSql>
+#include <QString>
+#include <QStringBuilder>
+
+#include "configobject.h"
+#include "library/dao/directorydao.h"
+#include "library/trackcollection.h"
+
+namespace {
+
+class DirectoryDAOTest : public testing::Test {
+  protected:
+    virtual void SetUp() {
+        m_pConfig = new ConfigObject<ConfigValue>(QDir::homePath().append("/").append(SETTINGS_PATH).append(SETTINGS_FILE));
+        m_pTrackCollection = new TrackCollection(m_pConfig);
+    }
+
+    virtual void TearDown() {
+        delete m_pTrackCollection;
+        delete m_pConfig;
+    }
+
+    ConfigObject<ConfigValue>* m_pConfig;
+    TrackCollection *m_pTrackCollection;
+};
+
+TEST_F(DirectoryDAOTest, addDirTest) {
+    DirectoryDAO m_DirectoryDao = m_pTrackCollection->getDirectoryDAO();
+    QString testdir = "TestDir";
+
+    bool success = m_DirectoryDao.addDirectory(testdir);
+    EXPECT_TRUE(success);
+
+    // QSqlQuery query(m_pTrackCollection->getDatabase());
+    // query.prepare("SELECT " % DIRECTORYDAO_DIR % " FROM " % DIRECTORYDAO_TABLE);
+
+    // success = query.exec();
+    // EXPECT_TRUE(success);
+
+    // QStringList dirs;
+    // while (query.next()) {
+    //     dirs << query.value(0).toString();
+    // }
+
+    // EXPECT_TRUE(dirs.size() == 1);
+    // if ( dirs.size() > 0) {
+    //     EXPECT_TRUE(dirs.at(0) == testdir);
+    // }
+
+}
+
+}  // namespace
