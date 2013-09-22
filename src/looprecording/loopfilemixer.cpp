321cdf29a643f6af778c502b3e1b52ae0d947e6d
Carl Pillot <carl.pillot@gmail.com>
2013-09-22 01:25:26 -0400

    Small modifications to try to get mixing working.

--- src/looprecording/loopfilemixer.cpp
+++ src/looprecording/loopfilemixer.cpp
@@ -60,36 +60,45 @@ void LoopFileMixer::slotProcess() {
 
     SNDFILE* pSndfile1 = sf_open(m_filePath1.toLocal8Bit(), SFM_READ, &sfInfo1);
     SNDFILE* pSndfile2 = sf_open(m_filePath2.toLocal8Bit(), SFM_READ, &sfInfo2);
-    SNDFILE* pSndfileOut = sf_open(m_filePath2.toLocal8Bit(), SFM_WRITE, &sfOutInfo);
+    SNDFILE* pSndfileOut = sf_open(m_dest.toLocal8Bit(), SFM_WRITE, &sfOutInfo);
 
-    if (pSndfile1 && pSndfile2 && pSndfileOut) {
-
-        sf_command(pSndfile1, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
-        sf_command(pSndfile2, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
-        sf_command(pSndfileOut, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
-
-    } else {
-        // error...
-        qDebug() << "sf_open error";
+    if (pSndfile1 == NULL) {
+        qDebug() << "LoopFileMixer::slotProcess() Error opening pSndfile1";
+        emit finished();
+        return;
+    } else if (pSndfile2 == NULL) {
+        qDebug() << "LoopFileMixer::slotProcess() Error opening pSndfile2";
+        emit finished();
+        return;
+    } else if (pSndfileOut == NULL) {
+        qDebug() << "LoopFileMixer::slotProcess() Error opening pSndfileOut";
         emit finished();
         return;
     }
 
-    while (int samples1 = sf_read_float(pSndfile1, m_pWorkBufferIn1, WORK_BUFFER_SIZE)) {
-        qDebug() << "Reading samples...";
+    sf_command(pSndfile1, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+    sf_command(pSndfile2, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+    sf_command(pSndfileOut, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
 
+    bool samplesAvailable = true;
+    while (samplesAvailable) {
+        int samples1 = sf_read_float(pSndfile1, m_pWorkBufferIn1, WORK_BUFFER_SIZE);
         int samples2 = sf_read_float(pSndfile2, m_pWorkBufferIn2, WORK_BUFFER_SIZE);
 
         qDebug() << "Reading samples... Samples 1: " << samples1 << " Samples 2: " << samples2;
 
-        SampleUtil::applyGain(m_pWorkBufferOut, 0.0, WORK_BUFFER_SIZE);
-        SampleUtil::addWithGain(m_pWorkBufferOut, m_pWorkBufferIn1, 1.0, samples1);
+        if (samples1 == 0) {
+            samplesAvailable = false;
+        } else {
+            SampleUtil::applyGain(m_pWorkBufferOut, 0.0, WORK_BUFFER_SIZE);
+            SampleUtil::addWithGain(m_pWorkBufferOut, m_pWorkBufferIn1, 1.0, samples1);
 
-        if (samples2 > 0) {
-            SampleUtil::addWithGain(m_pWorkBufferOut, m_pWorkBufferIn2, 1.0, samples2);
+            if (samples2 > 0) {
+                SampleUtil::addWithGain(m_pWorkBufferOut, m_pWorkBufferIn2, 1.0, samples2);
+            }
+            int written = sf_write_float(pSndfileOut, m_pWorkBufferOut, samples1);
+            qDebug() << written << " Samples written";
         }
-        int written = sf_write_float(pSndfileOut, m_pWorkBufferOut, samples1);
-        qDebug() << written << " Samples written";
     }
 
     sf_close(pSndfile1);
