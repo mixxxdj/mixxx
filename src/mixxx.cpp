7a30aad42dfc377a879ec462e45791f2b43185c6
Owen Williams <owilliams@mixxx.org>
2013-10-27 19:57:30 -0400

    Keep only two decks for now.

--- src/mixxx.cpp
+++ src/mixxx.cpp
@@ -361,10 +361,6 @@ MixxxApp::MixxxApp(QApplication *pApp, const CmdlineArgs& args)
     m_pPlayerManager = new PlayerManager(m_pConfig, m_pSoundManager, m_pEngine);
     m_pPlayerManager->addDeck();
     m_pPlayerManager->addDeck();
-    m_pPlayerManager->addDeck();
-    m_pPlayerManager->addDeck();
-    m_pPlayerManager->addSampler();
-    m_pPlayerManager->addSampler();
     m_pPlayerManager->addSampler();
     m_pPlayerManager->addSampler();
     m_pPlayerManager->addPreviewDeck();
