eff4fb22e969ea600d8bdfd0110f07e214ff2bc3
Owen Williams <owilliams@mixxx.org>
2013-11-24 22:42:56 -0500

    notes on manually testing master sync

--- src/test/enginesynctest.cpp
+++ src/test/enginesynctest.cpp
@@ -1,3 +1,11 @@
+// Tests for Master Sync.
+// There are no tests for actual deck playback, since I don't know how to mock that out yet.
+// The following manual tests should probably be performed:
+// * Quantize mode nudges tracks in sync, whether internal or deck master.
+// * Flinging tracks with the waveform should work.
+// * vinyl??
+
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
