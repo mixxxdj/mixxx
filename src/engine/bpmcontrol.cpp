mixxxdj/mixxx

commit 9c1b203ea8dffc66a3c03777a45a0543301f5a18

changed addition COs to COSs in ClockControl and BpmControl

src/engine/bpmcontrol.cpp
@@ -10,6 +10,7 @@
 #include "engine/bpmcontrol.h"
 #include "engine/enginechannel.h"
 #include "engine/enginemaster.h"
+#include "controlobjectslave.h"
 
 const int minBpm = 30;
 const int maxInterval = (int)(1000.*(60./(CSAMPLE)minBpm));
@@ -19,14 +20,9 @@ BpmControl::BpmControl(const char* _group,
                        ConfigObject<ConfigValue>* _config) :
         EngineControl(_group, _config),
         m_tapFilter(this, filterLength, maxInterval) {
-    m_pPlayButton = ControlObject::getControl(_group, "play");
-    m_pRateSlider = ControlObject::getControl(_group, "rate");
-    connect(m_pRateSlider, SIGNAL(valueChanged(double)),
-            this, SLOT(slotAdjustBpm()),
-            Qt::DirectConnection);
-    connect(m_pRateSlider, SIGNAL(valueChangedFromEngine(double)),
-            this, SLOT(slotAdjustBpm()),
-            Qt::DirectConnection);
+    m_pPlayButton = new ControlObjectSlave(_group, "play", this);
+    m_pRateSlider = new ControlObjectSlave(_group, "rate", this);
+    m_pRateSlider->connectValueChanged(SLOT(slotAdjustBpm()), Qt::DirectConnection);
 
     m_pRateRange = ControlObject::getControl(_group, "rateRange");
     connect(m_pRateRange, SIGNAL(valueChanged(double)),

src/engine/bpmcontrol.h
@@ -11,6 +11,7 @@
 class ControlObject;
 class ControlPushButton;
 class EngineBuffer;
+class ControlObjectSlave;
 
 class BpmControl : public EngineControl {
     Q_OBJECT
@@ -42,8 +43,8 @@ class BpmControl : public EngineControl {
     bool syncPhase(EngineBuffer* pOtherEngineBuffer);
 
     // ControlObjects that come from EngineBuffer
-    ControlObject* m_pPlayButton;
-    ControlObject* m_pRateSlider;
+    ControlObjectSlave* m_pPlayButton;
+    ControlObjectSlave* m_pRateSlider;
     ControlObject* m_pRateRange;
     ControlObject* m_pRateDir;
 

src/engine/clockcontrol.cpp
@@ -4,12 +4,13 @@
 #include "configobject.h"
 #include "cachingreader.h"
 #include "engine/enginecontrol.h"
+#include "controlobjectslave.h"
 
 ClockControl::ClockControl(const char* pGroup, ConfigObject<ConfigValue>* pConfig)
         : EngineControl(pGroup, pConfig) {
     m_pCOBeatActive = new ControlObject(ConfigKey(pGroup, "beat_active"));
     m_pCOBeatActive->set(0.0f);
-    m_pCOSampleRate = ControlObject::getControl(ConfigKey("[Master]","samplerate"));
+    m_pCOSampleRate = new ControlObjectSlave("[Master]","samplerate");
 }
 
 ClockControl::~ClockControl() {

src/engine/clockcontrol.h
@@ -7,6 +7,8 @@
 #include "trackinfoobject.h"
 #include "track/beats.h"
 
+class ControlObjectSlave;
+
 class ClockControl: public EngineControl {
     Q_OBJECT
   public:
@@ -25,7 +27,7 @@ class ClockControl: public EngineControl {
 
   private:
     ControlObject* m_pCOBeatActive;
-    ControlObject* m_pCOSampleRate;
+    ControlObjectSlave* m_pCOSampleRate;
     TrackPointer m_pTrack;
     BeatsPointer m_pBeats;
 };

commit aa09647f60566723ceb0ff36fb7556413104e63a

fixed bpm label was not updated by sync button, regression from a previous commit 9c1b203

src/engine/bpmcontrol.cpp
@@ -212,7 +212,7 @@ bool BpmControl::syncTempo(EngineBuffer* pOtherEngineBuffer) {
         // The desired rate is the other decks effective rate divided by this
         // deck's file BPM. This gives us the playback rate that will produce an
         // effective BPM equivalent to the other decks.
-        double fDesiredRate = fOtherBpm / fThisFileBpm;
+        double desiredRate = fOtherBpm / fThisFileBpm;
 
         // Test if this buffer's bpm is the double of the other one, and adjust
         // the rate scale. I believe this is intended to account for our BPM
@@ -221,28 +221,31 @@ bool BpmControl::syncTempo(EngineBuffer* pOtherEngineBuffer) {
 
         float fFileBpmDelta = fabs(fThisFileBpm - fOtherFileBpm);
         if (fabs(fThisFileBpm * 2.0 - fOtherFileBpm) < fFileBpmDelta) {
-            fDesiredRate /= 2.0;
+            desiredRate /= 2.0;
         } else if (fabs(fThisFileBpm - 2.0 * fOtherFileBpm) < fFileBpmDelta) {
-            fDesiredRate *= 2.0;
+            desiredRate *= 2.0;
         }
 
         // Subtract the base 1.0, now fDesiredRate is the percentage
         // increase/decrease in playback rate, not the playback rate.
-        fDesiredRate -= 1.0;
+        double desiredRateShift = desiredRate - 1.0;
 
         // Ensure the rate is within resonable boundaries. Remember, this is the
         // percent to scale the rate, not the rate itself. If fDesiredRate was -1,
         // that would mean the deck would be completely stopped. If fDesiredRate
         // is 1, that means it is playing at 2x speed. This limit enforces that
         // we are scaled between 0.5x and 2x.
-        if (fDesiredRate < 1.0 && fDesiredRate > -0.5)
+        if (desiredRateShift < 1.0 && desiredRateShift > -0.5)
         {
+            m_pEngineBpm->set(m_pFileBpm->get() * desiredRate);
+
+
             // Adjust the rateScale. We have to divide by the range and
             // direction to get the correct rateScale.
-            fDesiredRate = fDesiredRate / (m_pRateRange->get() * m_pRateDir->get());
-
+            double desiredRateSlider = desiredRateShift / (m_pRateRange->get() * m_pRateDir->get());
             // And finally, set the slider
-            m_pRateSlider->set(fDesiredRate);
+            m_pRateSlider->set(desiredRateSlider);
+
             return true;
         }
     }