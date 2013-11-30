e8a958119d3666bc7c8d256da1ba66138f1c8309
RJ Ryan <rryan@mixxx.org>
2013-11-30 01:11:38 -0500

    Send an ADD_EFFECT_CHAIN message when one is added.

--- src/effects/effectchainmanager.cpp
+++ src/effects/effectchainmanager.cpp
@@ -15,6 +15,11 @@ EffectChainManager::~EffectChainManager() {
 void EffectChainManager::addEffectChain(EffectChainPointer pEffectChain) {
     if (pEffectChain) {
         m_effectChains.append(pEffectChain);
+        EffectsRequest* pRequest = new EffectsRequest();
+        pRequest->type = EffectsRequest::ADD_EFFECT_CHAIN;
+        pRequest->AddEffectChain.pChain = pEffectChain->getEngineEffectChain();
+        m_pEffectsManager->writeRequest(pRequest);
+
     }
 }
 
