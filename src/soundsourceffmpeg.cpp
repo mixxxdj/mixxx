mixxxdj/mixxx

commit 4845e95acb542120191f309e050d785699730048

Header/include cleanups across the codebase.
* Eliminate all instances of including QtCore, QtGui, or QtXml.
* Replace all old-style Qt header inclusions with new-style (e.g. #include <application.h>).
* Include QtDebug instead of QDebug.
* Update many classes to include the Qt classes they use in their header and implementation file.

This speeds up compile and link performance and reduces the size of the compiled object files. My build directory dropped by 50MB.

src/soundsourceffmpeg.cpp
@@ -29,7 +29,7 @@
 //#include <fcntl.h>
 //#endif
 
-#include <QDebug>
+#include <QtDebug>
 
 static QMutex ffmpegmutex;
 

commit 817159206fd22fe8f4c8d78bc3167cb607a5a078

Fixed building with ffmpeg, a regression from 4845e95acb542120191f309e050d785699730048

src/soundsourceffmpeg.cpp
@@ -30,6 +30,7 @@
 //#endif
 
 #include <QtDebug>
+#include <QBuffer>
 
 static QMutex ffmpegmutex;
 