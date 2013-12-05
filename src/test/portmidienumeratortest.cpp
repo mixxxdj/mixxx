c2c603b58ee801c2a34f46334daaa5e98f535890
RJ Ryan <rryan@mixxx.org>
2013-12-04 21:36:00 -0500

    Make the Lemur linking support slightly more general.

--- src/test/portmidienumeratortest.cpp
+++ src/test/portmidienumeratortest.cpp
@@ -74,20 +74,23 @@ TEST_F(PortMidiEnumeratorTest, InputOutputPortsLinked) {
     // Bug 1033375 - contain string 'to' in device name, strip
     // ' input port ' and ' output port ' from strings not done
     // because of invalid strings
-    ASSERT_FALSE(shouldLinkInputToOutput(
+    ASSERT_TRUE(shouldLinkInputToOutput(
         "Traktor Kontrol X1 - 1 MIDI input 0",
         "Traktor Kontrol X1 - 1 MIDI output 0"));
 
-    // Lemur Daemon shows 8 port pairs named like:
-    // 'Daemon Input 1' and 'Daemon Output 2'
-    // We explicitly only support such names with numeric index, case sensitively
+    // Strip ' input ' from inputs and ' output ' from outputs.
+
+    // Lemur Daemon shows 8 port pairs named like: 'Daemon Input 1' and 'Daemon
+    // Output 2'
     ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input 1", "Daemon Output 1"));
     ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input 2", "Daemon Output 2"));
+    ASSERT_TRUE(shouldLinkInputToOutput("Daemon InPuT 2", "Daemon OuTpuT 2"));
     ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input 1234", "Daemon Output 1234"));
+    ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input ", "Daemon Output "));
+    ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input 1", "daemon output 1"));
+    ASSERT_TRUE(shouldLinkInputToOutput("Daemon Input 1", "Daemon Input 1"));
     ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input", "Daemon Output"));
-    ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input ", "Daemon Output "));
-    ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 1", "Daemon Input 1"));
-    ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 1", "daemon output 1"));
+    ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 1234", "Daemon Output 5678"));
     ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 1", "Daemon Output 1 "));
     ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 1", "Daemon Output 11"));
     ASSERT_FALSE(shouldLinkInputToOutput("Daemon Input 2", "Daemon Output 12"));
@@ -121,4 +124,3 @@ TEST_F(PortMidiEnumeratorTest, InputOutputPortsLinked) {
         "nanoKONTROL 2 3 SLIDER/KNOB"));
 
 }
-
