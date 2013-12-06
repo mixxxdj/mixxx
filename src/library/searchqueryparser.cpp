18010692cc51acdf1a48dab246a1592aec8c4345
RJ Ryan <rryan@mixxx.org>
2013-12-06 12:50:55 -0500

    Add location and album_artist search keywords.

--- src/library/searchqueryparser.cpp
+++ src/library/searchqueryparser.cpp
@@ -5,13 +5,15 @@
 SearchQueryParser::SearchQueryParser(QSqlDatabase& database)
         : m_database(database) {
     m_textFilters << "artist"
+                  << "album_artist"
                   << "album"
                   << "title"
                   << "genre"
                   << "composer"
                   << "grouping"
                   << "comment"
-                  << "key";
+                  << "key"
+                  << "location";
     m_numericFilters << "year"
                      << "track"
                      << "bpm"
@@ -24,6 +26,7 @@ SearchQueryParser::SearchQueryParser(QSqlDatabase& database)
     //m_specialFilters << "key";
 
     m_fieldToSqlColumns["artist"] << "artist" << "album_artist";
+    m_fieldToSqlColumns["album_artist"] << "album_artist";
     m_fieldToSqlColumns["album"] << "album";
     m_fieldToSqlColumns["title"] << "title";
     m_fieldToSqlColumns["genre"] << "genre";
@@ -38,6 +41,7 @@ SearchQueryParser::SearchQueryParser(QSqlDatabase& database)
     m_fieldToSqlColumns["key"] << "key";
     m_fieldToSqlColumns["played"] << "timesplayed";
     m_fieldToSqlColumns["rating"] << "rating";
+    m_fieldToSqlColumns["location"] << "location";
 
     m_allFilters.append(m_textFilters);
     m_allFilters.append(m_numericFilters);
