655a265861b0205656fb7396655ad46cb37f214b
RJ Ryan <rryan@mixxx.org>
2013-12-09 15:33:15 -0500

    Replace clock calculation in EngineSync with InternalClock.
    * InternalClock is an explicit class that manages the state of the internal
      clock.
    * Add a virtual abstract base class "Clock" that other clocks will inherit from.
    
    This paves the way to removing the distinction between a deck and a clock in
    EngineSync with a "Syncable" virtual class that represents both.

---
+++ src/engine/clock.h
@@ -0,0 +1,12 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+class Clock {
+    virtual double getBeatDistance() const = 0;
+    virtual void setBeatDistance(double beatDistance) = 0;
+
+    virtual double getBpm() const = 0;
+    virtual void setBpm(double bpm) = 0;
+};
+
+#endif /* CLOCK_H */
