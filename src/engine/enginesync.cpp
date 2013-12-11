ee0d8599ea7af75c4b82d71e0701904556abb7b7
RJ Ryan <rryan@mixxx.org>
2013-12-10 23:03:42 -0500

    Silence noisy debug.

--- src/engine/enginesync.cpp
+++ src/engine/enginesync.cpp
@@ -282,8 +282,7 @@ void EngineSync::notifyInstantaneousBpmChanged(Syncable* pSyncable, double bpm) 
 }
 
 void EngineSync::notifyBeatDistanceChanged(Syncable* pSyncable, double beat_distance) {
-    qDebug() << "EngineSync::notifyBeatDistanceChanged" << pSyncable->getGroup() << beat_distance;
-    if (pSyncable->getSyncMode() == SYNC_NONE) {
+    //qDebug() << "EngineSync::notifyBeatDistanceChanged" << pSyncable->getGroup() << beat_distance;
     if (pSyncable->getSyncMode() != SYNC_MASTER) {
         return;
     }
