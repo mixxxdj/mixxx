c16b845d6dc4195c355d1238bc3abda2bc5b7f00
RJ Ryan <rryan@mixxx.org>
2014-01-12 21:22:49 -0500

    Protect against invalid state numbers from skin.

--- src/widget/wpushbutton.cpp
+++ src/widget/wpushbutton.cpp
@@ -71,6 +71,11 @@ void WPushButton::setup(QDomNode node, const SkinContext& context) {
     while (!state.isNull()) {
         if (state.isElement() && state.nodeName() == "State") {
             int iState = context.selectInt(state, "Number");
+            if (iState >= m_iNoStates) {
+                qDebug() << "WPushButton provided state info for invalid state:" << iState;
+                continue;
+            }
+
             if (context.hasNode(state, "Pressed")) {
                 setPixmap(iState, true,
                           context.getSkinPath(context.selectString(state, "Pressed")));
