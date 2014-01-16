b893a73ab6082761e393d57f9c46e95ab759ae69
RJ Ryan <rryan@mixxx.org>
2014-01-15 23:46:04 -0500

    Add OS X and iOS utility method file.

---
+++ src/util/mac.cpp
@@ -0,0 +1,17 @@
+#include "util/mac.h"
+
+#ifdef Q_OS_MAC
+QString CFStringToQString(CFStringRef str) {
+    if (!str)
+        return QString();
+
+    CFIndex length = CFStringGetLength(str);
+    if (length == 0)
+        return QString();
+
+    QString string(length, Qt::Uninitialized);
+    CFStringGetCharacters(str, CFRangeMake(0, length), reinterpret_cast<UniChar *>
+        (const_cast<QChar *>(string.unicode())));
+    return string;
+}
+#endif
