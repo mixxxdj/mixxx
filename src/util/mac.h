a49e6e55ba22cafe787c1ac9ae3aa15f801387f0
RJ Ryan <rryan@mixxx.org>
2014-01-15 23:46:04 -0500

    Add OS X and iOS utility method file.

---
+++ src/util/mac.h
@@ -0,0 +1,15 @@
+// Platform-specific helpers for OS X and iOS.
+#ifndef MAC_H
+#define MAC_H
+
+#include <QtGlobal>
+
+#ifdef Q_OS_MAC
+#include <QString>
+#include <CoreFoundation/CFString.h>
+
+QString CFStringToQString(CFStringRef str);
+
+#endif
+
+#endif /* MAC_H */
