e053aba7c74dafc369ea8e5b005b0a62ebb4cb9f
RJ Ryan <rryan@mixxx.org>
2014-01-24 14:06:29 -0500

    Fix ramping gain for master volume and crossfader.
    This was removed when output bus support was added.

--- src/engine/enginemaster.h
+++ src/engine/enginemaster.h
@@ -163,11 +163,7 @@ class EngineMaster : public QObject, public AudioSource {
     QList<CSAMPLE> m_channelMasterGainCache;
     QList<CSAMPLE> m_channelHeadphoneGainCache;
 
-    struct OutputBus {
-        CSAMPLE* m_pBuffer;
-        OrientationVolumeGainCalculator m_gain;
-        QList<CSAMPLE> m_gainCache;
-    } m_outputBus[3];
+    CSAMPLE* m_pOutputBusBuffers[3];
     CSAMPLE* m_pMaster;
     CSAMPLE* m_pHead;
 
