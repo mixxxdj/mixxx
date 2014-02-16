mixxxdj/mixxx

commit 7ff2ffdd5063472b8dd791d3c088f7c7b849545a

Ignore MIDI_NOTE and rely on dParam when mapping Midi Pushbuttons. Probably fixes https://bugs.launchpad.net/mixxx/+bug/1277449

src/control/controlbehavior.cpp
@@ -184,19 +184,15 @@ ControlPushButtonBehavior::ControlPushButtonBehavior(ButtonMode buttonMode,
 
 void ControlPushButtonBehavior::setValueFromMidiParameter(
         MidiOpCode o, double dParam, ControlDoublePrivate* pControl) {
+    Q_UNUSED(o);
     // This block makes push-buttons act as power window buttons.
     if (m_buttonMode == POWERWINDOW && m_iNumStates == 2) {
-        if (o == MIDI_NOTE_ON) {
-            if (dParam > 0.) {
-                double value = pControl->get();
-                pControl->set(!value, NULL);
-                m_pushTimer.setSingleShot(true);
-                m_pushTimer.start(kPowerWindowTimeMillis);
-            }
-        } else if (o == MIDI_NOTE_OFF) {
-            if (!m_pushTimer.isActive()) {
-                pControl->set(0.0, NULL);
-            }
+        if (dParam > 0.) {
+            pControl->set(1., NULL);
+            m_pushTimer.setSingleShot(true);
+            m_pushTimer.start(kPowerWindowTimeMillis);
+        } else if (!m_pushTimer.isActive()) {
+            pControl->set(0., NULL);
         }
     } else if (m_buttonMode == TOGGLE || m_buttonMode == LONGPRESSLATCHING) {
         // This block makes push-buttons act as toggle buttons.
@@ -213,22 +209,22 @@ void ControlPushButtonBehavior::setValueFromMidiParameter(
                     m_pushTimer.setSingleShot(true);
                     m_pushTimer.start(kLongPressLatchingTimeMillis);
                 }
-            } else if (o == MIDI_NOTE_OFF) {
+            } else {
                 double value = pControl->get();
                 if (m_buttonMode == LONGPRESSLATCHING &&
-                        m_pushTimer.isActive() && value >= 1.0) {
+                        m_pushTimer.isActive() && value >= 1.) {
                     // revert toggle if button is released too early
-                    value = (int)(value - 1.0) % m_iNumStates;
+                    value = (int)(value - 1.) % m_iNumStates;
                     pControl->set(value, NULL);
                 }
             }
         }
     } else { // Not a toggle button (trigger only when button pushed)
-        if (o == MIDI_NOTE_ON) {
-            double value = pControl->get();
-            pControl->set(!value, NULL);
-        } else if (o == MIDI_NOTE_OFF) {
-            pControl->set(0.0, NULL);
+        if (dParam > 0.) {
+            pControl->set(1., NULL);
+        } else {
+            pControl->set(0., NULL);
         }
     }
 }
+

commit ba4400de995b75808e01fb5a2d1bf24bf2aeace0

Fixed releasing a Power window button from midi. Regression from 7ff2ffdd5063472b8dd791d3c088f7c7b849545a

src/control/controlbehavior.cpp
@@ -200,10 +200,13 @@ void ControlPushButtonBehavior::setValueFromMidiParameter(
     // This block makes push-buttons act as power window buttons.
     if (m_buttonMode == POWERWINDOW && m_iNumStates == 2) {
         if (pressed) {
-            pControl->set(1., NULL);
+            // Toggle on press
+            double value = pControl->get();
+            pControl->set(!value, NULL);
             m_pushTimer.setSingleShot(true);
             m_pushTimer.start(kPowerWindowTimeMillis);
         } else if (!m_pushTimer.isActive()) {
+            // Disable after releasing a long press
             pControl->set(0., NULL);
         }
     } else if (m_buttonMode == TOGGLE || m_buttonMode == LONGPRESSLATCHING) {