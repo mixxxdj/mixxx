mixxxdj/mixxx

commit f486e97e94268971d0cbe54b4c17bacf10ec8bcb

Fix tooltips, a regression from the last commit.

src/widget/wbasewidget.cpp
@@ -151,7 +151,7 @@ void WBaseWidget::updateTooltip() {
         QStringList debug;
         fillDebugTooltip(&debug);
 
-        QString base = baseTooltip();
+        QString base = m_pWidget->toolTip();
         if (!base.isEmpty()) {
             debug.append(QString("Tooltip: \"%1\"").arg(base));
         }

src/skin/legacyskinparser.cpp
@@ -1438,13 +1438,13 @@ void LegacySkinParser::setupBaseWidget(QDomNode node,
     // Tooltip
     if (m_pContext->hasNode(node, "Tooltip")) {
         QString toolTip = m_pContext->selectString(node, "Tooltip");
-        pBaseWidget->setBaseTooltip(toolTip);
+        pBaseWidget->prependBaseTooltip(toolTip);
     } else if (m_pContext->hasNode(node, "TooltipId")) {
         QString toolTipId = m_pContext->selectString(node, "TooltipId");
         QString toolTip = m_tooltips.tooltipForId(toolTipId);
 
         if (toolTipId.length() > 0) {
-            pBaseWidget->setBaseTooltip(toolTip);
+            pBaseWidget->prependBaseTooltip(toolTip);
         } else {
             qDebug() << "Invalid <TooltipId> in skin.xml:" << toolTipId;
         }
@@ -1702,7 +1702,7 @@ void LegacySkinParser::addShortcutToToolTip(WBaseWidget* pWidget, const QString&
         return;
     }
 
-    QString tooltip = pWidget->baseTooltip();
+    QString tooltip;
 
     // translate shortcut to native text
 #if QT_VERSION >= 0x040700
@@ -1720,5 +1720,5 @@ void LegacySkinParser::addShortcutToToolTip(WBaseWidget* pWidget, const QString&
     }
     tooltip += ": ";
     tooltip += nativeShortcut;
-    pWidget->setBaseTooltip(tooltip);
+    pWidget->appendBaseTooltip(tooltip);
 }

src/widget/wbasewidget.h
@@ -19,6 +19,16 @@ class WBaseWidget {
         return m_pWidget;
     }
 
+    void appendBaseTooltip(const QString& tooltip) {
+        m_baseTooltip.append(tooltip);
+        m_pWidget->setToolTip(m_baseTooltip);
+    }
+
+    void prependBaseTooltip(const QString& tooltip) {
+        m_baseTooltip.prepend(tooltip);
+        m_pWidget->setToolTip(m_baseTooltip);
+    }
+
     void setBaseTooltip(const QString& tooltip) {
         m_baseTooltip = tooltip;
         m_pWidget->setToolTip(tooltip);

commit 158fb15ad263d0167588b4b94d5d911d8c242692

Fixed growing tooltips in --developer mode, a regression from f486e97e94268971d0cbe54b4c17bacf10ec8bcb

src/widget/wbasewidget.cpp
@@ -151,7 +151,7 @@ void WBaseWidget::updateTooltip() {
         QStringList debug;
         fillDebugTooltip(&debug);
 
-        QString base = m_pWidget->toolTip();
+        QString base = baseTooltip();
         if (!base.isEmpty()) {
             debug.append(QString("Tooltip: \"%1\"").arg(base));
         }