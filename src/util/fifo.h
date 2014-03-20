4add83a6a48442349ba1145bb074e483d703be2f
Daniel Schürmann <daschuer@mixxx.org>
2014-03-20 22:04:11 +0100

    Make Fifo not failing due to size not power of 2; Expose aquire release interface

--- src/util/fifo.h
+++ src/util/fifo.h
@@ -10,35 +10,27 @@ template <class DataType>
 class FIFO {
   public:
     explicit FIFO(int size) {
+        size = roundUpToPowerOf2(size);
         m_data = new DataType[size];
-        m_initialized = PaUtil_InitializeRingBuffer(
-            &m_ringBuffer, sizeof(DataType), size, m_data) == 0;
-
-        if (!m_initialized) {
-            qDebug() << "ERROR: Could not initialize PA ring buffer.";
-        }
+        PaUtil_InitializeRingBuffer(
+                &m_ringBuffer, sizeof(DataType), size, m_data);
     }
     virtual ~FIFO() {
         delete [] m_data;
     }
     int readAvailable() const {
-        if (!m_initialized) { return 0; }
         return PaUtil_GetRingBufferReadAvailable(&m_ringBuffer);
     }
     int writeAvailable() const {
-        if (!m_initialized) { return 0; }
         return PaUtil_GetRingBufferWriteAvailable(&m_ringBuffer);
     }
     int read(DataType* pData, int count) {
-        if (!m_initialized) { return 0; }
         return PaUtil_ReadRingBuffer(&m_ringBuffer, pData, count);
     }
     int write(const DataType* pData, int count) {
-        if (!m_initialized) { return 0; }
         return PaUtil_WriteRingBuffer(&m_ringBuffer, pData, count);
     }
     void writeBlocking(const DataType* pData, int count) {
-        if (!m_initialized) { return; }
         int written = 0;
         while (written != count) {
             int i = write(pData, count);
@@ -46,10 +38,39 @@ class FIFO {
             written += i;
         }
     }
+    int aquireWriteRegions(int count,
+            DataType** dataPtr1, int* sizePtr1,
+            DataType** dataPtr2, int* sizePtr2) {
+        return PaUtil_GetRingBufferWriteRegions(&m_ringBuffer, count,
+            dataPtr1, sizePtr1, dataPtr2, sizePtr2);
+    }
+    int releaseWriteRegions(int count) {
+        return PaUtil_AdvanceRingBufferWriteIndex(&m_ringBuffer, count);
+    }
+    int aquireReadRegions(int count,
+            DataType** dataPtr1, int* sizePtr1,
+            DataType** dataPtr2, int* sizePtr2) {
+        return PaUtil_GetRingBufferReadRegions(&m_ringBuffer, count,
+            dataPtr1, sizePtr1, dataPtr2, sizePtr2);
+    }
+    int releaseReadRegions(int count) {
+        return PaUtil_AdvanceRingBufferReadIndex(&m_ringBuffer, count);
+    }
   private:
+    int roundUpToPowerOf2(int v) {
+        // From http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        v--;
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        v++;
+        return v;
+    }
+
     DataType* m_data;
     PaUtilRingBuffer m_ringBuffer;
-    bool m_initialized;
     DISALLOW_COPY_AND_ASSIGN(FIFO<DataType>);
 };
 
