324ca091420cefb7911d43f84273c8fe1ce7c173
RJ Ryan <rryan@mixxx.org>
2014-04-14 11:56:29 -0400

    Document flawed behavior of button and switch MIDI options with tests and comments.
    For clarifying the discussion in Bug #1307296.

--- src/test/midicontrollertest.cpp
+++ src/test/midicontrollertest.cpp
@@ -101,6 +101,86 @@ TEST_F(MidiControllerTest, ReceiveMessage_PushButtonCO_PushOnOn) {
     EXPECT_DOUBLE_EQ(0.0, cpb.get());
 }
 
+TEST_F(MidiControllerTest, ReceiveMessage_PushButtonCO_ToggleOnOff_ButtonMidiOption) {
+    // Using the button MIDI option allows you to use a MIDI toggle button as a
+    // push button.
+    ConfigKey key("[Channel1]", "hotcue_1_activate");
+    ControlPushButton cpb(key);
+
+    unsigned char channel = 0x01;
+    unsigned char control = 0x10;
+
+    MidiOptions options;
+    options.button = true;
+
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_ON | channel, control),
+                                options, key));
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_OFF | channel, control),
+                                options, key));
+    loadPreset(m_preset);
+
+    // NOTE(rryan): This behavior is broken!
+
+    // Toggle the switch on, sets the push button on.
+    receive(MIDI_NOTE_ON | channel, control, 0x7F);
+    EXPECT_LT(0.0, cpb.get());
+
+    // The push button is stuck down here!
+
+    // Toggle the switch off, sets the push button off.
+    receive(MIDI_NOTE_OFF | channel, control, 0x00);
+    EXPECT_DOUBLE_EQ(0.0, cpb.get());
+}
+
+TEST_F(MidiControllerTest, ReceiveMessage_PushButtonCO_ToggleOnOff_SwitchMidiOption) {
+    // Using the switch MIDI option interprets a MIDI toggle button as a toggle
+    // button rather than a momentary push button.
+    ConfigKey key("[Channel1]", "hotcue_1_activate");
+    ControlPushButton cpb(key);
+
+    unsigned char channel = 0x01;
+    unsigned char control = 0x10;
+
+    MidiOptions options;
+    options.sw = true;
+
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_ON | channel, control),
+                                options, key));
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_OFF | channel, control),
+                                options, key));
+    loadPreset(m_preset);
+
+    // NOTE(rryan): This behavior is broken!
+
+    // Toggle the switch on, sets the push button on.
+    receive(MIDI_NOTE_ON | channel, control, 0x7F);
+    EXPECT_LT(0.0, cpb.get());
+
+    // The push button is stuck down here!
+
+    // Toggle the switch off, sets the push button on again.
+    receive(MIDI_NOTE_OFF | channel, control, 0x00);
+    EXPECT_LT(0.0, cpb.get());
+
+    // NOTE(rryan): What is supposed to happen in this case? It's an open
+    // question I think. I think if you want to connect a switch MIDI control to
+    // a push button CO then the switch should directly set the CO. After all,
+    // the preset author asked for the switch to be interpreted as a switch. If
+    // they want the switch to act like a push button, they should use the
+    // button MIDI option.
+    //
+    // Most of our push buttons trigger behavior on press and do nothing on
+    // release, and most don't care about being "stuck down" except for hotcue
+    // and cue controls that have preview behavior.
+
+    // "reverse" is an example of a push button that is a push button because we
+    // want the default behavior to be momentary press and not toggle. If I
+    // mapped a switch to it, I would expect the switch to enable it (set it to
+    // 1) when the switch was enabled and set it to 0 when the switch was
+    // disabled. So I think we should change the switch option to behave like
+    // this.
+}
+
 TEST_F(MidiControllerTest, ReceiveMessage_PushButtonCO_PushCC) {
     // Some MIDI controllers (e.g. Korg nanoKONTROL) send momentary push-buttons
     // as (CC, 0x7f) for press and (CC, 0x00) for release.
@@ -181,6 +261,90 @@ TEST_F(MidiControllerTest, ReceiveMessage_ToggleCO_PushOnOn) {
     receive(MIDI_NOTE_ON | channel, control, 0x00);
 
     EXPECT_DOUBLE_EQ(0.0, cpb.get());
+}
+
+TEST_F(MidiControllerTest, ReceiveMessage_ToggleCO_ToggleOnOff_ButtonMidiOption) {
+    // Using the button MIDI option allows you to use a MIDI toggle button as a
+    // push button.
+    ConfigKey key("[Channel1]", "keylock");
+    ControlPushButton cpb(key);
+    cpb.setButtonMode(ControlPushButton::TOGGLE);
+
+    unsigned char channel = 0x01;
+    unsigned char control = 0x10;
+
+    MidiOptions options;
+    options.button = true;
+
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_ON | channel, control),
+                                options, key));
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_OFF | channel, control),
+                                options, key));
+    loadPreset(m_preset);
+
+    // NOTE(rryan): If the intended behavior of the button MIDI option is to
+    // make a toggle MIDI button act like a push button then this isn't
+    // working. The toggle on toggles the CO but the toggle off does nothing.
+
+    // Toggle the switch on, since it is interpreted as a button press it
+    // toggles the button on.
+    receive(MIDI_NOTE_ON | channel, control, 0x7F);
+    EXPECT_LT(0.0, cpb.get());
+
+    // Toggle the switch off, since it is interpreted as a button release it
+    // does nothing to the toggle button.
+    receive(MIDI_NOTE_OFF | channel, control, 0x00);
+    EXPECT_LT(0.0, cpb.get());
+}
+
+TEST_F(MidiControllerTest, ReceiveMessage_ToggleCO_ToggleOnOff_SwitchMidiOption) {
+    // Using the switch MIDI option interprets a MIDI toggle button as a toggle
+    // button rather than a momentary push button.
+    ConfigKey key("[Channel1]", "keylock");
+    ControlPushButton cpb(key);
+    cpb.setButtonMode(ControlPushButton::TOGGLE);
+
+    unsigned char channel = 0x01;
+    unsigned char control = 0x10;
+
+    MidiOptions options;
+    options.sw = true;
+
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_ON | channel, control),
+                                options, key));
+    addMapping(MidiInputMapping(MidiKey(MIDI_NOTE_OFF | channel, control),
+                                options, key));
+    loadPreset(m_preset);
+
+    // NOTE(rryan): If the intended behavior of switch MIDI option is to make a
+    // toggle MIDI button act like a toggle button then this isn't working. The
+    // toggle on presses the CO and the toggle off presses the CO. This toggles
+    // the control but allows it to get out of sync.
+
+    // Toggle the switch on, since it is interpreted as a button press it
+    // toggles the control on.
+    receive(MIDI_NOTE_ON | channel, control, 0x7F);
+    EXPECT_LT(0.0, cpb.get());
+
+    // Toggle the switch off, since it is interpreted as a button press it
+    // toggles the control off.
+    receive(MIDI_NOTE_OFF | channel, control, 0x00);
+    EXPECT_DOUBLE_EQ(0.0, cpb.get());
+
+    // Meanwhile, the GUI toggles the control on again.
+    // NOTE(rryan): Now the MIDI toggle button is out of sync with the toggle
+    // CO.
+    cpb.set(1.0);
+
+    // Toggle the switch on, since it is interpreted as a button press it
+    // toggles the control off (since it was on).
+    receive(MIDI_NOTE_ON | channel, control, 0x7F);
+    EXPECT_DOUBLE_EQ(0.0, cpb.get());
+
+    // Toggle the switch off, since it is interpreted as a button press it
+    // toggles the control on (since it was off).
+    receive(MIDI_NOTE_OFF | channel, control, 0x00);
+    EXPECT_LT(0.0, cpb.get());
 }
 
 TEST_F(MidiControllerTest, ReceiveMessage_ToggleCO_PushCC) {
