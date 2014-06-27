1ad32452275fbe3c2305456781e7ff79a6514c72
Marcos CARDINOT <mcardinot@gmail.com>
2014-06-27 22:59:32 +0100

    CoverArtCacheTest - removing database setup

--- src/test/coverartcache_test.cpp
+++ src/test/coverartcache_test.cpp
@@ -3,32 +3,9 @@
 
 #include "library/coverartcache.h"
 #include "library/dao/coverartdao.h"
-#include "library/trackcollection.h"
 #include "test/mixxxtest.h"
 
 class CoverArtCacheTest : public CoverArtCache, public MixxxTest {
-  protected:
-    virtual void SetUp() {
-        // make sure to use the current schema.xml file in the repo
-        config()->set(ConfigKey("[Config]","Path"),
-                      QDir::currentPath().append("/res"));
-        m_pTrackCollection = new TrackCollection(config());
-    }
-
-    virtual void TearDown() {
-        // make sure we clean up the db
-        QSqlQuery query(m_pTrackCollection->getDatabase());
-        query.prepare("DELETE FROM " % COVERART_TABLE);
-        query.exec();
-        query.prepare("DELETE FROM library");
-        query.exec();
-        query.prepare("DELETE FROM track_locations");
-        query.exec();
-
-        delete m_pTrackCollection;
-    }
-
-    TrackCollection* m_pTrackCollection;
 };
 
 const QString& coverLocationTest = ":/images/library/vinyl-record.png";
