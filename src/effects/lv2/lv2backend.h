8bc1a6e6bc05b6aeae974a7f4dbcb15b50be9b56
Nicu Badescu <badescunicu@yahoo.com>
2014-07-16 11:25:48 +0300

    Initialize the LilvWorld and use it to find the available plug ins; also implement getManifest, canInstantiateEffect, and instantiateEffect methods

--- src/effects/lv2/lv2backend.h
+++ src/effects/lv2/lv2backend.h
@@ -2,6 +2,8 @@
 #define LV2BACKEND_H
 
 #include "effects/effectsbackend.h"
+#include "effects/lv2/lv2manifest.h"
+#include <lilv-0/lilv/lilv.h>
 
 class LV2Backend : public EffectsBackend {
     Q_OBJECT
@@ -9,7 +11,18 @@ class LV2Backend : public EffectsBackend {
     LV2Backend(QObject* pParent=NULL);
     virtual ~LV2Backend();
 
+    const QSet<QString> getEffectIds() const;
+    EffectManifest getManifest(const QString& effectId) const;
+    bool canInstantiateEffect(const QString& effectId) const;
+    EffectPointer instantiateEffect(EffectsManager* pEffectsManager,
+                                    const QString& effectId);
+
   private:
+    void initializeProperties();
+    LilvWorld* m_pWorld;
+    QHash<QString, LilvNode*> m_properties;
+    QSet<LV2Manifest*> m_registeredEffects;
+
     QString debugString() const {
         return "LV2Backend";
     }
