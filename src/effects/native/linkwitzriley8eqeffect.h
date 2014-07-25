901f70bc8552039c1f758bdf34f6e71ed1b15318
Daniel Schürmann <daschuer@mixxx.org>
2014-07-25 02:17:49 +0200

    Added a LinkwitzRiley8EQeffect stub

---
+++ src/effects/native/linkwitzriley8eqeffect.h
@@ -0,0 +1,71 @@
+#ifndef LINKWITZRILEYEQEFFECT_H
+#define LINKWITZRILEYEQEFFECT_H
+
+#include <QMap>
+
+#include "controlobjectslave.h"
+#include "effects/effect.h"
+#include "effects/effectprocessor.h"
+#include "engine/effects/engineeffect.h"
+#include "engine/effects/engineeffectparameter.h"
+#include "engine/enginefilterlinkwitzriley8.h"
+#include "util.h"
+#include "util/types.h"
+#include "util/defs.h"
+#include "sampleutil.h"
+
+class LinkwitzRiley8EQEffectGroupState {
+  public:
+    LinkwitzRiley8EQEffectGroupState();
+    virtual ~LinkwitzRiley8EQEffectGroupState();
+
+    void setFilters(int sampleRate, int lowFreq, int highFreq);
+
+    EngineFilterLinkwtzRiley8Low* low;
+    EngineFilterLinkwtzRiley8Low* band;
+    EngineFilterLinkwtzRiley8High* high;
+
+    double old_low;
+    double old_mid;
+    double old_high;
+
+    CSAMPLE* m_pLowBuf;
+    CSAMPLE* m_pBandBuf;
+    CSAMPLE* m_pHighBuf;
+};
+
+class LinkwitzRiley8EQEffect : public GroupEffectProcessor<LinkwitzRiley8EQEffectGroupState> {
+  public:
+    LinkwitzRiley8EQEffect(EngineEffect* pEffect, const EffectManifest& manifest);
+    virtual ~LinkwitzRiley8EQEffect();
+
+    static QString getId();
+    static EffectManifest getManifest();
+
+    // See effectprocessor.h
+    void processGroup(const QString& group,
+                      LinkwitzRiley8EQEffectGroupState* pState,
+                      const CSAMPLE* pInput, CSAMPLE *pOutput,
+                      const unsigned int numSamples,
+                      const GroupFeatureState& groupFeatureState);
+
+  private:
+    QString debugString() const {
+        return getId();
+    }
+
+    EngineEffectParameter* m_pPotLow;
+    EngineEffectParameter* m_pPotMid;
+    EngineEffectParameter* m_pPotHigh;
+
+    ControlObjectSlave* m_pLoFreqCorner;
+    ControlObjectSlave* m_pHiFreqCorner;
+
+    int m_oldSampleRate;
+    int m_loFreq;
+    int m_hiFreq;
+
+    DISALLOW_COPY_AND_ASSIGN(LinkwitzRiley8EQEffect);
+};
+
+#endif /* LINKWITZRILEYEQEFFECT_H */
