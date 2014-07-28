c447457fed5b7863f1aff31d5c7a0e643b41b3ae
Marcos CARDINOT <mcardinot@gmail.com>
2014-07-28 15:33:00 +0100

    Improving CoverArtDelegate::paint() - remove unnecessary QStyleOptionViewItem copy

--- src/library/coverartdelegate.cpp
+++ src/library/coverartdelegate.cpp
@@ -28,25 +28,8 @@ void CoverArtDelegate::slotLock(bool lock) {
 void CoverArtDelegate::paint(QPainter *painter,
                              const QStyleOptionViewItem &option,
                              const QModelIndex &index) const {
-    painter->save();
-
-    // Populate the correct colors based on the styling
-    QStyleOptionViewItem newOption = option;
-    initStyleOption(&newOption, index);
-
-    // Set the palette appropriately based on whether the row is selected or
-    // not. We also have to check if it is inactive or not and use the
-    // appropriate ColorGroup.
-    if (newOption.state & QStyle::State_Selected) {
-        QPalette::ColorGroup colorGroup =
-                newOption.state & QStyle::State_Active ?
-                QPalette::Active : QPalette::Inactive;
-        painter->fillRect(newOption.rect,
-            newOption.palette.color(colorGroup, QPalette::Highlight));
-        painter->setBrush(newOption.palette.color(
-            colorGroup, QPalette::HighlightedText));
-    } else {
-        painter->fillRect(newOption.rect, newOption.palette.base());
+    if (option.state & QStyle::State_Selected) {
+        painter->fillRect(option.rect, option.palette.highlight());
     }
 
     TrackModel* trackModel = dynamic_cast<TrackModel*>(m_pTableView->model());
@@ -87,6 +70,4 @@ void CoverArtDelegate::paint(QPainter *painter,
             painter->drawPixmap(target, pixmap, source);
         }
     }
-
-    painter->restore();
 }
