6450eaa63d0ddfc3dc926fdc8e611a1fc429c29d
Nicu Badescu <badescunicu@yahoo.com>
2014-08-15 02:41:26 +0300

    Add lowpass shelving, peaking and highpass shelving biquad filter types

--- src/engine/enginefilterbiquad1.cpp
+++ src/engine/enginefilterbiquad1.cpp
@@ -1,13 +1,44 @@
 #include <QLocale>
 #include "engine/enginefilterbiquad1.h"
 
+EngineFilterBiquad1Low::EngineFilterBiquad1Low(int sampleRate,
+                                               double centerFreq, double Q) {
+    setFrequencyCorners(sampleRate, centerFreq, Q, 0);
+}
+
+void EngineFilterBiquad1Low::setFrequencyCorners(int sampleRate,
+                                                 double centerFreq,
+                                                 double Q,
+                                                 double dBgain) {
+    QString specification = "LsBq/" + QLocale().toString(Q) + "/" +
+                            QLocale().toString(dBgain);
+    setCoefs(qPrintable(specification), sampleRate, centerFreq);
+}
+
 EngineFilterBiquad1Band::EngineFilterBiquad1Band(int sampleRate,
                                                  double centerFreq, double Q) {
-    setFrequencyCorners(sampleRate, centerFreq, Q);
+    setFrequencyCorners(sampleRate, centerFreq, Q, 0);
 }
 
 void EngineFilterBiquad1Band::setFrequencyCorners(int sampleRate,
-                                                  double centerFreq, double Q) {
-    QString specification = "BpBq/" + QLocale().toString(Q);
+                                                  double centerFreq,
+                                                  double Q,
+                                                  double dBgain) {
+    QString specification = "PkBq/" + QLocale().toString(Q) + "/" +
+                            QLocale().toString(dBgain);
+    setCoefs(qPrintable(specification), sampleRate, centerFreq);
+}
+
+EngineFilterBiquad1High::EngineFilterBiquad1High(int sampleRate,
+                                                 double centerFreq, double Q) {
+    setFrequencyCorners(sampleRate, centerFreq, Q, 0);
+}
+
+void EngineFilterBiquad1High::setFrequencyCorners(int sampleRate,
+                                                  double centerFreq,
+                                                  double Q,
+                                                  double dBgain) {
+    QString specification = "HsBq/" + QLocale().toString(Q) + "/" +
+                            QLocale().toString(dBgain);
     setCoefs(qPrintable(specification), sampleRate, centerFreq);
 }
--- src/engine/enginefilterbiquad1.h
+++ src/engine/enginefilterbiquad1.h
@@ -3,11 +3,28 @@
 
 #include "engine/enginefilteriir.h"
 
-class EngineFilterBiquad1Band : public EngineFilterIIR<2, IIR_BP> {
+class EngineFilterBiquad1Low : public EngineFilterIIR<5, IIR_BP> {
+    Q_OBJECT
+  public:
+    EngineFilterBiquad1Low(int sampleRate, double centerFreq, double Q);
+    void setFrequencyCorners(int sampleRate, double centerFreq,
+                             double Q, double dBgain);
+};
+
+class EngineFilterBiquad1Band : public EngineFilterIIR<5, IIR_BP> {
     Q_OBJECT
   public:
     EngineFilterBiquad1Band(int sampleRate, double centerFreq, double Q);
-    void setFrequencyCorners(int sampleRate, double centerFreq, double Q);
+    void setFrequencyCorners(int sampleRate, double centerFreq,
+                             double Q, double dBgain);
+};
+
+class EngineFilterBiquad1High : public EngineFilterIIR<5, IIR_BP> {
+    Q_OBJECT
+  public:
+    EngineFilterBiquad1High(int sampleRate, double centerFreq, double Q);
+    void setFrequencyCorners(int sampleRate, double centerFreq,
+                             double Q, double dBgain);
 };
 
 #endif // ENGINEFILTERBIQUAD1_H
