eac4af8c620c61e9832759eeb2bf173dbb7b9eb1
Nicu Badescu <badescunicu@yahoo.com>
2014-08-16 10:34:06 +0300

    Remove getAllDiscoveredPlugins method

--- src/effects/lv2/lv2backend.h
+++ src/effects/lv2/lv2backend.h
@@ -13,24 +13,18 @@ class LV2Backend : public EffectsBackend {
 
     void enumeratePlugins();
     const QSet<QString> getEffectIds() const;
+    const QSet<QString> getDiscoveredPluginIds() const;
     EffectManifest getManifest(const QString& effectId) const;
     EffectManifest& getManifestReference(const QString& effectId);
     bool canInstantiateEffect(const QString& effectId) const;
     EffectPointer instantiateEffect(EffectsManager* pEffectsManager,
                                     const QString& effectId);
-    QList<QPair<QPair<QString, bool>, QString> > getAllDiscoveredPlugins();
 
   private:
     void initializeProperties();
     LilvWorld* m_pWorld;
     QHash<QString, LilvNode*> m_properties;
     QHash<QString, LV2Manifest*> m_registeredEffects;
-    // List used for displaying the available LV2 plugins
-    // It stores the name of the plugin, its availability and its id
-    // entry.first.first -> name
-    // entry.first.second -> availability
-    // entry.second -> id
-    QList<QPair<QPair<QString, bool>, QString> > m_allLV2Plugins;
 
     QString debugString() const {
         return "LV2Backend";
