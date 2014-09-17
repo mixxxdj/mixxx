059596c3203f56f44f4ca385638f9d3e6d1203be
RJ Ryan <rryan@mixxx.org>
2014-09-16 20:27:50 -0400

    Only define round on __WINDOWS__.
    Accidentally broke this in 214430.

--- src/util/math.h
+++ src/util/math.h
@@ -71,7 +71,7 @@ inline int roundUpToPowerOf2(int v) {
 }
 
 // MSVS 2013 (_MSC_VER 1800) introduced C99 support.
-#if _MSC_VER < 1800 
+#if defined(__WINDOWS__) &&  _MSC_VER < 1800
 inline int round(double x){
     return x < 0.0 ? ceil(x - 0.5) : floor(x + 0.5);
 }
