34de62d51e9137a379769396a22adf39764bc385
Uwe Klotz <uwe_klotz@web.de>
2014-11-10 13:41:49 +0100

    Add sample value constants

--- src/util/types.h
+++ src/util/types.h
@@ -1,9 +1,24 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-// 16-bit sample data.
+#include <climits>
+
+
+// 16-bit integer sample data
 typedef short int SAMPLE;
-// float sample data
+const SAMPLE SAMPLE_ZERO = 0;
+// asymmetric range
+const SAMPLE SAMPLE_MIN = SHRT_MIN;
+const SAMPLE SAMPLE_MAX = SHRT_MAX;
+
+
+// 32-bit floating-point sample data
 typedef float CSAMPLE;
+const CSAMPLE CSAMPLE_ZERO = 0.0f;
+const CSAMPLE CSAMPLE_PEAK = 1.0f;
+// symmetric range
+const CSAMPLE CSAMPLE_MAX =  CSAMPLE_PEAK;
+const CSAMPLE CSAMPLE_MIN = -CSAMPLE_PEAK;
+
 
 #endif /* TYPES_H */
