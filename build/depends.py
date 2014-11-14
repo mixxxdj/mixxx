25a5fef18eadece78e65390f808abdd66e359796
RJ Ryan <rryan@mixxx.org>
2014-11-14 00:19:33 -0500

    Add experiment modes for rapid development and testing.
    - Adds a static Experiment class with a tri-state mode flag that
      indicates whether the experiment mode is OFF, BASE, or EXPERIMENT.
    
    - Adds an "Enable Base" and "Enable Experiment" option to the Developer
      menu (shown when in --developer mode) with the Ctrl+Shift+B and
      Ctrl+Shift+E global shortcuts. Each one toggles between OFF and
      BASE/EXPERIMENT so you can choose exactly what time spans you would
      like to collect in your base and experiment buckets.
    
    - Updates StatsManager to segment collected stats into a base and
      experiment bucket. This allows you to quickly measure the difference a
      code change has on relevant counters / timers within the same
      execution of Mixxx.
    
    This is useful for quickly enabling and disabling a code change without
    a re-compile/re-run cycle to get an anecdotal sense of how it "feels" as
    well as a quantified sense of how it differs in terms of stats Mixxx
    collects.
    
    All stats collected via the usual Counter/Timer/ScopedTimer/etc. tools
    are segmented into a BASE STATS and EXPERIMENT STATS section printed to
    the log on exit.

--- build/depends.py
+++ build/depends.py
@@ -913,6 +913,7 @@ class MixxxCore(Feature):
                    "util/file.cpp",
                    "util/mac.cpp",
                    "util/task.cpp",
+                   "util/experiment.cpp",
 
                    '#res/mixxx.qrc'
                    ]
