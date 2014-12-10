36c9d5a94120041446e0f688482973f398ac9e1e
Daniel Schürmann <daschuer@mixxx.org>
2014-12-10 21:36:09 +0100

    make use of static set and get functions

--- src/test/enginebuffertest.cpp
+++ src/test/enginebuffertest.cpp
@@ -17,24 +17,24 @@ class EngineBufferTest : public MockedEngineBackendTest {
 TEST_F(EngineBufferTest, DisableKeylockResetsPitch) {
     // To prevent one-slider users from getting stuck on a key, unsetting
     // keylock resets the musical pitch.
-    ControlObject::getControl(ConfigKey(m_sGroup1, "file_bpm"))->set(128.0);
-    ControlObject::getControl(ConfigKey(m_sGroup1, "keylock"))->set(1.0);
-    ControlObject::getControl(ConfigKey(m_sGroup1, "pitch"))->set(0.5);
+    ControlObject::set(ConfigKey(m_sGroup1, "file_bpm"), 128.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "keylock"), 1.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "pitch"),0.5);
     ProcessBuffer();
 
-    ControlObject::getControl(ConfigKey(m_sGroup1, "keylock"))->set(0.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "keylock"), 0.0);
     // We require a buffer process to see that the keylock state has changed.
     ProcessBuffer();
-    ASSERT_EQ(0.0, ControlObject::getControl(ConfigKey(m_sGroup1, "pitch"))->get());
+    ASSERT_EQ(0.0, ControlObject::get(ConfigKey(m_sGroup1, "pitch")));
 }
 
 TEST_F(EngineBufferTest, TrackLoadResetsPitch) {
     // When a new track is loaded, the pitch value should be reset.
-    ControlObject::getControl(ConfigKey(m_sGroup1, "file_bpm"))->set(128.0);
-    ControlObject::getControl(ConfigKey(m_sGroup1, "pitch"))->set(0.5);
+    ControlObject::set(ConfigKey(m_sGroup1, "file_bpm"), 128.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "pitch"), 0.5);
     ProcessBuffer();
-    ASSERT_EQ(0.5, ControlObject::getControl(ConfigKey(m_sGroup1, "pitch"))->get());
+    ASSERT_EQ(0.5, ControlObject::get(ConfigKey(m_sGroup1, "pitch")));
 
     m_pChannel1->getEngineBuffer()->loadFakeTrack();
-    ASSERT_EQ(0.0, ControlObject::getControl(ConfigKey(m_sGroup1, "pitch"))->get());
+    ASSERT_EQ(0.0, ControlObject::get(ConfigKey(m_sGroup1, "pitch")));
 }
