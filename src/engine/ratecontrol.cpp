a6e5d514312304a1ab830fcfb7d6c1b451660092
Daniel Schürmann <daschuer@mixxx.org>
2014-12-28 22:19:14 +0100

    use toBool

--- src/engine/ratecontrol.cpp
+++ src/engine/ratecontrol.cpp
@@ -423,7 +423,7 @@ double RateControl::calculateSpeed(double baserate, double speed, bool paused,
     } else {
         double wheelFactor = getWheelFactor();
         double jogFactor = getJogFactor();
-        bool bVinylControlEnabled = m_pVCEnabled && m_pVCEnabled->get() > 0.0;
+        bool bVinylControlEnabled = m_pVCEnabled && m_pVCEnabled->toBool();
         bool useScratch2Value = m_pScratch2Enable->get() != 0;
 
         // By default scratch2_enable is enough to determine if the user is
@@ -435,7 +435,7 @@ double RateControl::calculateSpeed(double baserate, double speed, bool paused,
         }
 
         if (bVinylControlEnabled) {
-            if (m_pVCScratching && m_pVCScratching->get() > 0.0) {
+            if (m_pVCScratching->toBool()) {
                 *reportScratching = true;
             }
             rate = m_pVCRate->get();
