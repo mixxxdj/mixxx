d15fa5cb99d471b3516e44f2df28e8ac59430ba2
Jean Claveau <jean.claveau@c277.fr>
2015-01-11 21:41:49 +0100

    comments and review

--- src/effects/native/paneffect.cpp
+++ src/effects/native/paneffect.cpp
@@ -16,7 +16,8 @@ EffectManifest PanEffect::getManifest() {
     manifest.setName(QObject::tr("Pan"));
     manifest.setAuthor("The Mixxx Team");
     manifest.setVersion("1.0");
-    manifest.setDescription(QObject::tr("Bounce the sound from a channel to another"));
+    manifest.setDescription(QObject::tr(
+        "Bounce the sound from a channel to another, fastly or softly"));
     
     // TODO(jclaveau) : this doesn't look like a good name but doesn't exist on
     //  my mixer. Any suggestion?
@@ -33,6 +34,9 @@ EffectManifest PanEffect::getManifest() {
     strength->setMaximum(0.5);  // there are two steps per period so max is half
     strength->setDefault(0.5);
     
+    // On my mixer, the period is defined as a multiple of the BPM
+    // I wonder if I should implement it as the bouncing is really nice
+    // when it is synced.
     EffectManifestParameter* period = manifest.addParameter();
     period->setId("period");
     period->setName(QObject::tr("Period"));
@@ -45,11 +49,11 @@ EffectManifest PanEffect::getManifest() {
     period->setDefault(50.0);
     
     // This only extists for testing and finding the best value
-    // TODO : remove when a satisfying value is chosen
-    // The current value is taken with a simple laptop soundcard a sample rate
-    // of 44100
+    // TODO(jclaveau) : remove when a satisfying value is chosen
+    // The current value is taken with a simple laptop soundcard with a sample
+    // rate of 44100
     EffectManifestParameter* ramping = manifest.addParameter();
-    ramping->setId("ramping_treshold");
+    ramping->setId("ramping_threshold");
     ramping->setName(QObject::tr("Ramping"));
     ramping->setDescription("");
     ramping->setControlHint(EffectManifestParameter::CONTROL_KNOB_LINEAR);
@@ -59,7 +63,8 @@ EffectManifest PanEffect::getManifest() {
     ramping->setMaximum(0.015978f); // max good value with my soundcard 
     ramping->setDefault(0.003f);    // best value
     
-    // This control is hidden for the moment in Deere (replace by the previous one)
+    // This control is hidden for the moment in Deere (replace by the previous
+    // one wich would be useless soon)
     EffectManifestParameter* depth = manifest.addParameter();
     depth->setId("depth");
     depth->setName(QObject::tr("Depth"));
@@ -79,7 +84,7 @@ PanEffect::PanEffect(EngineEffect* pEffect, const EffectManifest& manifest)
           m_pDepthParameter(pEffect->getParameterById("depth")),
           m_pStrengthParameter(pEffect->getParameterById("strength")),
           m_pPeriodParameter(pEffect->getParameterById("period")),
-          m_pRampingParameter(pEffect->getParameterById("ramping_treshold"))
+          m_pRampingParameter(pEffect->getParameterById("ramping_threshold"))
            {
     Q_UNUSED(manifest);
 }
@@ -112,17 +117,19 @@ void PanEffect::processGroup(const QString& group, PanGroupState* pGroupState,
         gs.time = 0;
     }
     
+    // Normally, the position goes from 0 to 1 linearly. Here we make steps at
+    // 0.25 and 0.75 to have the sound fully on the right or fully on the left.
+    // At the end, the "position" value can describe a sinusoid or a square
+    // curve depending of the size of those steps.
+    
     // coef of the slope
     // a = (y2 - y1) / (x2 - x1)
     //       1  / ( 1 - 2 * stepfrac)
-    float a;
-    if( stepFrac != 0.5f ){
-        a = 1.0f / (1.0f - stepFrac * 2.0f);
-    } else {
-        a = 0.0001f;
-    }
+    float a = stepFrac != 0.5f ? 1.0f / (1.0f - stepFrac * 2.0f) : 1.0f;
     
-    // define the increasing of gain. 
+    // define the increasing of gain (the music sounds lower if only one 
+    // channel is enabled).
+    // TODO(jclaveau) : Challenge this value
     float lawCoef = 1.0f / sqrtf(2.0f) * 2.0f;
     
     // size of a segment of slope (controled by the "strength" parameter)
@@ -138,14 +145,14 @@ void PanEffect::processGroup(const QString& group, PanGroupState* pGroupState,
         // current quarter in the trigonometric circle
         float quarter = floorf(periodFraction * 4.0f);
         
-        // part of the period fraction being step (not in the slope)
+        // part of the period fraction being a step (not in the slope)
         CSAMPLE stepsFractionPart = floorf((quarter+1.0f)/2.0f) * stepFrac;
         
         // float inInterval = fmod( periodFraction, (period / 2.0) );
-        float inInterval = fmod( periodFraction, 0.5f );
+        float inStepInterval = fmod(periodFraction, 0.5f);
         
         CSAMPLE position;
-        if (inInterval > u && inInterval < (u + stepFrac)) {
+        if (inStepInterval > u && inStepInterval < (u + stepFrac)) {
             // at full left or full right
             position = quarter < 2.0f ? 0.25f : 0.75f;
         } else {
