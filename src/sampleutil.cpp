0e9a97253b45a748e8e2f0d7e1bef9cfa26bc2fa
Daniel Schürmann <daschuer@mixxx.org>
2015-02-06 21:59:09 +0100

    Replaced tabs with spaces

--- src/sampleutil.cpp
+++ src/sampleutil.cpp
@@ -19,58 +19,58 @@ typedef qint32 int32_t;
 
 
 static inline bool useAlignedAlloc() {
-	return (sizeof(long double) == 8 && sizeof(CSAMPLE*) <= 8 &&
-			sizeof(CSAMPLE*) == sizeof(size_t));
+    return (sizeof(long double) == 8 && sizeof(CSAMPLE*) <= 8 &&
+            sizeof(CSAMPLE*) == sizeof(size_t));
 }
 
 // static
 CSAMPLE* SampleUtil::alloc(unsigned int size) {
     // For optimal use of SSE registers, it is required to align
-	// the sample buffers to 16 Byte (128 bit) boundary
+    // the sample buffers to 16 Byte (128 bit) boundary
 
-	// Pointers returned by malloc are aligned for the largest scalar type,
-	// which is long double with usually 16 byte.
-	// An exception is MSVC X86 where long double is mapped to double.
+    // Pointers returned by malloc are aligned for the largest scalar type,
+    // which is long double with usually 16 byte.
+    // An exception is MSVC X86 where long double is mapped to double.
 
-	// In case of sizeof(long double) = 8
-	// this code over allocates the requested buffer to be able to
-	// shift the returned pointer to a 16 byte alignment
-	// In the memory before, a pointer to the original
-	// malloced area is stored used to free the memory
-	// This code can be replaced by C11 <stdlib.h> aligned_alloc()
-	// or MSVC ::_aligned_malloc(size, alignment) and  ::_aligned_free(ptr);
+    // In case of sizeof(long double) = 8
+    // this code over allocates the requested buffer to be able to
+    // shift the returned pointer to a 16 byte alignment
+    // In the memory before, a pointer to the original
+    // malloced area is stored used to free the memory
+    // This code can be replaced by C11 <stdlib.h> aligned_alloc()
+    // or MSVC ::_aligned_malloc(size, alignment) and  ::_aligned_free(ptr);
 
-	if (useAlignedAlloc()) {
-		// We need to shift the alignment to 16
-		const size_t alignment = 16;
-		const size_t unaligned_size = sizeof(CSAMPLE[size]) + alignment;
-		void* pUnaligned = std::malloc(unaligned_size);
-		if (pUnaligned == NULL) {
-			return NULL;
-		}
-		// Shift
-		void* pAlligned = (void*)(((size_t)pUnaligned & ~(alignment - 1)) + alignment);
-		// Store pointer to original relative to the shifted pointer
-		*((void**)(pAlligned) - 1) = pUnaligned;
-		return (CSAMPLE*)pAlligned;
-	} else {
-		// We are either correct aligned or on an exotic architecture
-		return new CSAMPLE[size];
-	}
+    if (useAlignedAlloc()) {
+        // We need to shift the alignment to 16
+        const size_t alignment = 16;
+        const size_t unaligned_size = sizeof(CSAMPLE[size]) + alignment;
+        void* pUnaligned = std::malloc(unaligned_size);
+        if (pUnaligned == NULL) {
+            return NULL;
+        }
+        // Shift
+        void* pAlligned = (void*)(((size_t)pUnaligned & ~(alignment - 1)) + alignment);
+        // Store pointer to original relative to the shifted pointer
+        *((void**)(pAlligned) - 1) = pUnaligned;
+        return (CSAMPLE*)pAlligned;
+    } else {
+        // We are either correct aligned or on an exotic architecture
+        return new CSAMPLE[size];
+    }
 }
 
 void SampleUtil::free(CSAMPLE* pBuffer) {
-	// See SampleUtil::alloc() for details
-	if (useAlignedAlloc()) {
-		if (pBuffer == NULL) {
-			return;
-		}
-		// Pointer to the original memory is stored before pBuffer
-		std::free(*((void**)((void*)pBuffer) - 1));
-	} else {
-		// We are either correct aligned or on an exotic architecture
-		delete[] pBuffer;
-	}
+    // See SampleUtil::alloc() for details
+    if (useAlignedAlloc()) {
+        if (pBuffer == NULL) {
+            return;
+        }
+        // Pointer to the original memory is stored before pBuffer
+        std::free(*((void**)((void*)pBuffer) - 1));
+    } else {
+        // We are either correct aligned or on an exotic architecture
+        delete[] pBuffer;
+    }
 }
 
 // static
