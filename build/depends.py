10f6da3f90d8b5f4cbffb9541510ebac774acf96
RJ Ryan <rryan@mixxx.org>
2015-02-26 20:21:56 -0500

    Disable Q_ASSERT in release builds.

--- build/depends.py
+++ build/depends.py
@@ -989,6 +989,12 @@ class MixxxCore(Feature):
         elif build.build_is_release:
             build.env.Append(CPPDEFINES='MIXXX_BUILD_RELEASE')
 
+            # In a release build we want to disable all Q_ASSERTs in Qt headers
+            # that we include. We can't define QT_NO_DEBUG because that would
+            # mean turning off QDebug output. qt_noop() is what Qt defines
+            # Q_ASSERT to be when QT_NO_DEBUG is defined.
+            build.env.Append(CPPDEFINES="'Q_ASSERT(x)=qt_noop()'")
+
         if int(SCons.ARGUMENTS.get('debug_assertions_fatal', 0)):
             build.env.Append(CPPDEFINES='MIXXX_DEBUG_ASSERTIONS_FATAL')
 
