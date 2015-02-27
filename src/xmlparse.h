88877b9655017d0f901112823c7374ce435df544
RJ Ryan <rryan@mixxx.org>
2015-02-26 22:37:20 -0500

    Delete unused code in XmlParse and clean things up a bit.

--- src/xmlparse.h
+++ src/xmlparse.h
@@ -1,53 +1,51 @@
-//
-// C++ Interface: xmlparse
-//
-// Description:
-//
-//
-// Author: Tue Haste Andersen <haste@diku.dk>, (C) 2003
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
-
 #ifndef XMLPARSE_H
 #define XMLPARSE_H
 
 #include <QDomNode>
 #include <QDomElement>
 #include <QDomDocument>
-#include <QVector>
-#include <QList>
 
-/**
-@author Tue Haste Andersen
-*/
+class XmlParse {
+  public:
+    // Searches for and returns a node named sNode in the children of
+    // nodeHeader. Returns a null QDomNode if one is not found.
+    static QDomNode selectNode(const QDomNode& nodeHeader,
+                               const QString& sNode);
 
-class XmlParse
-{
-public:
-    XmlParse();
-    ~XmlParse();
+    // Searches for and returns an element named sNode in the children of
+    // nodeHeader. Returns a null QDomElement if one is not found.
+    static QDomElement selectElement(const QDomNode& nodeHeader,
+                                     const QString& sNode);
 
-    static QDomNode selectNode(const QDomNode &nodeHeader, const QString sNode);
-    static QDomElement selectElement(const QDomNode &nodeHeader, const QString& sNode);
-    static int selectNodeInt(const QDomNode &nodeHeader, const QString sNode);
-    static float selectNodeFloat(const QDomNode &nodeHeader, const QString sNode);
-    static QString selectNodeQString(const QDomNode &nodeHeader, const QString sNode);
-    static QVector<long> *selectNodeLongArray(const QDomNode &nodeHeader, const QString sNode);
-    static QVector<char> *selectNodeCharArray(const QDomNode &nodeHeader, const QString sNode);
-    static QVector<char> *selectNodeHexCharArray(const QDomNode &nodeHeader, const QString sNode);
-    static QList<long> *selectNodeLongList(const QDomNode &nodeHeader, const QString sNode);
-    /** Open an XML file */
-    static QDomElement openXMLFile(QString path, QString name);
-    /** Add a node. Used to write xml document */
-    static QDomElement addElement(QDomDocument &, QDomElement &, QString, QString);
-    /** Add a binary node, long ints encoded as chars. Used to write xml document */
-    static QDomElement addElement(QDomDocument &, QDomElement &, QString, QList<long> *pData);
-    /** Add a binary node of chars. Used to write xml document */
-    static QDomElement addElement(QDomDocument &, QDomElement &, QString, QVector<char> *pData);
-    // Take a binary char array, encode as hex pairs write to xml
-    static QDomElement addHexElement(QDomDocument &, QDomElement &, QString, QVector<char> *pData);
+    // Searches for an element named sNode in the children of nodeHeader and
+    // parses the text value of its children as an integer. Returns 0 if sNode
+    // is not found in nodeHeader's children.
+    static int selectNodeInt(const QDomNode& nodeHeader,
+                             const QString& sNode);
+
+    // Searches for an element named sNode in the children of nodeHeader and
+    // parses the text value of its children as a float. Returns 0.0f if sNode
+    // is not found in nodeHeader's children.
+    static float selectNodeFloat(const QDomNode& nodeHeader,
+                                 const QString& sNode);
+
+    // Searches for an element named sNode in the children of nodeHeader and
+    // returns the text value of its children. Returns the empty string if sNode
+    // is not found in nodeHeader's children.
+    static QString selectNodeQString(const QDomNode& nodeHeader,
+                                     const QString& sNode);
+
+    // Open an XML file.
+    static QDomElement openXMLFile(const QString& path, const QString& name);
+
+    // Add an element named elementName to the provided header element with a
+    // child text node with the value textValue. Returns the created element.
+    static QDomElement addElement(QDomDocument& document, QDomElement& header,
+                                  const QString& elementName, const QString& textValue);
+
+  private:
+    XmlParse() {}
+    ~XmlParse() {}
 };
 
 #endif
