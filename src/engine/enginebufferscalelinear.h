7a40f1af39e1bf4d2162bbc5e0c21246dc86d0aa
Daniel Schürmann <daschuer@mixxx.org>
2015-03-17 22:28:08 +0100

    Use SampleUtil::copy() + some renaming

--- src/engine/enginebufferscalelinear.h
+++ src/engine/enginebufferscalelinear.h
@@ -52,18 +52,14 @@ class EngineBufferScaleLinear : public EngineBufferScale  {
     double m_dRate;
     double m_dOldRate;
 
-    /** Buffer for handling calls to ReadAheadManager */
-    CSAMPLE* buffer_int;
-    int buffer_int_size;
+    // Buffer for handling calls to ReadAheadManager
+    CSAMPLE* m_bufferInt;
+    int m_bufferIntSize;
     CSAMPLE m_fPrevSample[2];
     // The read-ahead manager that we use to fetch samples
     ReadAheadManager* m_pReadAheadManager;
-    double m_dCurSampleIndex;
-    double m_dNextSampleIndex;
-
-    /*QFile df;
-    QTextStream writer;
-    int buffer_count;*/
+    double m_dCurrentFrame;
+    double m_dNextFrame;
 };
 
 #endif
