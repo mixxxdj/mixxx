0a2552d9e758572022ef5f09e98f137649f6ed46
Daniel Schürmann <daschuer@mixxx.org>
2015-03-28 22:51:56 +0100

    Move dectaration of temp1 and temp2 closer to usage

--- src/sampleutil.cpp
+++ src/sampleutil.cpp
@@ -423,11 +423,10 @@ void SampleUtil::copyMultiToStereo(CSAMPLE* _RESTRICT pDest, const CSAMPLE* _RES
 
 // static
 void SampleUtil::reverse(CSAMPLE* pBuffer, int iNumSamples) {
-    CSAMPLE temp1, temp2;
     for (int j = 0; j < iNumSamples / 4; ++j) {
         const int endpos = (iNumSamples - 1) - j * 2 ;
-        temp1 = pBuffer[j * 2];
-        temp2 = pBuffer[j * 2 + 1];
+        CSAMPLE temp1 = pBuffer[j * 2];
+        CSAMPLE temp2 = pBuffer[j * 2 + 1];
         pBuffer[j * 2] = pBuffer[endpos - 1];
         pBuffer[j * 2 + 1] = pBuffer[endpos];
         pBuffer[endpos - 1] = temp1;
