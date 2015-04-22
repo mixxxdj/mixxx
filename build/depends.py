55ff4459d2879816d1e800d5a7c0f9b708172467
Alexandru Jercaianu <alex.jercaianu@gmail.com>
2015-04-22 22:35:38 +0300

    [Bug #1299029] phaser effect

--- build/depends.py
+++ build/depends.py
@@ -554,6 +554,7 @@ class MixxxCore(Feature):
                    "effects/native/moogladder4filtereffect.cpp",
                    "effects/native/reverbeffect.cpp",
                    "effects/native/echoeffect.cpp",
+                   "effects/native/phasereffect.cpp",
                    "effects/native/reverb/Reverb.cc",
 
                    "engine/effects/engineeffectsmanager.cpp",
