1e5b3e7f5349b2996c02e59936c270bc9e9e4197
Uwe Klotz <uwe_klotz@web.de>
2015-04-26 15:22:15 +0200

    Fix Coverity warning for SoundSourceSndFile
    *** CID 61970:  Uninitialized members  (UNINIT_CTOR)
    /src/sources/soundsourcesndfile.cpp: 17 in Mixxx::SoundSourceSndFile::SoundSourceSndFile(QUrl)()
    11         return list;
    12     }
    13
    14     SoundSourceSndFile::SoundSourceSndFile(QUrl url)
    15             : SoundSource(url),
    16               m_pSndFile(NULL) {
    >>>     CID 61970:  Uninitialized members  (UNINIT_CTOR)
    >>>     Non-static class member field "m_sfInfo.seekable" is not initialized in this constructor nor in any functions that it calls.
    17     }
    18
    19     SoundSourceSndFile::~SoundSourceSndFile() {
    20         close();
    21     }

--- src/sources/soundsourcesndfile.cpp
+++ src/sources/soundsourcesndfile.cpp
@@ -14,6 +14,7 @@ QList<QString> SoundSourceSndFile::supportedFileExtensions() {
 SoundSourceSndFile::SoundSourceSndFile(QUrl url)
         : SoundSource(url),
           m_pSndFile(NULL) {
+    memset(&m_sfInfo, 0, sizeof(m_sfInfo));
 }
 
 SoundSourceSndFile::~SoundSourceSndFile() {
@@ -22,7 +23,6 @@ SoundSourceSndFile::~SoundSourceSndFile() {
 
 Result SoundSourceSndFile::tryOpen(const AudioSourceConfig& /*audioSrcCfg*/) {
     DEBUG_ASSERT(!m_pSndFile);
-    memset(&m_sfInfo, 0, sizeof(m_sfInfo));
 #ifdef __WINDOWS__
     // Pointer valid until string changed
     const QString fileName(getLocalFileName());
