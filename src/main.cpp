e4eb68c6cd7d820ab558603e7ceab70fde841b64
Daniel Schürmann <daschuer@mixxx.org>
2015-04-28 21:59:40 +0200

    An nother ty to fix console encoding

--- src/main.cpp
+++ src/main.cpp
@@ -279,18 +279,19 @@ int main(int argc, char * argv[])
     //it seems like this code should be inline in MessageHandler() but for some reason having it there corrupts the messages sometimes -kousu 2/2009
 
 #ifdef __WINDOWS__
-        // Setup Windows console encoding
-        // toLocal8Bit() returns the file encoding format
-        // this does not necessarily match the console encoding
-        // in case of a German Windows XP to 10 console encoding is cp850
-        // where files encoding is cp1252
-        // Qt has no solution for it https://bugreports.qt.io/browse/QTBUG-13303
-        // http://stackoverflow.com/questions/1259084/what-encoding-code-page-is-cmd-exe-using
-        // We try to change the console encoding to file encoding
-        // For a German windows we expect
-        // LOCALE_IDEFAULTANSICODEPAGE "1252" // used by Qt toLocal8Bit
-        // LOCALE_IDEFAULTCODEPAGE "850" // Console
-        SetConsoleOutputCP(LOCALE_IDEFAULTANSICODEPAGE);
+    // Setup Windows console encoding
+    // toLocal8Bit() returns the file encoding format
+    // this does not necessarily match the console encoding
+    // in case of a German Windows XP to 10 console encoding is cp850
+    // where files encoding is cp1252
+    // Qt has no solution for it https://bugreports.qt.io/browse/QTBUG-13303
+    // http://stackoverflow.com/questions/1259084/what-encoding-code-page-is-cmd-exe-using
+    // We try to change the console encoding to file encoding
+    // For a German windows we expect
+    // LOCALE_IDEFAULTANSICODEPAGE "1252" // used by Qt toLocal8Bit
+    // LOCALE_IDEFAULTCODEPAGE "850" // Console
+    SetConsoleCP(LOCALE_IDEFAULTANSICODEPAGE);
+    SetConsoleOutputCP(LOCALE_IDEFAULTANSICODEPAGE);
   #ifdef DEBUGCONSOLE
     InitDebugConsole();
   #endif
@@ -328,6 +329,10 @@ int main(int argc, char * argv[])
 //    for (int i=0; i<argc; ++i)
 //        if(QString("--no-visuals")==argv[i])
 //            bVisuals = false;
+
+#ifdef __WINDOWS__
+     qDebug() << "using CP" << LOCALE_IDEFAULTANSICODEPAGE << "for console";
+#endif
 
 
 #ifdef __APPLE__
