b851bf3315b266647a24da90e1cf70c549b13dc0
Uwe Klotz <uwe_klotz@web.de>
2015-05-06 23:25:07 +0200

    Move MP3 specific constant into separate header file

--- src/sources/audiosource.h
+++ src/sources/audiosource.h
@@ -36,12 +36,6 @@ public:
     static const SINT kChannelCountMono = 1;
     static const SINT kChannelCountStereo = 2;
 
-    // In the worst case up to 29 MP3 frames need to be prefetched
-    // for accurate seeking:
-    // http://www.mars.org/mailman/public/mad-dev/2002-May/000634.html
-    // Used by both SoundSourceMp3 and SoundSourceCoreAudio.
-    static const SINT kMp3SeekFramePrefetchCount = 29;
-
     // Returns the number of channels. The number of channels
     // must be constant over time.
     inline SINT getChannelCount() const {
