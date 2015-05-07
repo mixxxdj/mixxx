b5de6e2e3fb3269172274e3a7279797c07e6fb2f
Uwe Klotz <uwe_klotz@web.de>
2015-05-07 16:54:51 +0200

    Log warning about unreadable tags in audio files

--- src/metadata/trackmetadatataglib.cpp
+++ src/metadata/trackmetadatataglib.cpp
@@ -205,12 +205,14 @@ bool readAudioProperties(TrackMetadata* pTrackMetadata,
         return false;
     }
     if (!pTrackMetadata) {
-        // implicitly succesful
+        // implicitly successful
         return true;
     }
     const TagLib::AudioProperties* pAudioProperties =
             file.audioProperties();
     if (!pAudioProperties) {
+        qWarning() << "Failed to read audio properties from file"
+                << file.name();
         return false;
     }
     readAudioProperties(pTrackMetadata, *pAudioProperties);
@@ -1072,9 +1074,11 @@ Result readTrackMetadataAndCoverArtFromFile(TrackMetadata* pTrackMetadata, QImag
                 }
             }
         }
+    } else {
+        qWarning() << "Unsupported file type" << fileType;
     }
 
-    qWarning() << "File" << fileName << "does not contain any supported tags that could be read!";
+    qWarning() << "Failed to read track metadata from file" << fileName;
     return ERR;
 }
 
