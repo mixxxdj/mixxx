e201e171d355705d383118176e7a766cce202d0c
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2015-05-09 09:41:06 +0300

    Remove header license files to favor LICENSE file

--- src/encoder/encoderffmpegcore.cpp
+++ src/encoder/encoderffmpegcore.cpp
@@ -1,21 +1,3 @@
-/****************************************************************************
-                   encoderffmpegcore.cpp  -  FFMPEG encoder for mixxx
-                             -------------------
-    copyright            : (C) 2012-2013 by Tuukka Pasanen
-                           (C) 2007 by Wesley Stessens
-                           (C) 1994 by Xiph.org (encoder example)
-                           (C) 1994 Tobias Rafreider (shoutcast and recording fixes)
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 //
 //   FFMPEG encoder class..
 //     - Supports what FFMPEG is compiled to supported
