0eb5ee790be406c890383f5435d38049685134e1
RJ Ryan <rryan@mixxx.org>
2015-06-04 16:31:14 -0400

    Add move constructor / move assignment operator for SampleBuffer.
    Fixes the build for -std=c++0x.

--- src/samplebuffer.h
+++ src/samplebuffer.h
@@ -71,14 +71,21 @@
 //
 class SampleBuffer {
     Q_DISABLE_COPY(SampleBuffer);
-
-public:
+  public:
     SampleBuffer()
             : m_data(NULL),
               m_size(0) {
     }
     explicit SampleBuffer(SINT size);
+    SampleBuffer(SampleBuffer&& other) {
+        swap(other);
+    }
     virtual ~SampleBuffer();
+
+    SampleBuffer& operator=(SampleBuffer&& other) {
+        swap(other);
+        return *this;
+    }
 
     SINT size() const {
         return m_size;
@@ -120,7 +127,7 @@ public:
     void fill(CSAMPLE value);
 
     class ReadableChunk {
-    public:
+      public:
         ReadableChunk(const SampleBuffer& buffer, SINT offset, SINT length)
             : m_data(buffer.data(offset)),
               m_size(length) {
@@ -138,13 +145,13 @@ public:
         const CSAMPLE& operator[](SINT index) const {
             return *data(index);
         }
-    private:
+      private:
         const CSAMPLE* m_data;
         SINT m_size;
     };
 
     class WritableChunk {
-    public:
+      public:
         WritableChunk(SampleBuffer& buffer, SINT offset, SINT length)
             : m_data(buffer.data(offset)),
               m_size(length) {
@@ -162,18 +169,17 @@ public:
         CSAMPLE& operator[](SINT index) const {
             return *data(index);
         }
-    private:
+      private:
         CSAMPLE* m_data;
         SINT m_size;
     };
 
-private:
+  private:
     CSAMPLE* m_data;
     SINT m_size;
 };
 
-namespace std
-{
+namespace std {
 
 // Template specialization of std::swap for SampleBuffer.
 template<>
@@ -181,7 +187,7 @@ inline void swap(SampleBuffer& lhs, SampleBuffer& rhs) {
     lhs.swap(rhs);
 }
 
-}
+}  // namespace std
 
 #endif // SAMPLEBUFFER_H
 =======
