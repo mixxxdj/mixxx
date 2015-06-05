0e916ff1cc98f2ee630cfdb655509a527a43e5ad
Uwe Klotz <uwe_klotz@web.de>
2015-06-05 10:35:10 +0200

    Unify export of SoundSources from plugin libraries

--- src/sources/soundsourceplugin.cpp
+++ src/sources/soundsourceplugin.cpp
@@ -2,6 +2,25 @@
 #include "defs_version.h"
 
 
+namespace Mixxx {
+
+namespace {
+
+void deleteSoundSource(SoundSource* pSoundSource) {
+    // The SoundSource must be deleted from within the external library
+    // that has allocated it.
+    delete pSoundSource;
+}
+
+} // anonymous namespace
+
+SoundSourcePointer exportSoundSourcePlugin(
+        SoundSourcePlugin* pSoundSourcePlugin) {
+    return SoundSourcePointer(pSoundSourcePlugin, deleteSoundSource);
+}
+
+} // namespace Mixxx
+
 extern "C" MIXXX_SOUNDSOURCEPLUGINAPI_EXPORT const char* Mixxx_getVersion() {
     return VERSION;
 }
