841f7b5527a5f8feef9516ae22e7bf490ac57b2b
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2015-07-17 20:14:52 +0300

    Now Mono files also works

--- src/encoder/encoderffmpegresample.cpp
+++ src/encoder/encoderffmpegresample.cpp
@@ -50,15 +50,25 @@ int EncoderFfmpegResample::openMixxx(enum AVSampleFormat inSampleFmt,
 
 unsigned int EncoderFfmpegResample::reSampleMixxx(AVFrame *inframe, quint8 **outbuffer) {
     quint8 *l_ptrBuf = NULL;
+    quint8 *l_ptrDataSrc1 = NULL;
+    quint8 *l_ptrDataSrc2 = NULL;
     bool l_bSupported = false;
     qint64 l_lInReadBytes = av_samples_get_buffer_size(NULL, m_pCodecCtx->channels,
                             inframe->nb_samples,
                             m_pCodecCtx->sample_fmt, 1);
 
+    // Force stereo
     qint64 l_lOutReadBytes = av_samples_get_buffer_size(NULL, m_pCodecCtx->channels,
                              inframe->nb_samples,
                              m_pOutSampleFmt, 1);
 
+    if (m_pCodecCtx->channels == 1) {
+        l_ptrDataSrc1 = inframe->data[0];
+        l_ptrDataSrc2 = inframe->data[0];
+    } else {
+        l_ptrDataSrc1 = inframe->data[0];
+        l_ptrDataSrc2 = inframe->data[1];
+    }
     // This is Cap frame or very much broken!
     // So return before something goes bad
     if (inframe->nb_samples <= 0) {
@@ -71,64 +81,68 @@ unsigned int EncoderFfmpegResample::reSampleMixxx(AVFrame *inframe, quint8 **out
     }
     l_ptrBuf = (quint8 *)av_malloc(l_lOutReadBytes);
 
-    switch(m_pCodecCtx->sample_fmt) {
+    switch (m_pCodecCtx->sample_fmt) {
 #if LIBAVCODEC_VERSION_INT >= 3482368
-        case AV_SAMPLE_FMT_FLTP: {
-            SampleUtil::interleaveBuffer((CSAMPLE *)l_ptrBuf, (CSAMPLE *)inframe->data[0],
-                                        (CSAMPLE *)inframe->data[1], l_lOutReadBytes / 8);
-            outbuffer[0] = l_ptrBuf;
-            l_bSupported = true;
+    case AV_SAMPLE_FMT_FLTP: {
+        if (m_pCodecCtx->channels > 1) {
+            SampleUtil::interleaveBuffer((CSAMPLE *)l_ptrBuf, (CSAMPLE *)l_ptrDataSrc1,
+                                         (CSAMPLE *)l_ptrDataSrc2, l_lOutReadBytes / 8);
+        } else {
+            memcpy(l_ptrBuf, l_ptrDataSrc1, l_lInReadBytes);
         }
-        break;
-        case AV_SAMPLE_FMT_S16P: {
+        outbuffer[0] = l_ptrBuf;
+        l_bSupported = true;
+    }
+    break;
+    case AV_SAMPLE_FMT_S16P: {
+        if (m_pCodecCtx->channels > 1) {
             quint8 *l_ptrConversion = (quint8 *)av_malloc(l_lInReadBytes);
-            quint16 *l_ptrSrc1 = (quint16 *) inframe->data[0];
-            quint16 *l_ptrSrc2 = (quint16 *) inframe->data[1];
             quint16 *l_ptrDest = (quint16 *) l_ptrConversion;
-
-            // note: LOOP VECTORIZED.
+            quint16 *l_ptrSrc1 = (quint16 *) l_ptrDataSrc1;
+            quint16 *l_ptrSrc2 = (quint16 *) l_ptrDataSrc2;
+            // De-Interleave (a.k.a remove planar to PCM)
             for (int i = 0; i < (l_lInReadBytes / 4); ++i) {
                 l_ptrDest[2 * i] = l_ptrSrc1[i];
                 l_ptrDest[2 * i + 1] = l_ptrSrc2[i];
             }
-
             SampleUtil::convertS16ToFloat32((CSAMPLE *)l_ptrBuf, (SAMPLE *)l_ptrConversion, l_lInReadBytes / 2);
-            outbuffer[0] = l_ptrBuf;
-            l_bSupported = true;
+        } else {
+            SampleUtil::convertS16ToFloat32((CSAMPLE *)l_ptrBuf, (SAMPLE *)l_ptrDataSrc1, l_lInReadBytes / 2);
         }
-        break;
+        outbuffer[0] = l_ptrBuf;
+        l_bSupported = true;
+    }
+    break;
 #endif
-        case AV_SAMPLE_FMT_FLT: {
-            memcpy(l_ptrBuf, inframe->data[0], l_lInReadBytes);
+    case AV_SAMPLE_FMT_FLT: {
+        memcpy(l_ptrBuf, l_ptrDataSrc1, l_lInReadBytes);
+        outbuffer[0] = l_ptrBuf;
+        l_bSupported = true;
+    }
+    break;
+    case AV_SAMPLE_FMT_S16: {
+        SampleUtil::convertS16ToFloat32((CSAMPLE *)l_ptrBuf, (SAMPLE *)l_ptrDataSrc1, l_lInReadBytes / 2);
+        outbuffer[0] = l_ptrBuf;
+        l_bSupported = true;
+    }
+    break;
 
-            outbuffer[0] = l_ptrBuf;
-            l_bSupported = true;
-        }
-        break;
-        case AV_SAMPLE_FMT_S16: {
-            SampleUtil::convertS16ToFloat32((CSAMPLE *)l_ptrBuf, (SAMPLE *)inframe->data[0], l_lInReadBytes / 2);
-            outbuffer[0] = l_ptrBuf;
-            l_bSupported = true;
-        }
-        break;
-
-        case AV_SAMPLE_FMT_NONE:
-        case AV_SAMPLE_FMT_U8:
-        case AV_SAMPLE_FMT_S32:
-        case AV_SAMPLE_FMT_DBL:
-        case AV_SAMPLE_FMT_U8P:
+    case AV_SAMPLE_FMT_NONE:
+    case AV_SAMPLE_FMT_U8:
+    case AV_SAMPLE_FMT_S32:
+    case AV_SAMPLE_FMT_DBL:
+    case AV_SAMPLE_FMT_U8P:
 #if LIBAVCODEC_VERSION_INT >= 3482368
-        case AV_SAMPLE_FMT_S32P:
-        case AV_SAMPLE_FMT_DBLP:
+    case AV_SAMPLE_FMT_S32P:
+    case AV_SAMPLE_FMT_DBLP:
 #endif
-        case AV_SAMPLE_FMT_NB:
-        default:
-            qDebug() << "Unsupported sample format:" << av_get_sample_fmt_name(m_pCodecCtx->sample_fmt);
+    case AV_SAMPLE_FMT_NB:
+    default:
+        qDebug() << "Unsupported sample format:" << av_get_sample_fmt_name(m_pCodecCtx->sample_fmt);
         break;
     }
 
-    if(l_bSupported == true)
-    {
+    if (l_bSupported == true) {
         return l_lOutReadBytes;
     }
 
