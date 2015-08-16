083633ef65f77e9da530dede115ddeb95c472d9d
Daniel Schürmann <daschuer@mixxx.org>
2015-08-16 23:21:19 +0200

    Added tests for all transitions to have a 100 % coverage of changeScannerState()

--- src/test/libraryscannertest.cpp
+++ src/test/libraryscannertest.cpp
@@ -23,68 +23,42 @@ class LibraryScannerTest : public MixxxTest {
 };
 
 TEST_F(LibraryScannerTest, ScannerRoundtrip) {
+    // Normal flow:
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::STARTING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::STARTING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::SCANNING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::SCANNING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::FINISHED);
     EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
 
+    // No Tracks:
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::STARTING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::STARTING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::IDLE);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
 
-    /*
-    bool LibraryScanner::changeScannerState(ScannerState newState) {
-        // Allowed State transitions:
-        // IDLE -> STARTING
-        // STARTING -> IDLE
-        // STARTING -> SCANNING
-        // SCANNING -> FINISHED
-        // FINISHED -> IDLE
-        // every state can change to CANCELING
-        // CANCELING -> IDLE
-        switch (newState) {
-        case IDLE:
-            // we are leaving STARTING  or CANCELING state
-            // m_state is already IDLE if a scan was canceled
-            m_state = IDLE;
-            m_stateMutex.unlock();
-            return true;
-        case STARTING:
-            // we need to lock the mutex during the STARTING state
-            // to prevent loosing cancel commands or start the scanner
-            // twice
-            if (m_stateMutex.tryLock()) {
-                if (m_state != IDLE) {
-                    qDebug() << "LibraryScanner: Scan already in progress.";
-                    m_stateMutex.unlock();
-                    return false;
-                }
-                m_state = STARTING;
-                return true;
-            } else {
-                qDebug() << "LibraryScanner: mutex locked, state =" << m_state;
-                return false;
-            }
-        case SCANNING:
-            DEBUG_ASSERT(m_state == STARTING);
-            // Transition protected by the mutex is over now
-            // Allow canceling
-            m_state = SCANNING;
-            m_stateMutex.unlock();
-            return true;
-        case CANCELING:
-            // canceling is always possible, but wait
-            // until there is no scan starting.
-            m_stateMutex.lock();
-            m_state = CANCELING;
-            return true;
-        case FINISHED:
-            // we must not lock the mutex here, because
-            // the mutex is already locked in case we
-            // are canceling.
-            // There is no race condition, since the state
-            // is set to IDLE after canceling as well
-            m_state = IDLE;
-            return true;
-        default:
-            DEBUG_ASSERT(false);
-            return false;
-        }
-        */
+    // Cancel during scaning:
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::STARTING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::STARTING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::SCANNING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::SCANNING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::CANCELING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::CANCELING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::FINISHED);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::IDLE);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
 
+    // restart during canceling :
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::CANCELING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::CANCELING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::STARTING);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::CANCELING);
+    m_pLibraryScanner->changeScannerState(LibraryScanner::IDLE);
+    EXPECT_EQ(m_pLibraryScanner->m_state, LibraryScanner::IDLE);
 }
 
