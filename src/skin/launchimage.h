fd89e5ca77904f30dc58dd08554187d97baf8b8f
Daniel Schürmann <daschuer@mixxx.org>
2015-08-20 22:36:42 +0200

    move launch image description to the header

--- src/skin/launchimage.h
+++ src/skin/launchimage.h
@@ -5,6 +5,35 @@
 
 class QProgressBar;
 
+// This is a widget that is shown in the Mixxx main window
+// until the skin is ready to use.
+// It shows a centered Image and a progress bar below.
+// By default a symbolic Mixxx icon and logo is shown.
+// It can be modified in the skin.xml file at <skin> sction,
+// to match the  skin like that:
+//    <LaunchImageStyle>
+//        LaunchImage { background-color: #202020; }
+//        QLabel {
+//            image: url(skin:/style/mixxx-icon-logo-symbolic.png);
+//            padding:0;
+//            margin:0;
+//            border:none;
+//            min-width: 208px;
+//            min-height: 48px;
+//            max-width: 208px;
+//            max-height: 48px;
+//        }
+//        QProgressBar {
+//            background-color: #202020;
+//            border:none;
+//            min-width: 208px;
+//            min-height: 3px;
+//            max-width: 208px;
+//            max-height: 3px;
+//        }
+//        QProgressBar::chunk { background-color: #ec4522; }
+//    </LaunchImageStyle>
+
 class LaunchImage: public QWidget {
     Q_OBJECT
   public:
