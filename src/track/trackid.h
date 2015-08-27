6471b748a4c3aeb58104d1fceaf2dbd876320bb8
Uwe Klotz <uwe_klotz@web.de>
2015-08-27 17:09:11 +0200

    Add workaround for missing C++11 feature in VS2013

--- src/track/trackid.h
+++ src/track/trackid.h
@@ -7,8 +7,17 @@
 
 class TrackId: public DbId {
 public:
+#if defined(_MSC_VER) && (_MSC_VER < 1500)
+    // NOTE(uklotzde): Inheriting constructors are supported since VS2015.
+    // Use a default constructor and a single-argument constructor with
+    // perfect forwarding instead.
+    TrackId() {}
+    template<typename T>
+    explicit TrackId(T&& arg): DbId(std::forward<T>(arg)) {}
+#else
     // Inherit constructors from base class
     using DbId::DbId;
+#endif
 };
 
 Q_DECLARE_METATYPE(TrackId)
