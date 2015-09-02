1cc60d9a74daa5a51a4f1b03c534e7178583391e
Sebastien Blaisot <sebastien@blaisot.org>
2015-09-02 20:54:00 +0200

    (wix) add %WIX% variable to package build batch to avoid modifying %PATH% on build server

--- build/wix/build32.bat
+++ build/wix/build32.bat
@@ -1,10 +1,10 @@
 @echo off
 
 for /F %%d IN (subdirs.txt) DO (
-  heat dir ..\..\dist32\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
-  candle -nologo -dPlatform=x86 -d%%dVar=..\..\dist32\%%d -arch x86 %%d.wxs
+  "%WIX%"\bin\heat.exe dir ..\..\dist32\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
+  "%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -d%%dVar=..\..\dist32\%%d -arch x86 %%d.wxs
 )
 
-candle -nologo -dPlatform=x86 -arch x86 mixxx.wxs
+"%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -arch x86 mixxx.wxs
 
-light -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-32.msi *.wixobj
+"%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-32.msi *.wixobj
--- build/wix/build64.bat
+++ build/wix/build64.bat
@@ -1,10 +1,10 @@
 @echo off
 
 for /F %%d IN (subdirs.txt) DO (
-  heat dir ..\..\dist64\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
-  candle -nologo -dPlatform=x64 -d%%dVar=..\..\dist64\%%d -arch x64 %%d.wxs
+  "%WIX%"\bin\heat.exe dir ..\..\dist64\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
+  "%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -d%%dVar=..\..\dist64\%%d -arch x64 %%d.wxs
 )
 
-candle -nologo -dPlatform=x64 -arch x64 mixxx.wxs
+"%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -arch x64 mixxx.wxs
 
-light -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-64.msi *.wixobj
+"%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-64.msi *.wixobj
