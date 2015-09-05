8decf89dd553cd9c5f1159bf3e557e2e4164fb3d
Sebastien Blaisot <sebastien@blaisot.org>
2015-09-05 16:28:01 +0200

    (wix) include VCRedist

--- build/wix/build32.bat
+++ build/wix/build32.bat
@@ -1,11 +1,14 @@
 @echo off
 
+REM set this to the folder where you build the dependencies
+set WINLIB_PATH=D:\mixxx-buildserver64
+
 del *.wixobj
 del *.wixpdb
 
 for /F %%d IN (subdirs.txt) DO (
   "%WIX%"\bin\heat.exe dir ..\..\dist32\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
-  "%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -d%%dVar=..\..\dist32\%%d -arch x86 %%d.wxs
+  "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x86 -d%%dVar=..\..\dist32\%%d -arch x86 %%d.wxs
 )
 
 SET promo=no
@@ -13,10 +16,10 @@ SET promo=no
 IF EXIST ..\..\dist32\promo (
   SET promo=yes
   "%WIX%"\bin\heat.exe dir ..\..\dist32\promo -nologo -sfrag -suid -ag -srd -cg promoComp -dr promoDir -out promo.wxs -sw5150 -var var.promoVar  
-  "%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -dpromoVar=..\..\dist32\promo -arch x86 promo.wxs
+  "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x86 -dpromoVar=..\..\dist32\promo -arch x86 promo.wxs
 )
 
-"%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -arch x86 warningDlg.wxs
-"%WIX%"\bin\candle.exe -nologo -dPlatform=x86 -dPromo=%promo% -arch x86 mixxx.wxs
+"%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x86 -arch x86 warningDlg.wxs
+"%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x86 -dPromo=%promo% -arch x86 mixxx.wxs
 
 "%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-32.msi *.wixobj
--- build/wix/build64.bat
+++ build/wix/build64.bat
@@ -1,11 +1,14 @@
 @echo off
 
+REM set this to the folder where you build the dependencies
+set WINLIB_PATH=D:\mixxx-buildserver64
+
 del *.wixobj
 del *.wixpdb
 
 for /F %%d IN (subdirs.txt) DO (
   "%WIX%"\bin\heat.exe dir ..\..\dist64\%%d -nologo -sfrag -suid -ag -srd -cg %%dComp -dr %%dDir -out %%d.wxs -sw5150 -var var.%%dVar  
-  "%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -d%%dVar=..\..\dist64\%%d -arch x64 %%d.wxs
+  "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -d%%dVar=..\..\dist64\%%d -arch x64 %%d.wxs
 )
 
 SET promo=no
@@ -13,10 +16,10 @@ SET promo=no
 IF EXIST ..\..\dist64\promo (
   SET promo=yes
   "%WIX%"\bin\heat.exe dir ..\..\dist64\promo -nologo -sfrag -suid -ag -srd -cg promoComp -dr promoDir -out promo.wxs -sw5150 -var var.promoVar  
-  "%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -dpromoVar=..\..\dist64\promo -arch x64 promo.wxs
+  "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -dpromoVar=..\..\dist64\promo -arch x64 promo.wxs
 )
 
-"%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -arch x64 warningDlg.wxs
-"%WIX%"\bin\candle.exe -nologo -dPlatform=x64 -dPromo=%promo% -arch x64 mixxx.wxs
+"%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -arch x64 warningDlg.wxs
+"%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -dPromo=%promo% -arch x64 mixxx.wxs
 
 "%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:en-us -loc Localization\en-us\mixxx_en-us.wxl -out mixxx-64.msi *.wixobj
