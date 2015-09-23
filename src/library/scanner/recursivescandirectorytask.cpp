mixxxdj/mixxx

commit fdb844158be230ffffd0c671d2682961cc975e3a

Update dir hashes after importing files
Only updated the Hash of a folder only if we know for sure that the
files inside have been scanned. Otherwise we will have an inconsistent
Library.

src/library/scanner/recursivescandirectorytask.cpp
@@ -79,14 +79,11 @@ void RecursiveScanDirectoryTask::run() {
     if (prevHash != newHash) {
         // Rescan that mofo! If importing fails then the scan was cancelled so
         // we return immediately.
-        if (!filesToImport.isEmpty()) {
-            m_pScanner->queueTask(new ImportFilesTask(m_pScanner, m_scannerGlobal,
-                                                      filesToImport, possibleCovers,
-                                                      m_pToken));
-        }
+        m_pScanner->queueTask(new ImportFilesTask(m_pScanner, m_scannerGlobal,
+                                                  dirPath, newHash, prevHashExists,
+                                                  filesToImport, possibleCovers,
+                                                  m_pToken));
 
-        // Insert or update the hash in the database.
-        emit(directoryHashed(dirPath, !prevHashExists, newHash));
     } else {
         emit(directoryUnchanged(dirPath));
     }

src/library/scanner/importfilestask.cpp
@@ -6,10 +6,14 @@
 
 ImportFilesTask::ImportFilesTask(LibraryScanner* pScanner,
                                  const ScannerGlobalPointer scannerGlobal,
+                                 const QString& dirPath,
+                                 const bool prevHashExists,
+                                 const int newHash,
                                  const QLinkedList<QFileInfo>& filesToImport,
                                  const QLinkedList<QFileInfo>& possibleCovers,
                                  SecurityTokenPointer pToken)
-        : ScannerTask(pScanner, scannerGlobal),
+        : ScannerTask(pScanner, scannerGlobal), m_dirPath(dirPath),
+          m_prevHashExists(prevHashExists), m_newHash(newHash),
           m_filesToImport(filesToImport),
           m_possibleCovers(possibleCovers),
           m_pToken(pToken) {
@@ -58,5 +62,7 @@ void ImportFilesTask::run() {
             emit(addNewTrack(pTrack));
         }
     }
+    // Insert or update the hash in the database.
+    emit(directoryHashed(m_dirPath, !m_prevHashExists, m_newHash));
     setSuccess(true);
 }

src/library/scanner/importfilestask.h
@@ -15,6 +15,9 @@ class ImportFilesTask : public ScannerTask {
   public:
     ImportFilesTask(LibraryScanner* pScanner,
                     const ScannerGlobalPointer scannerGlobal,
+                    const QString& dirPath,
+                    const bool prevHashExists,
+                    const int newHash,
                     const QLinkedList<QFileInfo>& filesToImport,
                     const QLinkedList<QFileInfo>& possibleCovers,
                     SecurityTokenPointer pToken);
@@ -23,6 +26,9 @@ class ImportFilesTask : public ScannerTask {
     virtual void run();
 
   private:
+    const QString m_dirPath;
+    const bool m_prevHashExists;
+    const int m_newHash;
     const QLinkedList<QFileInfo> m_filesToImport;
     const QLinkedList<QFileInfo> m_possibleCovers;
     SecurityTokenPointer m_pToken;

commit 1cfb8c4dd1d1bae527926778b321478f23ef8162

Fix parameter order of ImportFilesTask, broken since fdb844158be230ffffd0c671d2682961cc975e3a

src/library/scanner/recursivescandirectorytask.cpp
@@ -90,7 +90,7 @@ void RecursiveScanDirectoryTask::run() {
             if (!filesToImport.isEmpty()) {
                 m_pScanner->queueTask(
                         new ImportFilesTask(m_pScanner, m_scannerGlobal, dirPath,
-                                            newHash, prevHashExists, filesToImport,
+                                            prevHashExists, newHash, filesToImport,
                                             possibleCovers, m_pToken));
             } else {
                 emit(directoryHashedAndScanned(dirPath, !prevHashExists, newHash));