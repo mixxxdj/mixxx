ed554ca24b3ab47ec5d5084275e37675497ea81d
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2015-09-26 11:34:06 +0200

    Added potmeterparameters.cpp to build/depends.py

--- build/depends.py
+++ build/depends.py
@@ -525,6 +525,7 @@ class MixxxCore(Feature):
                    "controlpotmeter.cpp",
                    "controllinpotmeter.cpp",
                    "controllogpotmeter.cpp",
+                   "potmeterparameters.cpp",
                    "controleffectknob.cpp",
                    "controlpushbutton.cpp",
                    "controlindicator.cpp",
