4cc07d87adeffc3d02270aface2b39e6917124b8
Daniel Schürmann <daschuer@mixxx.org>
2015-10-23 08:26:51 +0200

    fix AutoPanEffect::computeLawCoefficient()

--- src/effects/native/autopaneffect.cpp
+++ src/effects/native/autopaneffect.cpp
@@ -215,8 +215,8 @@ void AutoPanEffect::processChannel(const ChannelHandle& handle, PanGroupState* p
 }
 
 double AutoPanEffect::computeLawCoefficient(double position) {
-    // position is a result of sin() so betwwen -1 and 1
+    // position is a result of sin() so between -1 and 1
     // full left/right => 1 + 1 / sqrt(abs(1 or -1) + 1) = 1,707106781
     // center => 1 + 1 / sqrt(abs(0) + 1) = 2
-    return 1 + 1 / sqrt(abs(position) + 1);
+    return 1 + 1 / sqrt(std::abs(position) + 1);
 }
