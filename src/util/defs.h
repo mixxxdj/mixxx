2aac3f2fb7f12b5ec28e8667b04e433fa961429f
Uwe Klotz <uwe_klotz@web.de>
2015-11-14 12:45:36 +0100

    Workaround for missing 'constexpr' in Visual Studio 2013 (12.0)

--- src/util/defs.h
+++ src/util/defs.h
@@ -1,6 +1,18 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+// Support for 'constexpr' is available since Visual Studio 2015
+// NOTE(uklotzde) This workaround is ugly, but could easily be
+// removed as soon as Visual Studio 2015 is required for building
+// Mixxx on Windows.
+#if defined(_MSC_VER) && _MSC_VER < 1900
+#undef MIXXX_HAS_CONSTEXPR
+#define mixxx_constexpr const
+#else
+#define MIXXX_HAS_CONSTEXPR 1
+#define mixxx_constexpr constexpr
+#endif
+
 // Used for returning errors from functions.
 enum Result {
     OK = 0,
