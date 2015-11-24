a939d976b12b4261f8ba14f7ba5e1f2ce9664342
Daniel Schürmann <daschuer@mixxx.org>
2015-11-24 23:32:50 +0100

    Split up rescaler functions and added a comment

--- src/util/rescaler.h
+++ src/util/rescaler.h
@@ -6,12 +6,18 @@
 class RescalerUtils {
   public:
 
+    // converts a value on a linear scale to a 1/x scale staring at 1
     static double linearToOneByX(double in, double inMin, double inMax, double outMax) {
-        return outMax / (((inMax - in) / (inMax - inMin) * (outMax - 1)) + 1);
+        double outRange = outMax - 1;
+        double inRange = inMax - inMin;
+        return outMax / (((inMax - in) / inRange * outRange) + 1);
     }
 
+    // converts value on a 1/x scale starting by 1 to a linear scale
     static double oneByXToLinear(double in, double inMax, double outMin, double outMax) {
-        return outMax - (((inMax / in) - 1) / (inMax - 1) * (outMax - outMin));
+        double outRange = outMax - outMin;
+        double inRange = inMax - 1;
+        return outMax - (((inMax / in) - 1) / inRange * outRange);
     }
 
   private:
