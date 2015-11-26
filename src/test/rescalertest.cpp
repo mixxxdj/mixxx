a22f93481ac60be31bfc3a45a85bca8f4e6ea669
Daniel Schürmann <daschuer@mixxx.org>
2015-11-26 22:10:49 +0100

    use EXPECT_DOUBLE_EQ in resacler test

--- src/test/rescalertest.cpp
+++ src/test/rescalertest.cpp
@@ -17,49 +17,49 @@ TEST_F(RescalerUtilsTest, Test) {
 
     // test upper border
     result = RescalerUtils::linearToOneByX(100, 0, 100, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     result = RescalerUtils::linearToOneByX(100, 50, 100, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     result = RescalerUtils::linearToOneByX(50, 40, 50, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     // test lower border
     result = RescalerUtils::linearToOneByX(0, 0, 100, 1000);
-    EXPECT_EQ(result, 1);
+    EXPECT_DOUBLE_EQ(result, 1);
 
     result = RescalerUtils::linearToOneByX(50, 50, 100, 1000);
-    EXPECT_EQ(result, 1);
+    EXPECT_DOUBLE_EQ(result, 1);
 
     result = RescalerUtils::linearToOneByX(40, 40, 50, 1000);
-    EXPECT_EQ(result, 1);
+    EXPECT_DOUBLE_EQ(result, 1);
 
     // test upper border inverse
     result = RescalerUtils::oneByXToLinear(100, 100, 0, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     result = RescalerUtils::oneByXToLinear(100, 100, 50, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     result = RescalerUtils::oneByXToLinear(50, 50, 50, 1000);
-    EXPECT_EQ(result, 1000);
+    EXPECT_DOUBLE_EQ(result, 1000);
 
     // test lower border inverse
     result = RescalerUtils::oneByXToLinear(1, 100, 0, 1000);
-    EXPECT_EQ(result, 0);
+    EXPECT_DOUBLE_EQ(result, 0);
 
     result = RescalerUtils::oneByXToLinear(1, 50, 100, 1000);
-    EXPECT_EQ(result, 100);
+    EXPECT_DOUBLE_EQ(result, 101);
 
     result = RescalerUtils::oneByXToLinear(1, 40, 50, 1000);
-    EXPECT_EQ(result, 50);
+    EXPECT_DOUBLE_EQ(result, 51);
 
     // Test Roundtrip
     // test upper border
     result = RescalerUtils::linearToOneByX(50, 0, 100, 1000);
     result = RescalerUtils::oneByXToLinear(result, 1000, 0, 100);
-    EXPECT_EQ(result, 50);
+    EXPECT_DOUBLE_EQ(result, 50);
 }
 
 }  // namespace
