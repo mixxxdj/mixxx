1cc7583fc889984e10dd1b2e3952a0aff4058daf
Daniel Schürmann <daschuer@mixxx.org>
2015-12-12 21:01:09 +0100

    fpclassify.h: add include guard

--- src/util/fpclassify.h
+++ src/util/fpclassify.h
@@ -1,3 +1,5 @@
+#ifndef UTIL_FPCLASSIFY_H
+#define UTIL_FPCLASSIFY_H
 
 #define isnan mixxx_isnan
 #define isinf mixxx_isinf
@@ -15,3 +17,5 @@ int mixxx_isfinite(double x);
 int mixxx_isnormal(double x);
 int mixxx_isnan(double x);
 int mixxx_isinf(double x);
+
+#endif // UTIL_FPCLASSIFY_H
