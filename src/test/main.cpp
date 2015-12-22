745a2d643b1c1fee4d08ffbeceb4b14febd4f65b
RJ Ryan <rryan@mixxx.org>
2015-12-22 17:34:55 -0500

    Disable ErrorDialogHandler when running tests.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 #include "util/console.h"
-
+#include "errordialoghandler.h"
 
 int main(int argc, char **argv) {
     Console console();
+    // We never want to popup error dialogs when running tests.
+    ErrorDialogHandler::setEnabled(false);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
