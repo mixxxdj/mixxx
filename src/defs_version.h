2de0d39922537a240fa83123c3ca5115a32e540a
RJ Ryan <rryan@mixxx.org>
2015-12-28 13:05:04 -0500

    Version bump to 2.0.0.1. \o/

--- src/defs_version.h
+++ src/defs_version.h
@@ -19,5 +19,5 @@
 // We define the version to be the branch name in Sconscript.env, unless it's a release branch
 #ifndef VERSION
     // In that case, this value is used
-    #define VERSION "2.0.0-rc2"
+    #define VERSION "2.0.0.1"
 #endif
