edf8be9fb3d4a63ccc424b42cef4fb2c528998bc
Sebastien Blaisot <sebastien@blaisot.org>
2016-01-07 01:22:18 +0100

    (wix) add multilingual MSI generation

--- build/wix/build64.bat
+++ build/wix/build64.bat
@@ -3,10 +3,17 @@
 REM set this to the folder where you build the dependencies
 set WINLIB_PATH=D:\mixxx-buildserver64
 
+REM The default language and corresponding LangID
+SET DefaultLanguage=en-us
+SET LangIds=1033
+
+REM Development tool settings
+SET WinSDKVersion=v7.0
 
 echo *** Cleaning
 if exist *.wixobj del *.wixobj
 if exist *.wixpdb del *.wixpdb 2>NUL
+if exist *.mst del *.mst 2>NUL
 if exist subdirs\*.* del /Q subdirs\*.*
 
 echo.
@@ -32,10 +39,28 @@ REM Harvest main DLL from install dir
 "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -arch x64 warningDlg.wxs
 "%WIX%"\bin\candle.exe -nologo -dWINLIBPATH=%WINLIB_PATH% -dPlatform=x64 -dPromo=%promo% -arch x64 mixxx.wxs
 
+echo.
+echo *** Building package for default language %DefaultLanguage%
+"%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:%DefaultLanguage% -loc Localization\mixxx_%DefaultLanguage%.wxl -out mixxx-64-multilingual.msi *.wixobj subdirs\*.wixobj
+
 FOR %%G IN (Localization\*.wxl) DO (
   REM skip 19 chars (Localization\mixxx_), keep until end -4 char (.wxl)
   set _locfile=%%G
   set _locale=!_locfile:~19,-4!
-  echo "*** Building package for locale !_locale!"
-  "%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:!_locale! -loc %%G -out mixxx-64-!_locale!.msi *.wixobj subdirs\*.wixobj
+  IF "!_locale!" NEQ "%DefaultLanguage%" (
+    REM Look for LangID in the wxl file. Don't ask me to explain the magic of this line ;)
+    for /f "delims=<> tokens=3" %%i in ('findstr "^[space]*<<String Id=.Language" Localization\mixxx_!_locale!.wxl') do set _LangID=%%i
+    echo.
+    echo *** Building package transform for locale !_locale! LangID !_LangID!
+    "%WIX%"\bin\light.exe -nologo -sw1076 -ext WixUIExtension -cultures:!_locale! -loc %%G -out mixxx-64-!_locale!.msi *.wixobj subdirs\*.wixobj
+    "%WIX%"\bin\torch.exe -nologo mixxx-64-multilingual.msi mixxx-64-!_locale!.msi -o !_locale!.mst
+    cscript "%ProgramFiles%\Microsoft SDKs\Windows\%WinSDKVersion%\Samples\sysmgmt\msi\scripts\wisubstg.vbs" mixxx-64-multilingual.msi !_locale!.mst !_LangID!
+    SET LangIDs=!LangIDs!,!_LangID!
+    del /Q mixxx-64-!_locale!.msi
+    del /Q !_locale!.mst
+  )
 )
+
+echo.
+echo *** Add all supported languages to MSI Package attribute
+cscript "%ProgramFiles%\Microsoft SDKs\Windows\%WinSDKVersion%\Samples\sysmgmt\msi\scripts\WiLangId.vbs" mixxx-64-multilingual.msi Package %LangIDs%
