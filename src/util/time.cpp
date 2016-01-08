c2af9e6eeb469718b9aa069b90a719fac80dd0d9
Uwe Klotz <uwe_klotz@web.de>
2016-01-08 19:25:58 +0100

    Move definition of Time::formatSeconds() into .cpp file

--- src/util/time.cpp
+++ src/util/time.cpp
@@ -6,3 +6,33 @@ LLTIMER Time::s_timer;
 bool Time::s_testMode = false;
 // static
 qint64 Time::s_testElapsed_nsecs = 0;
+
+// static
+QString Time::formatSeconds(double dSeconds, bool showCentis) {
+    if (dSeconds < 0) {
+        return "?";
+    }
+
+    const int days = static_cast<int>(dSeconds) / kSecondsPerDay;
+    dSeconds -= days * kSecondsPerDay;
+
+    // NOTE(uklotzde): Time() constructs a 'null' object, but
+    // we need 'zero' here.
+    QTime t = QTime(0, 0).addMSecs(dSeconds * kMillisPerSecond);
+
+    QString formatString =
+            (days > 0 ? (QString::number(days) %
+                         QLatin1String("'d', ")) : QString()) %
+            QLatin1String(days > 0 || t.hour() > 0 ? "hh:mm:ss" : "mm:ss") %
+            QLatin1String(showCentis ? ".zzz" : "");
+
+    QString timeString = t.toString(formatString);
+
+    // The format string gives us milliseconds but we want
+    // centiseconds. Slice one character off.
+    if (showCentis) {
+        timeString = timeString.left(timeString.length() - 1);
+    }
+
+    return timeString;
+}
