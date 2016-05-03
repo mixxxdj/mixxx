c997736351893b695f3ba7ff4e3a1765cbc5c7f0
RJ Ryan <rryan@mixxx.org>
2016-05-02 21:23:41 -0700

    Add M_PREDICT_TRUE / M_PREDICT_FALSE macros.

--- src/util/platform.h
+++ src/util/platform.h
@@ -7,6 +7,8 @@
 #define M_RESTRICT __restrict__
 #define M_PACK(statement) statement __attribute__((packed))
 #define M_MUST_USE_RESULT __attribute__((warn_unused_result))
+#define M_PREDICT_FALSE(x) (__builtin_expect(x, 0))
+#define M_PREDICT_TRUE(x) (__builtin_expect(!!(x), 1))
 #elif defined(_MSC_VER)
 // MSVC
 #define M_ALIGN(x) __declspec(align(x))
@@ -15,6 +17,8 @@
 // MSVC supports _Check_return_ but it's a prefix, not a suffix. We could do
 // something like M_PACK but for now don't bother.
 #define M_MUST_USE_RESULT
+#define M_PREDICT_FALSE(x) (x)
+#define M_PREDICT_TRUE(x) (x)
 #else
 #error We do not support your compiler. Please email mixxx-devel@lists.sourceforge.net and tell us about your use case.
 #endif
