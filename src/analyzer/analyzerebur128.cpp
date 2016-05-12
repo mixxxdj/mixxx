660e18f2ec3386b83dfaa2f90041deb48302d36f
RJ Ryan <rryan@mixxx.org>
2016-05-12 11:31:25 -0700

    Remove DEBUG_ASSERTs in AnalyzerEbur128. These are actually normal cases.

--- src/analyzer/analyzerebur128.cpp
+++ src/analyzer/analyzerebur128.cpp
@@ -40,6 +40,8 @@ bool AnalyzerEbur128::isDisabledOrLoadStoredSuccess(TrackPointer tio) const {
 void AnalyzerEbur128::cleanup() {
     if (isInitialized()) {
         ebur128_destroy(&m_pState);
+        // ebur128_destroy clears the pointer but let's not rely on that.
+        m_pState = nullptr;
     }
     DEBUG_ASSERT(!isInitialized());
 }
@@ -50,8 +52,7 @@ void AnalyzerEbur128::cleanup(TrackPointer tio) {
 }
 
 void AnalyzerEbur128::process(const CSAMPLE *pIn, const int iLen) {
-    DEBUG_ASSERT_AND_HANDLE(isInitialized()) {
-        qWarning() << "AnalyzerEbur128::process(): Not initialized!";
+    if (!isInitialized()) {
         return;
     }
     ScopedTimer t("AnalyzerEbur128::process()");
@@ -64,8 +65,7 @@ void AnalyzerEbur128::process(const CSAMPLE *pIn, const int iLen) {
 }
 
 void AnalyzerEbur128::finalize(TrackPointer tio) {
-    DEBUG_ASSERT_AND_HANDLE(isInitialized()) {
-        qWarning() << "AnalyzerEbur128::finalize(): Not initialized!";
+    if (!isInitialized()) {
         return;
     }
     double averageLufs;
