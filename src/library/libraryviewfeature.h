8b395e5a7c533f6d0ccf69db30f5909109f73cc9
jmigual <j.marce.igual@gmail.com>
2016-06-01 14:30:32 +0200

    Add comments and onSearch function

--- src/library/libraryviewfeature.h
+++ src/library/libraryviewfeature.h
@@ -15,14 +15,24 @@ class LibraryViewFeature {
 
     // A LibraryViewFeature always has a right and a left pane
     virtual QWidget* getLeftPane() = 0;
-    
+
     // Since the right pane can be shown in two panes, this must return a new
     // widget every time and handle the proper connections with the left pane
     // Qt does not allow for a widget to have multiple parents
     virtual QWidget* getRightPane() = 0;
 
+    // If it has not the search option, the search bar must be hidden
+    virtual bool hasSearch() = 0;
+
+  public slots:
+
+    // Receives the text to search text and the widget that will be affected by 
+    // the onSearch() event (because every feature will have multiple widgets
+    // showing
+    virtual void onSearch(QString& text, QWidget* content) = 0;
+
   signals:
-    
+
     void focused();
 };
 
