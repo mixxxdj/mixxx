7c667c6aec32aaa90e6547753a9fb7b5b17dee5d
Jordi Ortolá Ankum <jordi665@hotmail.com>
2016-06-23 01:51:42 +0200

    Add virtual destructor to controllerpresetvisitor

--- src/controllers/controllerpresetvisitor.h
+++ src/controllers/controllerpresetvisitor.h
@@ -7,6 +7,7 @@ class KeyboardControllerPreset;
 
 class ControllerPresetVisitor {
   public:
+    virtual ~ControllerPresetVisitor() {}
     virtual void visit(KeyboardControllerPreset* preset) = 0;
     virtual void visit(MidiControllerPreset* preset) = 0;
     virtual void visit(HidControllerPreset* preset) = 0;
@@ -14,6 +15,7 @@ class ControllerPresetVisitor {
 
 class ConstControllerPresetVisitor {
   public:
+    virtual ~ConstControllerPresetVisitor() {}
     virtual void visit(const KeyboardControllerPreset* preset) = 0;
     virtual void visit(const MidiControllerPreset* preset) = 0;
     virtual void visit(const HidControllerPreset* preset) = 0;
