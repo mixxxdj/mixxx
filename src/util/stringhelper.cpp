18545979dba6d279c57b0bc9b75b4f6141523d4b
jmigual <j.marce.igual@gmail.com>
2016-08-13 10:05:18 +0200

    Removed changed function

--- src/util/stringhelper.cpp
+++ src/util/stringhelper.cpp
@@ -18,18 +18,18 @@ QChar StringHelper::getFirstCharForGrouping(const QString& text) {
     }
     c = c.toUpper();
     QString letter(c);
+    QString limmit("Z");
+    if (QString::localeAwareCompare(limmit, letter) < 0) {
+        // The letter is above z so we must not change it this is due to 
+        // Chinese letters or Finnish sorting. In Finnish the correct sorting
+        // is a-z, å, ä, ö and the user will expect these letters at the
+        // end and not like this a, ä, å, b-o, ö, p-z
+        return c;
+    }
     
     // This removes the accents of the characters
     QString s1 = letter.normalized(QString::NormalizationForm_KD);    
     if (s1.size() > 0) {
-        if (QString::localeAwareCompare(s1, letter) != 0) {
-            // The letter does not sort together with it's normalized form,
-            // it is due to Chinese letters or Finnish sorting. In Finnish the 
-            // correct sorting is a-z, å, ä, ö and the user will expect these 
-            // letters at the end and not like this a, ä, å, b-o, ö, p-z
-            return c;
-        }
-        
         c = s1.at(0).toUpper();
     }
     return c;
