7bf032476ef4076ffbf865f1c14f4d52ce060189
Jordi Ortolá Ankum <jordi665@hotmail.com>
2016-08-20 17:40:30 +0200

    Add documentation to layoutsfilehandler

--- scripts/layouts_tool/layoutsfilehandler.h
+++ scripts/layouts_tool/layoutsfilehandler.h
@@ -13,44 +13,43 @@ class LayoutsFileHandler {
     LayoutsFileHandler();
     virtual ~LayoutsFileHandler();
 
+    // Read cpp file with given path and fill given layouts with Layout objects
     void open(QString& layoutsPath, QList<Layout>& layouts);
+
+    // Generate code for given layouts and overwrite given file with that code.
     void save(QFile& f, QList<Layout>& layouts);
 
   private:
+    // Iterate through lines in cpp file and find out with string comparisons
+    // the names of the layouts that are in the file (both variable names as
+    // names as placed in comments)
+    //
+    // // Spanish (Spain)                   <-- Layout name   (LayoutNamesData[1])
+    // static const KbdKeyChar[48][2] es_ES <-- variable name (LayoutNamesData[0])
+    //
+    // NOTE: If no comment is found, layout name will be the same as the
+    //       variable name
     LayoutNamesData getLayoutNames(QFile& cppFile);
 
     // Prepends definitions to layouts.h in order for this tool to be able to compile
-    // the layouts. It adds this:
-    //
-    //  struct KbdKeyChar {
-    //      char16_t character;
-    //      bool is_dead;
-    //  };
-    //
-    //  typedef const KbdKeyChar (*KeyboardLayoutPointer)[2];
+    // the layouts without errors
     void prependDefs(QFile& cppFile);
 
-    // Appends function to layouts.h to retrieve layouts. Function could look like this:
-    //
-    // extern "C" KeyboardLayoutPointer getLayout(std::string layoutName) {
-    //    if (layoutName == "en_US") return en_US;
-    //    if (layoutName == "es_ES") return es_ES;
-    //    if (layoutName == "fr_FR") return fr_FR;
-    //    else {
-    //        return nullptr;
-    //    }
-    // }
-    //
-    // If forInternUse is false, it doesn't add 'extern "C" '
+    // Appends function to layouts.h to access its layouts. When forInternUse is
+    // true it prepends the function with "extern 'C'".
     void appendGetLayoutsFunction(QFile& cppFile,
                                   const LayoutNamesData& layoutNames,
                                   bool forInternUse);
 
-    // Remove sections surrounded by SKIP_HEAD and SKIP_TAIL comments
+    // Remove sections surrounded by kSkipCommentHead and kSkipCommentTail comments
     void removeSkipParts(QFile& file);
 
+    // Compiles cpp layouts file and points given GetLayout_t function pointer to a function
+    // that can be used to access the layouts in the file. When done using this function, close
+    // the file using dlclose(handle)
     void compileLayoutsFile(const QString& cppPath, GetLayout_t& pFunction, void*& handle);
 
+    // Generate header file and overwrite given file
     void createHeaderFile(const QString& path);
 
     // Overwrite given file with QStringList where each QString represents one line.
