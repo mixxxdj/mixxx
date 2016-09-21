f1f05a44232f8e664d4b3326bb508c6acd497cc8
jmigual <j.marce.igual@gmail.com>
2016-09-21 19:31:11 +0200

    Redo saved queries table model to allow checkboxes

--- src/dialog/savedqueries/savedqueriestablemodel.cpp
+++ src/dialog/savedqueries/savedqueriestablemodel.cpp
@@ -1,3 +1,5 @@
+#include <QSqlQuery>
+#include <QSqlRecord>
 #include <QString>
 
 #include "library/dao/savedqueriesdao.h"
@@ -7,49 +9,108 @@
 SavedQueriesTableModel::SavedQueriesTableModel(LibraryFeature* pFeature, 
                                                QObject* parent,
                                                QSqlDatabase db) 
-        : QSqlTableModel(parent, db),
-          m_pFeature(pFeature) {
-    QString filter = "libraryFeature='%1'";
-    filter = filter.arg(m_pFeature->getSettingsName());
-    
-    setTable(SAVEDQUERYTABLE);
-    setEditStrategy(QSqlTableModel::OnManualSubmit);
-    setFilter(filter);
-    
-    select();
+        : QAbstractTableModel(parent),
+          m_pFeature(pFeature),
+          m_savedDao(db) {    
+    m_cachedData = m_savedDao.getSavedQueries(m_pFeature);
 }
 
 bool SavedQueriesTableModel::isColumnInternal(int column) {
-    return column != SavedQueryColums::QUERY &&
-            column != SavedQueryColums::TITLE &&
-            column != SavedQueryColums::PINNED;
+    return column != SavedQueryColumns::QUERY &&
+            column != SavedQueryColumns::TITLE &&
+            column != SavedQueryColumns::PINNED;
 }
 
 QVariant SavedQueriesTableModel::data(const QModelIndex& index, int role) const {  
-    if (index.column() == SavedQueryColums::PINNED) {
-        if (role == Qt::DisplayRole) return QVariant();
-        else if (role == Qt::CheckStateRole) {
-            QVariant baseData = QSqlTableModel::data(index, Qt::DisplayRole);
-            return baseData.toBool() ? Qt::Checked : Qt::Unchecked;
-        }
-    }
+    if (!index.isValid()) return QVariant();
     
-    return QSqlTableModel::data(index, role);
+    const SavedSearchQuery& sQuery = m_cachedData.at(index.row());
+    switch (index.column()) {
+        case SavedQueryColumns::QUERY:
+            if (role == Qt::DisplayRole || role == Qt::EditRole) return sQuery.query;
+            break;
+            
+        case SavedQueryColumns::TITLE:
+            if (role == Qt::DisplayRole || role == Qt::EditRole) return sQuery.title;
+            break;
+        
+        case SavedQueryColumns::PINNED:
+            if (role == Qt::CheckStateRole) {
+                return sQuery.pinned ? Qt::Checked : Qt::Unchecked;
+            }
+            break;
+    }
+    return QVariant();
+}
+
+bool SavedQueriesTableModel::setData(const QModelIndex& index, const QVariant& value, int role) {
+    if (!index.isValid()) return false;
+    
+    SavedSearchQuery& sQuery = m_cachedData[index.row()];
+    switch (index.column()) {
+        case SavedQueryColumns::QUERY:
+            if (role == Qt::EditRole) {
+                sQuery.query = value.toString();
+                return true;
+            }
+            break;
+            
+        case SavedQueryColumns::TITLE:
+            if (role == Qt::EditRole) {
+                sQuery.title = value.toString();
+                return true;
+            }
+            break;
+            
+        case SavedQueryColumns::PINNED:
+            if (role == Qt::CheckStateRole) {
+                sQuery.pinned = !sQuery.pinned;
+                return true;
+            }
+    }
+    return false;
 }
 
 QVariant SavedQueriesTableModel::headerData(int section, Qt::Orientation orientation, int role) const {
     if (role != Qt::DisplayRole || orientation != Qt::Horizontal) {
-        return QSqlTableModel::headerData(section, orientation, role);
+        return QAbstractTableModel::headerData(section, orientation, role);
     }
     
     switch (section) {
-        case SavedQueryColums::QUERY:
+        case SavedQueryColumns::QUERY:
             return tr("Query");
-        case SavedQueryColums::TITLE:
+        case SavedQueryColumns::TITLE:
             return tr("Title");
-        case SavedQueryColums::PINNED:
+        case SavedQueryColumns::PINNED:
             return tr("Pinned");
     }
     return "";
 }
 
+Qt::ItemFlags SavedQueriesTableModel::flags(const QModelIndex& index) const {
+    Qt::ItemFlags flags = QAbstractTableModel::flags(index);
+    flags |= Qt::ItemIsSelectable;
+    flags |= Qt::ItemIsEnabled;
+    flags |= Qt::ItemIsEditable;
+    
+    if (index.column() == SavedQueryColumns::PINNED) {
+        flags |= Qt::ItemIsUserCheckable;
+    }
+    return flags;
+}
+
+int SavedQueriesTableModel::rowCount(const QModelIndex& parent) const {
+    if (parent.isValid()) return 0;
+    return m_cachedData.length();
+}
+
+int SavedQueriesTableModel::columnCount(const QModelIndex& parent) const {
+    if (parent.isValid()) return 0;
+    return SavedQueryColumns::NUM_COLUMNS;
+}
+
+bool SavedQueriesTableModel::submit() {
+    for (const SavedSearchQuery& sQuery : m_cachedData) {
+        m_savedDao.updateSavedQuery(sQuery);
+    }
+}
--- src/dialog/savedqueries/savedqueriestablemodel.h
+++ src/dialog/savedqueries/savedqueriestablemodel.h
@@ -1,11 +1,11 @@
 #ifndef SAVEDQUERIESTABLEMODEL_H
 #define SAVEDQUERIESTABLEMODEL_H
 
-#include <QSqlTableModel>
+#include <QAbstractTableModel>
 
 #include "library/libraryfeature.h"
 
-class SavedQueriesTableModel : public QSqlTableModel
+class SavedQueriesTableModel : public QAbstractTableModel
 {
   public:
     SavedQueriesTableModel(LibraryFeature* pFeature,
@@ -14,10 +14,22 @@ class SavedQueriesTableModel : public QSqlTableModel
     
     bool isColumnInternal(int column);
     QVariant data(const QModelIndex& index, int role) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role) override;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    
+    int rowCount(const QModelIndex& parent) const;
+    int columnCount(const QModelIndex& parent) const;
+    
+public slots:
+    
+    bool submit() override;
+    
   private:
+    QList<SavedSearchQuery> m_cachedData;
     
     LibraryFeature* m_pFeature;
+    SavedQueriesDAO m_savedDao;
 };
 
 #endif // SAVEDQUERIESTABLEMODEL_H
