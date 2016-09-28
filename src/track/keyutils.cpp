51fe5138d9ebb933f4d55b8a6cff5b89693a97f2
Sebastien Blaisot <sebastien@blaisot.org>
2016-09-28 18:42:22 +0200

    Fix windows Build: activate QT initializer lists

--- src/track/keyutils.cpp
+++ src/track/keyutils.cpp
@@ -1,3 +1,11 @@
+// Horrible Hack to workaround QT 4.8 bug with MSVC compiler. lp:1627826
+// QT 4.8 explicitely disable Initializer list, but initializer list is
+// supported with MSVC 2012 SP2 +
+// http://code.qt.io/cgit/qt/qt.git/tree/src/corelib/global/qglobal.h#n910
+// TODO(XXX): Remove after QT4 deprecation, unneeded with QT5
+#ifdef __WINDOWS__
+  #define Q_COMPILER_INITIALIZER_LISTS
+#endif
 #include <QtDebug>
 #include <QMap>
 #include <QMutexLocker>
