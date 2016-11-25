72093c423c18c089da0c266d5cd00d60affae0c8
be_ <be.0@gmx.com>
2016-11-24 21:08:09 -0600

    switch to jQuery's extend to Lodash's assign and merge
    jQuery.extend() doesn't do deep merging for objects created by invoking
    a constructor with "new", only "plain objects"

--- res/controllers/Hercules-P32-scripts.js
+++ res/controllers/Hercules-P32-scripts.js
@@ -34,117 +34,6 @@ var samplerCrossfaderAssign = true;
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 'use strict';
-/*
-Vanilla JS port of jQuery's extend function
-from https://github.com/justmoon/node-extend/blob/master/index.js
-
-The MIT License (MIT)
-
-Copyright (c) 2014 Stefan Thomas
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-var hasOwn = Object.prototype.hasOwnProperty;
-var toStr = Object.prototype.toString;
-
-var isArray = function isArray(arr) {
-    if (typeof Array.isArray === 'function') {
-        return Array.isArray(arr);
-    }
-
-    return toStr.call(arr) === '[object Array]';
-};
-
-var isPlainObject = function isPlainObject(obj) {
-    if (!obj || toStr.call(obj) !== '[object Object]') {
-        return false;
-    }
-
-    var hasOwnConstructor = hasOwn.call(obj, 'constructor');
-    var hasIsPrototypeOf = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
-    // Not own constructor property must be Object
-    if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) {
-        return false;
-    }
-
-    // Own properties are enumerated firstly, so to speed up,
-    // if last one is own, then all properties are own.
-    var key;
-    for (key in obj) { /**/ }
-
-    return typeof key === 'undefined' || hasOwn.call(obj, key);
-};
-
-script.extend = function extend() {
-    var options, name, src, copy, copyIsArray, clone;
-    var target = arguments[0];
-    var i = 1;
-    var length = arguments.length;
-    var deep = false;
-
-    // Handle a deep copy situation
-    if (typeof target === 'boolean') {
-        deep = target;
-        target = arguments[1] || {};
-        // skip the boolean and the target
-        i = 2;
-    } else if ((typeof target !== 'object' && typeof target !== 'function') || target == null) {
-        target = {};
-    }
-
-    for (; i < length; ++i) {
-        options = arguments[i];
-        // Only deal with non-null/undefined values
-        if (options != null) {
-            // Extend the base object
-            for (name in options) {
-                src = target[name];
-                copy = options[name];
-
-                // Prevent never-ending loop
-                if (target !== copy) {
-                    // Recurse if we're merging plain objects or arrays
-                    if (deep && copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
-                        if (copyIsArray) {
-                            copyIsArray = false;
-                            clone = src && isArray(src) ? src : [];
-                        } else {
-                            clone = src && isPlainObject(src) ? src : {};
-                        }
-
-                        // Never move original objects, clone them
-                        target[name] = extend(deep, clone, copy);
-
-                    // Don't bring in undefined values
-                    } else if (typeof copy !== 'undefined') {
-                        target[name] = copy;
-                    }
-                }
-            }
-        }
-    }
-
-    // Return the modified object
-    return target;
-};
 
 /* end MIT licensed code */
 
@@ -155,7 +44,7 @@ var Control = function (options) {
     if (Array.isArray(options) && typeof options[0] === 'number') {
         this.midi = options;
     } else {
-        script.extend(this, options);
+        _.assign(this, options);
     }
 
     // This cannot be in the prototype; it must be unique to each instance.
@@ -530,9 +419,18 @@ LayerContainer.prototype = {
         }, recursive);
     },
     applyLayer: function (newLayer, operation) {
+        this.forEachControl(function (control) {
+            control.disconnect();
+        });
         // Recursively merge newLayer with this LayerContainer
-        script.extend(true, this, newLayer);
-        this.reconnectControls(operation);
+        _.merge(this, newLayer);
+        this.forEachControl(function (control) {
+            if (typeof operation === 'function') {
+                operation.call(this, control);
+            }
+            control.connect();
+            control.trigger();
+        });
     }
 };
 
