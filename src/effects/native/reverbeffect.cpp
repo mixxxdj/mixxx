35bf933b50e05d233493920b9f710798e71c95d9
be_ <be.0@gmx.com>
2016-12-05 20:59:43 -0600

    change reverb "Bandwidth" short name to "BW"

--- src/effects/native/reverbeffect.cpp
+++ src/effects/native/reverbeffect.cpp
@@ -26,7 +26,7 @@ EffectManifest ReverbEffect::getManifest() {
     EffectManifestParameter* time = manifest.addParameter();
     time->setId("bandwidth");
     time->setName(QObject::tr("Bandwidth"));
-    time->setName(QObject::tr("Band"));
+    time->setShortName(QObject::tr("BW"));
     time->setDescription(QObject::tr("Higher bandwidth values cause more "
             "bright (high-frequency) tones to be included"));
     time->setControlHint(EffectManifestParameter::CONTROL_KNOB_LINEAR);
