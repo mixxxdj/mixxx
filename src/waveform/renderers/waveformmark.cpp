f3557afed715b9597d852e78f589a790ea75b5f2
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2016-12-15 19:20:08 +0530

    reduce complication, add construct, destruct

--- src/waveform/renderers/waveformmark.cpp
+++ src/waveform/renderers/waveformmark.cpp
@@ -5,21 +5,22 @@
 
 #include "waveformmark.h"
 
-WaveformMark::WaveformMark(int hotCue)
+WaveformMark::WaveformMark( const QString& group,
+                            const QDomNode& node,
+                            const SkinContext& context,
+                            const WaveformSignalColors& signalColors,
+                            int hotCue)
     : m_iHotCue(hotCue) {
-}
-
-void WaveformMark::reset(int hotCue) {
-    WaveformMark(hotCue).swap(*this);
-}
-
-void WaveformMark::setup(const QString& group, const QDomNode& node,
-                         const SkinContext& context,
-                         const WaveformSignalColors& signalColors) {
     QString item = context.selectString(node, "Control");
     if (!item.isEmpty()) {
         m_pPointCos = std::make_unique<ControlProxy>(group, item);
     }
-
     m_properties = WaveformMarkProperties(node, context, signalColors);
+}
+
+WaveformMark::WaveformMark(int hotCue)
+    : m_iHotCue(hotCue){
+}
+
+WaveformMark::~WaveformMark(){
 }
