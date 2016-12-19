2a618b23903cfe6223879192b2a65bb01da5223d
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2016-12-19 15:13:06 +0530

    Inline the explicit Constructor WaveformMark

--- src/waveform/renderers/waveformmark.h
+++ src/waveform/renderers/waveformmark.h
@@ -24,7 +24,8 @@ class WaveformMark {
                                const WaveformSignalColors& signalColors,
                                int hotCue = kDefaultHotCue);
 
-    explicit WaveformMark(int hotCue = kDefaultHotCue);
+    explicit WaveformMark(int hotCue = kDefaultHotCue)
+        : m_iHotCue(hotCue){}
 
     // Disable copying
     WaveformMark(const WaveformMark&) = delete;
