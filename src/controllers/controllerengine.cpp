64b6f026fbcf2d6d5984d6a98592876fe6efcefe
ronso0 <medontknow@web.de>
2016-12-20 21:08:13 +0100

    Update controllerengine.cpp

--- src/controllers/controllerengine.cpp
+++ src/controllers/controllerengine.cpp
@@ -1377,12 +1377,12 @@ void ControllerEngine::brake(int deck, bool activate, double factor, double rate
         }
 
         // setup the filter with 'factor' affecting beta (default for alpha)
-        alphaBrake = 1.0/512;
+        double alphaBrake = 1.0/512;
         // avoid decimals for fine adjusting
         if (factor>1) {
             factor = ((factor-1)/10)+1;
         }
-        betaBrake = ((1.0/512)/1024)*factor; // default: (1.0/512)/1024
+        double betaBrake = ((1.0/512)/1024)*factor; // default: (1.0/512)/1024
         AlphaBetaFilter* filter = m_scratchFilters[deck];
         if (filter != nullptr) {
             filter->init(kAlphaBetaDt, rate, alphaBrake, betaBrake);
