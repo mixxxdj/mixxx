60372aad104cb3b3c5fd127188a40e581aab95ea
jmigual <j.marce.igual@gmail.com>
2017-02-15 21:10:39 +0100

    Fix recording feature pointers

--- src/library/features/recording/recordingfeature.h
+++ src/library/features/recording/recordingfeature.h
@@ -55,8 +55,8 @@ class RecordingFeature : public LibraryFeature {
     RecordingManager* m_pRecordingManager;
     
     QPointer<DlgRecording> m_pRecordingView;
-    QPointer<BrowseTableModel> m_pBrowseModel;
-    QPointer<ProxyTrackModel> m_pProxyModel;
+    parented_ptr<BrowseTableModel> m_pBrowseModel;
+    parented_ptr<ProxyTrackModel> m_pProxyModel;
 };
 
 #endif
