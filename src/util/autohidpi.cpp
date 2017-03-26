5618c86b99b093c1352403398c670ca44dca9def
Daniel Schürmann <daschuer@mixxx.org>
2017-03-26 22:17:59 +0200

    Added autoHiDpi for windows

--- src/util/autohidpi.cpp
+++ src/util/autohidpi.cpp
@@ -1,11 +1,36 @@
 
 #include <QProcess>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QDebug>
 
 #include "util/autohidpi.h"
 
 #ifdef __WINDOWS__
 
 double AutoHiDpi::getScaleFactor() {
+    // Windows 10 has a percentage setting in preferences, which
+    // finally changes the screen DPI setting
+    // 96 dpi = 100%
+    // 120 dpi = 125%
+    // 144 dpi = 150%
+    // 192 dpi = 200%
+    // 240 dpi = 250%
+    // 288 dpi = 300%
+    int dpiX = QApplication::desktop()->logicalDpiX();
+    qDebug() << "AutoHiDpi::getScaleFactor()" << dpiX;
+    // This follows the strategy used in Gnome/Unity
+    // that the widget size is doubled once the font size
+    // reaches the double vale threshold
+    if (dpiX < 96) {
+        return -1;
+    } else if (dpiX < 192) {
+        return 1;
+    } else if (dpiX < 288) {
+        return 2;
+    } else if (dpiX == 288) {
+        return 3;
+    }
     return -1;
 }
 
