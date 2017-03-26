bd3467c95ba114d1a686481277a71b0918a56444
ronso0 <medontknow@web.de>
2017-03-26 13:02:16 +0200

    fix default pitch slider range > 8%

--- src/preferences/dialog/dlgprefcontrols.cpp
+++ src/preferences/dialog/dlgprefcontrols.cpp
@@ -99,6 +99,11 @@ DlgPrefControls::DlgPrefControls(QWidget * parent, MixxxMainWindow * mixxx,
     connect(ComboBoxRateRange, SIGNAL(activated(int)),
             this, SLOT(slotSetRateRange(int)));
 
+    // If not present in the config, set the default value
+    if (!m_pConfig->exists(ConfigKey("[Controls]","RateRangePercent")) &&
+        !m_pConfig->exists(ConfigKey("[Controls]","RateRange")))
+        m_pConfig->set(ConfigKey("[Controls]","RateRangePercent"),ConfigValue(8));
+
     // Set default range as stored in config file
     if (m_pConfig->getValueString(ConfigKey("[Controls]", "RateRangePercent")).length() == 0) {
         // Fall back to old [Controls]RateRange
