82e48f4e5e4c3555fd5a578fd7cfd0761a6fdd4b
Uwe Klotz <uwe_klotz@web.de>
2017-05-28 17:33:57 +0200

    Use QLatin1String to avoid null check

--- src/util/logger.cpp
+++ src/util/logger.cpp
@@ -8,15 +8,13 @@ namespace {
 const char* const kLogPreambleSuffix = " -";
 const std::size_t kLogPreambleSuffixLen = std::strlen(kLogPreambleSuffix);
 
-inline QByteArray preambleChars(const char* logContext) {
+inline QByteArray preambleChars(const QLatin1String& logContext) {
     QByteArray preamble;
-    if (logContext != nullptr) {
-        std::size_t logContextLen = std::strlen(logContext);
-        if (logContextLen > 0) {
-            preamble.reserve(logContextLen + kLogPreambleSuffixLen);
-            preamble.append(logContext, logContextLen);
-            preamble.append(kLogPreambleSuffix, kLogPreambleSuffixLen);
-        }
+    std::size_t logContextLen = std::strlen(logContext.latin1());
+    if (logContextLen > 0) {
+        preamble.reserve(logContextLen + kLogPreambleSuffixLen);
+        preamble.append(logContext.latin1(), logContextLen);
+        preamble.append(kLogPreambleSuffix, kLogPreambleSuffixLen);
     }
     return preamble;
 }
@@ -26,11 +24,11 @@ inline QByteArray preambleChars(const char* logContext) {
 namespace mixxx {
 
 Logger::Logger(const char* logContext)
-    : m_preambleChars(preambleChars(logContext)) {
+    : m_preambleChars(preambleChars(QLatin1String(logContext))) {
 }
 
 Logger::Logger(const QLatin1String& logContext)
-    : m_preambleChars(preambleChars(logContext.latin1())) {
+    : m_preambleChars(preambleChars(logContext)) {
 }
 
 }  // namespace mixxx
