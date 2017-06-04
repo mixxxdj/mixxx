fbfb3a0c187ef2e37b0dbfa235a853f12bf8f934
Uwe Klotz <uwe_klotz@web.de>
2017-06-04 15:24:15 +0200

    Reuse temporary Mixxx DB in test class

--- src/test/queryutiltest.cpp
+++ src/test/queryutiltest.cpp
@@ -1,46 +1,27 @@
 #include <gtest/gtest.h>
-#include <QtDebug>
-#include <QDir>
-#include <QTemporaryFile>
+
+#include "test/librarytest.h"
 
 #include "library/queryutil.h"
 #include "util/db/sqllikewildcardescaper.h"
 
-class QueryUtilTest : public testing::Test {};
+class QueryUtilTest : public LibraryTest {
+  protected:
+    QueryUtilTest()
+        : m_fieldEscaper(collection()->database()) {
+    }
+
+    FieldEscaper m_fieldEscaper;
+};
 
 TEST_F(QueryUtilTest, FieldEscaperEscapesQuotes) {
-    QTemporaryFile databaseFile("mixxxdb.sqlite");
-    ASSERT_TRUE(databaseFile.open());
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setHostName("localhost");
-    db.setUserName("mixxx");
-    db.setPassword("mixxx");
-    qDebug() << "Temp file is" << databaseFile.fileName();
-    db.setDatabaseName(databaseFile.fileName());
-    ASSERT_TRUE(db.open());
-    FieldEscaper f(db);
-
-
     EXPECT_STREQ(qPrintable(QString("'foobar'")),
-                 qPrintable(f.escapeString("foobar")));
+                 qPrintable(m_fieldEscaper.escapeString("foobar")));
     EXPECT_STREQ(qPrintable(QString("'foobar''s'")),
-                 qPrintable(f.escapeString("foobar's")));
+                 qPrintable(m_fieldEscaper.escapeString("foobar's")));
 }
 
 TEST_F(QueryUtilTest, FieldEscaperEscapesForLike) {
-    QTemporaryFile databaseFile("mixxxdb.sqlite");
-    ASSERT_TRUE(databaseFile.open());
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setHostName("localhost");
-    db.setUserName("mixxx");
-    db.setPassword("mixxx");
-    qDebug() << "Temp file is" << databaseFile.fileName();
-    db.setDatabaseName(databaseFile.fileName());
-    ASSERT_TRUE(db.open());
-    FieldEscaper f(db);
-
     EXPECT_STREQ(qPrintable(QString("xx44xx4%yy4_yy")),
                  qPrintable(SqlLikeWildcardEscaper::apply("xx4xx%yy_yy", '4')));
 }
