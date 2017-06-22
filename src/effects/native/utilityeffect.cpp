91018136a9e9f9bfa38a9bdcfc7bcbd649f96bdf
Aris Stathakis <father.aris@gmail.com>
2017-06-22 06:11:07 +0300

    add ramp functionality to utility effect

--- src/effects/native/utilityeffect.cpp
+++ src/effects/native/utilityeffect.cpp
@@ -45,9 +45,6 @@ EffectManifest UtilityEffect::getManifest() {
     switchA->setMinimum(0);
     switchA->setMaximum(1);
 
-
-
-
     return manifest;
 }
 
@@ -62,6 +59,67 @@ UtilityEffect::~UtilityEffect() {
     //qDebug() << debugString() << "destroyed";
 }
 
+void UtilityEffect::rampGainCalculation(UtilityGroupState* pState,
+		                                   bool mute_state,
+										   CSAMPLE ramp_step,
+										   const EffectProcessor::EnableState enableState ) {
+
+	//decode enable state/mute state transitions for ramping volume properly.
+
+	// unmute->mute state transition
+	if ((pState->previous_mute_state)  && (!mute_state) ) {
+		//rampgain ramps down with rampstep till zero.
+		pState->ramp_state = UtilityGroupState::RAMPDOWN;
+		//set previous mute state to mute
+		pState->previous_mute_state = true;
+	}
+
+	// mute->unmute state transition
+	if ((!pState->previous_mute_state)  && (mute_state) ) {
+		//rampgain ramps up from zero to 1.0
+		pState->ramp_state = UtilityGroupState::RAMPUP;
+		//set previous mute state to unmute
+		pState->previous_mute_state = false;
+	}
+
+	// disable->enable state transition
+	if (pState->previous_enable_state == EffectProcessor::DISABLED && enableState == EffectProcessor::ENABLED) {
+		//ramp gain ramps up from zero to 1.0
+		pState->ramp_state = UtilityGroupState::RAMPUP;
+		//set previous enable state to enable
+		pState->previous_enable_state = EffectProcessor::ENABLED;
+	}
+
+	// enable->disable state transition
+	if ((pState->previous_enable_state == EffectProcessor::ENABLED) && (enableState == EffectProcessor::DISABLED)) {
+		//ramp gain ramps down with rampstep till zero.
+		pState->ramp_state = UtilityGroupState::RAMPDOWN;
+		//set previous enable state to disable
+		pState->previous_enable_state = EffectProcessor::DISABLED;
+	}
+
+	// increment or decrement rampgain depending on rampstate.
+	if(pState->ramp_state == UtilityGroupState::RAMPDOWN) {
+		pState->ramp_gain-=ramp_step;
+	}
+
+	if(pState->ramp_state == UtilityGroupState::RAMPUP) {
+		pState->ramp_gain+=ramp_step;
+	}
+
+	//add ramp gain guard to stop ramping.
+	if (pState->ramp_gain > 1.0) {
+		pState->ramp_gain =1.0;
+		pState->ramp_state = UtilityGroupState::RAMPNO;
+	}
+
+	if (pState->ramp_gain < 0.0) {
+		pState->ramp_gain =0.0;
+		pState->ramp_state = UtilityGroupState::RAMPNO;
+	}
+
+}
+
 void UtilityEffect::processChannel(const ChannelHandle& handle,
                                       UtilityGroupState* pState,
                                       const CSAMPLE* pInput, CSAMPLE* pOutput,
@@ -71,27 +129,37 @@ void UtilityEffect::processChannel(const ChannelHandle& handle,
                                       const GroupFeatureState& groupFeatures) {
     Q_UNUSED(handle);
     Q_UNUSED(groupFeatures);
-    Q_UNUSED(sampleRate); // we are normalized to 1
-    Q_UNUSED(enableState); // no need to ramp, it is just a Utility ;-)
-
 
 
     CSAMPLE effect_gain = m_pKnobA ?
     		m_pKnobA->value() : 0.0;
+
+    bool current_mute_state = m_pSwitchA->toBool();
+
+    //if effect is disabled and ramping complete return,
+    if ((enableState == EffectProcessor::DISABLED)&&(pState->ramp_state == UtilityGroupState::RAMPNO)) {
+        return;
+    }
+
+	//increase and decrease ramp factor from 0.0 to 1.0 and back with this step.
+	CSAMPLE ramp_step = (pState->ramp_time_in_seconds * numSamples)/(2.0*sampleRate);
+
+	UtilityEffect::rampGainCalculation(pState, current_mute_state, ramp_step, enableState );
 
 
     const int kChannels = 2;
     for (unsigned int i = 0; i < numSamples; i += kChannels) {
 
         // mute switch check
-        if (!m_pSwitchA->toBool()) {
-        	effect_gain = 0.0;
+        if (!current_mute_state) {
+        	//set previous mute state to mute
+        	pState->ramp_state = UtilityGroupState::RAMPDOWN;
         }
 
-        pState->hold_l = pInput[i] * effect_gain;
-        pState->hold_r = pInput[i+1] * effect_gain;
+        pState->previous_mute_state = current_mute_state;
 
-        pOutput[i] = pState->hold_l;
-        pOutput[i+1] = pState->hold_r;
+        pOutput[i] = pInput[i] * effect_gain * pState->ramp_gain;
+        pOutput[i+1] = pInput[i+1] * effect_gain * pState->ramp_gain;
+
     }
 }
--- src/effects/native/utilityeffect.h
+++ src/effects/native/utilityeffect.h
@@ -13,10 +13,25 @@
 struct UtilityGroupState {
     // Default accumulator to 1 so we immediately pick an input value.
     UtilityGroupState()
-            : hold_l(0),
+            : ramp_state(RAMPNO),
+			  previous_enable_state(EffectProcessor::DISABLED),
+			  previous_mute_state(false),
+			  ramp_time_in_seconds(0.8),
+			  ramp_factor(0.0),
+			  ramp_gain(1.0),
+              hold_l(0),
               hold_r(0) {
     }
-    CSAMPLE hold_l, hold_r;
+    enum RampState {
+        RAMPNO = 0x00,
+		RAMPUP = 0x01,
+		RAMPDOWN = 0x02
+    }ramp_state;
+    const CSAMPLE ramp_time_in_seconds;
+    CSAMPLE ramp_factor;
+    CSAMPLE ramp_gain;
+    EffectProcessor::EnableState previous_enable_state;
+    bool previous_mute_state;
 
 };
 
@@ -36,6 +51,11 @@ class UtilityEffect : public PerChannelEffectProcessor<UtilityGroupState> {
                         const unsigned int sampleRate,
                         const EffectProcessor::EnableState enableState,
                         const GroupFeatureState& groupFeatureState);
+
+    void rampGainCalculation(UtilityGroupState* pState,
+                        bool mute_state,
+						 CSAMPLE ramp_step,
+						 const EffectProcessor::EnableState enableState);
 
   private:
     QString debugString() const {
