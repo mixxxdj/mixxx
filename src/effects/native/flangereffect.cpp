edd89d0e37a335ec7cfe8de1d95c10467ca82bc7
be_ <be.0@gmx.com>
2017-07-05 15:27:00 -0500

    Flanger: clear buffers when disabling effect

--- src/effects/native/flangereffect.cpp
+++ src/effects/native/flangereffect.cpp
@@ -83,8 +83,6 @@ void FlangerEffect::processChannel(const ChannelHandle& handle,
                                    const EffectProcessor::EnableState enableState,
                                    const GroupFeatureState& groupFeatures) {
     Q_UNUSED(handle);
-    Q_UNUSED(enableState);
-    Q_UNUSED(groupFeatures);
 
     // TODO: remove assumption of stereo signal
     const int kChannels = 2;
@@ -133,5 +131,9 @@ void FlangerEffect::processChannel(const ChannelHandle& handle,
 
         pOutput[i] = pInput[i] + lfoDepth * delayedSampleLeft;
         pOutput[i+1] = pInput[i+1] + lfoDepth * delayedSampleRight;
+
+    if (enableState == EffectProcessor::DISABLING) {
+        SampleUtil::clear(delayLeft, numSamples);
+        SampleUtil::clear(delayRight, numSamples);
     }
 }
