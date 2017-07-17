4d4ce336d06becac7a4d209fcb7a10bc31583dc9
be_ <be.0@gmx.com>
2017-07-17 10:39:12 -0500

    add WEffectParameterKnobComposed widget
    This is needed to show effect parameter tooltips for knobs. Previously
    they were only shown when hovering the cursor over effect parameter
    names, which is not very easy to discover.

---
+++ src/widget/weffectparameterknobcomposed.cpp
@@ -0,0 +1,48 @@
+#include "widget/effectwidgetutils.h"
+#include "widget/weffectparameterknobcomposed.h"
+
+void WEffectParameterKnobComposed::setup(const QDomNode& node,
+                                         const SkinContext& context) {
+    WKnobComposed::setup(node, context);
+
+    EffectRackPointer pRack = EffectWidgetUtils::getEffectRackFromNode(
+            node, context, m_pEffectsManager);
+    EffectChainSlotPointer pChainSlot = EffectWidgetUtils::getEffectChainSlotFromNode(
+            node, context, pRack);
+    EffectSlotPointer pEffectSlot = EffectWidgetUtils::getEffectSlotFromNode(
+            node, context, pChainSlot);
+    EffectParameterSlotBasePointer pParameterSlot =
+            EffectWidgetUtils::getParameterSlotFromNode(
+                    node, context, pEffectSlot);
+    if (pParameterSlot) {
+        setEffectParameterSlot(pParameterSlot);
+    } else {
+        SKIN_WARNING(node, context)
+                << "EffectParameterKnobComposed node could not attach to effect parameter";
+    }
+};
+
+void WEffectParameterKnobComposed::parameterUpdated() {
+    if (m_pEffectParameterSlot) {
+        if (!m_pEffectParameterSlot->shortName().isEmpty()) {
+            //: %1 = effect parameter name; %2 = effect parameter description
+            setBaseTooltip(tr("%1: %2").arg(
+                              m_pEffectParameterSlot->name(),
+                              m_pEffectParameterSlot->description()));
+        } else {
+            setBaseTooltip(m_pEffectParameterSlot->description());
+        }
+    } else {
+        setBaseTooltip(tr("No effect loaded."));
+    }
+}
+
+void WEffectParameterKnobComposed::setEffectParameterSlot(
+        EffectParameterSlotBasePointer pParameterSlot) {
+    m_pEffectParameterSlot = pParameterSlot;
+    if (m_pEffectParameterSlot) {
+        connect(m_pEffectParameterSlot.data(), SIGNAL(updated()),
+                this, SLOT(parameterUpdated()));
+    }
+    parameterUpdated();
+}
