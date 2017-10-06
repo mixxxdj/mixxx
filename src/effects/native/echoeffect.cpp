5be86af498f269a540c97a8cda41effe8cac96a0
Daniel Schürmann <daschuer@mixxx.org>
2017-10-07 01:21:17 +0200

    Echo: use the whole delay buffer, ramp feedback

--- src/effects/native/echoeffect.cpp
+++ src/effects/native/echoeffect.cpp
@@ -6,6 +6,7 @@
 #include "util/math.h"
 
 #define INCREMENT_RING(index, increment, length) index = (index + increment) % length
+#define DECREMENT_RING(index, decrement, length) index = (index + length - decrement) % length
 
 constexpr int EchoGroupState::kMaxDelaySeconds;
 constexpr int EchoGroupState::kChannelCount;
@@ -132,7 +133,7 @@ void EchoEffect::processChannel(const ChannelHandle& handle, EchoGroupState* pGr
     double feedback_amount = m_pFeedbackParameter->value();
     double pingpong_frac = m_pPingPongParameter->value();
 
-    int delay_samples;
+    int delay_frames;
     if (groupFeatures.has_beat_length_sec) {
         // period is a number of beats
         if (m_pQuantizeParameter->toBool()) {
@@ -143,57 +144,50 @@ void EchoEffect::processChannel(const ChannelHandle& handle, EchoGroupState* pGr
         } else if (period < 1/8.0) {
             period = 1/8.0;
         }
-        delay_samples = period * groupFeatures.beat_length_sec
-                * sampleRate * EchoGroupState::kChannelCount;
+        delay_frames = period * groupFeatures.beat_length_sec * sampleRate;
     } else {
         // period is a number of seconds
         period = std::max(period, 1/8.0);
-        delay_samples = period * sampleRate * EchoGroupState::kChannelCount;
+        delay_frames = period * sampleRate;
     }
-    VERIFY_OR_DEBUG_ASSERT(delay_samples > 0) {
-        delay_samples = 1;
+    VERIFY_OR_DEBUG_ASSERT(delay_frames > 0) {
+        delay_frames = 1;
     }
+
+    int delay_samples = delay_frames * gs.kChannelCount;
     VERIFY_OR_DEBUG_ASSERT(delay_samples <= gs.delay_buf.size()) {
         delay_samples = gs.delay_buf.size();
     }
 
-    if (period < gs.prev_period) {
-        // If the delay time has shrunk, we may need to wrap the write position.
-        gs.write_position = gs.write_position % delay_samples;
-    } else if (period > gs.prev_period) {
-        // If the delay time has grown, we need to zero out the new portion
-        // of the buffer we are using.
-        SampleUtil::applyGain(gs.delay_buf.data(gs.prev_delay_samples), 0,
-                              gs.delay_buf.size() - gs.prev_delay_samples);
-    }
-
     int read_position = gs.write_position;
+    DECREMENT_RING(read_position, delay_samples, gs.delay_buf.size());
 
     // Feedback the delay buffer and then add the new input.
     const CSAMPLE_GAIN send_delta = (send_amount - gs.prev_send) /
             (numSamples / EchoGroupState::kChannelCount);
     const CSAMPLE_GAIN send_start = send_amount + send_delta;
-    for (unsigned int i = 0; i < numSamples; i += EchoGroupState::kChannelCount) {
-        CSAMPLE_GAIN send_ramped = send_start;
-        if (send_delta > 0.0) {
-            send_ramped += send_delta * i / EchoGroupState::kChannelCount;
-        }
-        gs.delay_buf[gs.write_position] *= feedback_amount;
-        gs.delay_buf[gs.write_position + 1] *= feedback_amount;
-        gs.delay_buf[gs.write_position] += pInput[i] * send_ramped;
-        gs.delay_buf[gs.write_position + 1] += pInput[i + 1] * send_ramped;
-        // Actual delays distort and saturate, so clamp the buffer here.
-        gs.delay_buf[gs.write_position] =
-                SampleUtil::clampSample(gs.delay_buf[gs.write_position]);
-        gs.delay_buf[gs.write_position + 1] =
-                SampleUtil::clampSample(gs.delay_buf[gs.write_position + 1]);
-        INCREMENT_RING(gs.write_position, EchoGroupState::kChannelCount, delay_samples);
-    }
 
-    // Pingpong the output.  If the pingpong value is zero, all of the
-    // math below should result in a simple copy of delay buf to pOutput.
+    const CSAMPLE_GAIN feedback_delta = (feedback_amount - gs.prev_feedback) /
+            (numSamples / EchoGroupState::kChannelCount);
+    const CSAMPLE_GAIN feedback_start = feedback_amount + feedback_delta;
+
     for (unsigned int i = 0; i < numSamples; i += EchoGroupState::kChannelCount) {
-        if (gs.ping_pong_left) {
+        CSAMPLE_GAIN send_ramped = send_start
+                + send_delta * i / EchoGroupState::kChannelCount;
+        CSAMPLE_GAIN feedback_ramped = feedback_start
+                + feedback_delta * i / EchoGroupState::kChannelCount;
+
+        // Actual delays distort and saturate, so clamp the buffer here.
+        gs.delay_buf[gs.write_position] = SampleUtil::clampSample(
+                pInput[i] * send_ramped +
+                gs.delay_buf[read_position] * feedback_ramped);
+        gs.delay_buf[gs.write_position + 1] = SampleUtil::clampSample(
+                pInput[i + 1] * send_ramped +
+                gs.delay_buf[read_position + 1] * feedback_ramped);
+
+        // Pingpong the output.  If the pingpong value is zero, all of the
+        // math below should result in a simple copy of delay buf to pOutput.
+        if (gs.ping_pong < delay_samples / 2) {
             // Left sample plus a fraction of the right sample, normalized
             // by 1 + fraction.
             pOutput[i] = pInput[i] +
@@ -215,10 +209,13 @@ void EchoEffect::processChannel(const ChannelHandle& handle, EchoGroupState* pGr
                     (1 + pingpong_frac)) / 2.0;
         }
 
-        INCREMENT_RING(read_position, EchoGroupState::kChannelCount, delay_samples);
-        // If the buffer has looped around, flip-flop the ping-pong.
-        if (read_position == 0) {
-            gs.ping_pong_left = !gs.ping_pong_left;
+        INCREMENT_RING(gs.write_position, EchoGroupState::kChannelCount,
+                gs.delay_buf.size());
+        INCREMENT_RING(read_position, EchoGroupState::kChannelCount,
+                gs.delay_buf.size());
+
+        if (++gs.ping_pong >= delay_samples) {
+            gs.ping_pong = 0;
         }
     }
 
@@ -226,7 +223,7 @@ void EchoEffect::processChannel(const ChannelHandle& handle, EchoGroupState* pGr
         gs.delay_buf.clear();
     }
 
-    gs.prev_period = period;
     gs.prev_send = send_amount;
+    gs.prev_feedback = feedback_amount;
     gs.prev_delay_samples = delay_samples;
 }
