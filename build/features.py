e87c3318c035a7420b3be3f61f53500d5a99546a
Uwe Klotz <uwe_klotz@web.de>
2017-11-14 23:49:36 +0100

    Fix inconsistent whitespaces

--- build/features.py
+++ build/features.py
@@ -12,7 +12,7 @@ class OpenGLES(Feature):
 
     def enabled(self, build):
         build.flags['opengles'] = util.get_flags(build.env, 'opengles', 0)
-	return int(build.flags['opengles'])
+        return int(build.flags['opengles'])
 
     def add_options(self, build, vars):
         vars.Add('opengles', 'Set to 1 to enable OpenGL-ES >= 2.0 support [Experimental]', 0)
@@ -20,8 +20,8 @@ class OpenGLES(Feature):
     def configure(self, build, conf):
         if not self.enabled(build):
             return
-	if build.flags['opengles']:
-	    build.env.Append(CPPDEFINES='__OPENGLES__')
+        if build.flags['opengles']:
+            build.env.Append(CPPDEFINES='__OPENGLES__')
 
     def sources(self, build):
         return []
