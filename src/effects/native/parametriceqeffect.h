6ef6218d0a99d151e08bffbcc02d3f32a0c5a847
Daniel Schürmann <daschuer@mixxx.org>
2017-11-17 23:13:21 +0100

    Added a comment about the use case of this effect

--- src/effects/native/parametriceqeffect.h
+++ src/effects/native/parametriceqeffect.h
@@ -16,6 +16,12 @@
 #include "util/types.h"
 #include "util/memory.h"
 
+// The ParamtricEQEffect models the mid bands from a SSL Black EQ (242)
+// with a gentle parameter range, as requested here:
+// https://www.mixxx.org/forums/viewtopic.php?f=3&t=9239&p=33312&hilit=SSL#p33312
+// The main use case is to tweak the room or recording sound, which is hard to acheve
+// with the sharp and wide curves of the mixing EQs.
+
 class ParametricEQEffectGroupState final {
   public:
     ParametricEQEffectGroupState();
