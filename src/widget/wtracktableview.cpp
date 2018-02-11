c8928667682d8d1712d12a4fb5f8e6c730aa8b9b
Uwe Klotz <uklotz@mixxx.org>
2018-02-11 20:32:17 +0100

    Show information about deferred/delayed export of metadata

--- src/widget/wtracktableview.cpp
+++ src/widget/wtracktableview.cpp
@@ -1496,6 +1496,7 @@ void WTrackTableView::slotExportTrackMetadataIntoFileTags() {
             // corresponding track object have been dropped. Otherwise
             // writing to files that are still used for playback might
             // cause crashes or at least audible glitches!
+            mixxx::DlgTrackMetadataExport::showMessageBoxOncePerSession();
             pTrack->markForMetadataExport();
         }
     }
