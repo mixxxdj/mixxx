ddefd23b69ce67099f13238957dcaeb67e0bea6f
Silvio Ankermann <silvio@booq.org>
2018-02-20 00:21:37 +0100

    Roland DJ-202: Add Mixer section

--- res/controllers/Roland_DJ-202-scripts.js
+++ res/controllers/Roland_DJ-202-scripts.js
@@ -12,12 +12,52 @@ DJ202.shutdown = function () {
 DJ202.Deck = function (deckNumbers, channel) {
     components.Deck.call(this, deckNumbers);
 
-
+    this.shiftButton = function (channel, control, value, status, group) {
+        if (value === 127) {
+            this.shift();
+        } else {
+            this.unshift();
+        }
+    };
 
     // ============================= TRANSPORT ==================================
-    this.play = new components.PlayButton([0x90 + channel, 0x00]);
+    this.play = new components.PlayButton([0x90 + channel, 0x00]); // LED doesn't stay on
     this.cue = new components.CueButton([0x90 + channel, 0x01]);
-    this.sync = new components.SyncButton([0x90 + channel, 0x02]);
+    this.sync = new components.SyncButton([0x90 + channel, 0x02]); // doesn't work properly
+
+    // =============================== MIXER ====================================
+    this.pregain = new components.Pot({
+            midi: [0xB0 + channel, 0x16],
+            inKey: 'pregain',
+    });
+    
+    this.eqKnob = [];
+    for (var k = 1; k <= 3; k++) {
+        this.eqKnob[k] = new components.Pot({
+            midi: [0xB0 + channel, 0x20 - k],
+            group: '[EqualizerRack1_' + this.currentDeck + '_Effect1]',
+            inKey: 'parameter' + k,
+        });
+    }
+    
+    this.filter = new components.Pot({
+        midi: [0xB0 + channel, 0x1A],
+        group: '[QuickEffectRack1_' + this.currentDeck + ']',
+        inKey: 'super1',
+    });
+
+    this.pfl = new components.Button({
+        midi: [0x90 + channel, 0x1B],
+        type: components.Button.prototype.types.toggle,
+        inKey: 'pfl',
+        outKey: 'pfl',
+        // missing: shift -> TAP
+    });
+
+    this.volume = new components.Pot({
+        midi: [0xB0 + channel, 0x1C],
+        inKey: 'volume',
+    });
 
     this.reconnectComponents(function (component) {
         if (component.group === undefined) {
