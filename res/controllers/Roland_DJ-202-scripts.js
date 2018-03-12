c2db15a2c125004e619c97dac9e13363625f11b2
Silvio Ankermann <silvio@booq.org>
2018-03-12 22:17:19 +0100

    Roland DJ-202: Code cleanup

--- res/controllers/Roland_DJ-202-scripts.js
+++ res/controllers/Roland_DJ-202-scripts.js
@@ -28,8 +28,9 @@ DJ202.crossfader = new components.Pot({
     inKey: 'crossfader',
 });
 
-DJ202.Deck = function (deckNumbers, channel) {
+DJ202.Deck = function (deckNumbers, offset) {
     components.Deck.call(this, deckNumbers);
+    channel = offset+1;
 
     this.shiftButton = function (channel, control, value, status, group) {
         if (value === 127) {
@@ -40,7 +41,7 @@ DJ202.Deck = function (deckNumbers, channel) {
     };
 
     this.loadTrack = new components.Button({
-        midi: [0x9F, 0x02 + channel],
+        midi: [0x9F, 0x02 + offset],
         unshift: function () {
             this.inKey = 'LoadSelectedTrack';
         },
@@ -50,45 +51,45 @@ DJ202.Deck = function (deckNumbers, channel) {
     });
 
     this.keylock = new components.Button({
-        midi: [0x90 + channel, 0x0D],
+        midi: [0x90 + offset, 0x0D],
         type: components.Button.prototype.types.toggle,
         inKey: 'keylock',
         outKey: 'keylock',
     });
     
     this.rate = new components.Pot({
-        midi: [0xB0 + channel, 0x09],
+        midi: [0xB0 + offset, 0x09],
         inKey: 'rate',
     });
 
     // ============================= TRANSPORT ==================================
-    this.play = new components.PlayButton([0x90 + channel, 0x00]); // LED doesn't stay on
-    this.cue = new components.CueButton([0x90 + channel, 0x01]);
-    this.sync = new components.SyncButton([0x90 + channel, 0x02]); // doesn't work properly
+    this.play = new components.PlayButton([0x90 + offset, 0x00]); // LED doesn't stay on
+    this.cue = new components.CueButton([0x90 + offset, 0x01]);
+    this.sync = new components.SyncButton([0x90 + offset, 0x02]); // doesn't work properly
 
     // =============================== MIXER ====================================
     this.pregain = new components.Pot({
-            midi: [0xB0 + channel, 0x16],
+            midi: [0xB0 + offset, 0x16],
             inKey: 'pregain',
     });
     
     this.eqKnob = [];
     for (var k = 1; k <= 3; k++) {
         this.eqKnob[k] = new components.Pot({
-            midi: [0xB0 + channel, 0x20 - k],
+            midi: [0xB0 + offset, 0x20 - k],
             group: '[EqualizerRack1_' + this.currentDeck + '_Effect1]',
             inKey: 'parameter' + k,
         });
     }
     
     this.filter = new components.Pot({
-        midi: [0xB0 + channel, 0x1A],
+        midi: [0xB0 + offset, 0x1A],
         group: '[QuickEffectRack1_' + this.currentDeck + ']',
         inKey: 'super1',
     });
 
     this.pfl = new components.Button({
-        midi: [0x90 + channel, 0x1B],
+        midi: [0x90 + offset, 0x1B],
         type: components.Button.prototype.types.toggle,
         inKey: 'pfl',
         outKey: 'pfl',
@@ -96,7 +97,7 @@ DJ202.Deck = function (deckNumbers, channel) {
     });
 
     this.volume = new components.Pot({
-        midi: [0xB0 + channel, 0x1C],
+        midi: [0xB0 + offset, 0x1C],
         inKey: 'volume',
     });
 
