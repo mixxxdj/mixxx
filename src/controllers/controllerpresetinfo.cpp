mixxxdj/mixxx

commit effbb061a0b0f55c5ece9e5c1df2f5665cca7817

Simplify PresetInfoEnumerator.
Most of the methods were unused.

src/controllers/controllerpresetinfoenumerator.cpp
@@ -4,134 +4,51 @@
 * @date Sat Jul 18 2015
 * @brief Enumerate list of available controller mapping presets
 */
-#include <QDirIterator>
-
-#include "controllers/controllerpresetinfo.h"
 #include "controllers/controllerpresetinfoenumerator.h"
+
+#include <QDirIterator>
 
 #include "controllers/defs_controllers.h"
 
 PresetInfoEnumerator::PresetInfoEnumerator(const QStringList& searchPaths)
         : m_controllerDirPaths(searchPaths) {
-    // Static list of supported default extensions, sorted by popularity
-    m_fileExtensions.append(QString(MIDI_PRESET_EXTENSION));
-    m_fileExtensions.append(QString(HID_PRESET_EXTENSION));
-    m_fileExtensions.append(QString(BULK_PRESET_EXTENSION));
-
     loadSupportedPresets();
 }
 
-PresetInfoEnumerator::~PresetInfoEnumerator() {
-    for (QMap<QString, ControllerPresetFileHandler*>::iterator it =
-                 m_presetFileHandlersByExtension.begin();
-         it != m_presetFileHandlersByExtension.end(); ++it) {
-        delete it.value();
-        it = m_presetFileHandlersByExtension.erase(it);
+QList<PresetInfo> PresetInfoEnumerator::getPresetsByExtension(const QString& extension) {
+    if (extension == MIDI_PRESET_EXTENSION) {
+        return m_midiPresets;
+    } else if (extension == HID_PRESET_EXTENSION) {
+        return m_hidPresets;
+    } else if (extension == BULK_PRESET_EXTENSION) {
+        return m_bulkPresets;
     }
-}
 
-bool PresetInfoEnumerator::isValidExtension(const QString extension) {
-    if (m_presetsByExtension.contains(extension))
-        return true;
-    return false;
-}
-
-bool PresetInfoEnumerator::hasPresetInfo(const QString extension, const QString name) {
-    // Check if preset info matching extension and preset name can be found
-    if (!isValidExtension(extension))
-        return false;
-
-    for (QMap<QString, QMap<QString, PresetInfo> >::const_iterator it =
-                 m_presetsByExtension.begin();
-         it != m_presetsByExtension.end(); ++it) {
-        for (QMap<QString, PresetInfo>::const_iterator it2 = it.value().begin();
-             it2 != it.value().end(); ++it2) {
-            if (name == it2.value().getName()) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool PresetInfoEnumerator::hasPresetInfo(const QString path) {
-    foreach (QString extension, m_presetsByExtension.keys()) {
-        QMap<QString, PresetInfo> presets = m_presetsByExtension[extension];
-        if (presets.contains(path))
-            return true;
-    }
-    return false;
-}
-
-PresetInfo PresetInfoEnumerator::getPresetInfo(const QString path) {
-    // Lookup and return controller script preset info by script path
-    // Return NULL if path is not found.
-    foreach (QString extension, m_presetsByExtension.keys()) {
-        QMap<QString, PresetInfo> presets = m_presetsByExtension[extension];
-        if (presets.contains(path))
-            return presets[path];
-    }
-    return PresetInfo();
-}
-
-QList<PresetInfo> PresetInfoEnumerator::getPresets(const QString extension) {
-    // Return list of PresetInfo items matching extension
-    // Returns empty list if no matching extension presets can be found
-    QList<PresetInfo> presets;
-    if (m_presetsByExtension.contains(extension)) {
-        presets = m_presetsByExtension[extension].values();
-        return m_presetsByExtension[extension].values();
-    }
     qDebug() << "Extension not registered to presetinfo" << extension;
-    return presets;
-}
-
-void PresetInfoEnumerator::addExtension(const QString extension) {
-    if (m_presetsByExtension.contains(extension))
-        return;
-    QMap<QString,PresetInfo> presets;
-    m_presetsByExtension[extension] = presets;
+    return QList<PresetInfo>();
 }
 
 void PresetInfoEnumerator::loadSupportedPresets() {
-    foreach (QString dirPath, m_controllerDirPaths) {
+    for (const QString& dirPath : m_controllerDirPaths) {
         QDirIterator it(dirPath);
         while (it.hasNext()) {
             it.next();
             const QString path = it.filePath();
-            foreach (QString extension, m_fileExtensions) {
-                if (!path.endsWith(extension))
-                    continue;
-                if (!m_presetsByExtension.contains(extension)) {
-                    addExtension(extension);
-                }
-                m_presetsByExtension[extension][path] = PresetInfo(path);
+
+            if (path.endsWith(MIDI_PRESET_EXTENSION, Qt::CaseInsensitive)) {
+                m_midiPresets.append(PresetInfo(path));
+            } else if (path.endsWith(HID_PRESET_EXTENSION, Qt::CaseInsensitive)) {
+                m_hidPresets.append(PresetInfo(path));
+            } else if (path.endsWith(BULK_PRESET_EXTENSION, Qt::CaseInsensitive)) {
+                m_bulkPresets.append(PresetInfo(path));
             }
         }
     }
 
-    foreach (QString extension, m_presetsByExtension.keys()) {
-        QMap<QString,PresetInfo> presets = m_presetsByExtension[extension];
-        qDebug() << "Extension" << extension << "total" << presets.keys().length() << "presets";
-    }
-}
-
-void PresetInfoEnumerator::updatePresets(const QString extension) {
-    QMap<QString,PresetInfo> presets;
-
-    if (m_presetsByExtension.contains(extension))
-        m_presetsByExtension.remove(extension);
-
-    foreach (QString dirPath, m_controllerDirPaths) {
-        QDirIterator it(dirPath);
-        while (it.hasNext()) {
-            it.next();
-            const QString path = it.filePath();
-            if (!path.endsWith(extension))
-                continue;
-            presets[path] = PresetInfo(path);
-        }
-    }
-
-    m_presetsByExtension[extension] = presets;
+    qDebug() << "Extension" << MIDI_PRESET_EXTENSION << "total"
+             << m_midiPresets.length() << "presets";
+    qDebug() << "Extension" << HID_PRESET_EXTENSION << "total"
+             << m_hidPresets.length() << "presets";
+    qDebug() << "Extension" << BULK_PRESET_EXTENSION << "total"
+             << m_bulkPresets.length() << "presets";
 }

src/controllers/dlgprefcontroller.cpp
@@ -253,10 +253,11 @@ void DlgPrefController::enumeratePresets() {
     m_ui.comboBoxPreset->setInsertPolicy(QComboBox::InsertAlphabetically);
     // Ask the controller manager for a list of applicable presets
     PresetInfoEnumerator* pie =  m_pControllerManager->getMainThreadPresetEnumerator();
-    QList<PresetInfo> presets = pie->getPresets(m_pController->presetExtension());
+    const QList<PresetInfo> presets = pie->getPresetsByExtension(
+        m_pController->presetExtension());
 
     PresetInfo match;
-    foreach (const PresetInfo& preset, presets) {
+    for (const PresetInfo& preset : presets) {
         m_ui.comboBoxPreset->addItem(nameForPreset(preset), preset.getPath());
         if (m_pController->matchPreset(preset)) {
             match = preset;

src/controllers/controllerpresetinfoenumerator.h
@@ -7,44 +7,29 @@
 #ifndef CONTROLLERPRESETINFOENUMERATOR_H
 #define CONTROLLERPRESETINFOENUMERATOR_H
 
-#include <QMap>
 #include <QList>
 #include <QString>
+#include <QStringList>
 
-#include "controllers/controllerpreset.h"
+#include "controllers/controllerpresetinfo.h"
 
 class PresetInfoEnumerator {
   public:
     PresetInfoEnumerator(const QStringList& searchPaths);
-    virtual ~PresetInfoEnumerator();
-
-    bool isValidExtension(const QString extension);
-
-    bool hasPresetInfo(const QString extension, const QString name);
-    bool hasPresetInfo(const QString path);
-
-    PresetInfo getPresetInfo(const QString path);
 
     // Return cached list of presets for this extension
-    QList<PresetInfo> getPresets(const QString extension);
-
-    // Updates presets matching given extension
-    void updatePresets(const QString extension);
+    QList<PresetInfo> getPresetsByExtension(const QString& extension);
 
   protected:
-    void addExtension(QString extension);
     void loadSupportedPresets();
 
   private:
-    QList<QString> m_fileExtensions;
-
     // List of paths for controller presets
     QList<QString> m_controllerDirPaths;
 
-    // Cached presets by extension. Map format is:
-    // [extension,[preset_path,preset]]
-    QMap<QString, QMap<QString, PresetInfo> > m_presetsByExtension;
-    QMap<QString, ControllerPresetFileHandler*> m_presetFileHandlersByExtension;
+    QList<PresetInfo> m_hidPresets;
+    QList<PresetInfo> m_midiPresets;
+    QList<PresetInfo> m_bulkPresets;
 };
 
 #endif

src/test/controller_preset_validation_test.cpp
@@ -189,7 +189,7 @@ bool lintPresetInfo(const PresetInfo& preset) {
 
 TEST_F(ControllerPresetValidationTest, MidiPresetsValid) {
     foreach (const PresetInfo& preset,
-             m_pEnumerator->getPresets(MIDI_PRESET_EXTENSION)) {
+             m_pEnumerator->getPresetsByExtension(MIDI_PRESET_EXTENSION)) {
         qDebug() << "Validating" << preset.getPath();
         EXPECT_TRUE(preset.isValid());
         EXPECT_TRUE(lintPresetInfo(preset));
@@ -199,7 +199,7 @@ TEST_F(ControllerPresetValidationTest, MidiPresetsValid) {
 
 TEST_F(ControllerPresetValidationTest, HidPresetsValid) {
     foreach (const PresetInfo& preset,
-             m_pEnumerator->getPresets(HID_PRESET_EXTENSION)) {
+             m_pEnumerator->getPresetsByExtension(HID_PRESET_EXTENSION)) {
         qDebug() << "Validating" << preset.getPath();
         EXPECT_TRUE(preset.isValid());
         EXPECT_TRUE(lintPresetInfo(preset));
@@ -209,7 +209,7 @@ TEST_F(ControllerPresetValidationTest, HidPresetsValid) {
 
 TEST_F(ControllerPresetValidationTest, BulkPresetsValid) {
     foreach (const PresetInfo& preset,
-             m_pEnumerator->getPresets(BULK_PRESET_EXTENSION)) {
+             m_pEnumerator->getPresetsByExtension(BULK_PRESET_EXTENSION)) {
         qDebug() << "Validating" << preset.getPath();
         EXPECT_TRUE(preset.isValid());
         EXPECT_TRUE(lintPresetInfo(preset));

commit 52c9a68427f05b4845e994cb83ed96d1a5005f30

sort controller mappings in user folder first then system folder
This was how it was done in 2.0. It is not self-explanatory and
kinda clunky, but it's better than the current situation where
they are all sorted alphabetically and users get confused why
they see two identically named mappings next to each other.

This behavior was broken by commit effbb06 because it removed the
QMap of mappings indexed by a QString for the file path.

src/controllers/controllerpresetinfo.cpp
@@ -29,7 +29,9 @@ PresetInfo::PresetInfo(const QString& preset_path)
     // info.forums      Link to mixxx forum discussion for the preset
     // info.wiki        Link to mixxx wiki for the preset
     // info.devices.product List of device matches, specific to device type
-    m_path = QFileInfo(preset_path).absoluteFilePath();
+    QFileInfo fileInfo(preset_path);
+    m_path = fileInfo.absoluteFilePath();
+    m_dirPath = fileInfo.dir().absolutePath();
     m_name = "";
     m_author = "";
     m_description = "";
@@ -50,7 +52,11 @@ PresetInfo::PresetInfo(const QString& preset_path)
     m_valid = true;
 
     QDomElement dom_name = info.firstChildElement("name");
-    if (!dom_name.isNull()) m_name = dom_name.text();
+    if (!dom_name.isNull()) {
+        m_name = dom_name.text();
+    } else {
+        m_name = fileInfo.baseName();
+    }
 
     QDomElement dom_author = info.firstChildElement("author");
     if (!dom_author.isNull()) m_author = dom_author.text();

src/controllers/controllerpresetinfo.h
@@ -48,14 +48,8 @@ class PresetInfo {
     }
 
     inline const QString getPath() const { return m_path; }
-
-    inline const QString getName() const {
-        if (m_name.length() != 0) {
-            return m_name;
-        } else {
-            return QFileInfo(m_path).baseName();
-        }
-    }
+    inline const QString getDirPath() const { return m_dirPath; }
+    inline const QString getName() const { return m_name; }
     inline const QString getDescription() const { return m_description; }
     inline const QString getForumLink() const { return m_forumlink; }
     inline const QString getWikiLink() const { return m_wikilink; }
@@ -69,6 +63,7 @@ class PresetInfo {
 
     bool m_valid;
     QString m_path;
+    QString m_dirPath;
     QString m_name;
     QString m_author;
     QString m_description;

src/controllers/controllerpresetinfoenumerator.cpp
@@ -12,7 +12,20 @@
 
 namespace {
 bool presetInfoNameComparator(const PresetInfo &a, const PresetInfo &b) {
-    return a.getName() < b.getName();
+    if (a.getDirPath() == b.getDirPath()) {
+        // FIXME: Mixxx copies every loaded mapping into the user mapping folder
+        // with a different file name. This is confusing, especially when developing
+        // a mapping and working on it in the user mapping folder. Sorting
+        // by file path here is a quick hack to keep the identically named mappings
+        // in a consistent order.
+        if (a.getName() == b.getName()) {
+            return a.getPath() < b.getPath();
+        } else {
+            return a.getName() < b.getName();
+        }
+    } else {
+        return a.getDirPath() < b.getDirPath();
+    }
 }
 }
 

src/controllers/dlgprefcontroller.cpp
@@ -229,15 +229,6 @@ void DlgPrefController::slotDirty() {
     m_bDirty = true;
 }
 
-const QString DlgPrefController::presetDisplayName(const PresetInfo& info) {
-    if (QFileInfo(info.getPath()).absoluteDir() == userPresetsPath(m_pConfig)) {
-        //: Shown in combobox to select controller mappings to distinguish user-installed mappings from mappings included in Mixxx
-        return info.getName() + " (" + tr("user") + ")";
-    } else {
-        return info.getName();
-    }
-}
-
 void DlgPrefController::enumeratePresets() {
     m_ui.comboBoxPreset->clear();
 
@@ -264,7 +255,7 @@ void DlgPrefController::enumeratePresets() {
 
     PresetInfo match;
     for (const PresetInfo& preset : presets) {
-        m_ui.comboBoxPreset->addItem(presetDisplayName(preset), preset.getPath());
+        m_ui.comboBoxPreset->addItem(preset.getName(), preset.getPath());
         if (m_pController->matchPreset(preset)) {
             match = preset;
         }
@@ -272,7 +263,7 @@ void DlgPrefController::enumeratePresets() {
 
     // Jump to matching device in list if it was found.
     if (match.isValid()) {
-        int index = m_ui.comboBoxPreset->findText(presetDisplayName(match));
+        int index = m_ui.comboBoxPreset->findText(match.getName());
         if (index != -1) {
             m_ui.comboBoxPreset->setCurrentIndex(index);
         }

src/controllers/dlgprefcontroller.h
@@ -86,7 +86,6 @@ class DlgPrefController : public DlgPreferencePage {
     void savePreset(QString path);
     void initTableView(QTableView* pTable);
 
-    const QString presetDisplayName(const PresetInfo& info);
     // Reload the mappings in the dropdown dialog
     void enumeratePresets();
 