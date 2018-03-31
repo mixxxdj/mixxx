b38038f2e2889d62f8938ea10e6e5af4f5a9427a
Daniel Schürmann <daschuer@mixxx.org>
2018-03-31 23:27:20 +0200

    Don't add skin: prefix to an empty path

--- src/skin/skincontext.h
+++ src/skin/skincontext.h
@@ -32,7 +32,8 @@ class SkinContext {
 
     // Gets a path relative to the skin path.
     QString makeSkinPath(const QString& relativePath) const {
-        if (relativePath.startsWith("/") || relativePath.contains(":")) {
+        if (relativePath.isEmpty() || relativePath.startsWith("/")
+                || relativePath.contains(":")) {
             // This is already an absolut path start with the root folder "/"
             // a windows drive letter e.g. "C:" or a qt search path prefix
             return relativePath;
