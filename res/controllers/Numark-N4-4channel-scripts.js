ddae75e67de763883de92d6f8b367d648126ab23
Swiftb0y <nikolaus.einhauser@web.de>
2018-04-21 21:00:39 +0200

    working commit, some bugfixing still required

--- res/controllers/Numark-N4-4channel-scripts.js
+++ res/controllers/Numark-N4-4channel-scripts.js
@@ -13,49 +13,56 @@ NumarkN4.warnAfterPosition = 0.8; // percentage after when the triangular LEDs s
 
 NumarkN4.blinkInterval=1000; //blinkInterval for the triangular Leds over the channels in milliseconds.
 
+NumarkN4.encoderResolution=0.05; // 1/encoderResolution = number of steps going from 0% to 100%
+
 // possible ranges (0.0..3.0 where 0.06=6%)
 NumarkN4.rateRanges = [0.06,// one semitone
                        0.24,// for maximum freedom
                      ];
+NumarkN4.loopSizes=[0.03125, 0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16, 32, 64];
 NumarkN4.QueryStatusMessage=[0xF0,0x00,0x01,0x3F,0x7F,0x47,0x60,0x00,0x01,0x54,0x01,0x00,0x00,0x00,0x00,0xF7];
-NumarkN4.ShutoffSequesnce=[0xF0,0x00,0x01,0x3F,0x7F,0x47,0xB0,0x39,0x00,0x01,0xF7];
+NumarkN4.ShutoffSequence=[0xF0,0x00,0x01,0x3F,0x7F,0x47,0xB0,0x39,0x00,0x01,0xF7];
 
-NumarkN4.highResGainMSB={
-  "[Channel1]":0,
-  "[Channel2]":0,
-  "[Channel3]":0,
-  "[Channel4]":0,
-};
 NumarkN4.vinylTouched = [false,false,false,false,];
 
-NumarkN4.init = function (id) {
+components.Encoder.prototype.input = function (channel, control, value, status, group) {
+  this.inSetParameter(
+    this.inGetParameter()+(
+      (value===0x01)?
+      NumarkN4.encoderResolution:
+      -NumarkN4.encoderResolution
+    )
+  );
+};
 
-  components.Component.prototype.send = function (value) {
-    // This Override is supposed to make integration automatic assignment of elements easier.
-    // Right now it just allows specifying the input and output bytes (even though the input bytes dont do anything right now.)
-    if (this.midi === undefined || this.midi[0] === undefined || this.midi[1] === undefined) {
-        return;
-    }
-    if (this.midi[2]===undefined){//check if output channel/type not explicitly defined
-      this.midi[2]=this.midi[0];
-    }
-    if (this.midi[3]===undefined){//check if output control not explicitly defined
-      this.midi[3]=this.midi[1];
-    }
-    midi.sendShortMsg(this.midi[2], this.midi[3], value);
-    if (this.sendShifted) {
-        if (this.shiftChannel) {
-            midi.sendShortMsg(this.midi[2] + this.shiftOffset, this.midi[3], value);
-        } else if (this.shiftControl) {
-            midi.sendShortMsg(this.midi[2], this.midi[3] + this.shiftOffset, value);
-        }
-    }
-  };
+components.Component.prototype.send = function (value) {
+  // This Override is supposed to make integration automatic assignment of elements easier.
+  // Right now it just allows specifying the input and output bytes (even though the input bytes dont do anything right now.)
+  if (this.midi === undefined || this.midi[0] === undefined || this.midi[1] === undefined) {
+      return;
+  }
+  if (this.midi[2]===undefined){//check if output channel/type not explicitly defined
+    this.midi[2]=this.midi[0];
+  }
+  if (this.midi[3]===undefined){//check if output control not explicitly defined
+    this.midi[3]=this.midi[1];
+  }
+  midi.sendShortMsg(this.midi[2], this.midi[3], value);
+  if (this.sendShifted) {
+      if (this.shiftChannel) {
+          midi.sendShortMsg(this.midi[2] + this.shiftOffset, this.midi[3], value);
+      } else if (this.shiftControl) {
+          midi.sendShortMsg(this.midi[2], this.midi[3] + this.shiftOffset, value);
+      }
+  }
+};
+
+NumarkN4.init = function (id) {
   NumarkN4.Decks=[];
   for (var iterator=1;iterator<=4;iterator++){
     print("instancing Deck"+iterator);
     NumarkN4.Decks[iterator] = new NumarkN4.Deck(iterator);
-    print("finished instancing Deck"+channel);
+    print("finished instancing Deck"+iterator);
 
   }
 
@@ -73,28 +80,30 @@ NumarkN4.topContainer = function (channel) {
 
   this.btnEffect1 = new components.Button({
     midi: [0x90+channel,0x13,0xB0+channel,0x0B],
-    key: "loop_in",
     shift: function () {
       this.group="[EffectRack1_EffectUnit1]";
+      this.type=components.Button.prototype.types.toggle;
       this.inKey="group_[Channel"+channel+"]_enable";
       this.outKey="group_[Channel"+channel+"]_enable";
     },
     unshift: function () {
       this.group=theContainer.group;
+      this.type=components.Button.prototype.types.push;
       this.inKey="loop_in";
       this.outKey="loop_in";
     },
   });
   this.btnEffect2 = new components.Button({
     midi: [0x90+channel,0x14,0xB0+channel,0x0C],
-    key: "loop_out",
     shift: function () {
       this.group="[EffectRack1_EffectUnit2]";
+      this.type=components.Button.prototype.types.toggle;
       this.inKey="group_[Channel"+channel+"]_enable";
       this.outKey="group_[Channel"+channel+"]_enable";
     },
     unshift: function () {
       this.group=theContainer.group;
+      this.type=components.Button.prototype.types.push;
       this.inKey="loop_out";
       this.outKey="loop_out";
     },
@@ -102,26 +111,28 @@ NumarkN4.topContainer = function (channel) {
   print("instancing btnSample3");
   this.btnSample3 = new components.Button({
     midi: [0x90+channel,0x15,0xB0+channel,0x0D],
-    key: "reloop_toggle",
     shift: function () {
+      this.type=components.Button.prototype.types.toggle;
       this.inKey="slip_enabled";
       this.outKey="slip_enabled";
     },
     unshift: function () {
-      this.inKey="reloop_toggle";
-      this.outKey="reloop_toggle";
+      this.type=components.Button.prototype.types.push;
+      this.inKey="beatloop_activate";
+      this.outKey="beatloop_activate";
     },
   });
   this.btnSample4 = new components.Button({
     midi: [0x90+channel,0x16,0xB0+channel,0x0E],
-    key: "beatloop_activate",
     shift: function () {
+      this.type=components.Button.prototype.types.toggle;
       this.inKey="repeat";
       this.outKey="repeat";
     },
     unshift: function () {
-      this.inKey="beatloop_activate";
-      this.outKey="beatloop_activate";
+      this.type=components.Button.prototype.types.push;
+      this.inKey="reloop_toggle";
+      this.outKey="loop_enabled";
     },
   });
   // custom Hotcue Buttons
@@ -129,22 +140,21 @@ NumarkN4.topContainer = function (channel) {
 
   for (var counter=0;counter<=3;counter++){
     this.hotcueButtons[counter] = new components.Button({
-      midi: [0x90+channel,0x27+counter,0xB0+channel,0x18],
-      key: "hotcue_"+(counter+1)+"_activate",
+      midi: [0x90+channel,0x27+counter,0xB0+channel,0x18+counter],
+      number: counter,
       shift: function () {
-        this.inKey="hotcue_"+(counter+5)+"_activate";
-        this.outKey="hotcue_"+(counter+5)+"_activate";
+        this.inKey="hotcue_"+(this.number+5)+"_activate";
+        this.outKey="hotcue_"+(this.number+5)+"_enabled";
       },
       unshift: function () {
-        this.inKey="hotcue_"+(counter+1)+"_activate";
-        this.outKey="hotcue_"+(counter+1)+"_activate";
+        this.inKey="hotcue_"+(this.number+1)+"_activate";
+        this.outKey="hotcue_"+(this.number+1)+"_enabled";
       },
     });
   }
-  this.encFxparam1 = new components.Encoder({
+  this.encFxParam1 = new components.Encoder({
     midi:[0xB0+channel,0x57],
     group: "[EffectRack1_EffectUnit1]",
-    inkey: "super1",
     shift: function () {
       this.inKey="mix";
     },
@@ -152,10 +162,9 @@ NumarkN4.topContainer = function (channel) {
       this.inKey="super1";
     },
   });
-  this.encFxparam2 = new components.Encoder({
+  this.encFxParam2 = new components.Encoder({
     midi:[0xB0+channel,0x58],
     group: "[EffectRack1_EffectUnit2]",
-    inkey: "super1",
     shift: function () {
       this.inKey="mix";
     },
@@ -164,32 +173,42 @@ NumarkN4.topContainer = function (channel) {
     },
   });
   this.encSample3 = new components.Encoder({
-    midi: [0xB0+channel,0x59],
-    inKey: "beatjump",
-    input: function (channel, control, value, status, group) {
-      if (value-0x01){
-        this.inSetValue(engine.getValue(this.group,"beatjump_size")); //positive direction
-      } else {
-        this.inSetValue(-engine.getValue(this.group,"beatjump_size")); //negative direction
-      }
-    },
+    midi: [0xB0+channel,0x5A],
+    currentLoopSizeIndex: 7,
     shift: function () {
-      this.inKey="beatjump_size";
+      this.group=theContainer.group;
+      this.inKey="beatloop_size";
+      this.input = function (channel, control, value, status, group) {
+        var valueChange=(value===1)?1:-1;
+        if ((this.currentLoopSizeIndex+valueChange)>=0&&(this.currentLoopSizeIndex+valueChange)<NumarkN4.loopSizes.length) {
+          this.currentLoopSizeIndex+=valueChange;
+          this.inSetValue(NumarkN4.loopSizes[this.currentLoopSizeIndex]);
+        }
+      };
     },
     unshift: function () {
-      this.inKey="beatjump";
-    },
-  });
-  this.encSample4 = new components.Encoder({
-    midi: [0xB0+channel,0x5A],
-    inKey: "beatloop_size",
-    shift: function () {
+      this.input=components.Encoder.prototype.input;
       this.group="[QuickEffectRack1_"+theContainer.group+"]";
       this.inKey="super1";
     },
+  });
+  this.encSample4 = new components.Encoder({
+    midi: [0xB0+channel,0x59],
+    currentJumpSizeIndex: 7, //custom property
+    shift: function () {
+      this.inKey="beatjump_size";
+      this.input = function (channel, control, value, status, group) {
+        var valueChange=(value===1)?1:-1;
+        if ((this.currentJumpSizeIndex+valueChange)>=0&&(this.currentJumpSizeIndex+valueChange)<NumarkN4.loopSizes.length) {
+          this.currentJumpSizeIndex+=valueChange;
+          this.inSetValue(NumarkN4.loopSizes[this.currentJumpSizeIndex]);
+        }
+      };
+    },
     unshift: function () {
-      this.group=theContainer.group;
-      this.inKey="beatloop_size";
+      this.input = function (channel, control, value, status, group) {
+        script.triggerControl(this.group,(value===1)?"beatjump_forward":"beatjump_backward");
+      };
     },
   });
   print("finished instancing topContainer"+channel);
@@ -222,30 +241,36 @@ NumarkN4.MixerTemplate = function () {
   this.navigationEncoderTick = new components.Encoder({
     midi: [0xB0, 0x44],
     group: "[Library]",
-    inKey: "MoveVertical",
+    stepsize: 1,
+    shift: function () {
+      this.inKey="MoveFocus";
+    },
+    unshift: function () {
+      this.inKey="MoveVertical";
+    },
     input: function (midiChannel,control,value,status,group) {
-      this.inSetValue(value===0x01?1:-1); // value "rescaling"; possibly inefficent.
+      this.inSetValue(value===0x01?this.stepsize:-this.stepsize); // value "rescaling"; possibly inefficent.
     },
   });
   this.navigationEncoderButton = new components.Button({
-    midi:[0x90,0x08],
-    group: "[Master]",
-    inKey: "maximize_library",
-    type: components.Button.prototype.types.toggle,
     shift: function () {
-      this.input=function (channel, control, value, status, group) {
-        theMixer.navigationEncoderTick.inKey=(value?"MoveFocus":"MoveVertical");
-      };
+      this.type=components.Button.prototype.types.push;
       this.group="[Library]";
       this.inKey="GoToItem";
+    //   this.input=function (midiChannel,control,value,status,group) {
+    //     if (value==0x7F) {
+    //     theMixer.navigationEncoderTick.stepsize = (value==0x7F?NumarkN4.LibraryStepSizeSpeedup:1);
+    //   } else {
+    //     this.inSetValue(this.isPress(midiChannel, control, value, status));
+    //   }
     },
     unshift: function () {
-      this.input=components.Button.prototype.input;
+      this.type=components.Button.prototype.types.toggle;
+      // this.input=components.Button.prototype.input;
       this.group="[Master]";
       this.inKey="maximize_library";
     },
   });
-
 };
 
 NumarkN4.MixerTemplate.prototype = new components.ComponentContainer();
@@ -258,10 +283,23 @@ NumarkN4.Deck = function (channel) {
   this.isSearching=false;
   var theDeck = this;
   this.topContainer = new NumarkN4.topContainer(channel);
-  // this.topContainer.reconnectComponents(function (component) {
-  //   component.group = theContainer.group;
-  // });
-  print("reconnectedComponents");
+  this.topContainer.reconnectComponents(function (component) {
+    if (component.group === undefined) {
+      component.group = this.group;
+    }
+  });
+  this.eqKnobs = [];
+  for (var i = 1; i <= 3; i++) {
+      this.eqKnobs[i] = new components.Pot({
+          midi: [0xB0, 0x29 + i + 5*(channel-1)],
+          group: '[EqualizerRack1_'+theDeck.group+'_Effect1]',
+          inKey: 'parameter' + i,
+      });
+  }
+  this.gainKnob = new components.Pot({
+    midi: [0xB0, 0x2C + 5*(channel-1)],
+    inKey: "pregain",
+  })
   // REVIEW: this.blinkTimer=engine.beginTimer(NumarkN4.blinkInterval,"NumarkN4.Deck"+channel+".manageChannelIndicator");
   //timer is more efficent is this case than a callback because it would be called too often.
   // REVIEW: engine.makeConnection(this.group,"track_loaded",function (value) {midi.sendShortMsg(0xB0,0x1D+channel,value?0x7F:0x00)});
@@ -270,6 +308,7 @@ NumarkN4.Deck = function (channel) {
     type: components.Button.prototype.types.powerWindow,
     state: false, //custom property
     inToggle: function () {
+      print("topContainer.group: "+theDeck.topContainer.group);
       this.state=!this.state;
       if (this.state) {
         theDeck.shift();
@@ -279,6 +318,11 @@ NumarkN4.Deck = function (channel) {
         NumarkN4.Mixer.unshift();
       }
       this.output(this.state);
+      theDeck.topContainer.reconnectComponents(function (component) {
+        if (component.group === undefined) {
+          component.group = this.group;
+        }
+      });
     },
   });
 
@@ -336,11 +380,11 @@ NumarkN4.Deck = function (channel) {
   });
   this.loadButton = new components.Button({
     midi:[0x90+channel,0x06],
-    key: "LoadSelectedTrack",
+    inKey: "LoadSelectedTrack",
     shift: function () {this.inKey="eject";},
     unshift: function () {this.inKey="LoadSelectedTrack";},
-    input: function () {
-      this.inSetParameter(true);
+    input: function (channelmidi,control,value) {
+      this.inSetParameter(this.inValueScale(value));
     },
   });
   this.playButton = new components.PlayButton({
@@ -362,17 +406,21 @@ NumarkN4.Deck = function (channel) {
       engine.scratchDisable(channel);
     }
   };
-  this.searchButton = function (channelmidi, control, value, status, group) {
-    theDeck.isSearching=!theDeck.isSearching;
-    midi.sendShortMsg(0xB0+channel,0x12,this.isSearching?0x7F:0x00);
-  };
+  this.searchButton = new components.Button({
+    midi: [0x90+channel,0x00,0xB0+channel,0x12],
+    input: function (channelmidi, control, value, status, group) {
+      theDeck.isSearching=!theDeck.isSearching;
+      this.output(theDeck.isSearching?0x7F:0x00)
+    },
+  });
 
   this.jogWheelTurn = function (channelmidi, control, value, status, group) {
+    value=(value<0x40?value:value-0x7F);
     if (theDeck.isSearching) {value*=NumarkN4.searchAmplification;}
     if (engine.isScratching(channel)) {
-      engine.scratchTick(channel,value<0x40?value:value-0x7F);
+      engine.scratchTick(channel,value);
     } else {
-      engine.setValue(theDeck.group,"jog",value<0x40?value:value-0x7F);
+      engine.setValue(theDeck.group,"jog",value);
     }
   };
   this.manageChannelIndicator = function () {
@@ -412,12 +460,20 @@ NumarkN4.Deck = function (channel) {
 
   this.keylockButton = new components.Button({
     midi: [0x90+channel,0x1B,0xB0+channel,0x10],
-    key: "keylock",
     type: components.Button.prototype.types.toggle,
+    key: "quantize",
+    // shift: function () {
+    //   this.inKey="quantize";
+    //   this.outKey="quantize";
+    // },
+    // unshift: function () {
+    //   this.inKey="keylock";
+    //   this.outKey="keylock";
+    // }
   });
   this.bpmSlider = new components.Pot({
     midi: [0xB0+channel,0x01,0xB0+channel,0x37], //only specifing input MSB
-    key: "rate",
+    inKey: "rate",
     invert: true,
     group: theDeck.group,
   });
@@ -499,7 +555,7 @@ NumarkN4.shutdown = function () {
 //      [x] FX Auto/Tap = hotcue
 //      [x] FX1 Param = FX1 Mix
 //      [x] FX2 Param = FX2 Mix
-//      [x] FX3 Param = beatjump_sizex
+//      [x] FX3 Param = beatjump_size
 //      [x] FX Beats multi = loop length box
 
 //MIXER: (MSB)
@@ -511,7 +567,7 @@ NumarkN4.shutdown = function () {
 //      [ ] Btn:
 //        [ ] short press: [Library] GoToItem
 //        [ ] long press: set enc.inKey="MoveFocus"
-//      [ ] Enc: MoveVertical / ^^
+//      [ ] Enc: MoveVertical / ^^C
 //  [x] Crossfader
 //  [x] InputSwitches (MixxxControl: "mute")
 //  [x] crossfaderCurve BUG: wierd behavior.
@@ -520,9 +576,9 @@ NumarkN4.shutdown = function () {
 //  Channel:
 //    [x] LOAD
 //    [x] Orientation
-//    [ ] gainKnob
-//    [ ] highknob
-//    [ ] midknob
-//    [ ] lowknob
+//    [x] gainKnob
+//    [x] highknob
+//    [x] midknob
+//    [x] lowknob
 //    [x] pfl
 //    [x] Volume
