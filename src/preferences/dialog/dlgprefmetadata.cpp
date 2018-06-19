97daf77fcf0c7e6ce0b21603f920955ad32937fc
David Hernandez <david.hm.1994@gmail.com>
2018-06-19 19:48:36 +0200

    Pre changing prefmetadata class

--- src/preferences/dialog/dlgprefmetadata.cpp
+++ src/preferences/dialog/dlgprefmetadata.cpp
@@ -2,21 +2,42 @@
 #include <QCheckBox>
 #include "broadcast/listenersfinder.h"
 #include "preferences/dialog/dlgprefmetadata.h"
+#include "preferences/dialog/ui_dlgfilelistenerbox.h"
 
 DlgPrefMetadata::DlgPrefMetadata(QWidget *pParent,UserSettingsPointer pSettings)
-        : DlgPreferencePage(pParent) {
+        : DlgPreferencePage(pParent),
+          fileListenerBox(new Ui::fileListenerBox){
     setupUi(this);
+    setupTableWidget(pSettings);
+}
+
+void DlgPrefMetadata::setupTableWidget(UserSettingsPointer pSettings) {
     QLinkedList<ScrobblingServicePtr> listeners =
             ListenersFinder::instance(pSettings).getAllServices();
+    setTableParameters(listeners.size()+5);
+    fillTableWithServices(listeners);
+}
+
+DlgPrefMetadata::~DlgPrefMetadata() {
+    delete fileListenerBox;
+}
+
+void DlgPrefMetadata::setTableParameters(int numberOfListeners) {
     listenersTableWidget->setColumnCount(2);
-    listenersTableWidget->setRowCount(listeners.size());
+    listenersTableWidget->setRowCount(numberOfListeners);
     QStringList headerLabels = {"Enabled","Name"};
     listenersTableWidget->setHorizontalHeaderLabels(headerLabels);
     listenersTableWidget->verticalHeader()->setVisible(false);
     listenersTableWidget->setShowGrid(false);
     listenersTableWidget->horizontalHeader()->setStretchLastSection(true);
+    /*connect(listenersTableWidget->selectionModel(),
+            SIGNAL(currentChanged(const QModelIndex&,const QModelIndex&)),
+            this,SLOT(slotCurrentListenerChanged(const QModelIndex&,const QModelIndex&)));*/
+}
+
+void DlgPrefMetadata::fillTableWithServices(const QLinkedList<ScrobblingServicePtr> &listeners) {
     unsigned int currentRow = 0;
-    for (ScrobblingServicePtr pService : listeners) {
+    for (const ScrobblingServicePtr &pService : listeners) {
         QTableWidgetItem *enabledItem = new QTableWidgetItem;
         enabledItem->setFlags(Qt::NoItemFlags);
         listenersTableWidget->setItem(currentRow,0,enabledItem);
@@ -26,5 +47,20 @@ DlgPrefMetadata::DlgPrefMetadata(QWidget *pParent,UserSettingsPointer pSettings)
         listenersTableWidget->setItem(currentRow,1,nameItem);
         ++currentRow;
     }
-
+    for (int i = listeners.size(); i < listeners.size() + 5; ++i) {
+        QTableWidgetItem *enabledItem = new QTableWidgetItem;
+        enabledItem->setFlags(Qt::NoItemFlags);
+        listenersTableWidget->setItem(i,0,enabledItem);
+        listenersTableWidget->setCellWidget(i,0,new QCheckBox);
+        QTableWidgetItem *nameItem = new QTableWidgetItem("Mock");
+        nameItem->setFlags(Qt::ItemIsSelectable);
+        listenersTableWidget->setItem(i,1,nameItem);
+    }
 }
+
+void DlgPrefMetadata::slotCurrentListenerChanged
+        (const QModelIndex &previous, const QModelIndex &current) {
+    listenersTableWidget->selectionModel()->select(previous,QItemSelectionModel::Clear);
+}
+
+
--- src/preferences/dialog/dlgprefmetadata.h
+++ src/preferences/dialog/dlgprefmetadata.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include "broadcast/scrobblingservice.h"
 #include "preferences/usersettings.h"
 #include "preferences/dlgpreferencepage.h"
 #include "preferences/dialog/ui_dlgprefmetadatadlg.h"
 
-class DlgPrefMetadata : public DlgPreferencePage, public Ui::DlgPrefMetadataDlg {
-public:
-    DlgPrefMetadata(QWidget *pParent, UserSettingsPointer pSettings);
+namespace Ui {
+    class fileListenerBox;
+}
 
+class DlgPrefMetadata : public DlgPreferencePage, public Ui::DlgPrefMetadataDlg {
+    Q_OBJECT
+  public:
+    DlgPrefMetadata(QWidget *pParent, UserSettingsPointer pSettings);
+    ~DlgPrefMetadata() override;
+  private:
+    void setupTableWidget(UserSettingsPointer pSettings);
+    void setTableParameters(int numberOfListeners);
+    void fillTableWithServices(const QLinkedList<ScrobblingServicePtr> &listeners);
+    Ui::fileListenerBox *fileListenerBox;
+  private slots:
+    void slotCurrentListenerChanged(const QModelIndex &previous, const QModelIndex &current);
 };
 
 
