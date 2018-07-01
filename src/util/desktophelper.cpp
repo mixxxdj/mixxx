707d8eb382ebb67fe6242106d07d53f54c135ad6
Daniel Schürmann <daschuer@mixxx.org>
2018-07-01 23:44:03 +0200

    Select Track in File Browser with fallback logic

--- src/util/desktophelper.cpp
+++ src/util/desktophelper.cpp
@@ -6,11 +6,19 @@
 #include <QDesktopServices>
 #include <QDir>
 #include <QUrl>
+#include <QProcess>
 
 namespace {
-    QString sOpenInFileBrowserCommand;
+    QString sSelectInFileBrowserCommand;
+
     QString getOpenInFileBrowserCommand() {
+        return "nemo --select %1";
         return ""; // no special command use QDesktopServices";
+    }
+
+    QString removeChildDir(const QString& path) {
+        int index = path.lastIndexOf(QChar('/'));
+        return path.left(index);
     }
 } // anonymous namespace
 
@@ -18,34 +26,45 @@ namespace {
 namespace mixxx {
 
 void DesktopHelper::openInFileBrowser(const QStringList& paths) {
-    if (sOpenInFileBrowserCommand.isNull()) {
-        sOpenInFileBrowserCommand = getOpenInFileBrowserCommand();
+    if (sSelectInFileBrowserCommand.isNull()) {
+        sSelectInFileBrowserCommand = getOpenInFileBrowserCommand();
     }
     QSet<QString> openedDirs;
-    QDir dir;
+    QString dirPath;
     for (const auto& path: paths) {
-        QStringList splittedPath = path.split("/");
-        do {
-            dir = splittedPath.join("/");
-            splittedPath.removeLast();
-        } while (!dir.exists() && splittedPath.size());
-
-        // This function does not work for a non-existent directory!
-        // so it is essential that in the worst case it try opening
-        // a valid directory, in this case, 'QDir::home()'.
-        // Otherwise nothing would happen...
-        if (!dir.exists()) {
-            // it ensures a valid dir for any OS (Windows)
-            dir = QDir::home();
-        }
-        // not open the same dir twice
-        QString dirPath = dir.absolutePath();
-        if (!openedDirs.contains(dirPath)) {
-            openedDirs.insert(dirPath);
-            if (sOpenInFileBrowserCommand.isEmpty()) {
-                QDesktopServices::openUrl(QUrl::fromLocalFile(dirPath));
-            } else {
+        dirPath = removeChildDir(path);
+        if (!sSelectInFileBrowserCommand.isEmpty() &&
+                QFile::exists(path)) {
+            if (!openedDirs.contains(dirPath)) {
+                openedDirs.insert(dirPath);
                 // special command, that supports also select the requested file
+                QString command = sSelectInFileBrowserCommand.arg(
+                        QUrl::fromLocalFile(path).toString());
+                qDebug() << "starting:" << command;
+                qDebug() << "parent:" << dirPath;
+                QProcess::startDetached(command);
+            }
+        } else {
+            QDir dir = dirPath;
+            while (!dir.exists() && dirPath.size()) {
+                dirPath = removeChildDir(dirPath);
+                dir = dirPath;
+            }
+
+            // This function does not work for a non-existent directory!
+            // so it is essential that in the worst case it try opening
+            // a valid directory, in this case, 'QDir::home()'.
+            // Otherwise nothing would happen...
+            if (!dir.exists()) {
+                // it ensures a valid dir for any OS (Windows)
+                dir = QDir::home();
+            }
+            // not open the same dir twice
+            dirPath = dir.absolutePath();
+            qDebug() << "opening:" << dirPath;
+            if (!openedDirs.contains(dirPath)) {
+                openedDirs.insert(dirPath);
+                QDesktopServices::openUrl(QUrl::fromLocalFile(dirPath));
             }
         }
     }
