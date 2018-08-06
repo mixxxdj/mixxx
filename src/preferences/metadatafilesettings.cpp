41bdfb9bacf504f1680fba167794d6a5514657d4
davidhm <david.hm.1994@gmail.com>
2018-08-06 14:31:13 +0200

    Resized combobox

--- src/preferences/metadatafilesettings.cpp
+++ src/preferences/metadatafilesettings.cpp
@@ -3,6 +3,7 @@
 #include <QLineEdit>
 #include <QFileDialog>
 #include <QAbstractItemView>
+#include <algorithm>
 
 #include "metadatafilesettings.h"
 
@@ -148,6 +149,7 @@ void MetadataFileSettings::cancelSettings() {
 void MetadataFileSettings::setupEncodingComboBox() {
     m_widgets.encodingBox->clear();
     QList<QByteArray> codecs = QTextCodec::availableCodecs();
+    std::sort(codecs.begin(),codecs.end());
 
     QList<QByteArray> preferredCodecs = {
             "latin1",
@@ -175,12 +177,14 @@ void MetadataFileSettings::setupEncodingComboBox() {
                 this,&MetadataFileSettings::slotMoreButtonComboboxPressed);
 
         m_remainingCodecs = codecs;
+        m_widgets.encodingBox->setEditable(false);
     }
 
     else {
         for (const QByteArray& codec : codecs) {
             m_widgets.encodingBox->addItem(codec);
         }
+        m_widgets.encodingBox->setEditable(true);
     }
     m_widgets.encodingBox->setCurrentText(s_latestSettings.fileEncoding);
 }
