7dab7fd26ae55d453ca65965a490622b00078d00
Nino Miškić-Pletenac <nino.mip@gmail.com>
2018-08-15 01:47:10 +0200

    Make sure Track::findCueByType() is not used for hotcues

--- src/track/track.cpp
+++ src/track/track.cpp
@@ -784,6 +784,9 @@ CuePointer Track::createAndAddCue() {
 }
 
 CuePointer Track::findCueByType(Cue::CueType type) const {
+    // This method cannot be used for hotcues because there can be
+    // multiple hotcues and this function returns only a single CuePointer.
+    DEBUG_ASSERT(type != Cue::CUE);
     QMutexLocker lock(&m_qMutex);
     for (const CuePointer& pCue: m_cuePoints) {
         if (pCue->getType() == type) {
