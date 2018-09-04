617a817b76805c7963f069131f2ee80d88e93ab2
Daniel Schürmann <daschuer@mixxx.org>
2018-09-04 21:51:56 +0200

    Used cmake install target

--- scripts/layouts_tool/build.sh
+++ scripts/layouts_tool/build.sh
@@ -8,8 +8,6 @@ function build {
     # Set directory variables
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     TEMP_DIR=${DIR}/.cmake_temp
-    BIN_DIR=${DIR}/bin
-    EXECUTABLE=${TEMP_DIR}/layoutstool
 
     # Create temp dir (after removing old one, if necessary)
     $(rm -rf ${TEMP_DIR})
@@ -18,17 +16,13 @@ function build {
     echo "Creating CMake files..."
     eval "cmake -B${TEMP_DIR} -H${DIR}"
 
-    echo "Running make..."
-    eval "make -C ${TEMP_DIR}"
-
-    echo "Copying executable to bin folder..."
-    eval "mkdir -p ${BIN_DIR}"
-    eval "mv -f ${EXECUTABLE} ${BIN_DIR}"
+    echo "Building"
+    eval "cmake --build ${TEMP_DIR} --target install"
 
     echo "Cleaning up the mess I made... :)"
     eval "rm -rf ${TEMP_DIR}"
 
-    echo "Done! Run the tool from ${BIN_DIR}/layoutstool"
+    echo "Done! Run the tool from bin/layoutstool"
 }
 
 build
