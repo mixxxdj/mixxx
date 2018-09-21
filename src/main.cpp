f31d23f29dbd8548cbc53cfba8a6979ed618dcc9
RJ Skerry-Ryan <rryan@mixxx.org>
2018-09-20 19:19:41 -0700

    Always create ErrorDialogHandler from the main thread. Fixes Bug #1748636.

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,11 @@ int main(int argc, char * argv[]) {
     // ErrorDialogHandler::errorDialog(). TODO(XXX): Remove this hack.
     QThread::currentThread()->setObjectName("Main");
 
+    // Create the ErrorDialogHandler in the main thread, otherwise it will be
+    // created in the thread of the first caller to instance(), which may not be
+    // the main thread. Bug #1748636.
+    ErrorDialogHandler::instance();
+
     mixxx::Logging::initialize(args.getSettingsPath(),
                                args.getLogLevel(),
                                args.getLogFlushLevel(),
