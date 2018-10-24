97c84a88ec5f5359f1dba3c3200f66bb339550d9
tock203 <tick@tock.tech>
2018-10-24 22:37:24 +0900

    Swap bpm and gain knob position

--- res/controllers/DJ TechTools-MIDI Fighter Twister-scripts.js
+++ res/controllers/DJ TechTools-MIDI Fighter Twister-scripts.js
@@ -6,31 +6,22 @@ var MidiFighterTwister = {
             return Math.pow(value / 4, 0.5) * 128;
         }
 
-        function updateBpmLed(group, ctrl, bpm) {
-            var fileBpm = engine.getValue(group, "file_bpm");
-            var bpmRangeRatio = engine.getValue(group, "rateRange");
-            var minBpm = fileBpm - fileBpm * bpmRangeRatio;
-            var bpmRange = fileBpm * bpmRangeRatio * 2;
-            var scaled = Math.max(0, Math.min(127, (bpm - minBpm) / bpmRange * 128));
-            midi.sendShortMsg(cc, ctrl, scaled);
-        }
-
         MidiFighterTwister.connections = [
-            engine.makeConnection("[Channel1]", "bpm", function(value) {
-                updateBpmLed("[Channel1]", 0x00, value);
+            engine.makeConnection("[Channel1]", "rate", function(value) {
+                midi.sendShortMsg(cc, 0x04, (value + 1) / 2 * 128);
             }),
-            engine.makeConnection("[Channel2]", "bpm", function(value) {
-                updateBpmLed("[Channel2]", 0x03, value);
+            engine.makeConnection("[Channel2]", "rate", function(value) {
+                midi.sendShortMsg(cc, 0x07, (value + 1) / 2 * 128);
             }),
             engine.makeConnection("[Master]", "crossfader", function(value) {
                 var scaled = (value + 1) / 2 * 128;
                 midi.sendShortMsg(cc, 0x0C, scaled);
             }),
             engine.makeConnection("[Channel1]", "pregain", function(value) {
-                midi.sendShortMsg(cc, 0x04, linearize(value));
+                midi.sendShortMsg(cc, 0x00, linearize(value));
             }),
             engine.makeConnection("[Channel2]", "pregain", function(value) {
-                midi.sendShortMsg(cc, 0x07, linearize(value));
+                midi.sendShortMsg(cc, 0x03, linearize(value));
             }),
             // High EQ
             engine.makeConnection("[EqualizerRack1_[Channel1]_Effect1]", "parameter3", function(value) {
@@ -75,14 +66,6 @@ var MidiFighterTwister = {
         for (var i = 0; i < MidiFighterTwister.connections.length; i++) {
             MidiFighterTwister.connections[i].disconnect();
         }
-    },
-    updateBpm: function(channel, control, value, status, group) {
-        var fileBpm = engine.getValue(group, "file_bpm");
-        var bpmRangeRatio = engine.getValue(group, "rateRange");
-        var minBpm = fileBpm - fileBpm * bpmRangeRatio;
-        var bpmRange = fileBpm * bpmRangeRatio * 2;
-        var scaledBpm = value / 128 * bpmRange + minBpm;
-        engine.setValue(group, "bpm", scaledBpm);
     },
     resetSuperKnob: function(channel, control, value, status, group) {
         engine.setValue(group, "super1", 0.5);
