343f02a1152a07ae171b47f2cac9eb179426fd7c
Daniel Schürmann <daschuer@mixxx.org>
2018-10-24 23:36:20 +0200

    Added Bernd Binder to the contributor list. Thank you very much.

--- src/dialog/dlgabout.cpp
+++ src/dialog/dlgabout.cpp
@@ -98,7 +98,8 @@ DlgAbout::DlgAbout(QWidget* parent) : QDialog(parent), Ui::DlgAboutDlg() {
             << "luzpaz"
             << "Sebastian Reu&szlig;e"
             << "Pawe&#322; Goli&#324;ski"
-            << "beenisss";
+            << "beenisss"
+            << "Bernd Binder";
 
     QStringList specialThanks;
     specialThanks
