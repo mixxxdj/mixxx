932ff41bdd6425a22da6fa50fb9af0f1430d4691
RJ Skerry-Ryan <rryan@mixxx.org>
2018-10-25 10:30:12 -0700

    Wording tweaks.

--- src/widget/wslidercomposed.cpp
+++ src/widget/wslidercomposed.cpp
@@ -243,16 +243,16 @@ void WSliderComposed::guiTick() {
 }
 
 void WSliderComposed::inputActivity() {
-    // Bug #1793015: Previously we would simply call QWidget::update in response
+    // Bug #1793015: With Qt 4, we would simply call QWidget::update in response
     // to input events that required re-rendering widgets, relying on Qt to
     // batch them together and deliver them at a reasonable frequency. On macOS,
-    // the behavior of QWidget::update seems to have changed such that render
-    // events happen much more frequently than they used to. To address this, we
-    // instead use a downsampling timer attached to the VSyncThread's render
-    // ticks for the waveform renderers. The timer invokes guiTick(), which is
-    // responsible for actually calling QWidget::update(). When input arrives,
-    // we call inputActivity to attach the timer. After 1 second of inactivity,
-    // we disconnect the timer.
+    // the behavior of QWidget::update in Qt 5 seems to have changed such that
+    // render events happen much more frequently than they used to. To address
+    // this, we instead use a downsampling timer attached to the VSyncThread's
+    // render ticks for the waveform renderers. The timer invokes guiTick(),
+    // which is responsible for actually calling QWidget::update(). When input
+    // arrives, we call inputActivity to attach the timer. After 1 second of
+    // inactivity, we disconnect the timer.
     m_lastActivity = mixxx::Time::elapsed();
     if (!m_guiTickTimer.isActive()) {
         m_guiTickTimer.start(mixxx::Duration::fromMillis(20));
