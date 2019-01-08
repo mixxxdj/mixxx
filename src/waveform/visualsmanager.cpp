bda63b1b1e9e66643650d1d82f972a37d041c21b
Daniel Schürmann <daschuer@mixxx.org>
2019-01-08 21:58:20 +0100

    use a common timer for al deck visuals and clean up

--- src/waveform/visualsmanager.cpp
+++ src/waveform/visualsmanager.cpp
@@ -3,14 +3,6 @@
 #include "waveform/waveformwidgetfactory.h"
 #include "control/controlobject.h"
 
-namespace {
-
-// Rate at which the playpos slider is updated
-const int kUpdateRate = 15; // updates per second
-// Number of kiUpdateRates that go by before we update BPM.
-const int kSlowUpdateDivider = 4; // kUpdateRate / kSlowUpdateDivider = 3.75 updates per sec
-
-} // anonymous namespace
 
 DeckVisuals::DeckVisuals(const QString& group)
         : m_group(group),
@@ -27,42 +19,33 @@ DeckVisuals::DeckVisuals(const QString& group)
 
     // Control used to communicate ratio playpos to GUI thread
     m_pVisualPlayPos = VisualPlayPosition::getVisualPlayPosition(m_group);
-
-    m_cpuTimer.start();
 }
 
 // this is called from WaveformWidgetFactory::render in the main thread with the
 // configured waveform frame rate
 void DeckVisuals::process(double remainingTimeTriggerSeconds) {
-    if (m_cpuTimer.elapsed() >= mixxx::Duration::fromMillis(1000 / kUpdateRate)) {
-        m_cpuTimer.restart();
+    double playPosition;
+    double tempoTrackSeconds;
+    m_pVisualPlayPos->getTrackTime(&playPosition, &tempoTrackSeconds);
 
-        double playPosition;
-        double tempoTrackSeconds;
-        m_pVisualPlayPos->getTrackTime(&playPosition, &tempoTrackSeconds);
+    double timeRemaining = (1.0 - playPosition) * tempoTrackSeconds;
+    m_pTimeRemaining->set(timeRemaining);
+    m_pTimeElapsed->set(tempoTrackSeconds - timeRemaining);
 
-        double timeRemaining = (1.0 - playPosition) * tempoTrackSeconds;
-        m_pTimeRemaining->set(timeRemaining);
-        m_pTimeElapsed->set(tempoTrackSeconds - timeRemaining);
-
-        if (!playButton.toBool() || // not playing
-                loopEnabled.toBool() || // in loop
-                tempoTrackSeconds <= remainingTimeTriggerSeconds || // track too short
-                timeRemaining > remainingTimeTriggerSeconds // before the trigger
-                ) {
-            m_pEndOfTrack->set(0.0);
-        } else {
-            m_pEndOfTrack->set(1.0);
-        }
-
-       // m_playposSlider->set(fFractionalPlaypos);
-       // m_pCueControl->updateIndicators();
-
-        // Update the BPM even more slowly
-        m_SlowTickCnt = (m_SlowTickCnt + 1) % kSlowUpdateDivider;
-        if (m_SlowTickCnt == 0) {
-            m_pVisualBpm->set(engineBpm.get());
-        }
-        m_pVisualKey->set(engineKey.get());
+    if (!playButton.toBool() || // not playing
+            loopEnabled.toBool() || // in loop
+            tempoTrackSeconds <= remainingTimeTriggerSeconds || // track too short
+            timeRemaining > remainingTimeTriggerSeconds // before the trigger
+            ) {
+        m_pEndOfTrack->set(0.0);
+    } else {
+        m_pEndOfTrack->set(1.0);
     }
+
+    // Update the BPM even more slowly
+    m_SlowTickCnt = (m_SlowTickCnt + 1) % kSlowUpdateDivider;
+    if (m_SlowTickCnt == 0) {
+        m_pVisualBpm->set(engineBpm.get());
+    }
+    m_pVisualKey->set(engineKey.get());
 }
