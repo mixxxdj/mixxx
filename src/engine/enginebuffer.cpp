1d62453aec751e279676507518cf4b4fc8b19969
Daniel Schürmann <daschuer@mixxx.org>
2019-01-08 23:12:36 +0100

    Added 0 check back as VERIFY_OR_DEBUG_ASSERT to guard agains div/0

--- src/engine/enginebuffer.cpp
+++ src/engine/enginebuffer.cpp
@@ -1180,6 +1180,11 @@ void EngineBuffer::postProcess(const int iBufferSize) {
 }
 
 void EngineBuffer::updateIndicators(double speed, int iBufferSize) {
+    VERIFY_OR_DEBUG_ASSERT(m_trackSampleRateOld && m_trackSamplesOld) {
+        // no track loaded, function not called in this case
+        return;
+    }
+
     // Increase samplesCalculated by the buffer size
     m_iSamplesSinceLastIndicatorUpdate += iBufferSize;
 
