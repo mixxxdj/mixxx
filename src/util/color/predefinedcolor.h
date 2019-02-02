dd29a4b5947108a6fe32ef74ee2600ffa4620880
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2019-02-02 19:26:14 +0100

    Improve code comments

--- src/util/color/predefinedcolor.h
+++ src/util/color/predefinedcolor.h
@@ -19,11 +19,12 @@ class PredefinedColor final {
 
     // The QColor that is used by default to render this PredefinedColor.
     const QColor m_defaultRgba;
-    // The name of the color used programatically, e.g. on skin files.
+    // The name of the color used programmatically, e.g. on skin files.
     const QString m_sName;
     // The name of the color used on UI.
     const QString m_sDisplayName;
     // An Id uniquely identifying this predefined color.
+    // This value is used to identify a color on the DB and control objects.
     const int m_iId;
 };
 typedef std::shared_ptr<const PredefinedColor> PredefinedColorPointer;
