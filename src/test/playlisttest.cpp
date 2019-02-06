efdf32139ebda4b9535285f56993b754c0db0b61
Daniel Schürmann <daschuer@mixxx.org>
2019-02-06 20:29:00 +0100

    Added playlisttest

---
+++ src/test/playlisttest.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include <QDebug>
+#include <QUrl>
+
+#include "library/parserm3u.h"
+
+
+class PlaylistTest : public testing::Test {};
+
+TEST_F(PlaylistTest, Normalize) {
+    ParserM3u parser;
+
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///foo/bar.mp3").toStdString().c_str(),
+            "/foo/bar.mp3");
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:foo/bar.mp3").toStdString().c_str(),
+            "foo/bar.mp3");
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///c:/foo/bar.mp3").toStdString().c_str(),
+            "/c:/foo/bar.mp3");
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///foo%20/bar.mp3").toStdString().c_str(),
+            "/foo /bar.mp3");
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("c:/foo/bar.mp3").toStdString().c_str(),
+            "c:/foo/bar.mp3");
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("c:\\foo\\bar.mp3").toStdString().c_str(),
+            "c:/foo/bar.mp3");
+}
