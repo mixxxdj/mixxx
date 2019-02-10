8e5b59463f0ea6adf82d6f62673364e0d1121636
Daniel Schürmann <daschuer@mixxx.org>
2019-02-10 22:14:47 +0100

    Make test conditional because Windows has no root folder

--- src/test/playlisttest.cpp
+++ src/test/playlisttest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <QtGlobal>
 #include <QDebug>
 #include <QUrl>
 
@@ -15,8 +16,13 @@ TEST_F(PlaylistTest, Normalize) {
             "/foo/bar.mp3");
     EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:foo/bar.mp3").toStdString().c_str(),
             "foo/bar.mp3");
+#ifdef Q_OS_WIN
+    EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///c:/foo/bar.mp3").toStdString().c_str(),
+            "c:/foo/bar.mp3");
+#else
     EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///c:/foo/bar.mp3").toStdString().c_str(),
             "/c:/foo/bar.mp3");
+#endif
     EXPECT_STREQ(parser.playlistEntrytoLocalFile("file:///foo%20/bar.mp3").toStdString().c_str(),
             "/foo /bar.mp3");
     EXPECT_STREQ(parser.playlistEntrytoLocalFile("c:/foo/bar.mp3").toStdString().c_str(),
