6ec68bc58947c5b7595e98b0b93e4a821ca5616b
Uwe Klotz <uklotz@mixxx.org>
2019-05-05 20:16:01 +0200

    Use -Wno-deprecated-copy only for C++

--- build/depends.py
+++ build/depends.py
@@ -1362,7 +1362,7 @@ class MixxxCore(Feature):
                 # Avoid many warnings from GCC 9 about implicitly defined copy assignment
                 # operators that are deprecated for classes with a user-provided copy
                 # constructor. This affects both Qt 5.12 and Mixxx.
-                build.env.Append(CCFLAGS='-Wno-deprecated-copy')
+                build.env.Append(CXXFLAGS='-Wno-deprecated-copy')
 
             if build.compiler_is_clang:
                 # Quiet down Clang warnings about inconsistent use of override
