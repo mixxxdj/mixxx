e67f8ec6807566da8f61e9e961fa7b071d7f0df3
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2019-05-12 14:55:58 +0200

    Roland DJ-505: Fix Sampler volume on Velocity Sampler mode

--- res/controllers/Roland_DJ-505-scripts.js
+++ res/controllers/Roland_DJ-505-scripts.js
@@ -1088,6 +1088,9 @@ DJ505.Sampler = function() {
 
     this.customSamplePlayback = function (channel, control, value, status, group) {
         if (value) {
+            // Volume has to be re-set because it could have been modified by
+            // the Performance Pads in Velocity Sampler mode
+            engine.setValue(group, "volume", engine.getValue("[Auxiliary3]", "volume"));
             engine.setValue(group, 'cue_gotoandplay', 1);
         }
     };
