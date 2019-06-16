f0759d9cfab673edea0fb0ae5f793e5c49c36c48
haslersn <sebastian.hasler@gmx.net>
2019-06-16 14:54:21 +0200

    parented_ptr: Use comments as in the Mixxx coding guidelines
    https://www.mixxx.org/wiki/doku.php/coding_guidelines#comments

--- src/util/parented_ptr.h
+++ src/util/parented_ptr.h
@@ -34,9 +34,7 @@ class parented_ptr {
     parented_ptr(const parented_ptr<T>&) = delete;
     parented_ptr& operator=(const parented_ptr<T>&) = delete;
 
-    /*
-     * If U* is convertible to T* then we also want parented_ptr<U> convertible to parented_ptr<T>
-     */
+    // If U* is convertible to T* then parented_ptr<U> is convertible to parented_ptr<T>
     template<
             typename U,
             typename = typename std::enable_if<std::is_convertible<U*, T*>::value, U>::type>
@@ -45,10 +43,7 @@ class parented_ptr {
         u.m_ptr = nullptr;
     }
 
-    /*
-     * If U* is convertible to T* then we also want parented_ptr<U> assignable to parented_ptr<T>
-     * E.g. parented_ptr<Base> base = make_parented<Derived>(); should work as expected.
-     */
+    // If U* is convertible to T* then parented_ptr<U> is assignable to parented_ptr<T>
     template<
             typename U,
             typename = typename std::enable_if<std::is_convertible<U*, T*>::value, U>::type>
@@ -98,13 +93,14 @@ inline parented_ptr<T> make_parented(Args&&... args) {
     return parented_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
-/// A use case for this function is when giving an object owned by `std::unique_ptr` to a Qt
-/// function, that will make the object owned by the Qt object tree. Example:
-/// ```
-/// parent->someFunctionThatAddsAChild(to_parented(child))
-/// ```
-/// where `child` is a `std::unique_ptr`. After the call, the created `parented_ptr` will
-/// automatically be destructed such that the DEBUG_ASSERT for an existent parent is triggered.
+// A use case for this function is when giving an object owned by `std::unique_ptr` to a Qt
+// function, that will make the object owned by the Qt object tree. Example:
+// ```
+// parent->someFunctionThatAddsAChild(to_parented(child))
+// ```
+// where `child` is a `std::unique_ptr`. After the call, the created `parented_ptr` will
+// automatically be destructed such that the DEBUG_ASSERT that checks whether a parent exists is
+// triggered.
 template<typename T>
 inline parented_ptr<T> to_parented(std::unique_ptr<T>& u) noexcept {
     // the DEBUG_ASSERT in the parented_ptr constructor will catch cases where the unique_ptr should
@@ -112,7 +108,8 @@ inline parented_ptr<T> to_parented(std::unique_ptr<T>& u) noexcept {
     return parented_ptr<T>{u.release()};
 }
 
-// Comparison operator definitions
+// Comparison operator definitions:
+
 template<typename T, typename U>
 inline bool operator==(const T* lhs, const parented_ptr<U>& rhs) noexcept {
     return lhs == rhs.get();
