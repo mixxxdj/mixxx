289effa1ee233ba554b97d2ab90ca15c1ebb63c6
Uwe Klotz <uklotz@mixxx.org>
2019-06-17 22:03:29 +0200

    Add Sebastian Hasler to list of recent contributors

--- src/dialog/dlgabout.cpp
+++ src/dialog/dlgabout.cpp
@@ -95,7 +95,8 @@ DlgAbout::DlgAbout(QWidget* parent) : QDialog(parent), Ui::DlgAboutDlg() {
             << "Kerrick Staley"
             << "Jan Holthuis"
             << "Raphael Graf"
-            << "YunQiang Su";
+            << "YunQiang Su"
+            << "Sebastian Hasler";
 
     QStringList specialThanks;
     specialThanks
