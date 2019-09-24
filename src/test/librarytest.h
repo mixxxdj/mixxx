1d9953a78446d49a83bbaf10bcde8be08043e1ee
Uwe Klotz <uklotz@mixxx.org>
2019-09-25 00:12:22 +0200

    Rename class member

--- src/test/librarytest.h
+++ src/test/librarytest.h
@@ -26,14 +26,14 @@ class LibraryTest : public MixxxTest,
         : m_mixxxDb(config(), kInMemoryDbConnection),
           m_dbConnectionPooler(m_mixxxDb.connectionPool()),
           m_dbConnection(mixxx::DbConnectionPooled(m_mixxxDb.connectionPool())),
-          m_trackCollection(std::make_unique<TrackCollection>(config())) {
+          m_pTrackCollection(std::make_unique<TrackCollection>(config())) {
         MixxxDb::initDatabaseSchema(m_dbConnection);
-        m_trackCollection->connectDatabase(m_dbConnection);
+        m_pTrackCollection->connectDatabase(m_dbConnection);
         GlobalTrackCache::createInstance(this);
     }
     ~LibraryTest() override {
-        m_trackCollection->disconnectDatabase();
-        m_trackCollection.reset();
+        m_pTrackCollection->disconnectDatabase();
+        m_pTrackCollection.reset();
         // With the track collection all remaining track references
         // should have been dropped before destroying the cache.
         GlobalTrackCache::destroyInstance();
@@ -48,12 +48,12 @@ class LibraryTest : public MixxxTest,
     }
 
     TrackCollection* collection() {
-        return m_trackCollection.get();
+        return m_pTrackCollection.get();
     }
 
   private:
     const MixxxDb m_mixxxDb;
     const mixxx::DbConnectionPooler m_dbConnectionPooler;
     QSqlDatabase m_dbConnection;
-    std::unique_ptr<TrackCollection> m_trackCollection;
+    std::unique_ptr<TrackCollection> m_pTrackCollection;
 };
