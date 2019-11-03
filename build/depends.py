37d5da132e59fcd237818bfa4bb71d9f5619a982
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2019-11-03 10:10:06 +0100

    Remove PredefinedColor
    # Conflicts:
    #	src/util/color/predefinedcolorsset.h

--- build/depends.py
+++ build/depends.py
@@ -1299,8 +1299,7 @@ class MixxxCore(Feature):
                    "src/util/desktophelper.cpp",
                    "src/util/widgetrendertimer.cpp",
                    "src/util/workerthread.cpp",
-                   "src/util/workerthreadscheduler.cpp",
-                   "src/util/color/predefinedcolor.cpp"
+                   "src/util/workerthreadscheduler.cpp"
                    ]
 
         proto_args = {
