2572916b75d8f5bd19b0226f5ea20ca4c3a14df5
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2019-11-04 11:21:43 +0100

    Add QColorDialog to ColorMenu

--- src/widget/colormenu.cpp
+++ src/widget/colormenu.cpp
@@ -1,26 +1,75 @@
 #include "widget/colormenu.h"
+
 #include "util/color/color.h"
 
-ColorMenu::ColorMenu(QWidget *parent)
-        : QMenu(parent) {
+ColorMenu::ColorMenu(QWidget* parent)
+        : QMenu(parent), m_pColorDialog(new QColorDialog(this)) {
     // If another title would be more appropriate in some context, setTitle
     // can be called again after construction.
     setTitle(tr("Set color"));
     useColorPalette(ColorPalette::mixxxHotcuesPalette);
+
+    connect(m_pColorDialog,
+            &QColorDialog::colorSelected,
+            this,
+            [this](const QColor& newColor) {
+                if (newColor.isValid()) {
+                    emit(colorPicked(newColor));
+                }
+            });
 }
 
 void ColorMenu::useColorPalette(const ColorPalette& colorPalette) {
     clear();
-    for (const auto& pColor : colorPalette.m_colorList) {
-        QAction* pColorAction = new QAction(this);
-        QPixmap pixmap(80, 80);
-        pixmap.fill(pColor);
-        pColorAction->setIcon(QIcon(pixmap));
+    m_pActionGroup = new QActionGroup(this);
+    m_pActionGroup->setExclusive(true);
 
-        m_pColorActions.append(pColorAction);
+    createPaletteColorsActions(colorPalette);
+    createOtherColorAction();
+}
+void ColorMenu::setCurrentColor(QColor currentColor) {
+    m_currentColor = currentColor;
+    selectCurrentColorAction(currentColor);
+}
+void ColorMenu::openColorDialog() {
+    m_pColorDialog->setCurrentColor(m_currentColor);
+    m_pColorDialog->open();
+}
+void ColorMenu::createPaletteColorsActions(const ColorPalette& colorPalette) {
+    for (const auto& color : colorPalette.m_colorList) {
+        QAction* pColorAction = new QAction(m_pActionGroup);
+        QPixmap pixmap(80, 80);
+        pixmap.fill(color);
+        pColorAction->setIcon(QIcon(pixmap));
+        pColorAction->setCheckable(true);
+        pColorAction->setData(color);
+
         addAction(pColorAction);
-        connect(pColorAction, &QAction::triggered, this, [pColor, this]() {
-            emit(colorPicked(pColor));
+        connect(pColorAction, &QAction::triggered, this, [color, this]() {
+            emit(colorPicked(color));
         });
+    }
+}
+
+void ColorMenu::createOtherColorAction() {
+    m_pOtherColorAction = new QAction(m_pActionGroup);
+    m_pOtherColorAction->setText("...");
+    m_pOtherColorAction->setCheckable(true);
+    addAction(m_pOtherColorAction);
+    connect(m_pOtherColorAction,
+            &QAction::triggered,
+            this,
+            &ColorMenu::openColorDialog);
+}
+
+void ColorMenu::selectCurrentColorAction(const QColor& currentColor) const {
+    for (QAction* pAction : actions()) {
+        QColor color = pAction->data().value<QColor>();
+        // Other color action is the last one, if we find it, we didn't
+        // matched to any color action.
+        if (color == currentColor || pAction == m_pOtherColorAction) {
+            pAction->setChecked(true);
+            return;
+        }
     }
 }
