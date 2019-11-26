93132df382b091c17985a70ae606b91fa249083a
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-26 17:28:04 +0100

    pioneer ddj400: get level meters to work

---
+++ res/controllers/Pioneer-DDJ-400-script.js
@@ -0,0 +1,88 @@
+var PioneerDDJ400 = {};
+
+print("HELLU");
+
+var LightsPioneerDDJ400 = {
+    deck1: {
+        vuMeter: {
+            status: 0xB0,
+            data1: 0x02,
+        },
+        playPause: {
+            status: 0x90,
+            data1: 0x0B,
+        },
+        shiftPlayPause: {
+            status: 0x90,
+            data1: 0x47,
+        },
+        cue: {
+            status: 0x90,
+            data1: 0x0C,
+        },
+        shiftCue: {
+            status: 0x90,
+            data1: 0x48,
+        },
+    },
+    deck2: {
+        vuMeter: {
+            status: 0xB0,
+            data1: 0x02,
+        },
+        playPause: {
+            status: 0x91,
+            data1: 0x0B,
+        },
+        shiftPlayPause: {
+            status: 0x91,
+            data1: 0x47,
+        },
+        cue: {
+            status: 0x91,
+            data1: 0x0C,
+        },
+        shiftCue: {
+            status: 0x91,
+            data1: 0x48,
+        },
+    },
+};
+
+
+PioneerDDJ400.init = function() {
+    // Connect the VU-Meter LEDS
+    engine.connectControl('[Channel1]','VuMeter','PioneerDDJ400.vuMeterUpdate');
+    engine.connectControl('[Channel2]','VuMeter','PioneerDDJ400.vuMeterUpdate');
+
+    // reset vumeter
+    PioneerDDJ400.toggleLight(LightsPioneerDDJ400.deck1.vuMeter, false);
+    PioneerDDJ400.toggleLight(LightsPioneerDDJ400.deck2.vuMeter, false);
+};
+
+
+PioneerDDJ400.vuMeterUpdate = function(value, group){
+    'use strict';
+    // The factor 150 here is a manual fit, looking at Mixxx's output and hte level
+    // meter on the DDJ 400.
+    var newVal = value * 150;
+
+    switch(group){
+        case '[Channel1]':
+            midi.sendShortMsg(0xB0, 0x02, newVal);
+            break;
+
+        case '[Channel2]':
+            midi.sendShortMsg(0xB1, 0x02, newVal);
+            break;
+    }
+
+};
+
+
+PioneerDDJ400.shutdown = function() {
+    'use strict';
+    // reset vumeter
+    PioneerDDJ400.toggleLight(LightsPioneerDDJ400.deck1.vuMeter, false);
+    PioneerDDJ400.toggleLight(LightsPioneerDDJ400.deck2.vuMeter, false);
+};
