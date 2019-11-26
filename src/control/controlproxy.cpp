fb29af142c1891e8626bfcae267297569728e84b
Daniel Schürmann <daschuer@mixxx.org>
2019-11-26 22:37:11 +0100

    Remove more redundant null checks

--- src/control/controlproxy.cpp
+++ src/control/controlproxy.cpp
@@ -5,31 +5,18 @@
 #include "control/control.h"
 #include "moc_controlproxy.cpp"
 
-namespace {
-const ConfigKey kNullKey; // because we return it as a reference
-} // namespace
-
 ControlProxy::ControlProxy(const QString& g, const QString& i, QObject* pParent, ControlFlags flags)
         : ControlProxy(ConfigKey(g, i), pParent, flags) {
 }
 
 ControlProxy::ControlProxy(const ConfigKey& key, QObject* pParent, ControlFlags flags)
-        : QObject(pParent),
-          m_pControl(nullptr) {
-    initialize(key, flags);
-}
-
-void ControlProxy::initialize(const ConfigKey& key, ControlFlags flags) {
-    // Prevent double initialization
-    DEBUG_ASSERT(!m_pControl);
-
-    // Prevent empty keys
-    VERIFY_OR_DEBUG_ASSERT(key.isValid() || flags.testFlag(ControlFlag::AllowInvalidKey)) {
-        return;
-    }
-
+        : QObject(pParent) {
     m_pControl = ControlDoublePrivate::getControl(key, flags);
-    DEBUG_ASSERT(m_pControl || flags.testFlag(ControlFlag::NoAssertIfMissing));
+    if (!m_pControl) {
+        DEBUG_ASSERT(flags.testFlag(ControlFlag::NoAssertIfMissing));
+        m_pControl = ControlDoublePrivate::getDefaultControl();
+    }
+    DEBUG_ASSERT(m_pControl);
     DEBUG_ASSERT(valid() || flags.testFlag(ControlFlag::NoAssertIfMissing));
 }
 
@@ -38,5 +25,5 @@ ControlProxy::~ControlProxy() {
 }
 
 const ConfigKey& ControlProxy::getKey() const {
-    return m_pControl ? m_pControl->getKey() : kNullKey;
+    return m_pControl->getKey();
 }
--- src/control/controlproxy.h
+++ src/control/controlproxy.h
@@ -27,15 +27,13 @@ class ControlProxy : public QObject {
             ControlFlags flags = ControlFlag::None);
     ~ControlProxy() override;
 
-    void initialize(const ConfigKey& key, ControlFlags flags = ControlFlag::None);
-
     const ConfigKey& getKey() const;
 
     template<typename Receiver, typename Slot>
     bool connectValueChanged(Receiver receiver,
             Slot func,
             Qt::ConnectionType requestedConnectionType = Qt::AutoConnection) {
-        if (!m_pControl) {
+        if (!valid()) {
             return false;
         }
 
@@ -108,12 +106,12 @@ class ControlProxy : public QObject {
     }
 
     inline bool valid() const {
-        return m_pControl != nullptr;
+        return m_pControl->getKey().isValid();
     }
 
     /// Returns the value of the object. Thread safe, non-blocking.
     inline double get() const {
-        return m_pControl ? m_pControl->get() : 0.0;
+        return m_pControl->get();
     }
 
     /// Returns the bool interpretation of the value. Thread safe, non-blocking.
@@ -123,42 +121,36 @@ class ControlProxy : public QObject {
 
     /// Returns the parameterized value of the object. Thread safe, non-blocking.
     inline double getParameter() const {
-        return m_pControl ? m_pControl->getParameter() : 0.0;
+        return m_pControl->getParameter();
     }
 
     /// Returns the parameterized value of the object. Thread safe, non-blocking.
     inline double getParameterForValue(double value) const {
-        return m_pControl ? m_pControl->getParameterForValue(value) : 0.0;
+        return m_pControl->getParameterForValue(value);
     }
 
     /// Returns the normalized parameter of the object. Thread safe, non-blocking.
     inline double getDefault() const {
-        return m_pControl ? m_pControl->defaultValue() : 0.0;
+        return m_pControl->defaultValue();
     }
 
   public slots:
     /// Sets the control value to v. Thread safe, non-blocking.
     void set(double v) {
-        if (m_pControl) {
-            m_pControl->set(v, this);
-        }
+        m_pControl->set(v, this);
     }
     /// Sets the control parameterized value to v. Thread safe, non-blocking.
     void setParameter(double v) {
-        if (m_pControl) {
-            m_pControl->setParameter(v, this);
-        }
+        m_pControl->setParameter(v, this);
     }
     /// Resets the control to its default value. Thread safe, non-blocking.
     void reset() {
-        if (m_pControl) {
-            // NOTE(rryan): This is important. The originator of this action does
-            // not know the resulting value so it makes sense that we should emit a
-            // general valueChanged() signal even though the change originated from
-            // us. For this reason, we provide nullptr here so that the change is
-            // not filtered in valueChanged()
-            m_pControl->reset();
-        }
+        // NOTE(rryan): This is important. The originator of this action does
+        // not know the resulting value so it makes sense that we should emit a
+        // general valueChanged() signal even though the change originated from
+        // us. For this reason, we provide NULL here so that the change is
+        // not filtered in valueChanged()
+        m_pControl->reset();
     }
 
   signals:
