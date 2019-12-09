dc8fbe779ddbe77f6b8135ffb00b68b416968249
Javier Vilarroig <javier.vilarroig@gmail.com>
2019-12-09 15:42:52 +0100

    Added Signature class

--- src/track/signature.h
+++ src/track/signature.h
@@ -1,12 +1,13 @@
 // signature.h
-// Created on: 06/11/20196 nov. 2019 by Javier Vilarroig
+// Created on: 06/11/2019 by Javier Vilarroig
 
 #pragma once
 
 namespace mixxx {
 
 // Musical Time signature
-// Right now is to be used for bar detection, so only the beats per bar is useful
+// For an explanation of the meaning look at https://en.wikipedia.org/wiki/Time_signature
+// Right now is to be used for bar detection only, so only the beats per bar is useful
 class Signature final {
   private:
     int m_beats;
