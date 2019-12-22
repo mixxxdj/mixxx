bcfca1175a0227ef04416a67a258a5c7c34d1ae0
Uwe Klotz <uklotz@mixxx.org>
2019-12-22 09:47:56 +0100

    Fix decimal rounding on Xenial

--- src/test/wbatterytest.cpp
+++ src/test/wbatterytest.cpp
@@ -9,7 +9,8 @@ class WBatteryTest : public MixxxTest {};
 TEST_F(WBatteryTest, formatTooltip) {
     EXPECT_EQ("0%", WBattery::formatTooltip(0));
     EXPECT_EQ("9%", WBattery::formatTooltip(9.4));
-    EXPECT_EQ("11%", WBattery::formatTooltip(10.5));
+    // NOTE(uklotzde): Rounding of the decimal 10.5 fails on Xenial
+    EXPECT_EQ("11%", WBattery::formatTooltip(10.51));
     EXPECT_EQ("99%", WBattery::formatTooltip(99.1));
     EXPECT_EQ("100%", WBattery::formatTooltip(99.9));
     EXPECT_EQ("100%", WBattery::formatTooltip(100));
