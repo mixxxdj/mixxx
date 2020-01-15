217ab2661d05d9b066f2300e04c73a355272dad6
DJ Phatso <48333945+DJPhatso@users.noreply.github.com>
2020-01-15 09:05:09 -0500

    Add files via upload

--- Hercules-DJControl-Inpulse-200-script.js
+++ Hercules-DJControl-Inpulse-200-script.js
@@ -1,30 +1,80 @@
-// ****************************************************************************
+////////////////////////////////////////////////////////////////////////
+// JSHint configuration                                               //
+////////////////////////////////////////////////////////////////////////
+/* global engine                                                      */
+/* global script                                                      */
+/* global print                                                       */
+/* global midi                                                        */
+//////////////////////////////////////////////////////////////////////// 
+var DJCi200 = {};
+///////////////////////////////////////////////////////////////
+//                       USER OPTIONS                        //
+///////////////////////////////////////////////////////////////
+
+
+
+// How fast scratching is.
+DJCi200.scratchScale = 1.0;
+
+// How much faster seeking (shift+scratch) is than scratching.
+DJCi200.scratchShiftMultiplier = 4;
+
+// How fast bending is.
+DJCi200.bendScale = 1.0;
+
+// DJControl_Inpulse_200_script.js
+//
+// ***************************************************************************
 // * Mixxx mapping script file for the Hercules DJControl Inpulse 200.
-// * Author: DJ Phatso
-// * Version 1.0 (March 2019)
+// * Author: DJ Phatso, contributions by Kerrick Staley
+// *
+// * Version 1.2 (December 2019)
 // * Forum: https://www.mixxx.org/forums/viewtopic.php?f=7&t=12592
 // * Wiki: https://mixxx.org/wiki/doku.php/hercules_djcontrol_inpulse_200
-
+//
+// Changes to v1.2
+// - Code cleanup.
+//
+// Changes to v1.1
+// - Fix seek-to-start and cue-master behavior.
+// - Tweak scratch, seek, and bend behavior.
+// - Controller knob/slider values are queried on startup, so MIXXX is synced.
+// - Fixed vinyl button behavior the first time it's pressed.
+//
+// v1.0 : Original forum release				  
+//
 // TODO: Functions that could be implemented to the script:
 //
-// * FX:
-//  - See how to preselect efffects for a rack
-//  - Tweak scratch effect for Pad 4			  
+//  FX:		
+//  - See how to preselect effects for a rack
+//  - Tweak scratch effect for Pad 4	
 //
+//************************************************************************
+//
+// We have to disable the no-unused-vars check because we have many MIDI
+// callbacks that receive a fixed list of arguments, but we usually don't use
+// most of these arguments. Eslint seems to make it relatively difficult to
+// disable this check on a case-by-case basis, so we disable it for the whole
+// file.
+// See this GitHub issue for more context:
+// https://github.com/eslint/eslint/issues/1939
+// *eslint-disable no-unused-vars*/
+
+DJCi200.kScratchActionNone = 0;
+DJCi200.kScratchActionScratch = 1;
+DJCi200.kScratchActionSeek = 2;
+DJCi200.kScratchActionBend = 3;
+
+//function DJCi200() {}
 
 
+DJCi200.init = function () {
 
-// ****************************************************************************
-function DJCi200() {}
-
-var DJCi200 = {};
-
-DJCi200.scratchButtonStateDA = true;
-DJCi200.scratchButtonStateDB = true;
-
-
-DJCi200.init = function() {
-
+    DJCi200.scratchButtonState = true;
+    DJCi200.scratchAction = {
+        1: DJCi200.kScratchActionNone,
+        2: DJCi200.kScratchActionNone
+    };
 
     //Turn On Vinyl button LED(one for each deck).
     midi.sendShortMsg(0x91, 0x03, 0x7F);
@@ -34,8 +84,8 @@ DJCi200.init = function() {
     midi.sendShortMsg(0x90, 0x05, 0x10);
 
     // Connect the Browser LEDs
-    engine.connectControl("[Library]", "MoveFocus");
-
+    engine.getValue("[Library]", "MoveFocus");
+    engine.getValue("[Master]", "maximize_library");
 
     //Set effects Levels - Dry/Wet
     engine.setParameter("[EffectRack1_EffectUnit1_Effect1]", "meta", 0.6);
@@ -47,172 +97,153 @@ DJCi200.init = function() {
     engine.setParameter("[EffectRack1_EffectUnit1]", "mix", 1);
     engine.setParameter("[EffectRack1_EffectUnit2]", "mix", 1);
 
+
+
     // Ask the controller to send all current knob/slider values over MIDI, which will update
     // the corresponding GUI controls in MIXXX.
     midi.sendShortMsg(0xB0, 0x7F, 0x7F);
 };
 
 
-// The Vinyl buttons, used to enable or disable scratching on the jog wheels (One per deck).
+// The Vinyl button, used to enable or disable scratching on the jog wheels (One per deck).
 
-DJCi200.vinylButtonDA = function(channel, control, value, status, group) {
+DJCi200.vinylButtonDA = function (channel, control, value, status, group) {
     if (value) {
-        if (DJCi200.scratchButtonStateDA) {
-            DJCi200.scratchButtonStateDA = false;
+        if (DJCi200.scratchButtonState) {
+            DJCi200.scratchButtonState = false;
             midi.sendShortMsg(0x91, 0x03, 0x00);
 
         } else {
-            DJCi200.scratchButtonStateDA = true;
+            DJCi200.scratchButtonState = true;
             midi.sendShortMsg(0x91, 0x03, 0x7F);
 
         }
     }
 };
 
-DJCi200.vinylButtonDB = function(channel, control, value, status, group) {
+DJCi200.vinylButtonDB = function (channel, control, value, status, group) {
     if (value) {
-        if (DJCi200.scratchButtonStateDB) {
-            DJCi200.scratchButtonStateDB = false;
+        if (DJCi200.scratchButtonState) {
+            DJCi200.scratchButtonState = false;
             midi.sendShortMsg(0x92, 0x03, 0x00);
 
         } else {
-            DJCi200.scratchButtonStateDB = true;
+            DJCi200.scratchButtonState = true;
             midi.sendShortMsg(0x92, 0x03, 0x7F);
 
         }
     }
 };
 
-// The touch action over the jog wheel
+DJCi200._scratchEnable = function (deck) {
 
-DJCi200.wheelTouchDA = function(channel, control, value, status, group) {
-    channel = channel + 1;
-    if (value > 0 && (engine.getValue("[Channel1]", "play") !== 1 || DJCi200.scratchButtonStateDA)) {
+    var alpha = 1.0 / 8;
+    var beta = alpha / 32;
+    engine.scratchEnable(deck, 248, 33 + 1 / 3, alpha, beta);
+
+};
+
+
+
+DJCi200._convertWheelRotation = function (value) {
+    // When you rotate the jogwheel, the controller always sends either 0x1
+    // (clockwise) or 0x7F (counter clockwise). 0x1 should map to 1, 0x7F
+    // should map to -1 (IOW it's 7-bit signed).
+    return value < 0x40 ? 1 : -1;
+};
+
+
+
+// The touch action on the jog wheel's top surface
+DJCi200.wheelTouch = function (channel, control, value, status, group) {
+    var deck = channel;
+    if (value > 0) {
         //  Touching the wheel.
-        var alpha = 1.0 / 8;
-        var beta = alpha / 32;
-        engine.scratchEnable(1, 400, 33 + 1 / 3, alpha, beta);
+        if (engine.getValue("[Channel" + deck + "]", "play") !== 1 || DJCi200.scratchButtonState) {
+            DJCi200._scratchEnable(deck);
+            DJCi200.scratchAction[deck] = DJCi200.kScratchActionScratch;
+        } else {
+            DJCi200.scratchAction[deck] = DJCi200.kScratchActionBend;
+        }
     } else {
         // Released the wheel.
-        engine.scratchDisable(1);
+        engine.scratchDisable(deck);
+        DJCi200.scratchAction[deck] = DJCi200.kScratchActionNone;
     }
 };
 
-DJCi200.wheelTouchDB = function(channel, control, value, status, group) {
-    channel = channel + 2;
-    if (value > 0 && (engine.getValue("[Channel2]", "play") !== 1 || DJCi200.scratchButtonStateDB)) {
-        // Touching the wheel.
-        var alpha = 1.0 / 8;
-        var beta = alpha / 32;
-        engine.scratchEnable(2, 400, 33 + 1 / 3, alpha, beta);
+
+
+// The touch action on the jog wheel's top surface while holding shift
+DJCi200.wheelTouchShift = function (channel, control, value, status, group) {
+    var deck = channel - 3;
+    // We always enable scratching regardless of button state.
+    if (value > 0) {
+        DJCi200._scratchEnable(deck);
+        DJCi200.scratchAction[deck] = DJCi200.kScratchActionSeek;
+
     } else {
         // Released the wheel.
-        engine.scratchDisable(2);
+        engine.scratchDisable(deck);
+        DJCi200.scratchAction[deck] = DJCi200.kScratchActionNone;
+
     }
 };
 
 
+// Scratching on the jog wheel (rotating it while pressing the top surface)
+DJCi200._scratchWheelImpl = function (deck, value) {
+    var interval = DJCi200._convertWheelRotation(value);
+    var scratchAction = DJCi200.scratchAction[deck];
 
-// Using the top of wheel for scratching (Vinyl button On) and bending (Vinyl button Off)
-DJCi200.scratchWheelDA = function(channel, control, value, status, group) {
 
-    var newValue;
-    if (value < 64) {
-        newValue = value;
+
+    if (scratchAction === DJCi200.kScratchActionScratch) {
+        engine.scratchTick(deck, interval * DJCi200.scratchScale);
+    } else if (scratchAction === DJCi200.kScratchActionSeek) {
+        engine.scratchTick(deck,
+            interval *  DJCi200.scratchScale *
+            DJCi200.scratchShiftMultiplier);
     } else {
-        newValue = value - 128;
-    }
+        DJCi200._bendWheelImpl(deck, value);
 
-    if (engine.isScratching(1)) {
-        engine.scratchTick(1, newValue); // Scratch!
-    } else {
-        engine.setValue('[Channel' + 1 + ']', 'jog', newValue); // Pitch bend
-    }
-};
-
-DJCi200.scratchWheelDB = function(channel, control, value, status, group) {
-
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-
-    // In either case, register the movement
-    if (engine.isScratching(2)) {
-        engine.scratchTick(2, newValue); // Scratch!
-    } else {
-        engine.setValue('[Channel' + 2 + ']', 'jog', newValue); // Pitch bend
-    }
-};
-
-// Using the side of wheel for the bending
-DJCi200.bendWheelDA = function(channel, control, value, status, group) {
-
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-
-    {
-        engine.setValue('[Channel' + 1 + ']', 'jog', newValue); // Pitch bend
     }
 };
 
 
-// The wheel that actually controls the bending
-DJCi200.bendWheelDB = function(channel, control, value, status, group) {
-
-    // A: For a control that centers on 0:
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-
-    {
-        engine.setValue('[Channel' + 2 + ']', 'jog', newValue); // Pitch bend
-    }
+// Scratching on the jog wheel (rotating it while pressing the top surface)
+DJCi200.scratchWheel = function (channel, control, value, status, group) {
+    var deck = channel;
+    DJCi200._scratchWheelImpl(deck, value);
 };
 
-// FX Pad #4 setting for scratching
-DJCi200.scratchPadDA = function(channel, control, value, status, group) {
-
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-
-    if (engine.isScratching(1)) {
-        engine.scratchTick(1, newValue); // Scratch!
-    }
+// Seeking on the jog wheel (rotating it while pressing the top surface and holding Shift)
+DJCi200.scratchWheelShift = function (channel, control, value, status, group) {
+    var deck = channel - 3;
+    DJCi200._scratchWheelImpl(deck, value);
 
 };
 
-DJCi200.scratchPadDB = function(channel, control, value, status, group) {
 
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
+DJCi200._bendWheelImpl = function (deck, value) {
+    var interval = DJCi200._convertWheelRotation(value);
+    engine.setValue('[Channel' + deck + ']', 'jog',
+        interval * DJCi200.bendScale);
+};
 
-    // In either case, register the movement
-    if (engine.isScratching(2)) {
-        engine.scratchTick(2, newValue); // Scratch!
-    }
+// Bending on the jog wheel (rotating using the edge)
+DJCi200.bendWheel = function (channel, control, value, status, group) {
+    var deck = channel;
+    DJCi200._bendWheelImpl(deck, value);
+};
+
+DJCi200.scratchPad = function (channel, control, value, status, group) {
+    var deck = channel;
+    DJCi200._scratchWheelImpl(deck, value);
 };
 
 
-
-DJCi200.shutdown = function() {
+DJCi200.shutdown = function () {
 
     midi.sendShortMsg(0xB0, 0x7F, 0x00);
 };
