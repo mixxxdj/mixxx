17b5c476515fff6fef831782afaf4204775efd10
ronso0 <ronso0@mixxx.org>
2020-01-21 11:27:56 +0100

    update 'pressed' also for pushbuttons with 1+ states via onConnectedControlChanged

--- src/widget/wpushbutton.cpp
+++ src/widget/wpushbutton.cpp
@@ -290,9 +290,7 @@ void WPushButton::onConnectedControlChanged(double dParameter, double dValue) {
     Q_UNUSED(dParameter);
     // Enums are not currently represented using parameter space so it doesn't
     // make sense to use the parameter here yet.
-    if (m_iNoStates == 1) {
-        m_bPressed = (dValue == 1.0);
-    }
+    m_bPressed = (dValue == 1.0);
 
     restyleAndRepaint();
 }
