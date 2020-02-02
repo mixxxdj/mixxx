ec4c3ad84900e9b645cdd62bddbae2b989443fd2
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-02-03 00:56:57 +0100

    mixer/basetrackplayer: Remove obsolete passthrough enabled handling

--- src/mixer/basetrackplayer.h
+++ src/mixer/basetrackplayer.h
@@ -78,7 +78,6 @@ class BaseTrackPlayerImpl : public BaseTrackPlayer {
     void slotPlayToggled(double);
 
   private slots:
-    void slotPassthroughEnabled(double v);
     void slotVinylControlEnabled(double v);
     void slotWaveformZoomValueChangeRequest(double pressed);
     void slotWaveformZoomUp(double pressed);
@@ -129,7 +128,6 @@ class BaseTrackPlayerImpl : public BaseTrackPlayer {
     std::unique_ptr<ControlProxy> m_pRateSlider;
     std::unique_ptr<ControlProxy> m_pPitchAdjust;
     std::unique_ptr<ControlProxy> m_pInputConfigured;
-    std::unique_ptr<ControlProxy> m_pPassthroughEnabled;
     std::unique_ptr<ControlProxy> m_pVinylControlEnabled;
     std::unique_ptr<ControlProxy> m_pVinylControlStatus;
 };
