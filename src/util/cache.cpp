2ebaedeb5f28c91bd8efb81061c0222b2f5dfdca
Uwe Klotz <uklotz@mixxx.org>
2020-02-12 09:04:33 +0100

    Add reasoning for cache key calculation

--- src/util/cache.cpp
+++ src/util/cache.cpp
@@ -10,6 +10,12 @@ cache_key_t calculateCacheKey(const QByteArray& bytes) {
     if (bytes.isEmpty()) {
         return key;
     }
+    // We should not make any assumptions about the significance of
+    // the given bytes, even if they are supposed to be the result of
+    // applying a hash function. Instead of truncating the information
+    // by using only the first sizeof(cache_key_t) bytes we slice the
+    // array into groups of sizeof(cache_key_t) bytes and combine them
+    // with XOR.
     size_t leftShiftBytes = 0;
     for (const auto nextByte : bytes) {
         // Only 8 bits are relevant and we don't want the sign
