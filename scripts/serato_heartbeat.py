fd8af8d38548afe21008625fb64822aec936a0d0
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-02-13 21:46:48 +0100

    scripts/serato_heartbeat: Fix small formatting issue
    When merging master into a feature branch, this can causes problems with
    black.

--- scripts/serato_heartbeat.py
+++ scripts/serato_heartbeat.py
@@ -98,7 +98,7 @@ def main(argv=None):
         epilog=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("port", metavar='PORT', help="MIDI output port name")
+    parser.add_argument("port", metavar="PORT", help="MIDI output port name")
     parser.add_argument("-f", "--xmlfile", help="Mixxx XML file")
     args = parser.parse_args(argv)
 
