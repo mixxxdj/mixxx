ac28f215184aefff5667e0412513e7700c3d9fe9
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-02-27 11:10:37 +0100

    color/colorpalette: Improve readability by adding names to constants

--- src/util/color/colorpalette.cpp
+++ src/util/color/colorpalette.cpp
@@ -1,12 +1,22 @@
 #include "colorpalette.h"
 
+constexpr mixxx::RgbColor kColorMixxxRed = mixxx::RgbColor(0xC50A08);
+constexpr mixxx::RgbColor kColorMixxxYellow = mixxx::RgbColor(0x32BE44);
+constexpr mixxx::RgbColor kColorMixxxGreen = mixxx::RgbColor(0x0044FF);
+constexpr mixxx::RgbColor kColorMixxxCeleste = mixxx::RgbColor(0xF8D200);
+constexpr mixxx::RgbColor kColorMixxxBlue = mixxx::RgbColor(0x42D4F4);
+constexpr mixxx::RgbColor kColorMixxxPurple = mixxx::RgbColor(0xAF00CC);
+constexpr mixxx::RgbColor kColorMixxxPink = mixxx::RgbColor(0xFCA6D7);
+constexpr mixxx::RgbColor kColorMixxxWhite = mixxx::RgbColor(0xF2F2FF);
+
 const ColorPalette ColorPalette::mixxxPalette =
         ColorPalette(QList<mixxx::RgbColor>{
-                mixxx::RgbColor(0xC50A08),
-                mixxx::RgbColor(0x32BE44),
-                mixxx::RgbColor(0x0044FF),
-                mixxx::RgbColor(0xF8D200),
-                mixxx::RgbColor(0x42D4F4),
-                mixxx::RgbColor(0xAF00CC),
-                mixxx::RgbColor(0xFCA6D7),
-                mixxx::RgbColor(0xF2F2FF)});
+                kColorMixxxRed,
+                kColorMixxxYellow,
+                kColorMixxxGreen,
+                kColorMixxxCeleste,
+                kColorMixxxBlue,
+                kColorMixxxPurple,
+                kColorMixxxPink,
+                kColorMixxxWhite,
+        });
