abea66910e8d296e247ba5c03170b01743359483
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-03-06 14:09:38 +0100

    util/color/colorpalette: Use shorter initialization for color constexprs

--- src/util/color/colorpalette.cpp
+++ src/util/color/colorpalette.cpp
@@ -1,14 +1,14 @@
 #include "colorpalette.h"
 
 namespace {
-constexpr mixxx::RgbColor kColorMixxxRed = mixxx::RgbColor(0xC50A08);
-constexpr mixxx::RgbColor kColorMixxxYellow = mixxx::RgbColor(0x32BE44);
-constexpr mixxx::RgbColor kColorMixxxGreen = mixxx::RgbColor(0x0044FF);
-constexpr mixxx::RgbColor kColorMixxxCeleste = mixxx::RgbColor(0xF8D200);
-constexpr mixxx::RgbColor kColorMixxxBlue = mixxx::RgbColor(0x42D4F4);
-constexpr mixxx::RgbColor kColorMixxxPurple = mixxx::RgbColor(0xAF00CC);
-constexpr mixxx::RgbColor kColorMixxxPink = mixxx::RgbColor(0xFCA6D7);
-constexpr mixxx::RgbColor kColorMixxxWhite = mixxx::RgbColor(0xF2F2FF);
+constexpr mixxx::RgbColor kColorMixxxRed(0xC50A08);
+constexpr mixxx::RgbColor kColorMixxxYellow(0x32BE44);
+constexpr mixxx::RgbColor kColorMixxxGreen(0x0044FF);
+constexpr mixxx::RgbColor kColorMixxxCeleste(0xF8D200);
+constexpr mixxx::RgbColor kColorMixxxBlue(0x42D4F4);
+constexpr mixxx::RgbColor kColorMixxxPurple(0xAF00CC);
+constexpr mixxx::RgbColor kColorMixxxPink(0xFCA6D7);
+constexpr mixxx::RgbColor kColorMixxxWhite(0xF2F2FF);
 } // anonymous namespace
 
 const ColorPalette ColorPalette::mixxxHotcuePalette =
