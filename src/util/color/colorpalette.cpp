5cda96df3d793d53aa9f47a349390dbcae54d0e5
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-03-06 13:59:06 +0100

    Use orange (not in palette) for "no color" schema migration and new cues

--- src/util/color/colorpalette.cpp
+++ src/util/color/colorpalette.cpp
@@ -1,6 +1,7 @@
 #include "colorpalette.h"
 
 namespace {
+
 constexpr mixxx::RgbColor kColorMixxxRed(0xC50A08);
 constexpr mixxx::RgbColor kColorMixxxYellow(0x32BE44);
 constexpr mixxx::RgbColor kColorMixxxGreen(0x0044FF);
@@ -9,6 +10,11 @@ constexpr mixxx::RgbColor kColorMixxxBlue(0x42D4F4);
 constexpr mixxx::RgbColor kColorMixxxPurple(0xAF00CC);
 constexpr mixxx::RgbColor kColorMixxxPink(0xFCA6D7);
 constexpr mixxx::RgbColor kColorMixxxWhite(0xF2F2FF);
+
+// Replaces "no color" values and is used for new cues if auto_hotcue_colors is
+// disabled
+constexpr mixxx::RgbColor kSchemaMigrationReplacementColor(0xFF8000);
+
 } // anonymous namespace
 
 const ColorPalette ColorPalette::mixxxHotcuePalette =
@@ -25,7 +31,7 @@ const ColorPalette ColorPalette::mixxxHotcuePalette =
                         kColorMixxxWhite,
                 });
 
-const mixxx::RgbColor ColorPalette::kDefaultCueColor = kColorMixxxWhite;
+const mixxx::RgbColor ColorPalette::kDefaultCueColor = kSchemaMigrationReplacementColor;
 
 mixxx::RgbColor ColorPalette::nextColor(mixxx::RgbColor color) const {
     //  Return first color if color not in palette ((-1 + 1) % size)
