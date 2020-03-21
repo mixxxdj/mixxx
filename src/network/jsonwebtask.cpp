0d80a2d8d3b32535d316996ab007bb4852a8bbfe
Uwe Klotz <uklotz@mixxx.org>
2020-03-21 01:51:07 +0100

    Remove all workarounds for Qt <5.9.5

--- src/network/jsonwebtask.cpp
+++ src/network/jsonwebtask.cpp
@@ -1,8 +1,5 @@
 #include "network/jsonwebtask.h"
 
-#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
-#include <QBuffer>
-#endif
 #include <QMetaMethod>
 #include <QMimeDatabase>
 #include <QNetworkRequest>
@@ -72,9 +69,7 @@ QNetworkRequest newRequest(
         const QUrl& url) {
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, JSON_CONTENT_TYPE);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     request.setAttribute(QNetworkRequest::FollowRedirectsAttribute, true);
-#endif
     return request;
 }
 
@@ -171,19 +166,10 @@ QNetworkReply* JsonWebTask::sendNetworkRequest(
                     << url
                     << body;
         }
-#if QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)
         return networkAccessManager->sendCustomRequest(
                 newRequest(url),
                 "PATCH",
                 body);
-#else
-        auto* buffer = new QBuffer(this);
-        buffer->setData(body);
-        return networkAccessManager->sendCustomRequest(
-                newRequest(url),
-                "PATCH",
-                buffer);
-#endif
     }
     case HttpRequestMethod::Delete: {
         DEBUG_ASSERT(content.isEmpty());
