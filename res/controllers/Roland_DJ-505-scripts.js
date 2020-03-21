f32b5f0a0ecb5d5df3c95d713c4650861a19fd94
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-03-21 11:32:33 +0100

    Roland DJ-505: Add support for setting sync_master control via SYNC button

--- res/controllers/Roland_DJ-505-scripts.js
+++ res/controllers/Roland_DJ-505-scripts.js
@@ -584,8 +584,12 @@ DJ505.Deck = function(deckNumbers, offset) {
     this.sync = new components.Button({
         midi: [0x90 + offset, 0x02],
         group: "[Channel" + deckNumbers + "]",
-        outKey: "sync_enabled",
+        outKey: "sync_mode",
+        flickerState: false,
         output: function(value, _group, _control) {
+            if (value === 2) {
+                value = this.flickerState;
+            }
             midi.sendShortMsg(this.midi[0], value ? 0x02 : 0x03, this.on);
         },
         input: function(channel, control, value, _status, _group) {
@@ -606,7 +610,11 @@ DJ505.Deck = function(deckNumbers, offset) {
                 script.triggerControl(this.group, "beatsync", 1);
             };
             this.onLongPress = function() {
-                engine.setValue(this.group, "sync_enabled", 1);
+                if (engine.getValue(this.group, "sync_enabled")) {
+                    script.toggleControl(this.group, "sync_master");
+                } else {
+                    engine.setValue(this.group, "sync_enabled", 1);
+                }
             };
         },
         shift: function() {
@@ -616,6 +624,20 @@ DJ505.Deck = function(deckNumbers, offset) {
             this.onLongPress = function() {
                 script.toggleControl(this.group, "quantize");
             };
+        },
+        connect: function() {
+            components.Button.prototype.connect.call(this); // call parent connect
+            this.flickerTimer = engine.beginTimer(500, function() {
+                this.flickerState = !this.flickerState;
+                this.trigger();
+            });
+        },
+        disconnect: function() {
+            components.Button.prototype.disconnect.call(this); // call parent disconnect
+            if (this.flickerTimer) {
+                engine.stopTimer(this.flickerTimer);
+                this.flickerTimer = 0;
+            }
         },
     });
 
