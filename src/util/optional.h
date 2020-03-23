5877f7194662522ebca8bd9a165a00078e600326
Uwe Klotz <uklotz@mixxx.org>
2020-03-23 14:06:54 +0100

    Remove std::optional workarounds

--- src/util/optional.h
+++ src/util/optional.h
@@ -1,24 +1,14 @@
 #pragma once
 
-#if __has_include(<optional>) || \
-    (defined(__cpp_lib_optional) && __cpp_lib_optional >= 201606L)
-
 #include <optional>
 
-#else
+#include <QtDebug>
 
-#include <experimental/optional>
-
-namespace std {
-
-using std::experimental::make_optional;
-using std::experimental::nullopt;
-using std::experimental::optional;
-
-// Workarounds for missing member functions:
-// option::has_value() -> explicit operator bool()
-// option::value() -> operator*()
-
-} // namespace std
-
-#endif
+template<typename T>
+QDebug operator<<(QDebug dbg, std::optional<T> arg) {
+    if (arg) {
+        return dbg << *arg;
+    } else {
+        return dbg << "nullopt";
+    }
+}
