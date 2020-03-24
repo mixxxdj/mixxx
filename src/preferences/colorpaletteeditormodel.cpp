04ed4681ccfe11de082a57964641d58eb2fdf85c
Daniel Schürmann <daschuer@mixxx.org>
2020-03-24 22:03:33 +0100

    Revert "Fill table with assigned cue numbers if no cue numbers are set"
    This reverts commit 30aaea8a51e98dd242848c0a46f12f53c59c3c65.

--- src/preferences/colorpaletteeditormodel.cpp
+++ src/preferences/colorpaletteeditormodel.cpp
@@ -103,15 +103,9 @@ void ColorPaletteEditorModel::setColorPalette(const ColorPalette& palette) {
     // Make a map of hotcue indices
     QMap<int, int> hotcueColorIndicesMap;
     QList<unsigned int> hotcueColorIndices = palette.getHotcueIndices();
-    if (hotcueColorIndices.size()) {
-        for (int i = 0; i < hotcueColorIndices.size(); i++) {
-            int colorIndex = hotcueColorIndices.at(i);
-            hotcueColorIndicesMap.insert(colorIndex, i);
-        }
-    } else {
-        for (int i = 0; i < palette.size(); i++) {
-            hotcueColorIndicesMap.insert(i, i);
-        }
+    for (int i = 0; i < hotcueColorIndices.size(); i++) {
+        int colorIndex = hotcueColorIndices.at(i);
+        hotcueColorIndicesMap.insert(colorIndex, i);
     }
 
     for (int i = 0; i < palette.size(); i++) {
