864f7f85d7b81e32cac8b1468e1bf5ad10ca23f6
Daniel Schürmann <daschuer@mixxx.org>
2020-03-29 20:58:30 +0200

    Extract threshold parameters and fix to format multible long lines at once.

--- scripts/line_length.py
+++ scripts/line_length.py
@@ -5,6 +5,12 @@ from typing import Optional
 from typing import Sequence
 from subprocess import call
 
+# We recommend a maximum line length of 80, but do allow up to 100 characters
+# if deemed necessary by the developer. Lines that exceed that limit will
+# be wrapped after 80 characters automatically.
+lineLengthTheshold = 100
+breakBefore = 80
+
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
@@ -12,44 +18,47 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     args = parser.parse_args(argv)
 
     for filename in args.filenames:
+        lineArguments = []
         with open(filename) as fd:
             for lineno, line in enumerate(fd):
-                if len(line) > 100:
+                if len(line) <= lineLengthTheshold:
                     continue
                 humanlineno = lineno + 1
                 print(f"{filename}:{humanlineno} Line is too long.")
-                call(
-                    [
-                        "clang-format",
-                        "-i",
-                        f"-lines={humanlineno}:{humanlineno}",
-                        "-style={"
-                        "BasedOnStyle: Google, "
-                        "IndentWidth: 4, "
-                        "TabWidth: 8, "
-                        "UseTab: Never, "
-                        "ColumnLimit: 80, "
-                        # clang-format normally unbreaks all short lines,
-                        # which is undesired.
-                        # This does not happen here because line is too long
-                        "AccessModifierOffset: -2, "
-                        "AlignAfterOpenBracket: DontAlign, "
-                        "AlignOperands: false, "
-                        "AllowShortFunctionsOnASingleLine: None, "
-                        "AllowShortIfStatementsOnASingleLine: false, "
-                        "AllowShortLoopsOnASingleLine: false, "
-                        "BinPackArguments: false, "
-                        "BinPackParameters: false, "
-                        "ConstructorInitializerIndentWidth: 8, "
-                        "ContinuationIndentWidth: 8, "
-                        "IndentCaseLabels: false, "
-                        "DerivePointerAlignment: false, "
-                        "ReflowComments: false, "
-                        "SpaceAfterTemplateKeyword: false, "
-                        "SpacesBeforeTrailingComments: 1}",
-                        f"{filename}",
-                    ]
-                )
+                lineArguments += [f"-lines={humanlineno}:{humanlineno}"]
+        if len(lineArguments) > 0:
+            call(
+                ["clang-format"]
+                + lineArguments
+                + [
+                    "-i",
+                    "-style={"
+                    "BasedOnStyle: Google, "
+                    "IndentWidth: 4, "
+                    "TabWidth: 8, "
+                    "UseTab: Never,"
+                    " " + f"ColumnLimit: {breakBefore}, "
+                    # clang-format normally unbreaks all short lines,
+                    # which is undesired.
+                    # This does not happen here because line is too long
+                    + "AccessModifierOffset: -2, "
+                    "AlignAfterOpenBracket: DontAlign, "
+                    "AlignOperands: false, "
+                    "AllowShortFunctionsOnASingleLine: None, "
+                    "AllowShortIfStatementsOnASingleLine: false, "
+                    "AllowShortLoopsOnASingleLine: false, "
+                    "BinPackArguments: false, "
+                    "BinPackParameters: false, "
+                    "ConstructorInitializerIndentWidth: 8, "
+                    "ContinuationIndentWidth: 8, "
+                    "IndentCaseLabels: false, "
+                    "DerivePointerAlignment: false, "
+                    "ReflowComments: false, "
+                    "SpaceAfterTemplateKeyword: false, "
+                    "SpacesBeforeTrailingComments: 1}",
+                    f"{filename}",
+                ]
+            )
     return 0
 
 
