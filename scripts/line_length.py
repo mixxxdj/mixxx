fbcc980765d5b2a2cae17782b3383d113cb79bb9
Daniel Schürmann <daschuer@mixxx.org>
2020-03-29 23:27:23 +0200

    Use capital style with underscores for the Python constants

--- scripts/line_length.py
+++ scripts/line_length.py
@@ -8,8 +8,8 @@ from subprocess import call
 # We recommend a maximum line length of 80, but do allow up to 100 characters
 # if deemed necessary by the developer. Lines that exceed that limit will
 # be wrapped after 80 characters automatically.
-lineLengthTheshold = 100
-breakBefore = 80
+LINE_LENGTH_THRESHOLD = 100
+BREAK_BEFORE = 80
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -21,7 +21,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         lineArguments = []
         with open(filename) as fd:
             for lineno, line in enumerate(fd):
-                if len(line) <= lineLengthTheshold:
+                if len(line) <= LINE_LENGTH_THRESHOLD:
                     continue
                 humanlineno = lineno + 1
                 print(f"{filename}:{humanlineno} Line is too long.")
@@ -37,7 +37,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
                     "IndentWidth: 4, "
                     "TabWidth: 8, "
                     "UseTab: Never,"
-                    " " + f"ColumnLimit: {breakBefore}, "
+                    " " + f"ColumnLimit: {BREAK_BEFORE}, "
                     # clang-format normally unbreaks all short lines,
                     # which is undesired.
                     # This does not happen here because line is too long
