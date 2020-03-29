61b52e64a7946e17948819dc6d3c79b7fb3b8971
Daniel Schürmann <daschuer@mixxx.org>
2020-03-29 13:43:06 +0200

    Avoid intermediate variable

--- scripts/line_length.py
+++ scripts/line_length.py
@@ -18,37 +18,38 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
                     continue
                 humanlineno = lineno + 1
                 print(f"{filename}:{humanlineno} Line is too long.")
-                lc = [
-                    "clang-format",
-                    "-i",
-                    f"-lines={humanlineno}:{humanlineno}",
-                    "-style={"
-                    "BasedOnStyle: Google, "
-                    "IndentWidth: 4, "
-                    "TabWidth: 8, "
-                    "UseTab: Never, "
-                    "ColumnLimit: 80, "
-                    # clang-format normally unbreaks all short lines,
-                    # which is undesired.
-                    # This does not happen here because line is too long
-                    "AccessModifierOffset: -2, "
-                    "AlignAfterOpenBracket: DontAlign, "
-                    "AlignOperands: false, "
-                    "AllowShortFunctionsOnASingleLine: None, "
-                    "AllowShortIfStatementsOnASingleLine: false, "
-                    "AllowShortLoopsOnASingleLine: false, "
-                    "BinPackArguments: false, "
-                    "BinPackParameters: false, "
-                    "ConstructorInitializerIndentWidth: 8, "
-                    "ContinuationIndentWidth: 8, "
-                    "IndentCaseLabels: false, "
-                    "DerivePointerAlignment: false, "
-                    "ReflowComments: false, "
-                    "SpaceAfterTemplateKeyword: false, "
-                    "SpacesBeforeTrailingComments: 1}",
-                    f"{filename}",
-                ]
-                call(lc)
+                call(
+                    [
+                        "clang-format",
+                        "-i",
+                        f"-lines={humanlineno}:{humanlineno}",
+                        "-style={"
+                        "BasedOnStyle: Google, "
+                        "IndentWidth: 4, "
+                        "TabWidth: 8, "
+                        "UseTab: Never, "
+                        "ColumnLimit: 80, "
+                        # clang-format normally unbreaks all short lines,
+                        # which is undesired.
+                        # This does not happen here because line is too long
+                        "AccessModifierOffset: -2, "
+                        "AlignAfterOpenBracket: DontAlign, "
+                        "AlignOperands: false, "
+                        "AllowShortFunctionsOnASingleLine: None, "
+                        "AllowShortIfStatementsOnASingleLine: false, "
+                        "AllowShortLoopsOnASingleLine: false, "
+                        "BinPackArguments: false, "
+                        "BinPackParameters: false, "
+                        "ConstructorInitializerIndentWidth: 8, "
+                        "ContinuationIndentWidth: 8, "
+                        "IndentCaseLabels: false, "
+                        "DerivePointerAlignment: false, "
+                        "ReflowComments: false, "
+                        "SpaceAfterTemplateKeyword: false, "
+                        "SpacesBeforeTrailingComments: 1}",
+                        f"{filename}",
+                    ]
+                )
     return 0
 
 
