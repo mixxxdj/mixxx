a5db490534cd4aa35cc97a352de27df963c4774c
Daniel Schürmann <daschuer@mixxx.org>
2020-03-29 13:30:55 +0200

    fix line length to the desired 100 and reduce indentation

--- scripts/line-length.py
+++ scripts/line-length.py
@@ -14,40 +14,41 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     for filename in args.filenames:
         with open(filename) as fd:
             for lineno, line in enumerate(fd):
-                if len(line) > 110:
-                    humanlineno = lineno + 1
-                    print(f"{filename}:{humanlineno} Line is too long.")
-                    lc = [
-                        "clang-format",
-                        "-i",
-                        f"-lines={humanlineno}:{humanlineno}",
-                        "-style={"
-                        "BasedOnStyle: Google, "
-                        "IndentWidth: 4, "
-                        "TabWidth: 8, "
-                        "UseTab: Never, "
-                        "ColumnLimit: 80, "
-                        # clang-format normally unbreaks all short lines,
-                        # which is undesired.
-                        # This does not happen here because line is too long
-                        "AccessModifierOffset: -2, "
-                        "AlignAfterOpenBracket: DontAlign, "
-                        "AlignOperands: false, "
-                        "AllowShortFunctionsOnASingleLine: None, "
-                        "AllowShortIfStatementsOnASingleLine: false, "
-                        "AllowShortLoopsOnASingleLine: false, "
-                        "BinPackArguments: false, "
-                        "BinPackParameters: false, "
-                        "ConstructorInitializerIndentWidth: 8, "
-                        "ContinuationIndentWidth: 8, "
-                        "IndentCaseLabels: false, "
-                        "DerivePointerAlignment: false, "
-                        "ReflowComments: false, "
-                        "SpaceAfterTemplateKeyword: false, "
-                        "SpacesBeforeTrailingComments: 1}",
-                        f"{filename}",
-                    ]
-                    call(lc)
+                if len(line) > 100:
+                    continue
+                humanlineno = lineno + 1
+                print(f"{filename}:{humanlineno} Line is too long.")
+                lc = [
+                    "clang-format",
+                    "-i",
+                    f"-lines={humanlineno}:{humanlineno}",
+                    "-style={"
+                    "BasedOnStyle: Google, "
+                    "IndentWidth: 4, "
+                    "TabWidth: 8, "
+                    "UseTab: Never, "
+                    "ColumnLimit: 80, "
+                    # clang-format normally unbreaks all short lines,
+                    # which is undesired.
+                    # This does not happen here because line is too long
+                    "AccessModifierOffset: -2, "
+                    "AlignAfterOpenBracket: DontAlign, "
+                    "AlignOperands: false, "
+                    "AllowShortFunctionsOnASingleLine: None, "
+                    "AllowShortIfStatementsOnASingleLine: false, "
+                    "AllowShortLoopsOnASingleLine: false, "
+                    "BinPackArguments: false, "
+                    "BinPackParameters: false, "
+                    "ConstructorInitializerIndentWidth: 8, "
+                    "ContinuationIndentWidth: 8, "
+                    "IndentCaseLabels: false, "
+                    "DerivePointerAlignment: false, "
+                    "ReflowComments: false, "
+                    "SpaceAfterTemplateKeyword: false, "
+                    "SpacesBeforeTrailingComments: 1}",
+                    f"{filename}",
+                ]
+                call(lc)
     return 0
 
 
