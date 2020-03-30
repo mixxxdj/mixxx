5c2880233f7f1520c8717e79ceec59d969848c19
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-03-30 11:42:10 +0200

    scripts/line_length: Use temporary config file

--- scripts/line_length.py
+++ scripts/line_length.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
+import re
+import os
+import subprocess
+import tempfile
 from typing import Optional
 from typing import Sequence
-from subprocess import call
 
 # We recommend a maximum line length of 80, but do allow up to 100 characters
 # if deemed necessary by the developer. Lines that exceed that limit will
@@ -17,48 +20,53 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument("filenames", nargs="*", help="Filenames to check")
     args = parser.parse_args(argv)
 
-    for filename in args.filenames:
-        lineArguments = []
-        with open(filename) as fd:
-            for lineno, line in enumerate(fd):
-                if len(line) <= LINE_LENGTH_THRESHOLD:
-                    continue
-                humanlineno = lineno + 1
-                print(f"{filename}:{humanlineno} Line is too long.")
-                lineArguments += [f"-lines={humanlineno}:{humanlineno}"]
-        if len(lineArguments) > 0:
-            call(
-                ["clang-format"]
-                + lineArguments
-                + [
-                    "-i",
-                    "-style={"
-                    "BasedOnStyle: Google, "
-                    "IndentWidth: 4, "
-                    "TabWidth: 8, "
-                    "UseTab: Never,"
-                    " " + f"ColumnLimit: {BREAK_BEFORE}, "
-                    # clang-format normally unbreaks all short lines,
-                    # which is undesired.
-                    # This does not happen here because line is too long
-                    + "AccessModifierOffset: -2, "
-                    "AlignAfterOpenBracket: DontAlign, "
-                    "AlignOperands: false, "
-                    "AllowShortFunctionsOnASingleLine: None, "
-                    "AllowShortIfStatementsOnASingleLine: false, "
-                    "AllowShortLoopsOnASingleLine: false, "
-                    "BinPackArguments: false, "
-                    "BinPackParameters: false, "
-                    "ConstructorInitializerIndentWidth: 8, "
-                    "ContinuationIndentWidth: 8, "
-                    "IndentCaseLabels: false, "
-                    "DerivePointerAlignment: false, "
-                    "ReflowComments: false, "
-                    "SpaceAfterTemplateKeyword: false, "
-                    "SpacesBeforeTrailingComments: 1}",
-                    f"{filename}",
-                ]
+    proc = subprocess.run(
+        ["clang-format", "--dump-config"], capture_output=True, text=True
+    )
+    proc.check_returncode()
+
+    with tempfile.TemporaryDirectory(prefix="clang-format") as tempdir:
+        # Create temporary config with ColumnLimit enabled
+        configfile = os.path.join(tempdir, ".clang-format")
+        with open(configfile, mode="w") as configfp:
+            configfp.write(
+                re.sub(
+                    r"(ColumnLimit:\s*)\d+",
+                    r"\g<1>{}".format(BREAK_BEFORE),
+                    proc.stdout,
+                )
             )
+
+        for filename in args.filenames:
+            lineArguments = []
+            with open(filename) as fd:
+                for lineno, line in enumerate(fd):
+                    if len(line) <= LINE_LENGTH_THRESHOLD:
+                        continue
+                    humanlineno = lineno + 1
+                    print(f"{filename}:{humanlineno} Line is too long.")
+                    lineArguments += [f"-lines={humanlineno}:{humanlineno}"]
+
+                if not lineArguments:
+                    continue
+
+                fd.seek(0)
+                cmd = [
+                    "clang-format",
+                    "--style=file",
+                    "--assume-filename={}".format(
+                        os.path.join(tempdir, "file.cpp")
+                    ),
+                    *lineArguments,
+                ]
+                proc = subprocess.run(
+                    cmd, stdin=fd, capture_output=True, text=True
+                )
+                proc.check_returncode()
+                print(proc.stderr)
+
+            with open(filename, mode="w+") as fp:
+                fp.write(proc.stdout)
     return 0
 
 
