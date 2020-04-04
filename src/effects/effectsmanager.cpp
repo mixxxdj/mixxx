08d48f0acedddc24ac484322f3f7cf5561fccd6b
Be <be@mixxx.org>
2020-04-04 16:22:25 -0500

    move EffectsBackends out of MixxxMainWindow into EffectsManager
    This was an ugly hack required by DlgPrefLV2 which was removed
    in 36c8f18a839e2b55e05cb13887a2ec8cb8e853bf

--- src/effects/effectsmanager.cpp
+++ src/effects/effectsmanager.cpp
@@ -5,7 +5,8 @@
 #include <QMetaType>
 #include <algorithm>
 
-#include "effects/backends/effectsbackend.h"
+#include "effects/backends/builtin/builtinbackend.h"
+#include "effects/backends/lv2/lv2backend.h"
 #include "effects/effectslot.h"
 #include "effects/effectxmlelements.h"
 #include "effects/presets/effectchainpreset.h"
@@ -42,6 +43,11 @@ EffectsManager::EffectsManager(QObject* pParent, UserSettingsPointer pConfig, Ch
 
     m_pNumEffectsAvailable = new ControlObject(ConfigKey("[Master]", "num_effectsavailable"));
     m_pNumEffectsAvailable->setReadOnly();
+
+    addEffectsBackend(EffectsBackendPointer(new BuiltInBackend()));
+#ifdef __LILV__
+    addEffectsBackend(EffectsBackendPointer(new LV2Backend()));
+#endif
 }
 
 EffectsManager::~EffectsManager() {
