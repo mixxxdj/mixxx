b1c9724098466acd5cb42ed695c5872f16262523
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-04-05 12:13:34 +0100

    add support for seeking

--- res/controllers/Pioneer-DDJ-200-scripts.js
+++ res/controllers/Pioneer-DDJ-200-scripts.js
@@ -22,11 +22,20 @@ DDJ200.touch = function (channel, control, value, status, group) {
     var deckNumber = script.deckFromGroup(group);
     if (value == 0) {
         // disable scratch
-        engine.scratchDisable(script.deckFromGroup(group));
+        engine.scratchDisable(deckNumber);
     } else {
         // enable scratch
         var alpha = 1.0 / 8;
-        var beta = alpha / 32;
-        engine.scratchEnable(deckNumber, 128, 33 + 1 / 3, alpha, beta);
+        engine.scratchEnable(deckNumber, 128, 33 + 1 / 3, alpha, alpha / 32);
     }
+}
+
+DDJ200.seek = function (channel, control, value, status, group) {
+    var oldPos = engine.getValue(group, "playposition");
+    // Since ‘playposition’ is normalized to unity, we need to scale by
+    // song duration in order for the jog wheel to cover the same amount
+    // of time given a constant turning angle.
+    var duration = engine.getValue(group, "duration");
+    var newPos = Math.max(0, oldPos + ((value - 64) * 0.2 / duration));
+    engine.setValue(group, "playposition", newPos); // Strip search
 }
