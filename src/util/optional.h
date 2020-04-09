604977d609f384b4a6618481c332e3b246938f7b
Uwe Klotz <uklotz@mixxx.org>
2020-04-09 19:12:26 +0200

    Remove <experimental/optional> hack from util/optional.h

--- src/util/optional.h
+++ src/util/optional.h
@@ -1,27 +1,6 @@
 #pragma once
 
-#if __has_include(<optional>) || \
-    (defined(__cpp_lib_optional) && __cpp_lib_optional >= 201606L)
-
 #include <optional>
-
-#else
-
-#include <experimental/optional>
-
-namespace std {
-
-using std::experimental::make_optional;
-using std::experimental::nullopt;
-using std::experimental::optional;
-
-// Workarounds for missing member functions:
-// option::has_value() -> explicit operator bool()
-// option::value() -> operator*()
-
-} // namespace std
-
-#endif
 
 #include <QtDebug>
 
