237d621135d4258429d0543943abe9bccd8ffd79
Daniel Schürmann <daschuer@mixxx.org>
2020-05-15 11:04:18 +0200

    Make use of CMakeDebHelper to improve CPack output

---
+++ build/CPackConfig.cmake
@@ -0,0 +1,6 @@
+# unlike CMakeLists.txt this file is include at cpack time, once per generator after CPack has set CPACK_GENERATOR 
+# to the actual generator being used. It allows per-generator setting of CPACK_* variables at cpack time.
+
+if (CPACK_GENERATOR MATCHES "DEB")
+  set( CPACK_INSTALL_SCRIPT ${CPACK_DEBIAN_INSTALL_SCRIPT} )
+endif()
