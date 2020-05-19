7e0cdf4bafc47c945908bb0690ef8663ee119bfc
Uwe Klotz <uklotz@mixxx.org>
2020-05-19 22:12:14 +0200

    Revert "Use math_clamp for adjusting popup positions on screen"
    This reverts commit 98887ef348385ba8364ade9116dc84cfbfe1d559.

--- src/util/widgethelper.cpp
+++ src/util/widgethelper.cpp
@@ -14,14 +14,17 @@ QPoint mapPopupToScreen(
         const QPoint& popupUpperLeft,
         const QSize& popupSize) {
     const auto screenSize = widget.windowHandle()->screen()->size();
-    const auto adjustedX = math_clamp(
-            popupUpperLeft.x(),
-            /*min*/ 0,
-            /*max*/ screenSize.width() - popupSize.width());
-    const auto adjustedY = math_clamp(
-            popupUpperLeft.y(),
-            /*min*/ 0,
-            /*max*/ screenSize.height() - popupSize.height());
+    // math_clamp() cannot be used, because if the dimensions of
+    // the popup menu are greater than the screen size a debug
+    // assertion would be triggered!
+    const auto adjustedX = math_max(0,
+            math_min(
+                    popupUpperLeft.x(),
+                    screenSize.width() - popupSize.width()));
+    const auto adjustedY = math_max(0,
+            math_min(
+                    popupUpperLeft.y(),
+                    screenSize.height() - popupSize.height()));
     return QPoint(adjustedX, adjustedY);
 }
 
