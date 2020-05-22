a53cb1cc0f914d275f033c0b3b1d1e60f4f82233
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-05-22 12:44:41 +0200

    controllers: Fix inconsistent missing overrides for controller presets

--- src/controllers/hid/hidcontrollerpreset.h
+++ src/controllers/hid/hidcontrollerpreset.h
@@ -15,7 +15,7 @@ class HidControllerPreset : public ControllerPreset {
 
     bool savePreset(const QString& fileName) const override;
 
-    virtual void accept(ControllerPresetVisitor* visitor);
-    virtual void accept(ConstControllerPresetVisitor* visitor) const;
-    virtual bool isMappable() const;
+    virtual void accept(ControllerPresetVisitor* visitor) override;
+    virtual void accept(ConstControllerPresetVisitor* visitor) const override;
+    virtual bool isMappable() const override;
 };
