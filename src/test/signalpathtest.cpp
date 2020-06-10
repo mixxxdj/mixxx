51691b99effa9450af5267d461c920585b5b32a7
Daniel Schürmann <daschuer@mixxx.org>
2020-06-11 00:20:12 +0200

    Use QString for groups in SignalPathTest

--- src/test/signalpathtest.cpp
+++ src/test/signalpathtest.cpp
@@ -1,13 +1,13 @@
 #include "test/signalpathtest.h"
 
-const char* BaseSignalPathTest::m_sMasterGroup = "[Master]";
-const char* BaseSignalPathTest::m_sInternalClockGroup = "[InternalClock]";
+const QString BaseSignalPathTest::m_sMasterGroup = QStringLiteral("[Master]");
+const QString BaseSignalPathTest::m_sInternalClockGroup = QStringLiteral("[InternalClock]");
 // these names need to match PlayerManager::groupForDeck and friends
-const char* BaseSignalPathTest::m_sGroup1 = "[Channel1]";
-const char* BaseSignalPathTest::m_sGroup2 = "[Channel2]";
-const char* BaseSignalPathTest::m_sGroup3 = "[Channel3]";
-const char* BaseSignalPathTest::m_sPreviewGroup = "[PreviewDeck1]";
-const char* BaseSignalPathTest::m_sSamplerGroup = "[Sampler1]";
+const QString BaseSignalPathTest::m_sGroup1 = QStringLiteral("[Channel1]");
+const QString BaseSignalPathTest::m_sGroup2 = QStringLiteral("[Channel2]");
+const QString BaseSignalPathTest::m_sGroup3 = QStringLiteral("[Channel3]");
+const QString BaseSignalPathTest::m_sPreviewGroup = QStringLiteral("[PreviewDeck1]");
+const QString BaseSignalPathTest::m_sSamplerGroup = QStringLiteral("[Sampler1]");
 const double BaseSignalPathTest::kDefaultRateRange = 0.08;
 const double BaseSignalPathTest::kDefaultRateDir = 1.0;
 const double BaseSignalPathTest::kRateRangeDivisor = kDefaultRateDir * kDefaultRateRange;
