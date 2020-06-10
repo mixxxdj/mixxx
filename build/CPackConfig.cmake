5c2a4d4626e34c0e78687975b106b9263d013cf6
Daniel Schürmann <daschuer@mixxx.org>
2020-06-10 13:33:14 +0200

    fix whitespace issues

--- build/CPackConfig.cmake
+++ build/CPackConfig.cmake
@@ -1,4 +1,4 @@
-# unlike CMakeLists.txt this file is include at cpack time, once per generator after CPack has set CPACK_GENERATOR 
+# unlike CMakeLists.txt this file is include at cpack time, once per generator after CPack has set CPACK_GENERATOR
 # to the actual generator being used. It allows per-generator setting of CPACK_* variables at cpack time.
 
 if (CPACK_GENERATOR MATCHES "DEB")
