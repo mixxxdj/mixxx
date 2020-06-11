b5420dcdeef033d678f50569fdbec72f2af405ab
Harshit Maurya <hmaurya999@gmail.com>
2020-06-11 22:15:17 +0530

    enum BeatGridMode: add comment

--- src/preferences/beatgridmode.h
+++ src/preferences/beatgridmode.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// It would be preferable to use QFlags here since it will enable checks
+// like beatGridMode.testFlag(), but in order to match with QComboBox indices,
+// we will hardcode these indices which correspond to the order of choices in
+// beatGridModeComboBox in DlgPrefWaveform.
 enum BeatGridMode {
     BEATS = 0,
     BEATS_DOWNBEATS = 1
