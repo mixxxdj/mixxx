34d1f9db57c72755455c2838a262ca62f8fed7db
Harshit Maurya <hmaurya999@gmail.com>
2020-06-17 00:05:36 +0530

    Frame: Add multiply and divide operations

--- src/track/frame.h
+++ src/track/frame.h
@@ -33,16 +33,42 @@ class Frame {
         return *this;
     }
 
+    Frame& operator*=(const double arg) {
+        m_dFrame *= arg;
+        return *this;
+    }
+
+    Frame& operator/=(const double arg) {
+        m_dFrame /= arg;
+        return *this;
+    }
+
+    Frame operator++(int) {
+        Frame temp(*this);
+        m_dFrame++;
+        return temp;
+    }
+
   private:
     double m_dFrame;
 };
 
+// Frames can be added to and subracted from other frames (not by a double)
 inline const Frame operator+(const Frame& frame1, const Frame& frame2) {
     return Frame(frame1.getValue() + frame2.getValue());
 }
 
 inline const Frame operator-(const Frame& frame1, const Frame& frame2) {
     return Frame(frame1.getValue() - frame2.getValue());
+}
+
+// Frames can be multiplied or divided by a double (not by another frame)
+inline const Frame operator*(const Frame& frame1, const double dFrame2) {
+    return Frame(frame1.getValue() * dFrame2);
+}
+
+inline const Frame operator/(const Frame& frame1, const double dFrame2) {
+    return Frame(frame1.getValue() / dFrame2);
 }
 
 inline bool operator<(const Frame& frame1, const Frame& frame2) {
