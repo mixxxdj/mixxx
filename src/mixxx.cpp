5a7cb769cab0cbc29b207de95fa6bccc4353268c
xerus2000 <27jf@pm.me>
2020-06-16 12:44:44 +0200

    Clean up MixxxMainWindow includes & documentation comments

--- src/mixxx.cpp
+++ src/mixxx.cpp
@@ -1,27 +1,9 @@
-/***************************************************************************
-                          mixxx.cpp  -  description
-                             -------------------
-    begin                : Mon Feb 18 09:48:17 CET 2002
-    copyright            : (C) 2002 by Tue and Ken Haste Andersen
-    email                :
-***************************************************************************/
-
-/***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************/
-
 #include "mixxx.h"
 
 #include <QDesktopServices>
 #include <QDesktopWidget>
 #include <QFileDialog>
 #include <QGLFormat>
-#include <QGLWidget>
 #include <QGuiApplication>
 #include <QInputMethod>
 #include <QLocale>
@@ -90,9 +72,10 @@
 #endif
 
 #if defined(Q_OS_LINUX)
-#include <QtX11Extras/QX11Info>
 #include <X11/Xlib.h>
 #include <X11/Xlibint.h>
+
+#include <QtX11Extras/QX11Info>
 // Xlibint.h predates C++ and defines macros which conflict
 // with references to std::max and std::min
 #undef max
