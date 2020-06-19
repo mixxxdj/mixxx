c5cccbb720c553dc98339905f47bbdefe4bc42f8
Daniel Schürmann <daschuer@mixxx.org>
2020-06-20 01:39:32 +0200

    Use the new linearCrossfadeBuffer function to avoid restrict warnings

--- src/engine/filters/enginefilteriir.h
+++ src/engine/filters/enginefilteriir.h
@@ -62,13 +62,20 @@ class EngineFilterIIR : public EngineFilterIIRBase {
     // this is can be used instead off a final process() call before pause
     // It fades to dry or 0 according to the m_startFromDry parameter
     // it is an alternative for using pauseFillter() calls
-    void processAndPauseFilter(const CSAMPLE* pIn, CSAMPLE* pOutput,
-                       const int iBufferSize) {
+    void processAndPauseFilter(const CSAMPLE* M_RESTRICT pIn,
+            CSAMPLE* M_RESTRICT pOutput,
+            const int iBufferSize) {
         process(pIn, pOutput, iBufferSize);
-        SampleUtil::copy2WithRampingGain(pOutput,
-                pOutput, 1.0, 0,  // fade out filtered
-                pIn, 0, m_startFromDry ? 1.0 : 0,  // fade in dry if requested
-                iBufferSize);
+        if (m_startFromDry) {
+            SampleUtil::linearCrossfadeBuffersOut(
+                    pOutput, // fade out filtered
+                    pIn,     // fade in dry
+                    iBufferSize);
+        } else {
+            SampleUtil::applyRampingGain(
+                    pOutput, 1.0, 0, // fade out filtered
+                    iBufferSize);
+        }
         pauseFilterInner();
     }
 
