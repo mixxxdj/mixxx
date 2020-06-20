32e1c2b646cc7e24c532f8c01f4eef888539d3aa
xerus2000 <27jf@pm.me>
2020-06-20 13:32:00 +0200

    Macros: Use ControlProxy

--- src/macros/macromanager.cpp
+++ src/macros/macromanager.cpp
@@ -6,12 +6,9 @@ MacroManager::MacroManager(
         PlayerManager* pPlayerManager)
         : m_pConfig(pConfig) {
     qDebug() << "MacroManager init";
-    auto hotcueActivate = new ControlPushButton(ConfigKey("[Channel1]", "hotcue_1_activate"));
-    connect(hotcueActivate,
-            &ControlObject::valueChanged,
-            this,
-            &MacroManager::slotHotcueActivate,
-            Qt::DirectConnection);
+    auto hotcueActivate = new ControlProxy("[Channel1]", "hotcue_1_activate", this);
+    hotcueActivate->connectValueChanged(
+            this, &MacroManager::slotHotcueActivate, Qt::DirectConnection);
 };
 
 void MacroManager::slotHotcueActivate(double v) {
