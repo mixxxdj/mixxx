5fb06fc62110d0f2c87512272ac4b15ab89b3dba
xerus2000 <27jf@pm.me>
2020-07-03 13:02:52 +0200

    Macros: Add simple test

--- src/test/macromanager_test.cpp
+++ src/test/macromanager_test.cpp
@@ -1,4 +1,14 @@
+#include "recording/macromanager.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
 #include "mixxxtest.h"
 
 class MacroManagerTest : public MixxxTest {
 };
+
+TEST(MacrosTest, CreateMacro) {
+    auto macro = new Macro();
+    ASSERT_EQ(macro->m_length, 0);
+}
