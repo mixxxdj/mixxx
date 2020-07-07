24b0d53b0ed2415602d865778e7248fb551380ec
xerus2000 <27jf@pm.me>
2020-07-07 12:21:54 +0200

    Macros: Expand MacroManager Unit tests

--- src/test/macromanager_test.cpp
+++ src/test/macromanager_test.cpp
@@ -15,15 +15,37 @@ TEST(MacrosTest, CreateMacro) {
     ASSERT_EQ(macro->m_length, 0);
 }
 
-TEST(MacroManagerTest, claimRecording) {
-    auto handle = ChannelHandleFactory().getOrCreateHandle("");
+TEST(MacroManagerTest, ClaimRecording) {
     auto mgr = MacroManager();
-    qDebug() << "Handle" << handle.handle();
-    ASSERT_EQ(mgr.m_macroRecordingState.load(), MacroState::Disabled);
+    EXPECT_EQ(mgr.isRecordingActive(), false);
+    mgr.claimRecording();
+    EXPECT_EQ(mgr.isRecordingActive(), false);
+    mgr.m_macroRecordingState.store(MacroState::Armed);
+    mgr.claimRecording();
+    EXPECT_EQ(mgr.isRecordingActive(), true);
+}
+
+TEST(MacroManagerTest, RecordCueJump) {
+    auto mgr = MacroManager();
+    ChannelHandle handle = ChannelHandleFactory().getOrCreateHandle("");
+    EXPECT_EQ(mgr.m_macroRecordingState.load(), MacroState::Disabled);
     mgr.notifyCueJump(handle, 0, 1);
-    ASSERT_EQ(mgr.m_macroRecordingState.load(), MacroState::Disabled);
-    ASSERT_EQ(mgr.m_activeChannel, nullptr);
+    EXPECT_EQ(mgr.m_activeChannel, nullptr);
+    EXPECT_EQ(mgr.getMacro().m_length, 0);
     mgr.m_macroRecordingState.store(MacroState::Armed);
     mgr.notifyCueJump(handle, 0, 1);
-    ASSERT_EQ(mgr.m_activeChannel->handle(), handle.handle());
+    EXPECT_EQ(mgr.m_activeChannel->handle(), handle.handle());
+    EXPECT_EQ(mgr.getMacro().actions[0].position, 0);
+    EXPECT_EQ(mgr.getMacro().actions[0].target, 1);
+    EXPECT_EQ(mgr.checkOrClaimRecording(handle), true);
+}
+
+TEST(MacroManagerTest, RecordingToggleControl) {
+    auto mgr = MacroManager();
+    ControlObject::set(ConfigKey(kMacroRecordingKey, "recording_toggle"), 1);
+    EXPECT_EQ(mgr.isRecordingActive(), true);
+    ControlObject::set(ConfigKey(kMacroRecordingKey, "recording_toggle"), 0);
+    EXPECT_EQ(mgr.isRecordingActive(), true);
+    ControlObject::set(ConfigKey(kMacroRecordingKey, "recording_toggle"), 1);
+    EXPECT_EQ(mgr.isRecordingActive(), false);
 }
