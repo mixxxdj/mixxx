1b7bdb4c406506ae5b62939be0bbfea9c4afbd84
xerus2000 <27jf@pm.me>
2020-07-09 00:09:58 +0200

    SchemaManager: Add documentation comments

--- src/database/schemamanager.h
+++ src/database/schemamanager.h
@@ -1,11 +1,14 @@
-#ifndef SCHEMAMANAGER_H
-#define SCHEMAMANAGER_H
+#pragma once
 
 #include <QSqlDatabase>
 
 #include "preferences/usersettings.h"
 #include "library/dao/settingsdao.h"
 
+/// The SchemaManager reads the database schema from the schemaFile
+/// (res/schema.xml in development) and is responsible for checking
+/// compatibility as well as upgrading the database if necessary.
+/// It also caches some information about the current version in a SettingsDAO.
 class SchemaManager {
   public:
     static const QString SETTINGS_VERSION_STRING;
@@ -26,8 +29,18 @@ class SchemaManager {
         return m_currentVersion;
     }
 
+    /// Check if the current version is backwards compatible with the given targetVersion.
+    ///
+    /// If the current version has no backwards compatibility info, it is
+    /// assumed that it is only compatible with itself.
+    ///
+    /// Returns true if the targetVersion is greater or equal to the
+    /// minimum compatible version of the current schema
     bool isBackwardsCompatibleWithVersion(int targetVersion) const;
 
+    /// Tries to upgrade the database schema to the given targetVersion.
+    /// First checks for version compatibility and whether the current version
+    /// already suffices the target and potentially aborts with the appropriate Result.
     Result upgradeToSchemaVersion(
             const QString& schemaFilename,
             int targetVersion);
@@ -38,5 +51,3 @@ class SchemaManager {
 
     int m_currentVersion;
 };
-
-#endif /* SCHEMAMANAGER_H */
