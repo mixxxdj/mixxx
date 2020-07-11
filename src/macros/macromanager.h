e894c07ca358c366c79d151d217b06f8b4a0904e
xerus2000 <27jf@pm.me>
2020-07-11 13:21:10 +0200

    Macros: Move into own folder & separate Macro class
    * Move from recording into own macros folder
    * Separate Macro class into own file

--- src/macros/macromanager.h
+++ src/macros/macromanager.h
@@ -8,12 +8,11 @@
 
 #include "control/controlpushbutton.h"
 #include "engine/channelhandle.h"
+#include "macros/macro.h"
 #include "recording/recordingmanagerbase.h"
 
 namespace {
-constexpr size_t kMaxMacroSize = 1000;
 const QString kMacroRecordingKey = QStringLiteral("[MacroRecording]");
-const QLoggingCategory macros("macros");
 } // namespace
 
 enum MacroState : uint8_t {
@@ -23,63 +22,6 @@ enum MacroState : uint8_t {
     Armed,
     /// An Action is currently being recorded
     Recording,
-};
-
-/// A MacroAction is the smallest piece of a Macro.
-/// It contains a position as well as the action to be taken at that position
-/// (currently only jumps to a target position are available).
-struct MacroAction {
-    MacroAction(){};
-    MacroAction(double position, double target)
-            : position(position), target(target){};
-    /// TODO(xerus) use FramePos once https://github.com/mixxxdj/mixxx/pull/2861 is merged
-    double position;
-    double target;
-};
-
-/// A Macro stores a list of actions.
-/// The action list is pre-populated at construction so that it can be written to in real-time code.
-class Macro {
-  public:
-    MacroAction actions[kMaxMacroSize];
-
-    Macro() {
-        std::fill_n(actions, kMaxMacroSize, MacroAction());
-    }
-
-    /// Append a jump action to this Macro by assigning the next available slot.
-    /// Only called from RT.
-    void appendJump(double origin, double target) {
-        VERIFY_OR_DEBUG_ASSERT(m_length < kMaxMacroSize) {
-            return;
-        }
-        qCDebug(macros) << "Appending jump from position" << origin << "to" << target;
-        actions[m_length].position = origin;
-        actions[m_length].target = target;
-        m_length++;
-    }
-
-    /// The amount of saved Actions
-    int getLength() {
-        return m_length;
-    }
-
-    /// Clears the contents of this Macro by setting its length to 0.
-    void clear() {
-        qCDebug(macros) << "Clearing Macro";
-        m_length = 0;
-    }
-
-    /// For debugging - dump all saved actions to debug output.
-    void dump() {
-        for (size_t i = 0; i < m_length; ++i) {
-            auto action = actions[i];
-            qCDebug(macros) << "Jump from " << action.position << " to " << action.target;
-        }
-    }
-
-  private:
-    size_t m_length = 0;
 };
 
 /// The MacroManager handles the recording of Macros and the [MacroRecording] controls.
