5c0471e48c77b05332af88d69ee7080cd4d8c869
xerus2000 <27jf@pm.me>
2020-07-31 17:07:44 +0200

    Update database help message

--- src/database/mixxxdb.cpp
+++ src/database/mixxxdb.cpp
@@ -77,8 +77,8 @@ bool MixxxDb::initDatabaseSchema(
     QString upgradeToVersionFailed =
             tr("Unable to upgrade your database schema to version %1")
             .arg(QString::number(schemaVersion));
-    QString helpEmail = tr("For help with database issues contact:") + "\n" +
-                           "mixxx-devel@lists.sourceforge.net";
+    QString helpContact = tr("For help with database issues consult:") + "\n" +
+            "https://www.mixxx.org/support";
 
     switch (SchemaManager(database).upgradeToSchemaVersion(schemaFile, schemaVersion)) {
         case SchemaManager::Result::CurrentVersion:
@@ -86,29 +86,29 @@ bool MixxxDb::initDatabaseSchema(
         case SchemaManager::Result::NewerVersionBackwardsCompatible:
             return true; // done
         case SchemaManager::Result::UpgradeFailed:
-            QMessageBox::warning(
-                    0, upgradeFailed,
+            QMessageBox::warning(0,
+                    upgradeFailed,
                     upgradeToVersionFailed + "\n" +
-                    tr("Your mixxxdb.sqlite file may be corrupt.") + "\n" +
-                    tr("Try renaming it and restarting Mixxx.") + "\n" +
-                    helpEmail + "\n\n" + okToExit,
+                            tr("Your mixxxdb.sqlite file may be corrupt.") +
+                            "\n" + tr("Try renaming it and restarting Mixxx.") +
+                            "\n" + helpContact + "\n\n" + okToExit,
                     QMessageBox::Ok);
             return false; // abort
         case SchemaManager::Result::NewerVersionIncompatible:
-            QMessageBox::warning(
-                    0, upgradeFailed,
+            QMessageBox::warning(0,
+                    upgradeFailed,
                     upgradeToVersionFailed + "\n" +
-                    tr("Your mixxxdb.sqlite file was created by a newer "
-                       "version of Mixxx and is incompatible.") +
-                    "\n\n" + okToExit,
+                            tr("Your mixxxdb.sqlite file was created by a newer "
+                               "version of Mixxx and is incompatible.") +
+                            "\n\n" + okToExit,
                     QMessageBox::Ok);
             return false; // abort
         case SchemaManager::Result::SchemaError:
-            QMessageBox::warning(
-                    0, upgradeFailed,
+            QMessageBox::warning(0,
+                    upgradeFailed,
                     upgradeToVersionFailed + "\n" +
-                    tr("The database schema file is invalid.") + "\n" +
-                    helpEmail + "\n\n" + okToExit,
+                            tr("The database schema file is invalid.") + "\n" +
+                            helpContact + "\n\n" + okToExit,
                     QMessageBox::Ok);
             return false; // abort
     }
