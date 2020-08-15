ef5621937d700020aaaf48bf2eae71351317e62a
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-08-15 23:55:32 +0200

    library/dao/playlistdao: Remove superfluous assignment
    Fixes this warning:
    
        src/library/dao/playlistdao.cpp:918:13: warning: Value stored to 'conflictFound' is never read [clang-analyzer-deadcode.DeadStores]
                    conflictFound = false;
                    ^

--- src/library/dao/playlistdao.cpp
+++ src/library/dao/playlistdao.cpp
@@ -915,7 +915,6 @@ void PlaylistDAO::shuffleTracks(const int playlistId, const QList<int>& position
 
             trackBPosition = positions.at(randomShuffleSetIndex);
             trackBId = trackPositionIds.value(trackBPosition);
-            conflictFound = false;
             int trackDistance = -1;
             int playlistEnd = trackPositionIds.count();
 
