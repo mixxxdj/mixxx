25794c95117fcce8fc8755796304cff75075d284
Be <be@mixxx.org>
2020-09-01 12:58:27 -0500

    EffectXmlElements: use QStringLiteral

--- src/effects/presets/effectxmlelements.h
+++ src/effects/presets/effectxmlelements.h
@@ -1,41 +1,41 @@
 namespace EffectXml {
-const QString FileHeader("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+const QString FileHeader(QStringLiteral("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"));
 // Version history:
 // 0 (Mixxx 2.1.0): initial support for saving state of effects
 const int kXmlSchemaVersion = 0;
 
-const QString Root("MixxxEffects");
-const QString SchemaVersion("SchemaVersion");
+const QString Root(QStringLiteral("MixxxEffects"));
+const QString SchemaVersion(QStringLiteral("SchemaVersion"));
 
-const QString Rack("Rack");
-const QString RackGroup("Group");
+const QString Rack(QStringLiteral("Rack"));
+const QString RackGroup(QStringLiteral("Group"));
 
-const QString ChainPresetList("ChainPresetList");
-const QString QuickEffectList("QuickEffectPresetList");
-const QString QuickEffectChainPresets("QuickEffectChains");
-const QString ChainPresetName("ChainPresetName");
+const QString ChainPresetList(QStringLiteral("ChainPresetList"));
+const QString QuickEffectList(QStringLiteral("QuickEffectPresetList"));
+const QString QuickEffectChainPresets(QStringLiteral("QuickEffectChains"));
+const QString ChainPresetName(QStringLiteral("ChainPresetName"));
 
-const QString VisibleEffects("VisibleEffects");
+const QString VisibleEffects(QStringLiteral("VisibleEffects"));
 
-const QString ChainsRoot("Chains");
-const QString Chain("EffectChain");
-const QString ChainSuperParameter("SuperParameterValue");
-const QString ChainId("Id");
-const QString ChainName("Name");
-const QString ChainDescription("Description");
-const QString ChainMixMode("MixMode");
+const QString ChainsRoot(QStringLiteral("Chains"));
+const QString Chain(QStringLiteral("EffectChain"));
+const QString ChainSuperParameter(QStringLiteral("SuperParameterValue"));
+const QString ChainId(QStringLiteral("Id"));
+const QString ChainName(QStringLiteral("Name"));
+const QString ChainDescription(QStringLiteral("Description"));
+const QString ChainMixMode(QStringLiteral("MixMode"));
 
-const QString EffectsRoot("Effects");
-const QString Effect("Effect");
-const QString EffectMetaParameter("MetaParameterValue");
-const QString EffectId("Id");
-const QString EffectBackendType("BackendType");
+const QString EffectsRoot(QStringLiteral("Effects"));
+const QString Effect(QStringLiteral("Effect"));
+const QString EffectMetaParameter(QStringLiteral("MetaParameterValue"));
+const QString EffectId(QStringLiteral("Id"));
+const QString EffectBackendType(QStringLiteral("BackendType"));
 
-const QString ParametersRoot("Parameters");
-const QString Parameter("Parameter");
-const QString ParameterId("Id");
-const QString ParameterValue("Value");
-const QString ParameterLinkType("LinkType");
-const QString ParameterLinkInversion("LinkInversion");
-const QString ParameterHidden("Hidden");
+const QString ParametersRoot(QStringLiteral("Parameters"));
+const QString Parameter(QStringLiteral("Parameter"));
+const QString ParameterId(QStringLiteral("Id"));
+const QString ParameterValue(QStringLiteral("Value"));
+const QString ParameterLinkType(QStringLiteral("LinkType"));
+const QString ParameterLinkInversion(QStringLiteral("LinkInversion"));
+const QString ParameterHidden(QStringLiteral("Hidden"));
 } // namespace EffectXml
