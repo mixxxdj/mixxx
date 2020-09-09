46a73badaf85ef602513d71b810fc08f3b8a59f3
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2020-09-09 23:20:29 +0200

    clang-format

--- src/controllers/hid/hidcontroller.cpp
+++ src/controllers/hid/hidcontroller.cpp
@@ -298,8 +298,10 @@ void HidController::sendBytesReport(QByteArray data, unsigned int reportID) {
     }
 }
 
-namespace {
-constexpr unsigned int sizeOfReportID = 1; // Size of an USB HID Report ID is always one byte
+
+namespace
+{
+    constexpr unsigned int sizeOfReportID = 1;  // Size of an USB HID Report ID is always one byte
 }
 
 void HidController::sendFeatureReport(
