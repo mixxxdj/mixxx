74a387a792e681fb7e12a6506ac3f795164e05ad
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-09-23 17:07:30 +0200

    tools/xmlformat: Manually fix some skin XML fromatting issues

--- tools/xmlformat.py
+++ tools/xmlformat.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 import argparse
 import sys
+import textwrap
 import xml.dom.minidom
 import xml.parsers.expat
+
+import cssbeautifier
+
+INDENT = " " * 2
+
+
+def get_node_indent(node):
+    if (
+        node.previousSibling
+        and node.previousSibling.nodeType == xml.dom.Node.TEXT_NODE
+    ):
+        text = node.previousSibling.data
+        return text.rpartition("\n")[2]
+
+
+def format_css(text):
+    css_opts = cssbeautifier.default_options()
+    css_opts.indent_size = 2
+    css_opts.indent_char = " "
+    css = cssbeautifier.beautify(text, opts=css_opts)
+    return textwrap.indent(css, INDENT)
+
+
+def walk_dom(node):
+    for child in node.childNodes:
+        yield child
+        yield from walk_dom(child)
 
 
 def main(argv=None):
@@ -34,9 +62,52 @@ def main(argv=None):
             continue
         output = "\n".join(
             line.rstrip()
-            for line in dom.toprettyxml(indent=" " * 4).splitlines()
+            for line in dom.toprettyxml(indent=INDENT).splitlines()
             if line.strip()
         )
+
+        # Fix some issues caused pretty printing skin files
+        dom = xml.dom.minidom.parseString(output)
+        for node in walk_dom(dom):
+            if node.nodeType == xml.dom.Node.ELEMENT_NODE:
+                # Fix indentation for Style Elements (Inline QSS)
+                if (
+                    node.tagName == "Style"
+                    and len(node.childNodes) == 1
+                    and node.childNodes[0].nodeType == xml.dom.Node.TEXT_NODE
+                ):
+                    indent = get_node_indent(node)
+                    node.childNodes[0].data = (
+                        "\n"
+                        + textwrap.indent(
+                            format_css(node.childNodes[0].data.strip()), indent
+                        )
+                        + "\n"
+                        + indent
+                    )
+                elif node.tagName in ("ConfigKey", "Group"):
+                    # Remove superfluous spaces from ConfigKey/Group elements
+                    for child in node.childNodes:
+                        if child.nodeType == xml.dom.Node.TEXT_NODE:
+                            child.data = child.data.strip()
+            elif node.nodeType == xml.dom.Node.COMMENT_NODE:
+                # Fix indentation for comments
+                indent = get_node_indent(node)
+                lines = node.data.strip().splitlines()
+                if len(lines) > 1:
+                    text = (
+                        lines[0].strip()
+                        + "\n"
+                        + textwrap.dedent("\n".join(lines[1:]))
+                    )
+                    node.data = (
+                        "\n" + textwrap.indent(text, indent) + "\n" + indent
+                    )
+
+        # Replace escaped quotes (e.g. in Style elements) with raw char and
+        # insert newline between XML declaration and root element
+        output = dom.toxml().replace("&quot;", '"').replace("?><", "?>\n<", 1)
+
         f.seek(0)
         f.truncate()
         f.write(output)
