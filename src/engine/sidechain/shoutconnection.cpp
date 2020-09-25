a6bdb677f48d007459a97d2e12d0c181b7ff05a4
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-09-25 12:02:40 +0200

    engine/sidechain/shoutconnection: Use mixxx::audio::SampleRate

--- src/engine/sidechain/shoutconnection.cpp
+++ src/engine/sidechain/shoutconnection.cpp
@@ -383,8 +383,15 @@ void ShoutConnection::updateFromPreferences() {
         qWarning() << "Error: unknown bit rate:" << iBitrate;
     }
 
-    int iMasterSamplerate = m_pMasterSamplerate->get();
-    if (m_format_is_ov && iMasterSamplerate == 96000) {
+    auto masterSamplerate = mixxx::audio::SampleRate(
+            static_cast<mixxx::audio::SampleRate::value_t>(
+                    m_pMasterSamplerate->get()));
+    VERIFY_OR_DEBUG_ASSERT(masterSamplerate.isValid()) {
+        qWarning() << "Invalid sample rate!" << masterSamplerate;
+        return;
+    }
+
+    if (m_format_is_ov && masterSamplerate == 96000) {
         errorDialog(tr("Broadcasting at 96 kHz with Ogg Vorbis is not currently "
                        "supported. Please try a different sample rate or switch "
                        "to a different encoding."),
@@ -394,7 +401,7 @@ void ShoutConnection::updateFromPreferences() {
     }
 
 #ifdef __OPUS__
-    if(m_format_is_opus && iMasterSamplerate != EncoderOpus::getMasterSamplerate()) {
+    if (m_format_is_opus && masterSamplerate != EncoderOpus::getMasterSamplerate()) {
         errorDialog(
             EncoderOpus::getInvalidSamplerateMessage(),
             tr("Unsupported sample rate")
@@ -443,7 +450,8 @@ void ShoutConnection::updateFromPreferences() {
                     pBroadcastSettings, this);
 
     QString errorMsg;
-    if(m_encoder->initEncoder(iMasterSamplerate, errorMsg) < 0) {
+    // TODO(XXX): Use mixxx::audio::SampleRate instead of int in initEncoder
+    if (m_encoder->initEncoder(static_cast<int>(masterSamplerate), errorMsg) < 0) {
         // e.g., if lame is not found
         // init m_encoder itself will display a message box
         kLogger.warning() << "**** Encoder init failed";
