2ae67da19ce8284df61afee03248fa6b83a70e5a
Uwe Klotz <uklotz@mixxx.org>
2020-09-30 22:47:14 +0200

    Update required schema version

--- src/database/mixxxdb.cpp
+++ src/database/mixxxdb.cpp
@@ -9,7 +9,7 @@
 const QString MixxxDb::kDefaultSchemaFile(":/schema.xml");
 
 //static
-const int MixxxDb::kRequiredSchemaVersion = 34;
+const int MixxxDb::kRequiredSchemaVersion = 35;
 
 namespace {
 
