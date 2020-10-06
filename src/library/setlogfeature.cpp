4f4eb01e8077762a1d04fdcf3bcddfe92e714d2f
ronso0 <ronso0@mixxx.org>
2020-10-06 02:41:43 +0200

    History feature: remove html stylesheet from cpp

--- src/library/setlogfeature.cpp
+++ src/library/setlogfeature.cpp
@@ -393,11 +393,9 @@ QString SetlogFeature::getRootViewHtml() const {
 
     QString html;
     html.append(QString("<h2>%1</h2>").arg(playlistsTitle));
-    html.append("<table border=\"0\" cellpadding=\"5\"><tr><td>");
     html.append(QString("<p>%1</p>").arg(playlistsSummary));
     html.append(QString("<p>%1</p>").arg(playlistsSummary2));
     html.append(QString("<p>%1</p>").arg(playlistsSummary3));
     html.append(QString("<p>%1</p>").arg(playlistsSummary4));
-    html.append("</td></tr></table>");
     return html;
 }
