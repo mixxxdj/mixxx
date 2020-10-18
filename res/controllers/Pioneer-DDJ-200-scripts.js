11425e1a511ef25e1e177c53347026788d7464de
Frank Breitling <frank.breitling@gmx.de>
2020-10-18 19:08:38 +0200

    Change from code review by Holzhaus

--- res/controllers/Pioneer-DDJ-200-scripts.js
+++ res/controllers/Pioneer-DDJ-200-scripts.js
@@ -1,5 +1,4 @@
 var DDJ200 = {
-    headMixSwitch: 0,
     jogDisabled: new Array(false, false, false, false, false)
 };
 
@@ -19,23 +18,24 @@ DDJ200.jog = function(channel, control, value, status, group) {
     // Convert value down to +1/-1
     // Register the movement
     var deckNumber = script.deckFromGroup(group);
-    if (DDJ200.jogDisabled[deckNumber]) { return; }
-    engine.setValue(group, "jog", value - 64);
+    if (! DDJ200.jogDisabled[deckNumber]) {
+        engine.setValue(group, "jog", value - 64);
+    }
 };
 
 DDJ200.touch = function(channel, control, value, status, group) {
     var deckNumber = script.deckFromGroup(group);
-    if (value === 0) {
+    if (value) {
+        // enable scratch
+        var alpha = 1.0 / 8;
+        engine.scratchEnable(deckNumber, 128, 33 + 1 / 3, alpha, alpha / 32);
+    } else {
         // disable jog to not prevent alignment and enable it after 900 ms
         DDJ200.jogDisabled[deckNumber] = true;
         engine.beginTimer(900, "DDJ200.jogDisabled["+deckNumber+"] = false;",
             true);
         // disable scratch
         engine.scratchDisable(deckNumber);
-    } else {
-        // enable scratch
-        var alpha = 1.0 / 8;
-        engine.scratchEnable(deckNumber, 128, 33 + 1 / 3, alpha, alpha / 32);
     }
 };
 
@@ -50,11 +50,10 @@ DDJ200.seek = function(channel, control, value, status, group) {
 };
 
 DDJ200.headmix = function(channel, control, value) {
-    if (value === 0) { return; }
-    DDJ200.headMixSwitch = 1 - DDJ200.headMixSwitch;
-    // toggle headMix knob to values of -1 and 1
-    engine.setValue("[Master]", "headMix", 2 * DDJ200.headMixSwitch - 1);
-
-    // now switch headMix LED with midi.sendShortMsg(0x96, 0x63, 0x7F or 0);
-    midi.sendShortMsg(0x90+channel, control, 0x7F * DDJ200.headMixSwitch);
+    if (value) {
+        var masterMixEnabled = (engine.getValue("[Master]", "headMix") > 0);
+        // toggle headMix knob between values of -1 and 1
+        engine.setValue("[Master]", "headMix", masterMixEnabled ? -1 : 1);
+        midi.sendShortMsg(0x96, 0x63, masterMixEnabled ? 0x7F : 0); // set LED
+    }
 };
