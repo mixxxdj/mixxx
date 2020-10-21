mixxxdj/mixxx

commit 03b79a6232d8e54ef1a72fcf334e6b6b7efae187

waveform/renderers/waveformrendermark: Use QRectF for saved loop drawing

src/waveform/renderers/waveformrendermark.cpp
@@ -94,10 +94,10 @@ void WaveformRenderMark::draw(QPainter* painter, QPaintEvent* /*event*/) {
                         gradient.setColorAt(0.75, QColor(Qt::transparent));
                         gradient.setColorAt(1, color);
                         painter->fillRect(
-                                QRect(QPoint(currentMarkPoint, 0),
-                                        QPoint(currentMarkEndPoint,
-                                                m_waveformRenderer
-                                                        ->getHeight())),
+                                QRectF(currentMarkPoint,
+                                        0,
+                                        currentMarkEndPoint,
+                                        m_waveformRenderer->getHeight()),
                                 QBrush(gradient));
                         visible = true;
                     }
@@ -137,9 +137,10 @@ void WaveformRenderMark::draw(QPainter* painter, QPaintEvent* /*event*/) {
                         gradient.setColorAt(0.75, QColor(Qt::transparent));
                         gradient.setColorAt(1, color);
                         painter->fillRect(
-                                QRect(QPoint(0, currentMarkPoint),
-                                        QPoint(m_waveformRenderer->getWidth(),
-                                                currentMarkEndPoint)),
+                                QRectF(0,
+                                        currentMarkPoint,
+                                        m_waveformRenderer->getWidth(),
+                                        currentMarkEndPoint),
                                 QBrush(gradient));
                         visible = true;
                     }

commit c346f6ad3ce81b07e0605c549333ac5ae4c1bd80

WaveformRenderMark: Use QRectF(QPointF, QPointF) for drawing saved loops
This fixes a regression from 03b79a6232d8e54ef1a72fcf334e6b6b7efae187.

src/waveform/renderers/waveformrendermark.cpp
@@ -94,10 +94,10 @@ void WaveformRenderMark::draw(QPainter* painter, QPaintEvent* /*event*/) {
                         gradient.setColorAt(0.75, QColor(Qt::transparent));
                         gradient.setColorAt(1, color);
                         painter->fillRect(
-                                QRectF(currentMarkPoint,
-                                        0,
-                                        currentMarkEndPoint,
-                                        m_waveformRenderer->getHeight()),
+                                QRectF(QPointF(currentMarkPoint, 0),
+                                        QPointF(currentMarkEndPoint,
+                                                m_waveformRenderer
+                                                        ->getHeight())),
                                 QBrush(gradient));
                         visible = true;
                     }
@@ -137,10 +137,9 @@ void WaveformRenderMark::draw(QPainter* painter, QPaintEvent* /*event*/) {
                         gradient.setColorAt(0.75, QColor(Qt::transparent));
                         gradient.setColorAt(1, color);
                         painter->fillRect(
-                                QRectF(0,
-                                        currentMarkPoint,
-                                        m_waveformRenderer->getWidth(),
-                                        currentMarkEndPoint),
+                                QRectF(QPointF(0, currentMarkPoint),
+                                        QPointF(m_waveformRenderer->getWidth(),
+                                                currentMarkEndPoint)),
                                 QBrush(gradient));
                         visible = true;
                     }