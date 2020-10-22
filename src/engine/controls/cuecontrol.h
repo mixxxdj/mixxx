479459188a002390c1b9fb3efda782394d13ae4c
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-10-22 14:43:08 +0200

    HotcueControl: Improve comment
    Co-authored-by: Daniel Schürmann <daschuer@mixxx.org>

--- src/engine/controls/cuecontrol.h
+++ src/engine/controls/cuecontrol.h
@@ -61,7 +61,7 @@ class HotcueControl : public QObject {
         Valid = 1,
         /// Hotcue is currently active (this only applies to Saved Loop cues
         /// while their loop is enabled). This status can be used by skins or
-        /// controller mappings to highlight a currently active saved loop,
+        /// controller mappings to highlight a the cue control that has saved the current loop,
         /// because resizing or moving the loop will make persistent changes to
         /// the cue.
         Active = 2,
