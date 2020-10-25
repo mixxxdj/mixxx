45eade2830fa65aae1e2a747855f692b2cc6b157
ronso0 <ronso0@mixxx.org>
2020-10-25 22:33:08 +0100

    waveformsignalcolors.h/waveformwidgetrenderer.h: break one-line get functions

--- src/waveform/renderers/waveformsignalcolors.h
+++ src/waveform/renderers/waveformsignalcolors.h
@@ -13,19 +13,45 @@ class WaveformSignalColors {
 
     bool setup(const QDomNode &node, const SkinContext& context);
 
-    inline const QColor& getSignalColor() const { return m_signalColor; }
-    inline const QColor& getLowColor() const { return m_lowColor; }
-    inline const QColor& getMidColor() const { return m_midColor; }
-    inline const QColor& getHighColor() const { return m_highColor; }
-    inline const QColor& getRgbLowColor() const { return m_rgbLowColor; }
-    inline const QColor& getRgbMidColor() const { return m_rgbMidColor; }
-    inline const QColor& getRgbHighColor() const { return m_rgbHighColor; }
-    inline const QColor& getAxesColor() const { return m_axesColor; }
-    inline const QColor& getPlayPosColor() const { return m_playPosColor; }
-    inline const QColor& getPlayedOverlayColor() const { return m_playedOverlayColor; }
-    inline const QColor& getPassthroughOverlayColor() const { return m_passthroughOverlayColor; }
-    inline const QColor& getBgColor() const { return m_bgColor; }
-    inline int getDimBrightThreshold() const { return m_dimBrightThreshold; }
+    inline const QColor& getSignalColor() const {
+        return m_signalColor;
+    }
+    inline const QColor& getLowColor() const {
+        return m_lowColor;
+    }
+    inline const QColor& getMidColor() const {
+        return m_midColor;
+    }
+    inline const QColor& getHighColor() const {
+        return m_highColor;
+    }
+    inline const QColor& getRgbLowColor() const {
+        return m_rgbLowColor;
+    }
+    inline const QColor& getRgbMidColor() const {
+        return m_rgbMidColor;
+    }
+    inline const QColor& getRgbHighColor() const {
+        return m_rgbHighColor;
+    }
+    inline const QColor& getAxesColor() const {
+        return m_axesColor;
+    }
+    inline const QColor& getPlayPosColor() const {
+        return m_playPosColor;
+    }
+    inline const QColor& getPlayedOverlayColor() const {
+        return m_playedOverlayColor;
+    }
+    inline const QColor& getPassthroughOverlayColor() const {
+        return m_passthroughOverlayColor;
+    }
+    inline const QColor& getBgColor() const {
+        return m_bgColor;
+    }
+    inline int getDimBrightThreshold() const {
+        return m_dimBrightThreshold;
+    }
 
   protected:
     void fallBackFromSignalColor();
