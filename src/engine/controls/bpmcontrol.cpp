mixxxdj/mixxx

commit c3cbb1d14186366f5d1d4c1100488f77df23c9c4

Use ControlObject::toBool() method to fix -Wfloat-conversion errors
This fixes a bunch of warnigs regarding implicit casts from double
to bool.

src/engine/controls/bpmcontrol.cpp
@@ -369,14 +369,14 @@ double BpmControl::calcSyncedRate(double userTweak) {
     m_dUserTweakingSync = userTweak != 0.0;
     double rate = 1.0;
     // Don't know what to do if there's no bpm.
-    if (m_pLocalBpm->get() != 0.0) {
+    if (m_pLocalBpm->toBool()) {
         rate = m_dSyncInstantaneousBpm / m_pLocalBpm->get();
     }
 
     // If we are not quantized, or there are no beats, or we're master,
     // or we're in reverse, just return the rate as-is.
-    if (!m_pQuantize->get() || isMaster(getSyncMode()) ||
-            !m_pBeats || m_pReverseButton->get()) {
+    if (m_pQuantize->toBool() || isMaster(getSyncMode()) ||
+            !m_pBeats || m_pReverseButton->toBool()) {
         m_resetSyncAdjustment = true;
         return rate + userTweak;
     }

src/control/controlobject.cpp
@@ -94,6 +94,12 @@ double ControlObject::get(const ConfigKey& key) {
     return pCop ? pCop->get() : 0.0;
 }
 
+// static
+bool ControlObject::toBool(const ConfigKey& key) {
+    const double value = ControlObject::get(key);
+    return (value != 0);
+}
+
 double ControlObject::getParameter() const {
     return m_pControl ? m_pControl->getParameter() : 0.0;
 }

src/control/controlobject.h
@@ -86,6 +86,7 @@ class ControlObject : public QObject {
 
     // Instantly returns the value of the ControlObject
     static double get(const ConfigKey& key);
+    static bool toBool(const ConfigKey& key);
 
     // Sets the ControlObject value. May require confirmation by owner.
     inline void set(double value) {

src/engine/controls/cuecontrol.cpp
@@ -416,7 +416,7 @@ void CueControl::trackLoaded(TrackPointer pNewTrack) {
     switch (seekOnLoadMode) {
     case SeekOnLoadMode::Beginning:
         // This allows users to load tracks and have the needle-drop be maintained.
-        if (!(m_pVinylControlEnabled->get() &&
+        if (!(m_pVinylControlEnabled->toBool() &&
                     m_pVinylControlMode->get() == MIXXX_VCMODE_ABSOLUTE)) {
             seekOnLoad(0.0);
         }

src/engine/controls/ratecontrol.cpp
@@ -401,13 +401,13 @@ double RateControl::calculateSpeed(double baserate, double speed, bool paused,
         double wheelFactor = getWheelFactor();
         double jogFactor = getJogFactor();
         bool bVinylControlEnabled = m_pVCEnabled && m_pVCEnabled->toBool();
-        bool useScratch2Value = m_pScratch2Enable->get() != 0;
+        bool useScratch2Value = m_pScratch2Enable->toBool();
 
         // By default scratch2_enable is enough to determine if the user is
         // scratching or not. Moving platter controllers have to disable
         // "scratch2_indicates_scratching" if they are not scratching,
         // to allow things like key-lock.
-        if (useScratch2Value && m_pScratch2Scratching->get()) {
+        if (useScratch2Value && m_pScratch2Scratching->toBool()) {
             *pReportScratching = true;
         }
 
@@ -478,9 +478,9 @@ double RateControl::calculateSpeed(double baserate, double speed, bool paused,
             int vcmode = m_pVCMode ? static_cast<int>(m_pVCMode->get()) : MIXXX_VCMODE_ABSOLUTE;
             // TODO(owen): Instead of just ignoring reverse mode, should we
             // disable absolute mode instead?
-            if (m_pReverseButton->get()
-                    && !m_pScratch2Enable->get()
-                    && (!bVinylControlEnabled || vcmode != MIXXX_VCMODE_ABSOLUTE)) {
+            if (m_pReverseButton->toBool() && !m_pScratch2Enable->toBool() &&
+                    (!bVinylControlEnabled ||
+                            vcmode != MIXXX_VCMODE_ABSOLUTE)) {
                 rate = -rate;
                 *pReportReverse = true;
             }

src/engine/controls/vinylcontrolcontrol.cpp
@@ -158,10 +158,10 @@ void VinylControlControl::slotControlVinylSeek(double fractionalPos) {
 
 bool VinylControlControl::isEnabled()
 {
-    return m_pControlVinylEnabled->get();
+    return m_pControlVinylEnabled->toBool();
 }
 
 bool VinylControlControl::isScratching()
 {
-    return m_pControlVinylScratching->get();
+    return m_pControlVinylScratching->toBool();
 }

src/engine/enginemaster.cpp
@@ -390,9 +390,9 @@ void EngineMaster::process(const int iBufferSize) {
     }
     //Trace t("EngineMaster::process");
 
-    bool masterEnabled = m_pMasterEnabled->get();
-    bool boothEnabled = m_pBoothEnabled->get();
-    bool headphoneEnabled = m_pHeadphoneEnabled->get();
+    bool masterEnabled = m_pMasterEnabled->toBool();
+    bool boothEnabled = m_pBoothEnabled->toBool();
+    bool headphoneEnabled = m_pHeadphoneEnabled->toBool();
 
     m_iSampleRate = static_cast<int>(m_pMasterSampleRate->get());
     m_iBufferSize = iBufferSize;
@@ -731,7 +731,7 @@ void EngineMaster::process(const int iBufferSize) {
         }
     }
 
-    if (m_pMasterMonoMixdown->get()) {
+    if (m_pMasterMonoMixdown->toBool()) {
         SampleUtil::mixStereoToMono(m_pMaster, m_pMaster, m_iBufferSize);
     }
 
@@ -776,7 +776,7 @@ void EngineMaster::processHeadphones(const CSAMPLE_GAIN masterMixGainInHeadphone
     // If Head Split is enabled, replace the left channel of the pfl buffer
     // with a mono mix of the headphone buffer, and the right channel of the pfl
     // buffer with a mono mix of the master output buffer.
-    if (m_pHeadSplitEnabled->get()) {
+    if (m_pHeadSplitEnabled->toBool()) {
         // note: NOT VECTORIZED because of in place copy
         for (unsigned int i = 0; i + 1 < m_iBufferSize; i += 2) {
             m_pHead[i] = (m_pHead[i] + m_pHead[i + 1]) / 2;

src/engine/enginevumeter.cpp
@@ -112,7 +112,10 @@ void EngineVuMeter::process(CSAMPLE* pIn, const int iBufferSize) {
         --m_peakDurationR;
     }
 
-    m_ctrlPeakIndicator->set(m_ctrlPeakIndicatorR->get() || m_ctrlPeakIndicatorL->get());
+    m_ctrlPeakIndicator->set(
+            (m_ctrlPeakIndicatorR->toBool() || m_ctrlPeakIndicatorL->toBool())
+                    ? 1.0
+                    : 0.0);
 }
 
 void EngineVuMeter::doSmooth(CSAMPLE &currentVolume, CSAMPLE newVolume)

commit f64f779ab21a73fe2e14f72a3359240a59567d4d

BpmControl: Fix wrong quantize check in calcSyncedRate()
This fixes a regression caused by c3cbb1d14186366f5d1d4c1100488f77df23c9c4.

src/engine/controls/bpmcontrol.cpp
@@ -384,7 +384,7 @@ double BpmControl::calcSyncedRate(double userTweak) {
 
     // If we are not quantized, or there are no beats, or we're master,
     // or we're in reverse, just return the rate as-is.
-    if (m_pQuantize->toBool() || isMaster(getSyncMode()) ||
+    if (!m_pQuantize->toBool() || isMaster(getSyncMode()) ||
             !m_pBeats || m_pReverseButton->toBool()) {
         m_resetSyncAdjustment = true;
         return rate + userTweak;