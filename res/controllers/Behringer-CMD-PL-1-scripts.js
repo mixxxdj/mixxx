7b2ed5b1060a7deeace7e86fdf8714830519ed55
Yann Le Doare <yann@linuxconsole.org>
2020-11-01 08:40:16 +0100

    Add Behringer-CMD-PL-1 controller

--- res/controllers/Behringer-CMD-PL-1-scripts.js
+++ res/controllers/Behringer-CMD-PL-1-scripts.js
@@ -1,29 +1,29 @@
 var CMDPL1 = {};
 
-CMDPL1.init = function () {
-   // mixxx --controllerDebug
-   print('> Init CMDPL1 Done');
+CMDPL1.init = function() {
+    // mixxx --controllerDebug
+    print("> Init CMDPL1 Done");
 };
 
 CMDPL1.pitchSlider = function(channel, control, value, status, group) {   // Lower the sensitivity of the pitch slider
-    var currentValue = engine.getValue(group,"rate");
-    print('> Current : '+currentValue);
-    print('> value : '+value);
+    var currentValue = engine.getValue(group, "rate");
+    print("> Current : "+currentValue);
+    print("> value : "+value);
     engine.setValue(group, "rate", (value-64)/64);
 };
 
 CMDPL1.wheel = function(channel, control, value, status, group) {   // Lower the sensitivity of the pitch slider
-    var currentValue = engine.getValue(group,"rate");
-    print('> Current : '+currentValue);
-    print('> value : '+value);
+    var currentValue = engine.getValue(group, "rate");
+    print("> Current : "+currentValue);
+    print("> value : "+value);
     engine.setValue(group, "rate", currentValue+(value-64)/128);
-   // engine.setValue(group,"rate",(value-64)/64);
+    // engine.setValue(group,"rate",(value-64)/64);
 };
 
 // The wheel that actually controls the scratching
 CMDPL1.wheelTurn = function(channel, control, value, status, group) {
     var deckNumber = script.deckFromGroup(group);
-    print('> value : '+deckNumber);
+    print("> value : "+deckNumber);
 
     // A: For a control that centers on 0:
     var newValue;
@@ -32,17 +32,17 @@ CMDPL1.wheelTurn = function(channel, control, value, status, group) {
     } else {
         newValue = value - 128;
     }
- 
+
     // B: For a control that centers on 0x40 (64):
     newValue = value - 64;
- 
+
     // --- End choice
- 
+
     // In either case, register the movement
     deckNumber = script.deckFromGroup(group);
     if (engine.isScratching(deckNumber)) {
         engine.scratchTick(deckNumber, newValue); // Scratch!
     } else {
-        engine.setValue(group, 'jog', newValue); // Pitch bend
+        engine.setValue(group, "jog", newValue); // Pitch bend
     }
 };
