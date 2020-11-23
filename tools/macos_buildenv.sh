5ef8ca3dd26bad0e5cf804a1c2b1af2fa8ce3bbc
Be <be@mixxx.org>
2020-11-23 11:37:44 -0600

    macos_buildenv.sh code style

--- tools/macos_buildenv.sh
+++ tools/macos_buildenv.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-if [ "$2" != "--ghactions" ] && ! $(return 0 2>/dev/null)
-then
+if [ "$2" != "--ghactions" ] && ! $(return 0 2>/dev/null); then
   echo "This script must be run by sourcing it:"
   echo "source $0 $@"
   exit 1
@@ -28,8 +27,7 @@ BUILDENV_SHA256=${lines[1]}
 
 case "$COMMAND" in
     name)
-        if [ "$1" = "--ghactions" ]
-        then
+        if [ "$1" = "--ghactions" ]; then
             echo "::set-output name=buildenv_name::$envname"
         else
             echo "$BUILDENV_NAME"
@@ -37,8 +35,7 @@ case "$COMMAND" in
         ;;
 
     setup)
-        if [[ "$BUILDENV_NAME" =~ .*macosminimum([0-9]*\.[0-9]*).* ]]
-        then
+        if [[ "$BUILDENV_NAME" =~ .*macosminimum([0-9]*\.[0-9]*).* ]]; then
             MACOSX_DEPLOYMENT_TARGET="${BASH_REMATCH[1]}"
         else
             echo "Build environment did not match expected pattern. Check ${MIXXX_ROOT}/cmake/macos_build_environment file." >&2
@@ -47,15 +44,12 @@ case "$COMMAND" in
 
         BUILDENV_PATH="${BUILDENV_BASEPATH}/${BUILDENV_NAME}"
         mkdir -p "${BUILDENV_BASEPATH}"
-        if [ ! -d "${BUILDENV_PATH}" ]
-        then
-            if [ "$1" != "--profile" ]
-            then
+        if [ ! -d "${BUILDENV_PATH}" ]; then
+            if [ "$1" != "--profile" ]; then
                 echo "Build environment $BUILDENV_NAME not found in mixxx repository, downloading it..."
                 curl "https://downloads.mixxx.org/builds/buildserver/2.3.x-unix/${BUILDENV_NAME}.tar.gz" -o "${BUILDENV_PATH}.tar.gz"
                 OBSERVED_SHA256=$(shasum -a 256 "${BUILDENV_PATH}.tar.gz"|cut -f 1 -d' ')
-                if [ $OBSERVED_SHA256 == $BUILDENV_SHA256 ]
-                then
+                if [ $OBSERVED_SHA256 == $BUILDENV_SHA256 ]; then
                     echo "Download matched expected SHA256 sum $BUILDENV_SHA256"
                 else
                     echo "ERROR: Download did not match expected SHA256 checksum!"
@@ -70,8 +64,7 @@ case "$COMMAND" in
                 echo "source ${BASH_SOURCE[0]} setup"
                 return # exit would quit the shell being started
             fi
-        elif [ "$1" != "--profile" ]
-        then
+        elif [ "$1" != "--profile" ]; then
             echo "Build environment found: ${BUILDENV_PATH}"
         fi
 
@@ -80,8 +73,7 @@ case "$COMMAND" in
         export Qt5_DIR="$(find "${BUILDENV_PATH}" -type d -path "*/cmake/Qt5")"
         export QT_QPA_PLATFORM_PLUGIN_PATH="$(find "${BUILDENV_PATH}" -type d -path "*/plugins")"
 
-        if [ "$1" = "--ghactions" ]
-        then
+        if [ "$1" = "--ghactions" ]; then
             echo "::set-output name=macosx_deployment_target::${MACOSX_DEPLOYMENT_TARGET}"
             echo "::set-output name=cmake_prefix_path::${CMAKE_PREFIX_PATH}"
             echo "::set-output name=path::${PATH}"
