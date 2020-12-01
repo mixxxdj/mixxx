db80252ee34968c686995ea36356ced1cb391c32
Be <be@mixxx.org>
2020-11-30 19:35:56 -0600

    remove unnecessary `inline`

--- src/effects/backends/effectsbackendmanager.h
+++ src/effects/backends/effectsbackendmanager.h
@@ -12,7 +12,7 @@ class EffectsBackendManager {
     EffectsBackendManager();
     ~EffectsBackendManager() = default;
 
-    inline const QList<EffectManifestPointer>& getManifests() const {
+    const QList<EffectManifestPointer>& getManifests() const {
         return m_manifests;
     };
     const QList<EffectManifestPointer> getManifestsForBackend(EffectBackendType backendType) const;
