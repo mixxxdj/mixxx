bea6016993bc15432ec79dd4de74aff37b0cfb5c
Be <be@mixxx.org>
2020-12-01 09:43:00 -0600

    EffectsBackendManager: update comment

--- src/effects/backends/effectsbackendmanager.cpp
+++ src/effects/backends/effectsbackendmanager.cpp
@@ -62,8 +62,8 @@ EffectManifestPointer EffectsBackendManager::getManifestFromUniqueId(
     EffectBackendType backendType =
             EffectManifest::backendTypeFromString(uid.mid(delimiterIndex + 1));
     VERIFY_OR_DEBUG_ASSERT(backendType != EffectBackendType::Unknown) {
-        // Mixxx 2.0 - 2.2 did not store the backend type in mixxx.cfg,
-        // so this code will be executed once when upgrading to Mixxx 2.3.
+        // Mixxx 2.0 - 2.3 did not store the backend type in mixxx.cfg,
+        // so this code will be executed once when upgrading to Mixxx 2.4.
         // This debug assertion is safe to ignore in that case. If it is
         // triggered at any later time, there is a bug somewhere.
         // Do not manipulate the string passed to this function, just pass
