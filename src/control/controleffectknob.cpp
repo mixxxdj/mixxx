ca658f81ef8e6a530be74ba34b05c9b37c7e6ba9
Be <be@mixxx.org>
2020-11-30 21:30:09 -0600

    ControlEffectKnob: clang-format

--- src/control/controleffectknob.cpp
+++ src/control/controleffectknob.cpp
@@ -1,24 +1,25 @@
 #include "control/controleffectknob.h"
 
-#include "util/math.h"
 #include "effects/backends/effectmanifestparameter.h"
+#include "util/math.h"
 
 ControlEffectKnob::ControlEffectKnob(const ConfigKey& key, double dMinValue, double dMaxValue)
         : ControlPotmeter(key, dMinValue, dMaxValue) {
 }
 
 void ControlEffectKnob::setBehaviour(EffectManifestParameter::ValueScaler type,
-                                     double dMinValue, double dMaxValue) {
+        double dMinValue,
+        double dMaxValue) {
     if (m_pControl == NULL) {
         return;
     }
 
     if (type == EffectManifestParameter::ValueScaler::LINEAR) {
-            m_pControl->setBehavior(new ControlLinPotmeterBehavior(
-                    dMinValue, dMaxValue, false));
+        m_pControl->setBehavior(new ControlLinPotmeterBehavior(
+                dMinValue, dMaxValue, false));
     } else if (type == EffectManifestParameter::ValueScaler::LINEAR_INVERSE) {
-            m_pControl->setBehavior(new ControlLinInvPotmeterBehavior(
-                    dMinValue, dMaxValue, false));
+        m_pControl->setBehavior(new ControlLinInvPotmeterBehavior(
+                dMinValue, dMaxValue, false));
     } else if (type == EffectManifestParameter::ValueScaler::LOGARITHMIC) {
         if (dMinValue == 0) {
             if (dMaxValue == 1.0) {
