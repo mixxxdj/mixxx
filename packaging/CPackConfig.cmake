38b65840f9d3525aaf3acd545c136f587260749a
Daniel Schürmann <daschuer@mixxx.org>
2020-12-10 15:00:56 +0100

    Remove extra spaces
    Co-authored-by: Jan Holthuis <holthuis.jan@googlemail.com>

--- packaging/CPackConfig.cmake
+++ packaging/CPackConfig.cmake
@@ -2,7 +2,7 @@
 # to the actual generator being used. It allows per-generator setting of CPACK_* variables at cpack time.
 
 if (CPACK_GENERATOR MATCHES "DEB")
-  set( CPACK_INSTALL_SCRIPT ${CPACK_DEBIAN_INSTALL_SCRIPT} )
+  set(CPACK_INSTALL_SCRIPT ${CPACK_DEBIAN_INSTALL_SCRIPT})
 endif()
 
 if (CPACK_GENERATOR MATCHES "External")
