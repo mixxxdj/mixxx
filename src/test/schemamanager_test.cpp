034825e6c26a8bbe3671d363e8c95fe3564bd5d2
Uwe Klotz <uklotz@mixxx.org>
2020-12-15 08:48:47 +0100

    Fix SchemaManagerTest.BackwardsCompatibleVersion

--- src/test/schemamanager_test.cpp
+++ src/test/schemamanager_test.cpp
@@ -15,7 +15,7 @@ TEST_F(SchemaManagerTest, CanUpgradeFreshDatabaseToRequiredVersion) {
                 MixxxDb::kRequiredSchemaVersion, MixxxDb::kDefaultSchemaFile);
         EXPECT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
     }
-    // Subsequent upgrade(s)
+    // Subsequent upgrade
     {
         SchemaManager schemaManager(dbConnection());
         SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
@@ -34,20 +34,18 @@ TEST_F(SchemaManagerTest, NonExistentSchema) {
 TEST_F(SchemaManagerTest, BackwardsCompatibleVersion) {
     // Establish preconditions for test
     {
-        // Upgrade to version 1 to get the settings table.
         SchemaManager schemaManager(dbConnection());
         SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
-                1, MixxxDb::kDefaultSchemaFile);
-        EXPECT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
-
-        SettingsDAO settings(dbConnection());
+                MixxxDb::kRequiredSchemaVersion, MixxxDb::kDefaultSchemaFile);
+        ASSERT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
 
         // Pretend the database version is one past the required version but
         // min_compatible is the required version.
-        settings.setValue(SchemaManager::SETTINGS_VERSION_STRING,
-                MixxxDb::kRequiredSchemaVersion + 1);
-        settings.setValue(SchemaManager::SETTINGS_MINCOMPATIBLE_STRING,
-                MixxxDb::kRequiredSchemaVersion);
+        SettingsDAO settings(dbConnection());
+        ASSERT_TRUE(settings.setValue(SchemaManager::SETTINGS_VERSION_STRING,
+                MixxxDb::kRequiredSchemaVersion + 1));
+        ASSERT_TRUE(settings.setValue(SchemaManager::SETTINGS_MINCOMPATIBLE_STRING,
+                MixxxDb::kRequiredSchemaVersion));
     }
 
     SchemaManager schemaManager(dbConnection());
@@ -59,20 +57,18 @@ TEST_F(SchemaManagerTest, BackwardsCompatibleVersion) {
 TEST_F(SchemaManagerTest, BackwardsIncompatibleVersion) {
     // Establish preconditions for test
     {
-        // Upgrade to version 1 to get the settings table.
         SchemaManager schemaManager(dbConnection());
         SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
-                1, MixxxDb::kDefaultSchemaFile);
-        EXPECT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
-
-        SettingsDAO settings(dbConnection());
+                MixxxDb::kRequiredSchemaVersion, MixxxDb::kDefaultSchemaFile);
+        ASSERT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
 
         // Pretend the database version is one past the required version and
         // min_compatible is one past the required version.
-        settings.setValue(SchemaManager::SETTINGS_VERSION_STRING,
-                MixxxDb::kRequiredSchemaVersion + 1);
-        settings.setValue(SchemaManager::SETTINGS_MINCOMPATIBLE_STRING,
-                MixxxDb::kRequiredSchemaVersion + 1);
+        SettingsDAO settings(dbConnection());
+        ASSERT_TRUE(settings.setValue(SchemaManager::SETTINGS_VERSION_STRING,
+                MixxxDb::kRequiredSchemaVersion + 1));
+        ASSERT_TRUE(settings.setValue(SchemaManager::SETTINGS_MINCOMPATIBLE_STRING,
+                MixxxDb::kRequiredSchemaVersion + 1));
     }
 
     SchemaManager schemaManager(dbConnection());
@@ -89,12 +85,12 @@ TEST_F(SchemaManagerTest, IgnoreDuplicateColumn) {
         SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
                 3, MixxxDb::kDefaultSchemaFile);
         ASSERT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
-    }
 
-    // Add a column that will be added again in version 24.
-    QSqlQuery query(dbConnection());
-    ASSERT_TRUE(
-            query.exec("ALTER TABLE library ADD COLUMN coverart_source TEXT"));
+        // Add a column that will be added again in version 24.
+        QSqlQuery query(dbConnection());
+        ASSERT_TRUE(
+                query.exec("ALTER TABLE library ADD COLUMN coverart_source TEXT"));
+    }
 
     SchemaManager schemaManager(dbConnection());
     SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
@@ -109,12 +105,12 @@ TEST_F(SchemaManagerTest, UpgradeFailed) {
         SchemaManager schemaManager(dbConnection());
         SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
                 3, MixxxDb::kDefaultSchemaFile);
-        EXPECT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
-    }
+        ASSERT_EQ(SchemaManager::Result::UpgradeSucceeded, result);
 
-    // Drop a table that is expected to exist.
-    QSqlQuery query(dbConnection());
-    EXPECT_TRUE(query.exec("DROP TABLE PlaylistTracks"));
+        // Drop a table that is expected to exist.
+        QSqlQuery query(dbConnection());
+        ASSERT_TRUE(query.exec("DROP TABLE PlaylistTracks"));
+    }
 
     SchemaManager schemaManager(dbConnection());
     SchemaManager::Result result = schemaManager.upgradeToSchemaVersion(
