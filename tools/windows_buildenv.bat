54dd8092654ecc53331df252a43238dd6ccc3dc4
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2020-12-16 13:12:46 +0100

    Changed root paths to ./build and ./install Changed debug symbol settings Added WiX installer steps to documentation

--- tools/windows_buildenv.bat
+++ tools/windows_buildenv.bat
@@ -16,11 +16,13 @@ IF NOT DEFINED BUILDENV_BASEPATH (
     SET BUILDENV_BASEPATH=%MIXXX_ROOT%\buildenv
 )
 
-IF NOT "%~1"=="name" (
-    ECHO ### Read name of prebuild environment from: %MIXXX_ROOT%\cmake\windows_build_environment_name ###
+IF NOT DEFINED BUILD_ROOT (
+    SET BUILD_ROOT=%MIXXX_ROOT%\build
 )
-CALL :READ_ENVNAME
-SET BUILDENV_PATH=%BUILDENV_BASEPATH%\!BUILDENV_NAME!
+
+IF NOT DEFINED INSTALL_ROOT (
+    SET INSTALL_ROOT=%MIXXX_ROOT%\install
+)
 
 IF "%~1"=="" (
     REM In case of manual start by double click no arguments are specified: Default to COMMAND_setup
@@ -33,27 +35,41 @@ IF "%~1"=="" (
 EXIT /B 0
 
 :COMMAND_name
-    ECHO "!BUILDENV_NAME!"
+    CALL :READ_ENVNAME
     IF DEFINED GITHUB_ENV (
-        ECHO BUILDENV_NAME=!BUILDENV_PATH! >> !GITHUB_ENV!
+        ECHO BUILDENV_NAME=!RETVAL! >> !GITHUB_ENV!
     )
     GOTO :EOF
 
 :COMMAND_setup
+    CALL :READ_ENVNAME
+    SET BUILDENV_NAME=%RETVAL%
+    SET BUILDENV_PATH=%BUILDENV_BASEPATH%\%BUILDENV_NAME%
+
     IF NOT DEFINED GITHUB_ENV (
         CALL :GENERATE_CMakeSettings_JSON
-    )
 
-    IF NOT EXIST %BUILDENV_BASEPATH% (
-        ECHO ### Create subdirectory buildenv ###
-        MD %BUILDENV_BASEPATH%
+        IF NOT EXIST %BUILDENV_BASEPATH% (
+            ECHO ### Create subdirectory buildenv ###
+            MD %BUILDENV_BASEPATH%
+        )
+
+        IF NOT EXIST %BUILD_ROOT% (
+            ECHO ### Create subdirectory build ###
+            MD %BUILD_ROOT%
+        )
+
+        IF NOT EXIST %INSTALL_ROOT% (
+            ECHO ### Create subdirectory install ###
+            MD %INSTALL_ROOT%
+        )
     )
 
     IF NOT EXIST %BUILDENV_PATH% (
         ECHO ### Download prebuild build environment ###
         SET BUILDENV_URL=https://downloads.mixxx.org/builds/buildserver/2.3.x-windows/!BUILDENV_NAME!.zip
         IF NOT EXIST !BUILDENV_PATH!.zip (
-            ECHO ### Download prebuild build environment from !BUILDENV_URL! ###
+            ECHO ### Download prebuild build environment from !BUILDENV_URL! to !BUILDENV_PATH!.zip ###
             BITSADMIN /transfer buildenvjob /download /priority normal !BUILDENV_URL! !BUILDENV_PATH!.zip
             REM TODO: verify download using sha256sum?
         )
@@ -106,9 +122,12 @@ EXIT /B 0
 
 
 :READ_ENVNAME
+    ECHO ### Read name of prebuild environment from: %MIXXX_ROOT%\packaging\windows\build_environment ###
     SET /P BUILDENV_NAME=<%MIXXX_ROOT%\packaging\windows\build_environment
     SET BUILDENV_NAME=!BUILDENV_NAME:PLATFORM=%PLATFORM%!
     SET BUILDENV_NAME=!BUILDENV_NAME:CONFIGURATION=%CONFIGURATION%!
+    SET RETVAL=%BUILDENV_NAME%
+    ECHO "%RETVAL%"
     GOTO :EOF
 
 :GENERATE_CMakeSettings_JSON
