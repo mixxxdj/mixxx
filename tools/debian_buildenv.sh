4ac8dfe935b4f6515fb9f65bc1ea3b214921c6f2
Be <be@mixxx.org>
2020-12-25 13:26:05 -0600

    debian_buildenv.sh: add g++ to package list

--- tools/debian_buildenv.sh
+++ tools/debian_buildenv.sh
@@ -21,6 +21,7 @@ case "$COMMAND" in
 
         sudo apt-get update
         sudo apt-get install -y --no-install-recommends \
+            g++ \
             ccache \
             cmake \
             debhelper \
