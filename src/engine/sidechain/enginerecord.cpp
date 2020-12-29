9ea6cb5de4489771344663c45ef415e007c8d82d
Albert Aparicio <aaparicio@posteo.net>
2020-12-29 12:57:25 +0100

    Code style adjustments for build compliance

--- src/engine/sidechain/enginerecord.cpp
+++ src/engine/sidechain/enginerecord.cpp
@@ -224,20 +224,23 @@ void EngineRecord::writeCueLine() {
                                     % 75);
 
     m_cueFile.write(QString("  TRACK %1 AUDIO\n")
-                    .arg((double)m_cueTrack, 2, 'f', 0, '0')
-                    .toUtf8());
+                            .arg((double)m_cueTrack, 2, 'f', 0, '0')
+                            .toUtf8());
 
     m_cueFile.write(QString("    TITLE \"%1\"\n")
-        .arg(m_pCurrentTrack->getTitle()).toUtf8());
+                            .arg(m_pCurrentTrack->getTitle())
+                            .toUtf8());
     m_cueFile.write(QString("    PERFORMER \"%1\"\n")
-        .arg(m_pCurrentTrack->getArtist()).toUtf8());
+                            .arg(m_pCurrentTrack->getArtist())
+                            .toUtf8());
 
     // Woefully inaccurate (at the seconds level anyways).
     // We'd need a signal fired state tracker
     // for the track detection code.
     m_cueFile.write(QString("    INDEX 01 %1:%2\n")
-                    .arg(getRecordedDurationStr())
-                    .arg((double)cueFrame, 2, 'f', 0, '0').toUtf8());
+                            .arg(getRecordedDurationStr())
+                            .arg((double)cueFrame, 2, 'f', 0, '0')
+                            .toUtf8());
 }
 
 // Encoder calls this method to write compressed audio
@@ -315,23 +318,21 @@ bool EngineRecord::openCueFile() {
 
     if (m_baAuthor.length() > 0) {
         m_cueFile.write(QString("PERFORMER \"%1\"\n")
-                        .arg(QString(m_baAuthor).replace(QString("\""), QString("\\\"")))
-                        .toUtf8());
+                                .arg(QString(m_baAuthor).replace(QString("\""), QString("\\\"")))
+                                .toUtf8());
     }
 
     if (m_baTitle.length() > 0) {
         m_cueFile.write(QString("TITLE \"%1\"\n")
-                        .arg(QString(m_baTitle).replace(QString("\""), QString("\\\"")))
-                        .toUtf8());
+                                .arg(QString(m_baTitle).replace(QString("\""), QString("\\\"")))
+                                .toUtf8());
     }
 
-    m_cueFile.write(QString("FILE \"%1\" %2\n").arg(
-            QFileInfo(m_fileName).fileName() //strip path
-                .replace(QString("\""), QString("\\\"")), // escape doublequote
-            (m_encoding == ENCODING_MP3) ? ENCODING_MP3  :
-            (m_encoding == ENCODING_AIFF) ? ENCODING_AIFF :
-            "WAVE" // MP3 and AIFF are recognized but other formats just use WAVE.
-        ).toUtf8());
+    m_cueFile.write(QString("FILE \"%1\" %2\n").arg(QFileInfo(m_fileName).fileName()                                                                        //strip path
+                                                               .replace(QString("\""), QString("\\\"")),                                                    // escape doublequote
+                                                       (m_encoding == ENCODING_MP3) ? ENCODING_MP3 : (m_encoding == ENCODING_AIFF) ? ENCODING_AIFF : "WAVE" // MP3 and AIFF are recognized but other formats just use WAVE.
+                                                       )
+                            .toUtf8());
     return true;
 }
 
