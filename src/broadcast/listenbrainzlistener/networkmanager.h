cc0ef07a51b8d053f4a946d66a9169c7d378ae2a
Daniel Schürmann <daschuer@mixxx.org>
2020-12-30 23:02:05 +0100

    Removed unused FakeNetworkManager

--- src/broadcast/listenbrainzlistener/networkmanager.h
+++ src/broadcast/listenbrainzlistener/networkmanager.h
@@ -9,16 +9,11 @@ class QNetworkRequest;
 class NetworkReply;
 class NetworkRequest;
 
+/// Handles network communications
 class NetworkManager : public QObject {
     Q_OBJECT
   public:
     virtual NetworkReply* post(const NetworkRequest* request, const QByteArray& data) = 0;
   signals:
     void finished(NetworkReply* reply);
-};
-
-class FakeNetworkManager : public NetworkManager {
-    Q_OBJECT
-  public:
-    NetworkReply* post(const NetworkRequest* request, const QByteArray& data) override;
 };
