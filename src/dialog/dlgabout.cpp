185c8f8532d9ba68dd0e53cead4ec20cc54e94a6
Daniel Schürmann <daschuer@mixxx.org>
2020-12-30 02:32:02 +0100

    Added Jörg Wartenberg to the contributor list. Thank you very much.

--- src/dialog/dlgabout.cpp
+++ src/dialog/dlgabout.cpp
@@ -98,6 +98,7 @@ DlgAbout::DlgAbout(QWidget* parent) : QDialog(parent), Ui::DlgAboutDlg() {
             << "Janek Fischer"
             << "Chris Hills"
             << "David Lowenfels"
+            << "J&ouml;rg Wartenberg"
             << "Sanskar Bajpai"
             << "Edward Millen"
             << "Frank Breitling"
