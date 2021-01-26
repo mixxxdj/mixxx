8210f97be124bdea59c0fe5953ccfd2efa51a568
Uwe Klotz <uklotz@mixxx.org>
2021-01-26 14:43:00 +0100

    Remove obsolete pre-C++17 workarounds in util/optional.h

--- src/util/optional.h
+++ src/util/optional.h
@@ -1,33 +1,7 @@
 #pragma once
 
-#if __has_include(<optional>) || \
-    (defined(__cpp_lib_optional) && __cpp_lib_optional >= 201606L)
-
-#include <optional>
-
-#else
-
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Winvalid-constexpr"
-#endif
-
-#include <experimental/optional>
-
-namespace std {
-
-using std::experimental::make_optional;
-using std::experimental::nullopt;
-using std::experimental::optional;
-
-// Workarounds for missing member functions:
-// option::has_value() -> explicit operator bool()
-// option::value() -> operator*()
-
-} // namespace std
-
-#endif
-
 #include <QtDebug>
+#include <optional>
 
 template<typename T>
 QDebug operator<<(QDebug dbg, std::optional<T> arg) {
