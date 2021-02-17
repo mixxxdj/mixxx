d6142502be83103c12a14c3f13b42f1484a541ea
JoergAtGithub <64457745+JoergAtGithub@users.noreply.github.com>
2021-02-17 23:47:16 +0100

    Update src/controllers/hid/hidcontroller.h
    Co-authored-by: Be <be.0@gmx.com>

--- src/controllers/hid/hidcontroller.h
+++ src/controllers/hid/hidcontroller.h
@@ -72,7 +72,7 @@ class HidController final : public Controller {
     static constexpr int kNumBuffers = 2;
     static constexpr int kBufferSize = 255;
     unsigned char m_pPollData[kNumBuffers][kBufferSize];
-    int m_LastPollSize;
+    int m_lastPollSize;
     int m_PollingBufferIndex;
 
     friend class HidControllerJSProxy;
