bb4bf6a720e4b288cdd435f1dc9ada66ca5758ed
Daniel Schürmann <daschuer@mixxx.org>
2021-02-25 07:58:21 +0100

    Use -DBL_MAX as inital sample position, and fix more tests

--- src/engine/enginebuffer.cpp
+++ src/engine/enginebuffer.cpp
@@ -70,7 +70,7 @@ EngineBuffer::EngineBuffer(const QString& group,
           m_pKeyControl(nullptr),
           m_pReadAheadManager(nullptr),
           m_pReader(nullptr),
-          m_filepos_play(kInitalSample),
+          m_filepos_play(kInitalSamplePosition),
           m_speed_old(0),
           m_tempo_ratio_old(1.),
           m_scratching_old(false),
@@ -515,7 +515,7 @@ void EngineBuffer::slotTrackLoaded(TrackPointer pTrack,
 
     m_pause.lock();
     m_visualPlayPos->setInvalid();
-    m_filepos_play = kInitalSample; // for execute seeks to 0.0
+    m_filepos_play = kInitalSamplePosition; // for execute seeks to 0.0
     m_pCurrentTrack = pTrack;
     m_pTrackSamples->set(iTrackNumSamples);
     m_pTrackSampleRate->set(iTrackSampleRate);
