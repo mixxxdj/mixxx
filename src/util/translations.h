a25c38846d6fc95ec431faffd151c0b8b4d98996
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-05-04 14:32:29 +0200

    Translations: Improve locale override logic

--- src/util/translations.h
+++ src/util/translations.h
@@ -13,93 +13,98 @@ namespace mixxx {
 class Translations {
   public:
     static void initializeTranslations(UserSettingsPointer pConfig,
-                                       QCoreApplication* pApp,
-                                       const QString& forcedLocale) {
-        QString resourcePath = pConfig->getResourcePath();
-        QString translationsFolder = resourcePath + "translations/";
+            QCoreApplication* pApp,
+            const QString& cmdlineLocaleString) {
+        QString translationsPath = pConfig->getResourcePath() + "translations/";
 
-        // Load Qt base translations
-        QString userLocale = forcedLocale;
-        QLocale systemLocale = QLocale::system();
+        // Set the default locale, if specified via command line or config
+        // file. If neither is the case, the default locale will be the
+        // system's locale.
+        {
+            QString customLocaleString = QString();
+            if (!cmdlineLocaleString.isEmpty()) {
+                customLocaleString = cmdlineLocaleString;
+            } else {
+                const QString configLocale = pConfig->getValueString(
+                        ConfigKey("[Config]", "Locale"));
+                if (!configLocale.isEmpty()) {
+                    customLocaleString = configLocale;
+                }
+            }
 
-        // Attempt to load user locale from config
-        if (userLocale.isEmpty()) {
-            userLocale = pConfig->getValueString(ConfigKey("[Config]", "Locale"));
+            if (!customLocaleString.isEmpty()) {
+                const QLocale customLocale = QLocale(customLocaleString);
+                // If the customLocaleString is not a valid locale, Qt will
+                // automatically use the "C" locale instead. In that case,
+                // let's print a warning.
+                if (customLocaleString.compare(
+                            QStringLiteral("C"), Qt::CaseInsensitive) != 0 &&
+                        customLocale.language() == QLocale::C) {
+                    qWarning() << "Custom locale not found, using 'C' locale "
+                                  "(check your configuration to avoid this "
+                                  "warning).";
+                }
+                QLocale::setDefault(customLocale);
+            }
         }
 
-        if (userLocale.isEmpty()) {
-            QLocale::setDefault(QLocale(systemLocale));
-        } else {
-            QLocale::setDefault(QLocale(userLocale));
+        QLocale locale = QLocale();
+
+        // Do not try to load translations if we're using 'C' locale.
+        if (locale.language() == QLocale::C) {
+            qDebug() << "Skipping loading of translations because the 'C' locale is used.";
+            return;
         }
 
-        // source language
-        if (userLocale == "en_US") {
+        // Don't bother loading a translation if the first ui-langauge is
+        // English because the interface is already in English. This fixes
+        // the case where the user's install of Qt doesn't have an explicit
+        // English translation file and the fact that we don't ship a
+        // mixxx_en.qm.
+        if (locale.language() == QLocale::English &&
+                (locale.country() == QLocale::UnitedStates ||
+                        locale.country() == QLocale::AnyCountry)) {
+            qDebug() << "Skipping loading of translations because the locale is 'en' or 'en_US'.";
             return;
         }
 
         // Load Qt translations for this locale from the system translation
         // path. This is the lowest precedence QTranslator.
-        QTranslator* qtTranslator = new QTranslator(pApp);
-        if (loadTranslations(systemLocale, userLocale, "qt", "_",
-                             QLibraryInfo::location(QLibraryInfo::TranslationsPath),
-                             qtTranslator)) {
-            pApp->installTranslator(qtTranslator);
+        QTranslator* pQtTranslator = new QTranslator(pApp);
+        if (pQtTranslator->load(locale,
+                    "qt",
+                    "_",
+                    QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+            pApp->installTranslator(pQtTranslator);
         } else {
-            delete qtTranslator;
+            delete pQtTranslator;
         }
 
         // Load Qt translations for this locale from the Mixxx translations
         // folder.
-        QTranslator* mixxxQtTranslator = new QTranslator(pApp);
-        if (loadTranslations(systemLocale, userLocale, "qt", "_",
-                             translationsFolder,
-                             mixxxQtTranslator)) {
-            pApp->installTranslator(mixxxQtTranslator);
+        QTranslator* pMixxxQtTranslator = new QTranslator(pApp);
+        if (pMixxxQtTranslator->load(locale, "qt", "_", translationsPath)) {
+            pApp->installTranslator(pMixxxQtTranslator);
         } else {
-            delete mixxxQtTranslator;
+            delete pMixxxQtTranslator;
         }
 
         // Load Mixxx specific translations for this locale from the Mixxx
         // translations folder.
-        QTranslator* mixxxTranslator = new QTranslator(pApp);
-        bool mixxxLoaded = loadTranslations(systemLocale, userLocale, "mixxx", "_",
-                                            translationsFolder, mixxxTranslator);
+        QTranslator* pMixxxTranslator = new QTranslator(pApp);
+        bool mixxxLoaded = pMixxxTranslator->load(locale, "mixxx", "_", translationsPath);
         if (mixxxLoaded) {
             qDebug() << "Loaded translations for locale"
-                     << (userLocale.size() > 0 ? userLocale : systemLocale.name())
-                     << "from" << translationsFolder;
-            pApp->installTranslator(mixxxTranslator);
+                     << locale.name()
+                     << "from" << translationsPath;
+            pApp->installTranslator(pMixxxTranslator);
         } else {
             qWarning() << "Failed to load translations for locale"
-                       << (userLocale.size() > 0 ? userLocale : systemLocale.name())
-                       << "from" << translationsFolder;
-            delete mixxxTranslator;
+                       << locale.name()
+                       << "from" << translationsPath;
+            delete pMixxxTranslator;
         }
     }
-
-  private:
-    static bool loadTranslations(const QLocale& systemLocale,
-            const QString& userLocale,
-            const QString& translation,
-            const QString& prefix,
-            const QString& translationPath,
-            QTranslator* pTranslator) {
-        if (userLocale.size() == 0) {
-            QStringList uiLanguages = systemLocale.uiLanguages();
-            if (uiLanguages.size() > 0 && uiLanguages.first() == "en") {
-                // Don't bother loading a translation if the first ui-langauge is
-                // English because the interface is already in English. This fixes
-                // the case where the user's install of Qt doesn't have an explicit
-                // English // TODO: ranslation file and the fact that we don't ship a
-                // mixxx_en.qm.
-                return false;
-            }
-            return pTranslator->load(systemLocale, translation, prefix, translationPath);
-        }
-        return pTranslator->load(translation + prefix + userLocale, translationPath);
-    }
-
     Translations() = delete;
 };
 
