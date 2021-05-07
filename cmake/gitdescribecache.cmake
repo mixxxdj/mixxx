34eb11ec2d24a18a20bc86ece0cea5a55fa048a0
Daniel Schürmann <daschuer@mixxx.org>
2021-05-07 07:50:39 +0200

    Depend reconfigure on CPackConfig.cmake

--- cmake/gitdescribecache.cmake
+++ cmake/gitdescribecache.cmake
@@ -5,4 +5,6 @@ execute_process(
   OUTPUT_STRIP_TRAILING_WHITESPACE
   ERROR_QUIET
 )
+message(STATUS ${GIT_DESCRIBE})
+# uses GIT_DESCRIBE
 configure_file(${CMAKE_SOURCE_DIR_}/cmake/gitdescribecache.in ${CMAKE_BINARY_DIR_}/gitdescribecache @ONLY)
