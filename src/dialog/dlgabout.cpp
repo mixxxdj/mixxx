d2196c1751e97b9bd0b64baa0a871b5425e6a1bb
Daniel Schürmann <daschuer@mixxx.org>
2021-05-10 19:17:11 +0200

    Added Balló György to the contributor list in the about box. Thank you very nuch.

--- src/dialog/dlgabout.cpp
+++ src/dialog/dlgabout.cpp
@@ -105,7 +105,8 @@ DlgAbout::DlgAbout(QWidget* parent) : QDialog(parent), Ui::DlgAboutDlg() {
             << "Justin Kourie"
             << "Waylon Robertson"
             << "Al Hadebe"
-            << "Javier Vilarroig";
+            << "Javier Vilarroig"
+            << "Ball&oacute; Gy&ouml;rgy";
 
     QStringList specialThanks;
     specialThanks
