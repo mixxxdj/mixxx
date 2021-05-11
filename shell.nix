ae580f0462ce8172ddbdd3c5b55b4ab52fcd1910
Daniel Poelzleithner <git@poelzi.org>
2021-05-12 01:07:04 +0200

    remove leftovers from python based pre-commit hook. Remove old colors

--- shell.nix
+++ shell.nix
@@ -278,20 +278,6 @@ in stdenv.mkDerivation rec {
     if [ -z "$LOCALE_ARCHIVE" ]; then
       export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive";
     fi
-    export PYTHONPATH=venv/lib/python3.7/site-packages/:$PYTHONPATH
-    export SOURCE_DATE_EPOCH=315532800
-    if [ -z $QT_MESSAGE_PATTERN ]; then
-      QT_MESSAGE_PATTERN="`echo -e \"\033[32m%{time h:mm:ss.zzz}\033[0m \"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-category}\033[35m %{category}:\033[35m%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"[\033[97m{{threadname}}\033[0m] \"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-debug}\033[34m%{type} \033[36m%{function}%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-info}\033[32m%{type}%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-warning}\033[93m%{type}%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-critical}\033[91m%{type}%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"%{if-fatal}\033[97m\033[41m%{type} \033[30m%{file}:%{line}%{endif}\"`"
-      QT_MESSAGE_PATTERN+="`echo -e \"\033[0m  %{message}\"`"
-      export QT_MESSAGE_PATTERN
-    fi
     echo -e "Mixxx development shell. Available commands:\n"
     echo " configure - configures cmake (only has to run once)"
     echo " build - compiles Mixxx"
@@ -321,7 +307,6 @@ in stdenv.mkDerivation rec {
         git-clang-format
         glibcLocales
         include-what-you-use
-        # for pre-commit installation since nixpkg.pre-commit may be to old
         python3
         nodejs
         shell-build
