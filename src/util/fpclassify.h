d4e8f6e8f2ba5e5a2e9e940dd2296129814a5278
Uwe Klotz <uklotz@mixxx.org>
2021-05-23 11:56:37 +0200

    fpclassify: Delete obsolete special case handling for MS VC++

--- src/util/fpclassify.h
+++ src/util/fpclassify.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#ifdef _MSC_VER
-
-// VC++ uses _isnan() instead of isnan() and !_finite instead of isinf.
-#include <float.h>
-#define isnan(x) _isnan(x)
-#define isinf(x) (!_finite(x))
-
-#else
-
-// We define 
+// We define
 // these as macros to prevent clashing with c++11 built-ins in the global
 // namespace. If you say "using std::isnan;" then this will fail to build with
 // std=c++11. See https://bugs.webkit.org/show_bug.cgi?id=59249 for some
@@ -31,5 +22,3 @@ int util_isfinite(double x);
 int util_isnormal(double x);
 int util_isnan(double x);
 int util_isinf(double x);
-
-#endif
