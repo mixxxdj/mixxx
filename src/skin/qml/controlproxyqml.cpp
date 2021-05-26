fc455bddee8cae2a5ff0264a252a6f51cb7febbd
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-05-26 12:21:13 +0200

    QML: Add more checks and warnings and expose keyValid/initialized props

--- src/skin/qml/controlproxyqml.cpp
+++ src/skin/qml/controlproxyqml.cpp
@@ -7,9 +7,22 @@ ControlProxyQml::ControlProxyQml(QObject* parent)
           m_pControlProxy(nullptr) {
 }
 
+bool ControlProxyQml::isKeyValid() const {
+    return m_coKey.isValid();
+}
+
+bool ControlProxyQml::isInitialized() const {
+    return m_pControlProxy != nullptr;
+}
+
 void ControlProxyQml::setGroup(const QString& group) {
+    const bool keyValidBeforeSet = isKeyValid();
     m_coKey.group = group;
-    initialize();
+    const bool keyValidAfterSet = isKeyValid();
+    if (keyValidBeforeSet != keyValidAfterSet) {
+        emit keyValidChanged(keyValidAfterSet);
+    }
+    tryInitialize();
 }
 
 const QString& ControlProxyQml::getGroup() const {
@@ -17,8 +30,13 @@ const QString& ControlProxyQml::getGroup() const {
 }
 
 void ControlProxyQml::setKey(const QString& key) {
+    const bool keyValidBeforeSet = isKeyValid();
     m_coKey.item = key;
-    initialize();
+    const bool keyValidAfterSet = isKeyValid();
+    if (keyValidBeforeSet != keyValidAfterSet) {
+        emit keyValidChanged(keyValidAfterSet);
+    }
+    tryInitialize();
 }
 
 const QString& ControlProxyQml::getKey() const {
@@ -26,7 +44,9 @@ const QString& ControlProxyQml::getKey() const {
 }
 
 void ControlProxyQml::setValue(double newValue) {
-    if (!m_pControlProxy) {
+    if (!isInitialized()) {
+        qWarning() << "QmlControlProxy: Attempted to set value" << newValue
+                   << "on non-initialized CO" << m_coKey;
         return;
     }
     m_pControlProxy->set(newValue);
@@ -34,14 +54,19 @@ void ControlProxyQml::setValue(double newValue) {
 }
 
 double ControlProxyQml::getValue() const {
-    if (!m_pControlProxy) {
-        return -1;
+    if (!isInitialized()) {
+        qWarning() << "QmlControlProxy: Attempted to get value from "
+                      "non-initialized CO"
+                   << m_coKey << "(returning 0)";
+        return 0;
     }
     return m_pControlProxy->get();
 }
 
 void ControlProxyQml::setParameter(double newValue) {
-    if (!m_pControlProxy) {
+    if (!isInitialized()) {
+        qWarning() << "QmlControlProxy: Attempted to set parameter" << newValue
+                   << "on non-initialized CO" << m_coKey;
         return;
     }
     m_pControlProxy->setParameter(newValue);
@@ -49,22 +74,76 @@ void ControlProxyQml::setParameter(double newValue) {
 }
 
 double ControlProxyQml::getParameter() const {
-    if (!m_pControlProxy) {
-        return -1;
+    if (!isInitialized()) {
+        qWarning() << "QmlControlProxy: Attempted to get parameter from "
+                      "non-initialized CO"
+                   << m_coKey << "(returning 0)";
+        return 0;
     }
     return m_pControlProxy->getParameter();
 }
 
-void ControlProxyQml::initialize() {
-    if (m_coKey.isValid()) {
-        m_pControlProxy = std::make_unique<ControlProxy>(
-                m_coKey, this, ControlFlag::AllowMissingOrInvalid);
-        m_pControlProxy->connectValueChanged(this, &ControlProxyQml::controlProxyValueChanged);
-        m_pControlProxy->emitValueChanged();
+bool ControlProxyQml::initialize() {
+    const bool wasInitialized = isInitialized();
+    VERIFY_OR_DEBUG_ASSERT(!wasInitialized || m_coKey != m_pControlProxy->getKey()) {
+        qWarning() << "QmlControlProxy: Tried to initialize although CO"
+                   << m_coKey
+                   << "is already initialized with same key, ignoring";
+        return false;
     }
+
+    VERIFY_OR_DEBUG_ASSERT(isKeyValid()) {
+        qWarning() << "QmlControlProxy: Tried to initialize CO" << m_coKey
+                   << " with invalid key, resetting...";
+        m_pControlProxy.reset();
+        if (wasInitialized) {
+            emit initializedChanged(false);
+        }
+        return false;
+    }
+
+    std::unique_ptr<ControlProxy> pControlProxy =
+            std::make_unique<ControlProxy>(
+                    m_coKey, this, ControlFlag::NoAssertIfMissing);
+    VERIFY_OR_DEBUG_ASSERT(pControlProxy != nullptr) {
+        qWarning() << "QmlControlProxy: Requested CO " << m_coKey
+                   << " returned nullptr, resetting...";
+        m_pControlProxy.reset();
+        if (wasInitialized) {
+            emit initializedChanged(false);
+        }
+        return false;
+    }
+
+    if (!pControlProxy->valid()) {
+        qWarning() << "QmlControlProxy: Invalid CO" << m_coKey << " requested, resetting...";
+        m_pControlProxy.reset();
+        if (wasInitialized) {
+            emit initializedChanged(false);
+        }
+        return false;
+    }
+
+    m_pControlProxy = std::move(pControlProxy);
+    if (!wasInitialized) {
+        emit initializedChanged(true);
+    }
+    m_pControlProxy->connectValueChanged(this, &ControlProxyQml::slotControlProxyValueChanged);
+    m_pControlProxy->emitValueChanged();
+
+    return true;
 }
 
-void ControlProxyQml::controlProxyValueChanged(double newValue) {
+bool ControlProxyQml::tryInitialize() {
+    // Initialize when the key is valid and the underlying control proxy was
+    // either not initialized or has a different key.
+    if (isKeyValid() && (!isInitialized() || m_coKey != m_pControlProxy->getKey())) {
+        return initialize();
+    }
+    return false;
+}
+
+void ControlProxyQml::slotControlProxyValueChanged(double newValue) {
     emit valueChanged(newValue);
     emit parameterChanged(m_pControlProxy->getParameter());
 }
--- src/skin/qml/controlproxyqml.h
+++ src/skin/qml/controlproxyqml.h
@@ -7,8 +7,16 @@
 
 class ControlProxyQml : public QObject {
     Q_OBJECT
-    Q_PROPERTY(QString group READ getGroup WRITE setGroup)
-    Q_PROPERTY(QString key READ getKey WRITE setKey)
+// The REQUIRED flag only exists in Qt 5.14 and later.
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    Q_PROPERTY(QString group READ getGroup WRITE setGroup NOTIFY groupChanged REQUIRED)
+    Q_PROPERTY(QString key READ getKey WRITE setKey NOTIFY keyChanged REQUIRED)
+#else
+    Q_PROPERTY(QString group READ getGroup WRITE setGroup NOTIFY groupChanged)
+    Q_PROPERTY(QString key READ getKey WRITE setKey NOTIFY keyChanged)
+#endif
+    Q_PROPERTY(QString keyValid READ isKeyValid NOTIFY keyValidChanged)
+    Q_PROPERTY(QString initialized READ isInitialized NOTIFY initializedChanged)
     Q_PROPERTY(double value READ getValue WRITE setValue NOTIFY valueChanged)
     Q_PROPERTY(double parameter READ getParameter WRITE setParameter NOTIFY parameterChanged)
 
@@ -27,18 +35,26 @@ class ControlProxyQml : public QObject {
     void setParameter(double newValue);
     double getParameter() const;
 
+    bool isKeyValid() const;
+    bool isInitialized() const;
+
   signals:
+    void groupChanged(const QString& group);
+    void keyChanged(const QString& key);
+    void keyValidChanged(bool valid);
+    void initializedChanged(bool initialized);
     void valueChanged(double newValue);
     void parameterChanged(double newParameter);
 
   private slots:
-    void controlProxyValueChanged(double newValue);
+    void slotControlProxyValueChanged(double newValue);
 
   private:
     // QML cannot pass arguments to C++ constructors so this class
     // needs to rely on the QML object setting the group and key
     // properties to initialize the ControlProxy.
-    void initialize();
+    bool initialize();
+    bool tryInitialize();
 
     QString m_emptyString;
     ConfigKey m_coKey;
