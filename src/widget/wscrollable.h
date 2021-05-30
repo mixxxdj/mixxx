b3b5456c3feea9bf68a5a2e799b8b9beb79d8225
Owen Williams <owilliams@mixxx.org>
2021-05-30 00:50:53 -0400

    WScrollable: Fix for moved skincontext.h

--- src/widget/wscrollable.h
+++ src/widget/wscrollable.h
@@ -5,7 +5,7 @@
 #include <QScrollArea>
 
 #include "preferences/usersettings.h"
-#include "skin/skincontext.h"
+#include "skin/legacy/skincontext.h"
 #include "widget/wbasewidget.h"
 
 class WScrollable : public QScrollArea, public WBaseWidget {
