0b2b294b907fdac395c61142cfdd099ef7733af4
Daniel Schürmann <daschuer@mixxx.org>
2021-05-31 14:38:38 +0200

    only fail qmlformat stage if Qt >= 5.15 is used

--- tools/qmlformat.py
+++ tools/qmlformat.py
@@ -7,19 +7,28 @@ import shutil
 import subprocess
 import pathlib
 import sys
+import re
 
 QMLFORMAT_MISSING_MESSAGE = """
-qmlformat is not installed. It is included in Qt 5.15 and later. If that Qt
-version is not available on your system, please use the SKIP environment
-variable when committing:
-
-    $ SKIP=qmlformat git commit
+qmlformat not found, please install
 """
 
 
 def main(argv=None):
     qmlformat_executable = shutil.which("qmlformat")
     if not qmlformat_executable:
+        # verify if qmlformat is available on this machine
+        moc_executable = shutil.which("moc")
+        if moc_executable:
+            moc_version = subprocess.check_output(
+                (moc_executable, "-v")
+            ).strip()
+            v = re.search("moc ([0-9]*)\\.([0-9]*)\\.[0-9]*", str(moc_version))
+            if v:
+                if int(v.group(1)) < 5:
+                    return 0
+                if int(v.group(1)) == 5 and int(v.group(2)) < 15:
+                    return 0
         print(QMLFORMAT_MISSING_MESSAGE.strip(), file=sys.stderr)
         return 1
 
