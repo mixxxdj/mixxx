335bab0aa1f1f759704dd0b4d3d57d06541b5cdd
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2021-06-25 16:46:53 +0200

    Fix tests

--- src/test/quickaction_test.h
+++ src/test/quickaction_test.h
@@ -38,14 +38,25 @@ class Counter : public QObject {
     double m_value;
 };
 
+class QuickActionTestControlObject : public ControlObject {
+  public:
+    using ControlObject::ControlObject;
+
+    inline void set(double value) {
+        if (m_pControl) {
+            m_pControl->set(value, this, true);
+        }
+    }
+};
+
 class QuickActionTest : public MixxxTest {
   public:
     QuickActionTest();
     ~QuickActionTest();
 
     std::shared_ptr<QuickActionsManager> pQuickActionsManager;
-    ControlObject co1;
-    ControlObject co2;
+    QuickActionTestControlObject co1;
+    QuickActionTestControlObject co2;
     ControlProxy coRecording;
     ControlProxy coTrigger;
     Value result;
