85e22c7bf1c6f48e21f4df982f66ddd8d71c8c5e
Jan Holthuis <jholthuis@mixxx.org>
2021-07-07 23:26:03 +0200

    Rename mixxx.{cpp,h} to mixxxmainwindow.{cpp,h}

--- src/preferences/dialog/dlgprefdeck.cpp
+++ src/preferences/dialog/dlgprefdeck.cpp
@@ -15,7 +15,7 @@
 #include "mixer/basetrackplayer.h"
 #include "mixer/playerinfo.h"
 #include "mixer/playermanager.h"
-#include "mixxx.h"
+#include "mixxxmainwindow.h"
 #include "moc_dlgprefdeck.cpp"
 #include "preferences/usersettings.h"
 #include "util/compatibility.h"
