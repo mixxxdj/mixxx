8a38e81a20b732dfc01cf950f270a7d6183611bd
Daniel Schürmann <daschuer@mixxx.org>
2021-07-08 17:13:09 +0200

    Move initalizer to the header

--- src/coreservices.cpp
+++ src/coreservices.cpp
@@ -102,8 +102,7 @@ inline QLocale inputLocale() {
 namespace mixxx {
 
 CoreServices::CoreServices(const CmdlineArgs& args)
-        : m_pLV2Backend(nullptr),
-          m_runtime_timer(QLatin1String("CoreServices::runtime")),
+        : m_runtime_timer(QLatin1String("CoreServices::runtime")),
           m_cmdlineArgs(args) {
 }
 
