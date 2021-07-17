bcf9a6f1002651de0523864e6e333dc67df978d1
Alonso Cárdenas <acardenas@bsd-peru.org>
2021-07-17 13:18:34 -0500

    Make mixxx more FreeBSD friendly

--- cmake/modules/FindLibUSB.cmake
+++ cmake/modules/FindLibUSB.cmake
@@ -57,7 +57,11 @@ find_path(LibUSB_INCLUDE_DIR
 mark_as_advanced(LibUSB_INCLUDE_DIR)
 
 find_library(LibUSB_LIBRARY
+if(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+  NAMES usb
+else
   NAMES usb-1.0
+endif()
   PATHS ${PC_LibUSB_LIBRARY_DIRS}
   DOC "LibUSB library"
 )
