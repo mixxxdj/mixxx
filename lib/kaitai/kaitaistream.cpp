1de76fdc035f1ab1c7ccdfaf652b14b813fbb81b
Alonso Cárdenas <acardenas@bsd-peru.org>
2021-07-17 13:19:51 -0500

    Make mixxx more FreeBSD friendly

--- lib/kaitai/kaitaistream.cpp
+++ lib/kaitai/kaitaistream.cpp
@@ -17,6 +17,11 @@
 #define bswap_16(x) _byteswap_ushort(x)
 #define bswap_32(x) _byteswap_ulong(x)
 #define bswap_64(x) _byteswap_uint64(x)
+#elif defined(__FreeBSD__)
+#include <sys/endian.h>
+#define bswap_16(x) bswap16(x)
+#define bswap_32(x) bswap32(x)
+#define bswap_64(x) bswap64(x)
 #else // !__APPLE__ or !_MSC_VER
 #include <endian.h>
 #include <byteswap.h>
