056d9c280bb146961cb9cf0ade070baa1ad4ff0c
Alonso Cárdenas <acardenas@bsd-peru.org>
2021-07-17 13:23:27 -0500

    Make mixxx more FreeBSD friendly

--- src/util/versionstore.cpp
+++ src/util/versionstore.cpp
@@ -77,6 +77,8 @@ QString VersionStore::platform() {
     QString base = QStringLiteral("Linux");
 #elif defined(__WINDOWS__)
     QString base = QStringLiteral("Windows");
+#elif defined(__FreeBSD__)
+    QString base = QStringLiteral("FreeBSD");        
 #else
     QString base = QStringLiteral("Unknown OS");
 #endif
