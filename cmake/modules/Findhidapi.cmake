f67bcc6c1f03b0613e9d696317a4902b3dfdc6f6
Be <be@mixxx.org>
2021-08-14 16:31:56 -0500

    CMake: fix detection of hidapi

--- cmake/modules/Findhidapi.cmake
+++ cmake/modules/Findhidapi.cmake
@@ -62,14 +62,6 @@ find_library(hidapi_LIBRARY
 )
 mark_as_advanced(hidapi_LIBRARY)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  hidapi
-  DEFAULT_MSG
-  hidapi_LIBRARY
-  hidapi_INCLUDE_DIR
-)
-
 # Version detection
 if (EXISTS "${hidapi_INCLUDE_DIR}/hidapi.h")
     file(READ "${hidapi_INCLUDE_DIR}/hidapi.h" hidapi_H_CONTENTS)
@@ -80,10 +72,22 @@ if (EXISTS "${hidapi_INCLUDE_DIR}/hidapi.h")
     string(REGEX MATCH "#define HID_API_VERSION_PATCH ([0-9]+)" _dummy "${hidapi_H_CONTENTS}")
     set(hidapi_VERSION_PATCH "${CMAKE_MATCH_1}")
     # hidapi_VERSION is only available starting with 0.10.0
-    if (hidapi_VERSION_MAJOR AND hidapi_VERSION_MINOR AND hidapi_VERSION_PATCH)
+    # Simply using if(NOT) does not work because 0 is a valid value, so compare to empty string.
+    if (NOT hidapi_VERSION_MAJOR STREQUAL "" AND
+        NOT hidapi_VERSION_MINOR STREQUAL "" AND
+        NOT hidapi_VERSION_PATCH STREQUAL "")
       set(hidapi_VERSION "${hidapi_VERSION_MAJOR}.${hidapi_VERSION_MINOR}.${hidapi_VERSION_PATCH}")
     endif()
-endif ()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  hidapi
+  DEFAULT_MSG
+  hidapi_LIBRARY
+  hidapi_INCLUDE_DIR
+  hidapi_VERSION
+)
 
 if(hidapi_FOUND)
   set(hidapi_LIBRARIES "${hidapi_LIBRARY}")
