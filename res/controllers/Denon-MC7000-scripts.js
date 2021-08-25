829caacce07e4624f65442f8270bcfbe33de837f
toszlanyi <toszlanyi@yahoo.de>
2021-08-25 04:27:02 +0200

    delete all default comments

--- res/controllers/Denon-MC7000-scripts.js
+++ res/controllers/Denon-MC7000-scripts.js
@@ -37,48 +37,48 @@ var MC7000 = {};
 // 1) Beat LED in Slicer mode (counts every 8 beats AFTER the CUE point)
 //    only works for CONSTANT TEMPO tracks
 //    needs beat grid and CUE point set
-MC7000.experimental = false; // default: false
+MC7000.experimental = true;
 
 // Wanna have Needle Search active while playing a track ?
 // In any case Needle Search is available holding "SHIFT" down.
-// can be true or false (recommended: false)
-MC7000.needleSearchPlay = false; // default: false
+// can be true or false
+MC7000.needleSearchPlay = false;
 
 // select if the previous sampler shall stop before a new sampler starts
 // true: a running sampler will stop before the new sampler starts
 // false: all triggered samplers will play simultaneously
-MC7000.prevSamplerStop = true; // default: true
+MC7000.prevSamplerStop = true;
 
 // Set Vinyl Mode on ("true") or off ("false") when MIXXX starts.
 // This sets the Jog Wheel touch detection / Vinyl Mode
 // and the Jog LEDs ("VINYL" on = spinny, "VINYL" off = track position).
-MC7000.VinylModeOn = true; // default: true
+MC7000.VinylModeOn = true;
 
 // Possible pitchfader rate ranges given in percent.
 // can be cycled through by the RANGE buttons.
 // All default values are the same as selectable in Mixxx Preferences
 MC7000.rateRanges = [
-    4/100,  // default: 4/100
-    6/100,  // default: 6/100
-    8/100,  // default: 8/100
-    10/100, // default: 10/100
-    16/100, // default: 16/100
-    24/100, // default: 24/100
-    50/100, // default: 50/100
-    90/100, // default: 90/100
+    4/100,
+    6/100,
+    8/100,
+    10/100,
+    16/100,
+    24/100,
+    50/100,
+    90/100,
 ];
 
 // Platter Ring LED mode
 // Mode 0 = Single "off" LED chase (all others "on")
 // Mode 1 = Single "on" LED chase (all others "off")
 // use "SHIFT" + "DECK #" to toggle between both modes
-MC7000.modeSingleLED = 1; // default: 1
+MC7000.modeSingleLED = 1;
 
 // Scratch algorithm parameters
 MC7000.scratchParams = {
-    recordSpeed: 33 + 1/3, // default: 33 + 1/3
-    alpha: (1.0/10),   // default: (1.0/10)
-    beta: (1.0/10)/32  // default: (1.0/10)/32
+    recordSpeed: 33 + 1/3,
+    alpha: (1.0/10),
+    beta: (1.0/10)/32
 };
 
 // Sensitivity factor of the jog wheel (also depends on audio latency)
@@ -86,7 +86,7 @@ MC7000.scratchParams = {
 // set to 0.5 with audio buffer set to 50ms
 // set to 1 with audio buffer set to 25ms
 // set to 3 with audio buffer set to 5ms
-MC7000.jogSensitivity = 1;
+MC7000.jogSensitivity = 3;
 
 /*/////////////////////////////////
 //      USER VARIABLES END       //
