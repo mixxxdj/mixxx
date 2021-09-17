mixxxdj/mixxx

commit 37c41e5daf19dcac0b148a5d02792f4c15c81078

Notify PlayerInfo class when a track is unloaded

src/mixer/playerinfo.cpp
@@ -166,7 +166,13 @@ void PlayerInfo::updateCurrentPlayingDeck() {
     int oldDeck = m_currentlyPlayingDeck.fetchAndStoreRelease(maxDeck);
     if (maxDeck != oldDeck) {
         emit currentPlayingDeckChanged(maxDeck);
-        emit currentPlayingTrackChanged(getCurrentPlayingTrack());
+        // Note: When starting Auto-DJ "play" might be processed before a new
+        // is track is fully loaded. currentPlayingTrackChanged() is then emitted
+        // later setTrackInfo()
+        TrackPointer pTrack = getCurrentPlayingTrack();
+        if (pTrack) {
+            emit currentPlayingTrackChanged(pTrack);
+        }
     }
 }
 

src/mixer/basetrackplayer.cpp
@@ -312,6 +312,8 @@ TrackPointer BaseTrackPlayerImpl::unloadTrack() {
         return TrackPointer();
     }
 
+    PlayerInfo::instance().setTrackInfo(getGroup(), TrackPointer());
+
     // Save the loops that are currently set in a loop cue. If no loop cue is
     // currently on the track, then create a new one.
     double loopStart = m_pLoopInPoint->get();

commit 8f1079999bf22ad61283018766a48ed4ec9ff911

Reset main window title when all decks stop playing
This fixes regression caused by 37c41e5daf19dcac0b148a5d02792f4c15c81078.

src/mixer/playerinfo.cpp
@@ -171,10 +171,7 @@ void PlayerInfo::updateCurrentPlayingDeck() {
         // Note: When starting Auto-DJ "play" might be processed before a new
         // is track is fully loaded. currentPlayingTrackChanged() is then emitted
         // after setTrackInfo().
-        TrackPointer pTrack = getCurrentPlayingTrack();
-        if (pTrack) {
-            emit currentPlayingTrackChanged(pTrack);
-        }
+        emit currentPlayingTrackChanged(getCurrentPlayingTrack());
     }
 }
 