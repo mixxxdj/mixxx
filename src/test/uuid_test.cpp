5ca10efa3e06b23df80f73f96d3a94a9e25f34bd
Uwe Klotz <uklotz@mixxx.org>
2021-09-19 18:25:44 +0200

    Improve QUuid to QString tests

--- src/test/uuid_test.cpp
+++ src/test/uuid_test.cpp
@@ -6,11 +6,14 @@ class UuidTest : public testing::Test {
 };
 
 TEST_F(UuidTest, uuidToNullableStringWithoutBracesNullTest) {
-    EXPECT_EQ(QString(),
-            uuidToNullableStringWithoutBraces(QUuid()));
+    EXPECT_STREQ(
+            qPrintable(QString{}),
+            qPrintable(uuidToNullableStringWithoutBraces(QUuid{})));
 }
 
 TEST_F(UuidTest, uuidToNullableStringWithoutBracesValidTest) {
-    EXPECT_EQ(QString("93a20486-90fd-4ea5-a869-ef669cacb0b2"),
-            uuidToNullableStringWithoutBraces(QUuid("93a20486-90fd-4ea5-a869-ef669cacb0b2")));
+    const QString validUuidString = QStringLiteral("93a20486-90fd-4ea5-a869-ef669cacb0b2");
+    EXPECT_STREQ(
+            qPrintable(validUuidString),
+            qPrintable(uuidToNullableStringWithoutBraces(QUuid(validUuidString))));
 }
