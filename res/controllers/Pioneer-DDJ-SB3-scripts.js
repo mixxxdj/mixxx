97a9c645963a8c194786df259a2a85e8c3907d01
Javier Vilalta <javier@dancephy.com>
2021-10-02 09:41:31 -0500

    fix line endings

--- res/controllers/Pioneer-DDJ-SB3-scripts.js
+++ res/controllers/Pioneer-DDJ-SB3-scripts.js
@@ -349,7 +349,7 @@ PioneerDDJSB3.Deck = function(deckNumber) {
     if (deckNumber > 2) {
         effectUnitNumber -= 2;
     }
-    
+
     // The Mixxx UI call this Gain, but on the controller the knob is labeled TRIM
     this.gainKnob = new components.Pot({
         unshift: function() {
