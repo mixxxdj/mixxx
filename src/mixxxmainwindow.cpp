24cbdd1472c737590814bdfbd6464e42358ecb7f
Daniel Schürmann <daschuer@mixxx.org>
2021-10-14 00:14:50 +0200

    Hold and keep strong reference to PlayerManager during one function

--- src/mixxxmainwindow.cpp
+++ src/mixxxmainwindow.cpp
@@ -140,8 +140,9 @@ void MixxxMainWindow::initialize() {
     // This allows us to turn off tooltips.
     installEventFilter(m_pCoreServices->getKeyboardEventFilter().get());
 
-    DEBUG_ASSERT(m_pCoreServices->getPlayerManager());
-    const QStringList visualGroups = m_pCoreServices->getPlayerManager()->getVisualPlayerGroups();
+    auto pPlayerManager = m_pCoreServices->getPlayerManager();
+    DEBUG_ASSERT(pPlayerManager);
+    const QStringList visualGroups = pPlayerManager->getVisualPlayerGroups();
     for (const QString& group : visualGroups) {
         m_pVisualsManager->addDeck(group);
     }
@@ -195,7 +196,7 @@ void MixxxMainWindow::initialize() {
             m_pCoreServices->getScreensaverManager(),
             m_pSkinLoader,
             m_pCoreServices->getSoundManager(),
-            m_pCoreServices->getPlayerManager(),
+            pPlayerManager,
             m_pCoreServices->getControllerManager(),
             m_pCoreServices->getVinylControlManager(),
             m_pCoreServices->getLV2Backend(),
@@ -292,19 +293,19 @@ void MixxxMainWindow::initialize() {
     // pointer to it.
     m_pLaunchImage = nullptr;
 
-    connect(m_pCoreServices->getPlayerManager().get(),
+    connect(pPlayerManager.get(),
             &PlayerManager::noMicrophoneInputConfigured,
             this,
             &MixxxMainWindow::slotNoMicrophoneInputConfigured);
-    connect(m_pCoreServices->getPlayerManager().get(),
+    connect(pPlayerManager.get(),
             &PlayerManager::noAuxiliaryInputConfigured,
             this,
             &MixxxMainWindow::slotNoAuxiliaryInputConfigured);
-    connect(m_pCoreServices->getPlayerManager().get(),
+    connect(pPlayerManager.get(),
             &PlayerManager::noDeckPassthroughInputConfigured,
             this,
             &MixxxMainWindow::slotNoDeckPassthroughInputConfigured);
-    connect(m_pCoreServices->getPlayerManager().get(),
+    connect(pPlayerManager.get(),
             &PlayerManager::noVinylControlInputConfigured,
             this,
             &MixxxMainWindow::slotNoVinylControlInputConfigured);
@@ -728,13 +729,14 @@ void MixxxMainWindow::connectMenuBar() {
     }
 #endif
 
-    if (m_pCoreServices->getPlayerManager()) {
-        connect(m_pCoreServices->getPlayerManager().get(),
+    auto pPlayerManager = m_pCoreServices->getPlayerManager();
+    if (pPlayerManager) {
+        connect(pPlayerManager.get(),
                 &PlayerManager::numberOfDecksChanged,
                 m_pMenuBar,
                 &WMainMenuBar::onNumberOfDecksChanged,
                 Qt::UniqueConnection);
-        m_pMenuBar->onNumberOfDecksChanged(m_pCoreServices->getPlayerManager()->numberOfDecks());
+        m_pMenuBar->onNumberOfDecksChanged(pPlayerManager->numberOfDecks());
     }
 
     if (m_pCoreServices->getTrackCollectionManager()) {
@@ -779,7 +781,8 @@ void MixxxMainWindow::connectMenuBar() {
 }
 
 void MixxxMainWindow::slotFileLoadSongPlayer(int deck) {
-    QString group = m_pCoreServices->getPlayerManager()->groupForDeck(deck - 1);
+    auto pPlayerManager = m_pCoreServices->getPlayerManager();
+    QString group = pPlayerManager->groupForDeck(deck - 1);
 
     QString loadTrackText = tr("Load track to Deck %1").arg(QString::number(deck));
     QString deckWarningMessage = tr("Deck %1 is currently playing a track.")
@@ -1115,8 +1118,9 @@ void MixxxMainWindow::checkDirectRendering() {
 bool MixxxMainWindow::confirmExit() {
     bool playing(false);
     bool playingSampler(false);
-    unsigned int deckCount = m_pCoreServices->getPlayerManager()->numDecks();
-    unsigned int samplerCount = m_pCoreServices->getPlayerManager()->numSamplers();
+    auto pPlayerManager = m_pCoreServices->getPlayerManager();
+    unsigned int deckCount = pPlayerManager->numDecks();
+    unsigned int samplerCount = pPlayerManager->numSamplers();
     for (unsigned int i = 0; i < deckCount; ++i) {
         if (ControlObject::toBool(
                     ConfigKey(PlayerManager::groupForDeck(i), "play"))) {
