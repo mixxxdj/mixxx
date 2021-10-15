e5fda817db28d7b9f8499da42dfdd133705d68d6
Daniel Schürmann <daschuer@mixxx.org>
2021-10-16 01:16:44 +0200

    Remove pointless check if group is empty.

--- src/effects/effectchain.cpp
+++ src/effects/effectchain.cpp
@@ -56,9 +56,7 @@ EffectChain::EffectChain(const QString& group,
     m_pControlChainLoaded =
             std::make_unique<ControlObject>(ConfigKey(m_group, "loaded"));
     m_pControlChainLoaded->setReadOnly();
-    if (group != QString()) {
-        m_pControlChainLoaded->forceSet(1.0);
-    }
+    m_pControlChainLoaded->forceSet(1.0);
 
     m_pControlChainEnabled =
             std::make_unique<ControlPushButton>(ConfigKey(m_group, "enabled"));
