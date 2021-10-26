8f22757faa924009d7be27cffa8493e76a8b088b
Jan Holthuis <jholthuis@mixxx.org>
2021-10-26 14:11:29 +0200

    WaveformRenderBeat: Pass QVector directly to `drawLines()`

--- src/waveform/renderers/waveformrenderbeat.cpp
+++ src/waveform/renderers/waveformrenderbeat.cpp
@@ -118,7 +118,7 @@ void WaveformRenderBeat::draw(QPainter* painter, QPaintEvent* /*event*/) {
         painter->setPen(pen);
 
         // Make sure to use constData to prevent detaches!
-        painter->drawLines(m_beats.constData(), m_beats.size());
+        painter->drawLines(m_beats);
     }
 
     if (!m_downbeats.isEmpty()) {
@@ -127,6 +127,6 @@ void WaveformRenderBeat::draw(QPainter* painter, QPaintEvent* /*event*/) {
         painter->setPen(pen);
 
         // Make sure to use constData to prevent detaches!
-        painter->drawLines(m_downbeats.constData(), m_downbeats.size());
+        painter->drawLines(m_downbeats);
     }
 }
