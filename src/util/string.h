0171cc39b1393c7ea4e93f0d7303b5bf31d14e6b
ronso0 <ronso0@mixxx.org>
2021-11-23 00:16:31 +0100

    add QFileDialog helper filePathWithSelectedExtension()

--- src/util/string.h
+++ src/util/string.h
@@ -2,9 +2,17 @@
 
 #include <QCollator>
 #include <QColor>
+#include <QFileInfo>
 #include <QLocale>
+#include <QRegularExpression>
 #include <QString>
 #include <QStringRef>
+
+namespace {
+
+static const QRegularExpression kExtRgxp(R"(\(\*\.(.*)\)$)");
+
+} //anonymous namespace
 
 // The default comparison of strings for sorting.
 class StringCollator {
@@ -36,4 +44,30 @@ inline QString coloredLinkString(
     return QStringLiteral("<a style=\"color:") + color.name() +
             QStringLiteral(";\" href=\"") + baseUrl + extUrl +
             QStringLiteral("\">") + text + QStringLiteral("</a>");
+}
+
+// Check if the extension from the file filter was added to the file base name.
+// Otherwise add it manually.
+// Works around https://bugreports.qt.io/browse/QTBUG-27186
+inline QString filePathWithSelectedExtension(const QString& fileLocationInput,
+        const QString& fileFilter) {
+    if (fileLocationInput.isEmpty()) {
+        return {};
+    }
+    QString fileLocation = fileLocationInput;
+    if (fileFilter.isEmpty()) {
+        return fileLocation;
+    }
+
+    // Extract 'ext' from QFileDialog file filter string 'Funky type (*.ext)'
+    QRegularExpressionMatch extMatch = kExtRgxp.match(fileFilter);
+    if (!extMatch.hasMatch()) {
+        return fileLocation;
+    }
+    const QString ext = extMatch.captured(1);
+    QFileInfo fileName(fileLocation);
+    if (!ext.isEmpty() && fileName.suffix() != ext) {
+        fileLocation.append(".").append(ext);
+    }
+    return fileLocation;
 }
