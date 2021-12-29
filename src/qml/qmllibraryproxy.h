e68a4d6b1352c193167a0da5b740a2329937367b
Jan Holthuis <jholthuis@mixxx.org>
2021-12-30 00:57:24 +0100

    QML: Don't use parented_ptr for model in QmlLibraryProxy
    The type needs to match the Q_PROPERTY declaration.

--- src/qml/qmllibraryproxy.h
+++ src/qml/qmllibraryproxy.h
@@ -14,14 +14,16 @@ class QmlLibraryTrackListModel;
 
 class QmlLibraryProxy : public QObject {
     Q_OBJECT
-    Q_PROPERTY(mixxx::qml::QmlLibraryTrackListModel* model MEMBER m_pModel CONSTANT)
+    Q_PROPERTY(mixxx::qml::QmlLibraryTrackListModel* model MEMBER m_pModelProperty CONSTANT)
 
   public:
     explicit QmlLibraryProxy(std::shared_ptr<Library> pLibrary, QObject* parent = nullptr);
 
   private:
     std::shared_ptr<Library> m_pLibrary;
-    parented_ptr<QmlLibraryTrackListModel> m_pModel;
+
+    /// This needs to be a plain pointer because it's used as a `Q_PROPERTY` member variable.
+    QmlLibraryTrackListModel* m_pModelProperty;
 };
 
 } // namespace qml
