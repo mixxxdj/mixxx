050ccbef5d768a232b9dd08eeb5000f8340b3cd9
Jan Holthuis <jholthuis@mixxx.org>
2022-01-10 23:51:41 +0100

    MathUtils: Fix eslint issues and add missing jsdoc comment

--- res/qml/Mixxx/MathUtils.mjs
+++ res/qml/Mixxx/MathUtils.mjs
@@ -1,3 +1,9 @@
-export function clamp(value, min, max) {
+/**
+ * @param {number} value Value
+ * @param {number} min Lower bound
+ * @param {number} max Upper bound
+ * @returns {number} Value clamped between min and max
+ */
+export const clamp = function(value, min, max) {
     return Math.max(Math.min(value, max), min);
 };
