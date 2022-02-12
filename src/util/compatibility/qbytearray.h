c8d9e22fcaaeea41b37ab281d4e3a2385e24954c
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2022-02-12 21:59:05 +0100

    Use passing by reference, in QT6 compatibility wrapper for QByteArray.replace

--- src/util/compatibility/qbytearray.h
+++ src/util/compatibility/qbytearray.h
@@ -3,16 +3,16 @@
 #include <QByteArray>
 
 inline QByteArray& qByteArrayReplaceWithPositionAndSize(QByteArray* qbytearray,
-        qsizetype index,
-        qsizetype len,
+        const qsizetype& index,
+        const qsizetype& len,
         const char* s,
-        qsizetype alen) {
+        const qsizetype& alen) {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     return qbytearray->replace(index, len, s, alen);
 #else
     return qbytearray->replace(static_cast<int>(index),
             static_cast<int>(len),
-            static_cast<const char*>(s),
+            s,
             static_cast<int>(alen));
 #endif
 }
