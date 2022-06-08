92e35a04f4ebb928d0c726cc251acb9e97511337
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2022-06-08 21:52:00 +0200

    Documented engine API functions

--- res/controllers/engine-api.d.ts
+++ res/controllers/engine-api.d.ts
@@ -1,62 +1,211 @@
 
-
 declare class ControllerScriptInterfaceLegacy {
-        public getValue(group : string, name : string) : number;
-        public setValue(group : string, name : string, newValue: number) : void;
-        public softTakeover(group : string, name : string, set : boolean) : void;
-        public getParameter(group : string, name : string) : number;
-        public setParameter(group : string, name : string, newValue : number) : void;
-        public getParameterForValue(group : string, name : string, value : number) : number;
-        public reset(group : string, name : string) : void;
-        public getDefaultValue(group : string, name : string) : number;
-        public getDefaultParameter(group : string, name : string) : number;
+    /**
+     * Gets the control value
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @returns Value of the control (within it's range according Mixxx Controls manual page)
+     */
+    public getValue(group:string, name:string):number;
 
-        /**
-         * Callback function called from the engine, when the value of a specified control changes
-         * @callback controlCallback
-         * @param {number} value New value of the connected control
-         * @param {string} group Group of the control e.g. "[Channel1]"
-         * @param {string} name Name of the control e.g. "play_indicator"
-         */
+    /**
+     * Sets a control value 
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @param newValue Value to be set (within it's range according Mixxx Controls manual page)
+     */
+    public setValue(group:string, name:string, newValue: number):void;
 
-        /** Connects a specified Mixxx Control with a callback function, which is executed if the value of the control changes
-         * @details This connection has a FIFO buffer - all value change events are processed in serial order.
-         * @param {string} group Group of the control e.g. "[Channel1]"
-         * @param {string} name Name of the control e.g. "play_indicator"
-         * @param {controlCallback} callback JS function, which will be called everytime, the value of the connected control changes.
-         */
-        public  makeConnection(group : string, name : string, callback : controlCallback);
+    /**
+     * Gets the control value normalized to a range of 0..1
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @returns Value of the control normalized to range of 0..1
+     */
+    public getParameter(group:string, name:string):number;
 
-        /** Connects a specified Mixxx Control with a callback function, which is executed if the value of the control changes
-         * @details This connection is unbuffered - when value change events occur faster, than the mapping can process them,
-         *          only the last value set for the control is processed.
-         * @param {string} group Group of the control e.g. "[Channel1]"
-         * @param {string} name Name of the control e.g. "VuMeter"
-         * @param {controlCallback} callback JS function, which will be called everytime, the value of the connected control changes.
-         */
-        public  makeUnbufferedConnection(group : string, name : string, callback : controlCallback);
-        /** @deprecated Use makeConnection instead */
-        public  connectControl(group : string, name : string, passedCallback : controlCallback, disconnect:boolean = false);
+    /**
+     * Sets the control value specified with normalized range of 0..1
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @param newValue Value to be set, normalized to a range of 0..1
+     */
+    public setParameter(group:string, name:string, newValue:number):void;
 
-        /** Called indirectly by the objects returned by connectControl */
-        public trigger(group : string, name : string) : void;
+    /**
+     * Normalizes a specified value using the range of the given control,
+     * to the range of 0..1
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @param value Value with the controls range according Mixxx Controls manual page
+     * @returns Value normalized to range of 0..1
+     */
+    public getParameterForValue(group:string, name:string, value:number):number;
 
-        /** @deprecated Use console.log instead */
-        public log(message : string) : void;
-        /**
-         * Callback function called from the engine, when the the interval of a timer is reached
-         * @callback timerCallback
-         */
-        public beginTimer(interval:number, scriptCode : timerCallback, oneShot:boolean = false);
-        public stopTimer(timerId : number) : void;
-        public scratchEnable(deck : number, intervalsPerRev : number, rpm : number, alpha : number, beta : number, ramp:boolean = true) : void;
-        public scratchTick(deck : number, interval : number) : void;
-        public scratchDisable(deck : number, ramp:boolean = true) : void;
-        public isScratching(deck : number) : boolean;
-        public softTakeover(group : string, name : string, set : boolean) : void;
-        public softTakeoverIgnoreNextValue(group : string, name : string) : void;
-        public brake(deck : number, activate : boolean, factor:number = 1.0, rate:number = 1.0) : void;
-        public spinback(deck : number, activate : boolean, factor:number = 1.8, rate:number = -10.0) : void;
-        public softStart(deck : number, activate : boolean, factor:number = 1.0) : void;
-    }
+    /**
+     * Resets the control to its default value
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     */
+    public reset(group:string, name:string):void;
+
+    /**
+     * Returns the default value of a control
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @returns Default value with the controls range according Mixxx Controls manual page
+     */
+    public getDefaultValue(group:string, name:string):number;
+
+    /**
+     * Returns the default value of a control, normalized to a range of 0..1
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @returns Default value of the specified control normalized to range of 0..1
+     */
+    public getDefaultParameter(group:string, name:string):number;
+
+    /** Connects a specified Mixxx Control with a callback function, which is executed if the value of the control changes
+     * @details This connection has a FIFO buffer - all value change events are processed in serial order.
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     * @param  callback JS function, which will be called everytime, the value of the connected control changes.
+     */
+    public makeConnection(group:string, name:string, callback:(value:number, group:string, name:string)=>any):any;
+
+    /** Connects a specified Mixxx Control with a callback function, which is executed if the value of the control changes
+     * @details This connection is unbuffered - when value change events occur faster, than the mapping can process them,
+     *          only the last value set for the control is processed.
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "VuMeter"
+     * @param callback JS function, which will be called everytime, the value of the connected control changes.
+     */
+    public  makeUnbufferedConnection(group:string, name:string, callback:(value:number, group:string, name:string)=>any):any;
+
+    /** @deprecated Use makeConnection instead */
+    public  connectControl(group:string, name:string, passedCallback:(value:number, group:string, name:string)=>any, disconnect:boolean = false):any;
+
+
+    /**
+     * Triggers the execution of the connected callback functions, with the actual value of a control
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "play_indicator"
+     */
+    public trigger(group:string, name:string):void;
+
+    /** @deprecated Use console.log instead */
+    public log(message:string):void;
+
+    /**
+     * Starts a timer that will call the specified script function
+     * @param interval Time in milliseconds until the function is executed
+     * @param scriptCode Function to be executed,
+     *                   you can also use closures as:
+     *                   function() { print("Executed Timer") }
+     * @param oneShot If true the function is only once,
+     *                if false the function is executed repeatedly  
+     * @returns timerId which is needed to stop a timer.
+     *          In case of an error, 0 is returned.
+     */
+    public beginTimer(interval:number, scriptCode:()=>any, oneShot:boolean = false):number;
+
+    /**
+     * Stops the specified timer
+     * @param timerId ID of the timer
+     */
+    public stopTimer(timerId:number):void;
+
+    /**
+     * Jogwheel function to be called when scratching starts (usually when the wheel is touched)
+     * This function contains an parametrizeable alpha-beta filter, which influences the
+     * responsiveness and looseness of the imaginary slip mat
+     * @param deck The deck number to use, e.g: 1
+     * @param intervalsPerRev The resolution of the MIDI control (in intervals per revolution, typically 128.)
+     * @param rpm The speed of the imaginary record at 0% pitch (in revolutions per minute (RPM) typically 33+1/3, adjust for comfort)
+     * @param alpha The alpha coefficient of the filter (start with 1/8 (0.125) and tune from there)
+     * @param beta The beta coefficient of the filter (start with alpha/32 and tune from there)
+     * @param ramp Set true to ramp the deck speed down. Set false to stop instantly
+     */
+    public scratchEnable(deck:number, intervalsPerRev:number, rpm:number, alpha:number, beta:number, ramp:boolean = true):void;
+
+    /**
+     * Function to be called each time the jogwheel is moved during scratching
+     * @param deck The deck number to use, e.g: 1
+     * @param interval The movement value (typically 1 for one "tick" forwards, -1 for one "tick" backwards)
+     */
+    public scratchTick(deck:number, interval:number):void;
+
+    /**
+     * Jogwheel function to be called when scratching ends (usually when the wheel is released)
+     * @param deck The deck number to use, e.g: 1
+     * @param ramp  Set true to ramp the deck speed up. Set false to jump to normal play speed instantly
+     */
+    public scratchDisable(deck:number, ramp:boolean = true):void;
+
+    /**
+     * Returns true if scratching is enabled
+     * @param deck The deck number to use, e.g: 1
+     * @returns True if scratching is enabled
+     */
+    public isScratching(deck:number):boolean;
+
+    /**
+     * If enabled, soft-takeover prevents sudden wide parameter changes,
+     * when on-screen control and hardware control divert.
+     * With soft-takeover you need to turn a hardware knob, until it reachs
+     * the position of the on-screen knob - than it takes over control.
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "pregain"
+     * @param enable Set true to enable soft-takeover for the specified control
+     */
+    public softTakeover(group:string, name:string, enable:boolean):void;
+
+    /**
+     * Inhibits a suden value change from the hardware control.
+     * Should be called when receiving input for the knob/fader,
+     * that switches its behavior (e.g. Shift-Button pressed)
+     * @param group Group of the control e.g. "[Channel1]"
+     * @param name Name of the control e.g. "pregain"
+     */
+    public softTakeoverIgnoreNextValue(group:string, name:string):void;
+
+    /**
+     * To achieve a brake effect of the playback speed
+     * Both engine.softStart() and engine.brake()/engine.spinback() can interrupt each other.
+     * @param deck The deck number to use, e.g: 1
+     * @param activate Set true to activate, or false to disable
+     * @param factor Defines how quickly the deck should come to a stop.
+     *               Start with a value of 1 and increase to increase the acceleration.
+     *               Be aware that brake called with low factors (about 0.5 and lower),
+     *               would keep the deck running altough the resulting very low sounds are not audible anymore.
+     * @param rate The initial speed of the deck when enabled. "1" (default) means 10x speed in forward.
+     *             Negative values like "-1" also work, though then it's spinning reverse obviously.
+     */
+    public brake(deck:number, activate:boolean, factor:number = 1.0, rate:number = 1.0):void;
+
+    /**
+     * To achieve a spinback effect of the playback speed
+     * Both engine.softStart() and engine.brake()/engine.spinback() can interrupt each other.
+     * @param deck The deck number to use, e.g: 1
+     * @param activate Set true to activate, or false to disable
+     * @param factor Defines how quickly the deck should come to normal playback rate.
+     *               Start with a value of 1 and increase to increase the acceleration.
+     *               Be aware that spinback called with low factors (about 0.5 and lower),
+     *               would keep the deck running altough the resulting very low sounds are not audible anymore.
+     * @param rate The initial speed of the deck when enabled. "-10" (default) means 10x speed in reverse.
+     *             Positive values like "10" also work, though then it's spinning forward obviously.
+     */
+    public spinback(deck:number, activate:boolean, factor:number = 1.8, rate:number = -10.0):void;
+
+    /**
+     * To achieve a forward acceleration effect from standstill to normal speed.
+     * Both engine.softStart() and engine.brake()/engine.spinback() can interrupt each other.
+     * @param deck The deck number to use, e.g: 1
+     * @param activate Set true to activate, or false to disable
+     * @param factor Defines how quickly the deck should come to normal playback rate.
+     *               Start with a value of 1 and increase to increase the acceleration. 
+     *               SoftStart with low factors would take a while until sound is audible.
+     */
+    public softStart(deck:number, activate:boolean, factor:number = 1.0):void;
+}
 var engine = new ControllerScriptInterfaceLegacy;
