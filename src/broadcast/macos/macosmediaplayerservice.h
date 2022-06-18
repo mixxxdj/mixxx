9c714f7a552150c6cd46b1947d5ef4a2287e3656
fwcd <fwcdmail@gmail.com>
2022-06-18 02:45:49 +0200

    MacOSMediaPlayerService: Add MacOSMediaPlayerService
    - Stub out MacOSMediaPlayerService
    - Broadcast now playing info to MPNowPlayingInfoCenter
    - Wire up MacOSMediaPlayerService
    - Remove unused dependency
    - Set now playing playback state
    - Define __MACOS_MEDIAPLAYER__ publicly
    - Set asset URL and media type for now playing
    - Set some dummy values for playback rate and time
    - Set MPNowPlayingInfoPropertyIsLiveStream
    - Register play command in MPRemoteCommandCenter
    - Successfully load cover art asynchronously to macOS media player
    - Handle case where broadcasted track is null

---
+++ src/broadcast/macos/macosmediaplayerservice.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QObject.h>
+
+#include "library/coverart.h"
+#include "mixer/playermanager.h"
+#include "util/cache.h"
+
+// TODO: Implement ScrobblingService once merged
+class MacOSMediaPlayerService : public QObject {
+    Q_OBJECT
+  public:
+    MacOSMediaPlayerService();
+  public slots:
+    void slotBroadcastCurrentTrack(TrackPointer pTrack); // TODO: override
+    void slotAllTracksPaused();                          // TODO: override
+  private slots:
+    void slotCoverFound(const QObject* pRequestor,
+            const CoverInfo& coverInfo,
+            const QPixmap& pixmap,
+            mixxx::cache_key_t requestedImageHash,
+            bool coverInfoUpdated);
+};
