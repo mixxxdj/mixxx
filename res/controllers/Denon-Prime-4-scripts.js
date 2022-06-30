9b8214e56df5f6f6dd23e1ba70ec19c6660c585e
whanake-music <whanake.music@gmail.com>
2022-06-30 17:38:16 +1200

    add quick FX knob support, add todo list to .js file

--- res/controllers/Denon-Prime-4-scripts.js
+++ res/controllers/Denon-Prime-4-scripts.js
@@ -25,3 +25,50 @@ Prime4.Deck = function(deckNumbers, midiChannel) {
 };
 
 Prime4.Deck.prototype = new components.Deck();
+
+// COMPONENTS TO IMPLEMENT:
+//
+// MIXER SECTION
+// Crossfader assignment switches (Deck 1, Deck 2, Deck 3, Deck 4)
+// Sweep FX buttons (FILTER, ECHO, WASH, NOISE)
+// Navigation buttons (BACK, FWD, LOAD (Deck 1, Deck 2))
+// Encoder knob click
+// VIEW button (Library fullscreen?)
+//
+// DECK 1 SECTION
+// SHIFT + Play/Stop button 
+// CUE button 
+// SYNC button
+// BEAT JUMP buttons
+// SKIP buttons
+// Deck 1-3 toggle buttons
+// CENSOR button
+// LOOP encoder
+// LOOP buttons
+// SLIP button
+// VINYL button
+// KEY LOCK + SYNC + RESET
+// Pitch slider
+// PITCH BEND buttons
+// BEAT GRID buttons
+// PARAMETER buttons
+// Jog wheel
+// HOT CUE mode
+// LOOP mode
+// AUTOLOOP mode
+// ROLL mode
+// SLICER mode
+// SLICER LOOP mode
+// Performance pads (1, 2, 3, 4, 5, 6, 7, 8)
+//
+//
+// FX SELECT knob
+// FX PARAMETER knob
+// FX FREQUENCY knob
+// FX WET/DRY knob
+// ON button
+// PARAM button
+// RESET button
+// BEATS buttons (<, >)
+//
+// NOTE: For now, FX buttons currently enable FX 1, 2 and 3 from effect units 1 and 2.
