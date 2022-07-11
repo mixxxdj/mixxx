ccb96508c38dbbd4b34eb136928c51743c5ab451
David Chocholaty <david.chocholaty@seznam.cz>
2022-07-11 08:33:03 +0200

    RingDelayBufferTest: add tests for a ring buffer
    The commit adds tests for the ring buffer for handling delay.
    The test set includes tests for testing the RingDelayBuffer::isEmpty,
    RingDelayBuffer::isFull, RingDelayBuffer::clear, then tests
    for checking the number of available items for reading and writing
    and at last tests for reading and writing without skipping the position
    with the read position and including skip with the read position
    on both sides and with both variants (circle around / not circle
    around).

---
+++ src/test/ringdelaybuffer_test.cpp
@@ -0,0 +1,201 @@
+// Tests for ringdelaybuffer.h
+
+#include "util/ringdelaybuffer.h"
+
+#include <gtest/gtest.h>
+
+#include <QTest>
+
+#include "test/mixxxtest.h"
+#include "util/samplebuffer.h"
+#include "util/types.h"
+
+namespace {
+
+class RingDelayBufferTest : public MixxxTest {
+  protected:
+    void SetUp() override {
+        m_pRingDelayBuffer = new RingDelayBuffer(m_ringDelayBufferSize);
+    }
+
+    void TearDown() override {
+        delete m_pRingDelayBuffer;
+    }
+
+    void AssertIdenticalBufferEquals(const CSAMPLE* pBuffer,
+            int iBufferLen,
+            const CSAMPLE* pReferenceBuffer,
+            int iReferenceBufferLength) {
+        EXPECT_EQ(iBufferLen, iReferenceBufferLength);
+
+        for (int i = 0; i < iBufferLen; ++i) {
+            EXPECT_FLOAT_EQ(pBuffer[i], pReferenceBuffer[i]);
+        }
+    }
+
+    RingDelayBuffer* m_pRingDelayBuffer;
+    const SINT m_ringDelayBufferSize = 8;
+};
+
+TEST_F(RingDelayBufferTest, ReadWriteNoSkipTest) {
+    const SINT numSamplesHalf = 2;
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+    const CSAMPLE inputBufferHalf[] = {-98.0, 98.0};
+    const CSAMPLE firstExpectedResult[] = {-100.0, 100.0};
+    const CSAMPLE secondExpectedResult[] = {-99.0, 99.0, -98.0, 98.0};
+    const CSAMPLE thirdExpectedResult[] = {-100.0, 100.0, -99.0, 99.0};
+
+    mixxx::SampleBuffer halfSizeOutput(numSamplesHalf);
+    mixxx::SampleBuffer output(numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->write(inputBuffer, numSamples), numSamples);
+    EXPECT_EQ(m_pRingDelayBuffer->read(halfSizeOutput.data(), numSamplesHalf), numSamplesHalf);
+
+    AssertIdenticalBufferEquals(halfSizeOutput.data(),
+            numSamplesHalf,
+            firstExpectedResult,
+            numSamplesHalf);
+
+    EXPECT_EQ(m_pRingDelayBuffer->write(inputBufferHalf, numSamplesHalf), numSamplesHalf);
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, secondExpectedResult, numSamples);
+
+    // Write and read over one ring.
+    EXPECT_EQ(m_pRingDelayBuffer->write(inputBuffer, numSamples), numSamples);
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, thirdExpectedResult, numSamples);
+}
+
+TEST_F(RingDelayBufferTest, ReadWriteSkipTest) {
+    const SINT numSamplesHalf = 2;
+    const SINT numSamples = 4;
+    const SINT firstJumpSizeLeft = -6;
+    const SINT secondJumpSizeLeft = -1;
+    const SINT firstJumpSizeRight = 5;
+    const SINT secondJumpSizeRight = 1;
+    const CSAMPLE firstInputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+    const CSAMPLE secondInputBuffer[] = {-98.0, 98.0, -97.0, 97.0};
+    const CSAMPLE firstExpectedResult[] = {-100.0, 100.0, -99.0, 99.0};
+    const CSAMPLE secondExpectedResult[] = {0.0, 0.0, -100.0, 100.0};
+    const CSAMPLE thirdExpectedResult[] = {100.0, -99.0, 99.0, -100.0};
+    const CSAMPLE fourthExpectedResult[] = {-97.0, 97.0, -98.0, 98.0};
+    const CSAMPLE fifthExpectedResult[] = {97.0, -98.0, 98.0, -97.0};
+
+    mixxx::SampleBuffer halfSizeOutput(numSamplesHalf);
+    mixxx::SampleBuffer output(numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->write(firstInputBuffer, numSamples), numSamples);
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, firstExpectedResult, numSamples);
+
+    // Move read position to the left (circle around).
+    EXPECT_EQ(m_pRingDelayBuffer->moveReadPositionBy(firstJumpSizeLeft), firstJumpSizeLeft);
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, secondExpectedResult, numSamples);
+
+    // Fill the second half of the delay buffer with the first input data.
+    EXPECT_EQ(m_pRingDelayBuffer->write(firstInputBuffer, numSamples), numSamples);
+
+    // Move read position to the left (not circle around).
+    EXPECT_EQ(m_pRingDelayBuffer->moveReadPositionBy(secondJumpSizeLeft), secondJumpSizeLeft);
+
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, thirdExpectedResult, numSamples);
+
+    // For the second cycle fill the first half of the delay buffer
+    // with second input data. Data are changed based on the testing
+    // use case.
+    EXPECT_EQ(m_pRingDelayBuffer->write(secondInputBuffer, numSamples), numSamples);
+
+    // Move read position to the right (circle around).
+    EXPECT_EQ(m_pRingDelayBuffer->moveReadPositionBy(firstJumpSizeRight), firstJumpSizeRight);
+
+    // Fill the second half of the delay buffer with the second input data.
+    EXPECT_EQ(m_pRingDelayBuffer->write(secondInputBuffer, numSamples), numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, fourthExpectedResult, numSamples);
+
+    // Move read position to the right (not circle around).
+    EXPECT_EQ(m_pRingDelayBuffer->moveReadPositionBy(secondJumpSizeRight), secondJumpSizeRight);
+
+    // Fill the first half of the delay buffer with the second input data.
+    EXPECT_EQ(m_pRingDelayBuffer->write(secondInputBuffer, numSamples), numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->read(output.data(), numSamples), numSamples);
+
+    AssertIdenticalBufferEquals(output.data(), numSamples, fifthExpectedResult, numSamples);
+}
+
+TEST_F(RingDelayBufferTest, IsEmptyTest) {
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+
+    EXPECT_TRUE(m_pRingDelayBuffer->isEmpty());
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_FALSE(m_pRingDelayBuffer->isEmpty());
+}
+
+TEST_F(RingDelayBufferTest, ClearTest) {
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+
+    EXPECT_TRUE(m_pRingDelayBuffer->isEmpty());
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_FALSE(m_pRingDelayBuffer->isEmpty());
+    m_pRingDelayBuffer->clear();
+    EXPECT_TRUE(m_pRingDelayBuffer->isEmpty());
+}
+
+TEST_F(RingDelayBufferTest, IsFullTest) {
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+
+    EXPECT_FALSE(m_pRingDelayBuffer->isFull());
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_FALSE(m_pRingDelayBuffer->isFull());
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_TRUE(m_pRingDelayBuffer->isFull());
+    m_pRingDelayBuffer->clear();
+
+    EXPECT_FALSE(m_pRingDelayBuffer->isFull());
+}
+
+TEST_F(RingDelayBufferTest, ReadAvailableItems) {
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+
+    EXPECT_EQ(m_pRingDelayBuffer->getReadAvailable(), 0);
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->getReadAvailable(), numSamples);
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->getReadAvailable(), 2 * numSamples);
+}
+
+TEST_F(RingDelayBufferTest, WriteAvailableItems) {
+    const SINT numSamples = 4;
+    const CSAMPLE inputBuffer[] = {-100.0, 100.0, -99.0, 99.0};
+
+    EXPECT_EQ(m_pRingDelayBuffer->getWriteAvailable(), m_ringDelayBufferSize);
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->getWriteAvailable(), m_ringDelayBufferSize - numSamples);
+    m_pRingDelayBuffer->write(inputBuffer, numSamples);
+
+    EXPECT_EQ(m_pRingDelayBuffer->getWriteAvailable(), 0);
+}
+
+} // namespace
