ae92d2e35dae140c966d7a28a305dfa2cf28dec1
Jan Koester <jan.koester@gmx.net>
2022-07-12 08:08:21 +0200

    initial prefpage

---
+++ src/preferences/dialog/dlgprefremotecontrol.cpp
@@ -0,0 +1,26 @@
+#include <remote/remote.h>
+
+#include "preferences/dialog/dlgprefremotecontrol.h"
+
+DlgPrefRemoteControl::DlgPrefRemoteControl(QWidget *pParent,UserSettingsPointer  pSettings) : DlgPreferencePage(pParent){
+    
+};
+
+DlgPrefRemoteControl::~DlgPrefRemoteControl(){
+};
+
+QUrl DlgPrefRemoteControl::helpUrl() const {
+    return QUrl();
+}
+
+void DlgPrefRemoteControl::slotUpdate(){
+          
+}
+      
+void DlgPrefRemoteControl::slotApply(){
+          
+}
+
+void DlgPrefRemoteControl::slotResetToDefaults(){
+          
+}
