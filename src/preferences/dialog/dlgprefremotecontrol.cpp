2a5689d4104597a86245c68b2d0ed8ed168e915d
Jan Koester <jan.koester@gmx.net>
2022-07-12 09:33:37 +0200

    initial prefpage

--- src/preferences/dialog/dlgprefremotecontrol.cpp
+++ src/preferences/dialog/dlgprefremotecontrol.cpp
@@ -5,6 +5,12 @@
 DlgPrefRemoteControl::DlgPrefRemoteControl(QWidget *pParent,UserSettingsPointer  pSettings) : DlgPreferencePage(pParent){
     m_pSettings=pSettings;
     setupUi(this);
+    if(QVariant(m_pSettings->get(ConfigKey("[RemoteControl]","addr")).value).toBool()){
+        this->remoteactv->setChecked(true);
+    }
+    this->remoteaddr->setText(m_pSettings->get(ConfigKey("[RemoteControl]","addr")).value);
+    this->remoteport->setText(m_pSettings->get(ConfigKey("[RemoteControl]","port")).value);
+    this->remotepass->setText(m_pSettings->get(ConfigKey("[RemoteControl]","pass")).value);
 };
 
 DlgPrefRemoteControl::~DlgPrefRemoteControl(){
@@ -19,12 +25,21 @@ void DlgPrefRemoteControl::slotUpdate(){
 }
       
 void DlgPrefRemoteControl::slotApply(){
-    m_pSettings->setValue(ConfigKey("RemoteControl","addr"),ConfigValue(remoteaddr->text()));
-    m_pSettings->setValue(ConfigKey("RemoteControl","port"),ConfigValue(remoteport->text()));
-    m_pSettings->setValue(ConfigKey("RemoteControl","pass"),ConfigValue(remotepass->text()));
+    if(this->remoteactv->isChecked()){
+        m_pSettings->set(ConfigKey("[RemoteControl]","addr"),ConfigValue(true));
+    }else{
+        m_pSettings->set(ConfigKey("[RemoteControl]","addr"),ConfigValue(false));
+    }
+    
+    m_pSettings->set(ConfigKey("[RemoteControl]","addr"),ConfigValue(this->remoteaddr->text()));
+    m_pSettings->set(ConfigKey("[RemoteControl]","port"),ConfigValue(this->remoteport->text()));
+    m_pSettings->set(ConfigKey("[RemoteControl]","pass"),ConfigValue(this->remotepass->text()));
     m_pSettings->save();
 }
 
 void DlgPrefRemoteControl::slotResetToDefaults(){
-          
+    this->remoteactv->setChecked(false);
+    this->remoteaddr->setText("0.0.0.0");
+    this->remoteport->setText("8443");
+    this->remotepass->setText("mixxx");      
 }
