e2351edcad45d44069755accd8b6aef2fbc7c3b0
whanake-music <whanake.music@gmail.com>
2022-07-18 09:45:21 +1200

    Prime 4: set 'off: 0x01' globally

--- res/controllers/Denon-Prime-4-scripts.js
+++ res/controllers/Denon-Prime-4-scripts.js
@@ -37,10 +37,10 @@ var Prime4 = {};
 // What default colour would you like each deck to be?
 // (Choose between "red", "yellow", "green", "cyan", "blue", "magenta", or "white")
 const deckColors = [
-    "red",   // Deck 1
-    "red",    // Deck 2
-    "red",     // Deck 3
-    "red",  // Deck 4
+    "cyan",  // Deck 1
+    "cyan",  // Deck 2
+    "magenta",  // Deck 3
+    "magenta",  // Deck 4
 ];
 
 /*
@@ -49,6 +49,9 @@ const deckColors = [
  *      DO NOT EDIT ANYTHING PAST THIS POINT
  *       UNLESS YOU KNOW WHAT YOU'RE DOING.
  */
+
+// 'Off' value sets lights to dim instead of off
+components.Button.prototype.off = 0x01;
 
 Prime4.rgbCode = {
     black: 0,
@@ -76,74 +79,71 @@ Prime4.rgbCode = {
     white: 63,
 };
 
-// 'Off' value sets lights to dim instead of off
-components.Button.prototype.off = 0x01;
-
-// Map RGB Hex values to MIDI values for Prime 4 LEDs
+// Map RGB Hex values to MIDI values for Prime 4's colour palette
 const Prime4ColorMapper = new ColorMapper({
-//    0x000055: 0x01,
-//    0x0000AA: 0x02,
-    0x0000FF: 0x03,
-    //    0x005500: 0x04,
-    //    0x005555: 0x05,
-    //    0x0055AA: 0x06,
-    //    0x0055FF: 0x07,
-    //    0x00AA00: 0x08,
-    //    0x00AA55: 0x09,
-    //    0x00AAAA: 0x0A,
-    //    0x00AAFF: 0x0B,
-    0x00FF00: 0x0C,
-    //    0x00FF55: 0x0D,
-    //    0x00FFAA: 0x0E,
-    0x00FFFF: 0x0F,
-    //    0x550000: 0x10,
-    //    0x550055: 0x11,
-    //    0x5500AA: 0x12,
-    //    0x5500FF: 0x13,
-    //    0x555500: 0x14,
-    //    0x555555: 0x15,
-    //    0x5555AA: 0x16,
-    //    0x5555FF: 0x17,
-    //    0x55AA00: 0x18,
-    //    0x55AA55: 0x19,
-    //    0x55AAAA: 0x1A,
-    //    0x55AAFF: 0x1B,
-    //    0x55FF00: 0x1C,
-    //    0x55FF55: 0x1D,
-    //    0x55FFAA: 0x1E,
-    //    0x55FFFF: 0x1F,
-    //    0xAA0000: 0x20,
-    //    0xAA0055: 0x21,
-    //    0xAA00AA: 0x22,
-    //    0xAA00FF: 0x23,
-    //    0xAA5500: 0x24,
-    //    0xAA5555: 0x25,
-    //    0xAA55AA: 0x26,
-    //    0xAA55FF: 0x27,
-    //    0xAAAA00: 0x28,
-    //    0xAAAA55: 0x29,
-    //    0xAAAAAA: 0x2A,
-    //    0xAAAAFF: 0x2B,
-    //    0xAAFF00: 0x2C,
-    //    0xAAFF55: 0x2D,
-    //    0xAAFFAA: 0x2E,
-    //    0xAAFFFF: 0x2F,
-    0xFF0000: 0x30,
-    //    0xFF0055: 0x31,
-    //    0xFF00AA: 0x32,
-    0xFF00FF: 0x33,
-    0xFF5500: 0x34,
-    //    0xFF5555: 0x35,
-    //    0xFF55AA: 0x36,
-    //    0xFF55FF: 0x37,
-    //    0xFFAA00: 0x38,
-    //    0xFFAA55: 0x39,
-    0xFFAAAA: 0x3A,
-    //    0xFFAAFF: 0x3B,
-    0xFFFF00: 0x3C,
-    //    0xFFFF55: 0x3D,
-    //    0xFFFFAA: 0x3E,
-    0xFFFFFF: 0x3F,
+//  0x000055: 0x01, // dark blue
+//  0x0000AA: 0x02, // dim blue
+    0x0000FF: 0x03, // blue
+    //  0x005500: 0x04, // dark green
+    //  0x005555: 0x05, // dark cyan
+    //  0x0055AA: 0x06,
+    //  0x0055FF: 0x07,
+    //  0x00AA00: 0x08, // dim green
+    //  0x00AA55: 0x09,
+    //  0x00AAAA: 0x0A, // dim cyan
+    //  0x00AAFF: 0x0B,
+    0x00FF00: 0x0C, // green
+    //  0x00FF55: 0x0D,
+    //  0x00FFAA: 0x0E,
+    0x00FFFF: 0x0F, // cyan
+    //  0x550000: 0x10, // dark red
+    //  0x550055: 0x11, // dark magenta
+    //  0x5500AA: 0x12,
+    //  0x5500FF: 0x13,
+    //  0x555500: 0x14, // dark yellow
+    //  0x555555: 0x15, // dark white/grey
+    //  0x5555AA: 0x16,
+    //  0x5555FF: 0x17,
+    //  0x55AA00: 0x18,
+    //  0x55AA55: 0x19,
+    //  0x55AAAA: 0x1A,
+    //  0x55AAFF: 0x1B,
+    //  0x55FF00: 0x1C,
+    //  0x55FF55: 0x1D,
+    //  0x55FFAA: 0x1E,
+    //  0x55FFFF: 0x1F,
+    //  0xAA0000: 0x20, // dim red
+    //  0xAA0055: 0x21,
+    //  0xAA00AA: 0x22, // dim magenta
+    //  0xAA00FF: 0x23,
+    //  0xAA5500: 0x24,
+    //  0xAA5555: 0x25,
+    //  0xAA55AA: 0x26,
+    //  0xAA55FF: 0x27,
+    //  0xAAAA00: 0x28, // dim yellow
+    //  0xAAAA55: 0x29,
+    //  0xAAAAAA: 0x2A, // dim white/grey
+    //  0xAAAAFF: 0x2B,
+    //  0xAAFF00: 0x2C,
+    //  0xAAFF55: 0x2D,
+    //  0xAAFFAA: 0x2E,
+    //  0xAAFFFF: 0x2F,
+    0xFF0000: 0x30, // red
+    //  0xFF0055: 0x31,
+    //  0xFF00AA: 0x32,
+    0xFF00FF: 0x33, // purple
+    0xFF5500: 0x34, // orange
+    //  0xFF5555: 0x35,
+    //  0xFF55AA: 0x36,
+    //  0xFF55FF: 0x37,
+    //  0xFFAA00: 0x38,
+    //  0xFFAA55: 0x39,
+    0xFFAAAA: 0x3A, // pink
+    //  0xFFAAFF: 0x3B,
+    0xFFFF00: 0x3C, // yellow
+    //  0xFFFF55: 0x3D,
+    //  0xFFFFAA: 0x3E,
+    0xFFFFFF: 0x3F, // white
 });
 
 const colDeck = [
@@ -163,9 +163,18 @@ Prime4.init = function() {
     midi.sendShortMsg(0x90, 0x75, 0x00); // Turn off all LEDs
     Prime4.leftDeck = new Prime4.Deck([1, 3], 4);
     Prime4.rightDeck = new Prime4.Deck([2, 4], 5);
+    Prime4.mixerStrip = new components.ComponentContainer();
     components.Button.prototype.isPress = function(channel, control, value, status) {
         return (status & 0xF0) === 0x90;
     };
+    /*
+	Prime4.leftDeck.forEachComponent(function (component) {
+		component.trigger();
+	});
+	Prime4.rightDeck.forEachComponent(function (component) {
+		component.trigger();
+	});
+    */
     midi.sendShortMsg(0x90, 0x0D, colDeckDark[0]); // PFL 1
     midi.sendShortMsg(0x91, 0x0D, colDeckDark[1]); // PFL 2
     midi.sendShortMsg(0x92, 0x0D, colDeckDark[2]); // PFL 3
@@ -191,42 +200,30 @@ Prime4.Deck = function(deckNumbers, midiChannel) {
     this.censorButton = new components.Button({
         midi: [0x90 + midiChannel, 0x01],
         key: "reverseroll",
-        on: 0x7f,
-        off: 0x01,
     });
 
     // Beatjump Backward
     this.bjumpBackButton = new components.Button({
         midi: [0x90 + midiChannel, 0x06],
         key: "beatjump_backward",
-        on: 0x7f,
-        off: 0x01,
     });
 
     // Beatjump Forward
     this.bjumpFwdButton = new components.Button({
         midi: [0x90 + midiChannel, 0x07],
         key: "beatjump_forward",
-        on: 0x7f,
-        off: 0x01,
     });
 
     this.syncButton = new components.SyncButton({
         midi: [0x90 + midiChannel, 0x08],
-        on: 0x7f,
-        off: 0x01,
     });
 
     this.cueButton = new components.CueButton({
         midi: [0x90 + midiChannel, 0x09],
-        on: 0x7f,
-        off: 0x01,
     });
 
     this.playButton = new components.PlayButton({
         midi: [0x90 + midiChannel, 0x0A],
-        on: 0x7f,
-        off: 0x01,
         unshift: function() {
             components.PlayButton.prototype.unshift.call(this);
             this.type = components.Button.prototype.types.toggle;
@@ -266,8 +263,6 @@ Prime4.Deck = function(deckNumbers, midiChannel) {
     this.keylockButton = new components.Button({
         midi: [0x90 + midiChannel, 0x22],
         key: "keylock",
-        on: 0x7f,
-        off: 0x01,
         type: components.Button.prototype.types.toggle,
     });
 
@@ -275,8 +270,6 @@ Prime4.Deck = function(deckNumbers, midiChannel) {
 	this.vinylButton = new components.Button({
 		midi: [0x90 + midiChannel, 0x23],
 		//TODO: Jog Wheel Functionality First
-		on: 0x7f,
-		off: 0x01,
 		type: components.Button.prototype.types.toggle,
 	});
 	*/
@@ -284,23 +277,17 @@ Prime4.Deck = function(deckNumbers, midiChannel) {
     this.slipButton = new components.Button({
         midi: [0x90 + midiChannel, 0x24],
         key: "slip_enabled",
-        on: 0x7f,
-        off: 0x01,
         type: components.Button.prototype.types.toggle,
     });
 
     this.loopInButton = new components.Button({
         midi: [0x90 + midiChannel, 0x25],
         key: "loop_in",
-        on: 0x7f,
-        off: 0x01,
     });
 
     this.loopOutButton = new components.Button({
         midi: [0x90 + midiChannel, 0x26],
         key: "loop_out",
-        on: 0x7f,
-        off: 0x01,
     });
 
     // Change from Deck 3/4 to Deck 1/2
@@ -373,40 +360,3 @@ Prime4.shiftState = function(channel, control, value) {
         Prime4.rightDeck.unshift();
     }
 };
-
-// COMPONENTS TO IMPLEMENT:
-//
-// MIXER SECTION
-// Crossfader assignment switches (Deck 1, Deck 2, Deck 3, Deck 4)
-// Sweep FX buttons (FILTER, ECHO, WASH, NOISE)
-// Navigation buttons (BACK, FWD, LOAD (Deck 1, Deck 2))
-// Encoder knob click
-// VIEW button (Library fullscreen?)
-//
-// DECK 1 SECTION
-// SKIP buttons
-// LOOP encoder
-// VINYL button
-// PITCH BEND buttons
-// BEAT GRID buttons
-// PARAMETER buttons
-// Jog wheel
-// HOT CUE mode
-// LOOP mode
-// AUTOLOOP mode
-// ROLL mode
-// SLICER mode
-// SLICER LOOP mode
-// Performance pads (1, 2, 3, 4, 5, 6, 7, 8)
-//
-//
-// FX SELECT knob
-// FX PARAMETER knob
-// FX FREQUENCY knob
-// FX WET/DRY knob
-// ON button
-// PARAM button
-// RESET button
-// BEATS buttons (<, >)
-//
-// NOTE: For now, FX buttons currently enable FX 1, 2 and 3 from effect units 1 and 2.
