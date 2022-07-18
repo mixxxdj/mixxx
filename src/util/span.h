b0ed62c573787794aea782b1387db43162f5e785
David Chocholaty <david.chocholaty@seznam.cz>
2022-07-18 14:46:11 +0200

    SpanUtil: add castToSizeType method
    This commit adds a method for casting data type of size to data type
    of size_type from std::span. At the same time, the appropriate
    lower bound checking is provided. For closer description,
    the size value can't be negative.

--- src/util/span.h
+++ src/util/span.h
@@ -2,6 +2,8 @@
 
 #include <span>
 
+#include "util/assert.h"
+
 namespace mixxx {
 
 /// The class offers a group of utilities (methods) for working with std::span.
@@ -12,11 +14,20 @@ class SpanUtil {
     /// is used, and the size of the data structure.
     template<typename T, typename S>
     static std::span<T> spanFromPtrLen(T* ptr, S size) {
-        return std::span<T>{ptr, static_cast<typename std::span<T>::size_type>(size)};
+        return std::span<T>{ptr, castToSizeType<T>(size)};
     }
 
   private:
-    // TODO(davidchocholaty) conversion method for SINT with appropriate checking and casting
+    /// The method casts data type of size to data type of size_type from std::span.
+    /// At the same time, the method provides appropriate lower bound checking.
+    template<typename T, typename S, typename T2 = typename std::span<T>::size_type>
+    static T2 castToSizeType(S size) {
+        VERIFY_OR_DEBUG_ASSERT(size >= 0) {
+            size = 0;
+        }
+
+        return static_cast<T2>(size);
+    }
 };
 
 } // namespace mixxx
