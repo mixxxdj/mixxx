1e34da3ceb30b1497bff862fcddc5b88c5425fa8
Daniel Schürmann <daschuer@mixxx.org>
2022-10-12 22:43:08 +0200

    Reformat MockNetworkAccessManager using clang-format

--- src/test/mock_networkaccessmanager.h
+++ src/test/mock_networkaccessmanager.h
@@ -24,8 +24,8 @@
 #include <QNetworkReply>
 #include <QUrl>
 
-#include "test_utils.h"
 #include "gmock/gmock.h"
+#include "test_utils.h"
 
 // Usage:
 // Create a MockNetworkAccessManager.
@@ -34,38 +34,37 @@
 // arrive, call MockNetworkReply::Done().
 
 class MockNetworkReply : public QNetworkReply {
-  Q_OBJECT
- public:
-  MockNetworkReply();
-  MockNetworkReply(const QByteArray& data);
+    Q_OBJECT
+  public:
+    MockNetworkReply();
+    MockNetworkReply(const QByteArray& data);
 
-  // Use these to set expectations.
-  void SetData(const QByteArray& data);
-  virtual void setAttribute(QNetworkRequest::Attribute code, const QVariant& value);
+    // Use these to set expectations.
+    void SetData(const QByteArray& data);
+    virtual void setAttribute(QNetworkRequest::Attribute code, const QVariant& value);
 
-  // Call this when you are ready for the finished() signal.
-  void Done();
+    // Call this when you are ready for the finished() signal.
+    void Done();
 
- protected:
-  MOCK_METHOD0(abort, void());
-  virtual qint64 readData(char* data, qint64);
-  virtual qint64 writeData(const char* data, qint64);
+  protected:
+    MOCK_METHOD0(abort, void());
+    virtual qint64 readData(char* data, qint64);
+    virtual qint64 writeData(const char* data, qint64);
 
-  QByteArray data_;
-  qint64 pos_;
+    QByteArray data_;
+    qint64 pos_;
 };
 
-
 class MockNetworkAccessManager : public QNetworkAccessManager {
-  Q_OBJECT
- public:
-  MockNetworkReply* ExpectGet(
-      const QString& contains,  // A string that should be present in the URL.
-      const QMap<QString, QString>& params,  // Required URL parameters.
-      int status,  // Returned HTTP status code.
-      const QByteArray& ret_data);  // Returned data.
- protected:
-  MOCK_METHOD3(createRequest, QNetworkReply*(Operation, const QNetworkRequest&, QIODevice*));
+    Q_OBJECT
+  public:
+    MockNetworkReply* ExpectGet(
+            const QString& contains,              // A string that should be present in the URL.
+            const QMap<QString, QString>& params, // Required URL parameters.
+            int status,                           // Returned HTTP status code.
+            const QByteArray& ret_data);          // Returned data.
+  protected:
+    MOCK_METHOD3(createRequest, QNetworkReply*(Operation, const QNetworkRequest&, QIODevice*));
 };
 
 #endif
