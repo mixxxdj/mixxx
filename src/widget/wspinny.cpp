7dc151d5e36c5acac9338446f94a7d7c8687bcc7
ronso0 <ronso0@mixxx.org>
2022-10-16 21:56:41 +0200

    WSpinny: ensure DlgCoverArtFullSize has a parent on xcb

--- src/widget/wspinny.cpp
+++ src/widget/wspinny.cpp
@@ -64,7 +64,7 @@ WSpinny::WSpinny(
           m_bClampFailedWarning(false),
           m_bGhostPlayback(false),
           m_pPlayer(pPlayer),
-          m_pDlgCoverArt(new DlgCoverArtFullSize(parent, pPlayer)),
+          m_pDlgCoverArt(new DlgCoverArtFullSize(this, pPlayer)),
           m_pCoverMenu(new WCoverArtMenu(this)) {
 #ifdef __VINYLCONTROL__
     m_pVCManager = pVCMan;
