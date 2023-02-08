a94fe730bfa08315794029d12c707d9032d9970a
ronso0 <ronso0@mixxx.org>
2023-02-08 23:20:10 +0100

    fullscreen: remove Linux menubar workaround
    It appears this is causing issues 1) on recent Ubuntu 22.04 and also 20.04 with the defaut window
    manager, as well as 2) with (unofficial) window manager extensions that enable the so-called 'global menu'
    (menubar is integrated in the window title bar) after toggling fullscreen (e.g. when switching skins
    while in fullscreen mode):
    * 1) 2) menubar hotkeys not working anymore
    * 2) menubar is not restored in the title bar but in the windwo itself

--- src/mixxx.cpp
+++ src/mixxx.cpp
@@ -1429,21 +1429,7 @@ void MixxxMainWindow::slotViewFullScreen(bool toggle) {
 
     if (toggle) {
         showFullScreen();
-#ifdef __LINUX__
-        // Fix for "No menu bar with ubuntu unity in full screen mode" Bug
-        // #885890 and Bug #1076789. Before touching anything here, please read
-        // those bugs.
-        createMenuBar();
-        connectMenuBar();
-        if (m_pMenuBar->isNativeMenuBar()) {
-            m_pMenuBar->setNativeMenuBar(false);
-        }
-#endif
     } else {
-#ifdef __LINUX__
-        createMenuBar();
-        connectMenuBar();
-#endif
         showNormal();
     }
     emit fullScreenChanged(toggle);
