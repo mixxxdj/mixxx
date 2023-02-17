mixxxdj/mixxx

commit fe7b602e653ae177ed11c9f6f2ba24d23680584b

Replace QString with QStringLiteral where possible

src/library/autodj/dlgautodj.cpp
@@ -367,13 +367,12 @@ void DlgAutoDJ::updateSelectionInfo() {
         }
     }
 
-    QString label;
-
     if (!indices.isEmpty()) {
-        label.append(mixxx::DurationBase::formatTime(duration));
-        label.append(QString(" (%1) / ").arg(indices.size()));
         labelSelectionInfo->setToolTip(tr("Displays the duration and number of selected tracks."));
-        labelSelectionInfo->setText(label);
+        labelSelectionInfo->setText(
+                QStringLiteral("%1 (%2) / ")
+                        .arg(mixxx::DurationBase::formatTime(duration),
+                                indices.size()));
         labelSelectionInfo->setEnabled(true);
     } else {
         labelSelectionInfo->setText("");
@@ -387,11 +386,8 @@ void DlgAutoDJ::updateTotalInfo() {
         return;
     }
 
-    QString label;
-    label.append(summary.getTrackDurationText());
-    label.append(QString(" (%1)").arg(summary.getTrackCount()));
-
-    labelTotalInfo->setText(label);
+    labelTotalInfo->setText(QStringLiteral("%1 (%2)").arg(
+            summary.getTrackDurationText(), summary.getTrackCount()));
 }
 
 bool DlgAutoDJ::hasFocus() const {

src/library/autodj/autodjfeature.cpp
@@ -128,7 +128,7 @@ void AutoDJFeature::updateTitle() {
     PlaylistSummary summary;
     if (m_pTrackCollection->playlists().readAutoDJPlaylistSummary(&summary) &&
             summary.getTrackCount() > 0) {
-        title.append(QString(" (%1) %2")
+        title.append(QStringLiteral(" (%1) %2")
                              .arg(QString::number(summary.getTrackCount()),
                                      summary.getTrackDurationText()));
     }

commit 86c9577692d35ae673fb160f831538393cc86f47

Fix regression caused by fe7b602e653ae177ed11c9f6f2ba24d23680584b

src/library/autodj/dlgautodj.cpp
@@ -375,7 +375,7 @@ void DlgAutoDJ::updateSelectionInfo() {
         labelSelectionInfo->setText(
                 QStringLiteral("%1 (%2) / ")
                         .arg(mixxx::DurationBase::formatTime(duration),
-                                indices.size()));
+                                QString::number(indices.size())));
         labelSelectionInfo->setEnabled(true);
     } else {
         labelSelectionInfo->setText("");
@@ -389,8 +389,9 @@ void DlgAutoDJ::updateTotalInfo() {
         return;
     }
 
-    labelTotalInfo->setText(QStringLiteral("%1 (%2)").arg(
-            summary.getTrackDurationText(), summary.getTrackCount()));
+    labelTotalInfo->setText(QStringLiteral("%1 (%2)")
+                                    .arg(summary.getTrackDurationText(),
+                                            QString::number(summary.getTrackCount())));
 }
 
 bool DlgAutoDJ::hasFocus() const {