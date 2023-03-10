61eafbd4e7f85ece38a765341bea4f7754cf42df
fwcd <fwcdmail@gmail.com>
2023-03-10 18:16:11 +0100

    ITunesMacOSImporter: Show all audio playlists

--- src/library/itunes/itunesmacosimporter.mm
+++ src/library/itunes/itunesmacosimporter.mm
@@ -22,77 +22,6 @@ class ImporterImpl {
           m_cancelImport(cancelImport),
           m_persistentIdToDbId([[NSMutableDictionary alloc] init]) {}
 
-    void importPlaylist(ITLibPlaylist* playlist, QSqlQuery& queryInsertToPlaylists,
-                        QSqlQuery& queryInsertToPlaylistTracks,
-                        TreeItem& treeItem) {
-        // TODO: Support folders and smart playlists
-
-        if (playlist.kind != ITLibPlaylistKindRegular) {
-            return;
-        }
-
-        int playlistId = persistentIdToDbId(playlist.persistentID);
-        QString playlistName = [playlist.name cStringUsingEncoding:NSUTF8StringEncoding];
-
-        queryInsertToPlaylists.bindValue(":id", playlistId);
-        queryInsertToPlaylists.bindValue(":name", playlistName);
-
-        // TODO: Handle duplicate playlists by appending id (see xml importer)
-
-        if (!queryInsertToPlaylists.exec()) {
-            LOG_FAILED_QUERY(queryInsertToPlaylists);
-            return;
-        }
-
-        treeItem.appendChild(playlistName);
-
-        int i = 0;
-        for (ITLibMediaItem* item in playlist.items) {
-            if (m_cancelImport) {
-                return;
-            }
-
-            queryInsertToPlaylistTracks.bindValue(":playlist_id", playlistId);
-            queryInsertToPlaylistTracks.bindValue(
-                ":track_id", persistentIdToDbId(item.persistentID));
-            queryInsertToPlaylistTracks.bindValue(":position", i);
-
-            if (!queryInsertToPlaylistTracks.exec()) {
-                LOG_FAILED_QUERY(queryInsertToPlaylistTracks);
-                return;
-            }
-
-            i++;
-        }
-    }
-
-    void importMediaItem(ITLibMediaItem* item, QSqlQuery& query) {
-        query.bindValue(":id", persistentIdToDbId(item.persistentID));
-        query.bindValue(":artist", [item.artist.name cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":title", [item.title cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":album", [item.album.title cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(
-            ":album_artist",
-            [item.album.albumArtist cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":genre", [item.genre cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":grouping", [item.grouping cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":year", static_cast<int>(item.year));
-        query.bindValue(":duration", static_cast<int>(item.totalTime / 1000));
-        query.bindValue(
-            ":location",
-            [[item.location path] cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":rating", static_cast<int>(item.rating / 20));
-        query.bindValue(":comment", [item.comments cStringUsingEncoding:NSUTF8StringEncoding]);
-        query.bindValue(":tracknumber", static_cast<int>(item.trackNumber));
-        query.bindValue(":bpm", static_cast<int>(item.beatsPerMinute));
-        query.bindValue(":bitrate", static_cast<int>(item.bitrate));
-
-        if (!query.exec()) {
-            LOG_FAILED_QUERY(query);
-            return;
-        }
-    }
-
     std::unique_ptr<TreeItem> importPlaylists(NSArray<ITLibPlaylist*>* playlists) {
         std::unique_ptr<TreeItem> rootItem = TreeItem::newRoot(m_parentFeature);
 
@@ -157,6 +86,114 @@ class ImporterImpl {
             int dbId = [m_persistentIdToDbId count];
             m_persistentIdToDbId[persistentId] = [NSNumber numberWithInt:dbId];
             return dbId;
+        }
+    }
+
+    bool isPlaylistShown(ITLibPlaylist* playlist) {
+        // Filter out the primary playlist (since we already show the library
+        // under the main iTunes node)
+        bool isPrimary;
+        if (@available(macOS 12.0, *)) {
+            isPrimary = [playlist isPrimary];
+        } else {
+            isPrimary = [playlist isMaster];
+        }
+        if (isPrimary) {
+            return false;
+        }
+
+        // Filter out folders for now
+        switch (playlist.kind) {
+            case ITLibPlaylistKindFolder:
+                return false;
+            default:
+                break;
+        }
+
+        // Filter out non-audio content
+        switch (playlist.distinguishedKind) {
+            case ITLibDistinguishedPlaylistKindMovies:
+            case ITLibDistinguishedPlaylistKindTVShows:
+            case ITLibDistinguishedPlaylistKindiTunesU:
+            case ITLibDistinguishedPlaylistKindMusicVideos:
+            case ITLibDistinguishedPlaylistKindLibraryMusicVideos:
+            case ITLibDistinguishedPlaylistKindHomeVideos:
+            case ITLibDistinguishedPlaylistKindApplications:
+            case ITLibDistinguishedPlaylistKindMusicShowsAndMovies:
+                return false;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    void importPlaylist(ITLibPlaylist* playlist, QSqlQuery& queryInsertToPlaylists,
+                        QSqlQuery& queryInsertToPlaylistTracks,
+                        TreeItem& treeItem) {
+        if (!isPlaylistShown(playlist)) {
+            return;
+        }
+
+        int playlistId = persistentIdToDbId(playlist.persistentID);
+        QString playlistName = [playlist.name cStringUsingEncoding:NSUTF8StringEncoding];
+
+        queryInsertToPlaylists.bindValue(":id", playlistId);
+        queryInsertToPlaylists.bindValue(":name", playlistName);
+
+        // TODO: Handle duplicate playlists by appending id (see xml importer)
+
+        if (!queryInsertToPlaylists.exec()) {
+            LOG_FAILED_QUERY(queryInsertToPlaylists);
+            return;
+        }
+
+        treeItem.appendChild(playlistName);
+
+        int i = 0;
+        for (ITLibMediaItem* item in playlist.items) {
+            if (m_cancelImport) {
+                return;
+            }
+
+            queryInsertToPlaylistTracks.bindValue(":playlist_id", playlistId);
+            queryInsertToPlaylistTracks.bindValue(
+                ":track_id", persistentIdToDbId(item.persistentID));
+            queryInsertToPlaylistTracks.bindValue(":position", i);
+
+            if (!queryInsertToPlaylistTracks.exec()) {
+                LOG_FAILED_QUERY(queryInsertToPlaylistTracks);
+                return;
+            }
+
+            i++;
+        }
+    }
+
+    void importMediaItem(ITLibMediaItem* item, QSqlQuery& query) {
+        query.bindValue(":id", persistentIdToDbId(item.persistentID));
+        query.bindValue(":artist", [item.artist.name cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":title", [item.title cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":album", [item.album.title cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(
+            ":album_artist",
+            [item.album.albumArtist cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":genre", [item.genre cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":grouping", [item.grouping cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":year", static_cast<int>(item.year));
+        query.bindValue(":duration", static_cast<int>(item.totalTime / 1000));
+        query.bindValue(
+            ":location",
+            [[item.location path] cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":rating", static_cast<int>(item.rating / 20));
+        query.bindValue(":comment", [item.comments cStringUsingEncoding:NSUTF8StringEncoding]);
+        query.bindValue(":tracknumber", static_cast<int>(item.trackNumber));
+        query.bindValue(":bpm", static_cast<int>(item.beatsPerMinute));
+        query.bindValue(":bitrate", static_cast<int>(item.bitrate));
+
+        if (!query.exec()) {
+            LOG_FAILED_QUERY(query);
+            return;
         }
     }
 };
