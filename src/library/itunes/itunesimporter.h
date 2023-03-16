48250c10c4e2e065b4fd56b0ea6bddf6c50c161c
fwcd <fwcdmail@gmail.com>
2023-03-17 00:43:47 +0100

    External libraries: Add todo notes regarding cancellation

--- src/library/itunes/itunesimporter.h
+++ src/library/itunes/itunesimporter.h
@@ -14,4 +14,11 @@ class ITunesImporter {
     virtual ~ITunesImporter() = default;
 
     virtual ITunesImport importLibrary() = 0;
+
+    // TODO: Add thread-safe `cancelImport` method and move `m_cancelImport`
+    // from ITunesFeature to the individual importers (replacing the current
+    // mechanism of passing (the private member) `m_cancelImport` by reference
+    // to each importer). We should then call `cancelImport` from the
+    // `ITunesFeature` destructor on the importer (which we'd need to store e.g.
+    // in a member of `ITunesFeature`).
 };
