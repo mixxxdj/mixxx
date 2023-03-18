ce8df1d30062e20cfa80b60243c53a5a622d0afb
fwcd <fwcdmail@gmail.com>
2023-03-18 03:07:02 +0100

    MacOSMediaPlayerService: Add doc comments

--- src/broadcast/macos/macosmediaplayerservice.h
+++ src/broadcast/macos/macosmediaplayerservice.h
@@ -10,6 +10,10 @@
 #include "util/cache.h"
 
 // TODO: Implement ScrobblingService once merged
+
+/// A service that interfaces with macOS's media controls and allows the user to
+/// view and control the playback through the system-wide control center, media
+/// keys etc.
 class MacOSMediaPlayerService : public QObject {
     Q_OBJECT
   public:
@@ -17,7 +21,9 @@ class MacOSMediaPlayerService : public QObject {
     ~MacOSMediaPlayerService() override;
 
   public slots:
+    /// Sends the updated track info to the external 'now playing' center.
     void slotBroadcastCurrentTrack(TrackPointer pTrack); // TODO: override
+    /// Notifies the external 'now playing' center that all tracks are paused.
     void slotAllTracksPaused();                          // TODO: override
 
   private:
@@ -26,18 +32,28 @@ class MacOSMediaPlayerService : public QObject {
     ControlProxy* m_pCPFadeNow;
     double m_lastSentPosition;
 
+    /// Fetches the currently playing deck. Returns a `nullptr` if nothing plays.
     DeckAttributes* getCurrentDeck();
+    /// Checks whether the given deck is the currently playing one.
     bool isCurrentDeck(DeckAttributes* attributes);
 
+    /// Called when the play state is toggled externally. Returns whether this was successful.
     bool togglePlayState();
+    /// Called when the play state is changed externally. Returns whether this was successful.
     bool updatePlayState(bool playing);
+    /// Called when the play position is changed externally. Returns whether this was successful.
     bool updatePlayPosition(double absolutePosition);
+    /// Called when the user skips to the next track externally. Returns whether
+    /// this was successful.
     bool skipToNextTrack();
 
   private slots:
+    /// Notifies the external 'now playing' center that the play state of some deck has changed.
     void slotPlayChanged(DeckAttributes* attributes, bool playing);
+    /// Notifies the external 'now playing' center that the play position of some deck has changed.
     void slotPlayPositionChanged(DeckAttributes* attributes, double position);
 
+    /// Updates the cover art if needed.
     void slotCoverFound(const QObject* pRequestor,
             const CoverInfo& coverInfo,
             const QPixmap& pixmap);
