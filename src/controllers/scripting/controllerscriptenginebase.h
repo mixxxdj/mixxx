063a39e6b38b6aab5fd6d1b9178bf15dec041844
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2023-04-12 14:01:51 +0200

    feat(controllers): make `--controller-pedantic` accessible via `ControllerScriptEngineBase::isPedantic`

--- src/controllers/scripting/controllerscriptenginebase.h
+++ src/controllers/scripting/controllerscriptenginebase.h
@@ -32,6 +32,10 @@ class ControllerScriptEngineBase : public QObject {
     void showScriptExceptionDialog(const QJSValue& evaluationResult, bool bFatal = false);
     void throwJSError(const QString& message);
 
+    bool isPedantic() const {
+        return m_bPedantic;
+    }
+
     inline void setTesting(bool testing) {
         m_bTesting = testing;
     };
@@ -50,6 +54,8 @@ class ControllerScriptEngineBase : public QObject {
 
     Controller* m_pController;
     const RuntimeLoggingCategory m_logger;
+
+    bool m_bPedantic;
 
     bool m_bTesting;
 
