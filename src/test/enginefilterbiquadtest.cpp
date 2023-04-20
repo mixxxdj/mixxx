c707ecd2d5cb62589ab62949479cb68ccb6d5e2f
Daniel Schürmann <daschuer@mixxx.org>
2023-04-20 10:23:31 +0200

    Add fidlib analysis tests

--- src/test/enginefilterbiquadtest.cpp
+++ src/test/enginefilterbiquadtest.cpp
@@ -19,4 +19,32 @@ TEST_F(EngineFilterBiquadTest, fidspecLengthIsLongEnough) {
     ASSERT_TRUE(FIDSPEC_LENGTH > strlen("LsBq/1.2200000000/-12.0000000000"));
 }
 
+TEST_F(EngineFilterBiquadTest, analysisPkBq) {
+    char spec_d[FIDSPEC_LENGTH];
+    format_fidspec(spec_d, sizeof(spec_d), "PkBq/%.10f/%.10f", 1.75, 0.0);
+
+    char* desc;
+    FidFilter* filt = fid_design(spec_d, 44100, 1000, 0, 0, &desc);
+    int delay = fid_calc_delay(filt);
+    EXPECT_EQ(delay, 0);
+    double resp0, phase0;
+    resp0 = fid_response_pha(filt, 1000 / 44100, &phase0);
+    EXPECT_DOUBLE_EQ(resp0, 1);
+    EXPECT_DOUBLE_EQ(phase0, 0);
+    free(filt);
+}
+
+TEST_F(EngineFilterBiquadTest, analysisLpBe4) {
+    char* desc;
+    FidFilter* filt = fid_design("LpBe4", 44100, 600, 0, 0, &desc);
+    int delay = fid_calc_delay(filt);
+    EXPECT_EQ(delay, 24);
+
+    double resp0, phase0;
+    resp0 = fid_response_pha(filt, 600 / 44100, &phase0);
+    EXPECT_DOUBLE_EQ(resp0, 1);
+    EXPECT_DOUBLE_EQ(phase0, 0);
+    free(filt);
+}
+
 } // namespace
