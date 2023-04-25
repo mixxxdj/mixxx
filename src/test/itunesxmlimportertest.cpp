52a1139ad16959abeb822fdc9c084ea6b43010f5
fwcd <fwcdmail@gmail.com>
2023-04-25 17:13:31 +0200

    ITunesXMLImporterTest: Test iTunes XML

--- src/test/itunesxmlimportertest.cpp
+++ src/test/itunesxmlimportertest.cpp
@@ -41,13 +41,21 @@ class MockITunesDAO : public ITunesDAO {
 using testing::_;
 using testing::Return;
 
-TEST_F(ITunesXMLImporterTest, ParseMacOSMusicXML) {
+namespace {
+std::unique_ptr<MockITunesDAO> makeMockDAO() {
     std::unique_ptr<MockITunesDAO> dao = std::make_unique<MockITunesDAO>();
 
     ON_CALL(*dao, importTrack(_)).WillByDefault(Return(true));
     ON_CALL(*dao, importPlaylist(_)).WillByDefault(Return(true));
     ON_CALL(*dao, importPlaylistRelation(_, _)).WillByDefault(Return(true));
     ON_CALL(*dao, applyPathMapping(_)).WillByDefault(Return(true));
+
+    return dao;
+}
+} // anonymous namespace
+
+TEST_F(ITunesXMLImporterTest, ParseMacOSMusicXML) {
+    std::unique_ptr<MockITunesDAO> dao = makeMockDAO();
 
     int root = kRootITunesPlaylistId;
     EXPECT_CALL(*dao, importPlaylistRelation(root, 1172)); // Downloaded (built-in playlist)
@@ -61,5 +69,28 @@ TEST_F(ITunesXMLImporterTest, ParseMacOSMusicXML) {
 
     std::unique_ptr<ITunesXMLImporter> importer =
             makeImporter("macOS Music Library.xml", std::move(dao));
+    importer->importLibrary();
+}
+
+TEST_F(ITunesXMLImporterTest, ParseITunesMusicXML) {
+    std::unique_ptr<MockITunesDAO> dao = makeMockDAO();
+
+    int root = kRootITunesPlaylistId;
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 98));  // Downloaded (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 125)); // Downloaded (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 134)); // Downloaded (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 140)); // Podcasts (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 147)); // Audiobooks (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 150)); // Genius (built-in playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 153)); // Folder A
+    EXPECT_CALL(*dao, importPlaylistRelation(153, 159));  // - Folder B
+    EXPECT_CALL(*dao, importPlaylistRelation(159, 165));  //   - Playlist A
+    EXPECT_CALL(*dao, importPlaylistRelation(159, 170));  //   - Playlist B
+    EXPECT_CALL(*dao, importPlaylistRelation(153, 174));  // - Playlist C
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 177)); // Downloaded (smart playlist)
+    EXPECT_CALL(*dao, importPlaylistRelation(root, 186)); // Playlist D
+
+    std::unique_ptr<ITunesXMLImporter> importer =
+            makeImporter("iTunes Music Library.xml", std::move(dao));
     importer->importLibrary();
 }
