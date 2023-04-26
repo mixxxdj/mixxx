9a2a047981297e6ce947ad1e1357b26f6fc21788
Daniel Schürmann <daschuer@mixxx.org>
2023-04-26 22:22:15 +0200

    fix windows_release_buildenv.bat

--- tools/windows_release_buildenv.bat
+++ tools/windows_release_buildenv.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-set DEFINED BUILDENV_RELEASE=TRUE && call "%~dp0windows_buildenv.bat" %* & set DEFINED BUILDENV_RELEASE=
+set BUILDENV_RELEASE=TRUE && call "%~dp0windows_buildenv.bat" %* & set BUILDENV_RELEASE=
