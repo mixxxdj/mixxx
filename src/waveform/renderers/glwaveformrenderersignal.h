396d0fe7ed9e8a5e5a7e06186df8fc07dba5351f
Daniel Schürmann <daschuer@mixxx.org>
2023-05-05 11:32:33 +0200

    Re-create GLWaveformRenderer without inherit from WaveformRendererSignalBase

--- src/waveform/renderers/glwaveformrenderersignal.h
+++ src/waveform/renderers/glwaveformrenderersignal.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <QGLContext>
-#include <QOpenGLFunctions_2_1>
-
+#include "waveform/renderers/glwaveformrenderer.h"
 #include "waveform/renderers/waveformrenderersignalbase.h"
 
 #if !defined(QT_NO_OPENGL) && !defined(QT_OPENGL_ES_2)
@@ -12,30 +10,11 @@
 /// Note that the Qt OpenGL WaveformRendererAbstracts are not GLWaveformRenderers because
 /// they do not call OpenGL functions directly. Instead, they inherit QGLWidget and use the
 /// QPainter API which Qt translates to OpenGL under the hood.
-class GLWaveformRendererSignal : public WaveformRendererSignalBase, protected QOpenGLFunctions_2_1 {
+class GLWaveformRendererSignal : public WaveformRendererSignalBase, public GLWaveformRenderer {
   public:
     GLWaveformRendererSignal(WaveformWidgetRenderer* waveformWidgetRenderer)
-            : WaveformRendererSignalBase(waveformWidgetRenderer),
-              m_pLastContext(nullptr) {
+            : WaveformRendererSignalBase(waveformWidgetRenderer) {
     }
-
-    virtual void onInitializeGL() {
-        initializeOpenGLFunctions();
-    }
-
-  protected:
-    // Somehow QGLWidget does not call QGLWidget::initializeGL on macOS, so hack around that
-    // by calling this in `draw` when the QGLContext has been made current.
-    // TODO: remove this when upgrading to QOpenGLWidget
-    void maybeInitializeGL() {
-        if (QGLContext::currentContext() != m_pLastContext) {
-            onInitializeGL();
-            m_pLastContext = QGLContext::currentContext();
-        }
-    }
-
-  private:
-    const QGLContext* m_pLastContext;
 };
 
 #endif // !defined(QT_NO_OPENGL) && !defined(QT_OPENGL_ES_2)
