63a626d02883b8845ddf1923edcce18bb804232d
m0dB <79429057+m0dB@users.noreply.github.com>
2023-05-27 02:44:41 +0200

    clear WInitialGLWidget

--- src/widget/winitialglwidget.cpp
+++ src/widget/winitialglwidget.cpp
@@ -1,9 +1,13 @@
 #include "widget/winitialglwidget.h"
 
+#include <QOpenGLFunctions>
+
 WInitialGLWidget::WInitialGLWidget(QWidget* parent)
         : WGLWidget(parent) {
 }
 
 void WInitialGLWidget::initializeGL() {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
     emit onInitialized();
 }
