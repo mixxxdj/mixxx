582a4092053d494560f181dcddcbedbb06f3b45e
Daniel Schürmann <daschuer@mixxx.org>
2023-06-05 18:54:33 +0200

    Improve error message in moc_included_test.cpp

--- src/util/moc_included_test.cpp
+++ src/util/moc_included_test.cpp
@@ -1,7 +1,8 @@
 #include "../mocs_compilation.cpp"
 
 // QT_VERSION will be defined by any moc_<header_base>.cpp file included from mocs_compilation.cpp
-// It is empty in case all moc files are included
+// It is empty in case all moc files are included, a requirement to speed up incremental builds.
+// See https://cmake.org/cmake/help/latest/prop_tgt/AUTOMOC.html for details.
 #ifdef QT_VERSION
-#error "Not all moc_<header_base>.cpp files included from their cpp sources. This is a Mixxx requirement to speed up the build process. See https://cmake.org/cmake/help/latest/prop_tgt/AUTOMOC.html for details."
+#error "mocs_compilation.cpp not empty. Move all #include "moc_<header_base>.cpp" lines from mocs_compilation.cpp to the cpp files of the related classes."
 #endif
