c01fe261a93bbc29dc068200b98dbae396e16f42
Daniel Schürmann <daschuer@mixxx.org>
2023-06-05 21:52:31 +0200

    Remove quotes from #error
    Co-authored-by: Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>

--- src/util/moc_included_test.cpp
+++ src/util/moc_included_test.cpp
@@ -4,5 +4,5 @@
 // It is empty in case all moc files are included, a requirement to speed up incremental builds.
 // See https://cmake.org/cmake/help/latest/prop_tgt/AUTOMOC.html for details.
 #ifdef QT_VERSION
-#error "mocs_compilation.cpp not empty. Move all #include "moc_<header_base>.cpp" lines from mocs_compilation.cpp to the cpp files of the related classes."
+#error mocs_compilation.cpp not empty. Move all #include "moc_<header_base>.cpp" lines from mocs_compilation.cpp to the cpp files of the related classes.
 #endif
