2565a8cfcb5d596f71554463ef548c0a6bfb4962
Al Hadebe <amhadebe@gmail.com>
2023-06-11 20:03:19 +1000

    Fix Typo

--- res/controllers/Numark-Scratch-scripts.js
+++ res/controllers/Numark-Scratch-scripts.js
@@ -264,7 +264,7 @@ NumarkScratch.Deck = function(number) {
     });
 
     const encoderInvert = function(key) {
-        if (NumarkScratch.invertloopFunction) {
+        if (NumarkScratch.invertloopEncoderFunction) {
             return key === "unshift" ? "shift" : "unshift";
         }
         return key;
