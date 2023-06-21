7174fea4891fff2f8ab0507bbb2f3cbccdbbcaec
Daniel Schürmann <daschuer@mixxx.org>
2023-06-21 11:00:18 +0200

    make QRegularExpression() static to fix Wclazy-use-static-qregularexpression

--- src/controllers/dlgprefcontroller.cpp
+++ src/controllers/dlgprefcontroller.cpp
@@ -748,7 +748,7 @@ QString DlgPrefController::askForMappingName(const QString& prefilledName) const
                "special characters.");
     QString fileExistsLabel = tr("A mapping file with that name already exists.");
     // Only allow the name to contain letters, numbers, whitespaces and _-+()/
-    const QRegularExpression rxRemove = QRegularExpression(
+    static const QRegularExpression rxRemove = QRegularExpression(
             QStringLiteral("[^[(a-zA-Z0-9\\_\\-\\+\\(\\)\\/|\\s]"));
 
     // Choose a new file (base) name
