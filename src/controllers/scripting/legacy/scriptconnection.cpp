6cbc251a3d621c65341bcbd62d6dc7cad2135dcd
Prikshit singh <prikshitsingh79@gmail.com>
2023-07-11 11:50:10 +0530

    corrected executeCallback() function

--- src/controllers/scripting/legacy/scriptconnection.cpp
+++ src/controllers/scripting/legacy/scriptconnection.cpp
@@ -5,36 +5,32 @@
 
 void ScriptConnection::executeCallback(double value) const {
     Trace executeCallbackTrace("JS %1 callback", key.item);
+    if (!m_preventRecursiveCalls) {
+        if (!m_callbackExecuting) {
+            m_callbackExecuting = true;
 
-    if (!m_callbackExecuting && !m_preventRecursiveCalls) {
-        m_callbackExecuting = true;
-        m_preventRecursiveCalls = true;
+            const auto args = QJSValueList{
+                    value,
+                    key.group,
+                    key.item,
+            };
+            QJSValue func = callback; // copy function because QJSValue::call is not const
+            QJSValue result = func.call(args);
 
-        const auto args = QJSValueList{
-                value,
-                key.group,
-                key.item,
-        };
-        QJSValue func = callback; // copy function because QJSValue::call is not const
-        QJSValue result = func.call(args);
+            m_callbackExecuting = false;
 
-        m_preventRecursiveCalls = false; // Reset the preventRecursiveCalls flag
-        m_callbackExecuting = false;
-
-        if (result.isError()) {
-            if (controllerEngine != nullptr) {
-                controllerEngine->showScriptExceptionDialog(result);
+            if (result.isError()) {
+                if (controllerEngine != nullptr) {
+                    controllerEngine->showScriptExceptionDialog(result);
+                }
+                qWarning() << "ControllerEngine: Invocation of connection " << id.toString()
+                           << "connected to (" + key.group + ", " + key.item + ") failed:"
+                           << result.toString();
             }
-            qWarning() << "ControllerEngine: Invocation of connection " << id.toString()
-                       << "connected to (" + key.group + ", " + key.item + ") failed:"
-                       << result.toString();
+
+        } else {
+            qCCritical(m_logger) << "Critical Error: Triggering a connection "
+                                    "inside the callback is not allowed.";
         }
-    
-    } else if (m_preventRecursiveCalls) {
-        // Handle preventing recursive calls without a warning
-        // Add any necessary logic here
-    } else {
-        qCCritical(m_logger) << "Critical Error: Triggering a connection inside the callback is not allowed.";
     }
-    
 }
