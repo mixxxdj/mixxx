fa31fb9ba2baf176b1042aee96c67e09fa3c56a3
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2023-08-11 01:54:18 +0200

    style(util): fix performancetimer indentation; add constexpr

--- src/util/performancetimer.h
+++ src/util/performancetimer.h
@@ -3,37 +3,36 @@
 
 #include "util/duration.h"
 
-class PerformanceTimer
-{
-public:
-  // TODO: make this configurable via a template parameter?
-  // note that std::chrono::steady_clock is not steady on all platforms
-  using ClockT = std::chrono::steady_clock;
-  PerformanceTimer()
-          : m_startTime(){};
+class PerformanceTimer {
+  public:
+    // TODO: make this configurable via a template parameter?
+    // note that std::chrono::steady_clock is not steady on all platforms
+    using ClockT = std::chrono::steady_clock;
+    constexpr PerformanceTimer()
+            : m_startTime(){};
 
-  void start() {
-      m_startTime = ClockT::now();
-  };
+    void start() {
+        m_startTime = ClockT::now();
+    };
 
-  mixxx::Duration elapsed() const {
-      return mixxx::Duration::fromStdDuration(ClockT::now() - m_startTime);
-  };
-  mixxx::Duration restart() {
-      const ClockT::time_point now = ClockT::now();
-      const auto dur = mixxx::Duration::fromStdDuration(now - m_startTime);
-      m_startTime = now;
-      return dur;
-  };
+    mixxx::Duration elapsed() const {
+        return mixxx::Duration::fromStdDuration(ClockT::now() - m_startTime);
+    };
+    mixxx::Duration restart() {
+        const ClockT::time_point now = ClockT::now();
+        const auto dur = mixxx::Duration::fromStdDuration(now - m_startTime);
+        m_startTime = now;
+        return dur;
+    };
 
-  mixxx::Duration difference(const PerformanceTimer& timer) const {
-      return mixxx::Duration::fromStdDuration(m_startTime - timer.m_startTime);
-  };
+    constexpr mixxx::Duration difference(const PerformanceTimer& timer) const {
+        return mixxx::Duration::fromStdDuration(m_startTime - timer.m_startTime);
+    };
 
-  bool running() const {
-      return m_startTime.time_since_epoch().count() != 0;
-  };
+    constexpr bool running() const {
+        return m_startTime.time_since_epoch().count() != 0;
+    };
 
-private:
-  std::chrono::time_point<ClockT> m_startTime;
+  private:
+    std::chrono::time_point<ClockT> m_startTime;
 };
