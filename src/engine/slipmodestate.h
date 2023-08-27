77164b8091ea52b01165aea2c6b12907c7cda1c8
Joerg <JoergAtGithub@worldwartweb.com>
2023-08-27 19:41:53 +0200

    Added a comment, which describes SlipModeState::Armed

--- src/engine/slipmodestate.h
+++ src/engine/slipmodestate.h
@@ -2,6 +2,7 @@
 
 enum class SlipModeState {
     Disabled = 0,
-    Armed = 1,
+    Armed = 1, // Slip mode was enabled while in a loop, the slip position will start running
+               // when this loop get disabled
     Running = 2,
 };
