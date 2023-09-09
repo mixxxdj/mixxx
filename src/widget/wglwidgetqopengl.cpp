9d850611502930c1e88d376fbca3197fe950cd02
Daniel Schürmann <daschuer@mixxx.org>
2023-09-10 00:03:01 +0200

    Remove unused isContextSharing()

--- src/widget/wglwidgetqopengl.cpp
+++ src/widget/wglwidgetqopengl.cpp
@@ -58,10 +58,6 @@ bool WGLWidget::isContextValid() const {
     return m_pOpenGLWindow && m_pOpenGLWindow->context() && m_pOpenGLWindow->context()->isValid();
 }
 
-bool WGLWidget::isContextSharing() const {
-    return true;
-}
-
 void WGLWidget::makeCurrentIfNeeded() {
     if (m_pOpenGLWindow && m_pOpenGLWindow->context() != QOpenGLContext::currentContext()) {
         m_pOpenGLWindow->makeCurrent();
