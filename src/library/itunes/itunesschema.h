90c170033639c664ab97748d5d120aaadfa14f2f
fwcd <fwcdmail@gmail.com>
2023-09-10 17:00:06 +0200

    ITunesFeature: Extract table names into `itunesschema.h`

---
+++ src/library/itunes/itunesschema.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <QString>
+
+#define ITUNES_LIBRARY_TABLE "itunes_library"
+#define ITUNES_PLAYLISTS_TABLE "itunes_playlists"
+#define ITUNES_PLAYLIST_TRACKS_TABLE "itunes_playlist_tracks"
