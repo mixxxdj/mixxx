2067b8b61140ed34e31febd8d7a2b56bc9d5c60b
ronso0 <ronso0@mixxx.org>
2023-09-19 23:56:42 +0200

    Keylock: add test for KeyunlockMode::KeepLockedKey

--- src/test/enginebuffertest.cpp
+++ src/test/enginebuffertest.cpp
@@ -45,6 +45,23 @@ TEST_F(EngineBufferTest, DisableKeylockResetsPitch) {
     ASSERT_EQ(0.0, ControlObject::get(ConfigKey(m_sGroup1, "pitch")));
 }
 
+TEST_F(EngineBufferTest, DisableKeylockKeepsPitch) {
+    // Pitch must not change when unlocking with KeyunlockMode::KeepLockedKey.
+    ControlObject::set(ConfigKey(m_sGroup1, "keylockMode"),
+            1.0); // KeylockMode::LockCurrentKey
+    ControlObject::set(ConfigKey(m_sGroup1, "keyunlockMode"),
+            1.0); // KeyunlockMode::KeepLockedKey
+    ControlObject::set(ConfigKey(m_sGroup1, "file_bpm"), 128.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "keylock"), 1.0);
+    ControlObject::set(ConfigKey(m_sGroup1, "pitch"), 0.5);
+    ProcessBuffer();
+
+    ControlObject::set(ConfigKey(m_sGroup1, "keylock"), 0.0);
+    // We require a buffer process to see that the keylock state has changed.
+    ProcessBuffer();
+    ASSERT_NEAR(0.5, ControlObject::get(ConfigKey(m_sGroup1, "pitch")), 1e-10);
+}
+
 TEST_F(EngineBufferTest, TrackLoadResetsPitch) {
     // When a new track is loaded, the pitch value should be reset.
     config()->set(ConfigKey("[Controls]","SpeedAutoReset"),
