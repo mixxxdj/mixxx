mixxxdj/mixxx

commit ae153e87cb99eb51cc5c42cdea806c1895ac02fe

Pref Interface: simplify, remove redundant updates

src/preferences/dialog/dlgprefinterface.cpp
@@ -50,7 +50,6 @@ DlgPrefInterface::DlgPrefInterface(
           m_dScaleFactor(1.0),
           m_minScaleFactor(1.0),
           m_dDevicePixelRatio(1.0),
-          m_bStartWithFullScreen(false),
           m_bRebootMixxxView(false) {
     setupUi(this);
 
@@ -178,10 +177,6 @@ DlgPrefInterface::DlgPrefInterface(
         skinPreviewLabel->hide();
     }
 
-    // Start in fullscreen mode
-    checkBoxStartFullScreen->setChecked(
-            m_pConfig->getValue(ConfigKey(kConfigGroup, kStartInFullscreenKey), 0) == 1);
-
     // Screensaver mode
     comboBoxScreensaver->clear();
     comboBoxScreensaver->addItem(tr("Allow screensaver to run"),
@@ -195,9 +190,6 @@ DlgPrefInterface::DlgPrefInterface(
     comboBoxScreensaver->setCurrentIndex(comboBoxScreensaver->findData(inhibitsettings));
 
     // Tooltip configuration
-    // Initialize checkboxes to match config
-    loadTooltipPreferenceFromConfig();
-    slotSetTooltips();  // Update disabled status of "only library" checkbox
     connect(buttonGroupTooltips,
             QOverload<QAbstractButton*>::of(&QButtonGroup::buttonClicked),
             this,
@@ -264,9 +256,7 @@ void DlgPrefInterface::slotUpdateSchemes() {
 }
 
 void DlgPrefInterface::slotUpdate() {
-    const QString skinNameOnUpdate =
-            m_pConfig->getValue(ConfigKey(kConfigGroup, kResizableSkinKey));
-    const SkinPointer pSkinOnUpdate = m_skins[skinNameOnUpdate];
+    const SkinPointer pSkinOnUpdate = m_pSkinLoader->getConfiguredSkin();
     if (pSkinOnUpdate != nullptr && pSkinOnUpdate->isValid()) {
         m_skinNameOnUpdate = pSkinOnUpdate->name();
     } else {
@@ -287,7 +277,7 @@ void DlgPrefInterface::slotUpdate() {
     spinBoxScaleFactor->setMinimum(m_minScaleFactor * 100);
 
     checkBoxStartFullScreen->setChecked(m_pConfig->getValue(
-            ConfigKey(kConfigGroup, kStartInFullscreenKey), m_bStartWithFullScreen));
+            ConfigKey(kConfigGroup, kStartInFullscreenKey), false));
 
     loadTooltipPreferenceFromConfig();
 

src/preferences/dialog/dlgprefinterface.h
@@ -76,7 +76,6 @@ class DlgPrefInterface : public DlgPreferencePage, public Ui::DlgPrefControlsDlg
     double m_dScaleFactor;
     double m_minScaleFactor;
     double m_dDevicePixelRatio;
-    bool m_bStartWithFullScreen;
     mixxx::ScreenSaverPreference m_screensaverMode;
 
     bool m_bRebootMixxxView;

commit e8a5bb78b2f20d03ea48d2b12382327b2c937b57

fix(dlgprefinterface): Fix crash on start when building with `-DQML=ON`
This fixes a regression from ae153e87cb99eb51cc5c42cdea806c1895ac02fe.

src/preferences/dialog/dlgprefinterface.cpp
@@ -257,14 +257,16 @@ void DlgPrefInterface::slotUpdateSchemes() {
 }
 
 void DlgPrefInterface::slotUpdate() {
-    const SkinPointer pSkinOnUpdate = m_pSkinLoader->getConfiguredSkin();
-    if (pSkinOnUpdate != nullptr && pSkinOnUpdate->isValid()) {
-        m_skinNameOnUpdate = pSkinOnUpdate->name();
-    } else {
-        m_skinNameOnUpdate = m_pSkinLoader->getDefaultSkinName();
+    if (m_pSkinLoader) {
+        const SkinPointer pSkinOnUpdate = m_pSkinLoader->getConfiguredSkin();
+        if (pSkinOnUpdate != nullptr && pSkinOnUpdate->isValid()) {
+            m_skinNameOnUpdate = pSkinOnUpdate->name();
+        } else {
+            m_skinNameOnUpdate = m_pSkinLoader->getDefaultSkinName();
+        }
+        ComboBoxSkinconf->setCurrentIndex(ComboBoxSkinconf->findText(m_skinNameOnUpdate));
+        slotUpdateSchemes();
     }
-    ComboBoxSkinconf->setCurrentIndex(ComboBoxSkinconf->findText(m_skinNameOnUpdate));
-    slotUpdateSchemes();
 
     m_localeOnUpdate = m_pConfig->getValue(ConfigKey(kConfigGroup, kLocaleKey));
     ComboBoxLocale->setCurrentIndex(ComboBoxLocale->findData(m_localeOnUpdate));
@@ -286,9 +288,11 @@ void DlgPrefInterface::slotUpdate() {
 }
 
 void DlgPrefInterface::slotResetToDefaults() {
-    int index = ComboBoxSkinconf->findText(m_pSkinLoader->getDefaultSkinName());
-    ComboBoxSkinconf->setCurrentIndex(index);
-    slotSetSkin(index);
+    if (m_pSkinLoader) {
+        int index = ComboBoxSkinconf->findText(m_pSkinLoader->getDefaultSkinName());
+        ComboBoxSkinconf->setCurrentIndex(index);
+        slotSetSkin(index);
+    }
 
     // Use System locale
     ComboBoxLocale->setCurrentIndex(0);
@@ -428,8 +432,9 @@ void DlgPrefInterface::slotApply() {
     }
 
     // load skin/scheme if necessary
-    if (m_pSkin->name() != m_skinNameOnUpdate ||
-            m_colorScheme != m_colorSchemeOnUpdate) {
+    if (m_pSkin &&
+            (m_pSkin->name() != m_skinNameOnUpdate ||
+                    m_colorScheme != m_colorSchemeOnUpdate)) {
         // ColorSchemeParser::setupLegacyColorSchemes() reads scheme from config
         emit reloadUserInterface();
         // Allow switching skins multiple times without closing the dialog