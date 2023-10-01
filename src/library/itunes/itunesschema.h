ce29d8fedf4125d9e998299136ffdf61e790a2ab
fwcd <fwcdmail@gmail.com>
2023-10-01 16:42:38 +0100

    ITunesSchema: Use constexpr static instead of macros

--- src/library/itunes/itunesschema.h
+++ src/library/itunes/itunesschema.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <QString>
-
-#define ITUNES_LIBRARY_TABLE "itunes_library"
-#define ITUNES_PLAYLISTS_TABLE "itunes_playlists"
-#define ITUNES_PLAYLIST_TRACKS_TABLE "itunes_playlist_tracks"
+constexpr static auto ITUNES_LIBRARY_TABLE = "itunes_library";
+constexpr static auto ITUNES_PLAYLISTS_TABLE = "itunes_playlists";
+constexpr static auto ITUNES_PLAYLIST_TRACKS_TABLE = "itunes_playlist_tracks";
