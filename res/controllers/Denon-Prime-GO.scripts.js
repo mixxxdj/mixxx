d51828b40217c3c97e62be3dd7e38a87fa06a2a1
whanake-music <whanake@tutanota.com>
2023-10-02 13:41:07 +1300

    Prime GO: add hotcue mode

--- res/controllers/Denon-Prime-GO.scripts.js
+++ res/controllers/Denon-Prime-GO.scripts.js
@@ -224,6 +224,8 @@ PrimeGo.Deck = function(deckNumber, midiChannel) {
         inKey: "volume",
     });
 
+    this.padGrid = new PrimeGo.PadSection(this, midiChannel - 2);
+
     this.reconnectComponents(function(c) {
         if (c.group === undefined) {
             c.group = this.currentDeck;
@@ -252,7 +254,6 @@ PrimeGo.shiftState = function(channel, control, value) {
 };
 
 
-/*
 //========== PERFORMANCE PADS ==========//
 
 // mode-select pad index list to avoid remembering MIDI values
@@ -264,5 +265,139 @@ PrimeGo.padMode = {
 };
 
 PrimeGo.PadSection = function(deck, offset) {
+    components.ComponentContainer.call(this);
+    const theContainer = this;
+
+    // Create component containers for each pad mode
+    const modes = new components.ComponentContainer({
+        "hotcue": new PrimeGo.WrappingArrayView([new PrimeGo.hotcueMode(deck, offset)], 0),
+    });
+
+    modes.forEachComponent(c => c.disconnect());
+
+    const controlToPadMode = control => {
+        // If a pad selector button has multiple modes, go to the first mode
+        // by default. Otherwise, go to the next mode in that button's list.
+        const nextPadMode = (a) => {
+            if (a.indexable.includes(this.currentMode)) {
+                return a.next();
+            } else {
+                a.index = 0;
+                return a.current();
+            }
+        };
+
+        let mode;
+
+        switch (control) {
+        case PrimeGo.padMode.HOTCUE:
+            mode = nextPadMode(modes.hotcue);
+            break;
+        case PrimeGo.padMode.LOOP:
+            mode = nextPadMode(modes.loop);
+            break;
+        case PrimeGo.padMode.ROLL:
+            mode = nextPadMode(modes.roll);
+            break;
+        }
+
+        return mode;
+    };
+
+    this.offset = offset;
+
+    this.padModeSelectLeds = new components.Component({
+        trigger: function() {
+            // NOTE: There need to be two `for` blocks here to ensure that
+            //       the active mode's LED gets lit last. Otherwise, selecting
+            //       a mode when pressing shift will result in that pad mode's
+            //       LED being unlit.
+            for (const modeLayers of Object.values(modes)) {
+                const mode = modeLayers.current();
+                if (theContainer.currentMode !== mode) {
+                    // Make this LED inactive
+                }
+            }
+            for (const modeLayers of Object.values(modes)) {
+                const mode = modeLayers.current();
+                if (theContainer.currentMode === mode) {
+                    // Make this LED active
+                }
+            }
+        },
+    }, false);
+
+    // Function for switching between pad modes
+    this.setPadMode = function(control) {
+        const newMode = controlToPadMode(control);
+
+        // Exit early if requested mode is already active or unavailable
+        if (newMode === this.currentMode || newMode === undefined) {
+            return;
+        }
+
+        // Disable LED of current mode button
+        if (this.currentMode) {
+            // Disconnect pads from current mode
+            this.currentMode.forEachComponent(function(component) {
+                component.disconnect();
+                component.outConnect = false;
+            });
+        }
+
+        // Connect pads to new mode
+        newMode.forEachComponent(function(component) {
+            component.outConnect = true;
+            component.connect();
+            component.trigger();
+        });
+
+        // Assign mode select buttons in XML file
+        this.padModeButtonPressed = function(channel, control, value, _status, _group) {
+            if (value) {
+                this.setPadMode(control);
+            }
+        };
+
+        this.currentMode = newMode;
+
+        theContainer.padModeSelectLeds.trigger();
+    };
+
+    // Start in hotcue mode
+    this.setPadMode(PrimeGo.padMode.HOTCUE);
 };
-*/
+
+PrimeGo.PadSection.prototype = Object.create(components.ComponentContainer.prototype);
+
+// Assign performance pads mapped in XML file
+PrimeGo.PadSection.prototype.performancePad = function(channel, control, value, status, group) {
+    // Convert pad `control` to index value (1 to 8)
+    const i = (control - 0x0E);
+
+    this.currentMode.pads[i].input(channel, control, value, status, group);
+};
+
+PrimeGo.hotcueMode = function(deck, offset) {
+    components.ComponentContainer.call(this);
+    this.ledControl = PrimeGo.padMode.HOTCUE;
+    // this.colourOn = ________
+    // this.colourOff = ________
+    this.pads = new components.ComponentContainer();
+    for (let i = 1; i <= 8; i++) {
+        this.pads[i] = new components.HotcueButton({
+            number: i,
+            group: deck.currentDeck,
+            midi: [0x92 + offset, 0x0E + i],
+            sendRGB: function(color_obj) {
+                const msg = [0xf0, 0x00, 0x02, 0x0b, 0x7f, 0x0c, 0x03, 0x00, 0x05, 0x02 + offset, 0x0e + i, color_obj.red>>1, color_obj.green>>1, color_obj.blue>>1, 0xf7];
+                // F0 00 02 0B 7F 0C 03 00 05 status midino red green blue F7
+                midi.sendSysexMsg(msg, msg.length);
+            },
+            //on: this.colourOn,
+            //off: this.colourOff,
+            outConnect: false,
+        });
+    }
+};
+PrimeGo.hotcueMode.prototype = Object.create(components.ComponentContainer.prototype);
