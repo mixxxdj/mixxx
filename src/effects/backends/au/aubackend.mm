356dabb2f01943469d60b2c3828b3a119fb5bee1
fwcd <fwcdmail@gmail.com>
2023-10-11 23:03:52 +0100

    AUBackend: Move implementations into class

--- src/effects/backends/au/aubackend.mm
+++ src/effects/backends/au/aubackend.mm
@@ -8,53 +8,43 @@
 /// An effects backend for Audio Unit (AU) plugins. macOS-only.
 class AUBackend : public EffectsBackend {
   public:
-    AUBackend();
-    ~AUBackend() override;
+    AUBackend() {
+    }
+
+    ~AUBackend() override {
+    }
 
     EffectBackendType getType() const override {
         return EffectBackendType::AU;
     };
 
-    const QList<QString> getEffectIds() const override;
-    EffectManifestPointer getManifest(const QString& effectId) const override;
-    const QList<EffectManifestPointer> getManifests() const override;
-    bool canInstantiateEffect(const QString& effectId) const override;
+    const QList<QString> getEffectIds() const override {
+        // TODO
+        return {};
+    }
+
+    EffectManifestPointer getManifest(const QString& effectId) const override {
+        // TODO
+        return {};
+    }
+
+    const QList<EffectManifestPointer> getManifests() const override {
+        // TODO
+        return {};
+    }
+
+    bool canInstantiateEffect(const QString& effectId) const override {
+        // TODO
+        return false;
+    }
+
     std::unique_ptr<EffectProcessor> createProcessor(
-            const EffectManifestPointer pManifest) const override;
+            const EffectManifestPointer pManifest) const override {
+        // TODO
+        return {};
+    }
 };
 
 EffectsBackendPointer createAUBackend() {
     return EffectsBackendPointer(new AUBackend());
-}
-
-AUBackend::AUBackend() {
-}
-
-AUBackend::~AUBackend() {
-}
-
-const QList<QString> AUBackend::getEffectIds() const {
-    // TODO
-    return {};
-}
-
-EffectManifestPointer AUBackend::getManifest(const QString& effectId) const {
-    // TODO
-    return {};
-}
-
-const QList<EffectManifestPointer> AUBackend::getManifests() const {
-    // TODO
-    return {};
-}
-
-bool AUBackend::canInstantiateEffect(const QString& effectId) const {
-    // TODO
-    return false;
-}
-
-std::unique_ptr<EffectProcessor> AUBackend::createProcessor(
-        const EffectManifestPointer pManifest) const {
-    // TODO
-    return {};
 }
