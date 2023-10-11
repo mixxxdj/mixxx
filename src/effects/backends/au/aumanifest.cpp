7c065270d55eb0eeba54d7a580cd59db56133173
fwcd <fwcdmail@gmail.com>
2023-10-12 00:22:32 +0100

    AUManifest: Set correct backend type

--- src/effects/backends/au/aumanifest.cpp
+++ src/effects/backends/au/aumanifest.cpp
@@ -1,6 +1,9 @@
 #include "effects/backends/au/aumanifest.h"
 
+#include "effects/defs.h"
+
 AUManifest::AUManifest(const QString& id, const QString& name) {
     setId(id);
     setName(name);
+    setBackendType(EffectBackendType::AU);
 }
