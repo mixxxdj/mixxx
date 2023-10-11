0e396166d6c29a04c2a588da8a9c5b8a5a362bdf
fwcd <fwcdmail@gmail.com>
2023-10-11 23:25:41 +0100

    AUManifest: Store AVAudioUnitComponent

--- src/effects/backends/au/aumanifest.h
+++ src/effects/backends/au/aumanifest.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#import <AVFAudio/AVFAudio.h>
+
 #include "effects/backends/effectmanifest.h"
 #include "effects/defs.h"
 
 class AUManifest : public EffectManifest {
+  public:
+    AUManifest(AVAudioUnitComponent* component);
+
+  private:
+    AVAudioUnitComponent* m_component;
 };
---
+++ src/effects/backends/au/aumanifest.mm
@@ -0,0 +1,5 @@
+#include "effects/backends/au/aumanifest.h"
+
+AUManifest::AUManifest(AVAudioUnitComponent* component)
+        : m_component(component) {
+}
