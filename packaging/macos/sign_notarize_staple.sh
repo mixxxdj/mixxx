523b19b3203bb318a6ba2a57ec275a254b94d7de
fwcd <fwcdmail@gmail.com>
2023-10-15 16:14:38 +0100

    Notarization: Migrate script to `notarytool`

--- packaging/macos/sign_notarize_staple.sh
+++ packaging/macos/sign_notarize_staple.sh
@@ -6,16 +6,20 @@ DMG_FILE="${1}"
 [ -z "${APPLE_BUNDLE_ID}" ] && echo 'Please set the APPLE_BUNDLE_ID env var.' >&2 && exit 1
 [ -z "${APPLE_ID_USERNAME}" ] && echo 'Please set the APPLE_ID_USERNAME env var.' >&2 && exit 1
 [ -z "${APPLE_APP_SPECIFIC_PASSWORD}" ] && echo 'Please set the APPLE_APP_SPECIFIC_PASSWORD env var.' >&2 && exit 1
-[ -z "${ASC_PROVIDER}" ] && echo 'Please set the ASC_PROVIDER env var.' >&2 && exit 1
+[ -z "${APPLE_TEAM_ID}" ] && echo 'Please set the APPLE_TEAM_ID env var.' >&2 && exit 1
 
 echo "Signing $DMG_FILE"
 codesign --verbose=4 --options runtime \
     --sign "${APPLE_CODESIGN_IDENTITY}" "$(dirname "$0")/Mixxx.entitlements" "${DMG_FILE}"
 
 echo "Notarizing $DMG_FILE"
-xcrun altool --notarize-app --primary-bundle-id "${APPLE_BUNDLE_ID}" --username "${APPLE_ID_USERNAME}" \
-    --password "${APPLE_APP_SPECIFIC_PASSWORD}" --asc-provider "${ASC_PROVIDER}" --file "${DMG_FILE}" \
-    --output-format xml > notarize_result.plist
+xcrun notarytool \
+    --primary-bundle-id "${APPLE_BUNDLE_ID}" \
+    --apple-id "${APPLE_ID_USERNAME}" \
+    --password "${APPLE_APP_SPECIFIC_PASSWORD}" \
+    --team-id "${APPLE_TEAM_ID}" \
+    --file "${DMG_FILE}" \
+    --output-format plist > notarize_result.plist
 UUID="$(/usr/libexec/PlistBuddy -c 'Print notarization-upload:RequestUUID' notarize_result.plist)"
 echo "Notarization UUID: $UUID"
 rm notarize_result.plist
@@ -25,9 +29,11 @@ sleep 5
 
 # wait for confirmation that notarization finished
 while true; do
-    xcrun altool --notarization-info "$UUID" \
-    --username "${APPLE_ID_USERNAME}" --password "${APPLE_APP_SPECIFIC_PASSWORD}" \
-    --output-format xml > notarize_status.plist
+    xcrun notarytool \
+        --notarization-info "$UUID" \
+        --apple-id "${APPLE_ID_USERNAME}" \
+        --password "${APPLE_APP_SPECIFIC_PASSWORD}" \
+        --output-format plist > notarize_status.plist
 
     # shellcheck disable=SC2181
     if [ "$?" != "0" ]; then
