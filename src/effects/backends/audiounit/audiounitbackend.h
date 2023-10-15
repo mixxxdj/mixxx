2fee192ccd6bc52686910eff33512271dd3370a4
fwcd <fwcdmail@gmail.com>
2023-10-15 04:09:11 +0100

    AudioUnitBackend: Remove redundant includes

--- src/effects/backends/audiounit/audiounitbackend.h
+++ src/effects/backends/audiounit/audiounitbackend.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <QList>
-#include <QString>
-
-#include "effects/backends/effectprocessor.h"
-#include "effects/backends/effectsbackend.h"
 #include "effects/defs.h"
 
 EffectsBackendPointer createAudioUnitBackend();
