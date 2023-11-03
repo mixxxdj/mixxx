1f4d8fb3e709ff34b675c9c60cf1d6c45b3a906c
ronso0 <ronso0@mixxx.org>
2023-11-03 15:16:51 +0100

    add 'tempo_tap' to control picker menu

--- src/controllers/controlpickermenu.cpp
+++ src/controllers/controlpickermenu.cpp
@@ -243,6 +243,7 @@ ControlPickerMenu::ControlPickerMenu(QWidget* pParent)
     addDeckAndSamplerControl("bpm_up_small", tr("BPM +0.1"), tr("Increase BPM by 0.1"), pBpmMenu);
     addDeckAndSamplerControl("bpm_down_small", tr("BPM -0.1"), tr("Decrease BPM by 0.1"), pBpmMenu);
     addDeckAndSamplerControl("bpm_tap", tr("BPM Tap"), tr("BPM tap button"), pBpmMenu);
+    addDeckAndSamplerControl("tempo_tap", tr("Tempo Tap"), tr("Tempo tap button"), pBpmMenu);
     pBpmMenu->addSeparator();
     addDeckAndSamplerControl("beats_adjust_faster",
             tr("Adjust Beatgrid Faster +.01"),
