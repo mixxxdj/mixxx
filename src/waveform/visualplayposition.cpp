71af1b958009dcd04af9967117215fb5654dc04e
Joerg <JoergAtGithub@worldwartweb.com>
2023-11-09 21:07:52 +0100

    Add additional conditions to ensure, that loop boundary adjustment of the visual playposition is only done, when the engine playposition is inside the loop.

--- src/waveform/visualplayposition.cpp
+++ src/waveform/visualplayposition.cpp
@@ -88,13 +88,17 @@ double VisualPlayPosition::determinePlayPosInLoopBoundries(
     if (data.m_loopEnabled) {
         double loopSize = data.m_loopEndPos - data.m_loopStartPos;
         if (loopSize > 0) {
-            if ((data.m_playRate < 0.0) && (interpolatedPlayPos < data.m_loopStartPos)) {
+            if ((data.m_playRate < 0.0) &&
+                    (interpolatedPlayPos < data.m_loopStartPos) &&
+                    (data.m_playPos >= data.m_loopStartPos)) {
                 interpolatedPlayPos = data.m_loopEndPos -
                         std::remainder(
                                 data.m_loopStartPos - interpolatedPlayPos,
                                 loopSize);
             }
-            if ((data.m_playRate > 0.0) && (interpolatedPlayPos > data.m_loopEndPos)) {
+            if ((data.m_playRate > 0.0) &&
+                    (interpolatedPlayPos > data.m_loopEndPos) &&
+                    (data.m_playPos <= data.m_loopEndPos)) {
                 interpolatedPlayPos = data.m_loopStartPos +
                         std::remainder(
                                 interpolatedPlayPos - data.m_loopEndPos,
