958342da13c3d14641ea36e79f8919c9e319d24e
Joerg <JoergAtGithub@worldwartweb.com>
2023-11-26 14:00:05 +0100

    Added comments

--- src/nativeeventhandlerwin.cpp
+++ src/nativeeventhandlerwin.cpp
@@ -18,6 +18,9 @@ bool WindowsEventHandler::nativeEventFilter(
         // when Windows setting "Show window contents while dragging" is enabled
         // and the user left-clicks and holds with the mouse on the titlebar of
         // a window
+        // For reference, have a look at:
+        // https://www.gamedev.net/forums/topic/520860-sizemove-loop-and-delay-in-defwindowproc/4382282/
+        // https://github.com/rust-windowing/winit/pull/839
         RedrawWindow(
                 msg->hwnd,
                 nullptr,
@@ -31,6 +34,8 @@ bool WindowsEventHandler::nativeEventFilter(
         DefSubclassProc(msg->hwnd, msg->message, msg->wParam, msg->lParam);
     }
 
+    // Returning false indicates that the event itself has not been finally processed here,
+    // so other recipients will also receive this message subsequently
     return false;
 }
 #endif
