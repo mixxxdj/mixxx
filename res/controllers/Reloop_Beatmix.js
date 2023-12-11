c0d4c7505a365a62411db1361f941903cf66aa2c
Eugene Erokhin <uncle.eugene@gmail.com>
2023-12-11 13:32:38 +0600

    Mostly cosmetic changes to improve readability.

--- res/controllers/Reloop_Beatmix.js
+++ res/controllers/Reloop_Beatmix.js
@@ -14,18 +14,18 @@ const CH2 = 0x91;
 const ON = 0x7F;
 const OFF = 0x00;
 
-const bNAVKNOB = 0x18;
-const bBEATMASH1 = 0x21;
-const bBEATMASH2 = 0x22;
+const NAVKNOB = 0x18;
+const BEATMASH1 = 0x21;
+const BEATMASH2 = 0x22;
 const QUICKJOGR = 0x26;
-const bPLUS = 0x27;
+const PLUS = 0x27;
 const QUICKJOGL = 0x28;
-const bMINUS = 0x26;
-const bSHIFTMINUS = 0x46;
-const bSHIFTPLUS = 0x47;
-const bSHIFTCUE = 0x4E;
-const bKEYLOCK = 0x4F;
-const bSHIFTLOAD = 0x52;
+const MINUS = 0x26;
+const SHIFTMINUS = 0x46;
+const SHIFTPLUS = 0x47;
+const SHIFTCUE = 0x4E;
+const KEYLOCK = 0x4F;
+const SHIFTLOAD = 0x52;
 
 const LIBRARYMAIN = 3;
 const LIBRARYTREE = 2;
@@ -48,24 +48,24 @@ Beatmix.init = function(_id, _debug) {
     }
 
     // Setting up constant lights. Those are "+" and "-" buttons in both normal and shift modes
-    midi.sendShortMsg(CH1, bMINUS, FULL);
-    midi.sendShortMsg(CH1, bPLUS, FULL);
-    midi.sendShortMsg(CH1, bSHIFTMINUS, FULL);
-    midi.sendShortMsg(CH1, bSHIFTPLUS, FULL);
-    midi.sendShortMsg(CH2, bMINUS, FULL);
-    midi.sendShortMsg(CH2, bPLUS, FULL);
-    midi.sendShortMsg(CH2, bSHIFTMINUS, FULL);
-    midi.sendShortMsg(CH2, bSHIFTPLUS, FULL);
+    midi.sendShortMsg(CH1, MINUS, ON);
+    midi.sendShortMsg(CH1, PLUS, ON);
+    midi.sendShortMsg(CH1, SHIFTMINUS, ON);
+    midi.sendShortMsg(CH1, SHIFTPLUS, ON);
+    midi.sendShortMsg(CH2, MINUS, ON);
+    midi.sendShortMsg(CH2, PLUS, ON);
+    midi.sendShortMsg(CH2, SHIFTMINUS, ON);
+    midi.sendShortMsg(CH2, SHIFTPLUS, ON);
 
     // Turning off FX units 1 and 2
     engine.setValue("[EffectRack1_EffectUnit1]", "enabled", 0);
     engine.setValue("[EffectRack1_EffectUnit2]", "enabled", 0);
 
     // Getting some functions state from the host
-    midi.sendShortMsg(CH1, bKEYLOCK, engine.getValue("[Channel1]", "keylock"));
-    midi.sendShortMsg(CH2, bKEYLOCK, engine.getValue("[Channel2]", "keylock"));
-    midi.sendShortMsg(CH1, bBEATMASH1, engine.getValue("[Channel1]", "quantize"));
-    midi.sendShortMsg(CH2, bBEATMASH2, engine.getValue("[Channel2]", "quantize"));
+    midi.sendShortMsg(CH1, KEYLOCK, engine.getValue("[Channel1]", "keylock"));
+    midi.sendShortMsg(CH2, KEYLOCK, engine.getValue("[Channel2]", "keylock"));
+    midi.sendShortMsg(CH1, BEATMASH1, engine.getValue("[Channel1]", "quantize"));
+    midi.sendShortMsg(CH2, BEATMASH2, engine.getValue("[Channel2]", "quantize"));
 
     // Debug message
     console.log("Reloop Beatmix: controller initialized.");
@@ -75,13 +75,13 @@ Beatmix.init = function(_id, _debug) {
 // and its state is indicated by Shift-Cue light.
 Beatmix.faderStart = function(channel, _control, value, _status, group) {
     const deck = script.deckFromGroup(group);
-    if (value === FULL) {
+    if (value === ON) {
         if (Beatmix.faderStartEnabled[deck]) {
             Beatmix.faderStartEnabled[deck] = false;
-            midi.sendShortMsg(CH1 + channel, bSHIFTCUE, OFF);
+            midi.sendShortMsg(CH1 + channel, SHIFTCUE, OFF);
         } else {
             Beatmix.faderStartEnabled[deck] = true;
-            midi.sendShortMsg(CH1 + channel, bSHIFTCUE, FULL);
+            midi.sendShortMsg(CH1 + channel, SHIFTCUE, ON);
         }
     }
 };
@@ -92,7 +92,9 @@ Beatmix.faderStart = function(channel, _control, value, _status, group) {
 // If you want to use fader as a hotcue button then what are you, a monster? :)
 Beatmix.fader = function(_channel, _control, value, _status, group) {
     const deck = script.deckFromGroup(group);
-    if (Beatmix.faderStartEnabled[deck] && value > faderStartThr && Beatmix.faderLastValue[deck] < faderStartThr) {
+    if (Beatmix.faderStartEnabled[deck] &&
+        value > faderStartThr &&
+        Beatmix.faderLastValue[deck] < faderStartThr) {
         engine.setValue(group, "play", 1);
     }
     engine.setParameter(group, "volume", value/127);
@@ -106,13 +108,13 @@ Beatmix.fader = function(_channel, _control, value, _status, group) {
 // Instead scratch mode is enabled and indicated by Shift-Load.
 Beatmix.toggleScratchMode = function(channel, _control, value, _status, group) {
     const deck = script.deckFromGroup(group);
-    if (value === FULL) {
+    if (value === ON) {
         Beatmix.scratchEnabled[deck] = !Beatmix.scratchEnabled[deck];
     }
     if (Beatmix.scratchEnabled[deck]) {
-        midi.sendShortMsg(CH1+channel, bSHIFTLOAD, FULL);
+        midi.sendShortMsg(CH1+channel, SHIFTLOAD, ON);
     } else {
-        midi.sendShortMsg(CH1+channel, bSHIFTLOAD, OFF);
+        midi.sendShortMsg(CH1+channel, SHIFTLOAD, OFF);
     }
 };
 
@@ -121,7 +123,7 @@ Beatmix.toggleScratchMode = function(channel, _control, value, _status, group) {
 Beatmix.wheelTouch = function(_channel, _control, value, _status, group) {
     const deck = script.deckFromGroup(group);
     if (Beatmix.scratchEnabled[deck]) {
-        if (value === FULL) {
+        if (value === ON) {
             const alpha = 1.0 / 8;
             const beta = alpha / 32;
             engine.scratchEnable(deck, 800, 33 + 1/3, alpha, beta);
@@ -152,7 +154,7 @@ Beatmix.wheelTurn = function(_channel, control, value, _status, group) {
 Beatmix.navKnob = function(_channel, control, value, _status, _group) {
     //console.log("control: " + control + ", value: " + value + ", status: " + status);
     const newValue = value - 64;
-    if (control === bNAVKNOB) {
+    if (control === NAVKNOB) {
         engine.setValue("[Library]", "focused_widget", LIBRARYMAIN);
     } else {
         engine.setValue("[Library]", "focused_widget", LIBRARYTREE);
@@ -163,7 +165,7 @@ Beatmix.navKnob = function(_channel, control, value, _status, _group) {
 // Beatmix.expandCollapse handles Shift-Knob-Click event. It will expand/collapse
 // currently selected library tree entry.
 Beatmix.expandCollapse = function(_channel, _control, value, _status, _group) {
-    if (value === FULL && engine.getValue("[Library]", "focused_widget") === LIBRARYTREE) {
+    if (value === ON && engine.getValue("[Library]", "focused_widget") === LIBRARYTREE) {
         engine.setValue("[Library]", "GoToItem", 1);
     } else {
         engine.setValue("[Library]", "focused_widget", LIBRARYTREE);
@@ -189,14 +191,14 @@ Beatmix.loopMove = function(_channel, _control, value, _status, group) {
 
 // Beatmix.fxSelClick toggles FX rack visibility.
 Beatmix.fxSelClick = function(_channel, _control, value, _status, _group) {
-    if (value === FULL) {
+    if (value === ON) {
         script.toggleControl("[EffectRack1]", "show");
     }
 };
 
 // Beatmix.smplSelClick toggles samplers visibility.
 Beatmix.smplSelClick = function(_channel, _control, value, _status, _group) {
-    if (value === FULL) {
+    if (value === ON) {
         script.toggleControl("[Samplers]", "show_samplers");
     }
 };
