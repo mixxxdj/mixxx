d6cbf27520ec91b666f82db6c85b2a630dad4329
Daniel Schürmann <daschuer@mixxx.org>
2024-01-04 08:42:53 +0100

    Add tools/createChangelog.py for creating changelog entries from merge commits

---
+++ tools/createChangelog.py
@@ -0,0 +1,24 @@
+import git
+import re
+
+repo_path = "."
+
+repo = git.Repo(repo_path)
+
+# Starting commit SHA
+start_commit = "869b95bcda92774588f99252b7c8a02b77f16c5f"
+
+# Regular expression pattern to extract PR number
+pr_pattern = re.compile(r"Merge pull request #(\d+)")
+
+# Iterate through commits
+for commit in repo.iter_commits(rev=f"{start_commit}..upstream/2.4"):
+    match = pr_pattern.search(commit.summary)
+    if match:
+        pr_number = match.group(1)
+        commit_message_lines = commit.message.split("\n")
+        print(
+            f"* {commit_message_lines[2]} "
+            + f"[#{pr_number}]"
+            + f"(https://github.com/mixxxdj/mixxx/pull/{pr_number})"
+        )
