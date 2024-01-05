feb5fea083656e0f0df24c1bf6713b112fdfbcfa
ronso0 <ronso0@mixxx.org>
2024-01-05 02:16:31 +0100

    Looping: get correct size when saving/reactivating loop with non-preset length

--- src/engine/controls/loopingcontrol.cpp
+++ src/engine/controls/loopingcontrol.cpp
@@ -1417,7 +1417,9 @@ double LoopingControl::findBeatloopSizeForLoop(
             }
         }
     }
-    return -1;
+
+    // No hit. Calculate the fractional beat length
+    return pBeats->numFractionalBeatsInRange(startPosition, endPosition);
 }
 
 void LoopingControl::updateBeatLoopingControls() {
