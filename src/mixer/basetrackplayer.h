51eec94a46353cc6b6d09c59dc120af242ec20bb
ronso0 <ronso0@mixxx.org>
2024-01-27 01:36:07 +0100

    Track rating: simplify by moving some logic from WStarRating to BaseTrackPlayerImpl

--- src/mixer/basetrackplayer.h
+++ src/mixer/basetrackplayer.h
@@ -40,15 +40,14 @@ class BaseTrackPlayer : public BasePlayer {
     virtual bool isTrackMenuControlAvailable() {
         return false;
     };
-    virtual void ensureStarControlsArePrepared(){};
 
   public slots:
     virtual void slotLoadTrack(TrackPointer pTrack, bool bPlay = false) = 0;
     virtual void slotCloneFromGroup(const QString& group) = 0;
     virtual void slotCloneDeck() = 0;
     virtual void slotEjectTrack(double) = 0;
-    virtual void slotSetTrackRating(int){};
     virtual void slotSetAndConfirmTrackMenuControl(bool){};
+    virtual void slotTrackRatingChangeRequest(int){};
 
   signals:
     void newTrackLoaded(TrackPointer pLoadedTrack);
@@ -58,7 +57,6 @@ class BaseTrackPlayer : public BasePlayer {
     void noVinylControlInputConfigured();
     void trackRatingChanged(int rating);
     void trackMenuChangeRequest(bool show);
-    void trackRatingChangeRequest(int change);
 };
 
 class BaseTrackPlayerImpl : public BaseTrackPlayer {
@@ -89,11 +87,6 @@ class BaseTrackPlayerImpl : public BaseTrackPlayer {
     /// connection is removed on skin reload and available again afterwards.
     bool isTrackMenuControlAvailable() final;
 
-    /// Controls to change the star rating.
-    /// Created on request only, because we need them only when there is
-    /// a star rating widget for this player.
-    void ensureStarControlsArePrepared() final;
-
     /// For testing, loads a fake track.
     TrackPointer loadFakeTrack(bool bPlay, double filebpm);
 
@@ -109,9 +102,10 @@ class BaseTrackPlayerImpl : public BaseTrackPlayer {
     /// to compensate so there is no audible change in volume.
     void slotAdjustReplayGain(mixxx::ReplayGain replayGain);
     void slotSetTrackColor(const mixxx::RgbColor::optional_t& color);
-    void slotSetTrackRating(int rating) final;
     /// Called via signal from WTrackProperty. Just set and confirm as requested.
     void slotSetAndConfirmTrackMenuControl(bool visible) final;
+    /// Slot for change signals from WStarRating (absolute values)
+    void slotTrackRatingChangeRequest(int rating) final;
     void slotPlayToggled(double);
 
   private slots:
@@ -122,6 +116,8 @@ class BaseTrackPlayerImpl : public BaseTrackPlayer {
     void slotLoadTrackFromDeck(double deck);
     void slotLoadTrackFromSampler(double sampler);
     void slotTrackColorChangeRequest(double value);
+    /// Slot for change signals from up/down controls (relative values)
+    void slotTrackRatingChangeRequestRelative(int change);
     void slotVinylControlEnabled(double v);
     void slotWaveformZoomValueChangeRequest(double pressed);
     void slotWaveformZoomUp(double pressed);
