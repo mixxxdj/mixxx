be02a205d90ed79d20ca450b9aec001681611148
fwcd <fwcd@fwcd.dev>
2024-01-27 20:30:18 +0100

    Engine: Add feature for building with/without Rubberband

--- src/effects/backends/builtin/builtinbackend.cpp
+++ src/effects/backends/builtin/builtinbackend.cpp
@@ -22,7 +22,9 @@
 #include "effects/backends/builtin/loudnesscontoureffect.h"
 #include "effects/backends/builtin/metronomeeffect.h"
 #include "effects/backends/builtin/phasereffect.h"
+#ifdef __RUBBERBAND__
 #include "effects/backends/builtin/pitchshifteffect.h"
+#endif
 #include "effects/backends/builtin/tremoloeffect.h"
 #include "effects/backends/builtin/whitenoiseeffect.h"
 
@@ -54,7 +56,9 @@ BuiltInBackend::BuiltInBackend() {
     registerEffect<PhaserEffect>();
     registerEffect<MetronomeEffect>();
     registerEffect<TremoloEffect>();
+#ifdef __RUBBERBAND__
     registerEffect<PitchShiftEffect>();
+#endif
     registerEffect<DistortionEffect>();
 }
 
