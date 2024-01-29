e0cbb80cedf1c1adcab2430bf5e225a4bb4086c7
fwcd <fwcd@fwcd.dev>
2024-01-29 17:12:29 +0100

    CmdlineArgs: Use user-accessible config directory

--- src/util/cmdlineargs.cpp
+++ src/util/cmdlineargs.cpp
@@ -11,6 +11,7 @@
 #include <QCoreApplication>
 #include <QProcessEnvironment>
 #include <QStandardPaths>
+#include <QtGlobal>
 
 #include "config.h"
 #include "defs_urls.h"
@@ -34,18 +35,34 @@ CmdlineArgs::CmdlineArgs()
           m_logFlushLevel(mixxx::kLogFlushLevelDefault),
 // We are not ready to switch to XDG folders under Linux, so keeping $HOME/.mixxx as preferences folder. see #8090
 #ifdef MIXXX_SETTINGS_PATH
-          m_settingsPath(QDir::homePath().append("/").append(MIXXX_SETTINGS_PATH)) {
-#else
-#ifdef __LINUX__
+          m_settingsPath(QDir::homePath().append("/").append(MIXXX_SETTINGS_PATH))
+#elif defined(__LINUX__)
 #error "We are not ready to switch to XDG folders under Linux"
-#endif
+#elif defined(Q_OS_IOS)
+          // On iOS we intentionally use a user-accessible subdirectory of the sandbox
+          // documents directory rather than the default app data directory. Specifically
+          // we use
+          //
+          //     <sandbox home>/Documents/Library/Application Support/Mixxx
+          //
+          // instead of the default (and hidden)
+          //
+          //     <sandbox home>/Library/Application Support/Mixxx
+          //
+          // This lets the user back up their mixxxdb, add custom controller mappings,
+          // potentially diagnose issues by accessing logs etc. via the native iOS files app.
+          m_settingsPath(
+                  QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)
+                          .append("/Library/Application Support/Mixxx"))
+#else
 
           // TODO(XXX) Trailing slash not needed anymore as we switches from String::append
           // to QDir::filePath elsewhere in the code. This is candidate for removal.
           m_settingsPath(
                   QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation)
-                          .append("/")) {
+                          .append("/"))
 #endif
+{
 }
 
 namespace {
