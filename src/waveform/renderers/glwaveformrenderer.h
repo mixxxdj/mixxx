4cae9d7bf39625d6817e8ac0353af07d71b41ada
Be <be@mixxx.org>
2024-02-12 13:51:15 -0600

    inherit from QOpenGLFunctions instead of QOpenGLFunctions_2_1
    The generic QOpenGLFunctions class supports OpenGL ES 2.0, the
    minimum supported by Qt 5. Mixxx builds and runs fine with this;
    desktop OpenGL 2.1 is only required for the legacy OpenGL
    waveform renderers.

--- src/waveform/renderers/glwaveformrenderer.h
+++ src/waveform/renderers/glwaveformrenderer.h
@@ -2,15 +2,15 @@
 
 #include <QtGui/qtgui-config.h> // for QT_NO_OPENGL and QT_OPENGL_ES_2
 
-#if !defined(QT_NO_OPENGL) && !defined(QT_OPENGL_ES_2)
+#if !defined(QT_NO_OPENGL)
 
-#include <QOpenGLFunctions_2_1>
+#include <QOpenGLFunctions>
 
-class GLWaveformRenderer : protected QOpenGLFunctions_2_1 {
+class GLWaveformRenderer : protected QOpenGLFunctions {
   public:
     virtual void initializeGL() {
         initializeOpenGLFunctions();
     }
 };
 
-#endif // !defined(QT_NO_OPENGL) && !defined(QT_OPENGL_ES_2)
+#endif // !defined(QT_NO_OPENGL)
