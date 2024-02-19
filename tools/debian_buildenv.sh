504d568339bfb996d6cbeead763d06224e024b79
Daniel Schürmann <daschuer@mixxx.org>
2024-02-19 07:11:29 +0100

    Install qml6-module-qtquick-layouts

--- tools/debian_buildenv.sh
+++ tools/debian_buildenv.sh
@@ -106,6 +106,7 @@ case "$1" in
             qml6-module-qt5compat-graphicaleffects \
             qml6-module-qtqml-workerscript \
             qml6-module-qtquick-controls \
+            qml6-module-qtquick-layouts \
             qml6-module-qtquick-nativestyle \
             qml6-module-qtquick-shapes \
             qml6-module-qtquick-templates \
