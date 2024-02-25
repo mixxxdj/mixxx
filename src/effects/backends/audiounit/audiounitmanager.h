440d1cf4b42656c86a0616abc710ea236dd53929
fwcd <fwcd@fwcd.dev>
2024-02-25 21:58:14 +0100

    AudioUnitManager: Add rationale for not using a mutex

--- src/effects/backends/audiounit/audiounitmanager.h
+++ src/effects/backends/audiounit/audiounitmanager.h
@@ -24,7 +24,10 @@ class AudioUnitManager {
     AudioUnitManager& operator=(const AudioUnitManager&) = delete;
 
     /// Fetches the audio unit if already instantiated.
-    /// Non-blocking and thread-safe.
+    ///
+    /// Non-blocking and thread-safe, since this method is intended to (also) be
+    /// called in a real-time context, e.g. from an audio thread, where we don't
+    /// want to e.g. block on a mutex.
     AudioUnit _Nullable getAudioUnit();
 
   private:
