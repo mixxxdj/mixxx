3f8ac551d2eea6364119541022c68365bacdd0e0
Milkii Brewster <git@milkmiruku.com>
2024-03-08 10:24:01 +0000

    feat: add additional BPM control options for more precise adjustments

--- src/controllers/controlpickermenu.cpp
+++ src/controllers/controlpickermenu.cpp
@@ -263,27 +263,27 @@ ControlPickerMenu::ControlPickerMenu(QWidget* pParent)
             tr("BPM tap button"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_halve",
-            tr("BPM Halve"),
+            tr("Halve BPM"),
             tr("Multiply current BPM by 0.5"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_twothirds",
-            tr("BPM Two Thirds"),
+            tr("2/3 BPM"),
             tr("Multiply current BPM by 0.666"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_threefourths",
-            tr("BPM Three Fourths"),
+            tr("3/4 BPM"),
             tr("Multiply current BPM by 0.75"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_fourthirds",
-            tr("BPM Four Thirds"),
+            tr("4/3 BPM"),
             tr("Multiply current BPM by 1.333"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_threehalves",
-            tr("BPM Three Halves"),
+            tr("3/2 BPM"),
             tr("Multiply current BPM by 1.5"),
             pBpmMenu);
     addDeckAndSamplerControl("beats_set_double",
-            tr("BPM Double"),
+            tr("Double BPM"),
             tr("Multiply current BPM by 2"),
             pBpmMenu);
     pBpmMenu->addSeparator();
