0570c0350a5e0b682da12cbbb312c535b6bb8cf1
ronso0 <ronso0@mixxx.org>
2024-03-10 15:03:44 +0100

    BpmControl: add missing delete for m_pTranslateBeatsMove

--- src/engine/controls/bpmcontrol.cpp
+++ src/engine/controls/bpmcontrol.cpp
@@ -146,6 +146,7 @@ BpmControl::~BpmControl() {
     delete m_pTranslateBeatsMove;
     delete m_pAdjustBeatsFaster;
     delete m_pAdjustBeatsSlower;
+    delete m_pTranslateBeatsMove;
 }
 
 mixxx::Bpm BpmControl::getBpm() const {
