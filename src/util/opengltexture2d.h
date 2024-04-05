a0a0ddb110f6ded1d81279a3962c474f1cb70833
Daniel Schürmann <daschuer@mixxx.org>
2024-04-05 07:33:31 +0200

    Add a comment that explains the OpenGLTexture2D class

--- src/util/opengltexture2d.h
+++ src/util/opengltexture2d.h
@@ -5,6 +5,8 @@
 
 class Paintable;
 
+/// This is an QOpenGLTexture, with additional methods to set the texture data,
+/// and default settings for 2D painting with lienar filtering and wrap mode.
 class OpenGLTexture2D : public QOpenGLTexture {
   public:
     OpenGLTexture2D();
