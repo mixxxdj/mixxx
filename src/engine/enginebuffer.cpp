fcba1e94248add462c63ce85d8e92fbe2246519b
yatigautam <yatigautam9412@gmail.com>
2024-04-16 19:11:44 +0530

    add playposition_samples

--- src/engine/enginebuffer.cpp
+++ src/engine/enginebuffer.cpp
@@ -1406,6 +1406,10 @@ mixxx::audio::FramePos EngineBuffer::queuedSeekPosition() const {
     return queuedSeek.position;
 }
 
+double calculateSamplePosition(double position, double sampleRate) {
+    return position * sampleRate;
+}
+
 void EngineBuffer::updateIndicators(double speed, int iBufferSize) {
     if (!m_playPos.isValid() ||
             !m_trackSampleRateOld.isValid() ||
