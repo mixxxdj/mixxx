mixxxdj/mixxx

commit 07f05f1a0ef39e12b870718c9f1203f6f9335b14

Introduce OpenGLTexture2D to get around nested allocations when creating textures

src/widget/wspinnyglsl.cpp
@@ -4,7 +4,6 @@
 #include <array>
 
 #include "moc_wspinnyglsl.cpp"
-#include "util/texture.h"
 
 WSpinnyGLSL::WSpinnyGLSL(
         QWidget* parent,
@@ -15,25 +14,10 @@ WSpinnyGLSL::WSpinnyGLSL(
         : WSpinnyBase(parent, group, pConfig, pVCMan, pPlayer) {
 }
 
-WSpinnyGLSL::~WSpinnyGLSL() {
-    cleanupGL();
-}
-
-void WSpinnyGLSL::cleanupGL() {
-    makeCurrentIfNeeded();
-    m_pBgTexture.reset();
-    m_pMaskTexture.reset();
-    m_pFgTextureScaled.reset();
-    m_pGhostTextureScaled.reset();
-    m_pLoadedCoverTextureScaled.reset();
-    m_pQTexture.reset();
-    doneCurrent();
-}
-
 void WSpinnyGLSL::coverChanged() {
     if (isContextValid()) {
         makeCurrentIfNeeded();
-        m_pLoadedCoverTextureScaled = createTexture(m_loadedCoverScaled);
+        m_loadedCoverTextureScaled.setData(m_loadedCoverScaled);
         doneCurrent();
     }
     // otherwise this will happen in initializeGL
@@ -55,11 +39,11 @@ void WSpinnyGLSL::resizeGL(int w, int h) {
 }
 
 void WSpinnyGLSL::updateTextures() {
-    m_pBgTexture = createTexture(m_pBgImage);
-    m_pMaskTexture = createTexture(m_pMaskImage);
-    m_pFgTextureScaled = createTexture(m_fgImageScaled);
-    m_pGhostTextureScaled = createTexture(m_ghostImageScaled);
-    m_pLoadedCoverTextureScaled = createTexture(m_loadedCoverScaled);
+    m_bgTexture.setData(m_pBgImage);
+    m_maskTexture.setData(m_pMaskImage);
+    m_fgTextureScaled.setData(m_fgImageScaled);
+    m_ghostTextureScaled.setData(m_ghostImageScaled);
+    m_loadedCoverTextureScaled.setData(m_loadedCoverScaled);
 }
 
 void WSpinnyGLSL::setupVinylSignalQuality() {
@@ -69,9 +53,9 @@ void WSpinnyGLSL::updateVinylSignalQualityImage(
         const QColor& qual_color, const unsigned char* data) {
     m_vinylQualityColor = qual_color;
     m_vinylQualityColor.setAlphaF(0.75f);
-    if (m_pQTexture) {
+    if (m_qTexture.isStorageAllocated()) {
         makeCurrentIfNeeded();
-        m_pQTexture->bind();
+        m_qTexture.bind();
         // Using a texture of one byte per pixel so we can store the vinyl
         // signal quality data directly. The VinylQualityShader will draw this
         // colorized with alpha transparency.
@@ -84,7 +68,7 @@ void WSpinnyGLSL::updateVinylSignalQualityImage(
                 GL_RED,
                 GL_UNSIGNED_BYTE,
                 data);
-        m_pQTexture->release();
+        m_qTexture.release();
         doneCurrent();
     }
 }
@@ -112,16 +96,16 @@ void WSpinnyGLSL::paintGL() {
 
     m_textureShader.setUniformValue(textureLocation, 0);
 
-    if (m_pBgTexture) {
-        drawTexture(m_pBgTexture.get());
+    if (m_bgTexture.isStorageAllocated()) {
+        drawTexture(&m_bgTexture);
     }
 
-    if (m_bShowCover && m_pLoadedCoverTextureScaled) {
-        drawTexture(m_pLoadedCoverTextureScaled.get());
+    if (m_bShowCover && m_loadedCoverTextureScaled.isStorageAllocated()) {
+        drawTexture(&m_loadedCoverTextureScaled);
     }
 
-    if (m_pMaskTexture) {
-        drawTexture(m_pMaskTexture.get());
+    if (m_maskTexture.isStorageAllocated()) {
+        drawTexture(&m_maskTexture);
     }
 
     // Overlay the signal quality drawing if vinyl is active
@@ -137,22 +121,22 @@ void WSpinnyGLSL::paintGL() {
     // and draw the image at the corner.
     // p.translate(width() / 2, height() / 2);
 
-    bool paintGhost = m_bGhostPlayback && m_pGhostTextureScaled;
+    bool paintGhost = m_bGhostPlayback && m_ghostTextureScaled.isStorageAllocated();
 
     if (paintGhost) {
         QMatrix4x4 rotate;
         rotate.rotate(m_fGhostAngle, 0, 0, -1);
         m_textureShader.setUniformValue(matrixLocation, rotate);
 
-        drawTexture(m_pGhostTextureScaled.get());
+        drawTexture(&m_ghostTextureScaled);
     }
 
-    if (m_pFgTextureScaled) {
+    if (m_fgTextureScaled.isStorageAllocated()) {
         QMatrix4x4 rotate;
         rotate.rotate(m_fAngle, 0, 0, -1);
         m_textureShader.setUniformValue(matrixLocation, rotate);
 
-        drawTexture(m_pFgTextureScaled.get());
+        drawTexture(&m_fgTextureScaled);
     }
 
     m_textureShader.release();
@@ -163,11 +147,10 @@ void WSpinnyGLSL::initializeGL() {
 
     updateTextures();
 
-    m_pQTexture.reset(new QOpenGLTexture(QOpenGLTexture::Target2D));
-    m_pQTexture->setMinMagFilters(QOpenGLTexture::Linear, QOpenGLTexture::Linear);
-    m_pQTexture->setSize(m_iVinylScopeSize, m_iVinylScopeSize);
-    m_pQTexture->setFormat(QOpenGLTexture::R8_UNorm);
-    m_pQTexture->allocateStorage(QOpenGLTexture::Red, QOpenGLTexture::UInt8);
+    m_qTexture.setMinMagFilters(QOpenGLTexture::Linear, QOpenGLTexture::Linear);
+    m_qTexture.setSize(m_iVinylScopeSize, m_iVinylScopeSize);
+    m_qTexture.setFormat(QOpenGLTexture::R8_UNorm);
+    m_qTexture.allocateStorage(QOpenGLTexture::Red, QOpenGLTexture::UInt8);
 
     m_textureShader.init();
     m_vinylQualityShader.init();
@@ -241,11 +224,11 @@ void WSpinnyGLSL::drawVinylQuality() {
     m_vinylQualityShader.setAttributeArray(
             texcoordLocation, GL_FLOAT, texarray.data(), 2);
 
-    m_pQTexture->bind();
+    m_qTexture.bind();
 
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-    m_pQTexture->release();
+    m_qTexture.release();
 
     m_vinylQualityShader.release();
 }

src/widget/wspinnyglsl.h
@@ -4,6 +4,7 @@
 
 #include "shaders/textureshader.h"
 #include "shaders/vinylqualityshader.h"
+#include "util/opengltexture2d.h"
 #include "widget/wspinnybase.h"
 
 class QOpenGLTexture;
@@ -16,7 +17,7 @@ class WSpinnyGLSL : public WSpinnyBase, private QOpenGLFunctions {
             UserSettingsPointer pConfig,
             VinylControlManager* pVCMan,
             BaseTrackPlayer* pPlayer);
-    ~WSpinnyGLSL() override;
+    ~WSpinnyGLSL() override = default;
 
   private:
     void draw() override;
@@ -26,7 +27,6 @@ class WSpinnyGLSL : public WSpinnyBase, private QOpenGLFunctions {
     void paintGL() override;
     void resizeGL(int w, int h) override;
     void drawTexture(QOpenGLTexture* texture);
-    void cleanupGL();
     void updateTextures();
 
     void setupVinylSignalQuality() override;
@@ -36,11 +36,11 @@ class WSpinnyGLSL : public WSpinnyBase, private QOpenGLFunctions {
 
     mixxx::TextureShader m_textureShader;
     mixxx::VinylQualityShader m_vinylQualityShader;
-    std::unique_ptr<QOpenGLTexture> m_pBgTexture;
-    std::unique_ptr<QOpenGLTexture> m_pMaskTexture;
-    std::unique_ptr<QOpenGLTexture> m_pFgTextureScaled;
-    std::unique_ptr<QOpenGLTexture> m_pGhostTextureScaled;
-    std::unique_ptr<QOpenGLTexture> m_pLoadedCoverTextureScaled;
-    std::unique_ptr<QOpenGLTexture> m_pQTexture;
+    OpenGLTexture2D m_bgTexture;
+    OpenGLTexture2D m_maskTexture;
+    OpenGLTexture2D m_fgTextureScaled;
+    OpenGLTexture2D m_ghostTextureScaled;
+    OpenGLTexture2D m_loadedCoverTextureScaled;
+    OpenGLTexture2D m_qTexture;
     QColor m_vinylQualityColor;
 };

src/widget/wvumeterglsl.cpp
@@ -6,14 +6,9 @@
 #include "moc_wvumeterglsl.cpp"
 #include "util/duration.h"
 #include "util/math.h"
-#include "util/texture.h"
 
 WVuMeterGLSL::WVuMeterGLSL(QWidget* pParent)
         : WVuMeterBase(pParent) {
-}
-
-WVuMeterGLSL::~WVuMeterGLSL() {
-    cleanupGL();
 }
 
 void WVuMeterGLSL::draw() {
@@ -27,8 +22,8 @@ void WVuMeterGLSL::draw() {
 void WVuMeterGLSL::initializeGL() {
     initializeOpenGLFunctions();
 
-    m_pTextureBack = createTexture(m_pPixmapBack);
-    m_pTextureVu = createTexture(m_pPixmapVu);
+    m_textureBack.setData(m_pPixmapBack);
+    m_textureVu.setData(m_pPixmapVu);
     m_textureShader.init();
 }
 
@@ -62,17 +57,17 @@ void WVuMeterGLSL::paintGL() {
 
     m_textureShader.setUniformValue(textureLocation, 0);
 
-    if (m_pTextureBack) {
+    if (m_textureBack.isStorageAllocated()) {
         QRectF sourceRect(0, 0, m_pPixmapBack->width(), m_pPixmapBack->height());
-        drawTexture(m_pTextureBack.get(), rect(), sourceRect);
+        drawTexture(&m_textureBack, rect(), sourceRect);
     }
 
     const double widgetWidth = width();
     const double widgetHeight = height();
 
-    if (m_pTextureVu) {
-        const double pixmapWidth = m_pTextureVu->width();
-        const double pixmapHeight = m_pTextureVu->height();
+    if (m_textureVu.isStorageAllocated()) {
+        const double pixmapWidth = m_textureVu.width();
+        const double pixmapHeight = m_textureVu.height();
         if (m_bHorizontal) {
             const double widgetPosition = math_clamp(widgetWidth * m_dParameter, 0.0, widgetWidth);
             QRectF targetRect(0, 0, widgetPosition, math_min(pixmapHeight, widgetHeight));
@@ -80,7 +75,7 @@ void WVuMeterGLSL::paintGL() {
             const double pixmapPosition = math_clamp(
                     pixmapWidth * m_dParameter, 0.0, pixmapWidth);
             QRectF sourceRect(0, 0, pixmapPosition, pixmapHeight);
-            drawTexture(m_pTextureVu.get(), targetRect, sourceRect);
+            drawTexture(&m_textureVu, targetRect, sourceRect);
 
             if (m_iPeakHoldSize > 0 && m_dPeakParameter > 0.0 &&
                     m_dPeakParameter > m_dParameter) {
@@ -102,7 +97,7 @@ void WVuMeterGLSL::paintGL() {
                                 0,
                                 pixmapPeakHoldSize,
                                 pixmapHeight);
-                drawTexture(m_pTextureVu.get(), targetRect, sourceRect);
+                drawTexture(&m_textureVu, targetRect, sourceRect);
             }
         } else {
             // vertical
@@ -113,7 +108,7 @@ void WVuMeterGLSL::paintGL() {
             const double pixmapPosition = math_clamp(
                     pixmapHeight * m_dParameter, 0.0, pixmapHeight);
             QRectF sourceRect(0, pixmapHeight - pixmapPosition, pixmapWidth, pixmapPosition);
-            drawTexture(m_pTextureVu.get(), targetRect, sourceRect);
+            drawTexture(&m_textureVu, targetRect, sourceRect);
 
             if (m_iPeakHoldSize > 0 && m_dPeakParameter > 0.0 &&
                     m_dPeakParameter > m_dParameter) {
@@ -134,7 +129,7 @@ void WVuMeterGLSL::paintGL() {
                         pixmapHeight - pixmapPeakPosition,
                         pixmapWidth,
                         pixmapPeakHoldSize);
-                drawTexture(m_pTextureVu.get(), targetRect, sourceRect);
+                drawTexture(&m_textureVu, targetRect, sourceRect);
             }
         }
     }
@@ -142,13 +137,6 @@ void WVuMeterGLSL::paintGL() {
     m_textureShader.disableAttributeArray(positionLocation);
     m_textureShader.disableAttributeArray(texcoordLocation);
     m_textureShader.release();
-}
-
-void WVuMeterGLSL::cleanupGL() {
-    makeCurrentIfNeeded();
-    m_pTextureBack.reset();
-    m_pTextureVu.reset();
-    doneCurrent();
 }
 
 void WVuMeterGLSL::drawTexture(QOpenGLTexture* texture,

src/widget/wvumeterglsl.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "shaders/textureshader.h"
+#include "util/opengltexture2d.h"
 #include "widget/wvumeterbase.h"
 
 class QOpenGLTexture;
@@ -12,16 +13,15 @@ class WVuMeterGLSL : public WVuMeterBase, private QOpenGLFunctions {
     Q_OBJECT
   public:
     explicit WVuMeterGLSL(QWidget* pParent = nullptr);
-    ~WVuMeterGLSL() override;
+    ~WVuMeterGLSL() override = default;
 
   private:
-    std::unique_ptr<QOpenGLTexture> m_pTextureBack;
-    std::unique_ptr<QOpenGLTexture> m_pTextureVu;
+    OpenGLTexture2D m_textureBack;
+    OpenGLTexture2D m_textureVu;
     mixxx::TextureShader m_textureShader;
 
     void draw() override;
     void initializeGL() override;
-    void cleanupGL();
     void paintGL() override;
     void drawTexture(QOpenGLTexture* texture, const QRectF& sourceRect, const QRectF& targetRect);
 };

CMakeLists.txt
@@ -1485,7 +1485,7 @@ else()
       src/shaders/textureshader.cpp
       src/shaders/unicolorshader.cpp
       src/shaders/vinylqualityshader.cpp
-      src/util/texture.cpp
+      src/util/opengltexture2d.cpp
       src/waveform/renderers/allshader/matrixforwidgetgeometry.cpp
       src/waveform/renderers/allshader/waveformrenderbackground.cpp
       src/waveform/renderers/allshader/waveformrenderbeat.cpp

src/util/opengltexture2d.cpp
@@ -1,0 +1,33 @@
+#include "util/opengltexture2d.h"
+
+#include <QPixmap>
+
+#include "widget/paintable.h"
+
+OpenGLTexture2D::OpenGLTexture2D()
+        : QOpenGLTexture(QOpenGLTexture::Target2D){};
+
+void OpenGLTexture2D::setData(const QImage& image) {
+    destroy();
+    if (!image.isNull()) {
+        QOpenGLTexture::setData(image);
+        setMinMagFilters(QOpenGLTexture::Linear, QOpenGLTexture::Linear);
+        setWrapMode(QOpenGLTexture::ClampToEdge);
+    }
+};
+
+void OpenGLTexture2D::setData(const QPixmap& pixmap) {
+    setData(pixmap.toImage());
+};
+
+void OpenGLTexture2D::setData(const QSharedPointer<Paintable>& pPaintable) {
+    if (pPaintable) {
+        setData(pPaintable->toImage());
+    }
+};
+
+void OpenGLTexture2D::setData(const std::shared_ptr<QImage>& pImage) {
+    if (pImage) {
+        setData(*pImage);
+    }
+};

src/util/opengltexture2d.h
@@ -1,0 +1,16 @@
+#pragma once
+
+#include <QOpenGLTexture>
+#include <QSharedPointer>
+
+class Paintable;
+
+class OpenGLTexture2D : public QOpenGLTexture {
+  public:
+    OpenGLTexture2D();
+
+    void setData(const QImage& image);
+    void setData(const QPixmap& pixmap);
+    void setData(const QSharedPointer<Paintable>& pPaintable);
+    void setData(const std::shared_ptr<QImage>& pImage);
+};

src/util/texture.cpp
@@ -1,37 +1,0 @@
-#include "util/texture.h"
-
-#include <QImage>
-#include <QOpenGLTexture>
-#include <QPixmap>
-
-#include "widget/paintable.h"
-
-std::unique_ptr<QOpenGLTexture> createTexture(const QImage& image) {
-    if (image.isNull()) {
-        return nullptr;
-    }
-    auto pTexture = std::make_unique<QOpenGLTexture>(image);
-    pTexture->setMinificationFilter(QOpenGLTexture::Linear);
-    pTexture->setMagnificationFilter(QOpenGLTexture::Linear);
-    pTexture->setWrapMode(QOpenGLTexture::ClampToEdge);
-
-    return pTexture;
-}
-
-std::unique_ptr<QOpenGLTexture> createTexture(const QPixmap& pixmap) {
-    return createTexture(pixmap.toImage());
-}
-
-std::unique_ptr<QOpenGLTexture> createTexture(const QSharedPointer<Paintable>& pPaintable) {
-    if (pPaintable.isNull()) {
-        return nullptr;
-    }
-    return createTexture(pPaintable->toImage());
-}
-
-std::unique_ptr<QOpenGLTexture> createTexture(const std::shared_ptr<QImage>& pImage) {
-    if (!pImage) {
-        return nullptr;
-    }
-    return createTexture(*pImage);
-}

commit bf4e6db0145460d89f5cebe198dc09f07c55337f

destroy spinny and vumeter textures inside openglcontext (regression introduced in 07f05f1a0ef39e12b870718c9f1203f6f9335b14)

src/widget/wspinnyglsl.cpp
@@ -14,6 +14,21 @@ WSpinnyGLSL::WSpinnyGLSL(
         : WSpinnyBase(parent, group, pConfig, pVCMan, pPlayer) {
 }
 
+WSpinnyGLSL::~WSpinnyGLSL() {
+    cleanupGL();
+}
+
+void WSpinnyGLSL::cleanupGL() {
+    makeCurrentIfNeeded();
+    m_bgTexture.destroy();
+    m_maskTexture.destroy();
+    m_fgTextureScaled.destroy();
+    m_ghostTextureScaled.destroy();
+    m_loadedCoverTextureScaled.destroy();
+    m_qTexture.destroy();
+    doneCurrent();
+}
+
 void WSpinnyGLSL::coverChanged() {
     if (isContextValid()) {
         makeCurrentIfNeeded();

src/widget/wspinnyglsl.h
@@ -17,7 +17,7 @@ class WSpinnyGLSL : public WSpinnyBase, private QOpenGLFunctions {
             UserSettingsPointer pConfig,
             VinylControlManager* pVCMan,
             BaseTrackPlayer* pPlayer);
-    ~WSpinnyGLSL() override = default;
+    ~WSpinnyGLSL() override;
 
   private:
     void draw() override;
@@ -27,6 +27,7 @@ class WSpinnyGLSL : public WSpinnyBase, private QOpenGLFunctions {
     void paintGL() override;
     void resizeGL(int w, int h) override;
     void drawTexture(QOpenGLTexture* texture);
+    void cleanupGL();
     void updateTextures();
 
     void setupVinylSignalQuality() override;

src/widget/wvumeterglsl.cpp
@@ -11,6 +11,10 @@ WVuMeterGLSL::WVuMeterGLSL(QWidget* pParent)
         : WVuMeterBase(pParent) {
 }
 
+WVuMeterGLSL::~WVuMeterGLSL() {
+    cleanupGL();
+}
+
 void WVuMeterGLSL::draw() {
     if (shouldRender()) {
         makeCurrentIfNeeded();
@@ -137,6 +141,13 @@ void WVuMeterGLSL::paintGL() {
     m_textureShader.disableAttributeArray(positionLocation);
     m_textureShader.disableAttributeArray(texcoordLocation);
     m_textureShader.release();
+}
+
+void WVuMeterGLSL::cleanupGL() {
+    makeCurrentIfNeeded();
+    m_textureBack.destroy();
+    m_textureVu.destroy();
+    doneCurrent();
 }
 
 void WVuMeterGLSL::drawTexture(QOpenGLTexture* texture,

src/widget/wvumeterglsl.h
@@ -13,7 +13,7 @@ class WVuMeterGLSL : public WVuMeterBase, private QOpenGLFunctions {
     Q_OBJECT
   public:
     explicit WVuMeterGLSL(QWidget* pParent = nullptr);
-    ~WVuMeterGLSL() override = default;
+    ~WVuMeterGLSL() override;
 
   private:
     OpenGLTexture2D m_textureBack;
@@ -22,6 +22,7 @@ class WVuMeterGLSL : public WVuMeterBase, private QOpenGLFunctions {
 
     void draw() override;
     void initializeGL() override;
+    void cleanupGL();
     void paintGL() override;
     void drawTexture(QOpenGLTexture* texture, const QRectF& sourceRect, const QRectF& targetRect);
 };