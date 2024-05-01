d1e95b6feccf987bc53f5045aca2f7535fc1725f
ronso0 <ronso0@mixxx.org>
2024-05-01 14:22:03 +0200

    move handling of keyboard mapping file to KeyboardEventFilter

--- src/coreservices.h
+++ src/coreservices.h
@@ -43,9 +43,7 @@ class CoreServices : public QObject {
         return m_pKeyboardEventFilter;
     }
 
-    std::shared_ptr<ConfigObject<ConfigValueKbd>> getKeyboardConfig() const {
-        return m_pKbdConfig;
-    }
+    std::shared_ptr<ConfigObject<ConfigValueKbd>> getKeyboardConfig() const;
 
     std::shared_ptr<mixxx::ControlIndicatorTimer> getControlIndicatorTimer() const {
         return m_pControlIndicatorTimer;
@@ -138,8 +136,6 @@ class CoreServices : public QObject {
     std::shared_ptr<Library> m_pLibrary;
 
     std::shared_ptr<KeyboardEventFilter> m_pKeyboardEventFilter;
-    std::shared_ptr<ConfigObject<ConfigValueKbd>> m_pKbdConfig;
-    std::shared_ptr<ConfigObject<ConfigValueKbd>> m_pKbdConfigEmpty;
 
     std::shared_ptr<mixxx::ScreensaverManager> m_pScreensaverManager;
 
