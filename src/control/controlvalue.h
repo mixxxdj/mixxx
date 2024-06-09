mixxxdj/mixxx

commit c236956884e3c7b9a47536f98eb7eb5a126a26e4

Fix and enable multi-threaded analysis

src/control/controlvalue.h
@@ -1,17 +1,16 @@
-#ifndef CONTROLVALUE_H
-#define CONTROLVALUE_H
+#pragma once
 
 #include <limits>
 
 #include <QAtomicInt>
 #include <QObject>
 
-#include "util/compatibility.h"
 #include "util/assert.h"
+#include "util/compatibility.h"
 
 // for lock free access, this value has to be >= the number of value using threads
 // value must be a fraction of an integer
-const int cRingSize = 8;
+const int cDefaultRingSize = 8;
 // there are basically unlimited readers allowed at each ring element
 // but we have to count them so max() is just fine.
 // NOTE(rryan): Wrapping max with parentheses avoids conflict with the max macro
@@ -25,22 +24,45 @@ const int cReaderSlotCnt = (std::numeric_limits<int>::max)();
 // readers or a write is occurring. A write to the value will fail if
 // m_readerSlots is not equal to cReaderSlotCnt (e.g. there is an active
 // reader).
-template<typename T>
+template <typename T>
 class ControlRingValue {
   public:
-    ControlRingValue()
-        : m_value(T()),
-          m_readerSlots(cReaderSlotCnt) {
+    ControlRingValue() : m_readerSlots(cReaderSlotCnt) {
     }
 
+    // Tries to copy the stored value if a reader slot is available.
+    // This is operation can be repeated multiple times for the same
+    // slot, because the stored value is preserved.
     bool tryGet(T* value) const {
         // Read while consuming one readerSlot
-        bool hasSlot = (m_readerSlots.fetchAndAddAcquire(-1) > 0);
-        if (hasSlot) {
+        if (m_readerSlots.fetchAndAddAcquire(-1) > 0) {
             *value = m_value;
+            m_readerSlots.fetchAndAddRelease(1);
+            return true;
+        } else {
+            // Otherwise a writer is active. The writer will reset
+            // the counter in m_readerSlots when releasing the lock
+            // and we must not re-add the substracted value here!
+            return false;
         }
-        (void)m_readerSlots.fetchAndAddRelease(1);
-        return hasSlot;
+    }
+
+    // A destructive read operation that tries to move the stored
+    // value into the provided argument if a reader slot is available.
+    // This is operation should not be repeated once it returned true,
+    // because the stored value becomes invalid after it has been moved.
+    bool tryGetOnce(T* value) {
+        // Read while consuming one readerSlot
+        if (m_readerSlots.fetchAndAddAcquire(-1) > 0) {
+            *value = std::move(m_value);
+            m_readerSlots.fetchAndAddRelease(1);
+            return true;
+        } else {
+            // Otherwise a writer is active. The writer will reset
+            // the counter in m_readerSlots when releasing the lock
+            // and we must not re-add the substracted value here!
+            return false;
+        }
     }
 
     bool trySet(const T& value) {
@@ -49,9 +71,10 @@ class ControlRingValue {
             m_value = value;
             m_readerSlots.fetchAndAddRelease(cReaderSlotCnt);
             return true;
+        } else {
+            return false;
         }
-        return false;
-   }
+    }
 
   private:
     T m_value;
@@ -64,13 +87,13 @@ class ControlRingValue {
 // ring-buffer of ControlRingValues and a read pointer and write pointer to
 // provide getValue()/setValue() methods which *sacrifice perfect consistency*
 // for the benefit of wait-free read/write access to a value.
-template<typename T, bool ATOMIC = false>
+template <typename T, int cRingSize, bool ATOMIC = false>
 class ControlValueAtomicBase {
   public:
     inline T getValue() const {
-        T value = T();
-        unsigned int index = static_cast<unsigned int>(load_atomic(m_readIndex)) % (cRingSize);
-        while (m_ring[index].tryGet(&value) == false) {
+        T value;
+        unsigned int index = static_cast<unsigned int>(load_atomic(m_readIndex)) % cRingSize;
+        while (!m_ring[index].tryGet(&value)) {
             // We are here if
             // 1) there are more then cReaderSlotCnt reader (get) reading the same value or
             // 2) the formerly current value is locked by a writer
@@ -79,7 +102,24 @@ class ControlValueAtomicBase {
             // m_currentIndex and in the mean while a reader locks the formally current value
             // because it has written cRingSize times. Reading the less recent value will fix
             // it because it is now actually the current value.
-            index = (index - 1) % (cRingSize);
+            index = (index - 1) % cRingSize;
+        }
+        return value;
+    }
+
+    inline T getValueOnce() {
+        T value;
+        unsigned int index = static_cast<unsigned int>(load_atomic(m_readIndex)) % cRingSize;
+        while (!m_ring[index].tryGetOnce(&value)) {
+            // We are here if
+            // 1) there are more then cReaderSlotCnt reader (get) reading the same value or
+            // 2) the formerly current value is locked by a writer
+            // Case 1 does not happen because we have enough (0x7fffffff) reader slots.
+            // Case 2 happens when the a reader is delayed after reading the
+            // m_currentIndex and in the mean while a reader locks the formally current value
+            // because it has written cRingSize times. Reading the less recent value will fix
+            // it because it is now actually the current value.
+            index = (index - 1) % cRingSize;
         }
         return value;
     }
@@ -89,20 +129,17 @@ class ControlValueAtomicBase {
         // This test is const and will be mad only at compile time
         unsigned int index;
         do {
-            index = (unsigned int)m_writeIndex.fetchAndAddAcquire(1)
-                    % (cRingSize);
+            index = static_cast<unsigned int>(m_writeIndex.fetchAndAddAcquire(1)) % cRingSize;
             // This will be repeated if the value is locked
             // 1) by another writer writing at the same time or
             // 2) a delayed reader is still blocking the formerly current value
             // In both cases writing to the next value will fix it.
         } while (!m_ring[index].trySet(value));
-        m_readIndex = (int)index;
+        m_readIndex = index;
     }
 
   protected:
-    ControlValueAtomicBase()
-        : m_readIndex(0),
-          m_writeIndex(1) {
+    ControlValueAtomicBase() : m_readIndex(0), m_writeIndex(1) {
         // NOTE(rryan): Wrapping max with parentheses avoids conflict with the
         // max macro defined in windows.h.
         DEBUG_ASSERT(((std::numeric_limits<unsigned int>::max)() % cRingSize) == (cRingSize - 1));
@@ -119,11 +156,15 @@ class ControlValueAtomicBase {
 // Specialized template for types that are deemed to be atomic on the target
 // architecture. Instead of using a read/write ring to guarantee atomicity,
 // direct assignment/read of an aligned member variable is used.
-template<typename T>
+template <typename T>
 class ControlValueAtomicBase<T, true> {
   public:
     inline T getValue() const {
         return m_value;
+    }
+
+    inline T getValueOnce() {
+        return std::move(m_value);
     }
 
     inline void setValue(const T& value) {
@@ -131,13 +172,11 @@ class ControlValueAtomicBase<T, true> {
     }
 
   protected:
-    ControlValueAtomicBase()
-            : m_value(T()) {
-    }
+    ControlValueAtomicBase() = default;
 
   private:
 #if defined(__GNUC__)
-    T m_value __attribute__ ((aligned(sizeof(void*))));
+    T m_value __attribute__((aligned(sizeof(void*))));
 #elif defined(_MSC_VER)
 #ifdef _WIN64
     T __declspec(align(8)) m_value;
@@ -154,14 +193,8 @@ class ControlValueAtomicBase<T, true> {
 // ControlValueAtomicBase to use. For types where sizeof(T) <= sizeof(void*),
 // the specialized implementation of ControlValueAtomicBase for types that are
 // atomic on the architecture is used.
-template<typename T>
-class ControlValueAtomic
-    : public ControlValueAtomicBase<T, sizeof(T) <= sizeof(void*)> {
+template <typename T, int cRingSize = cDefaultRingSize>
+class ControlValueAtomic : public ControlValueAtomicBase<T, cRingSize, sizeof(T) <= sizeof(void*)> {
   public:
-
-    ControlValueAtomic()
-        : ControlValueAtomicBase<T, sizeof(T) <= sizeof(void*)>() {
-    }
+    ControlValueAtomic() = default;
 };
-
-#endif /* CONTROLVALUE_H */

build/depends.py
@@ -772,7 +772,8 @@ class MixxxCore(Feature):
                    "src/engine/cachingreaderchunk.cpp",
                    "src/engine/cachingreaderworker.cpp",
 
-                   "src/analyzer/analyzerqueue.cpp",
+                   "src/analyzer/trackanalysisscheduler.cpp",
+                   "src/analyzer/analyzerthread.cpp",
                    "src/analyzer/analyzerwaveform.cpp",
                    "src/analyzer/analyzergain.cpp",
                    "src/analyzer/analyzerebur128.cpp",
@@ -982,7 +983,7 @@ class MixxxCore(Feature):
                    "src/library/bpmdelegate.cpp",
                    "src/library/previewbuttondelegate.cpp",
                    "src/library/coverartdelegate.cpp",
-				   "src/library/tableitemdelegate.cpp",
+                   "src/library/tableitemdelegate.cpp",
 
                    "src/library/treeitemmodel.cpp",
                    "src/library/treeitem.cpp",
@@ -1153,6 +1154,8 @@ class MixxxCore(Feature):
                    "src/util/indexrange.cpp",
                    "src/util/desktophelper.cpp",
                    "src/util/widgetrendertimer.cpp",
+                   "src/util/workerthread.cpp",
+                   "src/util/workerthreadscheduler.cpp",
                    ]
 
         proto_args = {

src/analyzer/analyzerebur128.h
@@ -11,6 +11,10 @@ class AnalyzerEbur128 : public Analyzer {
     AnalyzerEbur128(UserSettingsPointer pConfig);
     virtual ~AnalyzerEbur128();
 
+    static bool isEnabled(const ReplayGainSettings& rgSettings) {
+        return rgSettings.isAnalyzerEnabled(2);
+    }
+
     bool initialize(TrackPointer tio, int sampleRate, int totalSamples) override;
     bool isDisabledOrLoadStoredSuccess(TrackPointer tio) const override;
     void process(const CSAMPLE* pIn, const int iLen) override;

src/analyzer/analyzergain.h
@@ -18,6 +18,10 @@ class AnalyzerGain : public Analyzer {
     AnalyzerGain(UserSettingsPointer pConfig);
     virtual ~AnalyzerGain();
 
+    static bool isEnabled(const ReplayGainSettings& rgSettings) {
+        return rgSettings.isAnalyzerEnabled(1);
+    }
+
     bool initialize(TrackPointer tio, int sampleRate, int totalSamples) override;
     bool isDisabledOrLoadStoredSuccess(TrackPointer tio) const override;
     void process(const CSAMPLE* pIn, const int iLen) override;

src/analyzer/analyzerprogress.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include "util/math.h"
+
+
+typedef double AnalyzerProgress;
+
+constexpr AnalyzerProgress kAnalyzerProgressUnknown    = -1.0;
+constexpr AnalyzerProgress kAnalyzerProgressNone       =  0.0;  //   0.0 %
+constexpr AnalyzerProgress kAnalyzerProgressHalf       =  0.5;  //  50.0 %
+constexpr AnalyzerProgress kAnalyzerProgressFinalizing =  0.95; //  95.0 %
+constexpr AnalyzerProgress kAnalyzerProgressDone       =  1.0;  // 100.0%
+
+Q_DECLARE_METATYPE(AnalyzerProgress);
+
+// Integer [0, 100]
+inline
+int analyzerProgressPercent(AnalyzerProgress analyzerProgress) {
+    DEBUG_ASSERT(analyzerProgress >= kAnalyzerProgressNone);
+    return int((100 * (math_min(analyzerProgress, kAnalyzerProgressDone) - kAnalyzerProgressNone)) /
+            (kAnalyzerProgressDone - kAnalyzerProgressNone));
+}

src/analyzer/analyzerqueue.cpp
@@ -1,468 +1,0 @@
-#include "analyzer/analyzerqueue.h"
-
-#ifdef __VAMP__
-#include "analyzer/analyzerbeats.h"
-#include "analyzer/analyzerkey.h"
-#endif
-#include "analyzer/analyzergain.h"
-#include "analyzer/analyzerebur128.h"
-#include "analyzer/analyzerwaveform.h"
-#include "library/dao/analysisdao.h"
-#include "engine/engine.h"
-#include "mixer/playerinfo.h"
-#include "sources/soundsourceproxy.h"
-#include "sources/audiosourcestereoproxy.h"
-#include "track/track.h"
-#include "util/compatibility.h"
-#include "util/db/dbconnectionpooler.h"
-#include "util/db/dbconnectionpooled.h"
-#include "util/event.h"
-#include "util/timer.h"
-#include "util/trace.h"
-#include "util/logger.h"
-
-// Measured in 0.1%,
-// 0 for no progress during finalize
-// 1 to display the text "finalizing"
-// 100 for 10% step after finalize
-#define FINALIZE_PROMILLE 1
-
-namespace {
-
-mixxx::Logger kLogger("AnalyzerQueue");
-
-// Analysis is done in blocks.
-// We need to use a smaller block size, because on Linux the AnalyzerQueue
-// can starve the CPU of its resources, resulting in xruns. A block size
-// of 4096 frames per block seems to do fine.
-const mixxx::AudioSignal::ChannelCount kAnalysisChannels(mixxx::kEngineChannelCount);
-const SINT kAnalysisFramesPerBlock = 4096;
-const SINT kAnalysisSamplesPerBlock =
-        kAnalysisFramesPerBlock * kAnalysisChannels;
-
-QAtomicInt s_instanceCounter(0);
-
-} // anonymous namespace
-
-AnalyzerQueue::AnalyzerQueue(
-        mixxx::DbConnectionPoolPtr pDbConnectionPool,
-        const UserSettingsPointer& pConfig,
-        Mode mode)
-        : m_pDbConnectionPool(std::move(pDbConnectionPool)),
-          m_exit(false),
-          m_aiCheckPriorities(false),
-          m_sampleBuffer(kAnalysisSamplesPerBlock),
-          m_queue_size(0) {
-
-    if (mode != Mode::WithoutWaveform) {
-        m_pAnalysisDao = std::make_unique<AnalysisDao>(pConfig);
-        m_pAnalyzers.push_back(std::make_unique<AnalyzerWaveform>(m_pAnalysisDao.get()));
-    }
-    m_pAnalyzers.push_back(std::make_unique<AnalyzerGain>(pConfig));
-    m_pAnalyzers.push_back(std::make_unique<AnalyzerEbur128>(pConfig));
-#ifdef __VAMP__
-    m_pAnalyzers.push_back(std::make_unique<AnalyzerBeats>(pConfig));
-    m_pAnalyzers.push_back(std::make_unique<AnalyzerKey>(pConfig));
-#endif
-
-    connect(this, SIGNAL(updateProgress()),
-            this, SLOT(slotUpdateProgress()));
-
-    start(QThread::LowPriority);
-}
-
-AnalyzerQueue::~AnalyzerQueue() {
-    stop();
-    m_progressInfo.sema.release();
-    wait(); //Wait until thread has actually stopped before proceeding.
-}
-
-// This is called from the AnalyzerQueue thread
-bool AnalyzerQueue::isLoadedTrackWaiting(TrackPointer analysingTrack) {
-    const PlayerInfo& info = PlayerInfo::instance();
-    TrackPointer pTrack;
-    bool trackWaiting = false;
-    QList<TrackPointer> progress100List;
-    QList<TrackPointer> progress0List;
-
-    QMutexLocker locked(&m_qm);
-    QMutableListIterator<TrackPointer> it(m_queuedTracks);
-    while (it.hasNext()) {
-        TrackPointer& pTrack = it.next();
-        if (!pTrack) {
-            it.remove();
-            continue;
-        }
-        if (!trackWaiting) {
-            trackWaiting = info.isTrackLoaded(pTrack);
-        }
-        // try to load waveforms for all new tracks first
-        // and remove them from queue if already analysed
-        // This avoids waiting for a running analysis for those tracks.
-        int progress = pTrack->getAnalyzerProgress();
-        if (progress < 0) {
-            // Load stored analysis
-            bool processTrack = false;
-            for (auto const& pAnalyzer: m_pAnalyzers) {
-                if (!pAnalyzer->isDisabledOrLoadStoredSuccess(pTrack)) {
-                    processTrack = true;
-                }
-            }
-            if (!processTrack) {
-                progress100List.append(pTrack);
-                it.remove(); // since pTrack is a reference it is invalid now.
-            } else {
-                progress0List.append(pTrack);
-            }
-        } else if (progress == 1000) {
-            it.remove();
-        }
-    }
-
-    locked.unlock();
-
-    // update progress after unlock to avoid a deadlock
-    foreach (TrackPointer pTrack, progress100List) {
-        emitUpdateProgress(pTrack, 1000);
-    }
-    foreach (TrackPointer pTrack, progress0List) {
-        emitUpdateProgress(pTrack, 0);
-    }
-
-    if (info.isTrackLoaded(analysingTrack)) {
-        return false;
-    }
-    return trackWaiting;
-}
-
-// This is called from the AnalyzerQueue thread
-// The returned track might be NULL, up to the caller to check.
-TrackPointer AnalyzerQueue::dequeueNextBlocking() {
-    QMutexLocker locked(&m_qm);
-    if (m_queuedTracks.isEmpty()) {
-        Event::end("AnalyzerQueue process");
-        m_qwait.wait(&m_qm);
-        Event::start("AnalyzerQueue process");
-
-        if (m_exit) {
-            return TrackPointer();
-        }
-    }
-
-    const PlayerInfo& info = PlayerInfo::instance();
-    TrackPointer pLoadTrack;
-    QMutableListIterator<TrackPointer> it(m_queuedTracks);
-    while (it.hasNext()) {
-        TrackPointer& pTrack = it.next();
-        DEBUG_ASSERT(pTrack);
-        // Prioritize tracks that are loaded.
-        if (info.isTrackLoaded(pTrack)) {
-            kLogger.debug() << "Prioritizing" << pTrack->getTitle() << pTrack->getLocation();
-            pLoadTrack = pTrack;
-            it.remove();
-            break;
-        }
-    }
-
-    if (!pLoadTrack && !m_queuedTracks.isEmpty()) {
-        // no prioritized track found, use head track
-        pLoadTrack = m_queuedTracks.dequeue();
-    }
-
-    return pLoadTrack;
-}
-
-// This is called from the AnalyzerQueue thread
-bool AnalyzerQueue::doAnalysis(
-        TrackPointer pTrack,
-        mixxx::AudioSourcePointer pAudioSource) {
-
-    QTime progressUpdateInhibitTimer;
-    progressUpdateInhibitTimer.start(); // Inhibit Updates for 60 milliseconds
-
-    mixxx::AudioSourceStereoProxy audioSourceProxy(
-            pAudioSource,
-            kAnalysisFramesPerBlock);
-    DEBUG_ASSERT(audioSourceProxy.channelCount() == kAnalysisChannels);
-
-    mixxx::IndexRange remainingFrames = pAudioSource->frameIndexRange();
-    bool dieflag = false;
-    bool cancelled = false;
-    while (!dieflag && !remainingFrames.empty()) {
-        ScopedTimer t("AnalyzerQueue::doAnalysis block");
-
-        const auto inputFrameIndexRange =
-                remainingFrames.splitAndShrinkFront(
-                        math_min(kAnalysisFramesPerBlock, remainingFrames.length()));
-        DEBUG_ASSERT(!inputFrameIndexRange.empty());
-        const auto readableSampleFrames =
-                audioSourceProxy.readSampleFrames(
-                        mixxx::WritableSampleFrames(
-                                inputFrameIndexRange,
-                                mixxx::SampleBuffer::WritableSlice(m_sampleBuffer)));
-        // To compare apples to apples, let's only look at blocks that are
-        // the full block size.
-        if (readableSampleFrames.frameLength() == kAnalysisFramesPerBlock) {
-            // Complete analysis block of audio samples has been read.
-            for (auto const& pAnalyzer: m_pAnalyzers) {
-                pAnalyzer->process(
-                        readableSampleFrames.readableData(),
-                        readableSampleFrames.readableLength());
-            }
-        } else {
-            // Partial analysis block of audio samples has been read.
-            // This should only happen at the end of an audio stream,
-            // otherwise a decoding error must have occurred.
-            if (!remainingFrames.empty()) {
-                // EOF not reached -> Maybe a corrupt file?
-                kLogger.warning()
-                        << "Aborting analysis after failed to read sample data from"
-                        << pTrack->getLocation()
-                        << ": expected frames =" << inputFrameIndexRange
-                        << ", actual frames =" << readableSampleFrames.frameIndexRange();
-                dieflag = true; // abort
-                cancelled = false; // completed, no retry
-            }
-        }
-
-        // emit progress updates
-        // During the doAnalysis function it goes only to 100% - FINALIZE_PERCENT
-        // because the finalize functions will take also some time
-        //fp div here prevents insane signed overflow
-        const double frameProgress =
-                double(pAudioSource->frameLength() - remainingFrames.length()) /
-                double(pAudioSource->frameLength());
-        int progressPromille = frameProgress * (1000 - FINALIZE_PROMILLE);
-
-        if (m_progressInfo.track_progress != progressPromille) {
-            if (progressUpdateInhibitTimer.elapsed() > 60) {
-                // Inhibit Updates for 60 milliseconds
-                emitUpdateProgress(pTrack, progressPromille);
-                progressUpdateInhibitTimer.start();
-            }
-        }
-
-        // Since this is a background analysis queue, we should co-operatively
-        // yield every now and then to try and reduce CPU contention. The
-        // analyzer queue is CPU intensive so we want to get out of the way of
-        // the audio callback thread.
-        //QThread::yieldCurrentThread();
-        //QThread::usleep(10);
-
-        // has something new entered the queue?
-        if (m_aiCheckPriorities.fetchAndStoreAcquire(false)) {
-            if (isLoadedTrackWaiting(pTrack)) {
-                kLogger.debug() << "Interrupting analysis to give preference to a loaded track.";
-                dieflag = true;
-                cancelled = true;
-            }
-        }
-
-        if (m_exit) {
-            dieflag = true;
-            cancelled = true;
-        }
-
-        // Ignore blocks in which we decided to bail for stats purposes.
-        if (dieflag || cancelled) {
-            t.cancel();
-        }
-    }
-
-    return !cancelled; //don't return !dieflag or we might reanalyze over and over
-}
-
-void AnalyzerQueue::stop() {
-    m_exit = true;
-    QMutexLocker locked(&m_qm);
-    m_qwait.wakeAll();
-}
-
-void AnalyzerQueue::run() {
-    // If there are no analyzers, don't waste time running.
-    if (m_pAnalyzers.empty()) {
-        return;
-    }
-
-    const int instanceId = s_instanceCounter.fetchAndAddAcquire(1) + 1;
-    QThread::currentThread()->setObjectName(QString("AnalyzerQueue %1").arg(instanceId));
-
-    kLogger.debug() << "Entering thread";
-
-    execThread();
-
-    kLogger.debug() << "Exiting thread";
-}
-
-void AnalyzerQueue::execThread() {
-    // The thread-local database connection for waveform analysis must not
-    // be closed before returning from this function. Therefore the
-    // DbConnectionPooler is defined at this outer function scope,
-    // independent of whether a database connection will be opened
-    // or not.
-    mixxx::DbConnectionPooler dbConnectionPooler;
-    // m_pAnalysisDao remains null if no analyzer needs database access.
-    // Currently only waveform analyses makes use of it.
-    if (m_pAnalysisDao) {
-        dbConnectionPooler = mixxx::DbConnectionPooler(m_pDbConnectionPool); // move assignment
-        if (!dbConnectionPooler.isPooling()) {
-            kLogger.warning()
-                    << "Failed to obtain database connection for analyzer queue thread";
-            return;
-        }
-        // Obtain and use the newly created database connection within this thread
-        QSqlDatabase dbConnection = mixxx::DbConnectionPooled(m_pDbConnectionPool);
-        DEBUG_ASSERT(dbConnection.isOpen());
-        m_pAnalysisDao->initialize(dbConnection);
-    }
-
-    m_progressInfo.current_track.reset();
-    m_progressInfo.track_progress = 0;
-    m_progressInfo.queue_size = 0;
-    m_progressInfo.sema.release(); // Initialize with one
-
-    while (!m_exit) {
-        TrackPointer nextTrack = dequeueNextBlocking();
-
-        // It's important to check for m_exit here in case we decided to exit
-        // while blocking for a new track.
-        if (m_exit) {
-            break;
-        }
-
-        // If the track is NULL, try to get the next one.
-        // Could happen if the track was queued but then deleted.
-        // Or if dequeueNextBlocking is unblocked by exit == true
-        if (!nextTrack) {
-            emptyCheck();
-            continue;
-        }
-
-        kLogger.debug() << "Analyzing" << nextTrack->getTitle() << nextTrack->getLocation();
-
-        Trace trace("AnalyzerQueue analyzing track");
-
-        // Get the audio
-        mixxx::AudioSource::OpenParams openParams;
-        openParams.setChannelCount(kAnalysisChannels);
-        auto pAudioSource = SoundSourceProxy(nextTrack).openAudioSource(openParams);
-        if (!pAudioSource) {
-            kLogger.warning()
-                    << "Failed to open file for analyzing:"
-                    << nextTrack->getLocation();
-            emptyCheck();
-            continue;
-        }
-
-        bool processTrack = false;
-        for (auto const& pAnalyzer: m_pAnalyzers) {
-            // Make sure not to short-circuit initialize(...)
-            if (pAnalyzer->initialize(
-                    nextTrack,
-                    pAudioSource->sampleRate(),
-                    pAudioSource->frameLength() * kAnalysisChannels)) {
-                processTrack = true;
-            }
-        }
-
-        updateSize();
-
-        if (processTrack) {
-            emitUpdateProgress(nextTrack, 0);
-            bool completed = doAnalysis(nextTrack, pAudioSource);
-            if (!completed) {
-                // This track was cancelled
-                for (auto const& pAnalyzer: m_pAnalyzers) {
-                    pAnalyzer->cleanup(nextTrack);
-                }
-                queueAnalyseTrack(nextTrack);
-                emitUpdateProgress(nextTrack, 0);
-            } else {
-                // 100% - FINALIZE_PERCENT finished
-                emitUpdateProgress(nextTrack, 1000 - FINALIZE_PROMILLE);
-                // This takes around 3 sec on a Atom Netbook
-                for (auto const& pAnalyzer: m_pAnalyzers) {
-                    pAnalyzer->finalize(nextTrack);
-                }
-                emit(trackDone(nextTrack));
-                emitUpdateProgress(nextTrack, 1000); // 100%
-            }
-        } else {
-            emitUpdateProgress(nextTrack, 1000); // 100%
-            kLogger.debug() << "Skipping track analysis because no analyzer initialized.";
-        }
-        emptyCheck();
-    }
-
-    if (m_pAnalysisDao) {
-        // Invalidate reference to the thread-local database connection
-        // that will be closed soon. Not necessary, just in case ;)
-        m_pAnalysisDao->initialize(QSqlDatabase());
-    }
-
-    emit(queueEmpty()); // emit in case of exit;
-}
-
-void AnalyzerQueue::emptyCheck() {
-    updateSize();
-    if (m_queue_size == 0) {
-        emit(queueEmpty()); // emit asynchrony for no deadlock
-    }
-}
-
-void AnalyzerQueue::updateSize() {
-    QMutexLocker locked(&m_qm);
-    m_queue_size = m_queuedTracks.size();
-}
-
-// This is called from the AnalyzerQueue thread
-void AnalyzerQueue::emitUpdateProgress(TrackPointer track, int progress) {
-    if (!m_exit) {
-        // First tryAcqire will have always success because sema is initialized with on
-        // The following tries will success if the previous signal was processed in the GUI Thread
-        // This prevent the AnalysisQueue from filling up the GUI Thread event Queue
-        // 100 % is emitted in any case
-        if (progress < 1000 - FINALIZE_PROMILLE && progress > 0) {
-            // Signals during processing are not required in any case
-            if (!m_progressInfo.sema.tryAcquire()) {
-               return;
-            }
-        } else {
-            m_progressInfo.sema.acquire();
-        }
-        m_progressInfo.current_track = track;
-        m_progressInfo.track_progress = progress;
-        m_progressInfo.queue_size = m_queue_size;
-        emit(updateProgress());
-    }
-}
-
-//slot
-void AnalyzerQueue::slotUpdateProgress() {
-    if (m_progressInfo.current_track) {
-        m_progressInfo.current_track->setAnalyzerProgress(m_progressInfo.track_progress);
-        m_progressInfo.current_track.reset();
-    }
-    emit(trackProgress(m_progressInfo.track_progress / 10));
-    if (m_progressInfo.track_progress == 1000) {
-        emit(trackFinished(m_progressInfo.queue_size));
-    }
-    m_progressInfo.sema.release();
-}
-
-void AnalyzerQueue::slotAnalyseTrack(TrackPointer pTrack) {
-    // This slot is called from the decks and and samplers when the track was loaded.
-    queueAnalyseTrack(pTrack);
-    m_aiCheckPriorities = true;
-}
-
-// This is called from the GUI and from the AnalyzerQueue thread
-void AnalyzerQueue::queueAnalyseTrack(TrackPointer pTrack) {
-    if (pTrack) {
-        QMutexLocker locked(&m_qm);
-        if (!m_queuedTracks.contains(pTrack)) {
-            m_queuedTracks.enqueue(pTrack);
-            m_qwait.wakeAll();
-        }
-    }
-}

commit a396632184f38862debee9710008256deae2ada2

Fix missing template parameter in specialization
This is a regression since c236956884e3c7b9a47536f98eb7eb5a126a26e4 merged into 2.3 which prevents to use the fast ControlValueAtomicBase specialization.

src/control/controlvalue.h
@@ -123,8 +123,8 @@ class ControlValueAtomicBase {
 // Specialized template for types that are deemed to be atomic on the target
 // architecture. Instead of using a read/write ring to guarantee atomicity,
 // direct assignment/read of an aligned member variable is used.
-template <typename T>
-class ControlValueAtomicBase<T, true> {
+template<typename T, int cRingSize>
+class ControlValueAtomicBase<T, cRingSize, true> {
   public:
     inline T getValue() const {
         return m_value;