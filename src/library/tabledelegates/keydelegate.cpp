680c86fdcb860301ce4f871157639fc0cb51de35
danferns <dannywritescode@gmail.com>
2024-06-19 12:39:24 +0530

    implement the design for Key Column colors

--- src/library/tabledelegates/keydelegate.cpp
+++ src/library/tabledelegates/keydelegate.cpp
@@ -19,31 +19,41 @@ void KeyDelegate::paintItem(
         QPainter* painter,
         const QStyleOptionViewItem& option,
         const QModelIndex& index) const {
+    paintItemBackground(painter, option, index);
+
     QString keyText = index.data().toString();
     mixxx::track::io::key::ChromaticKey key = KeyUtils::guessKeyFromText(keyText);
     int openKeyNumber = KeyUtils::keyToOpenKeyNumber(key);
 
     if (openKeyNumber != 0) {
-        // Open Key numbers start from 1
-        const auto color =
+        const auto rgbColor =
                 mixxx::PredefinedColorPalettes::kDefaultKeyColorPalette.at(
-                        openKeyNumber - 1);
+                        openKeyNumber - 1); // Open Key numbers start from 1
+        const auto qcolor = mixxx::RgbColor::toQColor(rgbColor);
 
-        // this code is from the ColorDelegate, using it temporarily for testing.
-        if (color) {
-            painter->fillRect(option.rect, mixxx::RgbColor::toQColor(color));
-        } else {
-            // Filter out track color that is hidden
-            if (option.state & QStyle::State_Selected) {
-                painter->fillRect(option.rect, option.palette.highlight());
-            }
-        }
-
-        // Draw a border if the color cell has focus
-        if (option.state & QStyle::State_HasFocus) {
-            drawBorder(painter, m_focusBorderColor, option.rect);
-        }
+        // Draw the colored rectangle next to the key label
+        painter->fillRect(
+                option.rect.x() + 2,
+                option.rect.y() + 2,
+                4, // width
+                option.rect.height() - 4,
+                qcolor);
     }
 
-    // QStyledItemDelegate::paint(painter, option, index);
+    QString elidedText = option.fontMetrics.elidedText(
+            index.data().toString(),
+            Qt::ElideLeft,
+            columnWidth(index) - 8); // Subtracting total width of the rectangle
+
+    painter->drawText(option.rect.x() + 8,
+            option.rect.y(),
+            option.rect.width() - 8,
+            option.rect.height(),
+            Qt::AlignVCenter,
+            elidedText);
+
+    // Draw a border if the key cell has focus
+    if (option.state & QStyle::State_HasFocus) {
+        drawBorder(painter, m_focusBorderColor, option.rect);
+    }
 }
