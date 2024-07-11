7c9e6021126c50e67c1d616c87c1881b9e1c0be7
danferns <dannywritescode@gmail.com>
2024-07-11 14:12:28 +0530

    refactor: draw key text/color only if key is valid

--- src/library/tabledelegates/keydelegate.cpp
+++ src/library/tabledelegates/keydelegate.cpp
@@ -15,47 +15,45 @@ void KeyDelegate::paintItem(
 
     mixxx::track::io::key::ChromaticKey key =
             index.data().value<mixxx::track::io::key::ChromaticKey>();
-    // Get the key text with the user-provided notation
-    bool keyColorsEnabled = index.data(Qt::UserRole).value<bool>();
-    int rectWidth = 0; // only leave space for rect if it is rendered
-    if (keyColorsEnabled) {
-        const QColor keyColor = KeyUtils::keyToColor(key);
-        rectWidth = 8; // 4px width + 4px right padding
-        if (keyColor.isValid()) {
-            // Draw the colored rectangle next to the key label
-            painter->fillRect(
-                    option.rect.x(),
-                    option.rect.y() + 2,
-                    4, // width
-                    option.rect.height() - 4,
-                    keyColor);
+    if (key != mixxx::track::io::key::INVALID) {
+        // Display the key colors if enabled
+        bool keyColorsEnabled = index.data(Qt::UserRole).value<bool>();
+        if (keyColorsEnabled) {
+            const QColor keyColor = KeyUtils::keyToColor(key);
+            if (keyColor.isValid()) {
+                // Draw the colored rectangle next to the key label
+                painter->fillRect(
+                        option.rect.x(),
+                        option.rect.y() + 2,
+                        4, // width
+                        option.rect.height() - 4,
+                        keyColor);
+            }
         }
+
+        // Display the key text with the user-provided notation
+        int rectWidth = keyColorsEnabled ? 8 : 0; //  4px width + 4px right padding
+        const QString keyText = KeyUtils::keyToString(key);
+        QString elidedText = option.fontMetrics.elidedText(
+                keyText,
+                Qt::ElideLeft,
+                columnWidth(index) - rectWidth);
+
+        if (option.state & QStyle::State_Selected) {
+            // This uses selection-color from stylesheet for the text pen:
+            // #LibraryContainer QTableView {
+            //   selection-color: #fff;
+            // }
+            painter->setPen(QPen(option.palette.highlightedText().color()));
+        }
+
+        painter->drawText(option.rect.x() + rectWidth,
+                option.rect.y(),
+                option.rect.width() - rectWidth,
+                option.rect.height(),
+                Qt::AlignVCenter,
+                elidedText);
     }
-
-    QString keyText = KeyUtils::keyToString(key);
-    if (QString("INVALID").compare(keyText) == 0) { // if the key is invalid, display empty string
-        keyText.clear();
-    }
-
-    QString elidedText = option.fontMetrics.elidedText(
-            keyText,
-            Qt::ElideLeft,
-            columnWidth(index) - rectWidth);
-
-    if (option.state & QStyle::State_Selected) {
-        // This uses selection-color from stylesheet for the text pen:
-        // #LibraryContainer QTableView {
-        //   selection-color: #fff;
-        // }
-        painter->setPen(QPen(option.palette.highlightedText().color()));
-    }
-
-    painter->drawText(option.rect.x() + rectWidth,
-            option.rect.y(),
-            option.rect.width() - rectWidth,
-            option.rect.height(),
-            Qt::AlignVCenter,
-            elidedText);
 
     // Draw a border if the key cell has focus
     if (option.state & QStyle::State_HasFocus) {
