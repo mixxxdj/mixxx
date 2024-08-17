b060c68ac0740be0eaf236a36c020b3dd2adfdb7
fwcd <fwcd@fwcd.dev>
2024-08-17 03:59:25 +0200

    MC7000: Fix small stylistic typo

--- res/controllers/Denon-MC7000-scripts.js
+++ res/controllers/Denon-MC7000-scripts.js
@@ -98,7 +98,7 @@ MC7000.jogParams = {
     // set to 3 with audio buffer set to 5ms
     sensitivity: engine.getSetting("jogSensitivity") || 1,
     // Acceleration settings for the jog wheel in vinyl mode
-    // If enabled, the track speed will accelerate faster than the physical jogheel movement. Be aware, that the absolute track position will drift relative to the jogwheel position in  this mode!
+    // If enabled, the track speed will accelerate faster than the physical jogheel movement. Be aware that the absolute track position will drift relative to the jogwheel position in this mode!
     // (exponent: 0 and coefficient: 1 = no acceleration)
     acceleration: {
         // Toggles acceleration entirely.
