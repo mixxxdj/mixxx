241665824c15258a9fefa097792c54d56f334a5b
fwcd <fwcd@fwcd.dev>
2024-08-19 01:16:45 +0200

    Library: Remove automatic rescan on iOS

--- src/library/library.cpp
+++ src/library/library.cpp
@@ -703,7 +703,6 @@ void Library::requestRelocateIOSSandboxDirs() {
 
     // TODO: Do we need to handle external track collections?
 
-    bool needsRescan = false;
     QStringList rootDirs = m_pTrackCollectionManager->internalCollection()->getRootDirStrings();
 
     for (const QString& dir : rootDirs) {
@@ -722,13 +721,6 @@ void Library::requestRelocateIOSSandboxDirs() {
             qWarning() << "Could not relink music directory after iOS sandbox moved";
             continue;
         }
-
-        needsRescan = true;
-    }
-
-    if (needsRescan) {
-        qInfo() << "Rescanning library since iOS sandbox moved";
-        m_pTrackCollectionManager->startLibraryScan();
     }
 }
 #endif
