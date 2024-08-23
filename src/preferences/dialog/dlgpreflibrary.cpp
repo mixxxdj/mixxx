mixxxdj/mixxx

commit 6e588ec8201d86f7ecd4702fb5baa47b78fdfb5e

Preferences > Library: re-group options, add Track Search group
* apply consistent naming scheme: widgetType_some_library_option
* enable 'openExternalLinks' for settings dir button

src/preferences/dialog/dlgpreflibrary.cpp
@@ -46,24 +46,24 @@ DlgPrefLibrary::DlgPrefLibrary(
             &DlgPrefLibrary::requestRelocateDir,
             m_pLibrary.get(),
             &Library::slotRequestRelocateDir);
-    connect(PushButtonAddDir,
+    connect(pushButton_add_dir,
             &QPushButton::clicked,
             this,
             &DlgPrefLibrary::slotAddDir);
-    connect(PushButtonRemoveDir,
+    connect(pushButton_remove_dir,
             &QPushButton::clicked,
             this,
             &DlgPrefLibrary::slotRemoveDir);
-    connect(PushButtonRelocateDir,
+    connect(pushButton_relocate_dir,
             &QPushButton::clicked,
             this,
             &DlgPrefLibrary::slotRelocateDir);
-    connect(checkBox_SeratoMetadataExport,
+    connect(checkBox_serato_metadata_export,
             &QAbstractButton::clicked,
             this,
             &DlgPrefLibrary::slotSeratoMetadataExportClicked);
     const QString& settingsDir = m_pConfig->getSettingsPath();
-    connect(PushButtonOpenSettingsDir,
+    connect(pushButton_open_settings_dir,
             &QPushButton::clicked,
             [settingsDir] {
                 mixxx::DesktopHelper::openUrl(QUrl::fromLocalFile(settingsDir));
@@ -71,8 +71,8 @@ DlgPrefLibrary::DlgPrefLibrary(
 
     // Set default direction as stored in config file
     int rowHeight = m_pLibrary->getTrackTableRowHeight();
-    spinBoxRowHeight->setValue(rowHeight);
-    connect(spinBoxRowHeight,
+    spinBox_row_height->setValue(rowHeight);
+    connect(spinBox_row_height,
             QOverload<int>::of(&QSpinBox::valueChanged),
             this,
             &DlgPrefLibrary::slotRowHeightValueChanged);
@@ -84,16 +84,16 @@ DlgPrefLibrary::DlgPrefLibrary(
             this,
             &DlgPrefLibrary::slotBpmColumnPrecisionChanged);
 
-    searchDebouncingTimeoutSpinBox->setMinimum(WSearchLineEdit::kMinDebouncingTimeoutMillis);
-    searchDebouncingTimeoutSpinBox->setMaximum(WSearchLineEdit::kMaxDebouncingTimeoutMillis);
-    connect(searchDebouncingTimeoutSpinBox,
+    spinBox_search_debouncing_timeout->setMinimum(WSearchLineEdit::kMinDebouncingTimeoutMillis);
+    spinBox_search_debouncing_timeout->setMaximum(WSearchLineEdit::kMaxDebouncingTimeoutMillis);
+    connect(spinBox_search_debouncing_timeout,
             QOverload<int>::of(&QSpinBox::valueChanged),
             this,
             &DlgPrefLibrary::slotSearchDebouncingTimeoutMillisChanged);
 
     updateSearchLineEditHistoryOptions();
 
-    connect(libraryFontButton, &QAbstractButton::clicked, this, &DlgPrefLibrary::slotSelectFont);
+    connect(btn_library_font, &QAbstractButton::clicked, this, &DlgPrefLibrary::slotSelectFont);
 
     // TODO(XXX) this string should be extracted from the soundsources
     QString builtInFormatsStr = "Ogg Vorbis, FLAC, WAVE, AIFF";
@@ -127,7 +127,7 @@ DlgPrefLibrary::DlgPrefLibrary(
                 mixxx::DesktopHelper::openUrl(url);
             });
 
-    connect(checkBox_SyncTrackMetadata,
+    connect(checkBox_sync_track_metadata,
             &QCheckBox::toggled,
             this,
             &DlgPrefLibrary::slotSyncTrackMetadataToggled);
@@ -205,21 +205,22 @@ void DlgPrefLibrary::slotResetToDefaults() {
     spinbox_history_track_duplicate_distance->setValue(
             kHistoryTrackDuplicateDistanceDefault);
     spinbox_history_min_tracks_to_keep->setValue(1);
-    checkBox_SyncTrackMetadata->setChecked(false);
-    checkBox_SeratoMetadataExport->setChecked(false);
+    checkBox_sync_track_metadata->setChecked(false);
+    checkBox_serato_metadata_export->setChecked(false);
     checkBox_use_relative_path->setChecked(false);
-    checkBoxEditMetadataSelectedClicked->setChecked(kEditMetadataSelectedClickDefault);
+    checkBox_edit_metadata_selected_clicked->setChecked(kEditMetadataSelectedClickDefault);
     radioButton_dbclick_deck->setChecked(true);
     spinbox_bpm_precision->setValue(BaseTrackTableModel::kBpmColumnPrecisionDefault);
 
     radioButton_cover_art_fetcher_medium->setChecked(true);
 
-    spinBoxRowHeight->setValue(Library::kDefaultRowHeightPx);
+    spinBox_row_height->setValue(Library::kDefaultRowHeightPx);
     setLibraryFont(QApplication::font());
-    searchDebouncingTimeoutSpinBox->setValue(
+    spinBox_search_debouncing_timeout->setValue(
             WSearchLineEdit::kDefaultDebouncingTimeoutMillis);
-    checkBoxEnableSearchCompletions->setChecked(WSearchLineEdit::kCompletionsEnabledDefault);
-    checkBoxEnableSearchHistoryShortcuts->setChecked(
+    checkBox_enable_search_history_shortcuts->setChecked(
+            WSearchLineEdit::kCompletionsEnabledDefault);
+    checkBox_enable_search_history_shortcuts->setChecked(
             WSearchLineEdit::kHistoryShortcutsEnabledDefault);
 
     checkBox_show_rhythmbox->setChecked(true);
@@ -241,11 +242,11 @@ void DlgPrefLibrary::slotUpdate() {
             kHistoryMinTracksToKeepConfigKey,
             kHistoryMinTracksToKeepDefault));
 
-    checkBox_SyncTrackMetadata->setChecked(
+    checkBox_sync_track_metadata->setChecked(
             m_pConfig->getValue(kSyncTrackMetadataConfigKey, false));
-    checkBox_SeratoMetadataExport->setChecked(
+    checkBox_serato_metadata_export->setChecked(
             m_pConfig->getValue(kSyncSeratoMetadataConfigKey, false));
-    setSeratoMetadataEnabled(checkBox_SyncTrackMetadata->isChecked());
+    setSeratoMetadataEnabled(checkBox_sync_track_metadata->isChecked());
     checkBox_use_relative_path->setChecked(m_pConfig->getValue(
             kUseRelativePathOnExportConfigKey, false));
 
@@ -299,26 +300,26 @@ void DlgPrefLibrary::slotUpdate() {
     bool editMetadataSelectedClick = m_pConfig->getValue(
             kEditMetadataSelectedClickConfigKey,
             kEditMetadataSelectedClickDefault);
-    checkBoxEditMetadataSelectedClicked->setChecked(editMetadataSelectedClick);
+    checkBox_edit_metadata_selected_clicked->setChecked(editMetadataSelectedClick);
     m_pLibrary->setEditMetadataSelectedClick(editMetadataSelectedClick);
 
-    checkBoxEnableSearchCompletions->setChecked(m_pConfig->getValue(
+    checkBox_enable_search_history_shortcuts->setChecked(m_pConfig->getValue(
             kEnableSearchCompletionsConfigKey,
             WSearchLineEdit::kCompletionsEnabledDefault));
-    checkBoxEnableSearchHistoryShortcuts->setChecked(m_pConfig->getValue(
+    checkBox_enable_search_history_shortcuts->setChecked(m_pConfig->getValue(
             kEnableSearchHistoryShortcutsConfigKey,
             WSearchLineEdit::kHistoryShortcutsEnabledDefault));
 
     m_originalTrackTableFont = m_pLibrary->getTrackTableFont();
     m_iOriginalTrackTableRowHeight = m_pLibrary->getTrackTableRowHeight();
-    spinBoxRowHeight->setValue(m_iOriginalTrackTableRowHeight);
+    spinBox_row_height->setValue(m_iOriginalTrackTableRowHeight);
     setLibraryFont(m_originalTrackTableFont);
 
     const auto searchDebouncingTimeoutMillis =
             m_pConfig->getValue(
                     kSearchDebouncingTimeoutMillisConfigKey,
                     WSearchLineEdit::kDefaultDebouncingTimeoutMillis);
-    searchDebouncingTimeoutSpinBox->setValue(searchDebouncingTimeoutMillis);
+    spinBox_search_debouncing_timeout->setValue(searchDebouncingTimeoutMillis);
 
     const auto bpmColumnPrecision =
             m_pConfig->getValue(
@@ -438,7 +439,7 @@ void DlgPrefLibrary::slotSeratoMetadataExportClicked(bool checked) {
                             "recommended. Are you sure you want to enable this "
                             "option?"),
                     QMessageBox::Yes | QMessageBox::No) == QMessageBox::No) {
-            checkBox_SeratoMetadataExport->setChecked(false);
+            checkBox_serato_metadata_export->setChecked(false);
         }
     }
 }
@@ -454,18 +455,18 @@ void DlgPrefLibrary::slotApply() {
 
     m_pConfig->set(
             kSyncTrackMetadataConfigKey,
-            ConfigValue{checkBox_SyncTrackMetadata->isChecked()});
+            ConfigValue{checkBox_sync_track_metadata->isChecked()});
     m_pConfig->set(
             kSyncSeratoMetadataConfigKey,
-            ConfigValue{checkBox_SeratoMetadataExport->isChecked()});
+            ConfigValue{checkBox_serato_metadata_export->isChecked()});
 
     m_pConfig->set(kUseRelativePathOnExportConfigKey,
             ConfigValue((int)checkBox_use_relative_path->isChecked()));
 
     m_pConfig->set(kEnableSearchCompletionsConfigKey,
-            ConfigValue(checkBoxEnableSearchCompletions->isChecked()));
+            ConfigValue(checkBox_enable_search_history_shortcuts->isChecked()));
     m_pConfig->set(kEnableSearchHistoryShortcutsConfigKey,
-            ConfigValue(checkBoxEnableSearchHistoryShortcuts->isChecked()));
+            ConfigValue(checkBox_enable_search_history_shortcuts->isChecked()));
     updateSearchLineEditHistoryOptions();
 
     m_pConfig->set(ConfigKey("[Library]","ShowRhythmboxLibrary"),
@@ -507,9 +508,9 @@ void DlgPrefLibrary::slotApply() {
             ConfigValue(dbclick_status));
 
     m_pConfig->set(kEditMetadataSelectedClickConfigKey,
-            ConfigValue(checkBoxEditMetadataSelectedClicked->checkState()));
+            ConfigValue(checkBox_edit_metadata_selected_clicked->checkState()));
     m_pLibrary->setEditMetadataSelectedClick(
-            checkBoxEditMetadataSelectedClicked->checkState());
+            checkBox_edit_metadata_selected_clicked->checkState());
 
     QFont font = m_pLibrary->getTrackTableFont();
     if (m_originalTrackTableFont != font) {
@@ -517,7 +518,7 @@ void DlgPrefLibrary::slotApply() {
                        ConfigValue(font.toString()));
     }
 
-    int rowHeight = spinBoxRowHeight->value();
+    int rowHeight = spinBox_row_height->value();
     if (m_iOriginalTrackTableRowHeight != rowHeight) {
         m_pConfig->set(ConfigKey("[Library]","RowHeight"),
                        ConfigValue(rowHeight));
@@ -532,17 +533,20 @@ void DlgPrefLibrary::slotRowHeightValueChanged(int height) {
 }
 
 void DlgPrefLibrary::setLibraryFont(const QFont& font) {
-    libraryFont->setText(QString("%1 %2 %3pt").arg(
-        font.family(), font.styleName(), QString::number(font.pointSizeF())));
+    lineEdit_library_font->setText(
+            QString("%1 %2 %3pt")
+                    .arg(font.family(),
+                            font.styleName(),
+                            QString::number(font.pointSizeF())));
     m_pLibrary->setFont(font);
 
     // Don't let the font height exceed the row height.
     QFontMetrics metrics(font);
     int fontHeight = metrics.height();
-    spinBoxRowHeight->setMinimum(fontHeight);
+    spinBox_row_height->setMinimum(fontHeight);
     // library.cpp takes care of setting the new row height according to the
     // previous font height/ row height ratio
-    spinBoxRowHeight->setValue(m_pLibrary->getTrackTableRowHeight());
+    spinBox_row_height->setValue(m_pLibrary->getTrackTableRowHeight());
 }
 
 void DlgPrefLibrary::slotSelectFont() {
@@ -579,7 +583,7 @@ void DlgPrefLibrary::slotBpmColumnPrecisionChanged(int bpmPrecision) {
 }
 
 void DlgPrefLibrary::slotSyncTrackMetadataToggled() {
-    bool shouldSyncTrackMetadata = checkBox_SyncTrackMetadata->isChecked();
+    bool shouldSyncTrackMetadata = checkBox_sync_track_metadata->isChecked();
     if (isVisible() && shouldSyncTrackMetadata) {
         mixxx::DlgTrackMetadataExport::showMessageBoxOncePerSession();
     }
@@ -587,8 +591,8 @@ void DlgPrefLibrary::slotSyncTrackMetadataToggled() {
 }
 
 void DlgPrefLibrary::setSeratoMetadataEnabled(bool shouldSyncTrackMetadata) {
-    checkBox_SeratoMetadataExport->setEnabled(shouldSyncTrackMetadata);
+    checkBox_serato_metadata_export->setEnabled(shouldSyncTrackMetadata);
     if (!shouldSyncTrackMetadata) {
-        checkBox_SeratoMetadataExport->setChecked(false);
+        checkBox_serato_metadata_export->setChecked(false);
     }
 }

src/preferences/dialog/dlgpreflibrarydlg.ui
@@ -40,7 +40,7 @@
       </item>
 
       <item row="0" column="1">
-       <widget class="QPushButton" name="PushButtonAddDir">
+       <widget class="QPushButton" name="pushButton_add_dir">
         <property name="font">
          <font/>
         </property>
@@ -54,7 +54,7 @@
       </item>
 
       <item row="1" column="1">
-       <widget class="QPushButton" name="PushButtonRelocateDir">
+       <widget class="QPushButton" name="pushButton_relocate_dir">
         <property name="toolTip">
          <string>If an existing music directory is moved, Mixxx doesn't know where to find the audio files in it. Choose Relink to select the music directory in its new location. &lt;br/&gt; This will re-establish the links to the audio files in the Mixxx library.</string>
         </property>
@@ -65,7 +65,7 @@
       </item>
 
       <item row="2" column="1">
-       <widget class="QPushButton" name="PushButtonRemoveDir">
+       <widget class="QPushButton" name="pushButton_remove_dir">
         <property name="font">
          <font/>
         </property>
@@ -119,11 +119,11 @@
    <item>
     <widget class="QGroupBox" name="groupBox_AudioFileTags">
      <property name="title">
-      <string>Track Metadata Synchronization</string>
+      <string>Track Metadata Synchronization / Playlists</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_metadata">
       <item row="0" column="0" colspan="2">
-       <widget class="QCheckBox" name="checkBox_SyncTrackMetadata">
+       <widget class="QCheckBox" name="checkBox_sync_track_metadata">
         <property name="text">
          <string>Synchronize library track metadata from/to file tags</string>
         </property>
@@ -132,13 +132,22 @@
         </property>
        </widget>
       </item>
+
       <item row="1" column="0">
-       <widget class="QCheckBox" name="checkBox_SeratoMetadataExport">
+       <widget class="QCheckBox" name="checkBox_serato_metadata_export">
         <property name="text">
          <string>Synchronize Serato track metadata from/to file tags (experimental)</string>
         </property>
         <property name="toolTip">
          <string>Keeps track color, beat grid, bpm lock, cue points, and loops synchronized with SERATO_MARKERS/MARKERS2 file tags.&lt;br/&gt;&lt;br/&gt;WARNING: Enabling this option also enables the reimport of Serato metadata after files have been modified outside of Mixxx. On reimport existing metadata in Mixxx is replaced with the metadata found in file tags. Custom metadata not included in file tags like loop colors is lost.</string>
+        </property>
+       </widget>
+      </item>
+
+      <item row="2" column="0" colspan="2">
+       <widget class="QCheckBox" name="checkBox_use_relative_path">
+        <property name="text">
+         <string>Use relative paths for playlist export if possible</string>
         </property>
        </widget>
       </item>
@@ -154,7 +163,7 @@
      <layout class="QGridLayout" name="gridLayout_track_table_view">
 
       <item row="1" column="0" colspan="3">
-       <widget class="QCheckBox" name="checkBoxEditMetadataSelectedClicked">
+       <widget class="QCheckBox" name="checkBox_edit_metadata_selected_clicked">
         <property name="text">
          <string>Edit metadata after clicking selected track</string>
         </property>
@@ -162,7 +171,7 @@
       </item>
 
       <item row="2" column="0" colspan="3">
-       <widget class="QLabel" name="doubeClickActionLabel">
+       <widget class="QLabel" name="label_doubeClickAction">
         <property name="text">
          <string>Track Double-Click Action:</string>
         </property>
@@ -205,6 +214,58 @@
       </item>
 
       <item row="7" column="0">
+       <widget class="QLabel" name="label_rowHeight">
+        <property name="text">
+         <string>Library Row Height:</string>
+        </property>
+        <property name="alignment">
+         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
+        </property>
+       </widget>
+      </item>
+      <item row="7" column="1" colspan="2">
+       <widget class="QSpinBox" name="spinBox_row_height">
+        <property name="suffix">
+         <string> px</string>
+        </property>
+        <property name="minimum">
+         <number>5</number>
+        </property>
+        <property name="maximum">
+         <number>100</number>
+        </property>
+        <property name="value">
+         <number>20</number>
+        </property>
+       </widget>
+      </item>
+
+      <item row="8" column="0">
+       <widget class="QLabel" name="label_libraryFont">
+        <property name="text">
+         <string>Library Font:</string>
+        </property>
+        <property name="alignment">
+         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
+        </property>
+       </widget>
+      </item>
+      <item row="8" column="1">
+       <widget class="QLineEdit" name="lineEdit_library_font">
+        <property name="readOnly">
+         <bool>true</bool>
+        </property>
+       </widget>
+      </item>
+      <item row="8" column="2">
+       <widget class="QToolButton" name="btn_library_font">
+        <property name="text">
+         <string>...</string>
+        </property>
+       </widget>
+      </item>
+
+      <item row="9" column="0">
        <widget class="QLabel" name="label_bpm_precision">
         <property name="text">
          <string>BPM display precision:</string>
@@ -214,7 +275,7 @@
         </property>
        </widget>
       </item>
-      <item row="7" column="1" colspan="2">
+      <item row="9" column="1" colspan="2">
        <widget class="QSpinBox" name="spinbox_bpm_precision">
        </widget>
       </item>
@@ -222,6 +283,59 @@
      </layout>
     </widget>
    </item><!-- Track Table View -->
+
+   <item>
+    <widget class="QGroupBox" name="groupBox_Search">
+     <property name="title">
+      <string>Track Search</string>
+     </property>
+     <layout class="QGridLayout" name="gridLayout_search">
+
+      <item row="0" column="0">
+       <widget class="QLabel" name="label_searchDebouncingTimeout">
+        <property name="text">
+         <string>Search-as-you-type timeout:</string>
+        </property>
+        <property name="alignment">
+         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
+        </property>
+       </widget>
+      </item>
+      <item row="0" column="1" colspan="2">
+       <widget class="QSpinBox" name="spinBox_search_debouncing_timeout">
+        <property name="suffix">
+         <string> ms</string>
+        </property>
+       </widget>
+      </item>
+
+      <item row="1" column="0" colspan="3">
+       <widget class="QCheckBox" name="checkBox_enable_search_completions">
+        <property name="text">
+         <string>Enable search completions</string>
+        </property>
+       </widget>
+      </item>
+
+      <item row="2" column="0" colspan="3">
+       <widget class="QCheckBox" name="checkBox_enable_search_history_shortcuts">
+        <property name="text">
+         <string>Enable search history keyboard shortcuts</string>
+        </property>
+       </widget>
+      </item>
+
+      <item row="3" column="0" colspan="3">
+       <widget class="QCheckBox" name="checkBox_BpmHalfDoubleSearch">
+        <property name="text">
+         <string>Search also for half and double BPM with 'bpm:123'</string>
+        </property>
+       </widget>
+      </item>
+
+     </layout>
+    </widget>
+   </item><!-- Search box -->
 
    <item>
     <widget class="QGroupBox" name="groupBox_History">
@@ -290,110 +404,6 @@
      </layout>
     </widget>
    </item><!-- History -->
-
-   <item>
-    <widget class="QGroupBox" name="groupBox_Miscellaneous">
-     <property name="title">
-      <string>Miscellaneous</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_misc">
-
-      <item row="0" column="0" colspan="2">
-       <widget class="QCheckBox" name="checkBox_use_relative_path">
-        <property name="text">
-         <string>Use relative paths for playlist export if possible</string>
-        </property>
-       </widget>
-      </item>
-
-      <item row="1" column="0">
-       <widget class="QLabel" name="rowHeightLabel">
-        <property name="text">
-         <string>Library Row Height:</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="1" colspan="2">
-       <widget class="QSpinBox" name="spinBoxRowHeight">
-        <property name="suffix">
-         <string> px</string>
-        </property>
-        <property name="minimum">
-         <number>5</number>
-        </property>
-        <property name="maximum">
-         <number>100</number>
-        </property>
-        <property name="value">
-         <number>20</number>
-        </property>
-       </widget>
-      </item>
-
-      <item row="2" column="0">
-       <widget class="QLabel" name="libraryFontLabel">
-        <property name="text">
-         <string>Library Font:</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <widget class="QLineEdit" name="libraryFont">
-        <property name="readOnly">
-         <bool>true</bool>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="2">
-       <widget class="QToolButton" name="libraryFontButton">
-        <property name="text">
-         <string>...</string>
-        </property>
-       </widget>
-      </item>
-
-      <item row="3" column="0">
-       <widget class="QLabel" name="searchDebouncingTimeoutLabel">
-        <property name="text">
-         <string>Search-as-you-type timeout:</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="1" colspan="2">
-       <widget class="QSpinBox" name="searchDebouncingTimeoutSpinBox">
-        <property name="suffix">
-         <string> ms</string>
-        </property>
-       </widget>
-      </item>
-
-      <item row="4" column="0" colspan="3">
-       <widget class="QCheckBox" name="checkBoxEnableSearchCompletions">
-        <property name="text">
-         <string>Enable search completions</string>
-        </property>
-       </widget>
-      </item>
-      <item row="5" column="0" colspan="3">
-       <widget class="QCheckBox" name="checkBoxEnableSearchHistoryShortcuts">
-        <property name="text">
-         <string>Enable search history keyboard shortcuts</string>
-        </property>
-       </widget>
-      </item>
-
-     </layout>
-    </widget>
-   </item>
 
    <item>
     <widget class="QGroupBox" name="groupBox_external_libraries">
@@ -578,6 +588,9 @@
         <property name="wordWrap">
          <bool>true</bool>
         </property>
+        <property name="openExternalLinks">
+         <bool>true</bool>
+        </property>
        </widget>
       </item>
       <item>
@@ -591,7 +604,7 @@
        </widget>
       </item>
       <item>
-       <widget class="QPushButton" name="PushButtonOpenSettingsDir">
+       <widget class="QPushButton" name="pushButton_open_settings_dir">
         <property name="sizePolicy">
          <sizepolicy hsizetype="Fixed" vsizetype="Fixed">
           <horstretch>0</horstretch>
@@ -624,13 +637,13 @@
  <layoutdefault spacing="6" margin="11"/>
  <tabstops>
   <tabstop>dirList</tabstop>
-  <tabstop>PushButtonAddDir</tabstop>
-  <tabstop>PushButtonRelocateDir</tabstop>
-  <tabstop>PushButtonRemoveDir</tabstop>
+  <tabstop>pushButton_add_dir</tabstop>
+  <tabstop>pushButton_relocate_dir</tabstop>
+  <tabstop>pushButton_remove_dir</tabstop>
   <tabstop>checkBox_library_scan</tabstop>
-  <tabstop>checkBox_SyncTrackMetadata</tabstop>
-  <tabstop>checkBox_SeratoMetadataExport</tabstop>
-  <tabstop>checkBoxEditMetadataSelectedClicked</tabstop>
+  <tabstop>checkBox_sync_track_metadata</tabstop>
+  <tabstop>checkBox_serato_metadata_export</tabstop>
+  <tabstop>checkBox_edit_metadata_selected_clicked</tabstop>
   <tabstop>radioButton_dbclick_deck</tabstop>
   <tabstop>radioButton_dbclick_bottom</tabstop>
   <tabstop>radioButton_dbclick_top</tabstop>
@@ -639,12 +652,11 @@
   <tabstop>spinbox_history_track_duplicate_distance</tabstop>
   <tabstop>spinbox_history_min_tracks_to_keep</tabstop>
   <tabstop>checkBox_use_relative_path</tabstop>
-  <tabstop>spinBoxRowHeight</tabstop>
-  <tabstop>libraryFont</tabstop>
-  <tabstop>libraryFontButton</tabstop>
-  <tabstop>searchDebouncingTimeoutSpinBox</tabstop>
-  <tabstop>checkBoxEnableSearchCompletions</tabstop>
-  <tabstop>checkBoxEnableSearchHistoryShortcuts</tabstop>
+  <tabstop>spinBox_row_height</tabstop>
+  <tabstop>btn_library_font</tabstop>
+  <tabstop>spinBox_search_debouncing_timeout</tabstop>
+  <tabstop>checkBox_enable_search_completions</tabstop>
+  <tabstop>checkBox_enable_search_history_shortcuts</tabstop>
   <tabstop>checkBox_show_rhythmbox</tabstop>
   <tabstop>checkBox_show_banshee</tabstop>
   <tabstop>checkBox_show_itunes</tabstop>
@@ -655,7 +667,7 @@
   <tabstop>radioButton_cover_art_fetcher_high</tabstop>
   <tabstop>radioButton_cover_art_fetcher_medium</tabstop>
   <tabstop>radioButton_cover_art_fetcher_lowest</tabstop>
-  <tabstop>PushButtonOpenSettingsDir</tabstop>
+  <tabstop>pushButton_open_settings_dir</tabstop>
  </tabstops>
  <resources/>
  <connections/>

commit 0fbab4594702442029807d92ac786b8459190334

Preferences: Fix mixup of checkBox_enable_search_completions vs. checkBox_enable_search_history_shortcuts
Fixes the copy-paste error introduced during a refactoring in commit
6e588ec8201d86f7ecd4702fb5baa47b78fdfb5e (merged with PR #12072).

src/preferences/dialog/dlgpreflibrary.cpp
@@ -254,7 +254,7 @@ void DlgPrefLibrary::slotResetToDefaults() {
     setLibraryFont(QApplication::font());
     spinBox_search_debouncing_timeout->setValue(
             WSearchLineEdit::kDefaultDebouncingTimeoutMillis);
-    checkBox_enable_search_history_shortcuts->setChecked(
+    checkBox_enable_search_completions->setChecked(
             WSearchLineEdit::kCompletionsEnabledDefault);
     checkBox_enable_search_history_shortcuts->setChecked(
             WSearchLineEdit::kHistoryShortcutsEnabledDefault);
@@ -341,7 +341,7 @@ void DlgPrefLibrary::slotUpdate() {
     checkBox_edit_metadata_selected_clicked->setChecked(editMetadataSelectedClick);
     m_pLibrary->setEditMetadataSelectedClick(editMetadataSelectedClick);
 
-    checkBox_enable_search_history_shortcuts->setChecked(m_pConfig->getValue(
+    checkBox_enable_search_completions->setChecked(m_pConfig->getValue(
             kEnableSearchCompletionsConfigKey,
             WSearchLineEdit::kCompletionsEnabledDefault));
     checkBox_enable_search_history_shortcuts->setChecked(m_pConfig->getValue(
@@ -520,7 +520,7 @@ void DlgPrefLibrary::slotApply() {
             ConfigValue((int)checkBox_use_relative_path->isChecked()));
 
     m_pConfig->set(kEnableSearchCompletionsConfigKey,
-            ConfigValue(checkBox_enable_search_history_shortcuts->isChecked()));
+            ConfigValue(checkBox_enable_search_completions->isChecked()));
     m_pConfig->set(kEnableSearchHistoryShortcutsConfigKey,
             ConfigValue(checkBox_enable_search_history_shortcuts->isChecked()));
     updateSearchLineEditHistoryOptions();