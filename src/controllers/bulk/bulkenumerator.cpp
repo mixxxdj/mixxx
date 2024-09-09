776d90deaa8ac5d0a28833fdd1c6f704c509df48
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2024-09-09 15:50:45 +0200

    fixup! refactor: improve memory-safety of BulkEnumerator

--- src/controllers/bulk/bulkenumerator.cpp
+++ src/controllers/bulk/bulkenumerator.cpp
@@ -17,9 +17,9 @@ BulkEnumerator::BulkEnumerator(UserSettingsPointer pConfig)
           m_context(nullptr, &libusb_exit),
           m_pConfig(pConfig) {
     // TODO: use C++23 std::out_ptr instead
-    libusb_context* ctx;
-    libusb_init(&ctx);
-    m_context.reset(ctx);
+    libusb_context* pCtx;
+    libusb_init(&pCtx);
+    m_context.reset(pCtx);
 }
 
 BulkEnumerator::~BulkEnumerator() {
@@ -38,28 +38,28 @@ static bool is_interesting(const libusb_device_descriptor& desc) {
 
 QList<Controller*> BulkEnumerator::queryDevices() {
     qDebug() << "Scanning USB Bulk devices:";
-    libusb_device** listPtr; // contiguous array of pointers to libusb_device
-    ssize_t cnt = libusb_get_device_list(m_context.get(), &listPtr);
+    libusb_device** deviceListPointers; // contiguous array of pointers to libusb_device
+    ssize_t cnt = libusb_get_device_list(m_context.get(), &deviceListPointers);
     VERIFY_OR_DEBUG_ASSERT(cnt >= 0) {
         // this assumes that if libusb_get_device_list returned an error, no memory was allocated
         // and listPtr is still uninitialized. Unfortunately the libusb docs don't specify the state
         // of the arguments in the case of errors.
         qWarning() << "Error retrieving devicelist: " << libusb_error_name(cnt);
     }
-    auto list = std::unique_ptr<libusb_device*[],
+    auto pDeviceList = std::unique_ptr<libusb_device*[],
             decltype([](libusb_device** list) {
                 libusb_free_device_list(list, true);
-            })>(listPtr);
+            })>(deviceListPointers);
 
-    for (libusb_device* device : std::span(list.get(), cnt)) {
+    for (libusb_device* pDevice : std::span(pDeviceList.get(), cnt)) {
         struct libusb_device_descriptor desc;
 
-        libusb_get_device_descriptor(device, &desc);
+        libusb_get_device_descriptor(pDevice, &desc);
         if (!is_interesting(desc)) {
             continue;
         }
         struct libusb_device_handle* handle = nullptr;
-        int err = libusb_open(device, &handle);
+        int err = libusb_open(pDevice, &handle);
         if (err) {
             qWarning() << "Error opening a device:" << libusb_error_name(err);
             continue;
