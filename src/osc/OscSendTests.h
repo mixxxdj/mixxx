8d12ce0e45cf813ca24f39999197b7b9d02f3c62
Evelynne <Evelynne@Evelynne-Veys.com>
2024-09-16 12:23:19 +0200

    eve osc ok

---
+++ src/osc/OscSendTests.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDED_OSCSENDTESTS_H
+#define INCLUDED_OSCSENDTESTS_H
+
+namespace osc{
+
+void RunSendTests( unsigned long address, int port );
+
+} // namespace osc
+
+#endif /* INCLUDED_OSCSENDTESTS_H */
