0e4dd46b05734aa8fbb99b38eb98a43ca879e177
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2024-09-22 16:07:29 +0200

    chore: mark `DISALLOW_COPY_AND_ASSIGN` macro as deprecated

--- src/util/class.h
+++ src/util/class.h
@@ -1,7 +1,9 @@
 #pragma once
 
-// A macro to disallow the copy constructor and operator= functions
-// This should be used in the private: declarations for a class
+// DEPRECATED: Instead rely on "the rule of zero". See
+// https://en.cppreference.com/w/cpp/language/rule_of_three#Rule_of_zero
+// See also: Cpp Core Guidelines C.20, C.21 & C.22 as well as the clang-tidy rule
+// `modernize-use-equals-delete`
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
     TypeName(const TypeName&) = delete;    \
     void operator=(const TypeName&) = delete;
