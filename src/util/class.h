77798f102ed94e6a423580e9fcde4d3393d608fa
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2024-10-09 11:57:19 +0200

    fixup! chore: mark `DISALLOW_COPY_AND_ASSIGN` macro as deprecated

--- src/util/class.h
+++ src/util/class.h
@@ -1,9 +1,11 @@
 #pragma once
 
-// DEPRECATED: Instead rely on "the rule of zero". See
-// https://en.cppreference.com/w/cpp/language/rule_of_three#Rule_of_zero
-// See also: Cpp Core Guidelines C.20, C.21 & C.22 as well as the clang-tidy rule
-// `modernize-use-equals-delete`
+// DEPRECATED: Adhere to the rule "the rule of zero" or "the rule of five".
+// See https://en.cppreference.com/w/cpp/language/rule_of_three
+// Rationale: If a class deals with ownership, it should do nothing more and explicitly
+// define the behavior of all special member functions.
+// This macro is sprinkled into lots of big classes and ignores the move-related SMF's.
+// So its common use violates both principles and thus it should not be used anymore!
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
     TypeName(const TypeName&) = delete;    \
     void operator=(const TypeName&) = delete;
