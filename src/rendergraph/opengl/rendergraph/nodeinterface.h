2de8dedf3119546c733d71a0451f690a4b8093eb
m0dB <m0dB@mixxx.org>
2024-11-01 20:44:05 +0100

    missing file

---
+++ src/rendergraph/opengl/rendergraph/nodeinterface.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "backend/basenode.h"
+#include "rendergraph/assert.h"
+
+namespace rendergraph {
+
+template<class T_Node>
+class NodeInterface : public T_Node {
+  public:
+    void appendChildNode(std::unique_ptr<BaseNode> pNode) {
+        BaseNode* pRawNode = pNode.release();
+        T_Node::appendChildNode(pRawNode);
+    }
+    std::unique_ptr<BaseNode> detachChildNode(BaseNode* pNode) {
+        T_Node::removeChildNode(pNode);
+        return std::unique_ptr<BaseNode>(pNode);
+    }
+};
+
+} // namespace rendergraph
