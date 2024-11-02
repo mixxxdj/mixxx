d718db128bf6ac3f7c32b9a71648c3484d4472b4
Your Name <smanishsehrawat@gmail.com>
2024-11-02 17:07:36 +0530

    feat: add playposition_samples for sample-based position tracking

--- src/engine/enginebuffer.cpp
+++ src/engine/enginebuffer.cpp
@@ -167,6 +167,7 @@ EngineBuffer::EngineBuffer(const QString& group,
 
     m_pRepeat = new ControlPushButton(ConfigKey(m_group, "repeat"));
     m_pRepeat->setButtonMode(mixxx::control::ButtonMode::Toggle);
+    m_playposition_samples = new ControlObject<double>(ConfigKey(m_group, "playposition_samples"));
 
     m_pSampleRate = new ControlProxy(kAppGroup, QStringLiteral("samplerate"), this);
 
@@ -294,6 +295,7 @@ EngineBuffer::~EngineBuffer() {
 #endif
     delete m_pReadAheadManager;
     delete m_pReader;
+    delete m_playposition_samples;
 
     delete m_playButton;
     delete m_playStartButton;
@@ -464,6 +466,7 @@ void EngineBuffer::setNewPlaypos(mixxx::audio::FramePos position) {
     if (kLogger.traceEnabled()) {
         kLogger.trace() << m_group << "EngineBuffer::setNewPlaypos" << position;
     }
+    m_playposition_samples->set(m_playPos.toEngineSamplePos());
 
     m_playPos = position;
 
@@ -712,12 +715,16 @@ void EngineBuffer::slotControlSeek(double fractionalPos) {
 void EngineBuffer::seekAbs(mixxx::audio::FramePos position) {
     DEBUG_ASSERT(position.isValid());
     doSeekPlayPos(position, SEEK_STANDARD);
+    m_playposition_samples->set(position.toEngineSamplePos());
+
 }
 
 // WARNING: This method is called by EngineControl and runs in the engine thread
 void EngineBuffer::seekExact(mixxx::audio::FramePos position) {
     DEBUG_ASSERT(position.isValid());
     doSeekPlayPos(position, SEEK_EXACT);
+    m_playposition_samples->set(position.toEngineSamplePos());
+
 }
 
 double EngineBuffer::fractionalPlayposFromAbsolute(mixxx::audio::FramePos absolutePlaypos) {
@@ -1457,6 +1464,7 @@ void EngineBuffer::updateIndicators(double speed, int iBufferSize) {
         // called yet.
         return;
     }
+    m_playposition_samples->set(m_playPos.toEngineSamplePos());
 
     // Increase samplesCalculated by the buffer size
     m_iSamplesSinceLastIndicatorUpdate += iBufferSize;
