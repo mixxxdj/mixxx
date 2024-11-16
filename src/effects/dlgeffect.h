7d76beb6b9fe43b657becda879e982a5746016c4
fwcd <fwcd@fwcd.dev>
2024-11-16 04:14:33 +0100

    DlgEffect: Add support for embedding custom UI

--- src/effects/dlgeffect.h
+++ src/effects/dlgeffect.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <QDialog>
+#include <QWidget>
 
 class DlgEffect : public QDialog {
     Q_OBJECT
 
   public:
-    DlgEffect();
+    DlgEffect(QWidget* customUI = nullptr);
+
+    void setCustomUI(QWidget* customUI);
+
+  private:
+    QWidget* m_customUI;
 };
