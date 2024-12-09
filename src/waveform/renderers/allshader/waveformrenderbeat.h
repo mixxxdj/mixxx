f6a7fe420db120eb3476cae448851a2d65fabe1f
m0dB <m0dB@mixxx.org>
2024-12-09 17:57:01 +0100

    use geometrynodes for waveformrender beat

--- src/waveform/renderers/allshader/waveformrenderbeat.h
+++ src/waveform/renderers/allshader/waveformrenderbeat.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <QColor>
+#include <memory>
 
-#include "rendergraph/openglnode.h"
-#include "shaders/unicolorshader.h"
+#include "rendergraph/geometrynode.h"
 #include "util/class.h"
-#include "waveform/renderers/allshader/vertexdata.h"
-#include "waveform/renderers/allshader/waveformrenderer.h"
+#include "waveform/renderers/waveformrendererabstract.h"
 
 class QDomNode;
 class SkinContext;
@@ -16,23 +15,26 @@ class WaveformRenderBeat;
 }
 
 class allshader::WaveformRenderBeat final
-        : public allshader::WaveformRenderer,
-          public rendergraph::OpenGLNode {
+        : public ::WaveformRendererAbstract,
+          public rendergraph::GeometryNode {
   public:
     explicit WaveformRenderBeat(WaveformWidgetRenderer* waveformWidget,
             ::WaveformRendererAbstract::PositionSource type =
                     ::WaveformRendererAbstract::Play);
 
+    // Pure virtual from WaveformRendererAbstract, not used
+    void draw(QPainter* painter, QPaintEvent* event) override final;
+
     void setup(const QDomNode& node, const SkinContext& skinContext) override;
-    void paintGL() override;
-    void initializeGL() override;
+
+    // Virtuals for rendergraph::Node
+    void preprocess() override;
 
   private:
-    mixxx::UnicolorShader m_shader;
     QColor m_color;
-    VertexData m_vertices;
-
     bool m_isSlipRenderer;
+
+    bool preprocessInner();
 
     DISALLOW_COPY_AND_ASSIGN(WaveformRenderBeat);
 };
