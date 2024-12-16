7de07fc58374359c79c6642aeb3fad6ad4b1374c
Antoine C <mixxx@acolombier.dev>
2024-12-16 19:48:01 +0000

    design: add Christmas splash screen

--- src/skin/legacy/launchimage.cpp
+++ src/skin/legacy/launchimage.cpp
@@ -11,7 +11,7 @@
 #include "moc_launchimage.cpp"
 
 namespace {
-bool isIn2024ChristmasHollidays() {
+bool isIn2024ChristmasHolidays() {
     auto currentDate = QDate::currentDate();
     return currentDate >= QDate(2024, 12, 24) &&
             currentDate <= QDate(2025, 1, 6);
@@ -20,7 +20,7 @@ bool isIn2024ChristmasHollidays() {
 
 LaunchImage::LaunchImage(QWidget* pParent, const QString& styleSheet)
         : QWidget(pParent) {
-    if (isIn2024ChristmasHollidays()) {
+    if (isIn2024ChristmasHolidays()) {
         setStyleSheet(
                 "LaunchImage { background-color: #202020; }"
                 "QLabel { "
