136b644b2497aafd7532cb1c4913fe7e5e21d89c
Daniel Schürmann <daschuer@mixxx.org>
2025-01-05 23:06:30 +0100

    Pass diconnet() calls through MidiController, because the mapping might have changed and otherwise the call would disconnect an no longer used mapping.

--- src/controllers/midi/midicontroller.h
+++ src/controllers/midi/midicontroller.h
@@ -8,17 +8,18 @@
 #include "controllers/softtakeover.h"
 
 class MidiOutputHandler;
+class MidiController;
 
 class MidiInputHandleJSProxy final : public QObject {
     Q_OBJECT
   public:
     MidiInputHandleJSProxy(
-            const std::shared_ptr<LegacyMidiControllerMapping> mapping,
+            MidiController* pMidiController,
             const MidiInputMapping& inputMapping);
     Q_INVOKABLE bool disconnect();
 
   protected:
-    std::shared_ptr<LegacyMidiControllerMapping> m_mapping;
+    MidiController* m_pMidiController;
     MidiInputMapping m_inputMapping;
 };
 
@@ -53,6 +54,7 @@ class MidiController : public Controller {
     }
 
     bool matchMapping(const MappingInfo& mapping) override;
+    bool removeInputMapping(uint16_t key, const MidiInputMapping& mapping);
 
   signals:
     void messageReceived(unsigned char status, unsigned char control, unsigned char value);
