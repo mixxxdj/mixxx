419f78af468dd790400367922785cc253c50c2f1
Daniel Schürmann <daschuer@mixxx.org>
2025-01-27 01:36:54 +0100

    create num_items outsit the loop.
    Co-authored-by: Jan Holthuis <holthuis.jan@googlemail.com>

--- src/rendergraph/common/rendergraph/uniformset.cpp
+++ src/rendergraph/common/rendergraph/uniformset.cpp
@@ -3,7 +3,8 @@
 using namespace rendergraph;
 
 UniformSet::UniformSet(std::initializer_list<Uniform> list, const std::vector<QString>& names) {
-    for (std::size_t i = 0; i < list.size() && i < names.size(); ++i) {
+    const auto num_items = std::min(list.size(), names.size());
+    for (std::size_t i = 0; i < num_items; ++i) {
         add(Uniform{list.begin()[i].m_type, names[i]});
     }
 }
