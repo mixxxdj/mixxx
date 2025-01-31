635ec064cd2e2afbc5c8528878c9fab3c6095539
audministrator <94543975+audministrator@users.noreply.github.com>
2025-01-31 14:55:02 +0100

    Update dlglibraryexport.cpp
    Added //: "Engine DJ" must not be translated

--- src/library/export/dlglibraryexport.cpp
+++ src/library/export/dlglibraryexport.cpp
@@ -124,6 +124,7 @@ DlgLibraryExport::DlgLibraryExport(
     pLayout->addLayout(pFormLayout, 0, 1, 3, 1);
     pLayout->addLayout(pButtonBarLayout, 3, 0, 1, 2);
 
+    //: "Engine DJ" must not be translated            
     setLayout(pLayout);
     setWindowTitle(tr("Export Library to Engine DJ"));
 
