c8b7128ae0576da3d5e8329876fd582c850b892c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-02-07 19:35:35 +0100

    Improve documentation for WBasicLabel
    Co-authored-by: ronso0 <ronso0@mixxx.org>

--- src/widget/wbasiclabel.h
+++ src/widget/wbasiclabel.h
@@ -2,8 +2,9 @@
 
 #include <QLabel>
 
-/// Subclass of QLabel that allows the Up and Down arrow keys
-/// to be used like the Tab/Backtab keys.
+/// Subclass of QLabel for DlgTrackInfo/~Multi and similar dialogs
+/// that allows the Up and Down arrow keys to move keyboard focus
+/// to the prev/next focusable widget.
 ///
 /// See WLabel when you need to connect to a ControlObject.
 class WBasicLabel : public QLabel {
