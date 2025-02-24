5fb0bf7ff490c5f7e40303b46e0f2e64603a1326
ronso0 <ronso0@mixxx.org>
2025-02-24 22:19:41 +0100

    make WaveformOverviewRenderer a namespace

--- src/waveform/renderers/waveformoverviewrenderer.h
+++ src/waveform/renderers/waveformoverviewrenderer.h
@@ -8,34 +8,33 @@
 class QPainter;
 class WaveformSignalColors;
 
-class WaveformOverviewRenderer {
-  public:
-    static QImage render(ConstWaveformPointer pWaveform,
-            mixxx::OverviewType type,
-            const WaveformSignalColors& signalColors,
-            bool mono = false);
-    /// These paint methods allow "mono" rendering (mono-mixdown, bottom-aligned).
-    /// Note: Don't use with WOverview, it's not adjusted yet! It does some
-    /// additional scaling for normalization which will atm cut off the bottom part.
-    static void drawWaveformPartRGB(
-            QPainter* pPainter,
-            ConstWaveformPointer pWaveform,
-            int* start,
-            int end,
-            const WaveformSignalColors& signalColors,
-            bool mono = false);
-    static void drawWaveformPartLMH(
-            QPainter* pPainter,
-            ConstWaveformPointer pWaveform,
-            int* start,
-            int end,
-            const WaveformSignalColors& signalColors,
-            bool mono = false);
-    static void drawWaveformPartHSV(
-            QPainter* pPainter,
-            ConstWaveformPointer pWaveform,
-            int* start,
-            int end,
-            const WaveformSignalColors& signalColors,
-            bool mono = false);
-};
+namespace waveformOverviewRenderer {
+QImage render(ConstWaveformPointer pWaveform,
+        mixxx::OverviewType type,
+        const WaveformSignalColors& signalColors,
+        bool mono = false);
+/// These paint methods allow "mono" rendering (mono-mixdown, bottom-aligned).
+/// Note: Don't use with WOverview, it's not adjusted yet! It does some
+/// additional scaling for normalization which will atm cut off the bottom part.
+void drawWaveformPartRGB(
+        QPainter* pPainter,
+        ConstWaveformPointer pWaveform,
+        int* start,
+        int end,
+        const WaveformSignalColors& signalColors,
+        bool mono = false);
+void drawWaveformPartLMH(
+        QPainter* pPainter,
+        ConstWaveformPointer pWaveform,
+        int* start,
+        int end,
+        const WaveformSignalColors& signalColors,
+        bool mono = false);
+void drawWaveformPartHSV(
+        QPainter* pPainter,
+        ConstWaveformPointer pWaveform,
+        int* start,
+        int end,
+        const WaveformSignalColors& signalColors,
+        bool mono = false);
+} // namespace waveformOverviewRenderer
