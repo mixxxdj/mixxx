e0538345d9229b7e921d5fa25e32c5456ff8ff47
Antoine C. <mixxx@acolombier.dev>
2025-04-06 22:13:19 +0000

    feat: add QML setting popup

---
+++ src/qml/qmlsettingparameter.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <qglobal.h>
+#include <qquickitem.h>
+#include <qsortfilterproxymodel.h>
+#include <qstandarditemmodel.h>
+
+#include <QQuickItem>
+
+namespace mixxx {
+namespace qml {
+
+class QmlSettingGroup : public QQuickItem {
+    Q_OBJECT
+    Q_PROPERTY(QString label MEMBER m_label FINAL)
+    QML_NAMED_ELEMENT(SettingGroup)
+  public:
+    explicit QmlSettingGroup(QQuickItem* parent = nullptr);
+
+    const QString& label() const {
+        return m_label;
+    }
+  signals:
+    Q_INVOKABLE void activated();
+
+  private:
+    QString m_label;
+};
+
+class QmlSettingParameter : public QmlSettingGroup {
+    Q_OBJECT
+    Q_PROPERTY(QStringList keywords MEMBER m_keywords FINAL)
+    QML_NAMED_ELEMENT(SettingParameter)
+  public:
+    explicit QmlSettingParameter(QQuickItem* parent = nullptr);
+
+    const QStringList& keywords() const {
+        return m_keywords;
+    }
+
+  private:
+    QStringList m_keywords;
+};
+
+class QmlSettingParameterManager : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QSortFilterProxyModel* model READ model CONSTANT)
+    QML_NAMED_ELEMENT(SettingParameterManager)
+  public:
+    explicit QmlSettingParameterManager(QObject* parent = nullptr);
+
+    Q_INVOKABLE void indexParameters(int categoryIdx, QQuickItem* view);
+    Q_INVOKABLE void search(const QString& criteria);
+
+    QSortFilterProxyModel* model() {
+        return &m_model;
+    }
+
+  private:
+    QStandardItemModel m_sourceModel;
+    QSortFilterProxyModel m_model;
+};
+
+} // namespace qml
+} // namespace mixxx
