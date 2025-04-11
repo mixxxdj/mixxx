0b54d56d1ced52ecad0699c4cf2e27ed64767099
Jan Koester <jan.koester@tuxist.de>
2025-04-11 06:20:17 +0200

    settings added

--- src/remote/remote.h
+++ src/remote/remote.h
@@ -9,24 +9,16 @@
 
 class Library;
 
-namespace stefanfrings {
-    class HttpListener;
-};
-
 namespace mixxx {
    class RemoteController;
    class RemoteControl{
    public:
-       RemoteControl(UserSettingsPointer pConfig,TrackCollectionManager *trackscollmngr,QObject* pParent = nullptr);
+       RemoteControl(UserSettingsPointer pConfig,
+                     std::shared_ptr<TrackCollectionManager> trackscollmngr,
+                     QObject* pParent = nullptr);
        virtual ~RemoteControl();
    private:
-        UserSettingsPointer                                   m_pSettings;
-        TrackCollectionManager                               *m_pTrackCollectionManager;
-        std::shared_ptr<QSettings>                            m_HttpSettings;
-        std::shared_ptr<QSettings>                            m_FileSettings;
-        std::shared_ptr<QSettings>                            m_SessionSettings;
-        std::shared_ptr<RemoteController>                     m_RemoteController;
-        std::shared_ptr<::stefanfrings::HttpListener>         m_HttpListener;
+        std::shared_ptr<RemoteController>     m_RemoteController;
         QObject*                                              m_Parent;
    };
 }; 
