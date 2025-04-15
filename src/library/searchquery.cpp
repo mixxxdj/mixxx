360fe3bb4d333111b3837975dc499eecb6903be4
ronso0 <ronso0@mixxx.org>
2025-04-16 00:48:18 +0200

    Library scanner: find renamed files

--- src/library/searchquery.cpp
+++ src/library/searchquery.cpp
@@ -698,7 +698,7 @@ bool BpmFilterNode::match(const TrackPointer& pTrack) const {
     }
 
     double value = pTrack->getBpm();
-
+    pTrack->getBeats()->hasConstantTempo();
     switch (m_matchMode) {
     case MatchMode::Null: {
         return value == 0.0;
