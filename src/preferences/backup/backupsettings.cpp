e14a1e6e1acb23fd3969b28afa26c6240c92aa8c
Evelynne <Evelynne@Evelynne-Veys.com>
2025-05-05 11:35:03 +0200

    BUSettingsFind7zOnMac

--- src/preferences/backup/backupsettings.cpp
+++ src/preferences/backup/backupsettings.cpp
@@ -12,6 +12,18 @@ const QString kBackUpFrequency = "BackUpFrequency";
 const QString kLastBackUp = "LastBackUp";
 
 void createSettingsBackUp(UserSettingsPointer m_pConfig) {
+    // default the BackUp is set to enabled
+    if (!m_pConfig->exists(ConfigKey(kConfigGroup, kBackUpEnabled))) {
+        m_pConfig->set(ConfigKey(kConfigGroup, kBackUpEnabled), ConfigValue((int)1));
+    }
+    // default the BckUp frequency is set to always = on every start, can be daily to
+    if (!m_pConfig->exists(ConfigKey(kConfigGroup, kBackUpFrequency))) {
+        m_pConfig->set(ConfigKey(kConfigGroup, kBackUpFrequency), ConfigValue("always"));
+    }
+    if (!m_pConfig->exists(ConfigKey(kConfigGroup, kLastBackUp))) {
+        m_pConfig->set(ConfigKey(kConfigGroup, kLastBackUp), ConfigValue(""));
+    }
+
     qDebug() << "[BackUp] -> BackUp enabled: "
              << m_pConfig->getValue<bool>(
                         ConfigKey(kConfigGroup, kBackUpEnabled));
@@ -53,16 +65,22 @@ void createSettingsBackUp(UserSettingsPointer m_pConfig) {
     QString zipFilePath = backupFolder + "/Mixxx-BackUp-" + timestamp + ".7z";
 
     QString zipExecutable;
+
 #if defined(Q_OS_WIN)
     zipExecutable = "7z.exe";
+#elif defined(Q_OS_MACOS)
+    zipExecutable = "/usr/local/bin/7z";
+    if (!QFile::exists(zipExecutable) || !QFileInfo(zipExecutable).isExecutable()) {
+        qWarning() << "[BackUp] -> 7z not found at" << zipExecutable << ". Cannot create backup.";
+        return;
+    }
 #else
-    zipExecutable = "7z";
-#endif
-
-    if (QStandardPaths::findExecutable(zipExecutable).isEmpty()) {
+    zipExecutable = QStandardPaths::findExecutable("7z");
+    if (zipExecutable.isEmpty()) {
         qWarning() << "[BackUp] -> 7z not found in PATH. Cannot create backup.";
         return;
     }
+#endif
 
     // the settings directory is added to the BackUp except the analysis folder (can be to big)
     QStringList arguments;
