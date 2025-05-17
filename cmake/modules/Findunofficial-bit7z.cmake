3338d9d7ff770e537195cf1f914bffa36da10ceb
Evelynne <Evelynne@Evelynne-Veys.com>
2025-05-17 15:55:59 +0200

    cmake-lib-test

--- cmake/modules/Findunofficial-bit7z.cmake
+++ cmake/modules/Findunofficial-bit7z.cmake
@@ -9,20 +9,39 @@
 #   find_package(unofficial-bit7z CONFIG REQUIRED)
 #   target_link_libraries(your_target PRIVATE unofficial::bit7z::bit7z64)
 
-find_path(
-  BIT7Z_INCLUDE_DIR
-  NAMES bit7z.hpp
-  PATHS
-    "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include"
-    "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/bit7z"
+# Findunofficial-bit7z.cmake
+# Locates the bit7z library installed via vcpkg
+
+set(
+  VCPKG_POSSIBLE_INCLUDE_PATHS
+  "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include"
+  "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/bit7z"
+  "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/include"
+  "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/include/bit7z"
 )
 
+# Find headers
+find_path(
+  BIT7Z_INCLUDE_DIR
+  bit7z.hpp
+  PATHS ${VCPKG_POSSIBLE_INCLUDE_PATHS}
+  PATH_SUFFIXES bit7z
+  DOC "Path to bit7z headers"
+)
+
+# Library search paths
+set(
+  VCPKG_POSSIBLE_LIBRARY_PATHS
+  "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+  "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib"
+)
+
+# Find library
 find_library(
   BIT7Z_LIBRARY
   NAMES bit7z bit7z64
-  PATHS
-    "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
-    "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib"
+  PATHS ${VCPKG_POSSIBLE_LIBRARY_PATHS}
+  DOC "Path to bit7z library"
 )
 
 include(FindPackageHandleStandardArgs)
@@ -41,4 +60,27 @@ if(unofficial-bit7z_FOUND AND NOT TARGET unofficial::bit7z::bit7z64)
   )
 endif()
 
-mark_as_advanced(unofficial-bit7z_INCLUDE_DIR unofficial-bit7z_LIBRARY)
+# Windows DLL handling (now properly attached to your main target)
+if(WIN32 AND unofficial-bit7z_FOUND)
+  find_file(
+    BIT7Z_DLL
+    NAMES 7zip.dll
+    PATHS
+      "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin"
+      "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/bin"
+    NO_DEFAULT_PATH
+  )
+
+  if(BIT7Z_DLL AND TARGET mixxx-lib)
+    add_custom_command(
+      TARGET mixxx-lib
+      POST_BUILD
+      COMMAND
+        ${CMAKE_COMMAND} -E copy_if_different "${BIT7Z_DLL}"
+        "$<TARGET_FILE_DIR:mixxx-lib>"
+      COMMENT "Copying 7zip.dll to output directory"
+    )
+  endif()
+endif()
+
+mark_as_advanced(BIT7Z_INCLUDE_DIR BIT7Z_LIBRARY BIT7Z_DLL)
