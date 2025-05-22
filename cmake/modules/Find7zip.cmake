3d03106d8ee44f6eee60342e8b31ecf6916553f2
Evelynne <Evelynne@Evelynne-Veys.com>
2025-05-22 12:00:14 +0200

    cmake-lib-test-6

--- cmake/modules/Find7zip.cmake
+++ cmake/modules/Find7zip.cmake
@@ -13,43 +13,46 @@ Result Variables
 ``7zip_FOUND``, ``7zip_INCLUDE_DIRS``, ``7zip_LIBRARIES``
 #]=======================================================================]
 
-include(IsStaticLibrary)
+find_package(PkgConfig QUIET)
+if(PkgConfig_FOUND)
+  pkg_check_modules(PC_7zip QUIET 7zip)
+endif()
 
 find_path(
   7zip_INCLUDE_DIR
-  NAMES 7zVersion.h
-  PATH_SUFFIXES 7zip/C 7zip/CPP include/7zip include
-  PATHS /usr/include /usr/local/include
-  DOC "7-Zip include directory"
+  NAMES 7zip/C/7zip.h
+  HINTS ${PC_7zip_INCLUDE_DIRS}
+  DOC "7zip include directory"
 )
+mark_as_advanced(7zip_INCLUDE_DIR)
 
-if(WIN32)
-  find_library(
-    7zip_LIBRARY
-    NAMES 7z 7zip
-    PATH_SUFFIXES bin lib
-    DOC "7-Zip library"
-  )
-else()
-  find_library(7zip_LIBRARY NAMES 7z lib7z DOC "7-Zip library")
+find_library(
+  7zip_LIBRARY
+  NAMES 7zip
+  HINTS ${PC_7zip_LIBRARY_DIRS}
+  DOC "7zip library"
+)
+mark_as_advanced(7zip_LIBRARY)
+
+if(DEFINED PC_7zip_VERSION AND NOT PC_7zipliblo_VERSION STREQUAL "")
+  set(7zip_VERSION "${PC_7zip_VERSION}")
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   7zip
   REQUIRED_VARS 7zip_LIBRARY 7zip_INCLUDE_DIR
+  VERSION_VAR 7zip_VERSION
 )
 
 if(7zip_FOUND)
-  set(7zip_LIBRARIES "${7zip_LIBRARY}")
-  set(7zip_INCLUDE_DIRS "${7zip_INCLUDE_DIR}")
-
   if(NOT TARGET 7zip::7zip)
     add_library(7zip::7zip UNKNOWN IMPORTED)
     set_target_properties(
       7zip::7zip
       PROPERTIES
         IMPORTED_LOCATION "${7zip_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${PC_7zip_CFLAGS_OTHER}"
         INTERFACE_INCLUDE_DIRECTORIES "${7zip_INCLUDE_DIR}"
     )
   endif()
--- cmake/modules/Findunofficial-bit7z.cmake
+++ cmake/modules/Findunofficial-bit7z.cmake
@@ -38,74 +38,49 @@ The following cache variables may also be set:
 
 find_package(PkgConfig QUIET)
 if(PkgConfig_FOUND)
-  pkg_check_modules(PC_bit7z QUIET unofficial-bit7z)
+  pkg_check_modules(PC_unofficial-bit7z QUIET unofficial-bit7z)
 endif()
 
 find_path(
   unofficial-bit7z_INCLUDE_DIR
   NAMES bit7z.hpp
   PATH_SUFFIXES bit7z
-  HINTS
-    ${PC_bit7z_INCLUDE_DIRS}
-    "$ENV{VCPKG_INSTALLED_DIR}/$ENV{VCPKG_DEFAULT_TRIPLET}/include"
-    "/usr/local/include"
-    "/usr/include"
-  DOC "bit7z include directory"
+  HINTS ${PC_unofficial-bit7z_INCLUDE_DIRS}
+  DOC "unofficial-bit7z include directory"
 )
 mark_as_advanced(unofficial-bit7z_INCLUDE_DIR)
 
 find_library(
   unofficial-bit7z_LIBRARY
-  NAMES bit7z bit7z64
-  HINTS
-    ${PC_bit7z_LIBRARY_DIRS}
-    "$ENV{VCPKG_INSTALLED_DIR}/$ENV{VCPKG_DEFAULT_TRIPLET}/lib"
-    "/usr/local/lib"
-    "/usr/lib"
-  DOC "bit7z library"
+  NAMES bit7z
+  HINTS ${PC_unofficial-bit7z_LIBRARY_DIRS}
+  DOC "unofficial-bit7zlibrary"
 )
 mark_as_advanced(unofficial-bit7z_LIBRARY)
 
+if(
+  DEFINED PC_unofficial-bit7z_VERSION
+  AND NOT PC_unofficial-bit7z_VERSION STREQUAL ""
+)
+  set(bit7z_VERSION "${PC_unofficial-bit7z_VERSION}")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  unofficial-bit7z
+  bit7z
   REQUIRED_VARS unofficial-bit7z_LIBRARY unofficial-bit7z_INCLUDE_DIR
-  FAIL_MESSAGE "bit7z not found. Install via:\n  vcpkg install bit7z"
+  VERSION_VAR unofficial-bit7z_VERSION
 )
 
 if(unofficial-bit7z_FOUND)
-  set(unofficial-bit7z_LIBRARIES "${unofficial-bit7z_LIBRARY}")
-  set(unofficial-bit7z_INCLUDE_DIRS "${unofficial-bit7z_INCLUDE_DIR}")
-
-  if(NOT TARGET unofficial::bit7z::bit7z64)
-    add_library(unofficial::bit7z::bit7z64 UNKNOWN IMPORTED)
+  if(NOT TARGET unofficial-bit7z::unofficial-bit7z)
+    add_library(unofficial-bit7z::unofficial-bit7z UNKNOWN IMPORTED)
     set_target_properties(
-      unofficial::bit7z::bit7z64
+      unofficial-bit7z::unofficial-bit7z
       PROPERTIES
         IMPORTED_LOCATION "${unofficial-bit7z_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${PC_unofficial-bit7z_CFLAGS_OTHER}"
         INTERFACE_INCLUDE_DIRECTORIES "${unofficial-bit7z_INCLUDE_DIR}"
     )
-
-    # Handle DLL copy on Windows
-    if(WIN32)
-      find_file(
-        BIT7Z_DLL
-        NAMES 7zip.dll
-        HINTS
-          "$ENV{VCPKG_INSTALLED_DIR}/$ENV{VCPKG_DEFAULT_TRIPLET}/bin"
-          "${CMAKE_CURRENT_LIST_DIR}/../../bin"
-        NO_DEFAULT_PATH
-      )
-      if(BIT7Z_DLL AND TARGET mixxx-lib)
-        add_custom_command(
-          TARGET mixxx-lib
-          POST_BUILD
-          COMMAND
-            ${CMAKE_COMMAND} -E copy_if_different "${BIT7Z_DLL}"
-            "$<TARGET_FILE_DIR:mixxx-lib>"
-          COMMENT "Copying 7zip.dll to output directory"
-        )
-      endif()
-    endif()
   endif()
 endif()
