ded2c6b79e3bf7e3b2cae985bf769b6029520ed1
Antoine C. <mixxx@acolombier.dev>
2025-05-31 15:49:53 +0000

    fixup! feat: add Library cappability on QML

--- src/qml/qmllibrarytracklistcolumn.h
+++ src/qml/qmllibrarytracklistcolumn.h
@@ -20,7 +20,7 @@ class QmlLibraryTrackListColumn : public QObject {
     Q_PROPERTY(int fillSpan MEMBER m_fillSpan FINAL)
     Q_PROPERTY(int columnIdx MEMBER m_columnIdx FINAL)
     Q_PROPERTY(double preferredWidth MEMBER m_preferredWidth FINAL)
-    Q_PROPERTY(QQmlComponent* delegate MEMBER m_pDelegate FINAL)
+    Q_PROPERTY(QQmlComponent* delegate READ delegate FINAL)
     Q_PROPERTY(Role role MEMBER m_role FINAL)
     QML_NAMED_ELEMENT(TrackListColumn)
   public:
