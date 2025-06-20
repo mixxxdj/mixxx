922c57097e97e872eb8b1919267e18f6daa55222
Antonio Giordano <antoniogiordano@Antonios-MacBook-Air.local>
2025-06-20 17:28:16 +0200

    genredao.cpp changes

--- src/library/dao/genredao.cpp
+++ src/library/dao/genredao.cpp
@@ -1,8 +1,12 @@
 #include "library/dao/genredao.h"
 
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QVariant>
+
 #include "library/dao/trackschema.h"
 #include "util/db/dbconnection.h"
-#include "util/db/sqlite.h" // Include the SQLite header for SQuery
+#include "util/db/sqlite.h" // For SQuery and LOG_FAILED_QUERY
 #include "util/db/sqltransaction.h"
 #include "util/logger.h"
 
@@ -10,30 +14,31 @@ namespace {
 mixxx::Logger kLogger("GenreDAO");
 }
 
+// base DAO constructor
 GenreDAO::GenreDAO(DbConnection* dbConnection)
-        : DAO(dbConnection) {
+        : DAO() {
+    setDb(dbConnection);
 }
 
 DbId GenreDAO::addGenre(const QString& name) {
     const QString trimmedName = name.trimmed();
     if (trimmedName.isEmpty()) {
-        return DbId_Invalid;
+        return invalidId();
     }
 
     Genre existingGenre = getGenreByName(trimmedName);
-    if (existingGenre.id != DbId_Invalid) {
+    if (existingGenre.id != invalidId()) {
         return existingGenre.id;
     }
 
     QSqlQuery query = SQuery(m_pDb);
     query.prepare(QString("INSERT INTO %1 (%2) VALUES (:name)")
-                    .arg(mixxx::trackschema::TableGenres,
-                            mixxx::trackschema::GenresName));
+                    .arg(TableGenres, GenresName));
     query.bindValue(":name", trimmedName);
 
-    if (!exec(query)) {
+    if (!query.exec()) {
         LOG_FAILED_QUERY(query) << "Failed to insert new genre:" << trimmedName;
-        return DbId_Invalid;
+        return invalidId();
     }
     return query.lastInsertId().toLongLong();
 }
@@ -41,33 +46,28 @@ DbId GenreDAO::addGenre(const QString& name) {
 Genre GenreDAO::getGenreByName(const QString& name) {
     QSqlQuery query = SQuery(m_pDb);
     query.prepare(QString("SELECT %1, %2 FROM %3 WHERE %2 = :name")
-                    .arg(mixxx::trackschema::GenresId,
-                            mixxx::trackschema::GenresName,
-                            mixxx::trackschema::TableGenres));
+                    .arg(GenresId, GenresName, TableGenres));
     query.bindValue(":name", name);
 
-    if (!exec(query)) {
-        LOG_FAILED_QUERY(query) << "Failed to get genre by name:" << name;
+    if (!query.exec() || !query.next()) {
+        if (query.lastError().type() != QSqlError::NoError) {
+            LOG_FAILED_QUERY(query) << "Failed to get genre by name:" << name;
+        }
         return Genre();
-    }
-    if (!query.next()) {
-        return Genre(); // Not found, return invalid genre
     }
     return Genre{query.value(0).toLongLong(), query.value(1).toString()};
 }
 
 Genre GenreDAO::getGenreById(DbId id) {
-    if (id == DbId_Invalid) {
+    if (id == invalidId()) {
         return Genre();
     }
     QSqlQuery query = SQuery(m_pDb);
     query.prepare(QString("SELECT %1, %2 FROM %3 WHERE %1 = :id")
-                    .arg(mixxx::trackschema::GenresId,
-                            mixxx::trackschema::GenresName,
-                            mixxx::trackschema::TableGenres));
-    query.bindValue(":id", id);
+                    .arg(GenresId, GenresName, TableGenres));
+    query.bindValue(":id", QVariant::fromValue(id));
 
-    if (!exec(query) || !query.next()) {
+    if (!query.exec() || !query.next()) {
         return Genre();
     }
     return Genre{query.value(0).toLongLong(), query.value(1).toString()};
@@ -75,25 +75,25 @@ Genre GenreDAO::getGenreById(DbId id) {
 
 QList<Genre> GenreDAO::getGenresForTrack(DbId trackId) {
     QList<Genre> genres;
-    if (trackId == DbId_Invalid) {
+    if (trackId == invalidId()) {
         return genres;
     }
 
     QSqlQuery query = SQuery(m_pDb);
-    query.prepare(QString(
-            "SELECT g.%1, g.%2 FROM %3 g "
-            "JOIN %4 gt ON g.%1 = gt.%5 "
-            "WHERE gt.%6 = :trackId "
-            "ORDER BY g.%2")
-                    .arg(mixxx::trackschema::GenresId,
-                            mixxx::trackschema::GenresName,
-                            mixxx::trackschema::TableGenres,
-                            mixxx::trackschema::TableGenreTracks,
-                            mixxx::trackschema::GenreTracksGenreId,
-                            mixxx::trackschema::GenreTracksTrackId));
-    query.bindValue(":trackId", trackId);
+    query.prepare(
+            QString("SELECT g.%1, g.%2 FROM %3 AS g "
+                    "JOIN %4 AS gt ON g.%1 = gt.%5 "
+                    "WHERE gt.%6 = :trackId "
+                    "ORDER BY g.%2")
+                    .arg(GenresId,
+                            GenresName,
+                            TableGenres,
+                            TableGenreTracks,
+                            GenreTracksGenreId,
+                            GenreTracksTrackId));
+    query.bindValue(":trackId", QVariant::fromValue(trackId));
 
-    if (!exec(query)) {
+    if (!query.exec()) {
         LOG_FAILED_QUERY(query) << "Failed to get genres for track id:" << trackId;
         return genres;
     }
@@ -105,7 +105,7 @@ QList<Genre> GenreDAO::getGenresForTrack(DbId trackId) {
 }
 
 bool GenreDAO::setGenresForTrack(DbId trackId, const QList<DbId>& genreIds) {
-    if (trackId == DbId_Invalid) {
+    if (trackId == invalidId()) {
         return false;
     }
 
@@ -115,30 +115,27 @@ bool GenreDAO::setGenresForTrack(DbId trackId, const QList<DbId>& genreIds) {
         return false;
     }
 
-    // 1. Delete all existing genre associations for this track.
     QSqlQuery deleteQuery = SQuery(m_pDb);
     deleteQuery.prepare(QString("DELETE FROM %1 WHERE %2 = :trackId")
-                    .arg(mixxx::trackschema::TableGenreTracks,
-                            mixxx::trackschema::GenreTracksTrackId));
-    deleteQuery.bindValue(":trackId", trackId);
-    if (!exec(deleteQuery)) {
+                    .arg(TableGenreTracks, GenreTracksTrackId));
+    deleteQuery.bindValue(":trackId", QVariant::fromValue(trackId));
+    if (!deleteQuery.exec()) {
+        LOG_FAILED_QUERY(deleteQuery);
         transaction.rollback();
         return false;
     }
 
-    // 2. Insert the new associations.
     if (!genreIds.isEmpty()) {
         QSqlQuery insertQuery = SQuery(m_pDb);
         insertQuery.prepare(QString("INSERT INTO %1 (%2, %3) VALUES (:trackId, :genreId)")
-                        .arg(mixxx::trackschema::TableGenreTracks,
-                                mixxx::trackschema::GenreTracksTrackId,
-                                mixxx::trackschema::GenreTracksGenreId));
+                        .arg(TableGenreTracks, GenreTracksTrackId, GenreTracksGenreId));
 
         for (const DbId genreId : genreIds) {
-            if (genreId != DbId_Invalid) {
-                insertQuery.bindValue(":trackId", trackId);
-                insertQuery.bindValue(":genreId", genreId);
-                if (!exec(insertQuery)) {
+            if (genreId != invalidId()) {
+                insertQuery.bindValue(":trackId", QVariant::fromValue(trackId));
+                insertQuery.bindValue(":genreId", QVariant::fromValue(genreId));
+                if (!insertQuery.exec()) {
+                    LOG_FAILED_QUERY(insertQuery);
                     transaction.rollback();
                     return false;
                 }
@@ -151,14 +148,11 @@ bool GenreDAO::setGenresForTrack(DbId trackId, const QList<DbId>& genreIds) {
 
 QList<Genre> GenreDAO::getAllGenres() {
     QList<Genre> genres;
-    // Using a direct query as shorthand is not available/needed.
     QSqlQuery query = SQuery(m_pDb);
     query.prepare(QString("SELECT %1, %2 FROM %3 ORDER BY %2")
-                    .arg(mixxx::trackschema::GenresId,
-                            mixxx::trackschema::GenresName,
-                            mixxx::trackschema::TableGenres));
+                    .arg(GenresId, GenresName, TableGenres));
 
-    if (!exec(query)) {
+    if (!query.exec()) {
         LOG_FAILED_QUERY(query) << "Failed to get all genres";
         return genres;
     }
@@ -171,10 +165,7 @@ QList<Genre> GenreDAO::getAllGenres() {
 
 int GenreDAO::deleteUnusedGenres() {
     const QString sql = QString("DELETE FROM %1 WHERE %2 NOT IN (SELECT DISTINCT %3 FROM %4)")
-                                .arg(mixxx::trackschema::TableGenres,
-                                        mixxx::trackschema::GenresId,
-                                        mixxx::trackschema::GenreTracksGenreId,
-                                        mixxx::trackschema::TableGenreTracks);
+                                .arg(TableGenres, GenresId, GenreTracksGenreId, TableGenreTracks);
 
     QSqlQuery query = SQuery(m_pDb);
     if (!query.exec(sql)) {
