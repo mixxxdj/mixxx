f7b9aa1f1b703747f9716387dcfb827dd1e73630
danferns <dannywritescode@gmail.com>
2025-06-22 18:31:22 +0530

    refactor: visual_key_distance -> key_distance
    And, create a new visual_key_distance that is in sync with visual_key

--- src/waveform/visualsmanager.cpp
+++ src/waveform/visualsmanager.cpp
@@ -7,15 +7,26 @@ DeckVisuals::DeckVisuals(const QString& group)
         : m_group(group),
           m_SlowTickCnt(0),
           m_trackLoaded(false),
-          m_pPlayButton(std::make_unique<ControlProxy>(ConfigKey(group, "play"))),
-          m_pLoopEnabled(std::make_unique<ControlProxy>(ConfigKey(group, "loop_enabled"))),
+          m_pPlayButton(
+                  std::make_unique<ControlProxy>(ConfigKey(group, "play"))),
+          m_pLoopEnabled(std::make_unique<ControlProxy>(
+                  ConfigKey(group, "loop_enabled"))),
           m_pEngineBpm(std::make_unique<ControlProxy>(ConfigKey(group, "bpm"))),
-          m_pVisualBpm(std::make_unique<ControlProxy>(ConfigKey(m_group, "visual_bpm"))),
+          m_pVisualBpm(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "visual_bpm"))),
           m_pEngineKey(std::make_unique<ControlProxy>(ConfigKey(group, "key"))),
-          m_pVisualKey(std::make_unique<ControlProxy>(ConfigKey(m_group, "visual_key"))),
-          m_pTimeElapsed(std::make_unique<ControlProxy>(ConfigKey(m_group, "time_elapsed"))),
-          m_pTimeRemaining(std::make_unique<ControlProxy>(ConfigKey(m_group, "time_remaining"))),
-          m_pEndOfTrack(std::make_unique<ControlProxy>(ConfigKey(m_group, "end_of_track"))) {
+          m_pVisualKey(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "visual_key"))),
+          m_pEngineKeyDistance(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "key_distance"))),
+          m_pVisualKeyDistance(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "visual_key_distance"))),
+          m_pTimeElapsed(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "time_elapsed"))),
+          m_pTimeRemaining(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "time_remaining"))),
+          m_pEndOfTrack(std::make_unique<ControlProxy>(
+                  ConfigKey(m_group, "end_of_track"))) {
     // Control used to communicate ratio playpos to GUI thread
     m_pVisualPlayPos = VisualPlayPosition::getVisualPlayPosition(m_group);
 }
@@ -50,6 +61,7 @@ void DeckVisuals::process(double remainingTimeTriggerSeconds) {
         m_pVisualBpm->set(m_pEngineBpm->get());
     }
     m_pVisualKey->set(m_pEngineKey->get());
+    m_pVisualKeyDistance->set(m_pEngineKeyDistance->get());
 
     m_trackLoaded = trackLoaded;
 }
