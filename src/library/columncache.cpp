8d6059c2a0f7dda83fe5c4e4e7881711bcf04cbe
jstolberg <jsl731@pm.me>
2025-07-22 22:59:17 +0200

    columncache updated.

--- src/library/columncache.cpp
+++ src/library/columncache.cpp
@@ -176,7 +176,19 @@ constexpr ColumnProperties kColumnPropertiesByEnum[] = {
                 &PLAYLISTTRACKSTABLE_DATETIMEADDED,
                 QT_TRANSLATE_NOOP("BaseSqlTableModel", "Timestamp"),
                 kDefaultColumnWidth * 80 / 50},
-        DI(ColumnCache::COLUMN_REKORDBOX_ANALYZE_PATH){&REKORDBOX_ANALYZE_PATH, nullptr, 0}};
+        DI(ColumnCache::COLUMN_REKORDBOX_ANALYZE_PATH){&REKORDBOX_ANALYZE_PATH, nullptr, 0},
+        DI(ColumnCache::COLUMN_RELATIONTABLE_ID){
+                &RELATIONTABLE_ID,
+                nullptr,
+                0},
+        DI(ColumnCache::COLUMN_RELATIONTABLE_COMMENT){
+                &RELATIONTABLE_COMMENT,
+                nullptr,
+                kDefaultColumnWidth * 4},
+        DI(ColumnCache::COLUMN_RELATIONTABLE_DATETIMEADDED){
+                &RELATIONTABLE_DATETIMEADDED,
+                nullptr,
+                kDefaultColumnWidth}};
 static_assert(std::size(kColumnPropertiesByEnum) == ColumnCache::NUM_COLUMNS);
 
 #if defined(__clang__)
