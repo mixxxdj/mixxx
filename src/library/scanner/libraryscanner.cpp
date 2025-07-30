4a6d07378e0890159ebc9b94a2a86790052a1a49
Antonio Giordano <g73o82d65n79@protonmail.ch>
2025-07-30 20:00:40 +0200

    Adds a GenreDao member to LibraryScanner, ensuring the scanner can initialize and provide genre management support during track import and database operations

--- src/library/scanner/libraryscanner.cpp
+++ src/library/scanner/libraryscanner.cpp
@@ -100,7 +100,7 @@ LibraryScanner::LibraryScanner(
         const UserSettingsPointer& pConfig)
         : m_pDbConnectionPool(std::move(pDbConnectionPool)),
           m_analysisDao(pConfig),
-          m_trackDao(m_cueDao, m_playlistDao, m_analysisDao, m_libraryHashDao, pConfig),
+          m_trackDao(m_cueDao, m_playlistDao, m_analysisDao, m_genreDao, m_libraryHashDao, pConfig),
           m_stateSema(1), // only one transaction is possible at a time
           m_state(IDLE),
           m_manualScan(true) {
