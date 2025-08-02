8daef85a18be6fc9fddf3f89c613d9e0491d5be0
Jan Claußen <jan.claussen10@web.de>
2025-08-02 10:28:42 +0200

    xwax: Put MK2-related functions into its own source file

--- lib/xwax/timecoder.c
+++ lib/xwax/timecoder.c
@@ -22,7 +22,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #ifndef _MSC_VER
 #include <unistd.h>
 #endif
@@ -30,8 +29,8 @@
 #include <math.h>
 
 #include "debug.h"
-#include "filters.h"
 #include "timecoder.h"
+#include "timecoder_mk2.h"
 
 #define ZERO_THRESHOLD (128 << 16)
 
@@ -226,28 +225,6 @@ static inline bits_t lfsr(bits_t code, bits_t taps)
     return xrs & 0x1;
 }
 
-
-/*
- * Calculate LFSR bit (Traktor MK2 version)
- */
-
-static inline mk2bits_t lfsr_mk2(mk2bits_t code, mk2bits_t taps)
-{
-    mk2bits_t taken;
-    mk2bits_t xrs;
-
-    taken = u128_and(code, taps);
-    xrs = U128_ZERO;
-
-    while (u128_neq(taken, U128_ZERO)) {
-        xrs = u128_add(xrs, u128_and(taken, U128_ONE));
-        taken = u128_rshift(taken, 1);
-    }
-
-    return u128_and(xrs, U128_ONE);
-}
-
-
 /*
  * Linear Feedback Shift Register in the forward direction. New values
  * are generated at the least-significant bit.
@@ -264,22 +241,6 @@ static inline bits_t fwd(bits_t current, struct timecode_def *def)
 }
 
 /*
- * Linear Feedback Shift Register in the forward direction. New values
- * are generated at the least-significant bit. (Traktor MK2 version)
- */
-
-static inline mk2bits_t fwd_mk2(mk2bits_t current, struct timecode_def *def)
-{
-    mk2bits_t l;
-
-    /* New bits are added at the MSB; shift right by one */
-
-    l = lfsr_mk2(current, u128_or(def->taps_mk2,  U128_ONE));
-    return u128_or(u128_rshift(current, 1), u128_lshift(l, (def->bits - 1)));
-}
-
-
-/*
  * Linear Feedback Shift Register in the reverse direction
  */
 
@@ -293,27 +254,6 @@ static inline bits_t rev(bits_t current, struct timecode_def *def)
     l = lfsr(current, (def->taps >> 1) | (0x1 << (def->bits - 1)));
     return ((current << 1) & mask) | l;
 }
-
-
-/*
- * Linear Feedback Shift Register in the reverse direction
- * (Traktor MK2 version)
- */
-
-static inline mk2bits_t rev_mk2(mk2bits_t current, struct timecode_def *def)
-{
-    mk2bits_t l, mask;
-
-    /* New bits are added at the LSB; shift left one and mask */
-
-    mask = u128_sub(u128_lshift(U128_ONE,  def->bits), U128_ONE);
-    l = lfsr_mk2(current,
-         u128_or(u128_rshift(def->taps_mk2, 1), 
-         u128_lshift(U128_ONE, (def->bits - 1))));
-
-    return u128_or(u128_and(u128_lshift(current, 1), mask), l);
-}
-
 
 /*
  * Where necessary, build the lookup table required for this timecode
@@ -355,209 +295,6 @@ static int build_lookup(struct timecode_def *def)
 
     return 0;
 }
-
-
-/*
- * Where necessary, build the lookup table required for this timecode 
- * (Traktor MK2 version)
- *
- * Return: -1 if not enough memory could be allocated, otherwise 0
- */
-
-static int build_lookup_mk2(struct timecode_def *def)
-{
-    unsigned int n;
-    mk2bits_t current, next;
-
-    if (def->lookup)
-        return 0;
-
-    fprintf(stderr, "Building LUT for %d bit %dHz timecode (%s)\n",
-            def->bits, def->resolution, def->desc);
-
-    if (lut_init_mk2(&def->lut_mk2, def->length) == -1)
-        return -1;
-
-    current = def->seed_mk2;
-
-    for (n = 0; n < def->length; n++) {
-
-        /* timecode must not wrap */
-        assert(lut_lookup_mk2(&def->lut_mk2, &current) == (unsigned)-1);
-        lut_push_mk2(&def->lut_mk2, &current);
-
-        /* check symmetry of the lfsr functions */
-        next = fwd_mk2(current, def);
-        assert(u128_eq(rev_mk2(next, def), current));
-
-        current = next;
-    }
-
-    def->lookup = true;
-
-    return 0;
-}
-
-
-/* 
- * Caches the generated LUT on the disk.
- * This is only necessary for the Traktor MK2, since the size of their hash
- * tables is quite large.
- */
-
-int lut_store_mk2(struct timecode_def *def, const char *lut_dir_path)
-{
-    if (!def || !lut_dir_path)
-        return -1;
-
-    struct slot_mk2 *slot;
-    struct stat stats;
-    slot_no_t *hash;
-
-    int i, j, len, hashes;
-    char path[1024];
-    FILE *fp = NULL;
-    int r = 0;
-    int size;
-
-    sprintf(path, "%s/%s%s", lut_dir_path, def->name, ".lut");
-
-    fprintf(stdout, "Storing LUT at %s\n", path);
-    fp = fopen(path, "wb");
-    if (!fp) {
-        perror("fopen");
-        return -1;
-    }
-
-    for (i = 0; i < def->length; i++) {
-        slot = &def->lut_mk2.slot[i];
-
-        if (!slot) {
-            printf("slot_no: %d doesn't exist'\n", i);
-            r = -1;
-            goto out;
-        }
-        size = fwrite(slot, sizeof(struct slot_mk2), 1, fp);
-
-        if (!size) {
-            perror("fwrite");
-            r = -1;
-            goto out;
-        }
-    }
-
-    hashes = 1 << 16;
-    for (j = 0; j < hashes; j++) {
-        hash = &def->lut_mk2.table[j];
-
-        size = fwrite(hash, sizeof(slot_no_t), 1, fp);
-        if (!size) {
-            perror("fwrite");
-            r = -1;
-            goto out;
-        }
-    }
-
-    size = fwrite(&def->lut_mk2.avail, sizeof(slot_no_t), 1, fp);
-    if (!size) {
-        perror("fwrite");
-        r = -1;
-        goto out;
-    }
-
-out:
-    fclose(fp);
-
-    if (hashes != j || def->length != i)
-        fprintf(stderr, "Something went wrong: ");
-
-    fprintf(stderr, "Wrote %d hashes and %d slots to disk\n", j, i);
-
-    return r;
-}
-
-
-/* 
- * Loads the stored LUT from the disk.
- * This is only necessary for the Traktor MK2, since the size of their hash
- * tables is quite large.
- */
-
-int lut_load_mk2(struct timecode_def *def, const char *lut_dir_path)
-{
-    if (!def || !lut_dir_path)
-        return -1;
-
-    struct slot_mk2 *slot;
-
-    char path[1024];
-    int i, j, hashes;
-    int r = 0;
-    int size;
-    FILE *fp;
-    int len;
-
-    sprintf(path, "%s/%s%s", lut_dir_path, def->name, ".lut");
-
-    fprintf(stdout, "Loading LUT from %s\n", path);
-    fp = fopen(path, "rb");
-    if (!fp) {
-        fprintf(stderr, "LUT for %s not found on disk\n", def->desc);
-        return -1;
-    }
-
-    r = lut_init_mk2(&def->lut_mk2, def->length);
-    if (r) {
-        fprintf(stderr, "Couldn't initialise LUT\n");
-        goto out;
-    }
-
-    fprintf(stdout, "Loading LUT from %s\n", path);
-    for (i = 0; i < def->length; i++) {
-        slot = &def->lut_mk2.slot[i];
-
-        size = fread(slot, sizeof(struct slot_mk2), 1, fp);
-        if (!size) {
-            perror("fread");
-            r = -1;
-            goto out;
-        }
-    }
-
-    hashes = 1 << 16;
-    for (j = 0; j < hashes; j++) {
-
-        slot_no_t *hash = &def->lut_mk2.table[j];
-
-        size = fread(hash, sizeof(slot_no_t), 1, fp);
-        if (!size) {
-            perror("fread");
-            r = -1;
-            goto out;
-        }
-    }
-
-    size = fread(&def->lut_mk2.avail, sizeof(slot_no_t), 1, fp);
-    if (!size) {
-        perror("fwrite");
-        r = -1;
-    }
-        goto out;
-
-
-out:
-    fclose(fp);
-
-    if (hashes == j && def->length == i)
-        def->lookup = true;
-    else
-        fprintf(stderr, "Something went wrong: ");
-
-    fprintf(stderr, "Loaded %d hashes and %d slots from disk\n", j, i);
-
-    return r;
-}
-
 
 /*
  * Find a timecode definition by name
@@ -620,43 +357,6 @@ void timecoder_free_lookup(void) {
         }
     }
 }
-
-/*
- * Initialise a subcode decoder for the Traktor MK2
- */
-
-void mk2_subcode_init(struct mk2_subcode *sc)
-{
-    sc->valid_counter = 0;
-    sc->avg_reading = INT_MAX/2;
-    sc->avg_slope = INT_MAX/2;
-    sc->bit = U128_ZERO;
-
-    delayline_init(&sc->readings);
-
-    /* Initialise smoothing filters */
-    ema_init(&sc->ema_reading, 0.01);
-    ema_init(&sc->ema_slope, 0.01);
-}
-
-/*
- * Initialise filter values for the MK2 demodulation
- */
-
-static void init_mk2_channel(struct timecoder_channel *ch)
-{
-    ch->mk2.deriv_scaled = INT_MAX/2;
-    ch->mk2.rms = INT_MAX/2;
-    ch->mk2.rms_deriv = 0;
-
-    delayline_init(&ch->mk2.delayline);
-
-    ema_init(&ch->mk2.ema_filter, 3e-1);
-    derivative_init(&ch->mk2.differentiator);
-    rms_init(&ch->mk2.rms_filter, 1e-3);
-    rms_init(&ch->mk2.rms_deriv_filter, 1e-3);
-}
-
 
 /*
  * Initialise filter values for one channel
@@ -812,8 +512,8 @@ static inline void update_monitor(struct timecoder *tc, signed int x, signed int
         for (p = 0; p < SQ(size); p++) {
             if (tc->mon[p])
                 tc->mon[p] = tc->mon[p] * 7 / 8;
-        }
     }
+        }
 
     assert(ref > 0);
 
@@ -826,119 +526,6 @@ static inline void update_monitor(struct timecoder *tc, signed int x, signed int
 
     tc->mon[py * size + px] = 0xff; /* white */
 }
-
-
-#define FORWARD_FACTOR 1.5
-#define REVERSE_FACTOR 1.75
-static inline void detect_bit_flip(const int slope[2], int rms, int reading, int avg_reading,
-                                   mk2bits_t *bit, bool *bit_flipped, bool forwards, mk2bits_t one)
-{
-    double threshold;
-
-    if (*bit_flipped == false) {
-        if (forwards) {
-            threshold = rms / FORWARD_FACTOR;
-        } else {
-            threshold = rms / REVERSE_FACTOR;
-            one = u128_not(one);
-        }
-
-        if (u128_eq(*bit, u128_not(one)) && slope[0] > threshold && slope[1] > threshold) {
-            *bit = one;
-            *bit_flipped = true;
-        } else if (u128_eq(*bit, one) && slope[0] < -threshold && slope[1] < -threshold) {
-            *bit = u128_not(one);
-            *bit_flipped = true;
-        }
-    } else {
-        *bit_flipped = false;
-    }
-}
-
-
-static inline bool lfsr_verify(struct timecode_def *def, mk2bits_t *timecode, mk2bits_t *bitstream, 
-        mk2bits_t bit, bool forwards)
-{
-    if (forwards) {
-        *timecode = fwd_mk2(*timecode, def);
-        *bitstream = u128_add(u128_rshift(*bitstream, 1), u128_lshift(bit, (def->bits - 1)));
-    } else {
-        mk2bits_t mask = u128_sub(u128_lshift(U128_ONE, def->bits), U128_ONE);
-        *timecode = rev_mk2(*timecode, def);
-        *bitstream = u128_add(u128_and(u128_lshift(*bitstream, 1), mask), bit);
-    }
-
-    if (u128_eq(*timecode, *bitstream))
-        return true;
-    else
-        return false;
-}
-
-
-inline static void mk2_process_subcode(struct timecoder *tc, struct mk2_subcode *sc, signed int reading)
-{
-    int current_slope[2];
-
-    delayline_push(&sc->readings, reading);
-    sc->avg_reading = ema(&sc->ema_reading, reading);
-
-    /* Calculate absolute of average slope */
-    sc->avg_slope = ema(&sc->ema_slope, abs(reading - *delayline_at(&sc->readings, 1)));
-
-    /* Calculate current and last slope */
-    current_slope[0] =  (reading - *delayline_at(&sc->readings, 1));
-    current_slope[1] =  (reading - *delayline_at(&sc->readings, 2));
-
-    /* The bits only change when an offset jump occurs. Else the previous bit is taken  */
-    detect_bit_flip(current_slope, tc->secondary.mk2.rms, reading, sc->avg_reading, &sc->bit,
-                    &sc->recent_bit_flip, tc->forwards, U128(0x0, !tc->secondary.positive));
-    
-    if (lfsr_verify(tc->def, &sc->timecode, &sc->bitstream, sc->bit, tc->forwards)) {
-        (sc->valid_counter)++;
-    } else {
-        sc->timecode = sc->bitstream;
-        sc->valid_counter = 0;
-    }
-}
-
-
-static void mk2_process_bitstream(struct timecoder *tc, signed int reading) {
-
-    /* 
-     * Detect if the offset jumps on upper and lower bitstream
-     */
-    if (tc->secondary.positive)
-        mk2_process_subcode(tc, &tc->upper_subcode, reading);
-    else if (!tc->secondary.positive)
-        mk2_process_subcode(tc, &tc->lower_subcode, reading);
-
-    if (tc->lower_subcode.valid_counter > tc->upper_subcode.valid_counter) {
-        tc->mk2_bitstream = tc->lower_subcode.bitstream;
-        tc->mk2_timecode = tc->lower_subcode.timecode;
-    } else {
-        tc->mk2_bitstream = tc->upper_subcode.bitstream;
-        tc->mk2_timecode = tc->upper_subcode.timecode;
-    }
-
-    if (u128_eq(tc->mk2_timecode, tc->mk2_bitstream)) {
-        tc->valid_counter++;
-    } else {
-        tc->timecode = tc->bitstream;
-        tc->valid_counter = 0;
-    }
-    /* Take note of the last time we read a valid timecode */
-
-    tc->timecode_ticker = 0;
-
-    tc->ref_level -= tc->ref_level / REF_PEAKS_AVG;
-    tc->ref_level += abs((int) (tc->secondary.mk2.rms_deriv * tc->gain_compensation)) / REF_PEAKS_AVG;
-
-    debug("upper.valid_counter: %d, lower.valid_counter %d, forwards: %b\n", */
-           tc->upper.valid_counter,
-           tc->lower.valid_counter,
-           tc->forwards);
-}
-
 
 /*
  * Extract the bitstream from the sample value
@@ -991,54 +578,6 @@ static void process_bitstream(struct timecoder *tc, signed int m)
 	  b, tc->valid_counter == 0 ? 'x' : ' ',
 	  tc->valid_counter);
 }
-
-
-/*
- * Do Traktor-MK2-specific processing of the carrier wave
- *
- * Pushes samples into a delayline, computes the derivative, RMS values
- * and scales the derivative back up to the original signal's level. Ideally
- * the gain compensation should be done in the derivative and lowpass filter
- * structures by determining the amplitude response. I had this implemented
- * previously, but chose gain compensation by an RMS filter, since it is
- * easier to understand for developers not trained in signal processing.
- * 
- */
-
-static void mk2_process_carrier(struct timecoder *tc,
-        signed int primary, signed int secondary)
-{
-        /* Push the samples into the ringbuffer */
-        delayline_push(&tc->primary.mk2.delayline, primary);
-        delayline_push(&tc->secondary.mk2.delayline, secondary);
-
-        /* Compute the discrete derivative */
-        tc->primary.mk2.deriv = derivative(&tc->primary.mk2.differentiator,
-                                           ema(&tc->primary.mk2.ema_filter, primary));
-        tc->secondary.mk2.deriv = derivative(&tc->secondary.mk2.differentiator,
-                                             ema(&tc->secondary.mk2.ema_filter, secondary));
-
-        /* Compute the smoothed RMS value */
-        tc->primary.mk2.rms = rms(&tc->primary.mk2.rms_filter, primary);
-        tc->secondary.mk2.rms = rms(&tc->secondary.mk2.rms_filter, secondary);
-
-        /* Compute the smoothed RMS value for the derivative */
-        tc->primary.mk2.rms_deriv = rms(&tc->primary.mk2.rms_deriv_filter, tc->primary.mk2.deriv);
-        tc->secondary.mk2.rms_deriv = rms(&tc->secondary.mk2.rms_deriv_filter, tc->secondary.mk2.deriv);
-
-        /* Compute the gain compensation for the derivative*/
-        tc->gain_compensation = (double) tc->secondary.mk2.rms / tc->secondary.mk2.rms_deriv;
-
-        if (tc->gain_compensation > 30.0) // without this limit pitch becomes too sensitive
-            tc->gain_compensation = 30.0;
-
-        tc->dB = 20 * log10((double) tc->secondary.mk2.rms / INT_MAX);
-
-        /* Compute the scaled derivative */
-        tc->primary.mk2.deriv_scaled = tc->primary.mk2.deriv * tc->gain_compensation;
-        tc->secondary.mk2.deriv_scaled = tc->secondary.mk2.deriv * tc->gain_compensation;
-}
-
 
 /*
  * Process a single sample from the incoming audio
