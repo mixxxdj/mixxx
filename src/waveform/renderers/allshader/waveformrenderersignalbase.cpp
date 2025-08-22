bc7e97858f6d09f4835f17a759d8f41de47e8a1c
ronso0 <ronso0@mixxx.org>
2025-08-22 15:13:55 +0200

    remove Qt5 compatibility for QColor components

--- src/waveform/renderers/allshader/waveformrenderersignalbase.cpp
+++ src/waveform/renderers/allshader/waveformrenderersignalbase.cpp
@@ -1,7 +1,5 @@
 #include "waveform/renderers/allshader/waveformrenderersignalbase.h"
 
-#include "util/colorcomponents.h"
-
 namespace allshader {
 
 WaveformRendererSignalBase::WaveformRendererSignalBase(
@@ -15,27 +13,27 @@ void WaveformRendererSignalBase::draw(QPainter*, QPaintEvent*) {
 }
 
 void WaveformRendererSignalBase::setAxesColor(const QColor& axesColor) {
-    getRgbF(axesColor, &m_axesColor_r, &m_axesColor_g, &m_axesColor_b, &m_axesColor_a);
+    axesColor.getRgbF(&m_axesColor_r, &m_axesColor_g, &m_axesColor_b, &m_axesColor_a);
 }
 
 void WaveformRendererSignalBase::setColor(const QColor& color) {
-    getRgbF(color, &m_signalColor_r, &m_signalColor_g, &m_signalColor_b);
-    getHsvF(color, &m_signalColor_h, &m_signalColor_s, &m_signalColor_v);
+    color.getRgbF(&m_signalColor_r, &m_signalColor_g, &m_signalColor_b);
+    color.getHsvF(&m_signalColor_h, &m_signalColor_s, &m_signalColor_v);
 }
 
 void WaveformRendererSignalBase::setLowColor(const QColor& lowColor) {
-    getRgbF(lowColor, &m_rgbLowColor_r, &m_rgbLowColor_g, &m_rgbLowColor_b);
-    getRgbF(lowColor, &m_lowColor_r, &m_lowColor_g, &m_lowColor_b);
+    lowColor.getRgbF(&m_rgbLowColor_r, &m_rgbLowColor_g, &m_rgbLowColor_b);
+    lowColor.getRgbF(&m_lowColor_r, &m_lowColor_g, &m_lowColor_b);
 }
 
 void WaveformRendererSignalBase::setMidColor(const QColor& midColor) {
-    getRgbF(midColor, &m_rgbMidColor_r, &m_rgbMidColor_g, &m_rgbMidColor_b);
-    getRgbF(midColor, &m_midColor_r, &m_midColor_g, &m_midColor_b);
+    midColor.getRgbF(&m_rgbMidColor_r, &m_rgbMidColor_g, &m_rgbMidColor_b);
+    midColor.getRgbF(&m_midColor_r, &m_midColor_g, &m_midColor_b);
 }
 
 void WaveformRendererSignalBase::setHighColor(const QColor& highColor) {
-    getRgbF(highColor, &m_rgbHighColor_r, &m_rgbHighColor_g, &m_rgbHighColor_b);
-    getRgbF(highColor, &m_highColor_r, &m_highColor_g, &m_highColor_b);
+    highColor.getRgbF(&m_rgbHighColor_r, &m_rgbHighColor_g, &m_rgbHighColor_b);
+    highColor.getRgbF(&m_highColor_r, &m_highColor_g, &m_highColor_b);
 }
 
 } // namespace allshader
