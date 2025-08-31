191ec8464733eb06b617a67bd44bbab820af626e
ronso0 <ronso0@mixxx.org>
2025-09-01 00:52:20 +0200

    Overview: add mono option for decks

--- src/preferences/dialog/dlgprefwaveform.h
+++ src/preferences/dialog/dlgprefwaveform.h
@@ -65,6 +65,7 @@ class DlgPrefWaveform : public DlgPreferencePage, public Ui::DlgPrefWaveformDlg 
     void slotStemOutlineOpacity(float value);
     // overview options
     void slotSetWaveformOverviewType();
+    void slotSetOverviewStereoMode(bool mono);
     void slotSetOverviewMinuteMarkers(bool minuteMarkers);
     void slotSetOverviewScaling();
 
@@ -83,6 +84,7 @@ class DlgPrefWaveform : public DlgPreferencePage, public Ui::DlgPrefWaveformDlg 
 
     std::unique_ptr<ControlPushButton> m_pTypeControl;
     std::unique_ptr<ControlObject> m_pOverviewMinuteMarkersControl;
+    std::unique_ptr<ControlObject> m_pOverviewStereoControl;
 
     UserSettingsPointer m_pConfig;
     std::shared_ptr<Library> m_pLibrary;
