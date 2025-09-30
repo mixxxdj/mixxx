mixxxdj/mixxx

commit dfca85078b13f20b8a58d0cf1c90ceceb4514c9a

Pioneer DDJ-200: fixed PadMode Modeselector
removed externally accessible padInstance Containers (this.loopInstance
-> const loopInstance). These led to multiple trigger connections during
startup - too difficult to remove and handle them all cleanly with
setPads.
Implemented Deck-Switching internally, because without externally
connected padInstances component-js Deckswitching is also not working.

res/controllers/Pioneer-DDJ-200-scripts.js
@@ -255,48 +255,81 @@ DDJ200.DoubleRingBuffer = class {
 DDJ200.PadModeContainers.ModeSelector = function(group) {
     const deckOffset = (((script.deckFromGroup(group) - 1) % 2) * 2);
 
-    const updateSelectorLeds = () => {
-        this.forEachComponent(c => c.trigger());
-    };
+    const startupModeInstance = new DDJ200.PadModeContainers.Hotcue(deckOffset);
+    const loopInstance = new DDJ200.PadModeContainers.Loop(deckOffset);
+    const effectInstance = new DDJ200.PadModeContainers.Effect(deckOffset, group);
+    const jumpInstance = new DDJ200.PadModeContainers.Jump(deckOffset);
 
+    const padInstancesBuffer = new DDJ200.DoubleRingBuffer(
+        [startupModeInstance, loopInstance, effectInstance],
+        [jumpInstance]
+    );
+
+    let choosenPadInstance = startupModeInstance;
+
+    let startup = true;
     const setPads = newPadInstance => {
-        if (newPadInstance === this.choosenPadInstance) {
+        if (!startup && newPadInstance === choosenPadInstance) {
             return;
         }
-
-        this.choosenPadInstance.forEachComponent(function(component) {
+        startup = false;
+        choosenPadInstance.forEachComponent(function(component) {
             component.disconnect();
         });
-        this.choosenPadInstance = newPadInstance;
-        updateSelectorLeds();
-        this.choosenPadInstance.reconnectComponents(function(c) {
-            if (c.group === undefined) {
-                c.group = group;
+        choosenPadInstance = newPadInstance;
+        choosenPadInstance.forEachComponent(function(component) {
+            if (component.group === undefined) {
+                component.group = group;
+            }
+            component.connect();
+            component.trigger();
+        });
+    };
+
+    this.input = function(channel, control, value, status, group) {
+        choosenPadInstance.pads[control].input(channel, control, value, status, group);
+    };
+
+    this.changeMode = (isShifted) => {
+        if (isShifted !== padInstancesBuffer.isShifted) {
+            padInstancesBuffer.swapIndexable();
+        } else {
+            padInstancesBuffer.next();
+        };
+        setPads(padInstancesBuffer.current());
+    };
+
+    const updateDeckHelper = function(padInstance, newGroup) {
+        padInstance.forEachComponent(function(component) {
+            if (padInstance === choosenPadInstance) {
+                component.disconnect();
+            }
+            if (component.group === undefined) {
+                component.group = newGroup;
+            } else {
+                const anyChannelRegEx = /\[Channel\d+([\]_])/;
+                component.group = component.group.replace(anyChannelRegEx, `${newGroup.slice(0, -1)}$1`);
+            }
+            if (padInstance === choosenPadInstance) {
+                component.connect();
+                component.trigger();
             }
         });
     };
 
-    this.input = (isShifted) => {
-        if (isShifted !== this.padInstancesBuffer.isShifted) {
-            this.padInstancesBuffer.swapIndexable();
-        } else {
-            this.padInstancesBuffer.next();
-        };
-        setPads(this.padInstancesBuffer.current());
+    this.updateDeck = function(currentDeck) {
+        padInstancesBuffer.indexable[0].forEach(function(padInstance) {
+            updateDeckHelper(padInstance, currentDeck);
+        });
+        padInstancesBuffer.indexable[0].forEach(function(padInstance) {
+            updateDeckHelper(padInstance, currentDeck);
+        });
     };
 
-    this.startupModeInstance = new DDJ200.PadModeContainers.Hotcue(deckOffset);
-    this.loopInstance = new DDJ200.PadModeContainers.Loop(deckOffset);
-    this.effectInstance = new DDJ200.PadModeContainers.Effect(deckOffset, group);
-    this.jumpInstance = new DDJ200.PadModeContainers.Jump(deckOffset);
-
-    this.padInstancesBuffer = new DDJ200.DoubleRingBuffer(
-        [this.startupModeInstance, this.loopInstance, this.effectInstance],
-        [this.jumpInstance]
-    );
-
-    this.choosenPadInstance = new DDJ200.PadMode();
-    setPads(this.startupModeInstance);
+    this.choosenPadInstance = function() {
+        return choosenPadInstance;
+    };
+    setPads(startupModeInstance);
 };
 DDJ200.PadModeContainers.ModeSelector.prototype = new components.ComponentContainer();
 
@@ -349,14 +382,14 @@ DDJ200.init = function() {
         input: function(_channel, control, value, _status, _g) {
             if (value) {
                 DDJ200.decks.forEachComponentContainer(deck => {
-                    deck.padUnit.input((control === 0x5A));
+                    deck.padUnit.changeMode((control === 0x5A));
                 }, false);
                 this.indicatePadMode();
             };
         },
         indicatorStyle: function() {
             // use left Deck as reference for selected padInstance
-            return DDJ200.decks.left.padUnit.choosenPadInstance.indicatorStyle;
+            return DDJ200.decks.left.padUnit.choosenPadInstance().indicatorStyle;
         },
         blinkConnection: undefined,
         indicatePadMode: function() {
@@ -468,6 +501,9 @@ DDJ200.Deck = function(deckNumbers, midiChannel) {
             if (value) {
                 if (engine.getValue("[Skin]", "show_4decks")) {
                     theDeck.toggle();
+                    DDJ200.decks.forEachComponentContainer(deck => {
+                        deck.padUnit.updateDeck(deck.currentDeck);
+                    }, false);
                     if (theDeck.currentDeck === "[Channel3]" || theDeck.currentDeck === "[Channel4]") {
                         this.blinkConnection = engine.makeConnection("[App]", "indicator_250ms", function(value, _group, _control) {
                             theDeck.syncButton.send(theDeck.syncButton.on * value);

res/controllers/Pioneer DDJ-200.midi.xml
@@ -630,7 +630,7 @@
       <!-- ##### Hotcues ##### -->
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[0].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 1, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x00</midino>
@@ -640,7 +640,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[1].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 2, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x01</midino>
@@ -650,7 +650,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[2].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 3, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x02</midino>
@@ -660,7 +660,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[3].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 4, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x03</midino>
@@ -670,7 +670,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[4].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 5, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x04</midino>
@@ -680,7 +680,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[5].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 6, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x05</midino>
@@ -690,7 +690,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[6].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 7, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x06</midino>
@@ -700,7 +700,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[7].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 8, Deck 1: Set, preview, jump to</description>
         <status>0x97</status>
         <midino>0x07</midino>
@@ -710,7 +710,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[0].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 1, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x00</midino>
@@ -720,7 +720,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[1].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 2, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x01</midino>
@@ -730,7 +730,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[2].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 3, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x02</midino>
@@ -740,7 +740,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[3].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 4, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x03</midino>
@@ -750,7 +750,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[4].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 5, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x04</midino>
@@ -760,7 +760,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[5].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 6, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x05</midino>
@@ -770,7 +770,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[6].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 7, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x06</midino>
@@ -780,7 +780,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[7].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 8, Deck 2: Set, preview, jump to</description>
         <status>0x99</status>
         <midino>0x07</midino>
@@ -790,7 +790,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[0].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 1, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x00</midino>
@@ -800,7 +800,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[1].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 2, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x01</midino>
@@ -810,7 +810,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[2].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 3, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x02</midino>
@@ -820,7 +820,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[3].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 4, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x03</midino>
@@ -830,7 +830,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[4].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 5, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x04</midino>
@@ -840,7 +840,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[5].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 6, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x05</midino>
@@ -850,7 +850,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[6].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 7, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x06</midino>
@@ -860,7 +860,7 @@
       </control>
       <control>
         <group>[Channel1]</group>
-        <key>DDJ200.decks.left.padUnit.choosenPadInstance.pads[7].input</key>
+        <key>DDJ200.decks.left.padUnit.input</key>
         <description>Hotcue 8, Deck 1: Clear</description>
         <status>0x98</status>
         <midino>0x07</midino>
@@ -870,7 +870,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[0].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 1, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x00</midino>
@@ -880,7 +880,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[1].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 2, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x01</midino>
@@ -890,7 +890,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[2].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 3, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x02</midino>
@@ -900,7 +900,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[3].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 4, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x03</midino>
@@ -910,7 +910,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[4].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 5, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x04</midino>
@@ -920,7 +920,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[5].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 6, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x05</midino>
@@ -930,7 +930,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[6].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 7, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x06</midino>
@@ -940,7 +940,7 @@
       </control>
       <control>
         <group>[Channel2]</group>
-        <key>DDJ200.decks.right.padUnit.choosenPadInstance.pads[7].input</key>
+        <key>DDJ200.decks.right.padUnit.input</key>
         <description>Hotcue 8, Deck 2: Clear</description>
         <status>0x9A</status>
         <midino>0x07</midino>

commit 778e16851544d3f8624b802b7df6575318b42881

Pioneer DDJ-200: fixed Shift issue
Wihout external access to the pads (introduced in
dfca85078b13f20b8a58d0cf1c90ceceb4514c9a) lso also shift/unshift and
shutdown not working. Fixed this.

res/controllers/Pioneer-DDJ-200-scripts.js
@@ -337,6 +337,16 @@ DDJ200.init = function() {
     this.decks = new components.ComponentContainer({
         left: new DDJ200.Deck([1, 3], 1),
         right: new DDJ200.Deck([2, 4], 2),
+        shift: function() {
+            this.left.padUnit.choosenPadInstance().shift();
+            this.right.padUnit.choosenPadInstance().shift();
+            components.ComponentContainer.prototype.shift();
+        },
+        unshift: function() {
+            (this.left.padUnit.choosenPadInstance()).unshift();
+            this.right.padUnit.choosenPadInstance().unshift();
+            components.ComponentContainer.prototype.unshift();
+        }
     });
 
     this.headMixButton = new components.Component({
@@ -411,6 +421,8 @@ DDJ200.init = function() {
         DDJ200.headMixButton.shutdown();
         DDJ200.transFxButton.shutdown();
         DDJ200.decks.shutdown();
+        DDJ200.decks.left.padUnit.choosenPadInstance().shutdown();
+        DDJ200.decks.right.padUnit.choosenPadInstance().shutdown();
         engine.setValue("[Channel3]", "volume", 0);
         engine.setValue("[Channel4]", "volume", 0);
     };