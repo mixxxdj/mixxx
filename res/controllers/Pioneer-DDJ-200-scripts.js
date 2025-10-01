mixxxdj/mixxx

commit dcd6955af53146b1b112bcdc7085bc18c73c27bf

Pioneer DDJ-200: improved code readability
Co-authored-by: Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>

res/controllers/Pioneer-DDJ-200-scripts.js
@@ -581,10 +581,17 @@ DDJ200.Deck = function(deckNumbers, midiChannel) {
         });
     };
 
-    this.super1 = new components.Pot({
-        group: (engine.getSetting("useSuperAsGain"))?theDeck.currentDeck:`[QuickEffectRack1_${this.currentDeck}]`,
-        inKey: (engine.getSetting("useSuperAsGain"))?"pregain":"super1",
-    });
+    if (engine.getSetting("useSuperAsGain")) {
+        this.super1 = new components.Pot({
+            group: theDeck.currentDeck,
+            inKey: "pregain",
+        });
+    } else {
+        this.super1 = new components.Pot({
+            group: `[QuickEffectRack1_${this.currentDeck}]`;
+            inKey: "super1",
+        });
+    }
 
     this.reconnectComponents(function(c) {
         if (c.group === undefined) {

commit 0917d06baeafb6e0314d989e6974943280aff7b9

Pioneer DDJ-200: bugfix for code typo introduced in dcd6955af53146b1b

res/controllers/Pioneer-DDJ-200-scripts.js
@@ -592,7 +592,7 @@ DDJ200.Deck = function(deckNumbers, midiChannel) {
         });
     } else {
         this.super1 = new components.Pot({
-            group: `[QuickEffectRack1_${this.currentDeck}]`;
+            group: `[QuickEffectRack1_${this.currentDeck}]`,
             inKey: "super1",
         });
     }