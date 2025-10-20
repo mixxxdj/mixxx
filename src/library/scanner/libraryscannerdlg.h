65e71ae2109b5934762d34864e7a641bcbd2fcae
ronso0 <ronso0@mixxx.org>
2025-10-20 15:56:16 +0200

    Library scan dialog: add progress bar, improve display of current task

--- src/library/scanner/libraryscannerdlg.h
+++ src/library/scanner/libraryscannerdlg.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <QDialog>
+#include <QLabel>
+#include <QProgressBar>
 
+#include "util/parented_ptr.h"
 #include "util/performancetimer.h"
 
 class LibraryScannerDlg : public QDialog {
     Q_OBJECT
   public:
     LibraryScannerDlg(QWidget* pParent = nullptr);
+
+    void resetTaskCount();
+    void addQueuedTasks(int num);
 
   public slots:
     void slotUpdate(const QString& path);
@@ -18,9 +24,17 @@ class LibraryScannerDlg : public QDialog {
 
   signals:
     void scanCancelled();
-    void progress(const QString&);
 
   private:
+    void updateProgressBar();
+
     PerformanceTimer m_timer;
+
+    parented_ptr<QLabel> m_pLabelCurrent;
+    parented_ptr<QProgressBar> m_pProgressBar;
+
     bool m_bCancelled;
+    int m_tasksDone;
+    int m_tasksTotal;
+    bool m_showNoTasksQueuedWarning;
 };
