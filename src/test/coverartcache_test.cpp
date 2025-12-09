28434906b1feeb719372e2f3e55d12d3b8d83a5b
ronso0 <ronso0@mixxx.org>
2025-12-09 13:38:26 +0100

    SoundSource test: add non-latin chars to test file names

--- src/test/coverartcache_test.cpp
+++ src/test/coverartcache_test.cpp
@@ -55,7 +55,7 @@ class CoverArtCacheTest : public LibraryTest, public CoverArtCache {
 
 const QString kCoverFileTest = QStringLiteral("cover_test.jpg");
 const QString kCoverLocationTest = QStringLiteral("id3-test-data/") + kCoverFileTest;
-const QString kTrackLocationTest = QStringLiteral("id3-test-data/cover-test-png.mp3");
+const QString kTrackLocationTest = QStringLiteral("id3-test-data/cover-test-øé~ł€˚-png.mp3");
 
 // We need 3 separate test cases:
 // 1) loadCoverFromMetadata()
