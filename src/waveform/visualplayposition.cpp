c8e35f8cb5b1965876bf81e1587ae5afd423da04
Daniel Schürmann <daschuer@mixxx.org>
2026-01-22 07:22:09 +0100

    Replace ControlValueAtomic with a DelayRing that allows to access older values.

--- src/waveform/visualplayposition.cpp
+++ src/waveform/visualplayposition.cpp
@@ -53,8 +53,7 @@ void VisualPlayPosition::set(
     data.m_tempoTrackSeconds = tempoTrackSeconds;
     data.m_audioBufferMicroS = audioBufferMicroS;
 
-    // Atomic write
-    m_data.setValue(data);
+    m_data.push(data);
     m_valid.store(true);
 }
 
@@ -158,7 +157,7 @@ void VisualPlayPosition::getPlaySlipAtNextVSync(
         double* pPlayPosition,
         double* pSlipPosition) {
     if (m_valid.load()) {
-        const VisualPlayPositionData data = m_data.getValue();
+        const VisualPlayPositionData data = m_data.getAt(0);
         const double offsetSteps = calcOffsetAtNextVSync(pSyncTimeProvider, data);
 
         double interpolatedPlayPos = determinePlayPosInLoopBoundries(data, offsetSteps);
@@ -174,7 +173,7 @@ void VisualPlayPosition::getPlaySlipAtNextVSync(
 
 double VisualPlayPosition::getEnginePlayPos() {
     if (m_valid.load()) {
-        VisualPlayPositionData data = m_data.getValue();
+        VisualPlayPositionData data = m_data.getAt(0);
         return data.m_playPos;
     } else {
         return -1;
@@ -183,7 +182,7 @@ double VisualPlayPosition::getEnginePlayPos() {
 
 void VisualPlayPosition::getTrackTime(double* pPlayPosition, double* pTempoTrackSeconds) {
     if (m_valid.load()) {
-        VisualPlayPositionData data = m_data.getValue();
+        VisualPlayPositionData data = m_data.getAt(0);
         *pPlayPosition = data.m_playPos;
         *pTempoTrackSeconds = data.m_tempoTrackSeconds;
     } else {
