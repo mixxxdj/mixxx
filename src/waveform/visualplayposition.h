981f93ec381018e8f8c23748a288feff76ec9a85
Daniel Schürmann <daschuer@mixxx.org>
2026-01-22 08:33:25 +0100

    replace the boolean valid flag with delay ring level tracking.

--- src/waveform/visualplayposition.h
+++ src/waveform/visualplayposition.h
@@ -85,17 +85,13 @@ class VisualPlayPosition : public QObject {
     static void setCallbackEntryToDacSecs(double secs, const PerformanceTimer& time);
 
     void setInvalid() {
-        m_valid.store(false);
+        m_data.reset();
     };
-    bool isValid() const {
-        return m_valid.load();
-    }
 
   private:
     double calcOffsetAtNextVSync(VSyncTimeProvider* pSyncTimeProvider,
             const VisualPlayPositionData& data);
     DelayRing<VisualPlayPositionData, 16> m_data;
-    std::atomic<bool> m_valid;
     QString m_key;
     bool m_noTransport;
 
