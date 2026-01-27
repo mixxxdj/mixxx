fac884f84c83653e5f1b7e285c8d06c73599ad96
Daniel Fernandes <57069381+danferns@users.noreply.github.com>
2026-01-27 13:32:56 +0530

    Use C++ static_cast
    Co-authored-by: Daniel Schürmann <daschuer@mixxx.org>

--- src/library/basetracktablemodel.cpp
+++ src/library/basetracktablemodel.cpp
@@ -937,9 +937,9 @@ QVariant BaseTrackTableModel::roleValue(
                 return QVariant();
             }
 
-            const float tuningHz = (float)rawSiblingValue(
+            const float tuningHz =  static_cast<float>(rawSiblingValue(
                     index, ColumnCache::COLUMN_LIBRARYTABLE_TUNING_FREQUENCY)
-                                           .value<double>();
+                                           .value<double>());
             QVariantMap colorRect;
             if (tuningHz == 0) { // we do not have tuning information
                 colorRect["top"] = KeyUtils::keyToColor(key, s_keyColorPalette.value());
