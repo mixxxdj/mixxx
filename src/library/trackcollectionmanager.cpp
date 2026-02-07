21eede029631023fd481e2babfaa095f3453b89e
Daniel Schürmann <daschuer@mixxx.org>
2026-02-07 17:13:43 +0100

    Remove watched directory before removing the scanner.

--- src/library/trackcollectionmanager.cpp
+++ src/library/trackcollectionmanager.cpp
@@ -139,6 +139,10 @@ TrackCollectionManager::TrackCollectionManager(
 }
 
 TrackCollectionManager::~TrackCollectionManager() {
+    QStringList dirs = m_incomingDirWatcher.directories();
+    if (!dirs.isEmpty()) {
+        m_incomingDirWatcher.removePaths(dirs);
+    }
     if (m_pScanner) {
         while (m_pScanner->isRunning()) {
             kLogger.info() << "Stopping library scanner thread";
