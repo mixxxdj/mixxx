3290d23b8fd5de8c03548692c2a5019fbdf78f1e
Milkii Brewster <git@milkmiruku.com>
2026-02-18 12:22:03 +0000

    rename 'Stacked (RGB)' to 'Stacked' in overview type combobox
    Only one stacked variant remains so the RGB qualifier is redundant.

--- src/preferences/dialog/dlgprefwaveform.cpp
+++ src/preferences/dialog/dlgprefwaveform.cpp
@@ -41,7 +41,7 @@ DlgPrefWaveform::DlgPrefWaveform(
     waveformOverviewComboBox->addItem(tr("HSV"), QVariant::fromValue(OverviewType::HSV));
     waveformOverviewComboBox->addItem(tr("RGB"), QVariant::fromValue(OverviewType::RGB));
     waveformOverviewComboBox->addItem(
-            tr("Stacked (RGB)"), QVariant::fromValue(OverviewType::StackedRGB));
+            tr("Stacked"), QVariant::fromValue(OverviewType::StackedRGB));
     m_pTypeControl = std::make_unique<ControlPushButton>(kOverviewTypeCfgKey);
     m_pTypeControl->setStates(QMetaEnum::fromType<OverviewType>().keyCount());
     m_pTypeControl->setReadOnly();
