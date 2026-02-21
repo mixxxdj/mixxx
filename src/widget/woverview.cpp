9b62f8f596e74dcb038f1693bc2c5d12b026630b
Milkii Brewster <git@milkmiruku.com>
2026-02-21 00:02:03 +0000

    Add Simple overview waveform type

--- src/widget/woverview.cpp
+++ src/widget/woverview.cpp
@@ -1515,6 +1515,14 @@ bool WOverview::drawNextPixmapPart() {
                 nextCompletion,
                 m_signalColors,
                 !m_stereo);
+    } else if (m_type == OverviewType::Simple) {
+        waveformOverviewRenderer::drawWaveformPartSimple(
+                &painter,
+                pWaveform,
+                &m_actualCompletion,
+                nextCompletion,
+                m_signalColors,
+                !m_stereo);
     } else { // OverviewType::RGB:
         waveformOverviewRenderer::drawWaveformPartRGB(
                 &painter,
