ba6bde3ac5f2578c3bc5e435129118d33ff747d5
Milkii Brewster <git@milkmiruku.com>
2026-02-21 00:02:03 +0000

    Add Simple overview waveform type

--- src/preferences/dialog/dlgprefwaveform.cpp
+++ src/preferences/dialog/dlgprefwaveform.cpp
@@ -40,6 +40,7 @@ DlgPrefWaveform::DlgPrefWaveform(
             tr("Filtered"), QVariant::fromValue(OverviewType::Filtered));
     waveformOverviewComboBox->addItem(tr("HSV"), QVariant::fromValue(OverviewType::HSV));
     waveformOverviewComboBox->addItem(tr("RGB"), QVariant::fromValue(OverviewType::RGB));
+    waveformOverviewComboBox->addItem(tr("Simple"), QVariant::fromValue(OverviewType::Simple));
     m_pTypeControl = std::make_unique<ControlPushButton>(kOverviewTypeCfgKey);
     m_pTypeControl->setStates(QMetaEnum::fromType<OverviewType>().keyCount());
     m_pTypeControl->setReadOnly();
