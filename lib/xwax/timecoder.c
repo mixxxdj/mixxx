635246f4803ba84ee275835640636f2a0f8906f6
Jan Claußen <jan.claussen10@web.de>
2026-02-24 13:50:21 +0100

    xwax: Fix direction for SWITCH_POLARITY timecodes

--- lib/xwax/timecoder.c
+++ lib/xwax/timecoder.c
@@ -804,8 +804,11 @@ static void process_sample(struct timecoder *tc,
     else
         forwards = false;
 
-    if (tc->def->flags & SWITCH_PHASE)
+    if (tc->def->flags & SWITCH_PHASE) {
+        tc->pitch = -tc->pitch;
+        tc->freq = -tc->freq;
         forwards = !forwards;
+    }
 
     if (forwards != tc->forwards) { /* direction has changed */
         tc->forwards = forwards;
