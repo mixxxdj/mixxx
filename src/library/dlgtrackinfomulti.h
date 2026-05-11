10fb2385eb96c45a643aafa75e686f41eeabd7c2
ronso0 <ronso0@mixxx.org>
2026-05-11 15:37:25 +0200

    DlgTrackInfoMulti: add Find/Replace GUI for all string properties

--- src/library/dlgtrackinfomulti.h
+++ src/library/dlgtrackinfomulti.h
@@ -25,6 +25,19 @@ class DlgTrackInfoMulti : public QDialog, public Ui::DlgTrackInfoMulti {
     explicit DlgTrackInfoMulti(UserSettingsPointer pUserSettings);
     ~DlgTrackInfoMulti() override = default;
 
+    /// enum for editable QString track properties
+    enum class TrackProperty {
+        Invalid = 0,
+        Artist,
+        Title,
+        Album,
+        AlbumArtist,
+        Genre,
+        Grouping,
+        Composer,
+        Comment
+    };
+
     void loadTracks(const QList<TrackPointer>& pTracks);
     void focusField(const QString& property);
 
@@ -64,6 +77,8 @@ class DlgTrackInfoMulti : public QDialog, public Ui::DlgTrackInfoMulti {
     void slotCoverInfoSelected(const CoverInfoRelative& coverInfo);
     void slotReloadCoverArt();
 
+    void slotUpdateFindReplaceGUI();
+
     void slotOpenInFileBrowser();
 
   private:
@@ -99,6 +114,8 @@ class DlgTrackInfoMulti : public QDialog, public Ui::DlgTrackInfoMulti {
         }
         return trackIt.value();
     }
+
+    TrackProperty getSelectedFindReplacePropMaybeInvalid() const;
 
     const UserSettingsPointer m_pUserSettings;
 
