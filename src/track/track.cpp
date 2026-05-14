bdc4eeecce730444bd3a6dff6966b1c4583494a9
ronso0 <ronso0@mixxx.org>
2026-05-14 15:12:35 +0200

    update Loop cue immediately to trigger Waveform marker updates w/o CO connections

--- src/track/track.cpp
+++ src/track/track.cpp
@@ -1165,7 +1165,6 @@ void Track::removeCuesOfType(mixxx::CueType type) {
     QMutableListIterator<CuePointer> it(m_cuePoints);
     while (it.hasNext()) {
         CuePointer pCue = it.next();
-        // FIXME: Why does this only work for the Hotcue Type?
         if (pCue->getType() == type) {
             disconnect(pCue.get(), nullptr, this, nullptr);
             it.remove();
@@ -1180,6 +1179,25 @@ void Track::removeCuesOfType(mixxx::CueType type) {
         if (type == mixxx::CueType::Loop) {
             emit loopRemove();
         }
+        markDirtyAndUnlock(&locked);
+        emit cuesUpdated();
+    }
+}
+
+void Track::removeTempLoopCue() {
+    auto locked = lockMutex(&m_qMutex);
+    bool dirty = false;
+    QMutableListIterator<CuePointer> it(m_cuePoints);
+    while (it.hasNext()) {
+        CuePointer pCue = it.next();
+        if (pCue->getType() == mixxx::CueType::Loop && pCue->getHotCue() == Cue::kNoHotCue) {
+            disconnect(pCue.get(), nullptr, this, nullptr);
+            it.remove();
+            dirty = true;
+            break;
+        }
+    }
+    if (dirty) {
         markDirtyAndUnlock(&locked);
         emit cuesUpdated();
     }
