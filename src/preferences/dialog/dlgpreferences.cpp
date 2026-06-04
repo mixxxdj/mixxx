7b7fce17ec1c673f4c0e52bc4a9a1d13e6377ac1
Swarnadip Kar <swarnadip.kr@gmail.com>
2026-06-04 14:43:08 +0530

    Prefs: Register fingerprint page in DlgPreferences

--- src/preferences/dialog/dlgpreferences.cpp
+++ src/preferences/dialog/dlgpreferences.cpp
@@ -36,6 +36,7 @@
 #endif // __BROADCAST__
 
 #include "preferences/dialog/dlgprefbeats.h"
+#include "preferences/dialog/dlgpreffingerprint.h"
 #include "preferences/dialog/dlgprefkey.h"
 #include "preferences/dialog/dlgprefrecord.h"
 #include "preferences/dialog/dlgprefreplaygain.h"
@@ -229,6 +230,11 @@ DlgPreferences::DlgPreferences(
                           new QTreeWidgetItem(contentsTreeWidget, QTreeWidgetItem::Type)),
             tr("Key Detection"),
             "ic_preferences_keydetect.svg");
+    addPageWidget(PreferencesPage(
+                          new DlgPrefFingerprint(this, m_pConfig),
+                          new QTreeWidgetItem(contentsTreeWidget, QTreeWidgetItem::Type)),
+            tr("Fingerprint Analysis"),
+            "ic_preferences_bpmdetect.svg"); // TODO(CMRT): replace with dedicated fingerprint icon
     addPageWidget(PreferencesPage(
                           new DlgPrefReplayGain(this, m_pConfig),
                           new QTreeWidgetItem(contentsTreeWidget, QTreeWidgetItem::Type)),
