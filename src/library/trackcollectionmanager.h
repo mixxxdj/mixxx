7b75c05655d4f430c92d95995e6e61f32627aa40
Swarnadip Kar <swarnadip.kr@gmail.com>
2026-06-06 09:31:36 +0530

    Worker: Create and start AcoustIdWorker in TrackCollectionManager

--- src/library/trackcollectionmanager.h
+++ src/library/trackcollectionmanager.h
@@ -6,6 +6,7 @@
 #include <memory>
 
 #include "library/dao/directorydao.h"
+#include "musicbrainz/acoustidworker.h"
 #include "preferences/usersettings.h"
 #include "track/globaltrackcache.h"
 #include "util/db/dbconnectionpool.h"
@@ -139,4 +140,6 @@ class TrackCollectionManager: public QObject,
 
     // TODO: Extract and decouple LibraryScanner from TrackCollectionManager
     std::unique_ptr<LibraryScanner> m_pScanner;
+
+    std::unique_ptr<mixxx::AcoustIdWorker> m_pAcoustIdWorker;
 };
