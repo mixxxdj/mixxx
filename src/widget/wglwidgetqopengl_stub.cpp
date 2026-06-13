711c539d320e2e305667b4d0a4a871f088443a52
winnerspiros <winnerspiros@users.noreply.github.com>
2026-06-13 17:30:13 +0000

    fix: gersemi indentation + add WGLWidget stub for Android linker
    - Fix gersemi formatting: #if/#endif indented with 2 spaces
    - Add wglwidgetqopengl_stub.cpp with empty implementations of WGLWidget
      virtual methods (paintGL, resizeGL, initializeGL, etc.) for Android
      where QOPENGL=OFF and the real wglwidgetqopengl.cpp is not compiled

---
+++ src/widget/wglwidgetqopengl_stub.cpp
@@ -0,0 +1,66 @@
+// Stub implementations for WGLWidget on Android (QOPENGL=OFF)
+// This file provides empty implementations for the virtual methods declared
+// in wglwidgetqopengl.h so the linker can resolve symbols.
+#include "widget/wglwidget.h"
+
+WGLWidget::WGLWidget(QWidget* parent)
+        : QWidget(parent),
+          m_pOpenGLWindow(nullptr),
+          m_pContainerWidget(nullptr),
+          m_pTrackDropTarget(nullptr) {
+}
+
+WGLWidget::~WGLWidget() {
+}
+
+bool WGLWidget::isContextValid() const {
+    return false;
+}
+
+bool WGLWidget::shouldRender() const {
+    return false;
+}
+
+void WGLWidget::makeCurrentIfNeeded() {
+}
+
+void WGLWidget::doneCurrent() {
+}
+
+void WGLWidget::swapBuffers() {
+}
+
+void WGLWidget::paintGL() {
+}
+
+void WGLWidget::resizeGL(int w, int h) {
+    Q_UNUSED(w);
+    Q_UNUSED(h);
+}
+
+void WGLWidget::initializeGL() {
+}
+
+void WGLWidget::setTrackDropTarget(TrackDropTarget* pTarget) {
+    m_pTrackDropTarget = pTarget;
+}
+
+TrackDropTarget* WGLWidget::trackDropTarget() const {
+    return m_pTrackDropTarget;
+}
+
+QOpenGLWindow* WGLWidget::getOpenGLWindow() const {
+    return m_pOpenGLWindow;
+}
+
+void WGLWidget::showEvent(QShowEvent* event) {
+    QWidget::showEvent(event);
+}
+
+void WGLWidget::resizeEvent(QResizeEvent* event) {
+    QWidget::resizeEvent(event);
+}
+
+QPaintDevice* WGLWidget::paintDevice() {
+    return nullptr;
+}
