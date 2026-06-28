7fb08661b9e820f6b48cf6343ddc8158f4e3df30
Ayush Sah <75406648+xARSENICx@users.noreply.github.com>
2026-06-28 20:08:08 +0530

    fix(qml): create skin controls from QML

---
+++ src/qml/qmlskincontrolcreator.cpp
@@ -0,0 +1,124 @@
+#include "qml/qmlskincontrolcreator.h"
+
+#include "moc_qmlskincontrolcreator.cpp"
+#include "util/assert.h"
+
+namespace mixxx {
+namespace qml {
+
+QmlSkinControlCreator::QmlSkinControlCreator(QObject* parent)
+        : QObject(parent),
+          m_persist(false),
+          m_defaultValue(0.0),
+          m_buttonMode(ButtonMode::Toggle),
+          m_isComponentComplete(false) {
+}
+
+void QmlSkinControlCreator::componentComplete() {
+    m_isComponentComplete = true;
+    createControl();
+}
+
+void QmlSkinControlCreator::setGroup(const QString& group) {
+    if (m_key.group == group) {
+        return;
+    }
+    m_key.group = group;
+    emit groupChanged(group);
+    createControl();
+}
+
+const QString& QmlSkinControlCreator::getGroup() const {
+    return m_key.group;
+}
+
+void QmlSkinControlCreator::setKey(const QString& key) {
+    if (m_key.item == key) {
+        return;
+    }
+    m_key.item = key;
+    emit keyChanged(key);
+    createControl();
+}
+
+const QString& QmlSkinControlCreator::getKey() const {
+    return m_key.item;
+}
+
+void QmlSkinControlCreator::setPersist(bool persist) {
+    if (m_persist == persist) {
+        return;
+    }
+    m_persist = persist;
+    emit persistChanged(persist);
+    createControl();
+}
+
+bool QmlSkinControlCreator::getPersist() const {
+    return m_persist;
+}
+
+void QmlSkinControlCreator::setDefaultValue(double defaultValue) {
+    if (m_defaultValue == defaultValue) {
+        return;
+    }
+    m_defaultValue = defaultValue;
+    emit defaultValueChanged(defaultValue);
+    createControl();
+}
+
+double QmlSkinControlCreator::getDefaultValue() const {
+    return m_defaultValue;
+}
+
+void QmlSkinControlCreator::setButtonMode(ButtonMode buttonMode) {
+    if (m_buttonMode == buttonMode) {
+        return;
+    }
+    m_buttonMode = buttonMode;
+    emit buttonModeChanged(buttonMode);
+    if (m_pControl) {
+        m_pControl->setButtonMode(toControlButtonMode(m_buttonMode));
+    }
+}
+
+QmlSkinControlCreator::ButtonMode QmlSkinControlCreator::getButtonMode() const {
+    return m_buttonMode;
+}
+
+mixxx::control::ButtonMode QmlSkinControlCreator::toControlButtonMode(
+        ButtonMode buttonMode) {
+    switch (buttonMode) {
+    case ButtonMode::Push:
+        return mixxx::control::ButtonMode::Push;
+    case ButtonMode::Toggle:
+        return mixxx::control::ButtonMode::Toggle;
+    case ButtonMode::PowerWindow:
+        return mixxx::control::ButtonMode::PowerWindow;
+    case ButtonMode::LongPressLatching:
+        return mixxx::control::ButtonMode::LongPressLatching;
+    case ButtonMode::Trigger:
+        return mixxx::control::ButtonMode::Trigger;
+    }
+    DEBUG_ASSERT(false);
+    return mixxx::control::ButtonMode::Toggle;
+}
+
+void QmlSkinControlCreator::createControl() {
+    if (!m_isComponentComplete) {
+        return;
+    }
+    if (!m_key.isValid()) {
+        m_pControl.reset();
+        return;
+    }
+
+    m_pControl = std::make_unique<ControlPushButton>(
+            m_key,
+            m_persist,
+            m_defaultValue);
+    m_pControl->setButtonMode(toControlButtonMode(m_buttonMode));
+}
+
+} // namespace qml
+} // namespace mixxx
--- src/skin/skincontrols.cpp
+++ src/skin/skincontrols.cpp
@@ -51,74 +51,6 @@ SkinControls::SkinControls()
           m_showVinylControl(
                   ConfigKey(kSkinGroup, QStringLiteral("show_vinylcontrol")),
                   true,
-                  false),
-          m_showWaveforms(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_waveforms")),
-                  true,
-                  true),
-          m_showHotcues(ConfigKey(kSkinGroup, QStringLiteral("show_hotcues")),
-                  true,
-                  true),
-          m_show8Hotcues(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_8_hotcues")),
-                  true,
-                  true),
-          m_showIntroOutroCues(ConfigKey(kSkinGroup,
-                                       QStringLiteral("show_intro_outro_cues")),
-                  true,
-                  true),
-          m_showLoopControls(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_loop_controls")),
-                  true,
-                  true),
-          m_showBeatjumpControls(
-                  ConfigKey(
-                          kSkinGroup, QStringLiteral("show_beatjump_controls")),
-                  true,
-                  true),
-          m_showRateControls(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_rate_controls")),
-                  true,
-                  true),
-          m_showRateControlButtons(
-                  ConfigKey(kSkinGroup,
-                          QStringLiteral("show_rate_control_buttons")),
-                  true,
-                  true),
-          m_showKeyControls(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_key_controls")),
-                  true,
-                  true),
-          m_showEqKnobs(ConfigKey(kSkinGroup, QStringLiteral("show_eq_knobs")),
-                  true,
-                  true),
-          m_showEqKillButtons(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_eq_kill_buttons")),
-                  true,
-                  true),
-          m_showXfader(ConfigKey(kSkinGroup, QStringLiteral("show_xfader")),
-                  true,
-                  true),
-          m_showMainHeadMixer(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_main_head_mixer")),
-                  true,
-                  true),
-          m_equal4deckWaveforms(
-                  ConfigKey(
-                          kSkinGroup, QStringLiteral("equal_4deck_waveforms")),
-                  true,
-                  false),
-          m_timingShiftButtons(
-                  ConfigKey(kSkinGroup, QStringLiteral("timing_shift_buttons")),
-                  true,
-                  false),
-          m_showSuperKnobs(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_superknobs")),
-                  true,
-                  false),
-          m_showSamplerFx(
-                  ConfigKey(kSkinGroup, QStringLiteral("show_sampler_fx")),
-                  true,
                   false) {
     m_showEffectRack.setButtonMode(mixxx::control::ButtonMode::Toggle);
     m_showLibraryCoverArt.setButtonMode(mixxx::control::ButtonMode::Toggle);
@@ -132,23 +64,6 @@ SkinControls::SkinControls()
     m_showSettings.setButtonMode(mixxx::control::ButtonMode::Toggle);
     m_showSpinnies.setButtonMode(mixxx::control::ButtonMode::Toggle);
     m_showVinylControl.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showWaveforms.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showHotcues.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_show8Hotcues.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showIntroOutroCues.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showLoopControls.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showBeatjumpControls.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showRateControls.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showRateControlButtons.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showKeyControls.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showEqKnobs.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showEqKillButtons.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showXfader.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showMainHeadMixer.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_equal4deckWaveforms.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_timingShiftButtons.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showSuperKnobs.setButtonMode(mixxx::control::ButtonMode::Toggle);
-    m_showSamplerFx.setButtonMode(mixxx::control::ButtonMode::Toggle);
 
     m_showEffectRack.addAlias(ConfigKey(QStringLiteral("[EffectRack1]"), QStringLiteral("show")));
     m_showLibraryCoverArt.addAlias(ConfigKey(
