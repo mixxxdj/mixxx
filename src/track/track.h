aa507d0d53607e900709a820b49458d4335b74aa
Swarnadip Kar <swarnadip.kr@gmail.com>
2026-07-11 11:38:04 +0530

    Track/WTrackMenu: Add Copy/Add CMRT Hotcues to the Hotcues submenu

--- src/track/track.h
+++ src/track/track.h
@@ -476,6 +476,10 @@ class Track : public QObject {
     TrackPointer getCanonicalTrack() const;
     double getCmrtOffsetSeconds() const;
 
+    void copyOrMergeCmrtHotcues(TrackPointer pCanonical,
+            double offsetSeconds,
+            bool replaceExisting);
+
   signals:
     void artistChanged(const QString&);
     void titleChanged(const QString&);
