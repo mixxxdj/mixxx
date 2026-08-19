078b27d9b7bc9a6beb0bb889c1288daa94d60e1f
ronso0 <ronso0@mixxx.org>
2026-08-20 01:16:18 +0200

    remove stray comma, fix updateNotPlayed query

--- src/library/dao/trackdao.cpp
+++ src/library/dao/trackdao.cpp
@@ -2361,7 +2361,7 @@ bool TrackDAO::updatePlayCounterFromPlayedHistory(
                 m_database,
                 QStringLiteral(
                         "UPDATE library SET "
-                        "timesplayed=0,"
+                        "timesplayed=0 "
                         "WHERE id NOT IN("
                         "SELECT PlaylistTracks.track_id "
                         "FROM PlaylistTracks "
