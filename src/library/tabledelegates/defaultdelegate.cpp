ce31e749dbaded49ff70b31ce9b88e2625114f02
ronso0 <ronso0@mixxx.org>
2026-09-08 17:55:27 +0200

    Table delegate: move paintItemBackground() to DefaultDelegate
    works around a Qt6 bug on Wayland where opacity of Qt::BackgroundRole is ignored
    https://github.com/mixxxdj/mixxx/issues/17037

--- src/library/tabledelegates/defaultdelegate.cpp
+++ src/library/tabledelegates/defaultdelegate.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 
 #include "moc_defaultdelegate.cpp"
+#include "util/assert.h"
 
 DefaultDelegate::DefaultDelegate(QTableView* pTableView)
         : QStyledItemDelegate(pTableView) {
@@ -15,12 +16,39 @@ void DefaultDelegate::paint(
     QStyleOptionViewItem opt = option;
     initStyleOption(&opt, index);
 
+    // Workaround for a Qt6 bug occurring on Wayland (maybe also with other OS or
+    // compositors):
+    // Paint background color from model if available and not selected to
+    // ensure the alpha channel is respected.
+    paintItemBackground(painter, opt, index);
+    // Clear the background brush so QStyledItemDelegate doesn't paint it again
+    opt.backgroundBrush = QBrush();
+
     if (opt.state & QStyle::State_Selected) {
         setHighlightedTextColor(opt, index);
     }
     // TODO Guarantee font/bg contrast with ALL track colors
 
     QStyledItemDelegate::paint(painter, opt, index);
+}
+
+void DefaultDelegate::paintItemBackground(
+        QPainter* painter,
+        const QStyleOptionViewItem& option,
+        const QModelIndex& index) {
+    // If the row is not selected, paint the desired background color before
+    // painting the delegate item
+    if (option.showDecorationSelected &&
+            (option.state & QStyle::State_Selected)) {
+        return;
+    }
+    QVariant bgValue = index.data(Qt::BackgroundRole);
+    if (!bgValue.isValid()) {
+        return;
+    }
+    DEBUG_ASSERT(bgValue.canConvert<QBrush>());
+    const auto bgBrush = qvariant_cast<QBrush>(bgValue);
+    painter->fillRect(option.rect, bgBrush);
 }
 
 void DefaultDelegate::setHighlightedTextColor(
